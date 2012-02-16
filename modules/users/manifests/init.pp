3bf6d2254f319cabb19a955fe6a4df73fbe73c81
Alister Bulman <abulman@gmail.com>
2012-02-16 20:59:20 +0000

    add users::opg manifest

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -226,7 +226,6 @@ class users::govuk {
 
 
 class users::freerange {
-  include users::setup
   user { "tomw":
     comment => "Tom Ward (tom.ward@gofreerange.co.uk)",
     ensure     => present,
@@ -370,5 +369,64 @@ class users::other {
     type    => "ssh-rsa",
     user    => "deploy",
     require => User["deploy"];
+  }
+}
+
+# Office of the public guardian, LPA project
+class users::opg {
+  # How to remove a user:
+  #user { "johndoefake":
+  #  ensure     => absent
+  #}
+
+  user { "alister":
+    comment => "Alister Bulman (alister.bulman@betransformative.com)",
+    ensure     => present,
+    home       => "/home/alister",
+    managehome => true,
+    groups     => ["admin", "deploy"],
+    require    => Class["users::setup"],
+    shell      => '/bin/bash'
+  }
+  ssh_authorized_key { "alister_key1":
+    ensure  => present,
+    key     => extlookup("alister_key"),
+    type    => "ssh-rsa",
+    user    => "alister",
+    require => User["alister"]
+  }
+
+  user { "chrismo2012":
+    comment => "Chris Moreton (chris.moreton@betransformative.com)",
+    ensure     => present,
+    home       => "/home/chrismo2012",
+    managehome => true,
+    groups     => ["admin", "deploy"],
+    require    => Class["users::setup"],
+    shell      => '/bin/bash'
+  }
+  ssh_authorized_key { "chrismo2012_key1":
+    ensure  => present,
+    key     => extlookup("chrismo2012_key"),
+    type    => "ssh-rsa",
+    user    => "chrismo2012",
+    require => User["chrismo2012"]
+  }
+
+  user { "jamie":
+    comment => "jamie (Jamie.Burns@betransformative.com)",
+    ensure     => present,
+    home       => "/home/jamie",
+    managehome => true,
+    groups     => ["admin", "deploy"],
+    require    => Class["users::setup"],
+    shell      => '/bin/bash'
+  }
+  ssh_authorized_key { "jamie_key1":
+    ensure  => present,
+    key     => extlookup("jamie_key"),
+    type    => "ssh-rsa",
+    user    => "garethr",
+    require => User["jamie"]
   }
 }
