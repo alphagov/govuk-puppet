59d52af848ffe3792f6259c9c600ab6212aed4ec
James Hughes <james@yobriefca.se>
2012-02-20 17:36:02 +0000

    added frontend and api nodes

--- manifests/ertp_classes.pp
+++ manifests/ertp_classes.pp
@@ -1,6 +1,7 @@
 class ertp_base {
 }
 
+# ERTP MongoServer Configuration
 class ertp_mongo_server inherits ertp_base {
   include mongodb::server
   class {'mongodb::configure_replica_set':
@@ -8,12 +9,18 @@ class ertp_mongo_server inherits ertp_base {
   }
 }
 
+# ERTP Front End Server Configuration
 class ertp_frontend_server inherits ertp_base {
+  include jetty
+  include nginx
 }
 
+# ERTP API Server Configuration
 class ertp_api_server inherits ertp_base {
+  include jetty
 }
 
+# ERTP Development Configuration
 class ertp_development inherits ertp_base {
   include ertp_mongo_server
   include ertp_api_server
