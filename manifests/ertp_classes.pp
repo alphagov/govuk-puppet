d99e6f8aaf97231d15c404237baf0f93d9b63c99
James Hughes <james@yobriefca.se>
2012-02-20 16:27:39 +0000

    added ertp classes for boxes

--- manifests/ertp_classes.pp
+++ manifests/ertp_classes.pp
@@ -8,6 +8,14 @@ class ertp_mongo_server inherits ertp_base {
   }
 }
 
+class ertp_frontend_server inherits ertp_base {
+}
+
+class ertp_api_server inherits ertp_base {
+}
+
 class ertp_development inherits ertp_base {
   include ertp_mongo_server
+  include ertp_api_server
+  include ertp_frontend_server
 }
