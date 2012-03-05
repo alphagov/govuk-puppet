ee93f95cf63615743e1b7742f4fc4a1d35626fe8
Gareth Rushgrove <gareth@morethanseven.net>
2012-03-05 21:51:56 +0000

    remove the classes part of file names now in a directory

---
+++ manifests/classes/development.pp
@@ -0,0 +1,32 @@
+class development {
+  include base_packages::unix_tools
+  include govuk::testing_tools
+  include nginx::development
+  include mongodb::server
+  include apollo
+  include hosts
+  include solr
+  include apt
+
+  $mysql_password = ""
+  include mysql::server
+  include mysql::client
+
+  mysql::server::db {
+    "fco_development":          user => "fco",          password => "",           host => "localhost";
+    "needotron_development":    user => "needotron",    password => "",           host => "localhost";
+    "panopticon_development":   user => "panopticon",   password => "panopticon", host => "localhost";
+    "panopticon_test":          user => "panopticon",   password => "panopticon", host => "localhost";
+    "contactotron_development": user => "contactotron", password => "",           host => "localhost";
+  }
+
+  package {
+    "bundler":      provider => gem, ensure => "installed";
+    "foreman":      provider => gem, ensure => "installed";
+    "mysql2":       provider => gem, ensure => "installed", require => Class["mysql::client"];
+    "ruby-debug19": provider => gem, ensure => "installed", require => Package["build-essential"];
+    "rails":        provider => gem, ensure => "installed";
+    "passenger":    provider => gem, ensure => "installed";
+    "apache2":                       ensure => "absent";
+  }
+}
