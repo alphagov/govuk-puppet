528e89914df982eb647bbec4c659d4496a32d73e
Gareth Rushgrove <gareth@morethanseven.net>
2012-03-12 14:53:31 +0000

    change extdata path for cap deploys

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,7 @@ Exec {
   path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
 }
 
-$extlookup_datadir = "/etc/puppet/extdata"
+$extlookup_datadir = "/usr/share/puppet/production/current/extdata"
 $extlookup_precedence = ["%{fqdn}", "domain_%{domain}", "common"]
 
 import "modules.pp"
