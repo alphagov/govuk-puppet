057f59fdd845400740d71776716b507ddaa9d9e6
Gareth Rushgrove <gareth.rushgrove@digital.cabinet-office.gov.uk>
2012-03-16 07:46:28 -0700

    puppet lint cleanup for varnish module

---
+++ modules/varnish/manifests/install.pp
@@ -0,0 +1,20 @@
+class varnish::install {
+  package { 'varnish':
+    ensure => installed
+  }
+  file { '/etc/default/varnish':
+    ensure  => file,
+    source  => 'puppet:///modules/varnish/defaults',
+    require => Package['varnish']
+  }
+  file { '/etc/default/varnishncsa':
+    ensure  => file,
+    source  => 'puppet:///modules/varnish/ncsa-defaults',
+    require => Package['varnish']
+  }
+  file { '/etc/varnish/default.vcl':
+    ensure  => file,
+    content => template('varnish/default.vcl.erb'),
+    require => Package['varnish'],
+  }
+}
