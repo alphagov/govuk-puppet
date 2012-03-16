f3160fefae3ebbd16e941cac777f465ac5bbd766
Gareth Rushgrove <gareth.rushgrove@digital.cabinet-office.gov.uk>
2012-03-16 09:12:15 -0700

    tidy up mongodb module as per puppet-lint

--- modules/mongodb/manifests/init.pp
+++ modules/mongodb/manifests/init.pp
@@ -7,108 +7,108 @@ class mongodb::server {
 
 class mongodb::signing_key {
   include apt
-  apt::deb_key { "7F0CEB10": }
+  apt::deb_key { '7F0CEB10': }
 }
 
 class mongodb::repository {
   include apt
   include mongodb::signing_key
-  apt::deb_repository { "10gen":
-    url     => "http://downloads-distro.mongodb.org/repo/ubuntu-upstart",
-    dist    => "dist",
-    repo    => "10gen",
-    require => Class["mongodb::signing_key"],
+  apt::deb_repository { '10gen':
+    url     => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart',
+    dist    => 'dist',
+    repo    => '10gen',
+    require => Class['mongodb::signing_key'],
   }
 }
 
 class mongodb::package {
   include mongodb::repository
 
-  package { "mongodb-10gen":
+  package { 'mongodb-10gen':
     ensure  => installed,
-    require => Class["mongodb::repository"],
+    require => Class['mongodb::repository'],
   }
 }
 
 class mongodb::configuration {
-  file { "/etc/mongodb.conf":
+  file { '/etc/mongodb.conf':
     ensure  => present,
-    content  => template('mongodb/mongodb.conf'),
+    content => template('mongodb/mongodb.conf'),
     owner   => 'root',
     group   => 'root',
-    mode    => '644',
+    mode    => '0644',
     require => Package['mongodb-10gen'],
     notify  => Service['mongodb'],
   }
 
-  file { "/var/log/mongodb/mongod.log":
+  file { '/var/log/mongodb/mongod.log':
     ensure  => present,
     owner   => 'mongodb',
     group   => 'mongodb',
     require => Package['mongodb-10gen'],
-    mode    => '644',
+    mode    => '0644',
   }
-  
-  file { "/etc/init/mongodb.conf":
+
+  file { '/etc/init/mongodb.conf':
     ensure  => present,
-    content  => template('mongodb/upstart-standalone.conf'),
+    content => template('mongodb/upstart-standalone.conf'),
     owner   => 'root',
     group   => 'root',
-    mode    => '644',
+    mode    => '0644',
     require => Package['mongodb-10gen'],
     notify  => Service['mongodb'],
   }
 }
 
 class mongodb::service {
-  service { "mongodb":
-    enable     => true,
+  service { 'mongodb':
     ensure     => running,
+    enable     => true,
     hasrestart => true,
     hasstatus  => true,
-    require => [
-      Package["mongodb-10gen"],
-      File["/etc/mongodb.conf"],
-      File["/var/log/mongodb/mongod.log"],
-      File["/etc/init/mongodb.conf"],
+    require    => [
+      Package['mongodb-10gen'],
+      File['/etc/mongodb.conf'],
+      File['/var/log/mongodb/mongod.log'],
+      File['/etc/init/mongodb.conf'],
     ],
   }
 }
 
 class mongodb::backup($members) {
-  file { "/etc/cron.daily/automongodbbackup-replicaset":
+  file { '/etc/cron.daily/automongodbbackup-replicaset':
     ensure  => present,
-    content  => template('mongodb/automongodbbackup'),
+    content => template('mongodb/automongodbbackup'),
     owner   => 'root',
     group   => 'root',
-    mode    => '744',
+    mode    => '0744',
     require => Package['mongodb-10gen'],
   }
 }
 
 class mongodb::configure_replica_set($members) {
-  file { "/etc/mongodb":
-    ensure => 'directory',
+  file { '/etc/mongodb':
+    ensure  => 'directory',
     owner   => 'root',
     group   => 'root',
-    mode    => '644',
+    mode    => '0644',
   }
 
-  file { "/etc/mongodb/configure-replica-set.js":
+  file { '/etc/mongodb/configure-replica-set.js':
     ensure  => present,
-    content  => template('mongodb/configure-replica-set.js'),
+    content => template('mongodb/configure-replica-set.js'),
     owner   => 'root',
     group   => 'root',
-    mode    => '644',
-    require => [File["/etc/mongodb"]],
+    mode    => '0644',
+    require => File['/etc/mongodb'],
   }
 
-  exec { "configure-replica-set":
+  exec { 'configure-replica-set':
     command => "/usr/bin/mongo --host ${members[0]} /etc/mongodb/configure-replica-set.js",
     unless  => "/usr/bin/mongo --host ${members[0]} --quiet --eval 'rs.status().ok' | grep -q 1",
     require => [
-      File["/etc/mongodb/configure-replica-set.js"],
-      Service["mongodb"],
+      File['/etc/mongodb/configure-replica-set.js'],
+      Service['mongodb'],
     ],
   }
 }
