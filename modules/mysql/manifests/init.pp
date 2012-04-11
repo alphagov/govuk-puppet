423cbb723a3b2928dac5e38a116a76841025a1fe
Gareth Rushgrove <gareth@morethanseven.net>
2012-04-11 12:24:48 +0200

    correctly quote the mysql command to avoid failed run

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -67,7 +67,7 @@ class mysql::server {
 
     exec { "grant-${name}-db":
       unless  => "/usr/bin/mysql -h ${host} -u${user} -p${password} ${name}",
-      command => "/usr/bin/mysql -h ${host} -uroot -p${::mysql_password} -e \'grant all on ${name}.* to ${user}@'${host}' identified by '$password'; flush privileges;\'",
+      command => "/usr/bin/mysql -h ${host} -uroot -p${::mysql_password} -e \"grant all on ${name}.* to ${user}@'${host}' identified by '$password'; flush privileges;\"",
       require => [
         Service['mysql'],
         Exec["create-${name}-db"]
