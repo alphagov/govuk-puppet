070702e890a263f9faef633f92a737472c92742f
Gareth Rushgrove <gareth.rushgrove@digital.cabinet-office.gov.uk>
2012-04-28 09:23:38 -0700

    avoid the need for the extconf for situations where it's not present (testing, development)

--- modules/mysql/manifests/backup.pp
+++ modules/mysql/manifests/backup.pp
@@ -1,7 +1,7 @@
 class mysql::backup {
   /* Automated RDS backup for loading into dev/preview boxen */
   $mysql_backup_username = 'backup'
-  $mysql_backup_password = extlookup('mysql_backup')
+  $mysql_backup_password = extlookup('mysql_backup','')
   $mysql_backup_host = 'rds.cluster'
   $mysql_backup_email = 'govuk-dev@digital.cabinet-office.gov.uk'
 
