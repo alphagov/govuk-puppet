87e037bd4e81c936f8a8ae9b0919744b7208801a
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-05-18 02:41:10 -0700

    Fix shamefully broken elasticsearch module.
    Change 'config' requirement to 'package'.
    Fix configuration file building.
    Use init.d rather than upstart.
    Fix variable usage in elasticsearch.yml template.

--- modules/elasticsearch/manifests/config.pp
+++ modules/elasticsearch/manifests/config.pp
@@ -2,20 +2,20 @@
 # manages elasticsearch's configfiles
 class elasticsearch::config($cluster) {
   file {'/etc/elasticsearch/elasticsearch.yml':
-    source  => 'puppet:///modules/elasticsearch/etc/elasticsearch/elasticsearch.yml',
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    require => Class['elasticsearch::install'],
-    notify  => Class['elasticsearch::service']
-  }
-
-  file {'/etc/default/elasticsearch':
     content => template("elasticsearch/elasticsearch.yml.erb"),
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
-    require => Class['elasticsearch::install'],
+    require => Class['elasticsearch::package'],
+    notify  => Class['elasticsearch::service']
+  }
+
+  file {'/etc/default/elasticsearch':
+    source  => 'puppet:///modules/elasticsearch/etc/default/elasticsearch',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => Class['elasticsearch::package'],
     notify  => Class['elasticsearch::service']
   }
 }
--- modules/elasticsearch/manifests/service.pp
+++ modules/elasticsearch/manifests/service.pp
@@ -1,7 +1,6 @@
 class elasticsearch::service {
   service{'elasticsearch':
     ensure   => running,
-    provider => 'upstart',
     require  => Class['elasticsearch::config']
   }
 }
