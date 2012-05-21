f7f0131a00f1e50f2ea38be469bed6b96203ab7f
Kushal Pisavadia <kushi.p@gmail.com>
2012-05-21 06:52:52 -0700

    Add config for RDS on preview support machines.
    This is for use by the replication script to restore a production
    snapshot to preview. Putting it in a .my.cnf file saves entering
    connection information into the replication script and instead makes
    it implicit.

--- modules/users/manifests/setup.pp
+++ modules/users/manifests/setup.pp
@@ -20,6 +20,24 @@ class users::setup {
     mode    => '0755',
     require => User['deploy'],
   }
+  if $::govuk_platform == 'preview' and $::govuk_class == 'support' {
+    $mysql_host = 'rds.cluster'
+    $mysql_user = 'backup'
+    /*  WARNING: this fallback is in place to keep the rspec build happy.
+        However, it will fail silently if the external data is not present,
+        for example if deployed with the wrong version of the CSV file.
+    */
+    $mysql_password = extlookup('mysql_preview_backup', '')
+
+    file { '/home/deploy/.my.cnf':
+      ensure  => file,
+      owner   => 'deploy',
+      group   => 'deploy',
+      mode    => '0600',
+      require => User['deploy'],
+      content => template('users/my.cnf')
+    }
+  }
   ssh_authorized_key {
     'deploy_key_minglis':
       ensure  => present,
