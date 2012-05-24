bce49606268261eebc810a491f87b66df30f7c65
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2012-05-24 20:49:07 +0100

    MOved ertp-api scripts to init.pp

--- modules/ertp/manifests/init.pp
+++ modules/ertp/manifests/init.pp
@@ -7,3 +7,10 @@ class places::scripts {
     source => ['puppet:///modules/ertp/places.conf']
   }
 }
+
+class ertp-api::scripts {
+  file {'/etc/init/ertp-api.conf':
+    ensure => present,
+    source => ['puppet:///modules/ertp/ertp-api.conf']
+  }
+}
