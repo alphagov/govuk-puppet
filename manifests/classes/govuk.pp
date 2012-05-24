3923498e8085e514b079edc64dfc0a262e62532a
James Abley <james.abley@gmail.com>
2012-05-24 12:37:26 +0100

    Initial changes for Trade Tariff front-end
    Front-end webapp - tariff.
    
    Note that I haven't introduced Varnish production configuration yet. I'm
    hoping to delay this to the last responsible moment and by that time,
    the router will have been deployed and that part of Varnish
    configuration will have gone away.

--- manifests/classes/govuk.pp
+++ manifests/classes/govuk.pp
@@ -173,6 +173,8 @@ class govuk_base::ruby_app_server::frontend_server inherits govuk_base::ruby_app
       to => ['localhost:8080'];
     "tariff-api.$::govuk_platform.alphagov.co.uk":
       to => ['localhost:8080'];
+    "tariff.$::govuk_platform.alphagov.co.uk":
+      to => ['localhost:8080'];
   }
 
   apache2::vhost::passenger {
@@ -192,6 +194,8 @@ class govuk_base::ruby_app_server::frontend_server inherits govuk_base::ruby_app
       additional_port => 8086;
     "tariff-api.$::govuk_platform.alphagov.co.uk":
       additional_port => 8087;
+    "tariff.$::govuk_platform.alphagov.co.uk":
+      additional_port => 8088;
     "static.$::govuk_platform.alphagov.co.uk":;
   }
 
@@ -204,7 +208,7 @@ class govuk_base::ruby_app_server::frontend_server inherits govuk_base::ruby_app
 
   nginx::vhost::static { "static.$::govuk_platform.alphagov.co.uk":
     protected => false,
-    aliases   => ['calendars', 'planner', 'smartanswers', 'static', 'frontend', 'designprinciples', 'licencefinder', 'tariff-api'],
+    aliases   => ['calendars', 'planner', 'smartanswers', 'static', 'frontend', 'designprinciples', 'licencefinder', 'tariff-api', 'tariff'],
     ssl_only  => true
   }
 }
