a7d28e101326034ea6e045cdc5e38f12c700bfa7
Martyn Inglis <martyninglis@minglis-gds-macbook.internal.localnet>
2012-06-07 17:19:35 +0100

    Oops sorry. Deleted this.

--- modules/ertp/manifests/init.pp
+++ modules/ertp/manifests/init.pp
@@ -1,10 +1,15 @@
 class ertp {
 }
 
-class places::scripts {
-  file {'/etc/init/places.conf':
+class ertp::scripts {
+  file {'/etc/init/ertp.conf':
     ensure => present,
-    source => ['puppet:///modules/ertp/places.conf']
+    source => ['puppet:///modules/ertp/ertp.conf']
+  }
+
+  file {'/etc/init/ertp-ems-admin.conf':
+    ensure => present,
+    source => ['puppet:///modules/ertp/ertp-ems-admin.conf']
   }
 }
 
@@ -12,5 +17,12 @@ class ertp-api::scripts {
   file {'/etc/init/ertp-api.conf':
     ensure => present,
     source => ['puppet:///modules/ertp/ertp-api.conf']
+  }
+}
+
+class places::scripts {
+  file {'/etc/init/places.conf':
+    ensure => present,
+    source => ['puppet:///modules/ertp/places.conf']
   }
 }
