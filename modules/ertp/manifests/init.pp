f8d6987377b2b57597dbcb737ed08aec91645d5f
Martyn Inglis <martyninglis@minglis-gds-macbook.internal.localnet>
2012-06-11 16:08:35 +0100

    Fixed puppet tests for ertp

--- modules/ertp/manifests/init.pp
+++ modules/ertp/manifests/init.pp
@@ -26,3 +26,43 @@ class places::scripts {
     source => ['puppet:///modules/ertp/places.conf']
   }
 }
+
+class ertp::config {
+  file { '/etc/gds-ertp-config.properties':
+    ensure => present,
+    source => [
+      "puppet:///modules/ertp/gds-ertp-config.properties.${::govuk_platform}",
+      'puppet:///modules/ertp/gds-ertp-config.properties'
+    ]
+  }
+}
+
+class ertp::dwp::api::config {
+  file { '/etc/gds-ertp-config.properties':
+    ensure => present,
+    source => [
+      "puppet:///modules/ertp/gds-ertp-dwp-api-config.properties.${::govuk_platform}",
+      'puppet:///modules/ertp/gds-ertp-dwp-api-config.properties'
+    ]
+  }
+}
+
+class ertp::ems::api::config {
+  file { '/etc/gds-ertp-config.properties':
+    ensure => present,
+    source => [
+      "puppet:///modules/ertp/gds-ertp-ems-api-config.properties.${::govuk_platform}",
+      'puppet:///modules/ertp/gds-ertp-ems-api-config.properties'
+    ]
+  }
+}
+
+class ertp::citizen::api::config {
+  file { '/etc/gds-ertp-config.properties':
+    ensure => present,
+    source => [
+      "puppet:///modules/ertp/gds-ertp-citizen-api-config.properties.${::govuk_platform}",
+      'puppet:///modules/ertp/gds-ertp-citizen-api-config.properties'
+    ]
+  }
+}
