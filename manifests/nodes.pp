2b21c1166fe3c3bb83596f5fba9898f80ad657d4
Martyn Inglis <martyninglis@minglis-gds-macbook.internal.localnet>
2012-06-12 16:13:08 +0100

    Renamed all ero mentions to ems

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -15,7 +15,7 @@ node default {
     ertp-mongo:         { include ertp_base::mongo_server }
     ertp-api:           { include ertp_base::api_server::all }
     ertp-api-citizen:   { include ertp_base::api_server::citizen }
-    ertp-api-ero:       { include ertp_base::api_server::ero }
+    ertp-api-ems:       { include ertp_base::api_server::ems }
     ertp-frontend:      { include ertp_base::frontend_server }
     ertp-development:   { include ertp_base::development }
     default:            { }
--- modules/hosts/manifests/ertp-staging.pp
+++ modules/hosts/manifests/ertp-staging.pp
@@ -1,5 +1,5 @@
 class hosts::ertp-staging {
-  host { 'ertp-ero-api':      ip => '10.32.5.251' }
+  host { 'ertp-ems-api':      ip => '10.32.5.251' }
   host { 'ertp-citizen-api':  ip => '10.241.67.78' }
   host { 'ertp-mongo-1':      ip => '10.234.119.78' }
   host { 'ertp-mongo-2':      ip => '10.229.123.95' }
