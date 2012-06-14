69e21fcf7110c5bedbdb628929fbb5f55deb248e
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-06-14 02:14:20 +0100

    Fixing some lint

--- modules/hosts/manifests/elms-preview.pp
+++ modules/hosts/manifests/elms-preview.pp
@@ -1,4 +1,4 @@
 class hosts::elms-preview {
   host { 'elms-frontend':    ip => '10.237.35.45' }
-  host { 'elms-mongo':  ip => '10.234.74.235' }
+  host { 'elms-mongo':       ip => '10.234.74.235' }
 }
