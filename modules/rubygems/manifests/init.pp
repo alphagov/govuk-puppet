f485c5191d8dd9300d956afdc066d1cf836d11e4
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-06-20 16:11:12 +0100

    log output on failure to change rubygems version

--- modules/rubygems/manifests/init.pp
+++ modules/rubygems/manifests/init.pp
@@ -1,6 +1,7 @@
 class rubygems ($version) {
     exec { "rubygems-$version" :
-        command => "/usr/bin/gem install rubygems-update -v $version && /usr/bin/update_rubygems _${version}_",
-        unless  => "/usr/bin/gem -v | /bin/grep -q '^${version}$'"
+        command   => "/usr/bin/gem install rubygems-update -v $version && /usr/bin/update_rubygems _${version}_",
+        unless    => "/usr/bin/gem -v | /bin/grep -q '^${version}$'",
+        logoutput => on_failure
     }
 }
--- modules/rubygems/spec/classes/rubygems_spec.rb
+++ modules/rubygems/spec/classes/rubygems_spec.rb
@@ -4,6 +4,7 @@ describe 'rubygems', :type => :class do
   let(:params) { {'version' => '1.2.3.4.5.6'} }
   it { should contain_exec('rubygems-1.2.3.4.5.6').with({
       'command' => '/usr/bin/gem install rubygems-update -v 1.2.3.4.5.6 && /usr/bin/update_rubygems _1.2.3.4.5.6_',
-      'unless' => "/usr/bin/gem -v | /bin/grep -q '^1.2.3.4.5.6$'"
+      'unless' => "/usr/bin/gem -v | /bin/grep -q '^1.2.3.4.5.6$'",
+      'logoutput' => 'on_failure'
   })}
 end
