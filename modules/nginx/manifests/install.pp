de2ec6c67956b33032c708107394608b37d0b024
carl.massa <carl.massa@digital.cabinet-office.gov>
2012-06-21 09:36:34 -0700

    upgrading nginx to 1.2.1 on all machines

--- modules/nginx/manifests/install.pp
+++ modules/nginx/manifests/install.pp
@@ -4,20 +4,11 @@ class nginx::install {
     publisher => 'nginx',
     repo      => 'stable',
   }
-
-  if $::govuk_class == 'frontend' {
-    package { 'nginx-full':
-      ensure  => '1.2.1-0ubuntu0ppa1~lucid',
-      require => Exec['add_repo_nginx_ppa'],
-      notify  => Exec['nginx_reload'],
-    }
-  } else {
-    package { 'nginx-full':
-      ensure  => 'installed',
-      require => Exec['add_repo_nginx_ppa'],
-      notify  => Exec['nginx_reload'],
-    }
-  }
+  package { 'nginx-full':
+    ensure  => '1.2.1-0ubuntu0ppa1~lucid',
+    require => Exec['add_repo_nginx_ppa'],
+    notify  => Exec['nginx_reload'],
+  } 
   file { '/etc/nginx/nginx.conf':
     ensure  => file,
     source  => 'puppet:///modules/nginx/nginx.conf',
