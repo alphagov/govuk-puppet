8b7bac685eaf38ca3e0cced604c89f928d6b409f
carl.massa <carl.massa@digital.cabinet-office.gov>
2012-06-21 03:39:32 -0700

    gonna try install nginx-full instead of just nginx since nginx didn't really work from what we can tell

--- modules/nginx/manifests/install.pp
+++ modules/nginx/manifests/install.pp
@@ -6,12 +6,12 @@ class nginx::install {
   }
 
   if $::govuk_class == 'frontend' {
-    package { 'nginx':
+    package { 'nginx-full':
       ensure  => '1.2.1-0ubuntu0ppa1~lucid',
       require => Exec['add_repo_nginx_ppa'],
     }
   } else {
-    package { 'nginx':
+    package { 'nginx-full':
       ensure  => 'installed',
       require => Exec['add_repo_nginx_ppa'],
     }
@@ -19,13 +19,13 @@ class nginx::install {
   file { '/etc/nginx/nginx.conf':
     ensure  => file,
     source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
+    require => Package['nginx-full'],
     notify  => Exec['nginx_reload'],
   }
   file { '/etc/nginx/blockips.conf':
     ensure  => file,
     source  => 'puppet:///modules/nginx/blockips.conf',
-    require => Package['nginx'],
+    require => Package['nginx-full'],
     notify  => Exec['nginx_reload'],
   }
   file { ['/var/www', '/var/www/cache']:
@@ -45,10 +45,10 @@ class nginx::install {
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
-    require => Package['nginx']
+    require => Package['nginx-full']
   }
   file { '/etc/nginx/ssl':
     ensure  => directory,
-    require => Package['nginx'],
+    require => Package['nginx-full'],
   }
 }
--- modules/nginx/spec/classes/nginx_spec.rb
+++ modules/nginx/spec/classes/nginx_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../../../spec_helper'
 
 describe 'nginx', :type => :class do
-  it { should contain_package('nginx').with({
+  it { should contain_package('nginx-full').with({
       'ensure'  => 'installed'
   })}
 
