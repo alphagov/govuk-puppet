632a0e437e7e253f149a5452182b42bbd4140571
minglis <martyn.inglis@digital.cabinet-office.gov.uk>
2012-06-22 14:08:11 +0100

    IPs for the mongo replica set on

--- modules/hosts/manifests/places-staging.pp
+++ modules/hosts/manifests/places-staging.pp
@@ -1,5 +1,5 @@
 class hosts::places-staging {
-  host { 'places-mongo-1':      ip => '' }
-  host { 'places-mongo-2':      ip => '' }
-  host { 'places-mongo-3':      ip => '' }
+  host { 'places-mongo-1':      ip => '10.228.167.31' }
+  host { 'places-mongo-2':      ip => '10.234.122.78' }
+  host { 'places-mongo-3':      ip => '10.234.5.12' }
 }
