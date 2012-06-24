b8f3c86510034769d74245fab391ce6227c832e0
root <root@ubuntu.localdomain>
2012-06-23 22:41:54 -0700

    Revert "notified apache restart for rubygems"
    This reverts commit 2bf5d63414d9000e9194e58a2e420aab6bca36b5.

--- modules/ruby/manifests/rubygems.pp
+++ modules/ruby/manifests/rubygems.pp
@@ -2,7 +2,6 @@ class ruby::rubygems ($version) {
     exec { "rubygems-$version" :
         command   => "gem install rubygems-update -v $version && update_rubygems _${version}_",
         unless    => "gem -v | grep -q '^${version}$'",
-        logoutput => on_failure,
-        notify    => Exec['apache_graceful']
+        logoutput => on_failure
     }
 }
