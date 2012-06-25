b405312d473177770e890a0daf5c8b4059379352
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-06-25 14:48:55 +0100

    The correct word is 'it' not 'if' in a spec

--- modules/postgres/spec/classes/ubuntu_spec.rb
+++ modules/postgres/spec/classes/ubuntu_spec.rb
@@ -2,5 +2,5 @@ require_relative '../../../../spec_helper'
 
 describe 'postgres::ubuntu', :type => :class do
   let(:facts) { { :lsbdistcodename => 'precise' } }
-  if { should contain_file("/etc/postgresql/9.1/main/postgresql.conf" ) }
+  it { should contain_file("/etc/postgresql/9.1/main/postgresql.conf" ) }
 end
