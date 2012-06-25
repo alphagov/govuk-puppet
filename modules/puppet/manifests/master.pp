dcf935b9261da7d4976adbabd5ca1e55ab3f5f83
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-06-25 10:02:30 +0000

    just stop the daemonized puppetmaster manually, it's too messy to do it from puppet

--- modules/puppet/manifests/master.pp
+++ modules/puppet/manifests/master.pp
@@ -12,12 +12,4 @@ class puppet::master {
         provider => upstart,
         require  => File['/etc/init/puppetmaster.conf'],
     }
-
-    service { 'daemonized-puppetmaster': # kill any existing daemonized puppetmaster process
-        ensure   => stopped,
-        provider => base,
-        require  => Service['puppetmaster'], # don't kill the daemon til the upstart process is working
-        pattern  => '/usr/bin/puppet master$';
-    }
-
 }
