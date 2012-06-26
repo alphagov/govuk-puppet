293602980f0bd99a4081f8a4d950519a4372e042
David Illsley <david@illsley.org>
2012-06-26 09:56:34 +0100

    Add places-api to licensify hosts files

--- modules/hosts/manifests/elms-preview.pp
+++ modules/hosts/manifests/elms-preview.pp
@@ -1,4 +1,5 @@
 class hosts::elms-preview {
   host { 'elms-frontend':    ip => '10.237.35.45' }
   host { 'elms-mongo':       ip => '10.234.74.235' }
+  host { 'places-api':       ip => '10.229.118.175' }
 }
