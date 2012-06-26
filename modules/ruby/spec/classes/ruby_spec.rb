0c2b463adddc4bfb80273a8831bf846d8cf98b93
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-06-26 09:09:59 +0000

    install brightbox repo as a PPA repo

--- modules/ruby/spec/classes/ruby_spec.rb
+++ modules/ruby/spec/classes/ruby_spec.rb
@@ -4,8 +4,8 @@ describe 'ruby', :type => :class do
   let(:params) { {'version' => '1.2.3.4.5.6'} }
   it { should contain_package('ruby1.9.1').with({
       'ensure'  => '1.2.3.4.5.6',
-      'require' => 'Apt::Deb_repository[brightbox]'
+      'require' => 'Apt::Ppa_repository[brightbox]'
   })}
 
-  it { should contain_apt__deb_repository('brightbox') }
+  it { should contain_apt__ppa_repository('brightbox') }
 end
