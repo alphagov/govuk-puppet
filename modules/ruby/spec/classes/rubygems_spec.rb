d363d122648855c49e0ce8ffc0ae1f6399a1094b
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-06-26 09:45:43 +0000

    gah, forgot to add this to previous commit

--- modules/ruby/spec/classes/rubygems_spec.rb
+++ modules/ruby/spec/classes/rubygems_spec.rb
@@ -3,8 +3,8 @@ require_relative '../../../../spec_helper'
 describe 'ruby::rubygems', :type => :class do
   let(:params) { {'version' => '1.2.3.4.5.6'} }
   it { should contain_exec('rubygems-1.2.3.4.5.6').with({
-      'command' => '/usr/bin/gem install rubygems-update -v 1.2.3.4.5.6 && /usr/bin/update_rubygems _1.2.3.4.5.6_',
-      'unless' => "/usr/bin/gem -v | /bin/grep -q '^1.2.3.4.5.6$'",
+      'command' => 'gem install rubygems-update -v 1.2.3.4.5.6 && update_rubygems _1.2.3.4.5.6_',
+      'unless' => "gem -v | grep -q '^1.2.3.4.5.6$'",
       'logoutput' => 'on_failure'
   })}
 end
