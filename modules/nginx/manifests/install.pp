2c9e1c16093d3b93560fde2ad6269ebaa8d62437
z@ubuntu.(none) <z@ubuntu.(none)>
2012-06-27 04:58:20 -0700

    Ensuring nginx-common & nginx-full are upgraded along with nginx

--- modules/nginx/manifests/install.pp
+++ modules/nginx/manifests/install.pp
@@ -4,9 +4,19 @@ class nginx::install {
     publisher => 'nginx',
     repo      => 'stable',
   }
-  package { 'nginx':
+  package { 'nginx-common':
     ensure  => '1.2.1-0*',
     require => Apt::Ppa_Repository['nginx_ppa'],
+    notify  => Exec['nginx_restart'],
+  }
+  package { 'nginx-full':
+    ensure  => '1.2.1-0*',
+    require => [Apt::Ppa_Repository['nginx_ppa'],Package['nginx-common']],
+    notify  => Exec['nginx_restart'],
+  }
+  package { 'nginx':
+    ensure  => '1.2.1-0*',
+    require => [Apt::Ppa_Repository['nginx_ppa'],Package['nginx-full']],
     notify  => Exec['nginx_restart'],
   }
   file { '/etc/nginx/nginx.conf':
