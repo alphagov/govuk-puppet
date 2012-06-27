344589b512db6ff1ce6f2a9729b9cd767bc4ac95
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-06-27 15:20:41 +0000

    add a test for nagios::host

---
+++ modules/nagios/spec/defines/host_spec.rb
@@ -0,0 +1,6 @@
+require_relative '../../../../spec_helper'
+
+describe 'nagios::host', :type => :define do
+  let(:title) { 'bruce-forsyth' }
+  it { should contain_file('/etc/nagios3/conf.d/nagios_host_bruce-forsyth.cfg') }
+end
