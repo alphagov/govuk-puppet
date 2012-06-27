3f7a44d311a44e1726edb46035e4385f1149e071
James Abley <james.abley@gmail.com>
2012-06-27 16:55:02 +0100

    Remove Jason Cale authorized_keys
    And so my puppet known universe expands just a little further...

--- modules/users/manifests/freerange.pp
+++ modules/users/manifests/freerange.pp
@@ -37,6 +37,9 @@ class users::freerange {
   user { 'jasoncale':
     ensure     => absent,
   }
+  ssh_authorized_key { 'jasoncale':
+    ensure  => absent,
+  }
 
   user { 'jamesmead':
     ensure     => present,
@@ -91,6 +94,8 @@ class users::freerange {
       type    => 'ssh-rsa',
       user    => 'deploy',
       require => User['deploy'];
+    'deploy_key_jasoncale':
+      ensure  => absent;
     'deploy_key_tomw':
       ensure  => present,
       key     => extlookup('tomw_key', ''),
