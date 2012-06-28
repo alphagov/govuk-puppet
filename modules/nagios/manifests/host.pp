242b78971278638ee1ff267defa87438de91616f
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-06-28 13:43:39 +0000

    Adding Nagios::Check to replace built in service definition

--- modules/nagios/manifests/host.pp
+++ modules/nagios/manifests/host.pp
@@ -1,10 +1,16 @@
 define nagios::host (
-  $hostalias = $::fqdn,
-  $address   = $::ipaddress,
-  $use       = 'generic-host',
-  $hostname  = $title
+  $hostalias  = $::fqdn,
+  $address    = $::ipaddress,
+  $use        = 'generic-host',
+  $host_name  = $title
 ) {
-  file {"/etc/nagios3/conf.d/nagios_host_${hostname}.cfg":
+  file {"/etc/nagios3/conf.d/nagios_host_${host_name}":
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+  file {"/etc/nagios3/conf.d/nagios_host_${host_name}.cfg":
     ensure  => present,
     content => template('nagios/host.erb'),
     owner   => 'root',
