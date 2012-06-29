34db801ab103190736a94204a4bca509f60cd3a3
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-06-29 09:51:35 +0000

    remove orphaned exec resource

--- modules/nagios/manifests/init.pp
+++ modules/nagios/manifests/init.pp
@@ -1,10 +1,5 @@
 class nagios {
   include nagios::install, nagios::service
-  exec { 'fix_nagios_perms':
-    command     => '/bin/chmod -R 755 /etc/nagios3',
-    notify      =>  Service['nagios3'],
-    refreshonly => true,
-  }
   Nagios::Host   <<||>> { notify => Service['nagios3'] }
   Nagios::Check  <<||>> { notify => Service['nagios3'] }
 }
