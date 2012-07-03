3e4ab19638d57ad403afb39ccbb01bd26878767a
Mark Needham <mneedham@ubuntu.localdomain>
2012-07-03 09:29:02 -0700

    making lint (phil) happy

--- modules/ganglia/manifests/client/install.pp
+++ modules/ganglia/manifests/client/install.pp
@@ -5,17 +5,17 @@ class ganglia::client::install {
 
   file { '/usr/lib/ganglia/python_modules':
     ensure => directory
-   }
+  }
 
   file { '/usr/lib/ganglia/python_modules/diskstat.py':
-  	source => 'puppet:///modules/ganglia/usr/lib/ganglia/python_modules/diskstat.py',
-  	require => File['/usr/lib/ganglia/python_modules']
-  } 
-  
+    source  => 'puppet:///modules/ganglia/usr/lib/ganglia/python_modules/diskstat.py',
+    require => File['/usr/lib/ganglia/python_modules']
+  }
+
   file { '/etc/ganglia/conf.d/diskstat.pyconf':
-  	source => 'puppet:///modules/ganglia/etc/ganglia/conf.d/diskstat.pyconf',
-  	require => File['/usr/lib/ganglia/python_modules']
-  } 
+    source  => 'puppet:///modules/ganglia/etc/ganglia/conf.d/diskstat.pyconf',
+    require => File['/usr/lib/ganglia/python_modules']
+  }
 
   file { '/etc/ganglia/gmond.conf':
     source  => 'puppet:///modules/ganglia/gmond.conf',
