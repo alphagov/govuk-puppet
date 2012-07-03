dab8f83995de13e62385b760fad40d3a41826aae
Mark Needham <mneedham@ubuntu.localdomain>
2012-07-03 09:26:27 -0700

    adding extra disk/io stats

--- modules/ganglia/manifests/client/install.pp
+++ modules/ganglia/manifests/client/install.pp
@@ -2,6 +2,21 @@ class ganglia::client::install {
   package {
     'ganglia-monitor': ensure => 'installed';
   }
+
+  file { '/usr/lib/ganglia/python_modules':
+    ensure => directory
+   }
+
+  file { '/usr/lib/ganglia/python_modules/diskstat.py':
+  	source => 'puppet:///modules/ganglia/usr/lib/ganglia/python_modules/diskstat.py',
+  	require => File['/usr/lib/ganglia/python_modules']
+  } 
+  
+  file { '/etc/ganglia/conf.d/diskstat.pyconf':
+  	source => 'puppet:///modules/ganglia/etc/ganglia/conf.d/diskstat.pyconf',
+  	require => File['/usr/lib/ganglia/python_modules']
+  } 
+
   file { '/etc/ganglia/gmond.conf':
     source  => 'puppet:///modules/ganglia/gmond.conf',
     owner   => root,
