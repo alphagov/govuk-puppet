5e01fe054ba138482d00f361c0973ce6d95e948a
David Illsley <david@illsley.org>
2012-07-03 16:44:36 +0100

    Add preview certs to elms configurations

--- modules/nginx/manifests/config/elms.pp
+++ modules/nginx/manifests/config/elms.pp
@@ -12,5 +12,12 @@ class nginx::config::elms {
     require => Class['nginx::package'],
     notify  => Exec['nginx_reload'],
   }
-
+  file { "/etc/nginx/ssl/www.preview.alphagov.co.uk.crt":
+    ensure => present,
+    content => extlookup("www.preview.alphagov.co.uk_crt", ''),
+  }
+  file { "/etc/nginx/ssl/www.preview.alphagov.co.uk.key":
+    ensure => present,
+    content => extlookup("www.preview.alphagov.co.uk_key", ''),
+  }
 }
