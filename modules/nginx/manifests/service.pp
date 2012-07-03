a0578dd9894245ff70a4d88f14076988b1f0b6ef
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-07-03 08:44:02 +0000

    Whenever NginX is turned on, create a check in Nagios to alert if it isn't running

--- modules/nginx/manifests/service.pp
+++ modules/nginx/manifests/service.pp
@@ -5,4 +5,9 @@ class nginx::service {
     hasrestart => true,
     require    => Class['nginx::config'],
   }
+  @@nagios::check { "check_nginx_running_${::hostname}":
+    check_command       => 'check_nrpe!check_proc_running!nginx',
+    service_description => "check nginx on ${::govuk_class}-${::hostname}",
+    host_name           => "${::govuk_class}-${::hostname}",
+  }
 }
