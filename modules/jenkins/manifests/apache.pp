5b00a4822436d89dc5a12f63b4a9a449cc94dcc6
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-07-04 16:38:32 +0100

    Bugfix.

--- modules/jenkins/manifests/apache.pp
+++ modules/jenkins/manifests/apache.pp
@@ -45,7 +45,6 @@ class jenkins::apache {
   file { '/etc/apache2/envvars':
     ensure  => present,
     source  => 'puppet:///modules/apache2/envvars',
-    require => Class['apache2::install'],
     notify  => Service['apache2'],
   }
 }
