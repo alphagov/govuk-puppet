694c0be3373d68dce94d6dc8e0cbdf32d9ed0ded
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-07-05 09:50:21 +0100

    Add an "apt-get update" refresh to PPA repos.
    This should fix a problem where the "phantomjs" package wasn't being
    found on an initial bootstrap build.

--- modules/apt/manifests/ppa_repository.pp
+++ modules/apt/manifests/ppa_repository.pp
@@ -1,10 +1,15 @@
 define apt::ppa_repository($publisher, $repo, $ensure='present') {
+  exec { "apt-update-repo-$name":
+    command     => '/usr/bin/apt-get update',
+    refreshonly => true
+  }
   case $ensure {
     'present': {
       exec { "add_repo_$name":
         command => "/usr/bin/add-apt-repository ppa:$publisher/$repo && /usr/bin/apt-get update",
         creates => "/etc/apt/sources.list.d/${publisher}-${repo}-${::lsbdistcodename}.list",
-        require => Package['python-software-properties']
+        require => Package['python-software-properties'],
+        notify  => Exec["apt-update-repo-$name"]
       }
     }
     default: {
