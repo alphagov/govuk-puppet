d8e7d13ab904d4dadc615f4e5ec5b741bf413d78
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-07-05 19:53:16 +0000

    reject licence-finder from wget, add timeout

--- modules/mirror/manifests/init.pp
+++ modules/mirror/manifests/init.pp
@@ -8,14 +8,17 @@ class mirror {
     mode   => '0755',
   }
 
+  # Note that this exec task will only ever run once; a decision
+  # about how to run this periodically has yet to be made, so the
+  # exec task is left here merely to document the mirroring process
   exec { 'update-govuk-mirror':
     creates => '/usr/share/www/www.gov.uk',
-    command => '/usr/bin/wget -mE https://www.gov.uk',
+    command => '/usr/bin/wget -mE -R licence-finder https://www.gov.uk',
     cwd     => '/usr/share/www',
     require => File['/usr/share/www'],
     user    => 'www-data',
     group   => 'www-data',
-    timeout => 0, #takes forever, and wget has no respect for ETags :(
+    timeout => 3600, # 1 hour. wget doesn't understand ETags
   }
 
 }
