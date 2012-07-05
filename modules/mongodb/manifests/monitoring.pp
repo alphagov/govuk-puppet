560f6b980595218e9e1513ed43bbdc3bd4beef73
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-07-05 16:16:28 +0100

    Add Nagios checks via NRPE for mongodb

--- modules/mongodb/manifests/monitoring.pp
+++ modules/mongodb/manifests/monitoring.pp
@@ -1,5 +1,7 @@
 class mongodb::monitoring {
+
   include ganglia::client
+  include nagios::client
 
   file { '/etc/ganglia/conf.d/mongodb.pyconf':
     source  => 'puppet:///modules/mongodb/ganglia_mongodb.conf',
@@ -9,6 +11,34 @@ class mongodb::monitoring {
   file { '/usr/lib/ganglia/python_modules/mongodb.py':
     source  => 'puppet:///modules/mongodb/ganglia_mongodb.py',
     require => [Service['mongodb'],Service['ganglia-monitor']]
+  }
+
+  file { '/usr/lib/nagios/plugins/check_mongodb.py':
+    source  => 'puppet:///modules/mongodb/nagios_check_mongodb.py',
+    require => [Service['mongodb'],Package['nagios-nrpe-server']]
+  }
+
+  file { '/etc/nagios/nrpe.d/check_mongo.cfg':
+    source  => 'puppet:///modules/mongodb/nrpe_check_mongo.cfg',
+    require => [Service['mongodb'],Package['nagios-nrpe-server']]
+  }
+
+  @@nagios::check { "check_mongod_running_${::hostname}":
+    check_command       => 'check_nrpe!check_proc_running!mongod',
+    service_description => "check mongod running on ${::govuk_class}-${::hostname}",
+    host_name           => "${::govuk_class}-${::hostname}",
+  }
+
+  @@nagios::check { "check_mongod_responds_${::hostname}":
+    check_command       => 'check_nrpe!check_mongodb!connect!2!4',
+    service_description => "check mongod responds on ${::govuk_class}-${::hostname}",
+    host_name           => "${::govuk_class}-${::hostname}",
+  }
+
+  @@nagios::check { "check_mongod_lock_percentage_${::hostname}":
+    check_command       => 'check_nrpe!check_mongodb!connect!5!10',
+    service_description => "check mongod lock percentage on ${::govuk_class}-${::hostname}",
+    host_name           => "${::govuk_class}-${::hostname}",
   }
 
 }
