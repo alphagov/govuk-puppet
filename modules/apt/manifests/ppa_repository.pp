7d069600cf9cf7dcecceacb1aa05c45494435e48
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-07-06 11:26:28 +0100

    Revert "Add an "apt-get update" refresh to PPA repos."
    I'd missed that the 'apt-get update' command is already baked into adding
    the repository; we might as well keep them together unless and until
    there's a good reason to pull them apart.
    
    This reverts commit 694c0be3373d68dce94d6dc8e0cbdf32d9ed0ded.

--- modules/apt/manifests/ppa_repository.pp
+++ modules/apt/manifests/ppa_repository.pp
@@ -1,15 +1,10 @@
 define apt::ppa_repository($publisher, $repo, $ensure='present') {
-  exec { "apt-update-repo-$name":
-    command     => '/usr/bin/apt-get update',
-    refreshonly => true
-  }
   case $ensure {
     'present': {
       exec { "add_repo_$name":
         command => "/usr/bin/add-apt-repository ppa:$publisher/$repo && /usr/bin/apt-get update",
         creates => "/etc/apt/sources.list.d/${publisher}-${repo}-${::lsbdistcodename}.list",
-        require => Package['python-software-properties'],
-        notify  => Exec["apt-update-repo-$name"]
+        require => Package['python-software-properties']
       }
     }
     'absent': {
