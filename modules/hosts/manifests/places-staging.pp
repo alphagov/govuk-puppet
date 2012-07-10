2bd6f9d71d10d76f2b91c885ae07d40695207222
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-07-10 05:13:11 -0700

    Adding monitoring.cluster to the elms and places hosts file

--- modules/hosts/manifests/places-staging.pp
+++ modules/hosts/manifests/places-staging.pp
@@ -2,4 +2,5 @@ class hosts::places-staging {
   host { 'places-mongo-1':      ip => '10.228.167.31' }
   host { 'places-mongo-2':      ip => '10.234.122.78' }
   host { 'places-mongo-3':      ip => '10.234.5.12' }
+  host { 'monitoring.cluster':  ip => '10.51.62.202' }
 }
