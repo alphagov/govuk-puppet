0a6f94d48628dbb5ccaec3d6fdd6f8cef27317fd
Mark Needham <mark.needham@digital.cabinet-office.gov.uk>
2012-07-10 06:41:38 -0700

    pushing graphite to port 8090 and will use apache as a proxy

--- modules/graphite/manifests/init.pp
+++ modules/graphite/manifests/init.pp
@@ -1,6 +1,4 @@
 class graphite {
-  include govuk::repository
-
   class { 'nginx' : node_type => graphite }
   include graphite::package, graphite::config
 }
--- modules/graphite/manifests/package.pp
+++ modules/graphite/manifests/package.pp
@@ -1,4 +1,5 @@
 class graphite::package {
+  include govuk::repository
   package{['python-flup', 'python-carbon', 'python-graphite-web', 'python-txamqp', 'python-whisper']:
     ensure => present,
   }
