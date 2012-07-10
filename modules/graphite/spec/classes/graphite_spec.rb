500bdaf76189ea6a965f31802cf8c1e2d847bb06
Mark Needham <mark.needham@digital.cabinet-office.gov.uk>
2012-07-10 05:27:07 -0700

    adding some specs to make sure the main graphite packages are installed

---
+++ modules/graphite/spec/classes/graphite_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../../../../spec_helper'
+
+describe 'graphite', :type => :class do
+  it { should contain_package("python-graphite-web" ).with_ensure('present') }
+  it { should contain_package("python-carbon" ).with_ensure('present') }
+  it { should contain_package("python-whisper" ).with_ensure('present') }
+end
