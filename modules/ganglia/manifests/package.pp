a5729718c053aa752c3215213b0c70f8fe65fab3
Shodhan Sheth <shodhan.sheth@digital.cabinet-office.gov.uk>
2012-07-10 03:16:43 -0700

    Ensuring strict ganglia dependencies

--- modules/ganglia/manifests/package.pp
+++ modules/ganglia/manifests/package.pp
@@ -2,17 +2,21 @@ class ganglia::package {
   include wget, apache2
   package {
     'gmetad':
-      ensure => 'installed';
+      ensure  => 'installed',
+      require => Package['ganglia-webfrontend'];
     'rrdtool':
-      ensure => 'installed';
+      ensure  => 'installed',
+      require => Package['ganglia-webfrontend'];
     'php5':
-      ensure => 'installed',
-      notify => Service[apache2]
+      ensure  => 'installed',
+      require => Package['ganglia-webfrontend'],
+      notify  => Service[apache2]
   }
 
   wget::fetch { 'ganglia-webfrontend':
     source      => 'http://sourceforge.net/projects/ganglia/files/ganglia-web/3.5.0/ganglia-web-3.5.0.tar.gz/download',
-    destination => '/tmp/ganglia-web-3.5.0.tar.gz'
+    destination => '/tmp/ganglia-web-3.5.0.tar.gz',
+    require     => [Package['gmetad'],Package['rrdtool'],Package['php5']]
   }
   exec { 'ganglia_webfrontend_untar':
     command => 'tar zxf /tmp/ganglia-web-3.5.0.tar.gz && chown -R www-data:www-data /var/www/ganglia-web-3.5.0',
--- modules/ganglia/manifests/uninstall_old.pp
+++ modules/ganglia/manifests/uninstall_old.pp
@@ -1,10 +1,11 @@
 class ganglia::uninstall_old {
-  package { 'ganglia-webfrontend':
-    ensure => 'purged'
-  }
   file { '/var/www/ganglia2':
     ensure  => absent,
     recurse => true,
     force   => true
+  }
+  package { 'ganglia-webfrontend':
+    ensure  => 'purged',
+    require => File['/var/www/ganglia2']
   }
 }
