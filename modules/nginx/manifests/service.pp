e1ae1e59db539e198e848f5f66220886d2f4eaa5
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-07-10 08:47:19 -0700

    Add monitoring for NginX by creating a separate Vhost to monitor

--- modules/nginx/manifests/service.pp
+++ modules/nginx/manifests/service.pp
@@ -6,18 +6,40 @@ class nginx::service {
     require    => Class['nginx::config'],
   }
   include nagios::client
+
+  # Monitoring of NginX
+
+  file { '/etc/nginx/ssl/monitoring-vhost.sslkey':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/monitoring-vhost.sslkey',
+    require => Class['nginx::package']
+  }
+
+  file { '/etc/nginx/ssl/monitoring-vhost.sslcert':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/monitoring-vhost.sslcert',
+    require => Class['nginx::package'],
+  }
+
+  file { '/etc/nginx/sites-enabled/monitoring-vhost.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/monitoring-vhost-nginx.conf',
+    require => [File['/etc/nginx/ssl/monitoring-vhost.sslcert'],File['/etc/nginx/ssl/monitoring-vhost.sslkey']],
+    notify  => Exec['nginx_reload'],
+  }
+
   @@nagios::check { "check_nginx_running_${::hostname}":
     check_command       => 'check_nrpe!check_proc_running!nginx',
     service_description => "check nginx on ${::govuk_class}-${::hostname}",
     host_name           => "${::govuk_class}-${::hostname}",
   }
   @@nagios::check { "check_http_response_${::hostname}":
-    check_command       => 'check_http_port!www.gov.uk!5!10',
+    check_command       => 'check_http_port!monitoring-vhost.test!5!10',
     service_description => "check HTTP response on ${::govuk_class}-${::hostname}",
     host_name           => "${::govuk_class}-${::hostname}",
   }
   @@nagios::check { "check_https_response_${::hostname}":
-    check_command       => 'check_https_port!www.gov.uk!5!10',
+    check_command       => 'check_https_port!monitoring-vhost.test!5!10',
     service_description => "check HTTPS response ${::govuk_class}-${::hostname}",
     host_name           => "${::govuk_class}-${::hostname}",
   }
