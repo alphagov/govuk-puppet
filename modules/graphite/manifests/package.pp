403f4c88239df67b02c7bdfdd8ba10682255da69
Shodhan Sheth <shodhan.sheth@digital.cabinet-office.gov.uk>
2012-07-12 13:26:06 +0000

    adding dep on s3 repo

--- modules/graphite/manifests/package.pp
+++ modules/graphite/manifests/package.pp
@@ -1,6 +1,7 @@
 class graphite::package {
   include govuk::repository
   package{['python-flup', 'python-carbon', 'python-graphite-web', 'python-txamqp', 'python-whisper']:
-    ensure => present,
+    ensure  => present,
+    require => Apt::Deb_repository['gds']  
   }
 }
