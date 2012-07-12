afdb69a884c5717ad829c8cd0aaa88e84168936b
Shodhan Sheth <shodhan.sheth@digital.cabinet-office.gov.uk>
2012-07-12 13:48:07 +0000

    removing whitespace

--- modules/graphite/manifests/package.pp
+++ modules/graphite/manifests/package.pp
@@ -2,6 +2,6 @@ class graphite::package {
   include govuk::repository
   package{['python-flup', 'python-carbon', 'python-graphite-web', 'python-txamqp', 'python-whisper']:
     ensure  => present,
-    require => Apt::Deb_repository['gds']  
+    require => Apt::Deb_repository['gds']
   }
 }
