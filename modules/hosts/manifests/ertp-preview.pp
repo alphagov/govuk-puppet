f5e14159c6a152fd29102bd412764910420596cf
Nick Stenning <nick@whiteink.com>
2012-07-12 17:02:32 +0100

    Fix formatting for ertp-preview

--- modules/hosts/manifests/ertp-preview.pp
+++ modules/hosts/manifests/ertp-preview.pp
@@ -1,8 +1,20 @@
 class hosts::ertp-preview {
-  host { 'ertp-api':    ip => '10.239.86.141' }
-  host { 'ertp-mongo-1':  ip => '10.236.255.41' }
-  host { 'ertp-mongo-2':  ip => '10.32.0.78' }
-  host { 'ertp-mongo-3':  ip => '10.228.170.128' }
-  host { 'places-api':  ip => '10.229.118.175' }
-  host { 'monitoring.cluster': ip => '10.51.62.202' }
+  host { 'ertp-api':
+    ip => '10.239.86.141'
+  }
+  host { 'ertp-mongo-1':
+    ip => '10.236.255.41'
+  }
+  host { 'ertp-mongo-2':
+    ip => '10.32.0.78'
+  }
+  host { 'ertp-mongo-3':
+    ip => '10.228.170.128'
+  }
+  host { 'places-api':
+    ip => '10.229.118.175'
+  }
+  host { 'monitoring.cluster':
+    ip => '10.51.62.202'
+  }
 }
