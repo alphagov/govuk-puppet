b0610acef630f3bbacc577f63730337f06d0d8a8
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-07-12 16:26:50 +0000

    introduce anchor pattern to apache2 class
    see http://projects.puppetlabs.com/projects/puppet/wiki/Anchor_Pattern for details

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -5,9 +5,16 @@ class apache2($port='8080') {
     onlyif      => 'apache2ctl configtest',
   }
 
+  anchor {'apache2::start': }
+
   include apache2::install
   class { 'apache2::configure':
-    port => $port
+    port => $port,
   }
   include apache2::service
+
+  anchor {'apache2::end': }
+
+  Anchor['apache2::start'] -> Class['apache2::install'] -> Class['apache2::configure'] ~> Class['apache2::service']
+  Anchor['apache2::start'] ~> Class['apache2::service'] ~> Anchor['apache2::end']
 }
