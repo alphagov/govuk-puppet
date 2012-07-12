f4bcb1995942939d7353dfea4d7ee161eebcef81
minglis <martyn.inglis@digital.cabinet-office.gov.uk>
2012-07-12 17:19:34 +0100

    Updated the hosts file for ertp preview mongo replica set

--- modules/hosts/manifests/ertp-preview.pp
+++ modules/hosts/manifests/ertp-preview.pp
@@ -1,8 +1,8 @@
 class hosts::ertp-preview {
   host { 'ertp-api':    ip => '10.239.86.141' }
-  host { 'ertp-mongo-1':  ip => '10.236.255.41' }
-  host { 'ertp-mongo-2':  ip => '10.32.0.78' }
-  host { 'ertp-mongo-3':  ip => '10.228.170.128' }
+  host { 'ertp-mongo-1':  ip => '10.238.233.55' }
+  host { 'ertp-mongo-2':  ip => '10.234.66.40' }
+  host { 'ertp-mongo-3':  ip => '10.32.40.93' }
   host { 'places-api':  ip => '10.229.118.175' }
   host { 'monitoring.cluster': ip => '10.51.62.202' }
 }
