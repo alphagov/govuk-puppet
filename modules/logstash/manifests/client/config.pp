d6d83736d0069878d48ccdc0185003aecef70c9d
Shodhan Sheth <shodhan.sheth@digital.cabinet-office.gov.uk>
2012-07-13 02:29:52 -0700

    Client conf is now split into two files

--- modules/logstash/manifests/client/config.pp
+++ modules/logstash/manifests/client/config.pp
@@ -2,12 +2,20 @@ class logstash::client::config {
   file { '/etc/logstash' :
     ensure => directory
   }
+  file { '/etc/logstash/logstash-client' :
+    ensure  => directory,
+    require => File['/etc/logstash']
+  }
   file { '/etc/logstash/grok-patterns' :
     ensure => directory
   }
-  file { '/etc/logstash/logstash-client.conf' :
-    source  => 'puppet:///modules/logstash/etc/logstash/logstash-client.conf',
-    require => File['/etc/logstash']
+  file { '/etc/logstash/logstash-client/default.conf' :
+    source  => 'puppet:///modules/logstash/etc/logstash/logstash-client/default.conf',
+    require => File['/etc/logstash/logstash-client']
+  }
+  file { '/etc/logstash/logstash-client/apache.conf' :
+    source  => 'puppet:///modules/logstash/etc/logstash/logstash-client/apache.conf',
+    require => File['/etc/logstash/logstash-client']
   }
   file { '/etc/logstash/grok-patterns/apache-error' :
     source  => 'puppet:///modules/logstash/etc/logstash/grok-patterns/apache-error',
