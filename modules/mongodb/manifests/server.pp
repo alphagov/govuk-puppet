0b04a9cf551c6331567002766a13de4eb4ad459d
uttamk <uttamk@gds-vm.(none)>
2012-07-16 11:07:22 +0100

    Fixing the puppet rspec build

--- modules/mongodb/manifests/server.pp
+++ modules/mongodb/manifests/server.pp
@@ -2,7 +2,7 @@ class mongodb::server (
   replicaset = $govuk_platform) {
   include mongodb::repository
   include mongodb::package
-  class {'mongodb::configuration': replicaset => $replicaset}
+  class {'mongodb::configuration': replicaset => $::replicaset}
   include mongodb::service
   include mongodb::monitoring
 }
