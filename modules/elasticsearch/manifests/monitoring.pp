bcace4a32bdca0d1376e9bdfc989ba45303c623d
uttamk <uttamk@gds-vm.(none)>
2012-07-16 11:56:50 +0100

    Added nrpe-check for elastic search

--- modules/elasticsearch/manifests/monitoring.pp
+++ modules/elasticsearch/manifests/monitoring.pp
@@ -1,5 +1,8 @@
 class elasticsearch::monitoring {
-  file { '/etc/ganglia/conf.d/elasticsearch.pyconf':
+ include ganglia::client
+ include nagios::client  
+
+file { '/etc/ganglia/conf.d/elasticsearch.pyconf':
     source  => 'puppet:///modules/elasticsearch/elasticsearch.pyconf',
     require => [Service['elasticsearch'],Service['ganglia-monitor']]
   }
@@ -8,5 +11,12 @@ class elasticsearch::monitoring {
     source  => 'puppet:///modules/elasticsearch/elasticsearch.py',
     mode    => '0755',
     require => [Service['elasticsearch'],Service['ganglia-monitor']]
+  }
+
+ file { '/etc/nagios/nrpe.d/check_elasticsearch.cfg':
+    source  => 'puppet:///modules/elasticsearch/check_elasticsearch.cfg',
+    mode    => '0755',
+    require => [Service['elasticsearch'],Package['nagios-nrpe-server']],
+    notify  => Service['nagios-nrpe-server']
   }
 }
