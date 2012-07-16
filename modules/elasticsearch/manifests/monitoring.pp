1a9b1487044ed1ad63e33f3f8a0b7dbb62ee19e3
uttamk <uttamk@gds-vm.(none)>
2012-07-16 12:10:58 +0100

    Fixing the whitespace and tabs

--- modules/elasticsearch/manifests/monitoring.pp
+++ modules/elasticsearch/manifests/monitoring.pp
@@ -1,8 +1,8 @@
 class elasticsearch::monitoring {
- include ganglia::client
- include nagios::client  
+  include ganglia::client
+  include nagios::client
 
-file { '/etc/ganglia/conf.d/elasticsearch.pyconf':
+  file { '/etc/ganglia/conf.d/elasticsearch.pyconf':
     source  => 'puppet:///modules/elasticsearch/elasticsearch.pyconf',
     require => [Service['elasticsearch'],Service['ganglia-monitor']]
   }
@@ -13,7 +13,7 @@ file { '/etc/ganglia/conf.d/elasticsearch.pyconf':
     require => [Service['elasticsearch'],Service['ganglia-monitor']]
   }
 
- file { '/etc/nagios/nrpe.d/check_elasticsearch.cfg':
+  file { '/etc/nagios/nrpe.d/check_elasticsearch.cfg':
     source  => 'puppet:///modules/elasticsearch/check_elasticsearch.cfg',
     mode    => '0755',
     require => [Service['elasticsearch'],Package['nagios-nrpe-server']],
