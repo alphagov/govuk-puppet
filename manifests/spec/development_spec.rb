baa8cfb354f33c2143833860afb3612a7ca6617d
Nick Stenning <nick@whiteink.com>
2012-07-17 13:15:27 +0100

    Specify platform when running development specs

--- manifests/spec/development_spec.rb
+++ manifests/spec/development_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../spec_helper'
 
 describe 'development', :type => :class do
-  let(:facts) { { :govuk_class => "development" } }
+  let(:facts) { { :govuk_class => "development", :govuk_platform => "development" } }
   it { should create_package("apache2") }
   it { should_not raise_error(Puppet::ParseError) }
 end
