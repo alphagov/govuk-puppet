2981d973d60fd48e4f3499b28b3dbac4e6f804c3
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-07-18 13:48:49 +0000

    we should manage repos using sources.list.d at some point

--- modules/apt/manifests/deb_repository.pp
+++ modules/apt/manifests/deb_repository.pp
@@ -13,6 +13,8 @@ define apt::deb_repository($url, $repo, $ensure='present', $dist=$::lsbdistcoden
 
   case $ensure {
     'present': {
+      # TODO: much simpler to manage as a file resource in /etc/apt/sources.list.d/ instead of munging
+      # the sources.list file
       exec { "add_repo_$name":
         command => "/bin/echo 'deb $url $dist $repo' >> /etc/apt/sources.list",
         unless  => "/bin/grep -Fxqe 'deb $url $dist $repo' /etc/apt/sources.list",
