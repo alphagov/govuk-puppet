767a75ffd5d5341def1f2533a6fae87d02cc264f
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-07-19 09:48:22 +0000

    templatize puppetdb.conf

--- modules/puppet/manifests/master.pp
+++ modules/puppet/manifests/master.pp
@@ -13,7 +13,7 @@ class puppet::master {
       require => Apt::Deb_repository['puppetlabs-repo']
     }
     file {'/etc/puppet/puppetdb.conf':
-      source => 'puppet:///modules/puppet/etc/puppet/puppetdb.conf'
+      content => template('/puppet/etc/puppet/puppetdb.conf.erb'),
     }
     file {'/etc/puppet/routes.yaml':
       source => 'puppet:///modules/puppet/etc/puppet/routes.yaml'
