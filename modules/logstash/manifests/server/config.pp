1671009bf54bb55979e6fbb1d4157bb6545a6f76
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-07-20 15:16:15 +0100

    * configuring logstash elasticsearch for better performance

--- modules/logstash/manifests/server/config.pp
+++ modules/logstash/manifests/server/config.pp
@@ -8,5 +8,16 @@ class logstash::server::config {
     source  => 'puppet:///modules/logstash/etc/cron.daily/logstash_index_cleaner',
     mode    => '0755'
   }
-
+  file { '/var/apps/elasticsearch-0.18.7/bin/elasticsearch.in.sh' :
+    source  => 'puppet:///modules/logstash/elasticsearch.in.sh',
+    mode    => '0755',
+    require => File['untar-elasticsearch'],
+    notify  => Service['elasticsearch-0-18-7'],
+  }
+  file { '/var/apps/elasticsearch-0.18.7/config/elasticsearch.yml' :
+    source  => 'puppet:///modules/logstash/elasticsearch.yml',
+    mode    => '0644',
+    require => File['untar-elasticsearch'],
+    notify  => Service['elasticsearch-0-18-7'],
+  }
 }
