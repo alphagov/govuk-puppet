9897ebfae260656c29a67bea13b2bf4dab08743d
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-07-20 14:27:31 +0000

    remove dumb test

--- modules/puppet/spec/classes/puppet_spec.rb
+++ modules/puppet/spec/classes/puppet_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../../../spec_helper'
 
 describe 'puppet', :type => :class do
   it { should contain_file('/etc/puppet/puppet.conf') }
-  it { should contain_package("puppet").with_ensure('2.7.3').with_provider('gem') }
+  it { should contain_package("puppet").with_provider('gem') }
   it 'should schedule regular puppet updates' do
     should contain_cron('puppet').
            with_ensure('present').
