46dd74f7fb13fbb9adaee8cc7128b27a6db61825
Nick Stenning <nick@whiteink.com>
2012-07-23 14:56:48 +0100

    Don't enforce ssl for review-o-matic-explore

--- modules/govuk/manifests/apps/review_o_matic_explore.pp
+++ modules/govuk/manifests/apps/review_o_matic_explore.pp
@@ -1,8 +1,9 @@
 class govuk::apps::review_o_matic_explore( $port = 3023 ) {
   govuk::app { 'review-o-matic-explore':
-    config  => true,
-    vhost   => 'explore-reviewomatic',
-    require => Class['nodejs'],
-    port    => $port;
+    port           => $port,
+    config         => true,
+    vhost          => 'explore-reviewomatic',
+    vhost_ssl_only => false,
+    require        => Class['nodejs'];
   }
 }
