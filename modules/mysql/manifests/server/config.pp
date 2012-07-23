fbd669a109e450bbb786fee5b809e163c58def2f
Shodhan Sheth <shodhan.sheth@digital.cabinet-office.gov.uk>
2012-07-23 02:56:20 -0700

    Ensuring /etc/mysql exists

--- modules/mysql/manifests/server/config.pp
+++ modules/mysql/manifests/server/config.pp
@@ -1,9 +1,12 @@
 class mysql::server::config($server_id='1'){
+  file { '/etc/mysql':
+    ensure => 'directory'
+  }
   file { '/etc/mysql/my.cnf':
     owner    => 'mysql',
     group    => 'mysql',
     content  => template('mysql/my.cnf'),
     notify   => Service['mysql'],
-    require  => File['/var/lib/mysql'],
+    require => [File['/var/lib/mysql/my.cnf'],File['/etc/mysql']]
   }
 }
