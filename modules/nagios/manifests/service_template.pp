06b25982e051146def167be9dbf2c4d1968f6630
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-07-23 13:30:07 +0000

    allow configuration of contact groups in nagios::service_template

--- modules/nagios/manifests/service_template.pp
+++ modules/nagios/manifests/service_template.pp
@@ -1,4 +1,4 @@
-define nagios::service_template () {
+define nagios::service_template ($contact_groups) {
   file { "/etc/nagios3/conf.d/service_template_${name}.cfg":
     content => template('nagios/service_template.cfg.erb'),
     owner   => root,
--- modules/nagios/spec/defines/service_template_spec.rb
+++ modules/nagios/spec/defines/service_template_spec.rb
@@ -2,8 +2,10 @@ require_relative '../../../../spec_helper'
 
 describe 'nagios::service_template', :type => :define do
   let(:title) { 'important_service' }
+  let(:params) { {'contact_groups' => ['jims_friends','jims_enemies']}}
   it { should contain_file('/etc/nagios3/conf.d/service_template_important_service.cfg').
-    with_content(/name\s+important_service/)
+    with_content(/name\s+important_service/).
+    with_content(/contact_groups\s+jims_friends,jims_enemies/)
   }
 
 end
