590aebe605698c4e7daa89d1f053acfae94f60c4
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-07-23 09:05:44 +0000

    inform puppet that gmetad's init.d script has no status option

---
+++ modules/ganglia/spec/classes/ganglia__service_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../../../../spec_helper'
+
+describe 'ganglia::service', :type => :class do
+  it { should contain_service('gmetad').with({
+    'ensure'    => 'running',
+    'hasstatus' => false,
+  })}
+end
