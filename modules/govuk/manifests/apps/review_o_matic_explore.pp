8a93cb33c0ab06e1d76a30cb53de45f3e41f8932
Nick Stenning <nick@whiteink.com>
2012-07-23 14:18:56 +0100

    Fix review-o-matic explore vhost

--- modules/govuk/manifests/apps/review_o_matic_explore.pp
+++ modules/govuk/manifests/apps/review_o_matic_explore.pp
@@ -1,8 +1,7 @@
 class govuk::apps::review_o_matic_explore( $port = 3023 ) {
-  include nodejs
-
   govuk::app { 'review-o-matic-explore':
     config  => true,
+    vhost   => 'explore-reviewomatic',
     require => Class['nodejs'],
     port    => $port;
   }
