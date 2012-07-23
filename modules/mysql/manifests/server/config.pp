ee2e25e2c3b81530ab98b55ea78fc9dc0945113f
Shodhan Sheth <shodhan.sheth@digital.cabinet-office.gov.uk>
2012-07-23 03:37:59 -0700

    Satisfying lint

--- modules/mysql/manifests/server/config.pp
+++ modules/mysql/manifests/server/config.pp
@@ -7,6 +7,6 @@ class mysql::server::config($server_id='1'){
     group    => 'mysql',
     content  => template('mysql/my.cnf'),
     notify   => Service['mysql'],
-    require => [File['/var/lib/mysql/my.cnf'],File['/etc/mysql']]
+    require  => [File['/var/lib/mysql/my.cnf'],File['/etc/mysql']]
   }
 }
