e2f76ba6bd86754c3a6dfcd15ab7c01f6133b9d3
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-07-24 15:20:23 +0100

    use undef instead of empty string for absent params

--- modules/nagios/manifests/timeperiod.pp
+++ modules/nagios/manifests/timeperiod.pp
@@ -1,12 +1,12 @@
 define nagios::timeperiod (
   $timeperiod_alias,
-  $mon    = '',
-  $tue    = '',
-  $wed    = '',
-  $thu    = '',
-  $fri    = '',
-  $sat    = '',
-  $sun    = '' ) {
+  $mon    = undef,
+  $tue    = undef,
+  $wed    = undef,
+  $thu    = undef,
+  $fri    = undef,
+  $sat    = undef,
+  $sun    = undef ) {
     $timeperiod_name = $name
     file { "/etc/nagios3/conf.d/timeperiod_${name}.cfg":
       content => template('nagios/timeperiod.cfg.erb'),
