cd68f47f35a0ac14a0c4f4aef2c6a3a049b7c92e
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-07-24 14:07:30 +0100

    fix the range on the rabbitmq.consumers nagios::check

--- modules/logstash/manifests/server/monitoring.pp
+++ modules/logstash/manifests/server/monitoring.pp
@@ -23,7 +23,7 @@ class logstash::server::monitoring {
     host_name           => "${::govuk_class}-${::hostname}",
   }
   @@nagios::check { "check_rabbitmq_consumers_${::hostname}":
-    check_command       => 'check_ganglia_metric!rabbitmq.consumers!1:2.99:!-1:0.99',
+    check_command       => 'check_ganglia_metric!rabbitmq.consumers!1:2.99!-1:0.99',
     service_description => "check rabbitmq has some consumers on ${::hostname}",
     host_name           => "${::govuk_class}-${::hostname}",
   }
