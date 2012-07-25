f32d6a1280e9466a694e45cf1c4c59623dff26d5
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2012-07-25 09:03:55 +0000

    split out nagios::package class

--- modules/nagios/manifests/install.pp
+++ modules/nagios/manifests/install.pp
@@ -1,40 +1,5 @@
 class nagios::install {
-  include apache2
-  package {
-    'nagios3': ensure              => 'installed';
-    'nagios-images': ensure        => 'installed';
-    'nagios-plugins': ensure       => 'installed';
-    'libwww-perl': ensure          => 'installed';
-    'libcrypt-ssleay-perl': ensure => 'installed';
-  }
-  file { '/usr/local/bin/check_ganglia_metric':
-    source => 'puppet:///modules/nagios/check_ganglia_metric',
-    owner  => root,
-    group  => root,
-    mode   => '0755',
-  }
-  file { '/usr/local/bin/reversedns.py':
-    source => 'puppet:///modules/nagios/reversedns.py',
-    owner  => root,
-    group  => root,
-    mode   => '0755',
-  }
-  file { '/usr/local/bin/pagerduty_nagios.pl':
-    source => 'puppet:///modules/nagios/pagerduty_nagios.pl',
-    owner  => root,
-    group  => root,
-    mode   => '0755',
-  }
-  file { '/usr/local/bin/sendEmail':
-    source => 'puppet:///modules/nagios/sendEmail',
-    owner  => root,
-    group  => root,
-    mode   => '0755',
-  }
-  exec { '/usr/bin/easy_install nagaconda':
-    creates => '/usr/local/lib/python2.6/dist-packages/NagAconda-0.1.4-py2.6.egg',
-    require => Package['python-setuptools'],
-  }
+  include nagios::package
   file { '/etc/nagios3/conf.d/check_ganglia_nagios2.cfg':
     source  => 'puppet:///modules/nagios/nagios/check_ganglia_nagios2.cfg',
     owner   => root,
@@ -204,12 +169,6 @@ class nagios::install {
     group   => root,
     mode    => '0644',
     notify  => Service[nagios3],
-    require => Package[nagios3],
-  }
-  file { '/var/log/sendEmail':
-    ensure  => present,
-    owner   => nagios,
-    group   => nagios,
     require => Package[nagios3],
   }
   file { '/etc/nagios3/apache2.conf':
