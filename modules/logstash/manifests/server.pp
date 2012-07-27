33258cc57181fd284cd96d98961ab31e66f228fc
Mark Needham <mark.needham@digital.cabinet-office.gov.uk>
2012-07-27 07:54:50 +0000

    rewriting the dependencies of logstash so that config happens after package.

--- modules/logstash/manifests/server.pp
+++ modules/logstash/manifests/server.pp
@@ -1,11 +1,11 @@
 class logstash::server {
   class { 'logstash::server::package': }
-  class { 'logstash::server::config':
-    before    => Class['logstash::server::package']
-  }
-  class { 'logstash::server::service':
-    subscribe => [Class['logstash::server::package'],Class['logstash::server::config']],
-    before    => Class['logstash::server::monitoring']
-  }
+  class { 'logstash::server::config':  }
+  class { 'logstash::server::service': }
   class { 'logstash::server::monitoring': }
+
+  Class['logstash::server::package'] -> Class['logstash::server::config'] ~> Class['logstash::server::service']
+  Class['logstash::server::service'] -> Class['logstash::server::monitoring']
+  Class['logstash::server::package'] -> Class['logstash::server::service']
+  Class['logstash::server::package'] ~> Class['logstash::server::service']
 }
