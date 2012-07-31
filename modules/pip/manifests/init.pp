c20c7a546806eee5c0b4061c1c1147afb2caf2d4
Nick Stenning <nick@whiteink.com>
2012-07-31 18:19:24 +0100

    Add missing dep on govuk::repository for latest pip

--- modules/pip/manifests/init.pp
+++ modules/pip/manifests/init.pp
@@ -1,7 +1,7 @@
 class pip {
   package { 'python-pip':
     ensure  => '1.1',
-    require => [Package['python'], Package['python-setuptools']];
+    require => [Package['python'], Package['python-setuptools'], Class['govuk::repository']];
   }
 
   Package['python-pip'] -> Package <| provider == 'pip' and ensure != absent |>
