d7b8e5a9ce384eb9103ff988258312f8558965c3
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-01 18:36:34 +0100

    set more realistic thresholds for app mem usage

--- modules/govuk/manifests/app.pp
+++ modules/govuk/manifests/app.pp
@@ -157,7 +157,7 @@ define govuk::app::monitoring {
     host_name           => "${::govuk_class}-${::hostname}",
   }
   @@nagios::check { "check_${title}_unicorn_mem_usage${::hostname}":
-    check_command       => "check_ganglia_metric!procstat_${title}_mem!100000000!200000000",
+    check_command       => "check_ganglia_metric!procstat_${title}_mem!1000000000!2000000000",
     service_description => "Check the mem used by unicorn ${title} isnt too high",
     host_name           => "${::govuk_class}-${::hostname}",
   }
