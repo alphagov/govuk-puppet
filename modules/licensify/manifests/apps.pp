2171ce61f3b3ab506bb40ed000291d3876a6ad51
Nick Stenning <nick@whiteink.com>
2012-08-01 14:36:24 +0100

    Fix licensify environment

--- modules/licensify/manifests/apps.pp
+++ modules/licensify/manifests/apps.pp
@@ -8,7 +8,7 @@ class licensify::apps::licensify( $port = 9000 ) {
   govuk::app { 'licensify':
     app_type           => 'procfile',
     port               => $port,
-    environ_content    => 'LANG=en_GB.UTF-8\n',
+    environ_content    => template('licensify/environ'),
     nginx_extra_config => template('licensify/nginx_extra'),
   }
 }
@@ -17,7 +17,7 @@ class licensify::apps::licensify_admin( $port = 9500 ) {
   govuk::app { 'licensify-admin':
     app_type        => 'procfile',
     port            => $port,
-    environ_content => 'LANG=en_GB.UTF-8\n',
+    environ_content => template('licensify/environ'),
   }
 }
 
@@ -25,6 +25,6 @@ class licensify::apps::licensify_feed( $port = 9400 ) {
   govuk::app { 'licensify-feed':
     app_type        => 'procfile',
     port            => $port,
-    environ_content => 'LANG=en_GB.UTF-8\n',
+    environ_content => template('licensify/environ'),
   }
 }
