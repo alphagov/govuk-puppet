f94df56c003497725cc66e9689cf7e11a43fbbfc
David Illsley <david@illsley.org>
2012-08-03 09:31:52 +0100

    Turn on build cleanup for real

--- modules/licensify/manifests/build_clean.pp
+++ modules/licensify/manifests/build_clean.pp
@@ -1,6 +1,6 @@
 define licensify::build_clean {
   cron { "licensify-build-clean-$title":
-    command => "ls -dt1 '/data/vhost/${title}.${::govuk_platform}.alphagov.co.uk/${title}'-* | tail -n +11 | while read dir; do echo rm -rf \"\${dir}\"; done",
+    command => "ls -dt1 '/data/vhost/${title}.${::govuk_platform}.alphagov.co.uk/${title}'-* | tail -n +11 | while read dir; do rm -rf \"\${dir}\"; done",
     user    => 'deploy',
     require => User['deploy'],
     hour    => '*/6'
