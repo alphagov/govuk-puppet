b91309e0da3d45fc53e49b3df1f07f93a3b7c375
Nick Stenning <nick@whiteink.com>
2012-08-09 16:46:45 +0100

    Run cronjob against correct elasticsearch index

--- modules/logstash/manifests/server/config.pp
+++ modules/logstash/manifests/server/config.pp
@@ -6,13 +6,20 @@ class logstash::server::config (
   include logstash::config
 
 
-  file { '/etc/logstash/logstash-server.conf' :
+  file { '/etc/logstash/logstash-server.conf':
     content => template('logstash/etc/logstash/logstash-server.conf.erb'),
   }
 
-  file { '/etc/cron.daily/logstash_index_cleaner' :
+  file { '/var/apps/logstash/logstash_index_cleaner':
     source => 'puppet:///modules/logstash/etc/cron.daily/logstash_index_cleaner',
     mode   => '0755',
+  }
+
+  cron { "logstash-clean-indices"
+    command => "/var/apps/logstash/logstash_index_cleaner --port ${http_port} --days-to-keep 7",
+    user    => 'logstash',
+    minute  => '34',
+    hour    => '2',
   }
 
   elasticsearch::node { 'logstash-server':
