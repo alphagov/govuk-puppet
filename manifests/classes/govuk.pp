134f9549377df23bdc8b5a818053964fee2827b0
shodhan sheth <shodhan.sheth@digital.cabinet-office.gov.co.uk>
2012-08-10 11:54:20 +0000

    Adding comment, to make hack less of a nsaty surprise

--- manifests/classes/govuk.pp
+++ manifests/classes/govuk.pp
@@ -216,6 +216,9 @@ class govuk_base::cache_server inherits govuk_base {
 
   class { 'nginx': node_type => router}
 
+  # Have realised that this purge does not kick in the first puppet run[Newly provisioned machines]
+  # Needs to be fixed. Unsure of how apache sneaks in. 
+  # Kicks in the the next puppet run
   package { 'apache2':
     ensure => absent,
   }
