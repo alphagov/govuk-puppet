8c70752c87eb0519150e7cf17b146c97847b1460
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-11 12:00:30 +0100

    add new preview-graylog ip to reversedns.py

--- modules/nagios/files/reversedns.py
+++ modules/nagios/files/reversedns.py
@@ -29,7 +29,7 @@ if sys.argv[1] == "ip-10-236-86-54.eu-west-1.compute.internal":
         print "frontend.production.alphagov.co.uk"
 
 # hacks to pickup correct graphs, due to local hosts and ganglia name mismatch
-if sys.argv[1] in ['ip-10-54-182-112.eu-west-1.compute.internal', 'ip-10-236-86-54.eu-west-1.compute.internal', 'ip-10-250-157-37.eu-west-1.compute.internal', 'ip-10-53-54-49.eu-west-1.compute.internal']:
+if sys.argv[1] in ['ip-10-54-182-112.eu-west-1.compute.internal', 'ip-10-236-86-54.eu-west-1.compute.internal', 'ip-10-250-157-37.eu-west-1.compute.internal', 'ip-10-53-54-49.eu-west-1.compute.internal', 'ip-10-32-31-104.eu-west-1.compute.internal' ]:
         print sys.argv[1]
         exit(0)
 try:
