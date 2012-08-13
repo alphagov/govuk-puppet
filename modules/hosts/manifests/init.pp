25188250ab58299701c94b74ecad881d45e03399
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-13 11:25:37 +0100

    Revert "add different hosts file for skyscape provider"
    This reverts commit 76de3925d17a6f829bb219125ee1b9a9703aa077.

--- modules/hosts/manifests/init.pp
+++ modules/hosts/manifests/init.pp
@@ -1,15 +1,7 @@
 class hosts {
-  case $::govuk_provider {
-    sky:
-      case $::govuk_platform {
-        production: { include hosts::skyscape::production }
-        default:    { include hosts::skyscape::production }
-      }
-    default:
-      case $::govuk_platform {
-        production: { include hosts::production }
-        preview:    { include hosts::preview }
-        default:    { include hosts::development }
-      }
+  case $::govuk_platform {
+    production: { include hosts::production }
+    preview:    { include hosts::preview }
+    default:    { include hosts::development }
   }
 }
--- modules/hosts/manifests/skyscape/production.pp
+++
@@ -1,3 +0,0 @@
-class hosts::skyscape::production {
-  host { 'puppet':     ip => '10.0.0.6' }
-}
