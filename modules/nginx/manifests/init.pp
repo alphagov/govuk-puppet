49d671e45ec3de0192cd1a2097c29a5bf2012943
Nick Stenning <nick@whiteink.com>
2012-08-13 15:13:13 +0100

    Rejig monitoring vhost so it never fails

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -20,4 +20,21 @@ class nginx($node_type = 'UNSET') {
   anchor { 'nginx::end':
     require => Class['nginx::service'],
   }
+
+  # Monitoring of NginX
+  nginx::config::site { 'monitoring-vhost.test':
+    source  => 'puppet:///modules/nginx/sites/monitoring-vhost-nginx.conf',
+  }
+
+  @@nagios::check { "check_nginx_running_${::hostname}":
+    check_command       => 'check_nrpe!check_proc_running!nginx',
+    service_description => "check nginx on ${::govuk_class}-${::hostname}",
+    host_name           => "${::govuk_class}-${::hostname}",
+  }
+
+  @@nagios::check { "check_http_response_${::hostname}":
+    check_command       => 'check_http_port!monitoring-vhost.test!5!10',
+    service_description => "check HTTP response on ${::govuk_class}-${::hostname}",
+    host_name           => "${::govuk_class}-${::hostname}",
+  }
 }
