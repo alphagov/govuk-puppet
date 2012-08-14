c2a44fee0e5af5732640b79a3314a9b7caa4b44a
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-14 10:33:20 +0100

    add all the machines and services I know about in the mgmt zone

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -1,4 +1,12 @@
 class hosts::skyscape::production {
+  # These are Services
   host { 'puppet.production.internal':     ip => '10.0.0.6', host_aliases  => 'puppet' }
-  host { 'monitoring.production.internal': ip => '10.0.0.20', host_aliases => [ 'monitoring', 'monitoring.cluster' ] }
+  host { 'jenkins.production.internal':    ip => '10.0.0.8', host_aliases  => 'jenkins' }
+  host { 'monitoring.production.internal': ip => '10.0.0.20', host_aliases => [ 'monitoring', 'monitoring.cluster'] }
+  # These are Hosts:
+  host { 'ip-10-0-0-6.mgmt-sky.internal':  ip => '10.0.0.6',  host_aliases => [ 'ip-10-0-0-6' ] }
+  host { 'ip-10-0-0-8.mgmt-sky.internal':  ip => '10.0.0.8',  host_aliases => [ 'ip-10-0-0-8' ] }
+  host { 'ip-10-0-0-11.mgmt-sky.internal': ip => '10.0.0.11', host_aliases => [ 'ip-10-0-0-11' ] }
+  host { 'ip-10-0-0-12.mgmt-sky.internal': ip => '10.0.0.12', host_aliases => [ 'ip-10-0-0-12' ] }
+  host { 'ip-10-0-0-20.mgmt-sky.internal': ip => '10.0.0.20', host_aliases => [ 'ip-10-0-0-20' ] }
 }
