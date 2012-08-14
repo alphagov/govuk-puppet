a431739866104b01d80d8e17d3ac44d35c66f5b0
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-14 08:15:33 +0100

    Add an alias for monitoring.cluster to get Ganglia working

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -1,4 +1,4 @@
 class hosts::skyscape::production {
   host { 'puppet':     ip => '10.0.0.6', host_aliases  => 'puppet.prodution.internal' }
-  host { 'monitoring': ip => '10.0.0.20', host_aliases => 'monitoring.production.internal' }
+  host { 'monitoring': ip => '10.0.0.20', host_aliases => [ 'monitoring.production.internal', 'monitoring.cluster' ] }
 }
