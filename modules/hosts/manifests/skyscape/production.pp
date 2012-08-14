296c495da9c7ddbc2fb2dc228b8118e9b28e3dde
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-14 13:44:40 +0100

    We should define nodes and services on the same line

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -1,12 +1,8 @@
 class hosts::skyscape::production {
-  # These are Services
-  host { 'puppet.production.internal':     ip => '10.0.0.6', host_aliases  => 'puppet' }
-  host { 'jenkins.production.internal':    ip => '10.0.0.8', host_aliases  => 'jenkins' }
-  host { 'monitoring.production.internal': ip => '10.0.0.20', host_aliases => [ 'monitoring', 'monitoring.cluster'] }
-  # These are Hosts:
-  host { 'ip-10-0-0-6.mgmt-sky.internal':  ip => '10.0.0.6',  host_aliases => [ 'ip-10-0-0-6' ] }
-  host { 'ip-10-0-0-8.mgmt-sky.internal':  ip => '10.0.0.8',  host_aliases => [ 'ip-10-0-0-8' ] }
-  host { 'ip-10-0-0-11.mgmt-sky.internal': ip => '10.0.0.11', host_aliases => [ 'ip-10-0-0-11' ] }
-  host { 'ip-10-0-0-12.mgmt-sky.internal': ip => '10.0.0.12', host_aliases => [ 'ip-10-0-0-12' ] }
-  host { 'ip-10-0-0-20.mgmt-sky.internal': ip => '10.0.0.20', host_aliases => [ 'ip-10-0-0-20' ] }
+  # These are real hosts (1-1 mapping between Host and Service)
+  host { 'ip-10-0-0-6.mgmt-sky.internal':  ip => '10.0.0.6',  host_aliases => [ 'ip-10-0-0-6', 'puppet', 'puppet.production.internal' ] }
+  host { 'ip-10-0-0-8.mgmt-sky.internal':  ip => '10.0.0.8',  host_aliases => [ 'ip-10-0-0-8', 'jenkins', 'jenkins.production.internal' ] }
+  host { 'ip-10-0-0-11.mgmt-sky.internal': ip => '10.0.0.11', host_aliases => [ 'ip-10-0-0-11', 'jumpbox1' ] }
+  host { 'ip-10-0-0-12.mgmt-sky.internal': ip => '10.0.0.12', host_aliases => [ 'ip-10-0-0-12', 'jumpbox2' ] }
+  host { 'ip-10-0-0-20.mgmt-sky.internal': ip => '10.0.0.20', host_aliases => [ 'ip-10-0-0-20', 'monitoring', 'monitoring.cluster', 'monitoring.production.internal' ] }
 }
