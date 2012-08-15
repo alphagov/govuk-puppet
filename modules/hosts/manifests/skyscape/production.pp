3f5c84ae61f7ea259da6bfb9546b632fb99f42ab
shodhan sheth <shodhan.sheth@digital.cabinet-office.gov.co.uk>
2012-08-15 12:56:48 +0000

    Adding router hosts for skyscape

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -7,7 +7,13 @@ class hosts::skyscape::production {
   host { 'ip-10-0-0-12.mgmt-sky.internal':    ip => '10.0.0.12',  host_aliases => [ 'ip-10-0-0-12', 'jumpbox2' ] }
   host { 'ip-10-0-0-20.mgmt-sky.internal':    ip => '10.0.0.20',  host_aliases => [ 'ip-10-0-0-20', 'monitoring', 'monitoring.production.internal', 'monitoring.cluster' ] }
   host { 'ip-10-0-0-21.mgmt-sky.internal':    ip => '10.0.0.21',  host_aliases => [ 'ip-10-0-0-21', 'logging', 'logging.production.internal', 'graylog.cluster' ] }
+
   host { 'ip-10-2-0-2.frontend-sky.internal': ip => '10.2.0.2',   host_aliases => [ 'ip-10-2-0-2', 'production-frontend'] }
   host { 'ip-10-2-0-3.frontend-sky.internal': ip => '10.2.0.3',   host_aliases => [ 'ip-10-2-0-3', 'production-frontend-1'] }
   host { 'ip-10-2-0-4.frontend-sky.internal': ip => '10.2.0.4',   host_aliases => [ 'ip-10-2-0-4', 'production-frontend-2'] }
+
+  #TODO: change router.cluster to load balanced cluster ip
+  host { 'ip-10-1-0-3.router-sky.internal':   ip => '10.1.0.3',   host_aliases => [ 'ip-10-1-0-3', 'production-cache', 'router.cluster'] }
+  host { 'ip-10-1-0-4.router-sky.internal':   ip => '10.1.0.4',   host_aliases => [ 'ip-10-1-0-4', 'production-cache-1'] }
+  host { 'ip-10-1-0-5.router-sky.internal':   ip => '10.1.0.5',   host_aliases => [ 'ip-10-1-0-5', 'production-cache-2'] }
 }
