ffb5d879961552f113d319b8a6227e2ee679c327
shodhan sheth <shodhan.sheth@digital.cabinet-office.gov.co.uk>
2012-08-15 11:08:26 +0000

    Fixing lint

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -1,13 +1,13 @@
 class hosts::skyscape::production {
   # These are real hosts (1-1 mapping between Host and Service)
   # Anything that ends .cluster is maintained for backwards compatibility with EC2
-  host { 'ip-10-0-0-6.mgmt-sky.internal':  ip => '10.0.0.6',  host_aliases => [ 'ip-10-0-0-6', 'puppet', 'puppet.production.internal' ] }
-  host { 'ip-10-0-0-8.mgmt-sky.internal':  ip => '10.0.0.8',  host_aliases => [ 'ip-10-0-0-8', 'jenkins', 'jenkins.production.internal' ] }
-  host { 'ip-10-0-0-11.mgmt-sky.internal': ip => '10.0.0.11', host_aliases => [ 'ip-10-0-0-11', 'jumpbox1' ] }
-  host { 'ip-10-0-0-12.mgmt-sky.internal': ip => '10.0.0.12', host_aliases => [ 'ip-10-0-0-12', 'jumpbox2' ] }
-  host { 'ip-10-0-0-20.mgmt-sky.internal': ip => '10.0.0.20', host_aliases => [ 'ip-10-0-0-20', 'monitoring', 'monitoring.production.internal', 'monitoring.cluster' ] }
-  host { 'ip-10-0-0-21.mgmt-sky.internal': ip => '10.0.0.21', host_aliases => [ 'ip-10-0-0-21', 'logging', 'logging.production.internal', 'graylog.cluster' ] }
-  host { 'ip-10-2-0-2.frontend-sky.internal': ip => '10.2.0.2', host_aliases => [ 'ip-10-2-0-2', 'production-frontend'] }
-  host { 'ip-10-2-0-3.frontend-sky.internal': ip => '10.2.0.3', host_aliases => [ 'ip-10-2-0-3', 'production-frontend-1'] }
-  host { 'ip-10-2-0-4.frontend-sky.internal': ip => '10.2.0.4', host_aliases => [ 'ip-10-2-0-4', 'production-frontend-2'] }
+  host { 'ip-10-0-0-6.mgmt-sky.internal':     ip => '10.0.0.6',   host_aliases => [ 'ip-10-0-0-6', 'puppet', 'puppet.production.internal' ] }
+  host { 'ip-10-0-0-8.mgmt-sky.internal':     ip => '10.0.0.8',   host_aliases => [ 'ip-10-0-0-8', 'jenkins', 'jenkins.production.internal' ] }
+  host { 'ip-10-0-0-11.mgmt-sky.internal':    ip => '10.0.0.11',  host_aliases => [ 'ip-10-0-0-11', 'jumpbox1' ] }
+  host { 'ip-10-0-0-12.mgmt-sky.internal':    ip => '10.0.0.12',  host_aliases => [ 'ip-10-0-0-12', 'jumpbox2' ] }
+  host { 'ip-10-0-0-20.mgmt-sky.internal':    ip => '10.0.0.20',  host_aliases => [ 'ip-10-0-0-20', 'monitoring', 'monitoring.production.internal', 'monitoring.cluster' ] }
+  host { 'ip-10-0-0-21.mgmt-sky.internal':    ip => '10.0.0.21',  host_aliases => [ 'ip-10-0-0-21', 'logging', 'logging.production.internal', 'graylog.cluster' ] }
+  host { 'ip-10-2-0-2.frontend-sky.internal': ip => '10.2.0.2',   host_aliases => [ 'ip-10-2-0-2', 'production-frontend'] }
+  host { 'ip-10-2-0-3.frontend-sky.internal': ip => '10.2.0.3',   host_aliases => [ 'ip-10-2-0-3', 'production-frontend-1'] }
+  host { 'ip-10-2-0-4.frontend-sky.internal': ip => '10.2.0.4',   host_aliases => [ 'ip-10-2-0-4', 'production-frontend-2'] }
 }
