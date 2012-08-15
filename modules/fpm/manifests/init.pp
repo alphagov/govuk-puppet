140c0cee31fa18b61a337caeaa3bf6e1678ae5a4
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-15 12:51:58 +0100

    fix trailing whitespace

--- modules/fpm/manifests/init.pp
+++ modules/fpm/manifests/init.pp
@@ -1,5 +1,5 @@
 class fpm {
   package {
-    'fpm': ensure => '0.4.10', provider => gem, require => Package['libc6-dev']; 
+    'fpm': ensure => '0.4.10', provider => gem, require => Package['libc6-dev'];
   }
 }
