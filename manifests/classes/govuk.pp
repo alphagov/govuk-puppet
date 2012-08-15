68066c7839d78509a9b8fdf128c6a4e7cbf85c7e
Mark Needham <m.h.needham@gmail.com>
2012-08-15 17:04:06 +0100

    adding needotron as an app to backend so deployment will work on skyscape

--- manifests/classes/govuk.pp
+++ manifests/classes/govuk.pp
@@ -193,6 +193,9 @@ class govuk_base::ruby_app_server::backend_server inherits govuk_base::ruby_app_
   include govuk::apps::review_o_matic_explore
   include govuk::apps::tariff_api
   include govuk::apps::whitehall_admin
+  if($::govuk_provider == 'sky') {
+    include govuk::apps::need_o_tron
+  }
 }
 
 class govuk_base::ruby_app_server::frontend_server inherits govuk_base::ruby_app_server {
--- modules/govuk/manifests/apps/need_o_tron.pp
+++ modules/govuk/manifests/apps/need_o_tron.pp
@@ -1,2 +1,6 @@
-class govuk::apps::need_o_tron {
+class govuk::apps::need_o_tron($port = '4000') {
+  govuk::app { 'need_o_tron':
+    app_type => 'rack',
+    port     => $port;
+  }
 }
