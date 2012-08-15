a082f97545e4923511b2064dff97b0e356e28835
Philip Potter <philipgpotter@gmail.com>
2012-08-15 11:39:47 +0100

    remove unused parameter

--- modules/nagios/manifests/contact.pp
+++ modules/nagios/manifests/contact.pp
@@ -1,14 +1,13 @@
 define nagios::contact (
   $email,
   $service_notification_options = 'w,u,c,r',
-  $notification_period          = '24x7',
-  $config_template              = 'nagios/contact.cfg.erb'
+  $notification_period          = '24x7'
 ) {
 
   $contact_email = $email
 
   file {"/etc/nagios3/conf.d/contact_${name}.cfg":
-    content => template($config_template),
+    content => template('nagios/contact.cfg.erb'),
     require => Class['nagios::package'],
     notify  => Class['nagios::service'],
   }
