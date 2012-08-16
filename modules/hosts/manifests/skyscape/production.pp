027a27674a667549b3bcb19a391d07554d478729
Mark Needham <m.h.needham@gmail.com>
2012-08-16 14:43:07 +0100

    adding a host entry for our mysql master node

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -25,4 +25,6 @@ class hosts::skyscape::production {
   host { 'ip-10-3-0-6.backend-sky.internal':    ip => '10.3.0.6',   host_aliases => [ 'ip-10-3-0-6', 'mongo-1.production.internal', 'mongo.cluster'] }
   host { 'ip-10-3-0-7.backend-sky.internal':    ip => '10.3.0.7',   host_aliases => [ 'ip-10-3-0-7', 'mongo-2.production.internal'] }
   host { 'ip-10-3-0-8.backend-sky.internal':    ip => '10.3.0.8',   host_aliases => [ 'ip-10-3-0-8', 'mongo-3.production.internal'] }
+
+  host { 'ip-10-3-0-30.backend-sky.internal':   ip => '10.3.0.30',  host_aliases => [ 'ip-10-3-0-30', 'mysql-master', 'mysql-master.production.internal'] }
 }
