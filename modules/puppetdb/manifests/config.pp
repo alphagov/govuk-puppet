660203206f49a593159fd914bd5e5826ccabf96e
Philip Potter <philipgpotter@gmail.com>
2012-08-17 10:07:21 +0100

    fix puppetdb template which broke when govuk_provider fact wasnt present

--- modules/puppetdb/manifests/config.pp
+++ modules/puppetdb/manifests/config.pp
@@ -1,4 +1,9 @@
 class puppetdb::config {
+  $java_args = $govuk_provider ? {
+    'sky'   => '-Xmx1024m',
+    default => '-Xmx192m',
+  }
+
   file { '/etc/default/puppetdb':
     content => template('puppetdb/etc/default/puppetdb'),
   }
