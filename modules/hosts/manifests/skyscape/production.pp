17ecd6ec635f6399b7ff9194b65bf80e3ae23948
shodhan sheth <shodhan.sheth@digital.cabinet-office.gov.co.uk>
2012-08-17 13:16:35 +0000

    Adding router.cluster as hostname for loadbalnced router

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -13,13 +13,14 @@ class hosts::skyscape::production {
   host { 'ip-10-2-0-4.frontend-sky.internal':   ip => '10.2.0.4',   host_aliases => [ 'ip-10-2-0-4', 'production-frontend-2'] }
 
   #TODO: change router.cluster to load balanced cluster ip
-  host { 'ip-10-1-0-3.router-sky.internal':     ip => '10.1.0.3',   host_aliases => [ 'ip-10-1-0-3', 'production-cache', 'router.cluster'] }
+  host { 'ip-10-1-0-3.router-sky.internal':     ip => '10.1.0.3',   host_aliases => [ 'ip-10-1-0-3', 'production-cache'] }
   host { 'ip-10-1-0-4.router-sky.internal':     ip => '10.1.0.4',   host_aliases => [ 'ip-10-1-0-4', 'production-cache-1'] }
   host { 'ip-10-1-0-5.router-sky.internal':     ip => '10.1.0.5',   host_aliases => [ 'ip-10-1-0-5', 'production-cache-2'] }
+  host { 'router.cluster':     ip => '172.16.254.50' }
 
   #TODO: change panopticon.production to load balanced cluster ip
-  host { 'ip-10-3-0-2.backend-sky.internal':    ip => '10.3.0.2',   host_aliases => [ 'ip-10-3-0-2', 'production-backend', 'panopticon.production.alphagov.co.uk',
-                                                                                      'whiethall-admin.production.alphagov.co.uk'] }
+  host { 'ip-10-3-0-2.backend-sky.internal':    ip => '10.3.0.2',   host_aliases => [ 'production-backend', 'panopticon.production.alphagov.co.uk',
+                                                                                      'whitehall-admin.production.alphagov.co.uk'] }
   host { 'ip-10-3-0-3.backend-sky.internal':    ip => '10.3.0.3',   host_aliases => [ 'ip-10-3-0-3', 'production-backend-1'] }
   host { 'ip-10-3-0-4.backend-sky.internal':    ip => '10.3.0.4',   host_aliases => [ 'ip-10-3-0-4', 'production-backend-2'] }
   host { 'ip-10-3-0-5.backend-sky.internal':    ip => '10.3.0.5',   host_aliases => [ 'ip-10-3-0-5', 'production-support', 'support.cluster'] }
