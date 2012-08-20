f84e364b22618cf02254a75261dd7cdc194ead32
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-20 22:00:05 +0100

    install different python versions for 1004 and 1204

--- modules/pip/manifests/init.pp
+++ modules/pip/manifests/init.pp
@@ -1,8 +1,19 @@
 class pip {
+
+  case $::lsbdistcodename {
+    'precise': {
+        $version = '1.0-1build1'
+    }
+    default: {
+        $version = '1.1'
+    }
+  }
+
   package { 'python-pip':
-    ensure  => '1.1',
+    ensure  => $version,
     require => [Package['python'], Package['python-setuptools'], Class['govuk::repository']];
   }
 
   Package['python-pip'] -> Package <| provider == 'pip' and ensure != absent |>
+
 }
