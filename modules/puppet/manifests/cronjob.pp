a1b7f9f5cccb7357d56bfceadbf02f398ac855da
shodhan sheth <shodhan.sheth@digital.cabinet-office.gov.co.uk>
2012-08-22 08:16:32 +0000

    Removing crons from sky. Enable them once puppetmaster performance issues are fixed

--- modules/puppet/manifests/cronjob.pp
+++ modules/puppet/manifests/cronjob.pp
@@ -3,12 +3,24 @@ class puppet::cronjob {
   $first = fqdn_rand_fixed(30)
   $second = $first + 30
 
-  cron { 'puppet':
-    ensure  => present,
-    user    => 'root',
-    minute  => [$first, $second],
-    command => '/usr/bin/puppet agent --onetime --no-daemonize; /bin/chmod 644 /var/lib/puppet/state/*.yaml',
-    require => Package['puppet'];
+  case $::govuk_provider {
+    sky: {
+      cron { 'puppet':
+        ensure  => absent,
+        user    => 'root',
+        minute  => [$first, $second],
+        command => '/usr/bin/puppet agent --onetime --no-daemonize; /bin/chmod 644 /var/lib/puppet/state/*.yaml',
+        require => Package['puppet'];
+      }
+    }
+    default: {
+      cron { 'puppet':
+        ensure  => present,
+        user    => 'root',
+        minute  => [$first, $second],
+        command => '/usr/bin/puppet agent --onetime --no-daemonize; /bin/chmod 644 /var/lib/puppet/state/*.yaml',
+        require => Package['puppet'];
+      }
+    }
   }
-
 }
