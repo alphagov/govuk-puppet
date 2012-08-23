2421b1812f59f6169403c46f11eb6d19bca5e566
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-23 15:36:55 +0100

    The mysql::server::db module now handles blank passwords and the unless test works

--- modules/mysql/manifests/server/db.pp
+++ modules/mysql/manifests/server/db.pp
@@ -1,16 +1,37 @@
 define mysql::server::db($user, $password, $host, $root_password, $remote_host='%') {
+
+  case $password {
+    '': {
+        $passarg = ''
+    }
+    default: {
+        $passarg = "-p'${password}'"
+    }
+  }
+
+  case $root_password {
+    '': {
+        $rootpassarg = ''
+    }
+    default: {
+        $rootpassarg = "-p'${root_password}'"
+    }
+  }
+
+
   exec { "create-${name}-db":
-    unless  => "/usr/bin/mysql -h ${host} -uroot -p${root_password} -e 'use ${name}'",
-    command => "/usr/bin/mysql -h ${host} -uroot -p${root_password} -e \'create database ${name};\'",
+    unless  => "/usr/bin/mysql -h ${host} -uroot ${rootpassarg} -e 'use ${name}'",
+    command => "/usr/bin/mysql -h ${host} -uroot ${rootpassarg} -e \'create database ${name};\'",
     require => Class['mysql::server'],
   }
 
   exec { "grant-${name}-db":
-    unless  => "/usr/bin/mysql -h ${host} -u${user} -p'${password}' ${name}",
-    command => "/usr/bin/mysql -h ${host} -uroot -p'${root_password}' -e \"grant all on ${name}.* to ${user}@'${remote_host}' identified by '$password'; flush privileges;\"",
+    unless  => "/usr/bin/mysql -h ${::ipaddress} -u${user} ${passarg} ${name}",
+    command => "/usr/bin/mysql -h ${host} -uroot ${rootpassarg} -e \"grant all on ${name}.* to ${user}@'${remote_host}' identified by '$password'; flush privileges;\"",
     require => [
       Class['mysql::server'],
       Exec["create-${name}-db"]
     ],
   }
+
 }
