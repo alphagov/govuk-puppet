86fc6417ff548717a2bb6067fb28899b784935d4
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-23 12:12:28 +0100

    Add Java for Ubuntu 12.04

--- modules/java/manifests/init.pp
+++ modules/java/manifests/init.pp
@@ -3,15 +3,29 @@ class java(
   $version      = 'installed'
 ) {
 
-  $distribution_debian = $distribution ? {
-    jdk => 'sun-java6-jdk',
-    jre => 'sun-java6-jre',
+  case $::lsbdistcodename {
+    'precise': {
+      $owner = 'webupd8team'
+      $repo  = 'java'
+      $distribution_debian = $distribution ? {
+          jdk     => 'oracle-java7-installer',
+          jre     => 'oracle-java7-installer',
+      }
+    }
+    default: {
+      $owner = 'sun-java-community-team'
+      $repo  = 'sun-java6'
+      $distribution_debian = $distribution ? {
+          jdk     => 'sun-java6-jdk',
+          jre     => 'sun-java6-jre',
+      }
+    }
   }
 
   apt::repository { 'sun-java-community-team':
     type  => 'ppa',
-    owner => 'sun-java-community-team',
-    repo  => 'sun-java6',
+    owner => $owner,
+    repo  => $repo,
   }
 
   file { '/var/local/sun-java6.preseed':
