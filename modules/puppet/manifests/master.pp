0495aa0f70666a4fbab02a7bd3ebf88a030bf9b9
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-08-24 10:19:15 +0000

    split puppet::master into ::package, ::config, ::service

--- modules/puppet/manifests/master.pp
+++ modules/puppet/manifests/master.pp
@@ -1,8 +1,14 @@
-class puppet::master($unicorn_port='9090') {
+class puppet::master::package {
+  anchor {'puppet::master::package::begin':
+      before => [
+         Class['puppet::repository'],
+         Class['nginx'],
+         Class['unicornherder'],
+         ]
+  }
   include puppet::repository
   include nginx
   include unicornherder
-  include puppet::master::config::nginx
   package { 'unicorn':
     provider => gem,
   }
@@ -10,6 +16,40 @@ class puppet::master($unicorn_port='9090') {
     command => 'gem install rack --no-rdoc --no-ri --version 1.0.1',
     unless  => 'gem list | grep "rack.*1.0.1"'
   }
+  package { 'puppetdb-terminus':
+    ensure  => present,
+  }
+  file {'/var/run/puppetmaster':
+    ensure => directory,
+    owner  => 'puppet',
+    group  => 'puppet',
+  }
+  file { '/etc/init/puppetmaster.conf':
+    content => template('puppet/etc/init/puppetmaster.conf.erb'),
+  }
+
+  anchor {'puppet::master::package::end':
+      require => [
+         Class['puppet::repository'],
+         Class['nginx'],
+         Class['unicornherder'],
+         ]
+  }
+}
+
+class puppet::master::config ($unicorn_port = '9090') {
+  anchor {'puppet::master::config::begin':
+      before => [
+          Class['puppet::master::config::nginx'],
+      ]
+  }
+  include puppet::master::config::nginx
+  anchor {'puppet::master::config::end':
+      subscribe => [
+          Class['puppet::master::config::nginx'],
+      ]
+  }
+
   file { '/etc/puppet/config.ru':
     require => Exec['install rack 1.0.1'],
     source  => 'puppet:///modules/puppet/etc/puppet/config.ru'
@@ -17,37 +57,37 @@ class puppet::master($unicorn_port='9090') {
 
   file {'/etc/puppet/unicorn.conf':
     content => "worker_processes 4\n",
-  }
-
-  file {'/var/run/puppetmaster':
-    ensure => directory,
-    owner  => 'puppet',
-    group  => 'puppet',
-  }
-
-  package { 'puppetdb-terminus':
-    ensure  => present,
-  }
-
-  package { ['mysql-server-5.1','mysql-client-5.1']: #old storedconfigs, before we used puppetdb
-    ensure => purged
-  }
-
+  }    
   file {'/etc/puppet/puppetdb.conf':
     content => template('puppet/etc/puppet/puppetdb.conf.erb'),
   }
   file {'/etc/puppet/routes.yaml':
     source => 'puppet:///modules/puppet/etc/puppet/routes.yaml'
   }
+}
 
-  file { '/etc/init/puppetmaster.conf':
-    content => template('puppet/etc/init/puppetmaster.conf.erb'),
-    require => Package['puppet'],
-  }
-
+class puppet::master::service {
   service { 'puppetmaster':
     ensure   => running,
     provider => upstart,
     require  => File['/etc/init/puppetmaster.conf'],
-  }
+  }    
+}
+
+class puppet::master($unicorn_port='9090') {
+    include puppet::master::package, puppet::master::config, puppet::master::service
+    anchor {'puppet::master::begin':
+        before => [
+            Class['puppet::master::package'],
+            Class['puppet::master::config'],
+            Class['puppet::master::service'],
+        ]
+    }
+    anchor {'puppet::master::end':
+        subscribe => [
+            Class['puppet::master::package'],
+            Class['puppet::master::config'],
+            Class['puppet::master::service'],
+        ]
+    }
 }
--- modules/puppet/spec/classes/puppet_master_spec.rb
+++ modules/puppet/spec/classes/puppet_master_spec.rb
@@ -3,6 +3,6 @@ require_relative '../../../../spec_helper'
 describe 'puppet::master', :type => :class do
   let(:facts) { { :govuk_class => 'test', :govuk_platform => 'production' } }
 
-  it { should contain_file('/etc/init/puppetmaster.conf').with_require('Package[puppet]') }
+  it { should contain_file('/etc/init/puppetmaster.conf') }
   it { should contain_service('puppetmaster').with_provider('upstart').with_ensure('running').with_require('File[/etc/init/puppetmaster.conf]') }
 end
