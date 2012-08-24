cb94cd6db09379af4d057f6ede48d0519e8a67a8
shodhan sheth <shodhan.sheth@digital.cabinet-office.gov.co.uk>
2012-08-24 08:45:19 +0000

    Adding tariff api to skyscape hosts

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -26,7 +26,8 @@ class hosts::skyscape::production {
   #TODO: change panopticon.production to load balanced cluster ip
   host { 'ip-10-3-0-2.backend-sky.internal':    ip => '10.3.0.2',   host_aliases => [ 'production-backend', 'panopticon.production.alphagov.co.uk',
                                                                                       'whitehall-admin.production.alphagov.co.uk',
-                                                                                      'contentapi.preview.alphagov.co.uk'] }
+                                                                                      'contentapi.preview.alphagov.co.uk',
+                                                                                      'tariff-api.production.alphagov.co.uk'] }
   host { 'ip-10-3-0-3.backend-sky.internal':    ip => '10.3.0.3',   host_aliases => [ 'ip-10-3-0-3', 'production-backend-1', 'signon.production.alphagov.co.uk'] }
   host { 'ip-10-3-0-4.backend-sky.internal':    ip => '10.3.0.4',   host_aliases => [ 'ip-10-3-0-4', 'production-backend-2'] }
   host { 'ip-10-3-0-5.backend-sky.internal':    ip => '10.3.0.5',   host_aliases => [ 'ip-10-3-0-5', 'production-support', 'support.cluster'] }
