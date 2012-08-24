16dc975f486689439b3e28b58459a6910731b693
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-08-24 08:54:42 +0000

    Add the access and error logs for puppetmaster nginx

--- modules/puppet/manifests/master/config/nginx.pp
+++ modules/puppet/manifests/master/config/nginx.pp
@@ -4,13 +4,13 @@ class puppet::master::config::nginx {
   }
 
 # TODO: set up access_log and error_log and have logster or logstash read them
-#  @logster::cronjob { "nginx-vhost-puppetmaster":
-#    args => "--metric-prefix ${title} NginxGangliaLogster /var/log/nginx/puppetmaster-access.log",
-#  }
+  @logster::cronjob { "nginx-vhost-puppetmaster":
+    args => "--metric-prefix puppetmaster NginxGangliaLogster /var/log/nginx/puppetmaster-access.log",
+  }
 
-#  @@nagios::check { "check_nginx_5xx_${title}_on_${::hostname}":
-#    check_command       => "check_ganglia_metric!${title}_nginx_http_5xx!0.05!0.1",
-#    service_description => "check nginx error rate for ${title}",
-#    host_name           => "${::govuk_class}-${::hostname}",
-#  }
+  @@nagios::check { "check_nginx_5xx_puppetmaster_on_${::hostname}":
+    check_command       => "check_ganglia_metric!puppetmaster_nginx_http_5xx!0.05!0.1",
+    service_description => "check nginx error rate for puppetmaster",
+    host_name           => "${::govuk_class}-${::hostname}",
+  }
 }
