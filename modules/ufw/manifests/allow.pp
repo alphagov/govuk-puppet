c5d508fb488b2f0e4d1cb67c83d089c86c876433
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-08-28 13:05:07 +0100

    Stupid rake does not catch that trailing common

--- modules/ufw/manifests/allow.pp
+++ modules/ufw/manifests/allow.pp
@@ -17,14 +17,14 @@ define ufw::allow($proto='tcp', $port='all', $ip='', $from='any') {
   $command  =  $port ? {
     'all'   => "ufw allow proto $proto from $from to $ipadr",
     default => "ufw allow proto $proto from $from to $ipadr port $port",
-  },
+  }
 
   $unless  =  "$ipadr:$port" ? {
     'any:all'    => "ufw status | grep -E \" +ALLOW +$from_match\"",
     /[0-9]:all$/ => "ufw status | grep -E \"$ipadr/$proto +ALLOW +$from_match\"",
     /^any:[0-9]/ => "ufw status | grep -E \"$port/$proto +ALLOW +$from_match\"",
     default      => "ufw status | grep -E \"$ipadr $port/$proto +ALLOW +$from_match\"",
-  },
+  }
 
   exec { "ufw-allow-${proto}-from-${from}-to-${ipadr}-port-${port}":
     command => $command,
--- modules/ufw/manifests/deny.pp
+++ modules/ufw/manifests/deny.pp
@@ -17,12 +17,12 @@ define ufw::deny($proto='tcp', $port='all', $ip='', $from='any') {
   $command = $port ? {
     'all'   => "ufw deny proto $proto from $from to $ipadr",
     default => "ufw deny proto $proto from $from to $ipadr port $port",
-  },
+  }
 
   $unless  = $port ? {
     'all'   => "ufw status | grep -E \"$ipadr/$proto +DENY +$from_match\"",
     default => "ufw status | grep -E \"$ipadr $port/$proto +DENY +$from_match\"",
-  },
+  }
 
   exec { "ufw-deny-${proto}-from-${from}-to-${ipadr}-port-${port}":
     command => $command,
