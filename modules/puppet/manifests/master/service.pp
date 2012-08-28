3b597f0eec1a64d95f08c35675cb83e746f151a4
Philip Potter <philipgpotter@gmail.com>
2012-08-28 16:08:36 +0100

    remove unnecessary and encapsulation-breaking require

--- modules/puppet/manifests/master/service.pp
+++ modules/puppet/manifests/master/service.pp
@@ -2,7 +2,6 @@ class puppet::master::service {
   service { 'puppetmaster':
     ensure   => running,
     provider => upstart,
-    require  => File['/etc/init/puppetmaster.conf'],
     restart  => '/sbin/initctl reload puppetmaster',
   }
 }
--- modules/puppet/spec/classes/puppet_master_spec.rb
+++ modules/puppet/spec/classes/puppet_master_spec.rb
@@ -4,5 +4,5 @@ describe 'puppet::master', :type => :class do
   let(:facts) { { :govuk_class => 'test', :govuk_platform => 'production' } }
 
   it { should contain_file('/etc/init/puppetmaster.conf') }
-  it { should contain_service('puppetmaster').with_provider('upstart').with_ensure('running').with_require('File[/etc/init/puppetmaster.conf]') }
+  it { should contain_service('puppetmaster').with_provider('upstart').with_ensure('running') }
 end
