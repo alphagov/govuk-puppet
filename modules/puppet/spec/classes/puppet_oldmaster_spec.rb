c62d30d5d7da1492e5d4ae1cb480b5477254f126
Philip Potter <philipgpotter@gmail.com>
2012-08-28 10:46:43 +0100

    delete test for deleted class puppet::oldmaster

--- modules/puppet/spec/classes/puppet_oldmaster_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../../../../spec_helper'
-
-describe 'puppet::oldmaster', :type => :class do
-  let(:facts) { { :govuk_class => 'test', :govuk_platform => 'production' } }
-
-  it { should contain_file('/etc/init/puppetmaster.conf').with_require('Package[puppet]') }
-  it { should contain_service('puppetmaster').with_provider('upstart').with_ensure('running').with_require('File[/etc/init/puppetmaster.conf]') }
-end
