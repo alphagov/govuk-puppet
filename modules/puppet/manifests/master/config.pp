b002b3fe7b593683b32e371234b01321bdbd0820
Philip Potter <philipgpotter@gmail.com>
2012-08-28 10:38:03 +0100

    inline puppet::master::config::nginx into puppet::master::config

--- modules/puppet/manifests/master/config.pp
+++ modules/puppet/manifests/master/config.pp
@@ -1,14 +1,16 @@
 class puppet::master::config ($unicorn_port = '9090') {
-  anchor {'puppet::master::config::begin':
-      before => [
-          Class['puppet::master::config::nginx'],
-      ]
+  nginx::config::site { 'puppetmaster':
+    content => template('puppet/puppetmaster-vhost.conf'),
   }
-  include puppet::master::config::nginx
-  anchor {'puppet::master::config::end':
-      subscribe => [
-          Class['puppet::master::config::nginx'],
-      ]
+
+  @logster::cronjob { "nginx-vhost-puppetmaster":
+    args => "--metric-prefix puppetmaster NginxGangliaLogster /var/log/nginx/puppetmaster-access.log",
+  }
+
+  @@nagios::check { "check_nginx_5xx_puppetmaster_on_${::hostname}":
+    check_command       => "check_ganglia_metric!puppetmaster_nginx_http_5xx!0.05!0.1",
+    service_description => "check nginx error rate for puppetmaster",
+    host_name           => "${::govuk_class}-${::hostname}",
   }
 
   file { '/etc/puppet/config.ru':
