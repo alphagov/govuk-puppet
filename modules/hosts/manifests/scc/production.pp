26632ee794fe95bf91e0a7cd604ff9cf9345ae1e
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-08-30 12:36:15 +0000

    Fixing the host enteries for scc

--- modules/hosts/manifests/scc/production.pp
+++ modules/hosts/manifests/scc/production.pp
@@ -1,7 +1,10 @@
 class hosts::scc::production {
   # These are real hosts (1-1 mapping between Host and Service)
   # Anything that ends .cluster is maintained for backwards compatibility with EC2
-  host { 'dc2gdsjmp201.gds2.internal':       ip => '192.168.228.23',   host_aliases  => [ 'ip-10-0-0-6', 'puppet', 'puppet.production.internal' ] }
-  host { 'dc2gdsptm201.gds2.internal':       ip => '192.168.228.21',  host_aliases   => [ 'ip-10-0-0-11', 'jumpbox1' ] }
-  host { 'dc2gdsmon201.gds2.internal':       ip => '192.168.228.22',  host_aliases   => [ 'ip-10-0-0-20', 'monitoring', 'monitoring.production.internal', 'monitoring.cluster' ] }
+  # The jumpbox
+  host { 'dc2gdsjmp201.gds.internal':       ip => '192.168.228.21',   host_aliases  => [ 'jumpbox1' ] }
+  # The monitoring box
+  host { 'dc2gdsmon201.gds.internal':       ip => '192.168.228.22',  host_aliases   => [ 'monitoring', 'monitoring.production.internal', 'monitoring.cluster' ] }
+  # The puppet master
+  host { 'dc2gdsotn201.gds.internal':       ip => '192.168.228.23',  host_aliases   => [ 'dc2gdsptm201.gds.internal', 'puppet', 'puppet.production.internal'] }
 }
