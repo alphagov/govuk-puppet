f8adebaedfe1aeb3b74122af0bf80298a583b867
Philip Potter <philipgpotter@gmail.com>
2012-08-31 17:49:40 +0100

    unicornify frontend

--- manifests/classes/govuk_node/frontend_server.pp
+++ manifests/classes/govuk_node/frontend_server.pp
@@ -19,23 +19,17 @@ class govuk_node::frontend_server inherits govuk_node::base {
   include govuk::apps::designprinciples
   include govuk::apps::licencefinder
   include govuk::apps::publicapi
+  include govuk::apps::frontend
 
   class { 'nginx': node_type => frontend_server }
 
   apache2::vhost::passenger {
-    "www.${::govuk_platform}.alphagov.co.uk":
-      aliases         => ["frontend.${::govuk_platform}.alphagov.co.uk", 'www.gov.uk'];
     "search.${::govuk_platform}.alphagov.co.uk":
       additional_port => 8083;
     "static.${::govuk_platform}.alphagov.co.uk":;
   }
 
   nginx::config::vhost::proxy {
-    'www.gov.uk':
-      to      => ['localhost:8080'];
-    "www.$::govuk_platform.alphagov.co.uk":
-      to      => ['localhost:8080'],
-      aliases => ["frontend.$::govuk_platform.alphagov.co.uk"];
     "search.$::govuk_platform.alphagov.co.uk":
       to => ['localhost:8080'];
     }
@@ -45,12 +39,4 @@ class govuk_node::frontend_server inherits govuk_node::base {
     aliases   => ['calendars', 'planner', 'smartanswers', 'static', 'frontend', 'designprinciples', 'licencefinder', 'tariff', 'efg', 'feedback'],
     ssl_only  => true
   }
-
-  file { "/data/vhost/frontend.${::govuk_platform}.alphagov.co.uk":
-    ensure => link,
-    target => "/data/vhost/www.${::govuk_platform}.alphagov.co.uk",
-    owner  => 'deploy',
-    group  => 'deploy',
-  }
-
 }
---
+++ modules/govuk/manifests/apps/frontend.pp
@@ -0,0 +1,23 @@
+class govuk::apps::frontend( $port = 3005 ) {
+  govuk::app { 'frontend':
+    app_type      => 'rack',
+    port          => $port,
+    vhost         => 'www',
+    vhost_aliases => ['frontend'],
+  }
+
+  nginx::config::vhost::proxy {
+    'www.gov.uk':
+      to      => ["localhost:${port}"];
+  }
+
+  # nginx::config::vhost::static needs this link to be here
+  # it assumes a file structure of /data/vhost/{app}.{platform}.alphagov.co.uk/shared/public/{app}
+  # but for frontend, it's /data/vhost/www.{platform}.alphagov.co.uk/shared/public/frontend
+  file { "/data/vhost/frontend.${::govuk_platform}.alphagov.co.uk":
+    ensure => link,
+    target => "/data/vhost/www.${::govuk_platform}.alphagov.co.uk",
+    owner  => 'deploy',
+    group  => 'deploy',
+  }
+}
