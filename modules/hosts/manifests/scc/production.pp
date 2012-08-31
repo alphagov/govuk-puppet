01318ea6d06414c14bf4389024ce3a266ef727dd
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-08-31 10:46:28 +0000

    Add the host enteries for all the flat sites

--- modules/hosts/manifests/scc/production.pp
+++ modules/hosts/manifests/scc/production.pp
@@ -7,4 +7,8 @@ class hosts::scc::production {
   host { 'dc2gdsmon201.gds.internal':       ip => '192.168.228.22',  host_aliases   => [ 'dc2gdsmon201','monitoring', 'monitoring.production.internal', 'monitoring.cluster' ] }
   # The puppet master
   host { 'dc2gdsotn201.gds.internal':       ip => '192.168.228.23',  host_aliases   => [ 'dc2gdsotn201', 'dc2gdsptm201.gds.internal', 'puppet', 'puppet.production.internal'] }
+  # The flat sites
+  host { 'dc2gdsweb201.gds.internal':       ip => '192.168.228.24',  host_aliases   => [ 'dc2gdsweb201'] }
+  host { 'dc2gdsweb202.gds.internal':       ip => '192.168.228.25',  host_aliases   => [ 'dc2gdsweb202'] }
+  host { 'dc2gdsweb202.gds.internal':       ip => '192.168.228.26',  host_aliases   => [ 'dc2gdsweb203'] }
 }
