685288671cbfedfe375457db0b6e641207fc66f8
Philip Potter <philipgpotter@gmail.com>
2012-09-03 14:28:42 +0100

    check for the correct vhost name...

--- manifests/spec/govuk_node__frontend_server_spec.rb
+++ manifests/spec/govuk_node__frontend_server_spec.rb
@@ -5,7 +5,7 @@ describe 'govuk_node::frontend_server', :type => :class do
     let(:facts) { { :govuk_class => "frontend", :govuk_platform => 'preview' } }
     it { should_not raise_error(Puppet::ParseError) }
     it { should include_class('govuk::apps::frontend') }
-    it { should_not contain_apache2__vhost__passenger('www.production.alphagov.co.uk') }
+    it { should_not contain_apache2__vhost__passenger('www.preview.alphagov.co.uk') }
   end
 
   # Don't want to deploy unicorn frontend at the moment, still testing
