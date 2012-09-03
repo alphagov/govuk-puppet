064d4e572bc4883e0783d718857ba7ba05779b9e
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-09-03 11:46:35 +0100

    Change datainsight_web to underscore.

--- modules/govuk/manifests/apps/datainsight_web.pp
+++ modules/govuk/manifests/apps/datainsight_web.pp
@@ -1,4 +1,4 @@
-class govuk::apps::datainsight-web( $port = 3027 ) {
+class govuk::apps::datainsight_web( $port = 3027 ) {
   govuk::app { 'datainsight-web':
     app_type => 'rack',
     port     => $port;
