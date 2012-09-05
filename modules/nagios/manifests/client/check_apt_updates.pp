85c2ba6442bffe17e60240005170484064c44cb9
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-09-05 23:10:37 +0100

    vacuum some lint

--- modules/nagios/manifests/client/check_apt_updates.pp
+++ modules/nagios/manifests/client/check_apt_updates.pp
@@ -3,8 +3,8 @@ class nagios::client::check_apt_updates {
     source  => 'puppet:///modules/nagios/usr/lib/nagios/plugins/check_apt_updates',
   }
   @@nagios::check { "check_apt_updates_${::hostname}":
-    check_command           => 'check_nrpe_1arg!check_apt_updates',
+    check_command       => 'check_nrpe_1arg!check_apt_updates',
     service_description => "check package updates on ${::govuk_class}-${::hostname}",
-    host_name       => "${::govuk_class}-${::hostname}",
+    host_name           => "${::govuk_class}-${::hostname}",
   }
 }
