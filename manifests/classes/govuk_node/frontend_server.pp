b830a044985ba67de03a8e28185817b3ec4bb96b
Philip Potter <philipgpotter@gmail.com>
2012-09-05 13:56:13 +0100

    deploy static under unicorn to prod

--- manifests/classes/govuk_node/frontend_server.pp
+++ manifests/classes/govuk_node/frontend_server.pp
@@ -14,32 +14,7 @@ class govuk_node::frontend_server inherits govuk_node::base {
   include govuk::apps::publicapi
   include govuk::apps::frontend
   include govuk::apps::search
+  include govuk::apps::static
 
   include nginx
-
-  # WIP, transitioning static to unicorn but don't want changes to go to production yet
-  # -- ppotter, 2012-09-05
-  case $::govuk_platform {
-    production: {
-      class { 'apache2':
-        port => '8080'
-      }
-      class { 'passenger':
-        maxpoolsize => 12
-      }
-
-      apache2::vhost::passenger {
-        "static.${::govuk_platform}.alphagov.co.uk":;
-      }
-
-      nginx::config::vhost::static { "static.$::govuk_platform.alphagov.co.uk":
-        protected => false,
-        aliases   => ['calendars', 'planner', 'smartanswers', 'static', 'frontend', 'designprinciples', 'licencefinder', 'tariff', 'efg', 'feedback'],
-        ssl_only  => true
-      }
-    }
-    default: {
-      include govuk::apps::static
-    }
-  }
 }
