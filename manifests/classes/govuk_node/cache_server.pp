ebe8b82a64d604176683057090fa9fbf0011f746
Philip Potter <philipgpotter@gmail.com>
2012-09-05 15:47:08 +0100

    don't need this anymore

--- manifests/classes/govuk_node/cache_server.pp
+++ manifests/classes/govuk_node/cache_server.pp
@@ -7,14 +7,4 @@ class govuk_node::cache_server inherits govuk_node::base {
 
   class { 'nginx': node_type => router}
 
-  # Have realised that this purge does not kick in the first puppet run[Newly provisioned machines]
-  # Needs to be fixed. Unsure of how apache sneaks in.
-  # Kicks in the the next puppet run
-  package { 'apache2':
-    ensure => absent,
-  }
-
-  service { 'apache2':
-    ensure => stopped
-  }
 }
