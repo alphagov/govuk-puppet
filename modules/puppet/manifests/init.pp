0dc36384d055cde9298bb944840df48cec7291fc
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-09-05 17:12:07 +0100

    Manage govuk_puppet script in puppet. I'm So Meta, Even This Acronym.

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -17,6 +17,15 @@ class puppet {
     require => Package['puppet'];
   }
 
+  file { '/usr/local/bin/govuk_puppet':
+    ensure => present,
+    mode   => '0755',
+    source => [
+      "puppet:///modules/puppet/govuk_puppet_${::govuk_platform}",
+      'puppet:///modules/puppet/govuk_puppet'
+    ],
+  }
+
   service { 'puppet': # we're using cron, so we don't want the daemonized puppet agent
     ensure   => stopped,
     provider => base,
