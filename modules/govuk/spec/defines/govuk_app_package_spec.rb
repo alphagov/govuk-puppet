a52b06340566c74aedf5e55219e147826424fba5
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-09-05 16:03:00 +0100

    Refactor test to only do one it

--- modules/govuk/spec/defines/govuk_app_package_spec.rb
+++ modules/govuk/spec/defines/govuk_app_package_spec.rb
@@ -19,9 +19,11 @@ describe 'govuk::app::package', :type => :define do
       }
     end
 
-    it { should contain_file('/var/apps/giraffe').with_ensure('link') }
-    it { should contain_file('/var/run/giraffe') }
-    it { should contain_file('/var/log/giraffe') }
-    it { should contain_file('/data/vhost/giraffe.example.com') }
+    it do
+      should contain_file('/var/apps/giraffe').with_ensure('link')
+      should contain_file('/var/run/giraffe')
+      should contain_file('/var/log/giraffe')
+      should contain_file('/data/vhost/giraffe.example.com')
+    end
   end
 end
