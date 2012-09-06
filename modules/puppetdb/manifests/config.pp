93d8cde869934ba04ab533b64ee7205ab14fedcb
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-09-06 12:44:37 +0100

    Cater for the fact that the EC2 Production puppetmaster can also now handle 1024M for puppetdb

--- modules/puppetdb/manifests/config.pp
+++ modules/puppetdb/manifests/config.pp
@@ -1,7 +1,16 @@
 class puppetdb::config {
-  $java_args = $::govuk_provider ? {
-    'sky'   => '-Xmx1024m',
-    default => '-Xmx192m',
+
+  # This caters for Amazon Preview where puppetmaster is a small instance
+  # When Amazon is dead, we should just make this 1024
+  if $::govuk_provider == 'sky' {
+      $java_args = '-Xmx1024m'
+  } elsif $::govuk_provider == 'scc' {
+      $java_args = '-Xmx1024m'
+  } else {
+      $java_args = $::govuk_platform ? {
+        'production'   => '-Xmx1024m',
+        default        => '-Xmx192m',
+      }
   }
 
   file { '/etc/default/puppetdb':
