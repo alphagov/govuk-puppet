a8144f98160634547734a1605350e6636d50f2ef
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-09-07 15:42:38 +0000

    Add the correct etc/hosts entries for skyscape jenkins and puppet.

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -1,39 +1,6 @@
 class hosts::skyscape::production {
   # These are real hosts (1-1 mapping between Host and Service)
   # Anything that ends .cluster is maintained for backwards compatibility with EC2
-  host { 'ip-10-0-0-6.mgmt-sky.internal':       ip => '10.0.0.6',   host_aliases => [ 'ip-10-0-0-6', 'puppet', 'puppet.production.internal' ] }
-  host { 'ip-10-0-0-8.mgmt-sky.internal':       ip => '10.0.0.8',   host_aliases => [ 'ip-10-0-0-8', 'jenkins', 'jenkins.production.internal' ] }
-  host { 'ip-10-0-0-11.mgmt-sky.internal':      ip => '10.0.0.11',  host_aliases => [ 'ip-10-0-0-11', 'jumpbox1' ] }
-  host { 'ip-10-0-0-12.mgmt-sky.internal':      ip => '10.0.0.12',  host_aliases => [ 'ip-10-0-0-12', 'jumpbox2' ] }
-  host { 'ip-10-0-0-20.mgmt-sky.internal':      ip => '10.0.0.20',  host_aliases => [ 'ip-10-0-0-20', 'monitoring', 'monitoring.production.internal', 'monitoring.cluster' ] }
-  host { 'ip-10-0-0-21.mgmt-sky.internal':      ip => '10.0.0.21',  host_aliases => [ 'ip-10-0-0-21', 'logging', 'logging.production.internal', 'graylog.cluster' ] }
-
-  #TODO: use loadbalanced ip instead
-  host { 'ip-10-2-0-2.frontend-sky.internal':   ip => '10.2.0.2',   host_aliases => [ 'ip-10-2-0-2', 'production-frontend', 'search.production.alphagov.co.uk',
-                                                                                      'licencefinder.production.alphagov.co.uk',
-                                                                                      'calendars.production.alphagov.co.uk', 'smartanswers.production.alphagov.co.uk',
-                                                                                      'tariff.production.alphagov.co.uk', 'whitehall-frontend.production.alphagov.co.uk'] }
-  host { 'ip-10-2-0-3.frontend-sky.internal':   ip => '10.2.0.3',   host_aliases => [ 'ip-10-2-0-3', 'production-frontend-1'] }
-  host { 'ip-10-2-0-4.frontend-sky.internal':   ip => '10.2.0.4',   host_aliases => [ 'ip-10-2-0-4', 'production-frontend-2'] }
-  host { 'planner.production.alphagov.co.uk':   ip => '172.16.254.60' }
-
-
-  host { 'ip-10-1-0-3.router-sky.internal':     ip => '10.1.0.3',   host_aliases => [ 'ip-10-1-0-3', 'production-cache'] }
-  host { 'ip-10-1-0-4.router-sky.internal':     ip => '10.1.0.4',   host_aliases => [ 'ip-10-1-0-4', 'production-cache-1'] }
-  host { 'ip-10-1-0-5.router-sky.internal':     ip => '10.1.0.5',   host_aliases => [ 'ip-10-1-0-5', 'production-cache-2'] }
-  host { 'router.cluster':                      ip => '172.16.254.50' }
-
-  #TODO: change panopticon.production to load balanced cluster ip
-  host { 'ip-10-3-0-2.backend-sky.internal':    ip => '10.3.0.2',   host_aliases => [ 'production-backend', 'panopticon.production.alphagov.co.uk',
-                                                                                      'whitehall-admin.production.alphagov.co.uk',
-                                                                                      'contentapi.preview.alphagov.co.uk',
-                                                                                      'tariff-api.production.alphagov.co.uk'] }
-  host { 'ip-10-3-0-3.backend-sky.internal':    ip => '10.3.0.3',   host_aliases => [ 'ip-10-3-0-3', 'production-backend-1', 'signon.production.alphagov.co.uk'] }
-  host { 'ip-10-3-0-4.backend-sky.internal':    ip => '10.3.0.4',   host_aliases => [ 'ip-10-3-0-4', 'production-backend-2'] }
-  host { 'ip-10-3-0-5.backend-sky.internal':    ip => '10.3.0.5',   host_aliases => [ 'ip-10-3-0-5', 'production-support', 'support.cluster'] }
-  host { 'ip-10-3-0-20.backend-sky.internal':   ip => '10.3.0.20',  host_aliases => [ 'ip-10-3-0-20', 'mapit', 'mapit.alpha.gov.uk'] }
-  host { 'ip-10-3-0-6.backend-sky.internal':    ip => '10.3.0.6',   host_aliases => [ 'ip-10-3-0-6', 'mongo-1.production.internal', 'mongo.cluster'] }
-  host { 'ip-10-3-0-7.backend-sky.internal':    ip => '10.3.0.7',   host_aliases => [ 'ip-10-3-0-7', 'mongo-2.production.internal'] }
-  host { 'ip-10-3-0-8.backend-sky.internal':    ip => '10.3.0.8',   host_aliases => [ 'ip-10-3-0-8', 'mongo-3.production.internal'] }
-  host { 'ip-10-3-0-30.backend-sky.internal':   ip => '10.3.0.30',  host_aliases => [ 'ip-10-3-0-30', 'mysql-master', 'mysql-master.production.internal'] }
+  host { 'puppet-1.management.production'  :      ip => '10.0.0.2' }
+  host { 'jenkins-1.management.production' :      ip => '10.0.0.3' }
 }
