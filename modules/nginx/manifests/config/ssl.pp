14414196f687124472860e79adba7b39643caee6
Philip Potter <philipgpotter@gmail.com>
2012-09-07 14:33:11 +0100

    fix tests

--- modules/nginx/manifests/config/ssl.pp
+++ modules/nginx/manifests/config/ssl.pp
@@ -1,22 +1,25 @@
 define nginx::config::ssl( $certtype = undef ) {
   if $certtype == undef {
     if $name == 'www.gov.uk' {
-      $certtype = "www"
+      $certtype_real = "www"
     } elsif $name == 'www.preview.alphagov.co.uk' {
-      $certtype = "www"
+      $certtype_real = "www"
     } else {
-      $certtype = "wildcard_alphagov"
+      $certtype_real = "wildcard_alphagov"
     }
+  }
+  else {
+    $certtype_real = $certtype
   }
   file { "/etc/nginx/ssl/$name.crt":
     ensure  => present,
-    content => extlookup("${certtype}_crt", ''),
+    content => extlookup("${certtype_real}_crt", ''),
     require => Class['nginx::package'],
     notify  => Class['nginx::service'],
   }
   file { "/etc/nginx/ssl/$name.key":
     ensure  => present,
-    content => extlookup("${certtype}_key", ''),
+    content => extlookup("${certtype_real}_key", ''),
     require => Class['nginx::package'],
     notify  => Class['nginx::service'],
   }
