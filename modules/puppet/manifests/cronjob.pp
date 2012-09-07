30b06b9b21ba5a05f9988d296c03f4dbe52dd9a9
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-09-07 16:13:08 +0000

    Enable the puppet cronjob for skyscape

--- modules/puppet/manifests/cronjob.pp
+++ modules/puppet/manifests/cronjob.pp
@@ -3,18 +3,11 @@ class puppet::cronjob {
   $first = fqdn_rand_fixed(30)
   $second = $first + 30
 
-  case $::govuk_provider {
-    sky: {
-      cron { 'puppet': ensure => absent }
-    }
-    default: {
-      cron { 'puppet':
-        ensure  => present,
-        user    => 'root',
-        minute  => [$first, $second],
-        command => '/usr/bin/puppet agent --onetime --no-daemonize',
-        require => Package['puppet'];
-      }
-    }
+  cron { 'puppet':
+    ensure  => present,
+    user    => 'root',
+    minute  => [$first, $second],
+    command => '/usr/bin/puppet agent --onetime --no-daemonize',
+    require => Package['puppet'];
   }
 }
