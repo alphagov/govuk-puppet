6ef28e131a89035a185302387527440dd32fadfd
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-09-07 16:08:41 +0000

    Added the hosts entries for the first machines of every skyscape prod VDC

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -1,6 +1,16 @@
 class hosts::skyscape::production {
   # These are real hosts (1-1 mapping between Host and Service)
   # Anything that ends .cluster is maintained for backwards compatibility with EC2
-  host { 'puppet-1.management.production'  :      ip => '10.0.0.2' }
-  host { 'jenkins-1.management.production' :      ip => '10.0.0.3' }
+  #Management VDC machines
+  host { 'puppet-1.management.production'  : ip => '10.0.0.2', host_aliases => [ 'puppet' ] }
+  host { 'jenkins-1.management.production' : ip => '10.0.0.3' }
+
+  #Router VDC machines
+  host { 'cache-1.router.production'       : ip => '10.1.0.2' }
+
+  #Frontend VDC machines
+  host { 'frontend-1.frontend.production'  : ip => '10.2.0.2' }
+
+  #Backend VDC machines
+  host { 'backend-1.backend.production'    : ip => '10.3.0.2' }
 }
