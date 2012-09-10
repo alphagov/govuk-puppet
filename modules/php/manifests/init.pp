ba49a0b12916c5b73416fe770bdbc08bb042a882
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-09-10 15:34:34 +0100

    Add php class

---
+++ modules/php/manifests/init.pp
@@ -0,0 +1,27 @@
+class php {
+
+  apt::repository { 'fabianarias-php5':
+    owner => 'fabianarias',
+    repo  => 'php5',
+    type  => 'ppa',
+  }
+
+  package { 'php5-fpm':
+    ensure => present,
+    notify => Service['php5-fpm'],
+  }
+
+  file { '/etc/php5/fpm/pool.d/www.conf':
+    ensure  => present,
+    require => Package['php5-fpm'],
+    notify  => Service['php5-fpm'],
+    source  => 'puppet:///modules/php/www.conf',
+  }
+
+  service { 'php5-fpm':
+    ensure    => running,
+    hasstatus => false,
+    pattern   => 'php-fpm',
+  }
+
+}
