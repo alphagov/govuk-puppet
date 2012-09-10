69cd5ecb0b32d3e0f4fec7f8e584311bc9e4d79c
Philip Potter <philipgpotter@gmail.com>
2012-09-10 09:46:11 +0100

    remove unnecessary variable certtype_real

--- modules/nginx/manifests/config/ssl.pp
+++ modules/nginx/manifests/config/ssl.pp
@@ -2,17 +2,16 @@ define nginx::config::ssl( $certtype ) {
   if ! ($certtype in ['www','wildcard_alphagov']) {
     fail "${certtype} is not a valid certtype"
   }
-  $certtype_real = $certtype
 
   file { "/etc/nginx/ssl/$name.crt":
     ensure  => present,
-    content => extlookup("${certtype_real}_crt", ''),
+    content => extlookup("${certtype}_crt", ''),
     require => Class['nginx::package'],
     notify  => Class['nginx::service'],
   }
   file { "/etc/nginx/ssl/$name.key":
     ensure  => present,
-    content => extlookup("${certtype_real}_key", ''),
+    content => extlookup("${certtype}_key", ''),
     require => Class['nginx::package'],
     notify  => Class['nginx::service'],
   }
