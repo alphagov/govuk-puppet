5dfd9387375153316e3e407dad3c886a9f50de0b
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-09-10 19:20:45 +0100

    Remove apt_cacher::client as it seems to be rubbish :trollface:

--- modules/apt_cacher/manifests/client.pp
+++ modules/apt_cacher/manifests/client.pp
@@ -1,6 +1,6 @@
 class apt_cacher::client {
   file {'/etc/apt/apt.conf.d/01proxy':
-    ensure  => present,
+    ensure  => absent,
     content => 'Acquire::http { Proxy "http://support.cluster:3142"; };'
   }
 }
