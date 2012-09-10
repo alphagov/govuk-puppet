2ad0582f5bd235ee38d65302890ec6767470f120
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-09-10 17:28:51 +0100

    add an alias for mongo-1

--- modules/hosts/manifests/skyscape/production.pp
+++ modules/hosts/manifests/skyscape/production.pp
@@ -21,7 +21,7 @@ class hosts::skyscape::production {
   host { 'backend-2.backend.production'         : ip => '10.3.0.3' }
   host { 'backend-3.backend.production'         : ip => '10.3.0.4' }
   host { 'support-1.backend.production'         : ip => '10.3.0.5' , host_aliases => [ 'support-1', 'support.cluster']}
-  host { 'mongo-1.backend.production'           : ip => '10.3.0.6', host_aliases  => [ 'mongo.backend.production', 'mongodb.cluster' ] }
+  host { 'mongo-1.backend.production'           : ip => '10.3.0.6', host_aliases  => [ 'mongo-1', 'mongo.backend.production', 'mongodb.cluster' ] }
   host { 'mongo-2.backend.production'           : ip => '10.3.0.7' }
   host { 'mongo-3.backend.production'           : ip => '10.3.0.8' }
   host { 'mysql-master-1.backend.production'    : ip => '10.3.10.0', host_aliases => [ 'mysql-master-1', 'mysql.backend.production' ]  }
