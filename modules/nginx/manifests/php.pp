e9d3129184d08813019e086d5e656e232c3a957f
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-09-10 17:43:25 +0100

    Add nginx::php module

---
+++ modules/nginx/manifests/php.pp
@@ -0,0 +1,12 @@
+class nginx::php {
+
+  include php
+
+  file { '/etc/nginx/conf.d/php.conf':
+    ensure  => present,
+    content => 'upstream php { server unix:/var/run/php5-fpm.sock; }',
+    require => Class['nginx::package'],
+    notify  => Class['nginx::service'],
+  }
+
+}
