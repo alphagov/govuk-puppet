372c6d9a9cab62c8f8baf7a45d5184e3b0a28acb
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-09-11 16:14:55 +0000

    Enable health check for calendars

--- modules/govuk/manifests/apps/calendars.pp
+++ modules/govuk/manifests/apps/calendars.pp
@@ -1,6 +1,7 @@
 class govuk::apps::calendars( $port = 3011 ) {
   govuk::app { 'calendars':
-    app_type => 'rack',
-    port     => $port;
+    app_type          => 'rack',
+    port              => $port,
+    health_check_path => "/bank-holidays";
   }
 }
