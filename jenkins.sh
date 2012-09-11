0bd9d823d94c34f8da24367fc2897674bc70e021
Philip Potter <philipgpotter@gmail.com>
2012-09-11 09:21:35 +0100

    don't fail build if lint fails

--- jenkins.sh
+++ jenkins.sh
@@ -7,6 +7,9 @@ bundle exec rake test
 RESULT=$?
 
 mkdir -p build
+set +e
+# don't fail build if rake lint fails; it stops jenkins from parsing the results
 bundle exec rake lint >build/puppet-lint
+set -e
 
 exit $RESULT
