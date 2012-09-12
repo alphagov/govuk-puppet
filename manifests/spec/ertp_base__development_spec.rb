130b868fcaa032ef8fcd23b39efd696c6e6633fa
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-09-12 16:53:18 +0100

    Remove unused spec

--- manifests/spec/ertp_base__development_spec.rb
+++
@@ -1,6 +0,0 @@
-require_relative '../../spec_helper'
-
-describe 'ertp_base::development', :type => :class do
-  let(:facts) { { :govuk_class => "ertp-development", :govuk_platform => "development" } }
-  it { should_not raise_error(Puppet::ParseError) }
-end
