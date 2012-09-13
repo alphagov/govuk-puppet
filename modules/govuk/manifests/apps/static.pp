e46c9f54a029b5fc4935dba5b8cb832a5bd7ec86
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2012-09-13 11:42:47 +0100

    Make CDN deliver assets for datainsight

--- modules/govuk/manifests/apps/static.pp
+++ modules/govuk/manifests/apps/static.pp
@@ -8,7 +8,7 @@ class govuk::apps::static( $port = 3013 ) {
   nginx::config::vhost::static { "static.$::govuk_platform.alphagov.co.uk":
     to        => "localhost:${port}",
     protected => false,
-    aliases   => ['calendars', 'planner', 'smartanswers', 'static', 'frontend', 'designprinciples', 'licencefinder', 'tariff', 'efg', 'feedback'],
+    aliases   => ['calendars', 'planner', 'smartanswers', 'static', 'frontend', 'designprinciples', 'licencefinder', 'tariff', 'efg', 'feedback', 'datainsight-frontend'],
     ssl_only  => true
   }
 
