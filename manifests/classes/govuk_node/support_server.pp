566021a9d5af9822b11e67bd7e76ec4f7835f2fd
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-09-13 23:09:08 +0100

    Move mysql setup into support_server class

--- manifests/classes/govuk_node/support_server.pp
+++ manifests/classes/govuk_node/support_server.pp
@@ -11,6 +11,25 @@ class govuk_node::support_server inherits govuk_node::base {
     include mysql::backup
   }
 
+  if $::govuk_platform == 'preview' {
+    $mysql_host     = 'rds.cluster'
+    $mysql_user     = 'backup'
+    $mysql_password = extlookup('mysql_preview_backup', '')
+
+    file { '/home/deploy/.my.cnf':
+      ensure  => file,
+      owner   => 'deploy',
+      group   => 'deploy',
+      mode    => '0600',
+      require => User['deploy'],
+      content => "[client]
+host=${mysql_host}
+user=${mysql_user}
+password=${mysql_password}
+"
+    }
+  }
+
   include elasticsearch
   elasticsearch::node { "govuk-${::govuk_platform}": }
 }
--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -3,22 +3,4 @@ class users {
     name => 'admin',
     gid  => '3000',
   }
-  if $::govuk_platform == 'preview' and $::govuk_class == 'support' {
-    $mysql_host = 'rds.cluster'
-    $mysql_user = 'backup'
-    /*  WARNING: this fallback is in place to keep the rspec build happy.
-        However, it will fail silently if the external data is not present,
-        for example if deployed with the wrong version of the CSV file.
-    */
-    $mysql_password = extlookup('mysql_preview_backup', '')
-
-    file { '/home/deploy/.my.cnf':
-      ensure  => file,
-      owner   => 'deploy',
-      group   => 'deploy',
-      mode    => '0600',
-      require => User['deploy'],
-      content => template('users/my.cnf')
-    }
-  }
 }
