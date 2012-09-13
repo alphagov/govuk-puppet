d8fb0d02cfa07095d3a079651fda790f5cd0d101
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-09-13 22:34:28 +0100

    Clean up mirror module, and add missing dependencies

--- modules/mirror/manifests/init.pp
+++ modules/mirror/manifests/init.pp
@@ -1,31 +1,33 @@
 class mirror {
-  # requires wget and nginx modules
+
+  include wget
+
+  file { '/usr/local/bin/govuk_update_mirror':
+    ensure => present,
+    mode   => '0755',
+    source => 'puppet:///modules/mirror/govuk_update_mirror'
+  }
 
   file { '/usr/share/www':
     ensure => directory,
     owner  => 'www-data',
     group  => 'www-data',
-    mode   => '0755',
   }
 
-  file { '/usr/local/bin/govuk_update_mirror':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/mirror/govuk_update_mirror'
-  }
   file { '/usr/share/www/www.gov.uk':
-    ensure => directory,
-    owner  => 'www-data',
-    group  => 'www-data',
+    ensure  => directory,
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => File['/usr/share/www'],
   }
+
   cron { 'update-latest-to-mirror':
-        ensure  => present,
-        user    => 'root',
-        hour    => 00,# The time can be changed to any convenient time of the day
-        minute  => 00,# The time here is just added for the convenience of testing
-        command => '/usr/local/bin/govuk_update_mirror',
-        require => File['/usr/local/bin/govuk_update_mirror'],
+    ensure  => present,
+    user    => 'root',
+    hour    => '0',
+    minute  => '0',
+    command => '/usr/local/bin/govuk_update_mirror',
+    require => File['/usr/local/bin/govuk_update_mirror'],
   }
+
 }
