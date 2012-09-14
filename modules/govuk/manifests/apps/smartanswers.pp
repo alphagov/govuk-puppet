7aa26b66e41270eaf36913b5b644d9a2e38edc2e
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-09-14 16:20:44 +0000

    had the wrong health check url

--- modules/govuk/manifests/apps/smartanswers.pp
+++ modules/govuk/manifests/apps/smartanswers.pp
@@ -2,6 +2,6 @@ class govuk::apps::smartanswers( $port = 3010 ) {
   govuk::app { 'smartanswers':
     app_type          => 'rack',
     port              => $port,
-    health_check_path => '/becoming-a-driving-instructor';
+    health_check_path => '/become-a-driving-instructor';
   }
 }
