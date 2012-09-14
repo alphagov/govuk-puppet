2e9272de170ffba283b78050c0836745fe241a50
Philip Potter <philipgpotter@gmail.com>
2012-09-14 11:39:06 +0100

    fix quoting issue in mysql::user

--- modules/mysql/manifests/user.pp
+++ modules/mysql/manifests/user.pp
@@ -1,34 +1,34 @@
 define mysql::user ($root_password, $user_password, $username=$title, $remote_host='%', $db='*', $privileges='all') {
   case $user_password {
-    '': {
-        $userpassarg = ''
+    "": {
+        $userpassarg = ""
     }
     default: {
-        $userpassarg = "-p'${user_password}'"
+        $userpassarg = "'-p${user_password}'"
     }
   }
 
   case $root_password {
-    '': {
-        $rootpassarg = ''
+    "": {
+        $rootpassarg = ""
     }
     default: {
-        $rootpassarg = "-p'${root_password}'"
+        $rootpassarg = "'-p${root_password}'"
     }
   }
 
   case $db {
-    '*': {
-        $dbarg = ''
+    "*": {
+        $dbarg = ""
     }
     default: {
-        $dbarb = $db
+        $dbarg = "'${db}'"
     }
   }
 
   exec { "create_mysql_user_${title}":
-    unless  => "/usr/bin/mysql -u${username} '${userpassarg}' ${dbarg}",
-    command => "/usr/bin/mysql -uroot '${rootpassarg}' -e 'grant ${privileges} on ${db}.* to ${username}@\"${remote_host}\" identified by \"$user_password\"; flush privileges;'",
+    unless  => "/usr/bin/mysql -u${username} ${userpassarg} ${dbarg}",
+    command => "/usr/bin/mysql -uroot ${rootpassarg} -e 'grant ${privileges} on ${db}.* to ${username}@\"${remote_host}\" identified by \"$user_password\"; flush privileges;'",
     require => Class['mysql::server'],
   }
 }
