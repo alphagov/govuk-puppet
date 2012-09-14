e89a2c568deb390486efbd7fae51d200e8ebb4e9
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-09-14 15:45:31 +0000

    Added the host entry and health check path from smart answers

--- modules/govuk/manifests/apps/smartanswers.pp
+++ modules/govuk/manifests/apps/smartanswers.pp
@@ -1,6 +1,7 @@
 class govuk::apps::smartanswers( $port = 3010 ) {
   govuk::app { 'smartanswers':
-    app_type => 'rack',
-    port     => $port;
+    app_type          => 'rack',
+    port              => $port,
+    health_check_path => '/becoming-a-driving-instructor';
   }
 }
