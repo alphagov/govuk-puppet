9e52feb7f71c0093350ccc020374967139606c91
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-09-17 16:28:45 +0100

    Fix all the horrendous lint that @mwall put in my nice clean repo

--- modules/clamav/manifests/init.pp
+++ modules/clamav/manifests/init.pp
@@ -4,19 +4,19 @@ class clamav {
   }
 
   service { 'clamav-daemon':
-    ensure => 'running',
-    enable => 'true',
+    ensure  => 'running',
+    enable  => true,
     require => Package["clamav"]
   }
 
   file { '/opt/clamav/etc/clamd.conf':
+    ensure => present,
     notify => Service["clamav-daemon"],
-    ensure  => present,
     source => 'puppet:///modules/clamav/opt/clamav/etc/clamd.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
-  
+
   Package['clamav'] -> File['/opt/clamav/etc/clamd.conf']
 }
