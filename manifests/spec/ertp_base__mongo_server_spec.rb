8a31617fa20ea68db3f9f46cd17a97e1ac2f2222
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-09-17 10:49:26 +0100

    Fix the tests for mongodb

--- manifests/spec/ertp_base__mongo_server_spec.rb
+++ manifests/spec/ertp_base__mongo_server_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../spec_helper'
   describe "ertp_base::mongo_server", :type => :class do
     let(:facts) { { :govuk_class => "ertp-mongo", :govuk_platform => environment } }
     it do
-      should create_package("mongodb-10gen")
+      should create_package("mongodb20-10gen")
       should_not raise_error(Puppet::ParseError)
     end
   end
--- modules/mongodb/spec/classes/mongodb_spec.rb
+++ modules/mongodb/spec/classes/mongodb_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../../../spec_helper'
 describe 'mongodb::server', :type => :class do
   let(:facts) { { 'govuk_class' => 'preview', 'govuk_platform' => 'carlisAwesome' } }
   it do
-    should contain_package('mongodb-10gen')
+    should contain_package('mongodb20-10gen')
     should contain_file('/etc/mongodb.conf')
   end
 end
