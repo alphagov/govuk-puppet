e7d8815781b7785e2e9e87c735eb0147ef00b296
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-09-19 16:21:41 +0100

    Fix syntax error and change parameter to nginx_extra_config

--- modules/govuk/manifests/apps/contentapi.pp
+++ modules/govuk/manifests/apps/contentapi.pp
@@ -2,8 +2,8 @@ class govuk::apps::contentapi( $port = 3022 ) {
   govuk::app { 'contentapi':
     app_type          => 'rack',
     port              => $port,
-    health_check_path => '/search.json';
-    extra_config      => 'proxy_set_header API-PREFIX $http_api_prefix;'
+    health_check_path => '/search.json',
+    nginx_extra_config      => 'proxy_set_header API-PREFIX $http_api_prefix;';
   }
 
   @@nagios::check { "check_contentapi_responsiveness":
