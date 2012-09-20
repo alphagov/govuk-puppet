b9bdc602a7ea2a81cd1521db85a5def605cdad51
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-09-20 10:30:50 +0000

    More static mappings for the licensify boxes

--- modules/nagios/files/usr/local/bin/reversedns.py
+++ modules/nagios/files/usr/local/bin/reversedns.py
@@ -12,7 +12,10 @@ STATIC_MAPPING = {
     'ip-10-229-67-207.eu-west-1.compute.internal': 'whitehall.production.alphagov.co.uk',
     'ip-10-32-18-246.eu-west-1.compute.internal': 'puppet.cluster',
     'ip-10-237-35-45' : 'ip-10-237-35-45.eu-west-1.compute.internal',
-    'ip-10-229-67-16' : 'ip-10-229-67-16.eu-west-1.compute.internal'
+    'ip-10-229-67-16' : 'ip-10-229-67-16.eu-west-1.compute.internal',
+    'ip-10-239-11-229': 'ip-10-239-11-229.eu-west-1.compute.internal',
+    'ip-10-32-34-170' : 'ip-10-32-34-170.eu-west-1.compute.internal',
+    'ip-10-32-57-17'  : 'ip-10-32-57-17.eu-west-1.compute.internal'
 
 }
 
