c8f368a34fcb1e523ed834a449d54f0e1f5391f0
Philip Potter <philipgpotter@gmail.com>
2012-09-21 16:17:14 +0100

    fix haproxy test

--- modules/haproxy/spec/defines/balance_ssl_spec.rb
+++ modules/haproxy/spec/defines/balance_ssl_spec.rb
@@ -17,17 +17,17 @@ describe 'haproxy::balance_ssl', :type => :define do
 
     it 'should create haproxy fragment listening on 8080' do
       # ensure correct order
-      should contain_concat__fragment('haproxy_listen_giraffe').
+      should contain_concat__fragment('haproxy_listen_ssl_giraffe').
         with_content(/listen giraffe 0.0.0.0:8080.*server giraffe-1.*server giraffe-2.*server giraffe-3/m)
       # ensure correct config
-      should contain_concat__fragment('haproxy_listen_giraffe').
+      should contain_concat__fragment('haproxy_listen_ssl_giraffe').
         with_content(/server giraffe-1 1.1.1.1:443.*port 9090/)
     end
 
     it 'should create nginx proxy allowing everyone access' do
       should contain_nginx__config__ssl('giraffe.dev.gov.uk')
-      should contain_nginx__config__site('giraffe.dev.gov.uk')
-      should_not contain_nginx__config__site('giraffe.dev.gov.uk').with_content(/deny all/)
+      should contain_nginx__config__site('ssl_giraffe.dev.gov.uk')
+      should_not contain_nginx__config__site('ssl_giraffe.dev.gov.uk').with_content(/deny all/)
     end
   end
 
@@ -44,7 +44,7 @@ describe 'haproxy::balance_ssl', :type => :define do
 
     it 'should create nginx proxy only allowing local access' do
       should contain_nginx__config__ssl('giraffe.dev.gov.uk')
-      should contain_nginx__config__site('giraffe.dev.gov.uk').
+      should contain_nginx__config__site('ssl_giraffe.dev.gov.uk').
         with_content(/deny all/)
     end
   end
