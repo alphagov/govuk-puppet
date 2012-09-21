3c6438ebdbf534308ac8a07308dbfd5d17d92277
Philip Potter <philipgpotter@gmail.com>
2012-09-21 10:38:42 +0100

    fix HAProxy configuration

--- modules/haproxy/manifests/init.pp
+++ modules/haproxy/manifests/init.pp
@@ -6,8 +6,15 @@ class haproxy {
     notify => Service[haproxy],
   }
 
+  file {'/etc/default/haproxy':
+    require => Package[haproxy],
+    notify  => Service[haproxy],
+    content => "ENABLED=1\n",
+  }
+
   concat {'/etc/haproxy/haproxy.cfg':
-    notify => Service[haproxy],
+    require => Package[haproxy],
+    notify  => Service[haproxy],
   }
 
   concat::fragment {"haproxy_header":
