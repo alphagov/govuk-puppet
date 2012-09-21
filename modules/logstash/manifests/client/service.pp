c07cc343f2ced0c8e2971f3487e40565b21bd20c
shodhan sheth <shodhan.sheth@digital.cabinet-office.gov.co.uk>
2012-09-21 10:07:04 +0000

    Adding restart cron for logstash as a workaround for leaking file descriptors

--- modules/logstash/manifests/client/service.pp
+++ modules/logstash/manifests/client/service.pp
@@ -9,6 +9,14 @@ class logstash::client::service {
     require  => File['/etc/init/logstash-client.conf'],
   }
 
+  cron { 'logstash-client':
+    ensure  => present,
+    user    => 'root',
+    hour  => [0, 8, 16],
+    command => '/usr/sbin/service logstash-client restart #Restart to ensure leaking file descriptors are given up',
+    require => Service['logstash-client'];
+  }
+
   @nagios::nrpe_config { 'check_logstash_client':
     source  => 'puppet:///modules/logstash/etc/nagios/nrpe.d/check_logstash_client.cfg',
   }
