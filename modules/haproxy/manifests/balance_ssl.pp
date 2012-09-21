dbf2203f2b67fe546bd7c50a68c7d028312ea463
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-09-21 15:02:25 +0100

    Duplicate the SSL load balancer for non-SSL

--- modules/haproxy/manifests/balance_ssl.pp
+++ modules/haproxy/manifests/balance_ssl.pp
@@ -1,7 +1,7 @@
 define haproxy::balance_ssl ($servers, $listen_port, $health_check_port, $internal_only = false) {
   concat::fragment {"haproxy_listen_$title":
     target  => '/etc/haproxy/haproxy.cfg',
-    content => template('haproxy/listen_fragment.erb'),
+    content => template('haproxy/listen_fragment_ssl.erb'),
     order   => 10,
   }
 
