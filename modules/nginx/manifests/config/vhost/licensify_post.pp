ec4076642164f85450e66294ee04976ddde216e1
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-09-21 14:27:50 +0000

    Getting around a potential puppet string interpolation bug

--- modules/nginx/manifests/config/vhost/licensify_post.pp
+++ modules/nginx/manifests/config/vhost/licensify_post.pp
@@ -1,15 +1,15 @@
 define nginx::config::vhost::licensify_post($to_port) {
+  $vhost_name = "post.licensify.${::govuk_platform}.alphagov.co.uk"
+  nginx::config::ssl { $vhost_name: certtype => 'wildcard_alphagov' }
+  nginx::config::site { $vhost_name: content => template('nginx/licensify-post-vhost.conf') }
 
-  nginx::config::ssl { $name: certtype => 'wildcard_alphagov' }
-  nginx::config::site { $name: content => template('nginx/licensify-post-vhost.conf') }
-
-  @logster::cronjob { "nginx-vhost-${name}":
-    args => "--metric-prefix ${name} NginxGangliaLogster /var/log/nginx/${name}-access.log",
+  @logster::cronjob { "nginx-vhost-${vhost_name}":
+    args => "--metric-prefix ${vhost_name} NginxGangliaLogster /var/log/nginx/${vhost_name}-access.log",
   }
 
-  @@nagios::check { "check_nginx_5xx_${name}_on_${::hostname}":
-    check_command       => "check_ganglia_metric!${name}_nginx_http_5xx!0.05!0.1",
-    service_description => "check nginx error rate for ${name}",
+  @@nagios::check { "check_nginx_5xx_${vhost_name}_on_${::vhost_name}":
+    check_command       => "check_ganglia_metric!${vhost_name}_nginx_http_5xx!0.05!0.1",
+    service_description => "check nginx error rate for ${vhost_name}",
     host_name           => "${::govuk_class}-${::hostname}",
   }
 }
