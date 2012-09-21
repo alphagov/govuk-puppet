7b6d7c930f232e5b4e8179fe8a8e434d8cde7143
Philip Potter <philipgpotter@gmail.com>
2012-09-21 11:53:09 +0100

    front HAProxy with nginx to handle vhost multiplexing

--- modules/haproxy/manifests/balance.pp
+++ modules/haproxy/manifests/balance.pp
@@ -4,4 +4,15 @@ define haproxy::balance ($servers, $listen_port, $health_check_port) {
     content => template('haproxy/listen_fragment.erb'),
     order   => 10,
   }
+
+  include nginx
+
+  $vhost_suffix = extlookup('app_domain_suffix','dev.gov.uk')
+  $vhost = "${title}.${vhost_suffix}"
+  $ssl_only = true
+
+  nginx::config::ssl{$vhost: certtype => "wildcard_alphagov" }
+  nginx::config::site{$vhost:
+    content => template('haproxy/nginx_ssl_proxy.erb')
+  }
 }
