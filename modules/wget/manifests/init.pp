b0c1b92d43572959bad98fcbdb9731337a4a8f8f
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-09-25 14:03:23 +0100

    I'm an idiot. I meant 1.14.

--- modules/wget/manifests/init.pp
+++ modules/wget/manifests/init.pp
@@ -1,10 +1,10 @@
 class wget {
 
-  # wget v0.14 is provided by the GDS Packages repository
+  # wget v1.14 is provided by the GDS Packages repository
   include govuk::repository
 
   package { 'wget':
-    ensure => '0.14',
+    ensure => '1.14',
   }
 
 }
