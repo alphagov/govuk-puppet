bf8f8b6b956ec190c848b06bc992f8401385d290
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-09-27 11:05:37 +0100

    Ensure Crypt::SSLeay is installed on the redirector
    We're going to be running the redirector integration tests *from* the
    redirector server itself -- they take 5m to run this way, and over 40m if we
    have to wait for the network overhead of running them from Jenkins. In order
    to do this, Crypt::SSLeay needs to be installed.
    
    At some point in the future when we're confident of the redirector, this can
    be removed.

--- manifests/classes/govuk_node/redirector_server.pp
+++ manifests/classes/govuk_node/redirector_server.pp
@@ -2,4 +2,15 @@
 class govuk_node::redirector_server inherits govuk_node::base {
   include nginx
   include govuk::apps::redirector
+
+  # We're going to be running the redirector integration tests *from* the
+  # redirector server itself -- they take 5m to run this way, and over 40m if we
+  # have to wait for the network overhead of running them from Jenkins. In order
+  # to do this, Crypt::SSLeay needs to be installed.
+  #
+  # At some point in the future when we're confident of the redirector, this can
+  # be removed.
+  package { 'libcrypt-ssleay-perl':
+    ensure => present,
+  }
 }
