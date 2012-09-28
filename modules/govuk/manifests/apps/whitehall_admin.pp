ffa217c0971f6f97b30c62868e342718fe84360f
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-09-28 17:55:00 +0100

    Monitoring check for whitehall scheduled publishing
    It's scheduled to run every 15 minutes. We ensure that it's run at least
    once in the last 16 minutes, allowing a minute's grace.

--- modules/govuk/manifests/apps/whitehall_admin.pp
+++ modules/govuk/manifests/apps/whitehall_admin.pp
@@ -4,4 +4,11 @@ class govuk::apps::whitehall_admin( $port = 3026 ) {
     port              => $port,
     health_check_path => '/';
   }
+
+  @@nagios::check { "check_scheduled_publishing":
+    check_command       => 'check_graphite_metric!hitcount(stats.govuk.app.whitehall.scheduled_publishing.call_count,"16minutes"))!0.9:100!0.9:100',
+    service_description => 'scheduled publishing should run at least once every 16 minutes',
+    host_name           => "${::govuk_class}-${::hostname}",
+  }
+
 }
