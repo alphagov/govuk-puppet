d579d47626c7ce4b71c1213278056ad86668b376
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-09-28 10:25:27 +0000

    Make the purge init_d script kill process only if its not upstart

--- modules/loadbalancer/files/purge_init_d.sh
+++ modules/loadbalancer/files/purge_init_d.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+if[ -f /etc/init.d/nginx ]; then
+  ps aux | grep "nginx: master process" | grep -v grep | awk ' { print $2 } ' | xargs kill
+   /bin/rm -f /etc/init.d/nginx
+   /usr/sbin/update-rc.d nginx remove
+fi
 
-/usr/sbin/service stop nginx
-/usr/sbin/service stop haproxy
-set -e
-/bin/rm -f /etc/init.d/nginx
-/bin/rm -f /etc/init.d/haproxy
-/usr/sbin/update-rc.d nginx remove
-/usr/sbin/update-rc.d haproxy remove
+if[ -f /etc/init.d/nginx ]; then
+  ps aux | grep "/usr/sbin/haproxy" | grep -v grep | awk ' { print $2 } ' | xargs kill
+  /bin/rm -f /etc/init.d/haproxy
+  /usr/sbin/update-rc.d haproxy remove
+fi
