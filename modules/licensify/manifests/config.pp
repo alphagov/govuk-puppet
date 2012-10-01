d342628e5e2e7d9b75da18ac9d133cf52d6f6c56
Mat Wall <mwall@alphagov.co.uk>
2012-10-01 16:52:01 +0100

    Removed duplicate AWS settings from config

--- modules/licensify/manifests/config.pp
+++ modules/licensify/manifests/config.pp
@@ -1,8 +1,5 @@
 class licensify::config {
 
-  $aws_access_key_id = extlookup('aws_access_key_id', '')
-  $aws_secret_key = extlookup('aws_secret_key', '')
-
   file { '/etc/gds-ertp-config.properties':
     ensure => present,
     source => [
