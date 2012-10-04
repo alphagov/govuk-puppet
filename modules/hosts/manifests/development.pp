90d33761f23b13c35d4d8dacb188e9d6f842d074
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-10-04 14:32:39 +0100

    Add whitehall.dev into dev hosts

--- modules/hosts/manifests/development.pp
+++ modules/hosts/manifests/development.pp
@@ -34,4 +34,5 @@ class hosts::development {
   host { 'datainsight-todays-activity-recorder.dev.gov.uk': ip => '127.0.0.1' }
   host { 'datainsight-format-success-recorder.dev.gov.uk':  ip => '127.0.0.1' }
   host { 'feedback.dev.gov.uk':                             ip => '127.0.0.1' }
+  host { 'whitehall.dev.gov.uk':                            ip => '127.0.0.1' }
 }
