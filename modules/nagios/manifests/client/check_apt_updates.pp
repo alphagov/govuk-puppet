3649f90bfb7be31cd5aaeab9a8f27cc4a051e40d
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-10-04 15:21:35 +0100

    This is bad syntax for NRPE arguments and we are lucky the script still worked.

--- modules/nagios/manifests/client/check_apt_updates.pp
+++ modules/nagios/manifests/client/check_apt_updates.pp
@@ -3,7 +3,7 @@ class nagios::client::check_apt_updates {
     source  => 'puppet:///modules/nagios/usr/lib/nagios/plugins/check_apt_updates',
   }
   @@nagios::check { "check_apt_updates_${::hostname}":
-    check_command       => 'check_nrpe!check_apt_updates!100!100',
+    check_command       => 'check_nrpe!check_apt_updates!100 100',
     service_description => "check package updates on ${::govuk_class}-${::hostname}",
     host_name           => "${::govuk_class}-${::hostname}",
   }
