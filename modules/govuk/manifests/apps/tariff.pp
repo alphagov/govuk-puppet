dfb56c87b6cd0e3fbce60db3cfca2ed4a5569ccb
James Abley <james.abley@gmail.com>
2012-10-04 14:09:34 +0100

    Tariff healthchecks which use the DB
    Want to do full-stack health check.

--- modules/govuk/manifests/apps/tariff.pp
+++ modules/govuk/manifests/apps/tariff.pp
@@ -2,6 +2,6 @@ class govuk::apps::tariff($port = 3017) {
   govuk::app { 'tariff':
     app_type          => 'rack',
     port              => $port,
-    health_check_path => '/trade-tariff';
+    health_check_path => '/trade-tariff/sections';
   }
 }
--- modules/govuk/manifests/apps/tariff_api.pp
+++ modules/govuk/manifests/apps/tariff_api.pp
@@ -2,6 +2,6 @@ class govuk::apps::tariff_api($port = 3018) {
   govuk::app { 'tariff-api':
     app_type          => 'rack',
     port              => $port,
-    health_check_path => '/';
+    health_check_path => '/sections';
   }
 }
