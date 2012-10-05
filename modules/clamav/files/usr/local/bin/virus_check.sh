5a7285d5704956f5c3777bd709071ac5833a3655
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-10-05 15:01:11 +0100

    Don't abort virus scan if a file with error encountered
    If clamav can't parse a file for some reason, we'll just treat it as
    infected and log an error message. We've observed PDFs that had parsing
    errors.

--- modules/clamav/files/usr/local/bin/virus_check.sh
+++ modules/clamav/files/usr/local/bin/virus_check.sh
@@ -14,8 +14,8 @@ for file in `find . -type f`; do
     rsync -R --remove-source-files $file $3
     ;;
   *)
+    rsync -R --remove-source-files $file $3
     echo "Checking file with 'clamscan $file' failed for unknown reason" >&2
-    exit 1
     ;;
   esac
 done
