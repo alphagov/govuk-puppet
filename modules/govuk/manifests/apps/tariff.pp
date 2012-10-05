de2bddb59be1b5f9657c94a208d21babc862a37c
Matthew Ford <matt@bitzesty.com>
2012-10-05 18:30:52 +0200

    Update modules/govuk/manifests/apps/tariff.pp
    update with healthcheck url

--- modules/govuk/manifests/apps/tariff.pp
+++ modules/govuk/manifests/apps/tariff.pp
@@ -2,6 +2,6 @@ class govuk::apps::tariff($port = 3017) {
   govuk::app { 'tariff':
     app_type          => 'rack',
     port              => $port,
-    health_check_path => '/trade-tariff/sections';
+    health_check_path => '/trade-tariff/healthcheck';
   }
 }
