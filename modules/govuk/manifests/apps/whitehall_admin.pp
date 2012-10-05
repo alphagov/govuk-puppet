07ddef25f45ee49f60e1a50da58c7f89ae3ef7db
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-10-05 11:42:05 +0100

    Add healthchecks for Whitehall and Whitehall-admin

--- modules/govuk/manifests/apps/whitehall_admin.pp
+++ modules/govuk/manifests/apps/whitehall_admin.pp
@@ -2,7 +2,7 @@ class govuk::apps::whitehall_admin( $port = 3026 ) {
   govuk::app { 'whitehall-admin':
     app_type          => 'rack',
     port              => $port,
-    health_check_path => '/';
+    health_check_path => '/healthcheck';
   }
 
   file { "/data/uploads":
--- modules/govuk/manifests/apps/whitehall_frontend.pp
+++ modules/govuk/manifests/apps/whitehall_frontend.pp
@@ -2,6 +2,6 @@ class govuk::apps::whitehall_frontend( $port = 3020 ) {
   govuk::app { 'whitehall-frontend':
     app_type          => 'rack',
     port              => $port,
-    health_check_path => '/government/home';
+    health_check_path => '/healthcheck';
   }
 }
