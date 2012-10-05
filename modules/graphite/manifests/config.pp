245c0a54264e6ca3487280ff1091463a41228599
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-10-05 11:07:44 +0000

    Adding the full path for the carbon cache check

--- modules/graphite/manifests/config.pp
+++ modules/graphite/manifests/config.pp
@@ -41,7 +41,7 @@ class graphite::config {
   }
 
   @@nagios::check { "check_carbon_cache_running_on_${::hostname}":
-    check_command       => 'check_nrpe_1arg!check_procs -a carbon-cache 1:',
+    check_command       => 'check_nrpe_1arg!/usr/lib/nagios/plugins/check_procs -a carbon-cache 1:',
     service_description => "check if carbon-cache is running on ${::govuk_class}-${::hostname}",
     host_name           => "${::govuk_class}-${::hostname}",
   }
