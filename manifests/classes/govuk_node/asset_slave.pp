c50327281f0d1687aa89aef2133a406196f78739
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-10-05 14:53:48 +0100

    Make asset slave sync assets from master
    This allows the slave to be a 'hot standby' assets server.
    
    We use NFS to mount the assets from the master to avoid setting up ssh
    keypairs or an rsync server.

--- manifests/classes/govuk_node/asset_slave.pp
+++ manifests/classes/govuk_node/asset_slave.pp
@@ -1,2 +1,25 @@
 class govuk_node::asset_slave inherits govuk_node::asset_master {
+
+  file { "/data/master-uploads":
+    ensure  => 'directory',
+    owner   => 'assets',
+    group   => 'assets',
+    mode    => '0664',
+    require => [User['assets'], Group['assets']],
+  }
+
+  mount { "/data/master-uploads":
+    ensure  => "mounted",
+    device  => "asset-master.${::govuk_platform}.alphagov.co.uk:/mnt/uploads",
+    fstype  => "nfs",
+    options => "ro",
+    atboot  => true,
+    require => [File["/data/master-uploads"]],
+  }
+
+  cron { 'sync-assets-from-master':
+    user      => 'assets',
+    minute    => '*/5',
+    command   => '/usr/bin/rsync -r /data/master-uploads/ /mnt/uploads',
+  }
 }
