75684a205bcfcaa45882a9884e44730359b2c6a5
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-10-06 15:04:46 +0100

    Tidy up the Backup class and add it to the backup server manifest

--- modules/backup/manifests/client.pp
+++ modules/backup/manifests/client.pp
@@ -1,17 +1,8 @@
 class backup::client {
 
-    user { 'backup':
-      ensure     => present,
-      comment    => 'Backup User <webops@digital.cabinet-office.gov.uk>',
-      home       => "/home/$title",
-      managehome => true,
-      groups     => ['backup'],
-      require    => Group['backup'],
-      shell      => '/bin/bash',
-    }
-
-  govuk::user { 'backup':
+    govuk::user { 'backup':
       fullname  => 'Backup User',
       email     => 'webops@digital.cabinet-office.gov.uk';
     }
+
 }
