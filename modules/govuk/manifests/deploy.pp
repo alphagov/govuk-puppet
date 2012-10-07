087cc065b563990a60e5b25a6734965882816244
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-10-07 12:54:59 +0100

    Revert switching on $govuk_provider for asset host

--- modules/govuk/manifests/deploy.pp
+++ modules/govuk/manifests/deploy.pp
@@ -74,21 +74,11 @@ class govuk::deploy {
 
   $app_domain = extlookup('app_domain_suffix', 'dev.gov.uk')
 
-  case $::govuk_provider {
-    sky: {
-      # This will change. When we know what our assets CDN is going to be
-      # called and when we can put it there this should point there. For the
-      # moment we serve assets direct from static. Ugh. -NS
-      $asset_host = "https://static.${app_domain}"
-    }
-    default: {
-      $asset_host = $::govuk_platform ? {
-        'production'  => "https://d17tffe05zdvwj.cloudfront.net",
-        'preview'     => "https://djb1962t8apu5.cloudfront.net",
-        'development' => "http://static.${app_domain}",
-        default       => "https://static.${app_domain}",
-      }
-    }
+  $asset_host = $::govuk_platform ? {
+    'production'  => "https://d17tffe05zdvwj.cloudfront.net",
+    'preview'     => "https://djb1962t8apu5.cloudfront.net",
+    'development' => "http://static.${app_domain}",
+    default       => "https://static.${app_domain}",
   }
 
   file { '/etc/govuk/asset_host.conf':
