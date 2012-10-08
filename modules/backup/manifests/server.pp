277cf962c30221112f7faed4443e675b6b7236e7
Philip Potter <philipgpotter@gmail.com>
2012-10-08 11:57:41 +0100

    use correct puppet syntax

--- modules/backup/manifests/server.pp
+++ modules/backup/manifests/server.pp
@@ -4,21 +4,21 @@ class backup::server {
 
   file {'/data/backups':
     ensure  => directory,
-    user    => 'backup',
+    owner   => 'backup',
     mode    => '0700',
     require => User['backup']
   }
 
   file {'/home/backup/.ssh':
     ensure  => directory,
-    user    => 'backup',
+    owner   => 'backup',
     mode    => '0700',
     require => User['backup']
   }
 
   file {'/home/backup/.ssh/id_rsa':
     ensure   => file,
-    user     => 'backup',
+    owner    => 'backup',
     mode     => '0600',
     contents => extlookup('backup_key_private', ''),
     require  => File['/home/backup/.ssh'],
