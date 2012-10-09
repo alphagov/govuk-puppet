b7868cefa77f08b7e4a261262286f41e0c359666
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-10-09 11:56:00 +0100

    Add an alias of internalsupport to the support app

--- modules/govuk/manifests/apps/support.pp
+++ modules/govuk/manifests/apps/support.pp
@@ -1,6 +1,7 @@
 class govuk::apps::support($port = 3031) {
   govuk::app { 'support':
     app_type          => 'rack',
+    vhost_aliases     => ['internalsupport'],
     port              => $port,
     health_check_path => '/';
   }
