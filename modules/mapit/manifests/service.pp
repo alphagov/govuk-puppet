0e307925762222f3cb3b3ddc73fa2fe8ea288828
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-10-11 11:50:14 +0100

    Fix missing comma

--- modules/mapit/manifests/service.pp
+++ modules/mapit/manifests/service.pp
@@ -2,10 +2,10 @@ class mapit::service {
   service { 'mapit':
       ensure    =>  running,
       provider  =>  upstart,
-      subscribe =>  [ 
+      subscribe =>  [
                       File['/etc/init/mapit.conf'],
                       File['/data/vhost/mapit/mapit/conf/general.yml']
-                    ]
+                    ],
       require   =>  [
                       File['/etc/init/mapit.conf'],
                       Exec['unzip_mapit']
