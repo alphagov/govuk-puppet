90f937fa09d090041b6bd846b18bbd026563db26
Mark Norman Francis <norm@cackhanded.net>
2012-10-11 15:07:28 +0100

    Add 404 and 5xx nagios alerts to the redirector.

--- manifests/classes/govuk_node/redirector_server.pp
+++ manifests/classes/govuk_node/redirector_server.pp
@@ -17,4 +17,19 @@ class govuk_node::redirector_server inherits govuk_node::base {
   nginx::config::vhost::default { 'default':
     status => '444',
   }
+
+  @logster::cronjob { "nginx-redirector":
+    args => "--metric-prefix redirector RedirectorGangliaLogster /var/log/nginx/access.log",
+  }
+
+  @@nagios::check { "check_nginx_404_redirector_on_${::hostname}":
+    check_command       => "check_ganglia_metric!redirector_nginx_http_404!0.05!0.1",
+    service_description => "check nginx 404 rate for redirector",
+    host_name           => "${::govuk_class}-${::hostname}",
+  }
+  @@nagios::check { "check_nginx_5xx_redirector_on_${::hostname}":
+    check_command       => "check_ganglia_metric!redirector_nginx_http_5xx!0.05!0.1",
+    service_description => "check nginx error rate for redirector",
+    host_name           => "${::govuk_class}-${::hostname}",
+  }
 }
