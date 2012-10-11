859d89213a883776e77ccd0ee5d2af79d8cee848
Mat Wall <mwall@alphagov.co.uk>
2012-10-11 11:51:15 +0100

    Create the /etc/licensing directory for config files for licensify

--- modules/licensify/manifests/apps.pp
+++ modules/licensify/manifests/apps.pp
@@ -1,6 +1,13 @@
 class licensify::apps::base {
   $aws_access_key_id = extlookup('aws_access_key_id', '')
   $aws_secret_key = extlookup('aws_secret_key', '')
+
+  file { '/etc/licensing':
+    ensure  => directory,
+    mode    => '0755',
+    owner   => 'deploy',
+    group   => 'deploy',
+  }
 }
 
 class licensify::apps {
@@ -17,6 +24,7 @@ class licensify::apps::licensify( $port = 9000 ) inherits licensify::apps::base 
     environ_content    => template('licensify/environ'),
     nginx_extra_config => template('licensify/nginx_extra'),
     health_check_path  => '/api/licences'
+    require            => File['/etc/licensing'],
   }
 
   nginx::config::vhost::licensify_upload{ 'licensify':}
@@ -31,6 +39,7 @@ class licensify::apps::licensify_admin( $port = 9500 ) inherits licensify::apps:
     environ_content   => template('licensify/environ'),
     vhost_protected   => false,
     health_check_path => "/login"
+    require           => File['/etc/licensing'],
   }
 
   licensify::build_clean { 'licensify-admin': }
@@ -42,6 +51,7 @@ class licensify::apps::licensify_feed( $port = 9400 ) inherits licensify::apps::
     app_type        => 'procfile',
     port            => $port,
     environ_content => template('licensify/environ'),
+    require         => File['/etc/licensing'],
   }
 
   licensify::build_clean { 'licensify-feed': }
