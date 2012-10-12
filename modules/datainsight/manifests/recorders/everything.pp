94e5943e34ed17e588e2a1495c76be4615a63249
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-10-12 10:10:10 +0100

    Fixing Typo

--- modules/datainsight/manifests/recorders/everything.pp
+++ modules/datainsight/manifests/recorders/everything.pp
@@ -1,13 +1,13 @@
 class datainsight::recorders::everything {
 
   datainsight::recorder { 'everything': port => '3105' }
-  
-  file { [ '/var/lib/datainsight-narrative-recorder.json' ]:
+
+  file { [ '/var/tmp/datainsight-everything-recorder.json' ]:
     ensure  => present,
     owner   => 'deploy',
     group   => 'deploy',
   }
-  
+
   file { [ '/var/data/datainsight/everything' ]:
     ensure  => directory,
     owner   => 'deploy',
