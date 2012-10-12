d0e66dbe69ea710bcca7286aa3d436ee998a1bb6
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-10-12 10:01:03 +0100

    Add files for datainsight everything recorder.

--- modules/datainsight/manifests/recorders/everything.pp
+++ modules/datainsight/manifests/recorders/everything.pp
@@ -1,5 +1,17 @@
 class datainsight::recorders::everything {
 
   datainsight::recorder { 'everything': port => '3105' }
+  
+  file { [ '/var/lib/datainsight-narrative-recorder.json' ]:
+    ensure  => present,
+    owner   => 'deploy',
+    group   => 'deploy',
+  }
+  
+  file { [ '/var/data/datainsight/everything' ]:
+    ensure  => directory,
+    owner   => 'deploy',
+    group   => 'deploy',
+  }
 
 }
