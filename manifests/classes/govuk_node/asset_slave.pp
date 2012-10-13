03fa3cb2bf4e01270bd38361548bed80eed67cde
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-10-13 12:38:44 +0100

    remove duplicate firewall rule from asset::slave

--- manifests/classes/govuk_node/asset_slave.pp
+++ manifests/classes/govuk_node/asset_slave.pp
@@ -22,10 +22,6 @@ class govuk_node::asset_slave inherits govuk_node::asset_base {
     require => [File["/data/master-uploads"]],
   }
 
-  ufw::allow {'Allow all backend machines to access':
-    from => '10.3.0.0/16',
-  }
-
   cron { 'sync-assets-from-master':
     user      => 'assets',
     minute    => '*/5',
