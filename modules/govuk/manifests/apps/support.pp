e710ba7b74b551707cee2aa965c16b9f743baf43
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-10-15 16:11:05 +0100

    Revert "Make Support app respond on support.* and internalsupport.*"
    This reverts commit 77ad9664ace927713fa1bf9b985d963545506f58.

--- modules/govuk/manifests/apps/support.pp
+++ modules/govuk/manifests/apps/support.pp
@@ -1,17 +1,8 @@
 class govuk::apps::support($port = 3031) {
   govuk::app { 'support':
-    app_type           => 'rack',
-    vhost_aliases      => ['internalsupport'],
-    port               => $port,
-    health_check_path  => '/';
-    enable_nginx_vhost => false,
-  }
-  nginx::config::vhost::proxy { "support.$::govuk_platform.alphagov.co.uk":
-        to                    => ["localhost:${port}"],
-        protected             => true,
-        ssl_only              => true,
-        server_names          => ['support.*','internalsupport.*'],
-        health_check_path     => $health_check_path,
-        health_check_port     => $health_check_port,
+    app_type          => 'rack',
+    vhost_aliases     => ['internalsupport'],
+    port              => $port,
+    health_check_path => '/';
   }
 }
