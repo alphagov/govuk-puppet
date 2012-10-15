f4f1f300ce6c742d0376ab12248a459252508b02
Uttam Kini <uttam.kini@digital.cabinet-office.gov.uk>
2012-10-15 10:11:03 +0000

    Changing the name of uploadlicensify to uploadlicence in the haproxy config

--- manifests/classes/govuk_node/licensify_load_balancer.pp
+++ manifests/classes/govuk_node/licensify_load_balancer.pp
@@ -25,7 +25,7 @@ class govuk_node::licensify_load_balancer {
   }
 # Licensify upload pdf public endpoint
   haproxy::balance_https {
-    'uploadlicensify':
+    'uploadlicence':
       servers             => $licensify_frontend_servers,
       internal_only       => true,
       health_check_port   => 15500,  #The health check port is the same as licensify, since it is the same app listening on a different vhost
