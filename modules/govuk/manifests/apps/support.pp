102371ece6d3984afaddc8d8b803e52af0a15cd6
shodhan sheth <shodhan.sheth@digital.cabinet-office.gov.co.uk>
2012-10-15 17:28:15 +0000

    Adding htpasswd for support

--- modules/govuk/manifests/apps/support.pp
+++ modules/govuk/manifests/apps/support.pp
@@ -1,8 +1,16 @@
 class govuk::apps::support($port = 3031) {
+
+  file { '/etc/support.htpasswd':
+    ensure => present,
+    source => 'puppet:///modules/govuk/support.htpasswd'
+    }
   govuk::app { 'support':
-    app_type          => 'rack',
-    vhost_aliases     => ['internalsupport'],
-    port              => $port,
-    health_check_path => '/';
+    app_type                => 'rack',
+    vhost_aliases           => ['internalsupport'],
+    port                    => $port,
+    health_check_path       => '/',
+    nginx_extra_app_config  => 'auth_basic "Enter your password";
+    auth_basic_user_file  /etc/support.htpasswd;
+    ';
   }
 }
