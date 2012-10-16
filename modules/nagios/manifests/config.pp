7bbaac9adff9ea9b74f63387bc637d6d668af778
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-10-16 09:18:33 +0100

    Moving Whitehall checks from Urgent to High This check is not reliable. It fires multiple times overnight. Each of those times would result in a callout of the on-call who could do nothing about it. That is not acceptable and hence this alert is being deprioritised as per Gareth until it has been made more robust

--- modules/nagios/manifests/config.pp
+++ modules/nagios/manifests/config.pp
@@ -132,14 +132,14 @@ class nagios::config ($platform = $::govuk_platform) {
     @@nagios::check { "check_scheduled_publishing_${::hostname}":
       check_command       => "check_graphite_metric_since!hitcount(stats.govuk.app.whitehall.scheduled_publishing.call_rate,'16minutes')!16minutes!0.9:100!0.9:100",
       service_description => 'whitehall sched pub not run in prev 16m',
-      use                 => 'govuk_urgent_priority',
+      use                 => 'govuk_high_priority',
       host_name           => "${::govuk_class}-${::hostname}",
     }
 
     @@nagios::check { "check_no_overdue_scheduled_editions_${::hostname}":
       check_command       => "check_graphite_metric!stats.gauges.govuk.app.whitehall.scheduled_publishing.due!0!0",
       service_description => 'whitehall has overdue scheduled editions',
-      use                 => 'govuk_urgent_priority',
+      use                 => 'govuk_high_priority',
       host_name           => "${::govuk_class}-${::hostname}",
     }
 
