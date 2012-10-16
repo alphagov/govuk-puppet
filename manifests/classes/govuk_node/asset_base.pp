fe1f64e0f110a8b528ec41f555819942f63dbd47
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-10-16 17:21:15 +0100

    Revert "Firewall only exists in skyscape"
    This reverts commit 6c57a94fb0560c70ea2a1de21300c0a56b6de364.

--- manifests/classes/govuk_node/asset_base.pp
+++ manifests/classes/govuk_node/asset_base.pp
@@ -29,13 +29,8 @@ class govuk_node::asset_base inherits govuk_node::base {
     notify  => Service['nfs-kernel-server'],
   }
 
-  case $::govuk_provider {
-    'sky', 'scc': {
-      ufw::allow { 'Allow all access from backend machines':
-        from => '10.3.0.0/16',
-      }
-    }
-    default: {}
+  ufw::allow { 'Allow all access from backend machines':
+    from => '10.3.0.0/16',
   }
 
   package { 'nfs-kernel-server':
