90795bcd06dd3e99d194861f86047a9564b59413
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-10-16 16:51:41 +0100

    Periodically rescan clean directory for viruses

---
+++ modules/clamav/files/usr/local/bin/virus_scan.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+INCOMING_DIR="$1"
+INFECTED_DIR="$2"
+CLEAN_DIR="$3"
+CLAMSCAN_CMD="/opt/clamav/bin/clamscan"
+
+usage() {
+  echo "USAGE: $0 <incoming_dir> <infected_dir> [<clean_dir>]"
+  echo
+  echo "Purpose:"
+  echo "Scans <incoming_dir> and moves infected files to <infected_dir>. Optionally"
+  echo "moves clean files to <clean_dir>."
+  echo
+  exit 1
+}
+
+if [ $# -lt 2 ]; then
+  usage
+fi
+
+make_result_file() {
+  if [ -n "`which tempname`" ]; then
+    RESULTFILE=`tempfile -p CLAM-`
+  elif [ -n "`which mktemp`" ]; then
+    RESULTFILE=`mktemp -t CLAM-`
+  else
+    echo "ERROR: no mktemp or tempname command"
+    exit 1
+  fi
+}
+
+check_dir_exists() {
+  if [ ! -d "$1" ] || [ ! -w "$1" ]; then
+    echo "ERROR: directory '$1' does not exist or is not writable"
+    exit 1
+  fi
+}
+
+check_dir_exists $INCOMING_DIR
+check_dir_exists $INFECTED_DIR
+if [ -n "$CLEAN_DIR" ]; then
+  check_dir_exists $CLEAN_DIR
+fi
+
+set -o errexit # fail on errors
+pushd "$INCOMING_DIR" > /dev/null 2>&1
+set +o errexit # disable fail on errors
+
+make_result_file
+$CLAMSCAN_CMD --no-summary --stdout -r . > "$RESULTFILE"
+set -o errexit # enable fail on errors
+
+echo "`grep 'FOUND$' "$RESULTFILE" | wc -l` viruses found"
+echo "`grep ': OK$' "$RESULTFILE" | wc -l` clean found"
+
+grep 'FOUND$' "$RESULTFILE" | sed 's/: [^:]* FOUND$//' | rsync --remove-source-files --files-from=- . "$INFECTED_DIR/."
+if [ -n "$CLEAN_DIR" ]; then
+  grep ': OK$' "$RESULTFILE" | sed 's/: OK$//' | rsync --remove-source-files --files-from=- . "$CLEAN_DIR/."
+fi
+rm $RESULTFILE
+popd
