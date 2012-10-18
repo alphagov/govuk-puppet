5616f70cfa5d3c406f20e56c98115ae7d1a3c16a
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-10-18 19:22:34 +0100

    Tone down the Router 5xx alert rate

--- modules/router/manifests/nginx.pp
+++ modules/router/manifests/nginx.pp
@@ -73,7 +73,7 @@ class router::nginx {
   }
 
   @@nagios::check { "check_nginx_5xx_on_${::hostname}":
-    check_command       => 'check_ganglia_metric!nginx_http_5xx!0.05!0.1',
+    check_command       => 'check_ganglia_metric!nginx_http_5xx!1!5',
     service_description => 'router nginx high 5xx rate',
     host_name           => "${::govuk_class}-${::hostname}",
   }
