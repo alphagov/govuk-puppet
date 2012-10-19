33020a26758e77e4f75dc1cf992ab5013abfe268
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-10-19 11:31:40 +0000

    Remove reference to dead file I just removed

--- modules/govuk/manifests/apps/support.pp
+++ modules/govuk/manifests/apps/support.pp
@@ -1,9 +1,5 @@
 class govuk::apps::support($port = 3031) {
 
-  file { '/etc/support.htpasswd':
-    ensure => present,
-    source => 'puppet:///modules/govuk/support.htpasswd'
-    }
   govuk::app { 'support':
     app_type                => 'rack',
     port                    => $port,
