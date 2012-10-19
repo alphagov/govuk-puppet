370a089c583c9bd55f9cedc039c0ee943a5df78f
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-10-19 17:28:09 +0100

    Add back healthchecks for EFG application
    These were accidentally removed when moving the application to listen on
    www.sflg.gov.uk. I've put them back, but in a slightly bodgy way that we
    should definitely fix at some point in the near future. See comments in
    
        modules/govuk/manifests/apps/efg.pp
    
    for details.

--- modules/govuk/manifests/apps/efg.pp
+++ modules/govuk/manifests/apps/efg.pp
@@ -2,18 +2,40 @@ class govuk::apps::efg( $port = 3019 ) {
   govuk::app { 'efg':
     app_type           => 'rack',
     port               => $port,
-    health_check_path  => "/healthcheck",
     enable_nginx_vhost => false,
   }
 
   $vhost_name = extlookup('efg_domain', 'efg.dev.gov.uk')
 
+  # BEWARE A BODGE
+  #
+  # Because the EFG app lives, unprotected, at, efg_domain (probably
+  # www.sflg.gov.uk), I've had to copy and paste some of the health check code
+  # from govuk::app. Ideally, we'd move this back to
+  # efg.production.alphagov.co.uk or similar, and terminate SSL for
+  # www.sflg.gov.uk independently at the top of the stack.
+  #
+  # -NS 2012-10-19
+
+  $health_check_path = "/healthcheck"
+  $health_check_port = $port + 6500
+  $ssl_health_check_port = $port + 6400
+  @ufw::allow {
+    "allow-loadbalancer-health-check-efg-http-from-all":
+      port => $health_check_port;
+    "allow-loadbalancer-health-check-efg-https-from-all":
+      port => $ssl_health_check_port;
+  }
+
   nginx::config::vhost::proxy { $vhost_name:
-    to                => ["localhost:${port}"],
-    aliases           => ["efg.production.alphagov.co.uk"],
-    protected         => false,
-    ssl_only          => true,
-    extra_config      => '
+    to                    => ["localhost:${port}"],
+    aliases               => ["efg.production.alphagov.co.uk"],
+    protected             => false,
+    health_check_path     => $health_check_path,
+    health_check_port     => $health_check_port,
+    ssl_only              => true,
+    ssl_health_check_port => $ssl_health_check_port,
+    extra_config          => '
   location /sflg/ {
     rewrite ^ https://$server_name/? permanent;
   }
@@ -22,6 +44,12 @@ class govuk::apps::efg( $port = 3019 ) {
     rewrite ^ https://efg.production.alphagov.co.uk/? redirect;
   }
 ';
+  }
+
+  @@nagios::check { "check_app_efg_up_on_${::hostname}":
+    check_command       => "check_nrpe!check_app_up!${port} ${health_check_path}",
+    service_description => "efg app running",
+    host_name           => "${::govuk_class}-${::hostname}",
   }
 
   @@nagios::check { "check_efg_login_failures_${::hostname}":
