dbf15f80beeb9859cdbb28b739fef83bee99e614
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-10-19 11:39:13 +0000

    Restrict support to https only
    This should reduce session cookie exposure, but also
    prevents a confusing/non-functional experience if you
    attempt to authenticate with signon having visited the
    app on http.

--- modules/govuk/manifests/apps/support.pp
+++ modules/govuk/manifests/apps/support.pp
@@ -3,6 +3,7 @@ class govuk::apps::support($port = 3031) {
   govuk::app { 'support':
     app_type                => 'rack',
     port                    => $port,
+    vhost_ssl_only          => true,
     health_check_path       => '/';
   }
 }
