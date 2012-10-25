c84479d198ca3fbf267e936e358d88a97e7d8372
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-10-25 15:35:25 +0100

    Password protect migratorator API

--- modules/govuk/manifests/apps/migratorator.pp
+++ modules/govuk/manifests/apps/migratorator.pp
@@ -1,8 +1,19 @@
 class govuk::apps::migratorator( $port = 3015 ) {
+
+  include govuk::htpasswd
+
   govuk::app { 'migratorator':
-    app_type          => 'rack',
-    port              => $port,
-    vhost_ssl_only    => true,
-    health_check_path => '/',
+    app_type           => 'rack',
+    port               => $port,
+    vhost_ssl_only     => true,
+    health_check_path  => '/',
+    nginx_extra_config => '
+    location / {
+      deny all;
+      auth_basic            "Migratorator";
+      auth_basic_user_file  /etc/govuk.htpasswd;
+      satisfy any;
+    }',
   }
+
 }
