b658a41e506312beefe33650c85d306e1b175737
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-10-25 19:13:53 +0100

    Fix Migratorator password protection

--- modules/govuk/manifests/apps/migratorator.pp
+++ modules/govuk/manifests/apps/migratorator.pp
@@ -8,12 +8,11 @@ class govuk::apps::migratorator( $port = 3015 ) {
     vhost_ssl_only     => true,
     health_check_path  => '/',
     nginx_extra_config => '
-    location / {
       deny all;
       auth_basic            "Migratorator";
       auth_basic_user_file  /etc/govuk.htpasswd;
       satisfy any;
-    }',
+    ',
   }
 
 }
