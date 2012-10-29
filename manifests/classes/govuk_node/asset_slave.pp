61b43b4c16c60743a89d555cdfdad6b474b7c1be
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-10-29 09:21:48 +0000

    Remove the mode from /data/master-uploads as puppet cannot set this on a ro filesystem

--- manifests/classes/govuk_node/asset_slave.pp
+++ manifests/classes/govuk_node/asset_slave.pp
@@ -9,7 +9,6 @@ class govuk_node::asset_slave inherits govuk_node::asset_base {
     ensure  => 'directory',
     owner   => 'assets',
     group   => 'assets',
-    mode    => '0664',
     require => [User['assets'], Group['assets']],
   }
 
