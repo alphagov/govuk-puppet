be443f161d44025e4f2f0da72693604323f76d35
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-10-31 11:09:32 +0000

    Hack up the innards of Puppet with mock extdata
    It seems that there is no nice way to mock the provision of extdata for the
    test environment. This commit adds a MockExtdata module which provides a few
    helper methods for managing extdata when running rspec tests.
    
    Also sets the app_domain extdata value to a default of 'test.gov.uk'

--- spec_helper.rb
+++ spec_helper.rb
@@ -1,6 +1,50 @@
+require 'csv'
 require 'rspec-puppet'
 
+module MockExtdata
+
+  def set_extdata(h)
+    @mock_extdata = h
+  end
+
+  def update_extdata(h)
+    @mock_extdata.merge!(h)
+  end
+
+  def setup_extdata_stubs
+    @mock_extdata ||= {}
+
+    old_exists = File.method(:exists?)
+    File.stub(:exists?) do |f|
+      if f =~ /extdata\/[^\/]*\.csv$/
+        true
+      else
+        old_exists.call(f)
+      end
+    end
+
+    old_read = CSV.method(:read)
+    CSV.stub(:read) do |f, *args|
+      if f =~ /extdata\/[^\/]*\.csv$/
+        @mock_extdata.to_a
+      else
+        old_read.call(f, *args)
+      end
+    end
+  end
+
+end
+
+HERE = File.expand_path(File.dirname(__FILE__))
+
 RSpec.configure do |c|
-  c.module_path = File.expand_path(File.join(File.dirname(__FILE__), 'modules'))
-  c.manifest    = File.expand_path(File.join(File.dirname(__FILE__))) + "/manifests/site.pp"
+  c.module_path = File.join(HERE, 'modules')
+  c.manifest    = File.join(HERE, 'manifests', 'site.pp')
+
+  c.include MockExtdata
+
+  c.before do
+    setup_extdata_stubs
+    set_extdata({ 'app_domain' => 'test.gov.uk' })
+  end
 end
