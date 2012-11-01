feb64ab3b1c834d49461703d64c0b708b61fc7ae
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-11-01 12:04:39 +0000

    Simplify spec_helper.rb

--- spec_helper.rb
+++ spec_helper.rb
@@ -1,6 +1,8 @@
 require 'rspec-puppet'
 
+HERE = File.expand_path(File.dirname(__FILE__))
+
 RSpec.configure do |c|
-  c.module_path = File.expand_path(File.join(File.dirname(__FILE__), 'modules'))
-  c.manifest    = File.expand_path(File.join(File.dirname(__FILE__))) + "/manifests/site.pp"
+  c.module_path = File.join(HERE, 'modules')
+  c.manifest    = File.join(HERE, 'manifests', 'site.pp')
 end
