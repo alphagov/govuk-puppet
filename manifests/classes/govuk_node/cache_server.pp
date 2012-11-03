b059a301114ae21a39195f129ef81407a9ed5f4f
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-11-03 18:31:06 +0000

    Allow Varnish purging from backend machines

--- manifests/classes/govuk_node/cache_server.pp
+++ manifests/classes/govuk_node/cache_server.pp
@@ -14,4 +14,14 @@ class govuk_node::cache_server inherits govuk_node::base {
   nginx::config::vhost::default { 'default':
     status => '444',
   }
+
+  case $::govuk_provider {
+    'sky': {
+        ufw::allow { 'Allow varnish cache bust from backend machines':
+          from => '10.3.0.0/24',
+          port => '7999'
+        }
+    }
+    default: {}
+  }
 }
