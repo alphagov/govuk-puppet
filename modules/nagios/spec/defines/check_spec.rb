15a5ee841dadb40c171d72cb3f99c8a4e2adc0e2
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-11-03 14:13:10 +0000

    This test has been "disabled with xit" for months.

--- modules/nagios/spec/defines/check_spec.rb
+++ modules/nagios/spec/defines/check_spec.rb
@@ -26,19 +26,19 @@ end
 
 
 
-describe 'nagios::check', :type => :define do
-  let(:title) { 'bad-description' }
-  let(:pre_condition) {  'nagios::host{"bruce-forsyth":}' }
-  let(:params) {{
-    "check_command" => "nice-to-see-you",
-    "host_name" => "bruce-forsyth",
-    "service_description" => "greengrocer's apostrophe's"
-  }}
-
-  # can't work out how to get puppetmaster to use kwalify gem, so disabling this for now
-  xit do
-    expect {
-      should contain_file('/etc/nagios3/conf.d/nagios_host_bruce-forsyth/bad-description.cfg')
-    }.to raise_error(Puppet::Error, /Failed kwalify schema validation/)
-  end
-end
+#describe 'nagios::check', :type => :define do
+#  let(:title) { 'bad-description' }
+#  let(:pre_condition) {  'nagios::host{"bruce-forsyth":}' }
+#  let(:params) {{
+#    "check_command" => "nice-to-see-you",
+#    "host_name" => "bruce-forsyth",
+#    "service_description" => "greengrocer's apostrophe's"
+#  }}
+#
+#  # can't work out how to get puppetmaster to use kwalify gem, so disabling this for now
+#  xit do
+#    expect {
+#      should contain_file('/etc/nagios3/conf.d/nagios_host_bruce-forsyth/bad-description.cfg')
+#    }.to raise_error(Puppet::Error, /Failed kwalify schema validation/)
+#  end
+#end
