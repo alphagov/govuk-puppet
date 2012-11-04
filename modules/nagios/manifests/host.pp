fa2811314d20862fde7789b98659d501b26b6c15
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-11-04 10:44:04 +0000

    Need a title here so it works with export

--- modules/nagios/manifests/host.pp
+++ modules/nagios/manifests/host.pp
@@ -5,7 +5,7 @@ define nagios::host (
   $host_name  = $::fqdn
 ) {
 
-  file {"/etc/nagios3/conf.d/nagios_host_${host_name}":
+  file {"/etc/nagios3/conf.d/nagios_host_${title}":
     ensure  => directory,
     purge   => true,
     force   => true,
@@ -14,7 +14,7 @@ define nagios::host (
     notify  => Class['nagios::service'],
   }
 
-  file {"/etc/nagios3/conf.d/nagios_host_${host_name}.cfg":
+  file {"/etc/nagios3/conf.d/nagios_host_${title}.cfg":
     ensure  => present,
     content => template('nagios/host.erb'),
     require => Class['nagios::package'],
