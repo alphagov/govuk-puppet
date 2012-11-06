6a2d15b183bc895caccefe7593f1a16348630e15
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-11-06 10:59:47 +0000

    Restrict access to the Staging and Production jumpboxes to only people I like

--- modules/ssh/manifests/config.pp
+++ modules/ssh/manifests/config.pp
@@ -1,5 +1,5 @@
 class ssh::config {
-
+  $ssh_allow_users = extlookup('ssh_allow_users', '*')
   file { '/etc/ssh/sshd_config':
     content => template('ssh/sshd_config.erb'),
     mode    => '0400',
