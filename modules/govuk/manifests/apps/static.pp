63c180bebf53053a6f061663ecc9cc99cafcda75
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-11-08 11:52:14 +0000

    Set X-Forwarded-Host on static
    When static proxy-passes to backend apps, it needs to set the
    X-Forwarded-Host header so that backend apps know what the external
    host name is. There's a particular issue[1] affecting detailed guides at
    the moment which provked this change:- when files are being
    virus-scanned (or have been quarantined), we issue a redirect at the
    asset URL to a placeholder. Currently that redirect sends people to a
    whitehall-admin url which prompts for basic auth password. This is
    causing trouble on the live site at the moment.
    
    [1] https://www.pivotaltracker.com/story/show/38720285

--- modules/govuk/manifests/apps/static.pp
+++ modules/govuk/manifests/apps/static.pp
@@ -17,6 +17,10 @@ class govuk::apps::static( $port = 3013 ) {
     server_names      => ['static.*', 'assets.*'],
     extra_root_config => "location ~ ^/government/(assets|uploads)/ {
       proxy_set_header Host $whitehall_frontend_host;
+      proxy_set_header X-Real-IP \$remote_addr;
+      proxy_set_header X-Forwarded-Server \$host;
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Host \$host;
       proxy_pass http://$whitehall_frontend_host;
     }
     ",
