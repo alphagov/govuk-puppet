a3097087f88f35116100b0bca5e642c46e4d30b2
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-11-20 10:44:27 +0000

    Add some docs to envvar manifests

--- modules/govuk/manifests/app/envvar.pp
+++ modules/govuk/manifests/app/envvar.pp
@@ -1,3 +1,12 @@
+# govuk::app::envvar
+#
+# Defines an app-specific environment variable to be exported when the app is
+# run using govuk_spinup or govuk_setenv.
+#
+# Unlike govuk::envvar, the dependency between the govuk::app::envvar and the
+# application is explicitly encoded, so changing a govuk::app::envvar should
+# automatically restart the relevant application.
+
 define govuk::app::envvar (
   # Name of the application with which this env var is associated
   $app,
--- modules/govuk/manifests/envvar.pp
+++ modules/govuk/manifests/envvar.pp
@@ -1,3 +1,13 @@
+# govuk::envvar
+#
+# Defines a global GOV.UK environment variable, which will be exported for all
+# applications when using govuk_spinup or govuk_setenv.
+#
+# NB: most applications will alter their behaviour on the basis of the value
+# of govuk::envvars, but there is no explicit dependency encoded here. You may
+# need to restart an application before it picks up a changed environment
+# variable.
+
 define govuk::envvar ($value, $envdir = '/etc/govuk/env.d', $varname = $title) {
 
   file { "${envdir}/${varname}":
