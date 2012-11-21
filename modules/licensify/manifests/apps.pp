609546ed40db7c671a79a3db2d6589909c0589e9
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-11-21 00:55:33 +0000

    Oh joy. Oh wonder. Oh why am I doing this at 1AM?

--- modules/licensify/manifests/apps.pp
+++ modules/licensify/manifests/apps.pp
@@ -68,16 +68,16 @@ define licensify::apps::envvars($app) {
   $aws_access_key_id = extlookup('aws_access_key_id', '')
   $aws_secret_key = extlookup('aws_secret_key', '')
 
-  Govuk::App::Envvar {
-    app => $app,
+  govuk::app::envvar { "${app}-LANG":
+    varname => 'LANG',
+    value   => 'en_GB.UTF-8',
   }
-  govuk::app::envvar { 'LANG':
-    value => 'en_GB.UTF-8',
+  govuk::app::envvar { "${app}-AWS_ACCESS_KEY_ID":
+    varname => 'AWS_ACCESS_KEY_ID',
+    value   => $aws_access_key_id,
   }
-  govuk::app::envvar { 'AWS_ACCESS_KEY_ID':
-    value => $aws_access_key_id,
-  }
-  govuk::app::envvar { 'AWS_SECRET_KEY':
-    value => $aws_secret_key,
+  govuk::app::envvar { "${app}-AWS_SECRET_KEY":
+    varname => 'AWS_SECRET_KEY',
+    value   => $aws_secret_key,
   }
 }
