2529b4176b338a4dc2abdb871b1415916a9f62ee
Philip Potter <philipgpotter@gmail.com>
2012-11-22 15:16:15 +0000

    stop rogue apache2 processes on monitoring

--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -1,7 +1,14 @@
 class monitoring {
 
+  # because this is a monitoring server, we install
+  # nagios3-cgi which has apache2 as a Recommends:
+  # so it may get unintentionally installed. This
+  # gets rid of it in that eventuality.
   package { 'apache2':
     ensure => 'purged',
+  }
+  service { 'apache2':
+    ensure => 'stopped',
   }
 
   package { 'python-rrdtool':
