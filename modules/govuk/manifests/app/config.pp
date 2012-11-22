ee9af314d402719d5154961e96aad6ca1995856b
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-11-22 17:17:33 +0000

    Use govuk_setenv to set an app's environment
    The govuk_spinup script used to set some environment variables which may
    be needed by other application scripts such as cron jobs or batch
    processes. This change gives a standard way to invoke such batch scripts
    with the correct environment.
    
    The following variables are set globally on a machine, on the basis that
    they are the same for all applications:
    
      GOVUK_ASSET_HOST
      GOVUK_ENV
      RAILS_ENV
      RACK_ENV
    
    These variables are set for each app:
    
      GOVUK_USER
      GOVUK_GROUP
      GOVUK_APP_TYPE
      PORT
      GOVUK_APP_NAME
      GOVUK_APP_ROOT
      GOVUK_APP_RUN
      GOVUK_APP_LOGROOT
      GOVUK_STATSD_PREFIX
    
    I've removed all environment setting from govuk_spinup and into
    govuk_setenv.
    
    Note, when deploying this it will trigger a restart of every application
    so some care will need to be exercised to avoid downtime.

--- modules/govuk/manifests/app/config.pp
+++ modules/govuk/manifests/app/config.pp
@@ -30,13 +30,46 @@ define govuk::app::config (
   # Ensure config dir exists
   file { "/etc/govuk/${title}":
     ensure  => 'directory',
-    require => File['/etc/govuk'],
   }
 
   # Ensure env dir exists
   file { "/etc/govuk/${title}/env.d":
     ensure  => 'directory',
-    require => File["/etc/govuk/${title}"],
+  }
+
+  # This sets the default app for this resource type in the current scope
+  Govuk::App::Envvar {
+    app => $title
+  }
+
+  govuk::app::envvar {
+    "${title}-GOVUK_USER":
+      varname => "GOVUK_USER",
+      value   => "deploy";
+    "${title}-GOVUK_GROUP":
+      varname => "GOVUK_GROUP",
+      value   => "deploy";
+    "${title}-GOVUK_APP_TYPE":
+      varname => "GOVUK_APP_TYPE",
+      value   => $app_type;
+    "${title}-PORT":
+      varname => "PORT",
+      value   => $port;
+    "${title}-GOVUK_APP_NAME":
+      varname => "GOVUK_APP_NAME",
+      value   => $title;
+    "${title}-GOVUK_APP_ROOT":
+      varname => "GOVUK_APP_ROOT",
+      value   => "/var/apps/${title}";
+    "${title}-GOVUK_APP_RUN":
+      varname => "GOVUK_APP_RUN",
+      value   => "/var/run/${title}";
+    "${title}-GOVUK_APP_LOGROOT":
+      varname => "GOVUK_APP_LOGROOT",
+      value   => "/var/log/${title}";
+    "${title}-GOVUK_STATSD_PREFIX":
+      varname => "GOVUK_STATSD_PREFIX",
+      value   => "govuk.app.${title}.${::hostname}";
   }
 
   # Install service
