938487b35248a292d114a62aca9f8f5421000ab0
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-11-26 13:37:28 +0000

    Install dummy logstash-client.conf to avoid puppet errors

--- modules/monitoring/manifests/client.pp
+++ modules/monitoring/manifests/client.pp
@@ -7,6 +7,9 @@ class monitoring::client {
 
   # Disable logstash for now
   #include logstash::client
+  file { '/etc/init/logstash-client.conf':
+    content => 'exec echo',
+  }
   service { 'logstash-client':
     ensure => stopped,
   }
