77494e9411481556eb036a368290a65078272101
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-11-26 11:04:27 +0000

    Disable logstash::client

--- modules/monitoring/manifests/client.pp
+++ modules/monitoring/manifests/client.pp
@@ -2,7 +2,15 @@ class monitoring::client {
   include nagios::client
   include ganglia::client
   include graphite::client
-  include logstash::client
   include logster
   include auditd
+
+  # Disable logstash for now
+  #include logstash::client
+  service { 'logstash-client':
+    ensure => stopped,
+  }
+  @@nagios::check { "check_logstash_client_running_${::hostname}":
+    ensure => absent,
+  }
 }
