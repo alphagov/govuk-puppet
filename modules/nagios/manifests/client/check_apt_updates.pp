d6a7ce2648dfd22c4766dc82cb7010ff7afd57f7
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-11-28 11:23:06 +0000

    Change the threshold for package update warnings to 200

--- modules/nagios/manifests/client/check_apt_updates.pp
+++ modules/nagios/manifests/client/check_apt_updates.pp
@@ -3,7 +3,7 @@ class nagios::client::check_apt_updates {
     source  => 'puppet:///modules/nagios/usr/lib/nagios/plugins/check_apt_updates',
   }
   @@nagios::check { "check_apt_updates_${::hostname}":
-    check_command       => 'check_nrpe!check_apt_updates!100 100',
+    check_command       => 'check_nrpe!check_apt_updates!200 1000',
     service_description => "outstanding package updates",
     host_name           => $::fqdn,
   }
