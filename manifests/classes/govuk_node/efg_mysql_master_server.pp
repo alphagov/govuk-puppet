3e80a4d8aa19c0c3a44c7e5f11f8ac94facda71a
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-11-29 13:36:25 +0000

    Add the mysql::backup class to the master servers. Machines are being backed up but mysql::backup was no longer in the manifest

--- manifests/classes/govuk_node/efg_mysql_master_server.pp
+++ manifests/classes/govuk_node/efg_mysql_master_server.pp
@@ -2,6 +2,7 @@ class govuk_node::efg_mysql_master_server inherits govuk_node::base {
   $root_password = extlookup('mysql_root', '')
   $replica_password = extlookup('mysql_replica_password', '')
   $master_server_id = '1'
+  include mysql::backup
 
   class { 'mysql::server':
     root_password => $root_password,
