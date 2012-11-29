448daa62f9bd121842664713f582fe952785cfe5
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-11-29 19:30:29 +0000

    Remove old logster cronjob. Don't include jetty.

--- manifests/classes/govuk_node/cache_server.pp
+++ manifests/classes/govuk_node/cache_server.pp
@@ -1,10 +1,8 @@
 class govuk_node::cache_server inherits govuk_node::base {
 
   include govuk::htpasswd
-  include jetty
   include mirror
   include nginx
-  include router
   include router::nginx
 
   class { 'varnish':
@@ -26,5 +24,10 @@ class govuk_node::cache_server inherits govuk_node::base {
         }
     }
     default: {}
+  }
+
+  # FIXME: Remove soon
+  cron { "logster-cronjob-jetty":
+    ensure => 'absent',
   }
 }
