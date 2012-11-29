d3d131bf4eddac00219c6cf4d264d87083af8193
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-11-29 11:20:00 +0000

    Change the name of the resources for backup::directory

--- manifests/classes/govuk_node/backup.pp
+++ manifests/classes/govuk_node/backup.pp
@@ -2,25 +2,25 @@ class govuk_node::backup {
     include govuk_node::base
     include backup::server
 
-    backup::directory {'backup_mongodb_backups_mongo.backend.production':
+    backup::directory {'backup_mongodb_backups_mongo':
         directory => '/var/lib/automongodbbackup/',
         host_name => 'mongo-1',
         fq_dn     => 'mongo-1.backend.production',
     }
 
-    backup::directory {'backup_mongodb_backups_licensify-mongo.backend.production':
+    backup::directory {'backup_mongodb_backups_licensify_mongo':
         directory => '/var/lib/automongodbbackup/',
         host_name => 'licensify-mongo-1',
         fq_dn     => 'licensify-mongo-1.licensify.production',
     }
 
-    backup::directory {'backup_mysql_backups_mysql.backend.production':
+    backup::directory {'backup_mysql_backups_mysql':
         directory => '/var/lib/automysqlbackup/',
         host_name => 'mysql-slave-1',
         fq_dn     => 'mysql-slave-1.backend.production',
     }
 
-    backup::directory {'backup_mysql_backups_mysql.efg.production':
+    backup::directory {'backup_mysql_backups_efg_mysql':
         directory => '/var/lib/automysqlbackup/',
         host_name => 'efg-mysql-slave-1',
         fq_dn     => 'efg-mysql-slave-1.efg.production',
