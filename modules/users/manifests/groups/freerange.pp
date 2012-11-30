5e4733b86afbe012c7923899db924c0d4422d9b2
Bradley Wright <brad@intranation.com>
2012-11-30 21:22:02 +0000

    Farewell GoFreerange, we knew you well

--- modules/users/manifests/groups/freerange.pp
+++ modules/users/manifests/groups/freerange.pp
@@ -3,18 +3,22 @@ class users::groups::freerange {
   govuk::user { 'jasoncale': ensure => absent }
 
   govuk::user { 'tomw':
+    ensure     => absent,
     fullname   => 'Tom Ward',
     email      => 'tom.ward@gofreerange.co.uk',
   }
   govuk::user { 'chrisroos':
+    ensure     => absent,
     fullname   => 'Chris Roos',
     email      => 'chris.roos@gofreerange.com';
   }
   govuk::user { 'jamesmead':
+    ensure     => absent,
     fullname   => 'James Mead',
     email      => 'james@floehopper.org';
   }
   govuk::user { 'lazyatom':
+    ensure     => absent,
     fullname   => 'James Adam',
     email      => 'james.adam@gofreerange.com';
   }
