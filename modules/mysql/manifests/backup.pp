812fc496afb14b47186b816d4dfcb6d77d6cc09a
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-12-03 19:44:15 +0000

    Update modules/mysql/manifests/backup.pp
    Change the backup user to match the password

--- modules/mysql/manifests/backup.pp
+++ modules/mysql/manifests/backup.pp
@@ -1,6 +1,6 @@
 class mysql::backup {
 
-  $mysql_backup_username = 'backup'
+  $mysql_backup_username = 'root'
   $mysql_backup_password = extlookup('mysql_root','')
   $mysql_backup_host = 'localhost'
   $mysql_backup_email = 'zd-alrt-normal@digital.cabinet-office.gov.uk'
