912aab630f38e4556ee64f98c72e769407836219
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-12-04 14:12:52 +0000

    This test does not work unless you run it against 127.0.0.1

--- modules/mysql/manifests/user.pp
+++ modules/mysql/manifests/user.pp
@@ -27,13 +27,13 @@ define mysql::user ($root_password, $user_password, $username=$title, $remote_ho
   }
 
   exec { "create_mysql_user_${title}":
-    unless  => "/usr/bin/mysql -u${username} ${userpassarg}",
+    unless  => "/usr/bin/mysql -h 127.0.0.1 -u${username} ${userpassarg}",
     command => "/usr/bin/mysql -uroot ${rootpassarg} -e 'FLUSH PRIVILEGES; CREATE USER \"${username}\"@\"${remote_host}\" IDENTIFIED BY \"$user_password\"; FLUSH PRIVILEGES;'",
     require => Class['mysql::server'],
   }
 
   exec { "create_mysql_database_${db}_for_${title}":
-    unless  => "/usr/bin/mysql -u${username} ${userpassarg} ${dbarg}",
+    unless  => "/usr/bin/mysql -h 127.0.0.1 -u${username} ${userpassarg} ${dbarg}",
     command => "/usr/bin/mysql -uroot ${rootpassarg} -e 'GRANT ${privileges} PRIVILEGES ON ${db}.* TO \"${username}\"@\"${remote_host}\" WITH GRANT OPTION; FLUSH PRIVILEGES;'",
     require => [Class['mysql::server'], Exec["create_mysql_user_${title}"]],
   }
