0592447cfbdc73c0da45fb315b6ab55a3ce90b91
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-12-10 17:21:50 +0000

    Add intercept_errors option to nginx::config::vhost::proxy

---
+++ modules/nginx/spec/defines/nginx_config_vhost_proxy_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../../../../spec_helper'
+
+describe 'nginx::config::vhost::proxy', :type => :define do
+  let(:title) { 'rabbit' }
+  let(:facts) do
+    { :govuk_platform => 'production' }
+  end
+
+  context 'with a list of upstreams' do
+    let(:params) do
+      { :to => ['a.internal', 'b.internal', 'c.internal'] }
+    end
+
+    it 'should install a proxy vhost' do
+      should contain_nginx__config__site('rabbit')
+        .with_content(/server a\.internal;/)
+        .with_content(/server b\.internal;/)
+        .with_content(/server c\.internal;/)
+    end
+  end
+
+  context 'with intercept_errors true' do
+    let(:params) do
+      {
+        :to => ['a.internal'],
+        :intercept_errors => true,
+      }
+    end
+
+    it 'should install simple_error_pages' do
+      should contain_nginx__config__site('rabbit')
+        .with_content(/include \/etc\/nginx\/simple_error_pages\.conf;/)
+    end
+  end
+
+end
