dcb1a0cd3b666b205dd3594b0357792410076b39
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-12-10 15:40:16 +0000

    travel-advice-publisher should be SSL only

--- modules/govuk/manifests/apps/travel_advice_publisher.pp
+++ modules/govuk/manifests/apps/travel_advice_publisher.pp
@@ -2,6 +2,7 @@ class govuk::apps::travel_advice_publisher( $port = 3035 ) {
   govuk::app { 'travel-advice-publisher':
     app_type          => 'rack',
     port              => $port,
+    vhost_ssl_only    => true,
     health_check_path => '/',
   }
 }
