60ca6f57860c40cdda97c7c2c3bef5402b34fc2b
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-12-12 11:26:53 +0000

    Add some offsite-backups for Production

---
+++ modules/backup/manifests/offsite.pp
@@ -0,0 +1,19 @@
+class backup::offsite {
+
+  cron { 'offsite-backup':
+    command => '/usr/local/bin/offsite-backup',
+    user    => 'govuk-backup',
+    hour    => 8,
+    minute  => 13,
+    require => File['/usr/local/bin/offsite-backup'],
+  }
+
+  file { '/usr/local/bin/offsite-backup':
+    ensure => present,
+    source => 'puppet:///modules/backup/usr/local/bin/offsite-backup',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+}
