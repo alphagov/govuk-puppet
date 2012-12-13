892225ab0252932b59a866e8d932c51e7c9a2220
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-12-13 10:47:10 +0000

    Change this so it passes the nagios check name uniqueness check

--- modules/backup/manifests/offsite/monitoring.pp
+++ modules/backup/manifests/offsite/monitoring.pp
@@ -1,33 +1,33 @@
 class backup::offsite::monitoring {
 
-  $offsite_host = 'offsite-backup.production.alphagov.co.uk'
-  $offsite_short_name = 'offsite-backup'
+  $offsite_fqdn = 'offsite-backup.production.alphagov.co.uk'
+  $offsite_hostname = 'offsite-backup'
 
-  nagios::host { $offsite_host:
-    hostalias => $offsite_host,
-    address   => $offsite_host,
+  nagios::host { $offsite_fqdn:
+    hostalias => $offsite_fqdn,
+    address   => $offsite_fqdn,
   }
 
-  nagios::check { "check_ping_${offsite_short_name}":
+  nagios::check { "check_ping_${offsite_hostname}":
     check_command       => 'check_ping!100.0,20%!500.0,60%',
     notification_period => '24x7',
     use                 => 'govuk_high_priority',
     service_description => 'unable to ping',
-    host_name           => $offsite_host,
+    host_name           => $offsite_fqdn,
   }
 
-  nagios::check { "check_disk_${offsite_short_name}":
+  nagios::check { "check_disk_${offsite_hostname}":
     check_command       => 'check_nrpe_1arg!check_xvda1',
     service_description => 'high disk usage',
     use                 => 'govuk_high_priority',
-    host_name           => $offsite_host,
+    host_name           => $offsite_fqdn,
   }
 
-  nagios::check { "check_ssh_${offsite_short_name}":
+  nagios::check { "check_ssh_${offsite_hostname}":
     check_command       => 'check_ssh',
     use                 => 'govuk_high_priority',
     service_description => 'unable to ssh',
-    host_name           => $offsite_host,
+    host_name           => $offsite_fqdn,
   }
 
 }
