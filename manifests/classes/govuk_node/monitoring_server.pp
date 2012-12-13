8f8ac69b7163fd6f8726e55ad4d702755f5d76af
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-12-13 10:30:49 +0000

    Add some basic checks for our offsite-backup server

--- manifests/classes/govuk_node/monitoring_server.pp
+++ manifests/classes/govuk_node/monitoring_server.pp
@@ -1,5 +1,12 @@
 class govuk_node::monitoring_server inherits govuk_node::base {
 
   include monitoring
+  
+  $offsite_backup = extlookup('offsite-backups', 'off')
+
+  case $offsite_backup {
+    "on":    { include backup::offsite::monitoring }
+    default: {}
+  }
 
 }
