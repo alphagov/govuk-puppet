e30eb723678b94d611513d8310d633a766e267a3
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-12-13 21:27:55 +0000

    Remove the ping check for the offsite-backup machine We cannot ping out from Skyscape, so it is not possible to do a ping check for this machine we will have to live with only having a check that SSH is accessible and that we can connect to check the disk space. I think that will be enough for now.

--- modules/backup/manifests/offsite/monitoring.pp
+++ modules/backup/manifests/offsite/monitoring.pp
@@ -8,14 +8,6 @@ class backup::offsite::monitoring {
     address   => $offsite_fqdn,
   }
 
-  nagios::check { "check_ping_${offsite_hostname}":
-    check_command       => 'check_ping!100.0,20%!500.0,60%',
-    notification_period => '24x7',
-    use                 => 'govuk_high_priority',
-    service_description => 'unable to ping',
-    host_name           => $offsite_fqdn,
-  }
-
   nagios::check { "check_disk_${offsite_hostname}":
     check_command       => 'check_nrpe_1arg!check_xvda1',
     service_description => 'high disk usage',
