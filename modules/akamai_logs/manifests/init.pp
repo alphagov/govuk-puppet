1b0464e7e1b64ebe09c35efdafd7634661d60e0d
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-12-14 13:07:42 +0000

    Add missing colons

--- modules/akamai_logs/manifests/init.pp
+++ modules/akamai_logs/manifests/init.pp
@@ -58,15 +58,15 @@ class akamai_logs {
     matches => "/var/log/akamai/*.log"
   }
 
-  @nagios::plugin { "check_akamai_logs"
+  @nagios::plugin { "check_akamai_logs":
     content => template("akamai_logs/check_akamai_logs.sh.erb")
   }
 
-  @nagios::nrpe_config { "check_akamai_logs"
+  @nagios::nrpe_config { "check_akamai_logs":
     content => "command[check_akamai_logs]=/usr/lib/nagios/plugins/check_akamai_logs"
   }
 
-  @@nagios::check { "check_akamai_logs_${::hostname}"
+  @@nagios::check { "check_akamai_logs_${::hostname}":
     check_command       => 'check_nrpe_1arg!check_akamai_logs',
     service_description => "no recently fetched akamai logs",
     host_name           => $::fqdn
