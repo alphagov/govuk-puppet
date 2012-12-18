bfbae7e9ddd424a0edeb3f52bc50b7949f2c97c8
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-12-18 14:16:04 +0000

    Remove duplicate instantiation of release app

--- manifests/classes/govuk_node/backend_server.pp
+++ manifests/classes/govuk_node/backend_server.pp
@@ -37,7 +37,6 @@ class govuk_node::backend_server inherits govuk_node::base {
     default: {
       include govuk::apps::review_o_matic_explore
       include govuk::apps::travel_advice_publisher # Only add to EC2 (i.e. preview) for now
-      include govuk::apps::release
     }
   }
 
