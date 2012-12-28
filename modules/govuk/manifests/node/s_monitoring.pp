7c4a72e6d21f6fd9742323ed0dfefd5b44f591b3
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-12-28 22:55:09 +0000

    Enable campfire notification for Production

--- modules/govuk/manifests/node/s_monitoring.pp
+++ modules/govuk/manifests/node/s_monitoring.pp
@@ -9,4 +9,16 @@ class govuk::node::s_monitoring inherits govuk::node::s_base {
     default: {}
   }
 
+  $campfire = extlookup('campfire','off')
+  case $campfire {
+    "on":     {
+      nagios::campfire_contact {'campfire_notification':
+        campfire_subdomain => extlookup('campfire_subdomain','unset'),
+        campfire_token     => extlookup('campfire_token','unset'),
+        campfire_room      => extlookup('campfire_room','unset'),
+      }
+    }
+    default : {}
+  }
+
 }
