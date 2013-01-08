4200844741831008e203770d2782cc0dbed2521e
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-01-08 14:41:29 +0000

    Explicitly manage the puppet host on Places boxes

--- modules/hosts/manifests/places_preview.pp
+++ modules/hosts/manifests/places_preview.pp
@@ -1,4 +1,5 @@
 class hosts::places_preview {
+  host { 'puppet':             ip => '10.33.163.224' }
   host { 'places-mongo-1':     ip => '10.239.81.159' }
   host { 'places-mongo-2':     ip => '10.234.37.141' }
   host { 'places-mongo-3':     ip => '10.234.78.68' }
