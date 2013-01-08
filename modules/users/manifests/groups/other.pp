70c7e36e2de09b504870b7df45d442989f5d8158
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-01-08 11:15:10 +0000

    Clean up user groups, move ssh keys into Puppet
    @samjsharpe and I couldn't see any particularly good reason for SSH public
    keys to be in extdata, not least now that the deployment repository is locked
    down and most developers won't be able to add their own SSH keys to extdata.
    
    This commit modifies govuk::user to accept SSH key data directly rather than
    via extdata, and ports all existing keys into the `users` module.
    
    Other small changes include:
    
    - Removing the freerange group, as none of them work here any more
    - Cleanups to the govuk::user code, and some documentation for parameters

--- modules/users/manifests/groups/other.pp
+++ modules/users/manifests/groups/other.pp
@@ -1,18 +1,21 @@
 class users::groups::other {
 
   govuk::user { 'tomstuart':
-    fullname   => 'Tom Stuart',
-    email      => 'tom@experthuman.com';
+    fullname => 'Tom Stuart',
+    email    => 'tom@experthuman.com',
+    ssh_key  => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA86O8grQpsp6YHl3LJsy8w7NmoCsVDFDVjnX2gsKQArd5USj79q44Jknj00uxjbTGEUn2BEfXMiCNtipYWXCyaCiJuszL8jwSKGNOXH6LXfO8DpTD9Ie9at2jLbZwsThCqMAhQKH8ChZT8VdLHAgUilQjaRu0Ds7svbH9fQppDRB+lxbTCtffwhNLTYWZJgoqGxtUWfueDKQvSgWLo9xXddDiOwdlBMvcMIwvX0WAq3epXbNFzhahnR5cDyaQoFZhoD+AEvWC4ONnOLaY0ntR17IQmRbnjq/nBbAPnb9YCEYWp3DwbjIARx/lHDZ/Uy5OToBjGsEmLvYw2Y9n43Urkw==',
   }
 
   govuk::user { 'chrismdp':
     fullname => 'Chris Parsons',
-    email    => 'chris@thinkcodelearn.com';
+    email    => 'chris@thinkcodelearn.com',
+    ssh_key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDJm73q5IG8F6389f7D4478pU+iWMOvDmuxYCWP5ueKkPfck87+HZl1exxrhEHFLTFC/2S4cOJL4rCzqw2YQ88RsVjEjrLtgX+HPaWZ9YkfUTQzgab/lxkhSvrOd+UyTS6veJDYPGUJnPs27pR9zAjmMBHqldDennYDkfRE/d+KlcLhoglHpjhVLaNEUwS98ruyrO/pnS6xa9ECEdfXuewbeKAX2ZF/rCDd+aX9+8QZj4RcSwRdAMePg10W71qf2yGodfDppf3Xj64a4LJfeav9Ns8AoJpGnpIUfDaK0bXsPqAKLzw6pKnKzlZEUj75nw2vhC0tRaPT7s9wHLw1F3+l',
   }
 
   govuk::user { 'murraysteele':
     fullname => 'Murray Steele',
-    email    => 'murray.steele@digital.cabinet-office.gov.uk';
+    email    => 'murray.steele@digital.cabinet-office.gov.uk',
+    ssh_key  => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA0t9lhZB9GEa+Vt58PYYtpIIusxRN9Pthtv2PyModLhw9kKKT65FL+uUaCno/st4qORVlpUcs9hO9NhmMD/+poAczLWdagH2vvqDMxGpKXuUL+L3kVrncYH5iSBVLK+vVZ1H8/eAjaYuaPZBacMXgKCx+vTo6yB5SI8PbeSHeHTSKeOSYbPWjFP9gmqVTK4pB66dSDikO84j2t20T+nW6DP7Bnv1zPTli2r2qSTBFj+zxDh1biwgLvY5KRxKpvS2/zykRJJHzJTlxuIx90pyRY6BVXb6Qe9ZSOR/KZApwbrYhgzsn085oaYMGVUV9UWzzKFmNYkiVvh3rQsPZXXvEWQ==',
   }
 
 }
