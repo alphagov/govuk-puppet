38b47e9859968b30cbb1f436d3681c8a35a0f9c7
Philip Potter <philipgpotter@gmail.com>
2013-01-08 16:01:53 +0000

    connect management server to our VPN
    This commit creates a vpnc module to manage vpn client connections back to
    aviation house. This is needed by our management (jenkins) servers to access
    the new github enterprise repos.

--- modules/govuk/manifests/node/s_management.pp
+++ modules/govuk/manifests/node/s_management.pp
@@ -1,4 +1,5 @@
 class govuk::node::s_management inherits govuk::node::s_base {
   include govuk::node::s_management_base
   include jenkins::master
+  include govuk::vpnc
 }
---
+++ modules/vpnc/manifests/init.pp
@@ -0,0 +1,27 @@
+class vpnc (
+  $gateway,
+  $group,
+  $group_pw,
+  $user,
+  $password
+) {
+  package {'vpnc':
+    ensure => present,
+  }
+
+  file {"/etc/vpnc/network.conf":
+    mode    => '0600',
+    content => template("vpnc/network.conf.erb"),
+    notify  => Service['vpnc'],
+  }
+
+  file {'/etc/init/vpnc.conf':
+    source  => 'puppet:///vpnc/etc/init/vpnc.conf',
+    notify  => Service['vpnc'],
+  }
+
+  service {'vpnc':
+    ensure  => running,
+    require => Package['vpnc'],
+  }
+}
