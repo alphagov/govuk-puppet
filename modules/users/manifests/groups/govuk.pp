e2a717e45d87e8a77dd43a9809c9aa73b41d0d40
Aaron <aaron.keogh@digital.cabinet-office.gov.uk>
2013-01-09 09:48:58 +0000

    Adding my ssh-key to govuk.pp
    Now Nick has tidied up the user groups my account details are the only
    ones without ssh-key details so I'm adding them.

--- modules/users/manifests/groups/govuk.pp
+++ modules/users/manifests/groups/govuk.pp
@@ -14,6 +14,7 @@ class users::groups::govuk {
   govuk::user { 'aaronkeogh':
     fullname => 'Aaron Keogh',
     email    => 'aaron.keogh@digital.cabinet-office.gov.uk',
+    ssh_key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC7HA9e91WlSQtjKdRqIsE3SFjG4nbBE5W1LwdmUfMbsm8TFli1foREvx7tHtClSC/KQtec2BrtmCKxwApN/LZAEPR7gRyOe3c3FWA55ZygPVvCcJyzePS/e0DVN5NqTuQO4IYFfqOHJmxyJTZ+Tgub8x9YYlsRPud9mvckb4sRe1ydTSJuDDqBcfDKcPyw0xNCbP50QbhVpdr7H6XqbShyIQogN5O9NTvHySBpSbgqEcog7zOJz/zWCc5heXcAo+EYbas8HIaQhlgmmWprbIxJ8LGB5povYnP3WMWs0GN2mw/axDLRxcjN1czDZdCeqAawuFovqtE1foGLjr3mHz3n',
   }
   govuk::user { 'alex_tea':
     fullname => 'Alex Torrance',
