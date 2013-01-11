e89071b459d60e095b24bd4a4f774c2cc50d6088
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-01-11 16:55:20 +0000

    Add a note about when we can remove MySQL from the Mongo servers

--- modules/govuk/manifests/node/s_mongo.pp
+++ modules/govuk/manifests/node/s_mongo.pp
@@ -1,5 +1,7 @@
 class govuk::node::s_mongo inherits govuk::node::s_base {
   include mongodb::server
+  #TODO: When Preview is no longer in EC2, the Mongo Servers 
+  #should not include the s_mysql_master class 
   include govuk::node::s_mysql_master
 
   case $::govuk_provider {
