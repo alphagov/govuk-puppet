0f0a76d6e2e3534dd007c8489f47f797b4863e14
James Abley <james.abley@gmail.com>
2013-01-11 14:56:44 +0000

    Accept license terms for Oracle Java 7
    oracle-java7-installer presents a license, so we need the
    oracle-java7-installer.preseed to define that we accept the license.

--- modules/java/manifests/oracle7/jdk.pp
+++ modules/java/manifests/oracle7/jdk.pp
@@ -2,10 +2,18 @@ class java::oracle7::jdk ( $ensure = present ) {
 
   if $ensure != absent {
     include govuk::ppa
+
+    File['/var/local/oracle-java7-installer.preseed'] -> Package['oracle-java7-installer']
+  }
+
+  file { '/var/local/oracle-java7-installer.preseed':
+    ensure => $ensure,
+    source => "puppet:///modules/${module_name}/oracle-java7-installer.preseed",
   }
 
   package { 'oracle-java7-installer':
-    ensure => $ensure,
+    ensure       => $ensure,
+    responsefile => '/var/local/oracle-java7-installer.preseed',
   }
 
 }
