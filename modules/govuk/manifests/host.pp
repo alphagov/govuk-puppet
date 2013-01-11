53ac03e66f7c62df19e8dbba16764926a5fa689c
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-01-11 12:31:45 +0000

    Add govuk::host type, to simplify host definition
    The govuk::host type is a simple wrapper around Puppet's native `host` type,
    providing a single place to encode GOV.UK-specific conventions. In particular,
    the construction of a hostname as shortname.vdcname.orgname is now managed
    internally by govuk::host.
    
    For each host, an alias of shortname.vdcname (what I'm currently calling an
    Internally-Qualified Domain Name) is added by default, and other aliases can
    be defined using additional parameters to the type.

---
+++ modules/govuk/manifests/host.pp
@@ -0,0 +1,46 @@
+# == Type: govuk::host
+#
+# Make defining a host that abides to GOV.UK convention easier
+#
+# === Parameters
+#
+# [*ensure*]
+#   The desired state of the host entry, "present" or "absent".
+#
+# [*hostname*]
+#   The host's short name, e.g. "jumpbox-1"
+#
+# [*vdc*]
+#   The host's destination virtual data centre, e.g. "management"
+#
+# [*ip*]
+#   The host's IP address
+#
+# [*legacy_aliases*]
+#   An array of legacy host aliases.
+#
+# [*service_aliases*]
+#   An array of service names aliased to this host.
+#
+define govuk::host(
+  $vdc,
+  $ip,
+  $hostname = $title,
+  $ensure = present,
+  $legacy_aliases = [],
+  $service_aliases = [],
+
+  # The following parameters should typically not be customized
+  $service_suffix = 'cluster',
+  $platform = $::govuk_platform,
+) {
+
+  $service_aliases_real = regsubst($service_aliases, '$', ".${service_suffix}")
+
+  host { "${hostname}.${vdc}.${platform}":
+    ensure       => $ensure,
+    ip           => $ip,
+    host_aliases => ["${hostname}.${vdc}", $service_aliases_real, $legacy_aliases],
+  }
+
+}
---
+++ modules/govuk/spec/defines/govuk_host_spec.rb
@@ -0,0 +1,46 @@
+require_relative '../../../../spec_helper'
+
+describe 'govuk::host', :type => :define do
+  let(:title) { 'giraffe' }
+  let(:params) { { :ip => '1.2.3.4', :vdc => 'foobar' } }
+  let(:facts) { { :govuk_platform => 'test' } }
+
+  it do
+    should contain_host('giraffe.foobar.test')
+      .with_ip('1.2.3.4')
+      .with_host_aliases(['giraffe.foobar', [], []])
+  end
+
+  context 'with legacy aliases' do
+    let(:params) do
+      {
+        :ip => '1.2.3.4',
+        :vdc => 'foobar',
+        :legacy_aliases => ['foo', 'bar', 'baz'],
+      }
+    end
+
+    it do
+      should contain_host('giraffe.foobar.test')
+        .with_ip('1.2.3.4')
+        .with_host_aliases(['giraffe.foobar', [], ['foo', 'bar', 'baz']])
+    end
+  end
+
+  context 'with service aliases' do
+    let(:params) do
+      {
+        :ip => '1.2.3.4',
+        :vdc => 'foobar',
+        :service_aliases => ['foo', 'bar', 'baz'],
+        :service_suffix => 'donkeys',
+      }
+    end
+
+    it do
+      should contain_host('giraffe.foobar.test')
+        .with_ip('1.2.3.4')
+        .with_host_aliases(['giraffe.foobar', ['foo.donkeys', 'bar.donkeys', 'baz.donkeys'], []])
+    end
+  end
+end
