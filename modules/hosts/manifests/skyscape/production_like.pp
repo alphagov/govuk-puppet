60a3dcbb061bf36bcb7a664e76749ea18e619ce1
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-01-11 12:35:18 +0000

    Clean up hosts::production_like with govuk::host
    This uses the govuk::host type added in 53ac03e to clean up the definition of
    hosts in hosts::skyscape::production_like.
    
    The only side-effect of this change *should* be the additional creation of
    shortname.vdcname aliases for all the hosts. I haven't made any attempt to
    clear up the mess of legacy names ("mysql.master", "backend-1.mongo", etc.)

--- modules/hosts/manifests/skyscape/production_like.pp
+++ modules/hosts/manifests/skyscape/production_like.pp
@@ -5,89 +5,110 @@ class hosts::skyscape::production_like ($platform = $::govuk_platform) {
   $app_domain = extlookup('app_domain')
 
   #management vdc machines
-  host { "puppet-1.management.${platform}":
-    ip           => '10.0.0.2',
-    host_aliases => ['puppet-1', 'puppet'],
+  govuk::host { 'puppet-1':
+    ip             => '10.0.0.2',
+    vdc            => 'management',
+    legacy_aliases => ['puppet-1', 'puppet'],
   }
-  host { "jenkins-1.management.${platform}":
-    ip => '10.0.0.3',
+  govuk::host { 'jenkins-1':
+    ip  => '10.0.0.3',
+    vdc => 'management',
   }
-  host { "monitoring.management.${platform}":
-    ip           => '10.0.0.20',
-    host_aliases => ['monitoring', 'monitoring.cluster', "nagios.${app_domain}", "ganglia.${app_domain}", "graphite.${app_domain}"],
+  govuk::host { 'monitoring':
+    ip              => '10.0.0.20',
+    vdc             => 'management',
+    legacy_aliases  => ['monitoring', "nagios.${app_domain}", "ganglia.${app_domain}", "graphite.${app_domain}"],
+    service_aliases => ['monitoring'],
   }
-  host { "logging.management.${platform}":
-    ip           => '10.0.0.21',
-    host_aliases => ['logging', 'graylog.cluster'],
+  govuk::host { 'logging':
+    ip              => '10.0.0.21',
+    vdc             => 'management',
+    legacy_aliases  => ['logging'],
+    service_aliases => ['graylog'],
   }
-  host { "jumpbox-1.management.${platform}":
-    ip => '10.0.0.100',
+  govuk::host { 'jumpbox-1':
+    ip  => '10.0.0.100',
+    vdc => 'management',
   }
-  host { "jumpbox-2.management.${platform}":
-    ip => '10.0.0.200',
+  govuk::host { 'jumpbox-2':
+    ip  => '10.0.0.200',
+    vdc => 'management',
   }
-  host { "backup-1.management.${platform}":
-    ip => '10.0.0.50',
+  govuk::host { 'backup-1':
+    ip  => '10.0.0.50',
+    vdc => 'management',
   }
-  host { 'vcd00003.vpn.skyscapecs.net':
-    ip => '10.202.5.11',
-  }
-  host { "exception-handler-1.management.${platform}":
-    ip           => '10.0.0.4',
-    host_aliases => ['exception-handler-1', 'exception-handler', "errbit.${app_domain}"]
+  govuk::host { 'exception-handler-1':
+    ip             => '10.0.0.4',
+    vdc            => 'management',
+    legacy_aliases => ['exception-handler-1', 'exception-handler', "errbit.${app_domain}"]
   }
 
   #router vdc machines
-  host { "cache-1.router.${platform}":
-    ip => '10.1.0.2',
+  govuk::host { 'cache-1':
+    ip  => '10.1.0.2',
+    vdc => 'router',
   }
-  host { "cache-2.router.${platform}":
-    ip => '10.1.0.3',
+  govuk::host { 'cache-2':
+    ip  => '10.1.0.3',
+    vdc => 'router',
   }
-  host { "cache-3.router.${platform}":
-    ip => '10.1.0.4',
+  govuk::host { 'cache-3':
+    ip  => '10.1.0.4',
+    vdc => 'router',
   }
+
   #router lb vhosts
-  host { "cache.router.${platform}":
-    ip           => '10.1.1.1',
-    host_aliases => ['cache', 'cache.cluster', 'router.cluster', 'www.gov.uk']
+  govuk::host { 'cache':
+    ip              => '10.1.1.1',
+    vdc             => 'router',
+    legacy_aliases  => ['cache', 'www.gov.uk'],
+    service_aliases => ['cache', 'router'],
   }
 
   #frontend vdc machines
-  host { "frontend-1.frontend.${platform}":
-    ip           => '10.2.0.2',
-    host_aliases => ['frontend-1']
+  govuk::host { 'frontend-1':
+    ip             => '10.2.0.2',
+    vdc            => 'frontend',
+    legacy_aliases => ['frontend-1']
   }
-  host { "frontend-2.frontend.${platform}":
-    ip           => '10.2.0.3',
-    host_aliases => ['frontend-2']
+  govuk::host { 'frontend-2':
+    ip             => '10.2.0.3',
+    vdc            => 'frontend',
+    legacy_aliases => ['frontend-2']
   }
-  host { "frontend-3.frontend.${platform}":
-    ip           => '10.2.0.4',
-    host_aliases => ['frontend-3']
+  govuk::host { 'frontend-3':
+    ip             => '10.2.0.4',
+    vdc            => 'frontend',
+    legacy_aliases => ['frontend-3']
   }
-  host { "whitehall-frontend-1.frontend.${platform}":
-    ip           => '10.2.0.5',
-    host_aliases => ['whitehall-frontend-1']
+  govuk::host { 'whitehall-frontend-1':
+    ip             => '10.2.0.5',
+    vdc            => 'frontend',
+    legacy_aliases => ['whitehall-frontend-1']
   }
-  host { "whitehall-frontend-2.frontend.${platform}":
-    ip           => '10.2.0.6',
-    host_aliases => ['whitehall-frontend-2']
+  govuk::host { 'whitehall-frontend-2':
+    ip             => '10.2.0.6',
+    vdc            => 'frontend',
+    legacy_aliases => ['whitehall-frontend-2']
   }
 
 
   #frontend lb vhosts
-  host { "frontend-lb-2.frontend.${platform}":
-    ip           => '10.2.0.102',
-    host_aliases => ['frontend-lb-2']
+  govuk::host { 'frontend-lb-1':
+    ip             => '10.2.0.101',
+    vdc            => 'frontend',
+    legacy_aliases => ['frontend-lb-1'],
   }
-  host { "frontend-lb-1.frontend.${platform}":
-    ip           => '10.2.0.101',
-    host_aliases => ['frontend-lb-1']
+  govuk::host { 'frontend-lb-2':
+    ip             => '10.2.0.102',
+    vdc            => 'frontend',
+    legacy_aliases => ['frontend-lb-2'],
   }
-  host { "frontend-internal-lb.frontend.${platform}":
-    ip           => '10.2.1.2',
-    host_aliases => [
+  govuk::host { 'frontend-internal-lb':
+    ip             => '10.2.1.2',
+    vdc            => 'frontend',
+    legacy_aliases => [
       'frontend-internal-lb',
       "businesssupportfinder.${app_domain}",
       "calendars.${app_domain}",
@@ -109,65 +130,82 @@ class hosts::skyscape::production_like ($platform = $::govuk_platform) {
   }
 
   #backend vdc machines
-  host { "backend-1.backend.${platform}":
-    ip           => '10.3.0.2',
-    host_aliases => ['backend-1']
+  govuk::host { 'backend-1':
+    ip             => '10.3.0.2',
+    vdc            => 'backend',
+    legacy_aliases => ['backend-1'],
   }
-  host { "backend-2.backend.${platform}":
-    ip           => '10.3.0.3',
-    host_aliases => ['backend-2']
+  govuk::host { 'backend-2':
+    ip             => '10.3.0.3',
+    vdc            => 'backend',
+    legacy_aliases => ['backend-2'],
   }
-  host { "backend-3.backend.${platform}":
-    ip           => '10.3.0.4',
-    host_aliases => ['backend-3']
+  govuk::host { 'backend-3':
+    ip             => '10.3.0.4',
+    vdc            => 'backend',
+    legacy_aliases => ['backend-3'],
   }
-  host { "support-1.backend.${platform}":
-    ip           => '10.3.0.5',
-    host_aliases => ['support-1', 'support.cluster']
+  govuk::host { 'support-1':
+    ip              => '10.3.0.5',
+    vdc             => 'backend',
+    legacy_aliases  => ['support-1'],
+    service_aliases => ['support'],
   }
-  host { "mongo-1.backend.${platform}":
-    ip           => '10.3.0.6',
-    host_aliases => ['mongo-1', "mongo.backend.${platform}", 'mongodb.cluster', 'backend-1.mongo']
+  govuk::host { 'mongo-1':
+    ip              => '10.3.0.6',
+    vdc             => 'backend',
+    legacy_aliases  => ['mongo-1', "mongo.backend.${platform}", 'backend-1.mongo'],
+    service_aliases => ['mongodb'],
   }
-  host { "mongo-2.backend.${platform}":
-    ip           => '10.3.0.7',
-    host_aliases => ['mongo-2', 'backend-2.mongo']
+  govuk::host { 'mongo-2':
+    ip             => '10.3.0.7',
+    vdc            => 'backend',
+    legacy_aliases => ['mongo-2', 'backend-2.mongo'],
   }
-  host { "mongo-3.backend.${platform}":
-    ip           => '10.3.0.8',
-    host_aliases => ['mongo-3', 'backend-3.mongo']
+  govuk::host { 'mongo-3':
+    ip             => '10.3.0.8',
+    vdc            => 'backend',
+    legacy_aliases => ['mongo-3', 'backend-3.mongo'],
   }
-  host { "mapit-server-1.backend.${platform}":
-    ip           => '10.3.0.9',
-    host_aliases => ['mapit-server-1','mapit.alpha.gov.uk']
+  govuk::host { 'mapit-server-1':
+    ip             => '10.3.0.9',
+    vdc            => 'backend',
+    legacy_aliases => ['mapit-server-1', 'mapit.alpha.gov.uk']
   }
-  host { "mapit-server-2.backend.${platform}":
-    ip           => '10.3.0.10',
-    host_aliases => ['mapit-server-2']
+  govuk::host { 'mapit-server-2':
+    ip             => '10.3.0.10',
+    vdc            => 'backend',
+    legacy_aliases => ['mapit-server-2'],
   }
-  host { "redis-1.backend.${platform}":
-    ip           => '10.3.0.50',
-    host_aliases => ['redis-1.backend', 'redis-1']
+  govuk::host { 'redis-1':
+    ip             => '10.3.0.50',
+    vdc            => 'backend',
+    legacy_aliases => ['redis-1'],
   }
-  host { "mysql-master-1.backend.${platform}":
-    ip           => '10.3.10.0',
-    host_aliases => ['mysql-master-1', 'master.mysql', "mysql.backend.${platform}"]
+  govuk::host { 'mysql-master-1':
+    ip             => '10.3.10.0',
+    vdc            => 'backend',
+    legacy_aliases => ['mysql-master-1', 'master.mysql', "mysql.backend.${platform}"],
   }
-  host { "mysql-slave-1.backend.${platform}":
-    ip           => '10.3.10.1',
-    host_aliases => ['mysql-slave-1', 'slave.mysql']
+  govuk::host { 'mysql-slave-1':
+    ip             => '10.3.10.1',
+    vdc            => 'backend',
+    legacy_aliases => ['mysql-slave-1', 'slave.mysql'],
   }
-  host { "backend-lb-1.backend.${platform}":
-    ip           => '10.3.0.101',
-    host_aliases => ['backend-lb-1']
+  govuk::host { 'backend-lb-1':
+    ip             => '10.3.0.101',
+    vdc            => 'backend',
+    legacy_aliases => ['backend-lb-1']
   }
-  host { "backend-lb-2.backend.${platform}":
-    ip           => '10.3.0.102',
-    host_aliases => ['backend-lb-2']
+  govuk::host { 'backend-lb-2':
+    ip             => '10.3.0.102',
+    vdc            => 'backend',
+    legacy_aliases => ['backend-lb-2']
   }
-  host { "backend-internal-lb.backend.${platform}":
-    ip           => '10.3.1.2',
-    host_aliases => [
+  govuk::host { 'backend-internal-lb':
+    ip             => '10.3.1.2',
+    vdc            => 'backend',
+    legacy_aliases => [
       'backend-internal-lb',
       "canary-backend.${app_domain}",
       "contentapi.${app_domain}",
@@ -184,19 +222,22 @@ class hosts::skyscape::production_like ($platform = $::govuk_platform) {
       "tariff-api.${app_domain}",
       "travel-advice-publisher.${app_domain}",
       "whitehall-admin.${app_domain}"
-    ]
+    ],
   }
-  host { "asset-master.backend.${platform}":
-    ip           => '10.3.0.20',
-    host_aliases => ['asset-master', "asset-master.${app_domain}"]
+  govuk::host { 'asset-master':
+    ip             => '10.3.0.20',
+    vdc            => 'backend',
+    legacy_aliases => ['asset-master', "asset-master.${app_domain}"],
   }
-  host { "asset-slave.backend.${platform}":
-    ip           => '10.3.0.21',
-    host_aliases => ['asset-slave', "asset-slave.${app_domain}"]
+  govuk::host { 'asset-slave':
+    ip             => '10.3.0.21',
+    vdc            => 'backend',
+    legacy_aliases => ['asset-slave', "asset-slave.${app_domain}"],
   }
-  host { "datainsight-1.backend.${platform}":
-    ip           => '10.3.0.30',
-    host_aliases => [
+  govuk::host { 'datainsight-1':
+    ip             => '10.3.0.30',
+    vdc            => 'backend',
+    legacy_aliases => [
       'datainsight-1',
       "datainsight.${app_domain}",
       "datainsight-narrative-recorder.${app_domain}",
@@ -205,86 +246,106 @@ class hosts::skyscape::production_like ($platform = $::govuk_platform) {
       "datainsight-everything-recorder.${app_domain}",
       "datainsight-format-success-recorder.${app_domain}",
       "datainsight-insidegov-recorder.${app_domain}"
-    ]
+    ],
   }
-  host { "akamai-logs-1.backend.${platform}":
-    ip           => '10.3.0.11',
-    host_aliases => ['akamai-logs-1']
+  govuk::host { 'akamai-logs-1':
+    ip             => '10.3.0.11',
+    vdc            => 'backend',
+    legacy_aliases => ['akamai-logs-1'],
   }
-  host { "akamai-logs-backup-1.backend.${platform}":
-    ip           => '10.3.0.12',
-    host_aliases => ['akamai-logs-backup-1']
+  govuk::host { 'akamai-logs-backup-1':
+    ip             => '10.3.0.12',
+    vdc            => 'backend',
+    legacy_aliases => ['akamai-logs-backup-1'],
   }
 
   # elms (licence finder) vdc machines
-  host { "licensify-frontend-1.licensify.${platform}":
-    ip           => '10.5.0.2',
-    host_aliases => ['licensify-frontend-1']
+  govuk::host { 'licensify-frontend-1':
+    ip             => '10.5.0.2',
+    vdc            => 'licensify',
+    legacy_aliases => ['licensify-frontend-1'],
   }
-  host { "licensify-frontend-2.licensify.${platform}":
-    ip           => '10.5.0.3',
-    host_aliases => ['licensify-frontend-2']
+  govuk::host { 'licensify-frontend-2':
+    ip             => '10.5.0.3',
+    vdc            => 'licensify',
+    legacy_aliases => ['licensify-frontend-2'],
   }
-  host { "licensify-backend-1.licensify.${platform}":
-    ip           => '10.5.0.4',
-    host_aliases => ['licensify-backend-1']
+  govuk::host { 'licensify-backend-1':
+    ip             => '10.5.0.4',
+    vdc            => 'licensify',
+    legacy_aliases => ['licensify-backend-1'],
   }
-  host { "licensify-backend-2.licensify.${platform}":
-    ip           => '10.5.0.5',
-    host_aliases => ['licensify-backend-2']
+  govuk::host { 'licensify-backend-2':
+    ip             => '10.5.0.5',
+    vdc            => 'licensify',
+    legacy_aliases => ['licensify-backend-2'],
   }
-  host { "licensify-mongo-1.licensify.${platform}":
-    ip           => '10.5.0.6',
-    host_aliases => ['licensify-mongo-1']
+  govuk::host { 'licensify-mongo-1':
+    ip             => '10.5.0.6',
+    vdc            => 'licensify',
+    legacy_aliases => ['licensify-mongo-1'],
   }
-  host { "licensify-mongo-2.licensify.${platform}":
-    ip           => '10.5.0.7',
-    host_aliases => ['licensify-mongo-2']
+  govuk::host { 'licensify-mongo-2':
+    ip             => '10.5.0.7',
+    vdc            => 'licensify',
+    legacy_aliases => ['licensify-mongo-2'],
   }
-  host { "licensify-mongo-3.licensify.${platform}":
-    ip           => '10.5.0.8',
-    host_aliases => ['licensify-mongo-3']
+  govuk::host { 'licensify-mongo-3':
+    ip             => '10.5.0.8',
+    vdc            => 'licensify',
+    legacy_aliases => ['licensify-mongo-3'],
   }
-  host { "licensify-lb-1.licensify.${platform}":
-    ip           => '10.5.0.101',
-    host_aliases => ['licensify-lb-1']
+  govuk::host { 'licensify-lb-1':
+    ip             => '10.5.0.101',
+    vdc            => 'licensify',
+    legacy_aliases => ['licensify-lb-1'],
   }
-  host { "licensify-lb-2.licensify.${platform}":
-    ip           => '10.5.0.102',
-    host_aliases => ['licensify-lb-2']
+  govuk::host { 'licensify-lb-2':
+    ip             => '10.5.0.102',
+    vdc            => 'licensify',
+    legacy_aliases => ['licensify-lb-2'],
   }
 
   #licensify load balancer v-shield edge
-  host { 'licensify-internal-lb.licensify':
-    ip           => '10.5.1.2',
-    host_aliases => ["licensify.${app_domain}", "uploadlicensify.${app_domain}", "licensify-admin.${app_domain}"]
+  govuk::host { 'licensify-internal-lb':
+    ip             => '10.5.1.2',
+    vdc            => 'licensify',
+    legacy_aliases => ["licensify.${app_domain}", "uploadlicensify.${app_domain}", "licensify-admin.${app_domain}"],
   }
 
   #efg vdc machines
-  host { "efg-mysql-master-1.efg.${platform}":
-    ip           => '10.4.0.10',
-    host_aliases => ['efg-mysql-master-1', 'efg.master.mysql']
+  govuk::host { 'efg-mysql-master-1':
+    ip             => '10.4.0.10',
+    vdc            => 'efg',
+    legacy_aliases => ['efg-mysql-master-1', 'efg.master.mysql'],
   }
-  host { "efg-frontend-1.efg.${platform}":
-    ip           => '10.4.0.2',
-    host_aliases => ['efg-frontend-1']
+  govuk::host { 'efg-frontend-1':
+    ip             => '10.4.0.2',
+    vdc            => 'efg',
+    legacy_aliases => ['efg-frontend-1'],
   }
-  host { "efg-mysql-slave-1.efg.${platform}":
-    ip           => '10.4.0.11',
-    host_aliases => ['efg-mysql-slave-1', 'efg.slave.mysql']
+  govuk::host { 'efg-mysql-slave-1':
+    ip             => '10.4.0.11',
+    vdc            => 'efg',
+    legacy_aliases => ['efg-mysql-slave-1', 'efg.slave.mysql'],
   }
 
   # redirector vdc machines
-  host { "redirector-1.redirector.${platform}":
-    ip           => '10.6.0.2',
-    host_aliases => ['redirector-1']
+  govuk::host { 'redirector-1':
+    ip             => '10.6.0.2',
+    vdc            => 'redirector',
+    legacy_aliases => ['redirector-1'],
   }
-  host { "redirector-2.redirector.${platform}":
-    ip           => '10.6.0.3',
-    host_aliases => ['redirector-2']
+  govuk::host { 'redirector-2':
+    ip             => '10.6.0.3',
+    vdc            => 'redirector',
+    legacy_aliases => ['redirector-2'],
   }
 
-  # apto vdc
+  # 3rd-party hosts
+  host { 'vcd00003.vpn.skyscapecs.net':
+    ip => '10.202.5.11',
+  }
   host { 'gds01prod.aptosolutions.co.uk':
     ip => '217.171.99.28',
   }
