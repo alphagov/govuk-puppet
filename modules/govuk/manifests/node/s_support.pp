748e750d9df880100122dcca278e1310bd4e2e55
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-01-16 11:35:08 +0000

    We do not use support boxes to provide MySQL backups any more

--- modules/govuk/manifests/node/s_support.pp
+++ modules/govuk/manifests/node/s_support.pp
@@ -4,13 +4,6 @@ class govuk::node::s_support inherits govuk::node::s_base {
   include apt_cacher::server
   include apt_cacher::client
 
-  if $::govuk_platform == 'production' {
-    # Since these backups are only for the purposes of restoring production
-    # data to preview and development, it makes no sense to configure them on
-    # any environment but production.
-    include mysql::backup
-  }
-
   if $::govuk_platform == 'preview' {
     $mysql_host     = 'rds.cluster'
     $mysql_user     = 'backup'
