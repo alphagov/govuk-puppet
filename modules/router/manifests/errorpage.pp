9129a4c574d51af3e2c7170a718bd7cfbd50d3a7
Philip Potter <philipgpotter@gmail.com>
2013-01-16 17:26:58 +0000

    use silent version of curl

--- modules/router/manifests/errorpage.pp
+++ modules/router/manifests/errorpage.pp
@@ -3,7 +3,7 @@ define router::errorpage () {
   $app_domain = extlookup('app_domain')
 
   exec {"update_error_page_${title}":
-    command => "curl -f https://static.${app_domain}/templates/${title}.html.erb -o /usr/share/nginx/www/${title}.html",
+    command => "curl -sf https://static.${app_domain}/templates/${title}.html.erb -o /usr/share/nginx/www/${title}.html",
     user    => 'deploy',
     group   => 'deploy',
   }
