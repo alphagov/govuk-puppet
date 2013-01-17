63564783624be5cd7f777d327a43592cdfd41763
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-01-17 12:21:57 +0000

    Silence rkhunter by default

--- modules/rkhunter/manifests/config.pp
+++ modules/rkhunter/manifests/config.pp
@@ -2,7 +2,12 @@ class rkhunter::config {
 
   file { '/etc/default/rkhunter':
     ensure => 'present',
-    source => 'puppet:///modules/rkhunter/rkhunter-defaults',
+    source => 'puppet:///modules/rkhunter/etc/default/rkhunter',
+  }
+
+  file { '/etc/rkhunter.conf.local':
+    ensure => 'present',
+    source => 'puppet:///modules/rkhunter/etc/rkhunter.conf.local',
   }
 
 }
