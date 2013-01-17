c46b97d47a011c6f5e0e4fe2d0057550b26a9e15
Dan Carley <dan.carley@gmail.com>
2013-01-17 15:44:49 +0000

    Pass package_ensure to puppetdb spec test
    Pass [new] required parameter to class. Check that the package receives the
    appropriate value.

--- modules/puppetdb/spec/classes/puppetdb_spec.rb
+++ modules/puppetdb/spec/classes/puppetdb_spec.rb
@@ -3,7 +3,9 @@ require_relative '../../../../spec_helper'
 describe 'puppetdb', :type => :class do
   # on sky, bump -Xmx to 1024m
   let (:facts) {{ :govuk_provider => 'sky' }}
+  let (:params) {{ :package_ensure => '1.2.3' }}
   it { should contain_file('/etc/init/puppetdb.conf').with_content(/-Xmx1024m/) }
+  it { should contain_package('puppetdb').with_ensure('1.2.3') }
 end
 
 describe 'puppetdb::config', :type => :class do
