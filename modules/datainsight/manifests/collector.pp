a15ca5b714d71f42049d879cd85fb7fedb0dcef5
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-01-22 12:07:54 +0000

    Add govuk envvar to datainsight collectors.
    This is so that the relevant environment files are
    available for govuk_setenv

--- modules/datainsight/manifests/collector.pp
+++ modules/datainsight/manifests/collector.pp
@@ -13,11 +13,44 @@ define datainsight::collector {
     group  => 'deploy';
   }
 
+  file { "/var/apps/${vhost}":
+    ensure => symlink,
+    owner  => 'deploy',
+    target => "/data/vhost/${vhost_full}/current"
+  }
+
+  file { "/var/log/${vhost}":
+    ensure => symlink,
+    owner  => 'deploy',
+    target => "/data/vhost/${vhost_full}/current/log"
+  }
+
   @logstash::collector { "app-${vhost}":
     content => template("datainsight/collector_logstash.conf.erb"),
   }
 
   @logrotate::conf { "${vhost}-app":
     matches => "/data/vhost/${vhost_full}/shared/log/*.log",
+  }
+
+  govuk::app::envvar {
+    "${vhost}-GOVUK_USER":
+      varname => "GOVUK_USER",
+      value   => "deploy";
+    "${vhost}-GOVUK_GROUP":
+      varname => "GOVUK_GROUP",
+      value   => "deploy";
+    "${vhost}-GOVUK_APP_NAME":
+      varname => "GOVUK_APP_NAME",
+      value   => $vhost;
+    "${vhost}-GOVUK_APP_ROOT":
+      varname => "GOVUK_APP_ROOT",
+      value   => "/var/apps/${vhost}";
+    "${vhost}-GOVUK_APP_LOGROOT":
+      varname => "GOVUK_APP_LOGROOT",
+      value   => "/var/log/${vhost}";
+    "${vhost}-GOVUK_STATSD_PREFIX":
+      varname => "GOVUK_STATSD_PREFIX",
+      value   => "govuk.app.${vhost}.${::hostname}";
   }
 }
