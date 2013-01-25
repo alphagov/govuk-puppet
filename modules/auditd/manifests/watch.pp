3033cfe74cf98fb8b02795c3c2f4089d6c89fe0b
Philip Potter <philipgpotter@gmail.com>
2013-01-25 10:53:01 +0000

    Revert auditd::watch feature
    Reverts "add auditd::watch for watching particular files"
    Reverts "convert audit.rules from File to Concat resource"
    This reverts commit 8af04ac62990214f76e8fb788d55e35ab7641167.
    This reverts commit d9e569b7adbe80e98d68349ef212bf5bc4b1f8b1.
    
    The reason I originally wanted this feature was to monitor a user's
    id_rsa. Unfortunately, it would also log every *legitimate* access to
    the private key, and I cannot think of a sensible way to distinguish
    "correct" from "incorrect" accesses in the face of a user who has root
    on the box.

--- modules/auditd/manifests/watch.pp
+++
@@ -1,6 +0,0 @@
-define auditd::watch ($file) {
-  concat::fragment {"auditd-watch-${title}":
-    target  => '/etc/audit/audit.rules',
-    content => "-w ${file} -k ${title}\n",
-  }
-}
--- modules/auditd/spec/classes/auditd_spec.rb
+++ modules/auditd/spec/classes/auditd_spec.rb
@@ -1,11 +1,6 @@
 require_relative '../../../../spec_helper'
 
 describe 'auditd', :type => :class do
-  let (:facts) do
-    {
-      :concat_basedir => '/undefined'
-    }
-  end
   it do
     should contain_file('/etc/audit/audit.rules')
     should contain_file('/etc/audit/auditd.conf')
