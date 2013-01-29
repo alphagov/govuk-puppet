b84575f3839b3e98efb5c8dd458b32f7205d7bb3
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-01-29 11:13:42 +0000

    Install default vhost on load balancers
    The downstream load balancers can't specify an HTTP Host header, so we need to
    make sure that something will respond to them when they attempt to health
    check. This installs a default vhost returning 200 OK to satisfy them.

--- modules/loadbalancer/manifests/init.pp
+++ modules/loadbalancer/manifests/init.pp
@@ -1,3 +1,12 @@
 class loadbalancer {
+
   include nginx
+
+  # Install a default vhost returning 200 OK for the purposes of the
+  # downstream vShield Edge load balancer healthchecks
+  nginx::config::vhost::default { 'default':
+    status         => '200',
+    status_message => '',
+  }
+
 }
