56acdf7446262c5b46200637f1fa27e5a7729bba
Dan Carley <dan.carley@gmail.com>
2013-01-31 16:18:09 +0000

    (#42354153) Remove defunct firewall comments
    All rules have now be moved to their respective modules.

--- modules/govuk/manifests/firewall.pp
+++ modules/govuk/manifests/firewall.pp
@@ -7,18 +7,4 @@ class govuk::firewall {
   Ufw::Deny <| |>
   Ufw::Limit <| |>
 
-  # The rules in this module are based on an audit of all ports listening
-  # for our EC2 Preview and Production infrastructure. The CSV describing all
-  # listening ports is saved as port_audit.csv
-
-  # To generate it I did:
-  #   fab preview sdo:'netstat -plant | grep LISTEN' > ~/preview-netstat
-  #   fab production sdo:'netstat -plant | grep LISTEN' > ~/production-netstat
-  #   cat production-netstat preview-netstat |\
-  #         egrep -v "(Disconnecting|Executing task|sudo:|^Done|^$)" |\
-  #         awk '{print $3,$6,$7,$9}' | egrep -v '^tcp6' |\
-  #         grep -v "127.0.0.1:" | sed 's:0.0.0.0\:\*::g' |\
-  #         sed "s: [0-9]*/::g" | cut -d: -f2 | sort | uniq -c |\
-  #         awk 'BEGIN{OFS=",";}{print $1,$2,$3}' > port_audit.csv
-
 }
