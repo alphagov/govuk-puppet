af34d87e96fbb4721a36ca7c4a2f7a159721a6bf
Dan Carley <dan.carley@gmail.com>
2013-02-04 11:39:20 +0000

    (#43635273) Upgrade Graphite to 0.9.10
    Using packages from this PPA pulled into our own PPA:
    
    - https://launchpad.net/~sidnei/+archive/stats
    
    Versions are pinned. The name of python-graphite-web has changed.

--- modules/graphite/manifests/package.pp
+++ modules/graphite/manifests/package.pp
@@ -1,19 +1,29 @@
 class graphite::package {
 
-  apt::repository { 'ubuntuus':
-    url  => 'http://us.archive.ubuntu.com/ubuntu',
-    repo => 'multiverse',
-  }
+  include govuk::ppa
 
   package { [
     'python-flup',
-    'python-carbon',
-    'python-graphite-web',
     'python-txamqp',
-    'python-simplejson',
-    'python-whisper'
+    'python-simplejson'
   ]:
-    ensure => present,
+    ensure  => present,
+  }
+
+  package { 'python-whisper':
+    ensure  => "0.9.10-1+r52-1ppa22~${::lsbdistcodename}1",
+  }
+
+  package { 'python-carbon':
+    ensure  => "0.9.10-1+r245-1ppa25~${::lsbdistcodename}1",
+  }
+
+  package { 'python-graphite-web':
+    ensure  => absent,
+  }
+  package { 'python-graphite':
+    ensure  => "0.9.10-2+r845-1ppa31~${::lsbdistcodename}1",
+    require => Package['python-graphite-web'],
   }
 
   file { '/var/log/graphite':
--- modules/graphite/spec/classes/graphite_spec.rb
+++ modules/graphite/spec/classes/graphite_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../../../../spec_helper'
 
 describe 'graphite', :type => :class do
+  let(:facts) {{ :lsbdistcodename => 'precise' }}
+
   it do
-    should contain_package("python-graphite-web").with_ensure('present')
-    should contain_package("python-carbon").with_ensure('present')
-    should contain_package("python-whisper").with_ensure('present')
+    should contain_package("python-graphite").with_ensure(/precise/)
+    should contain_package("python-carbon").with_ensure(/precise/)
+    should contain_package("python-whisper").with_ensure(/precise/)
     should contain_service("carbon_cache")
     should contain_service("graphite")
   end
