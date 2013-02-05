5315d9688b7b1a55612e745085fc9b2be1d76f99
Dan Carley <dan.carley@gmail.com>
2013-02-05 07:45:09 +0000

    (#43475347) Manage collectd-core package as well
    collectd doesn't have a versioned dependency on collectd-core so if we want
    to upgrade it to a given version we have to manage both.

--- modules/collectd/manifests/package.pp
+++ modules/collectd/manifests/package.pp
@@ -1,5 +1,7 @@
 class collectd::package {
-  package { 'collectd':
+  # collectd doesn't have a versioned dependency on collectd-core so if we
+  # want to upgrade it to a given version we have to manage both.
+  package { ['collectd', 'collectd-core']:
     ensure  => present,
   }
 }
