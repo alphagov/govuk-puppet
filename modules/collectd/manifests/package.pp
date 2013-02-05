acb776ee62f407c481f14315059cd18c1f28436f
Dan Carley <dan.carley@gmail.com>
2013-02-05 11:16:32 +0000

    (#43475347) Upgrade collectd to 5.1
    Using package imported into our PPA from:
    
    - https://launchpad.net/~jeff-kerzner/+archive/collectd
    
    Supports Lucid and Precise.

--- modules/collectd/manifests/package.pp
+++ modules/collectd/manifests/package.pp
@@ -1,7 +1,13 @@
 class collectd::package {
+  # Allow this to fail() on unsupported versions of Ubuntu.
+  $package_version = $::lsbdistcodename ? {
+    'lucid'   => '5.1.0-3',
+    'precise' => '5.1.0-2',
+  }
+
   # collectd doesn't have a versioned dependency on collectd-core so if we
   # want to upgrade it to a given version we have to manage both.
   package { ['collectd', 'collectd-core']:
-    ensure  => present,
+    ensure  => $package_version,
   }
 }
