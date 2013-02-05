0ca70e438f45fdc761842229384b8bbeeb3e44ba
Dan Carley <dan.carley@gmail.com>
2013-02-05 11:29:15 +0000

    (#43475347) Collectd write out to Graphite
    Enable the write_graphite plugin to export metrics into Graphite. This
    presently assumes that collectd::server and graphite are running on the same
    host, for network efficiency.

--- modules/collectd/manifests/server/config.pp
+++ modules/collectd/manifests/server/config.pp
@@ -2,4 +2,9 @@ class collectd::server::config inherits collectd::config {
   File['/etc/collectd/conf.d/network.conf'] {
     source  => 'puppet:///modules/collectd/etc/collectd/conf.d/network.conf.server',
   }
+
+  file { '/etc/collectd/conf.d/graphite.conf':
+    ensure  => present,
+    source  => 'puppet:///modules/collectd/etc/collectd/conf.d/graphite.conf',
+  }
 }
