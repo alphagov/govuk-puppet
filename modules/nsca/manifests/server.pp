da7ca1e394092e0839de49f7b517aeec24cbc46d
Philip Potter <philipgpotter@gmail.com>
2013-02-05 17:40:56 +0000

    Add firewall rule for NSCA
    I'd really appreciate a look-over from @samjsharpe or @nickstenning to
    make sure what I'm doing is the right thing here.

--- modules/nsca/manifests/server.pp
+++ modules/nsca/manifests/server.pp
@@ -2,6 +2,10 @@ class nsca::server {
   include nagios # to set up the 'nagios' user for nsca
   include nsca
 
+  @ufw::allow { "allow-nsca-from-all":
+    port => 5667,
+  }
+
   service {'nsca':
     ensure  => running,
     require => [Package['nagios3'],Package['nsca']],
