4b897ff5068e98579024db3551874a160757f255
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-02-06 15:31:47 +0000

    Manage the update-motd package This commit makes sure that the update-motd package is installed. This package runs under pam_motd and is responsible for creating /var/run/motd on SSH login. By managing the /etc/update-motd.d/ directory and ensuring that by default it is empty, we can have a blank motd by default

--- modules/base_packages/manifests/init.pp
+++ modules/base_packages/manifests/init.pp
@@ -30,6 +30,18 @@ class base_packages {
     ensure => installed
   }
 
+  # This package creates a dynamic motd generated on ssh login
+  package { 'update-motd': ensure => installed}
+
+  # Removing the default files created by update-motd
+  file { '/etc/update-motd.d/':
+    ensure  => directory,
+    # Clear all scripts from this dir that are not puppet managed
+    purge   => true,
+    recurse => true,
+    require => Package['update-motd'],
+  }
+
   case $::lsbdistcodename {
     'precise': {
         package{
