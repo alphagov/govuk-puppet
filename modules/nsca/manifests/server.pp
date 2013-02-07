2737e48cd66e247adf5dc175e43375d4766e34e6
Philip Potter <philipgpotter@gmail.com>
2013-02-07 17:30:29 +0000

    add comment explaining cross-module dependency

--- modules/nsca/manifests/server.pp
+++ modules/nsca/manifests/server.pp
@@ -8,6 +8,14 @@ class nsca::server {
 
   service {'nsca':
     ensure  => running,
+    # ugh. cross-module dependency alert.
+    # we require Package['nagios3'] because it creates the 'nagios'
+    # user which the nsca process depends on. I don't want to depend
+    # on Class['nagios'] because that feels a bit too restrictive - we
+    # don't want to stop nsca starting before nagios3 has started.
+    #
+    # if you can imagine a better way of doing this, please do it! :)
+    # -- ppotter 2013-02-07
     require => [Package['nagios3'],Package['nsca']],
   }
 }
