09ee4ca0a52c637f45bcf3047bb92733eb73e3f7
Dan Carley <dan.carley@gmail.com>
2013-02-11 11:35:05 +0000

    (#39116501) Document nginx::config::vhost::default
    Document current params before I add a new one.

--- modules/nginx/manifests/config/vhost/default.pp
+++ modules/nginx/manifests/config/vhost/default.pp
@@ -1,3 +1,24 @@
+# == Define: nginx::config::vhost::default
+#
+# Create a catchall vhost for Nginx.
+#
+# === Parameters
+#
+# [*extra_config*]
+#   Raw Nginx config lines to insert into the vhost config.
+#
+# [*status*]
+#   HTTP response code to return.
+#   Default: 500
+#
+# [*status_message*]
+#   HTTP response content to return.
+#   Default: {"error": "Fell through to default vhost"}
+#
+# [*ssl_certtype*]
+#   Type of SSL cert passed to nginx::config::ssl
+#   Default: wildcard_alphagov
+#
 define nginx::config::vhost::default(
   $extra_config = '',
   $status = '500',
