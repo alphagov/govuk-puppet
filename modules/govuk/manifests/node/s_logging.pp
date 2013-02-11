f377180c241e707c9c6e79b5784b305525acf5aa
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-02-11 15:48:31 +0000

    (#44040271) while we still need a logging class we dont actually want todo anything yet.

--- modules/govuk/manifests/node/s_logging.pp
+++ modules/govuk/manifests/node/s_logging.pp
@@ -1,21 +1,2 @@
 class govuk::node::s_logging inherits govuk::node::s_base {
-  include nginx
-
-  if $::govuk_provider == 'sky' {
-    $es_heap_size = '8g'
-  } else {
-    $es_heap_size = '4g'
-  }
-
-  class { 'logstash::server':
-    es_heap_size => $es_heap_size,
-  }
-
-  $app_domain = extlookup('app_domain')
-
-  nginx::config::vhost::proxy {
-    "logging.${app_domain}":
-      to        => ['localhost:9292'],
-      aliases   => ["graylog.${app_domain}"],
-  }
 }
