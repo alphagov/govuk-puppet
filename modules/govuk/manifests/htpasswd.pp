a05885fba543b949064531a2768071039abf62d6
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-02-11 11:34:31 +0000

    Fix a typo in govuk::htpasswd

--- modules/govuk/manifests/htpasswd.pp
+++ modules/govuk/manifests/htpasswd.pp
@@ -1,10 +1,10 @@
 class govuk::htpasswd {
 
   $http_username = extlookup('http_username','notset')
-  $http_passhash = extlookup('http_password','notset')
+  $http_passhash = extlookup('http_passhash','notset')
   file { '/etc/govuk.htpasswd':
     ensure  => 'present',
-    content => "${http_username}:${http_passhash}\n",
+    content => "${http_username}:${http_passhash}",
   }
 
 }
