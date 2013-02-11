4635ff0f89fe0975b74652e22b7a219d1d30c86b
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-02-11 10:12:44 +0000

    Move username/passhash from govuk::htpasswd to extdata

--- modules/govuk/manifests/htpasswd.pp
+++ modules/govuk/manifests/htpasswd.pp
@@ -1,8 +1,10 @@
 class govuk::htpasswd {
 
+  $http_username = extlookup('http_username','notset')
+  $http_passhash = extlookup('http_password','notset')
   file { '/etc/govuk.htpasswd':
-    ensure => 'present',
-    source => 'puppet:///modules/govuk/govuk.htpasswd',
+    ensure  => 'present',
+    content => template('govuk/govuk.htpasswd.erb'),
   }
 
 }
