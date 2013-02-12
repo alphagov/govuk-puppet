bf9be2c056df7328e4d085503fb3968f75c1c450
Dan Carley <dan.carley@gmail.com>
2013-02-12 16:32:03 +0000

    (#44273387) Populate nameservers from extlookup
    Class docs contain usage and rationale. Google public DNS has been added to
    the deployment repo, under common.

--- modules/resolvconf/manifests/config.pp
+++ modules/resolvconf/manifests/config.pp
@@ -1,6 +1,20 @@
+# == Class: resolvconf::config
+#
+# Configure nameservers by prepending them to `resolv.conf.d/head` which
+# ensures they will always come first.
+#
+# Using this rather brute force method, instead of `interfaces(5)`, because
+# it requires no other parsing or knowledge about the network configuration.
+#
+# Values can be populated with the extlookup key `resolvconf_nameservers`.
+# If not specified, it will default to a stock-empty config. This is the
+# preferred behavour for hosts that use DHCP - like EC2 and Vagrant.
+#
 class resolvconf::config {
+  $nameservers = extlookup('resolvconf_nameservers', [])
+
   file { '/etc/resolvconf/resolv.conf.d/head':
-    ensure => present,
-    source => 'puppet:///modules/resolvconf/etc/resolvconf/resolv.conf.d/head',
+    ensure  => present,
+    content => template('resolvconf/etc/resolvconf/resolv.conf.d/head.erb'),
   }
 }
