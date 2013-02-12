18f51c7b049eb58a2f1bd960d3c9f3c6b0ce09a1
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-12 15:16:38 +0000

    Set ES heap size from machine mem

--- modules/govuk/manifests/node/s_elasticsearch.pp
+++ modules/govuk/manifests/node/s_elasticsearch.pp
@@ -1,10 +1,12 @@
 class govuk::node::s_elasticsearch inherits govuk::node::s_base {
 
+  $es_heap_size = $::memtotalmb / 4 * 3
+
   include java::oracle7::jre
 
   class { 'elasticsearch':
     cluster_name       => 'logging',
-    heap_size          => '12g',
+    heap_size          => "${es_heap_size}m",
     number_of_replicas => '0',
     require            => Class['java::oracle7::jre'],
   }
