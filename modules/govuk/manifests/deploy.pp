65b1d5d763b1ad8ce5e7c637d677ae4aeb5f4e99
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-12 16:07:44 +0000

    Add govuk_logpipe tool for shipping logs

--- modules/govuk/manifests/deploy.pp
+++ modules/govuk/manifests/deploy.pp
@@ -171,4 +171,18 @@ class govuk::deploy {
     'RAILS_ENV': value => $govuk_env;
     'RACK_ENV':  value => $govuk_env;
   }
+
+  # tagalog provides logship, used by govuk_logpipe
+  package { 'tagalog':
+    ensure   => present,
+    provider => 'pip',
+  }
+
+  # govuk_logpipe pipes application logs
+  file { '/usr/local/bin/govuk_logpipe':
+    ensure  => present,
+    source  => 'puppet:///modules/govuk/bin/govuk_logpipe',
+    mode    => '0755',
+    require => Package['tagalog'],
+  }
 }
