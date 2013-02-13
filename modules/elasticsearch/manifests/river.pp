6b8f4e689e2e2564f510d057c9b288d3f4f796fc
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-13 15:47:52 +0000

    Add a type to manage elasticsearch rivers

---
+++ modules/elasticsearch/manifests/river.pp
@@ -0,0 +1,47 @@
+# == Type: elasticsearch::river
+#
+# Create an elasticsearch river
+#
+# === Parameters
+#
+# [*river_name*]
+#   The name of the river.
+
+# [*content*]
+#   The content to use to create the river. This should be a valid JSON
+#   document. NB: This is a bit of a hack, and you should not have any single
+#   quotes anywhere in your JSON document!
+#
+define elasticsearch::river (
+  $content,
+  $ensure = 'present',
+  $river_name = $title
+) {
+
+  # Install a river using the es-river tool installed as part of
+  # `estools` by the elasticsearch::package class.
+  case $ensure {
+
+    'present': {
+      exec { "create-elasticsearch-river-${river_name}":
+        command => "echo '${content}' | es-river create '${river_name}'",
+        unless  => "echo '${content}' | es-river compare '${river_name}'",
+        require => Class['elasticsearch::service'],
+      }
+    }
+
+    'absent': {
+      exec { "delete-elasticsearch-river-${river_name}":
+        command => "es-river delete '${river_name}'",
+        onlyif  => "es-river get '${river_name}'",
+        require => Class['elasticsearch::service'],
+      }
+    }
+
+    default: {
+      fail("Invalid 'ensure' value '${ensure}' for elasticsearch::river")
+    }
+
+  }
+
+}
