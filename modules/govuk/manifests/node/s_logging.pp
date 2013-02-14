da7eb0ad89c47d16fd26e4e0a7ca7d2ca0db1c88
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-02-14 15:59:42 +0000

    [#44040347] allow other machines to contact the syslog server on 514/tcp

--- modules/govuk/manifests/node/s_logging.pp
+++ modules/govuk/manifests/node/s_logging.pp
@@ -1,3 +1,18 @@
 class govuk::node::s_logging inherits govuk::node::s_base {
+
+# we want this to be a syslog server.
 class { 'rsyslog::server': }
+
+# we want all the other machines to be able to send syslog on 514/tcp to this machine
+  @ufw::allow {
+    'allow-syslog-from-backend':
+      from => '10.3.0.0/16',
+      port => 514;
+    'allow-syslog-from-frontend':
+      from => '10.2.0.0/16',
+      port => 514;
+    'allow-syslog-from-management':
+      from => '10.0.0.0/16',
+      port => 514;
+  }
 }
