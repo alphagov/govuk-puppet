48a8e091e40078465f29322b6186b8600b05258d
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-02-15 13:46:54 +0000

    Revert "Setup datainsight-govuk-collector"
    This reverts commit 2357651e6b6944434c56e0ac590d7298838c1b56.

--- modules/govuk/manifests/node/s_datainsight.pp
+++ modules/govuk/manifests/node/s_datainsight.pp
@@ -14,7 +14,6 @@ class govuk::node::s_datainsight inherits govuk::node::s_base {
 
   datainsight::collector { 'narrative': }
   datainsight::collector { 'ga': }
-  datainsight::collector { 'govuk': }
   datainsight::collector { 'insidegov': }
   datainsight::collector { 'nongovuk-reach': }
 }
