84a616020b9e876a395525c6e6048eb49243afb0
Garima Singh <igarimasingh@gmail.com>
2013-02-18 13:54:44 +0000

    Adding ssh access for garimas

--- modules/users/manifests/groups/govuk.pp
+++ modules/users/manifests/groups/govuk.pp
@@ -101,6 +101,11 @@ govuk::user { 'benilovj':
     email    => 'gareth.rushgrove@digital.cabinet-office.gov.uk',
     ssh_key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCuJRONJm0HaO+YfY2PnV8vk+v0FuZGxgCliS8wF3RiP/A5qV2jtSwy4KqctLRS2nSCT2klRmVENT2SXzdaCI9CDLIvzX10ePUUhE8PYux5dC/RPPRAGq+pxivdbu4avsc0Zkhq+v0h/og/+7gtwlan8TGYJGBy0UM9h1CWmd072W9Oi5rBDw0tLLMWl6T8CwAuXaU2tq/TW5jeXNVekUgQlUiA4FYFJW3h3G0rkZJ9p4WKU9ER+sRI9UxUBSeRjyjPvSKdFIKlSBlTBStT0f7V3pXvTQFDzaCY+f1VO/JKZr5dHlStKhxZir5tPG140CHApaC5Mg60NseIdOyU5kzp',
   }
+  govuk::user { 'garimas':
+    fullname => 'Garima Singh',
+    email    => 'garima.singh@digital.cabinet-office.gov.uk',
+    ssh_key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCVHp5WGK5UseUbg8Qz7x7geR4RInaM4Zrmd2FeyW23I259otKrqaeRJLzcSaliz4esRCWYopK0q5iTjK4oOSgr4BM36L9B7+YYIDcsG9F7FHoabxoDwrfzWHYhknweQpZ0S78oY0PUcGiwSXT1FUHHOTyMOuYETZ6/BKI58+dwUz/tj2uCG27fXXZozQRxOtUQgYq7afGRpifiDFX6b8z2gjhM6N1wP9WyqL67cuXO6jGaMDDKh8eLbPswOFxcBn/rqjUpVfDVqfBOAbL+6OgpJKdTZQtCYEgW3d78scZ5DZazFGGQqLQ+we8srov2ev4OISPjSHnNxN/rrlSD7j1R',
+  }
   govuk::user { 'heathd':
     fullname => 'David Heath',
     email    => 'david.heath@digital.cabinet-office.gov.uk',
