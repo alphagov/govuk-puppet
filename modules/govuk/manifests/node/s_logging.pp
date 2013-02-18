3af61416bc8948c6a0e9d3e30cc96c70eef33554
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-02-18 13:20:51 +0000

    There are more than 3 VDCs, we should allow logging from all of them

--- modules/govuk/manifests/node/s_logging.pp
+++ modules/govuk/manifests/node/s_logging.pp
@@ -5,14 +5,8 @@ class govuk::node::s_logging inherits govuk::node::s_base {
 
 # we want all the other machines to be able to send syslog on 514/tcp to this machine
   @ufw::allow {
-    'allow-syslog-from-backend':
-      from => '10.3.0.0/16',
-      port => 514;
-    'allow-syslog-from-frontend':
-      from => '10.2.0.0/16',
-      port => 514;
-    'allow-syslog-from-management':
-      from => '10.0.0.0/16',
+    'allow-syslog-from-anywhere':
+      from => '10.0.0.0/8',
       port => 514;
   }
 }
