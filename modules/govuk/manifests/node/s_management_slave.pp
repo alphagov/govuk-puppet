69c06a30336c0e4161265662cd98064af53eb828
Dan Carley <dan.carley@gmail.com>
2013-02-19 15:38:47 +0000

    Apply govuk::openconnect to management+slave
    Will configure OpenConnect VPN on CI nodes in EC2. Applying "by hand" with
    `--tags` for the moment.

--- modules/govuk/manifests/node/s_management_slave.pp
+++ modules/govuk/manifests/node/s_management_slave.pp
@@ -1,6 +1,7 @@
 class govuk::node::s_management_slave inherits govuk::node::s_base {
   include govuk::node::s_management
   include jenkins::slave
+  include govuk::openconnect
 
   ssh_authorized_key { 'management_server_master':
     type => rsa,
