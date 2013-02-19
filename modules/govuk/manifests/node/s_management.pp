6822020d25b3255bfe6f93fbcf4fa91cc1bc8b10
Dan Carley <dan.carley@gmail.com>
2013-02-19 15:57:09 +0000

    Host['github.gds'] on CI EC2.

--- modules/govuk/manifests/node/s_management.pp
+++ modules/govuk/manifests/node/s_management.pp
@@ -2,4 +2,9 @@ class govuk::node::s_management inherits govuk::node::s_base {
   include govuk::node::s_management_base
   include jenkins::master
   include govuk::openconnect
+
+  host { 'github.gds':
+    ip      => '192.168.9.110',
+    comment => 'Ignore VPN DNS and set static host for GHE',
+  }
 }
--- modules/govuk/manifests/node/s_management_slave.pp
+++ modules/govuk/manifests/node/s_management_slave.pp
@@ -3,6 +3,11 @@ class govuk::node::s_management_slave inherits govuk::node::s_base {
   include jenkins::slave
   include govuk::openconnect
 
+  host { 'github.gds':
+    ip      => '192.168.9.110',
+    comment => 'Ignore VPN DNS and set static host for GHE',
+  }
+
   ssh_authorized_key { 'management_server_master':
     type => rsa,
     key  => extlookup('jenkins_key', 'NO_KEY_IN_EXTDATA'),
