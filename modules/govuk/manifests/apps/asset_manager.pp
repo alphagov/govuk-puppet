ea25b4a8de3263b4dd25b9f92f6c9fccbe265fbd
Kushal Pisavadia <kushi.p@gmail.com>
2013-02-20 17:38:41 +0000

    Use X-Accel-Redirect to serve files from asset-management
    For X-Accel-Redirect to work in Rails the headers need to be added for
    a request for X-Sendfile-Type and the X-Accel-Mapping for the correct
    output to be sent to nginx.
    
    The /raw/ path is just used internally between the application and
    nginx for the file mapping.

--- modules/govuk/manifests/apps/asset_manager.pp
+++ modules/govuk/manifests/apps/asset_manager.pp
@@ -1,8 +1,21 @@
 class govuk::apps::asset_manager( $port = 3037 ) {
   govuk::app { 'asset-manager':
-    app_type          => 'rack',
-    port              => $port,
-    vhost_ssl_only    => true,
-    health_check_path => '/',
+    app_type           => 'rack',
+    port               => $port,
+    vhost_ssl_only     => true,
+    health_check_path  => '/',
+    nginx_extra_config => '
+    client_max_body_size 500m;
+
+    proxy_set_header X-Sendfile-Type X-Accel-Redirect;
+    proxy_set_header X-Accel-Mapping /var/govuk/asset-manager/uploads/assets/=/raw/;
+
+    # /raw/(.*) is the path mapping sent from the rails application to
+    # nginx and is immediately picked up. /raw/(.*) is not available
+    # publicly as it\'s an internal path mapping.
+    location ~ /raw/(.*) {
+      internal;
+      alias /var/apps/asset-manager/uploads/assets/$1;
+    }'
   }
 }
