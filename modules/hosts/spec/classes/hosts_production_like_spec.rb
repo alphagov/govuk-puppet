69c357a4aa86768c2b378da9a3329e7825ded16e
Dan Carley <dan.carley@gmail.com>
2013-02-21 11:12:50 +0000

    Wrap hosts specs within same describe block
    Having dupe describe blocks didn't seem to upset rspec-puppet. However we
    can remove some duplication and make the `-fd` output read nicerer.

--- modules/hosts/spec/classes/hosts_production_like_spec.rb
+++ modules/hosts/spec/classes/hosts_production_like_spec.rb
@@ -1,25 +1,18 @@
 require_relative '../../../../spec_helper'
 
 describe 'hosts::skyscape::production_like', :type => :class do
-  context 'with website_host not set we expect default behaviour of putting in www.gov.uk' do
+  context 'with website_host => www.gov.uk (default)' do
     it { should contain_govuk__host('cache').with_legacy_aliases(['cache', "www.test.gov.uk", "www-origin.test.gov.uk", "www.gov.uk"]) }
   end
-end 
 
-describe 'hosts::skyscape::production_like', :type => :class do
+  context 'with website_host => www.doesnt.exist' do
+    before :each do
+      update_extdata({'website_host' => 'www.doesnt.exist',})
+    end
 
-  before :each do
-    update_extdata({'website_host' => 'www.doesnt.exist',})
-  end
+    #force refresh of the extdata - yes its a hack
+    let(:facts) {{ :foo => "bar" }}
 
-  #force refresh of the extdata - yes its a hack
-  let(:facts) { {:foo => "bar"} }
-
-  context 'with website_host set to a non-default value we expect it in the list' do
     it { should contain_govuk__host('cache').with_legacy_aliases(['cache', "www.test.gov.uk", "www-origin.test.gov.uk", "www.doesnt.exist"]) }
   end
 end 
-
-
-
-
