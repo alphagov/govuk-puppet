cc916ee3238b5473bfe83e58cbbb54b4df810ad7
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-21 12:05:38 +0000

    Serve ci.alphagov.co.uk Jenkins with nginx

--- modules/govuk/manifests/node/s_management.pp
+++ modules/govuk/manifests/node/s_management.pp
@@ -4,6 +4,20 @@ class govuk::node::s_management inherits govuk::node::s_base {
   include jenkins::master
   include govuk::openconnect
 
+  package { 'apache2':
+    ensure => 'purged'
+  }
+
+  # Close connection if vhost not known
+  nginx::config::vhost::default { 'default':
+    status         => '444',
+    status_message => '',
+  }
+
+  nginx::config::site { 'jenkins':
+    content => template('govuk/node/s_management/jenkins.conf.erb'),
+  }
+
   host { 'github.gds':
     ip      => '192.168.9.110',
     comment => 'Ignore VPN DNS and set static host for GHE',
