06c257ad3b477403bc8af202fba32e6762764037
Dan Carley <dan.carley@gmail.com>
2013-02-21 17:34:16 +0000

    Remove VPN direct includes from s_management_slave
    These are already included via `govuk::node::s_management`.

--- modules/govuk/manifests/node/s_management_slave.pp
+++ modules/govuk/manifests/node/s_management_slave.pp
@@ -1,12 +1,6 @@
 class govuk::node::s_management_slave inherits govuk::node::s_base {
   include govuk::node::s_management
   include jenkins::slave
-  include govuk::openconnect
-
-  host { 'github.gds':
-    ip      => '192.168.9.110',
-    comment => 'Ignore VPN DNS and set static host for GHE',
-  }
 
   ssh_authorized_key { 'management_server_master':
     type => rsa,
