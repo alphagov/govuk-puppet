3a10d8316924ba4d454d5a81d0a6ba5f272bf895
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-25 17:34:23 +0000

    Add govuk_production_access group

---
+++ modules/users/manifests/groups/govuk_production_access.pp
@@ -0,0 +1,25 @@
+class users::groups::govuk_production_access {
+
+  include users::alext
+  include users::annashipman
+  include users::carlmassa
+  include users::dafydd
+  include users::dai
+  include users::davidillsley
+  include users::davidt
+  include users::dcarley
+  include users::garethr
+  include users::heathd
+  include users::jabley
+  include users::james
+  include users::jamiec
+  include users::kushalp
+  include users::mazz
+  include users::mwall
+  include users::nickstenning
+  include users::ppotter
+  include users::psd
+  include users::robyoung
+  include users::ssharpe
+
+}
