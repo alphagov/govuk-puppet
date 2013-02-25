3e2d943caa1f756f034a777104191a0f2c37fdba
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-02-25 12:02:26 +0000

    [#44040467] Apply the rsyslog::logstash class to the logging machines

--- modules/govuk/manifests/node/s_logging.pp
+++ modules/govuk/manifests/node/s_logging.pp
@@ -2,6 +2,8 @@ class govuk::node::s_logging inherits govuk::node::s_base {
 
 # we want this to be a syslog server.
   class { 'rsyslog::server': }
+# we also want it to send stuff to logstash
+  class { 'rsyslog::logstash': }
 
 # we want all the other machines to be able to send syslog on 514/tcp to this machine
   @ufw::allow {
