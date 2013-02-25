a477078a68540b5c810274438cdf9b9eebdf8666
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-02-25 18:13:46 +0000

    [#44040467] filter syslog messages

--- modules/govuk/manifests/node/s_logging.pp
+++ modules/govuk/manifests/node/s_logging.pp
@@ -27,4 +27,52 @@ include java::oracle7::jre
     require => Wget::Fetch['logstash-monolithic'],
   }
 
+#configure logstash inputs
+  logstash::input::tcp {'syslog':
+    type => 'syslog',
+    port => '5544',
+  } 
+  logstash::input::udp {'syslog':
+    type => 'syslog',
+    port => '5544',
+  } 
+#configure logstash filters
+  logstash::filter::grok {'syslog':
+    type => 'syslog',
+      pattern => [ "<%{POSINT:syslog_pri}>%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} %{PROG:syslog_program}(?:\[%{POSINT:syslog_pid}\])?: %{GREEDYDATA:syslog_message}" ],
+      add_field => { "received_at" => "%{@timestamp}", "received_from" => "%{@source_host}" },
+      order => '11',
+}
+  logstash::filter::syslog_pri {'syslog':
+    type => 'syslog',
+      order => '12',
+}
+  logstash::filter::date {'syslog':
+    type => 'syslog',
+    match => [ "syslog_timestamp", "MMM dd HH:mm:ss",
+          "MMM  d HH:mm:ss" ],
+      order => '13',
+}
+  logstash::filter::mutate {'syslog-1':
+    type => 'syslog',
+      order => '14',
+exclude_tags => [ "_grokparsefailure" ],
+replace => { "@source_host" => "%{syslog_hostname}", "@message" => "%{syslog_message}" },
+}
+
+  logstash::filter::mutate {'syslog-2':
+    type => 'syslog',
+      order => '15',
+remove => [ "syslog_hostname", "syslog_message", "syslog_timestamp" ],
+}
+
+
+#configure logstash outputs
+  logstash::output::elasticsearch_http {'syslog':
+    host => "elasticsearch-1.management",
+    index => "logs-current",
+  }
+
+  
+
 }
