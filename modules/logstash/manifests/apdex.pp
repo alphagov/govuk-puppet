5b4747f34e8ef3a67d29c2b1cd1d01ae0151d9cc
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-02-25 12:08:35 +0000

    [#44040467] remove our current logstash module since I intend to replace it with https://github.com/electrical/puppet-logstash

--- modules/logstash/manifests/apdex.pp
+++
@@ -1,9 +0,0 @@
-define logstash::apdex(
-  $instance_class = undef
-){
-  cron { "apdex-for-${title}":
-    command => "/usr/local/bin/apdex.sh ${instance_class} ${title}",
-    user    => 'logstash',
-    minute  => '30',
-  }
-}
--- modules/logstash/manifests/config.pp
+++
@@ -1,8 +0,0 @@
-class logstash::config {
-
-  file { '/etc/logstash/grok-patterns/' :
-    source  => 'puppet:///modules/logstash/etc/logstash/grok-patterns/',
-    recurse => true,
-  }
-
-}
