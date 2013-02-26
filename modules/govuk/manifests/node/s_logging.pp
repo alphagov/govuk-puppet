2a344d0836ab57e6588602418bb0ef18a3598268
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-02-26 12:11:32 +0000

    make the fiel pas lint checks

--- modules/govuk/manifests/node/s_logging.pp
+++ modules/govuk/manifests/node/s_logging.pp
@@ -13,66 +13,70 @@ class govuk::node::s_logging inherits govuk::node::s_base {
   }
 
 include java::oracle7::jre
-# TODO: this should really be done with a package. 
+# TODO: this should really be done with a package.
 
   wget::fetch { 'logstash-monolithic':
-    source => "https://logstash.objects.dreamhost.com/release/logstash-1.1.9-monolithic.jar",
+    source      => "https://logstash.objects.dreamhost.com/release/logstash-1.1.9-monolithic.jar",
     destination => "/var/tmp/logstash-1.1.9-monolithic.jar",
-    require            => Class['java::oracle7::jre'],
+    require     => Class['java::oracle7::jre'],
   }
   class { 'logstash':
-    provider => 'custom',
-    jarfile  => 'file:///var/tmp/logstash-1.1.9-monolithic.jar',
+    provider    => 'custom',
+    jarfile     => 'file:///var/tmp/logstash-1.1.9-monolithic.jar',
     installpath => '/srv/logstash',
-    require => Wget::Fetch['logstash-monolithic'],
+    require     => Wget::Fetch['logstash-monolithic'],
   }
 
 #configure logstash inputs
   logstash::input::tcp {'syslog':
     type => 'syslog',
     port => '5544',
-  } 
+  }
   logstash::input::udp {'syslog':
     type => 'syslog',
     port => '5544',
-  } 
-#configure logstash filters
-  logstash::filter::grok {'syslog':
-    type => 'syslog',
-      pattern => [ "<%{POSINT:syslog_pri}>%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} %{PROG:syslog_program}(?:\[%{POSINT:syslog_pid}\])?: %{GREEDYDATA:syslog_message}" ],
-      add_field => { "received_at" => "%{@timestamp}", "received_from" => "%{@source_host}" },
-      order => '11',
-}
-  logstash::filter::syslog_pri {'syslog':
-    type => 'syslog',
-      order => '12',
-}
-  logstash::filter::date {'syslog':
-    type => 'syslog',
-    match => [ "syslog_timestamp", "MMM dd HH:mm:ss",
-          "MMM  d HH:mm:ss" ],
-      order => '13',
-}
-  logstash::filter::mutate {'syslog-1':
-    type => 'syslog',
-      order => '14',
-exclude_tags => [ "_grokparsefailure" ],
-replace => { "@source_host" => "%{syslog_hostname}", "@message" => "%{syslog_message}" },
-}
-
-  logstash::filter::mutate {'syslog-2':
-    type => 'syslog',
-      order => '15',
-remove => [ "syslog_hostname", "syslog_message", "syslog_timestamp" ],
-}
-
-
-#configure logstash outputs
-  logstash::output::elasticsearch_http {'syslog':
-    host => "elasticsearch-1.management",
-    index => "logs-current",
   }
 
-  
+#configure logstash filters
+  logstash::filter::grok {'syslog':
+    type      => 'syslog',
+    pattern   => [ "<%{POSINT:syslog_pri}>%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} %{PROG:syslog_program}(?:\[%{POSINT:syslog_pid}\])?: %{GREEDYDATA:syslog_message}" ],
+    add_field => {
+      "received_at"   => "%{@timestamp}",
+      "received_from" => "%{@source_host}"
+      },
+    order     => '11',
+  }
+  logstash::filter::syslog_pri {'syslog':
+    type  => 'syslog',
+    order => '12',
+  }
+  logstash::filter::date {'syslog':
+    type  => 'syslog',
+    match => [ "syslog_timestamp", "MMM dd HH:mm:ss",
+          "MMM  d HH:mm:ss" ],
+    order => '13',
+  }
+  logstash::filter::mutate {'syslog-1':
+    type         => 'syslog',
+    order        => '14',
+    exclude_tags => [ "_grokparsefailure" ],
+    replace      => {
+      "@source_host" => "%{syslog_hostname}",
+      "@message"     => "%{syslog_message}"
+      },
+  }
+  logstash::filter::mutate {'syslog-2':
+    type   => 'syslog',
+    order  => '15',
+    remove => [ "syslog_hostname", "syslog_message", "syslog_timestamp" ],
+  }
+
+#configure logstash outputs
+
+  logstash::output::elasticsearch_http {'syslog':
+    host  => "elasticsearch-1.management",
+    index => "logs-current",
+  }
 
 }
