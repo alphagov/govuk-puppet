79db07d4f95c363ad8d55e8dbc3518bc9c76fd94
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-27 17:23:23 +0000

    Management slave shouldn't include all of management

--- modules/govuk/manifests/node/s_management_slave.pp
+++ modules/govuk/manifests/node/s_management_slave.pp
@@ -1,5 +1,5 @@
 class govuk::node::s_management_slave inherits govuk::node::s_base {
-  include govuk::node::s_management
+  include govuk::node::s_management_base
   include jenkins::slave
 
   ssh_authorized_key { 'management_server_master':
