2774dd3f204dd92876dd2db2c54089b67b556243
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-02-27 11:45:01 +0000

    [#45176853] actually renaming the class helps.

--- modules/govuk/manifests/node/s_logging_elasticsearch.pp
+++ modules/govuk/manifests/node/s_logging_elasticsearch.pp
@@ -1,4 +1,4 @@
-class govuk::node::s_elasticsearch inherits govuk::node::s_base {
+class govuk::node::s_logging_elasticsearch inherits govuk::node::s_base {
 
   $es_heap_size = $::memtotalmb / 4 * 3
 
