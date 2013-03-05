0463b4d36cb2e74606f32ee9d9d4b9f887e74acc
Dan Carley <dan.carley@gmail.com>
2013-03-05 13:57:29 +0000

    [#45048111] Import logster v0.0.1
    Importing the codebase of logster from it's v0.0.1 tag on github:etsy/logster
    
    This is presently unreferenced by the Puppet manifests. It will be used as a
    base for any in-house customisations that we need to port over, then the
    manifests can be switched over and the new version used.

---
+++ modules/logster/files/usr/share/logster/logster_helper.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+###
+###  Copyright 2011, Etsy, Inc.
+###
+###  This file is part of Logster.
+###  
+###  Logster is free software: you can redistribute it and/or modify
+###  it under the terms of the GNU General Public License as published by
+###  the Free Software Foundation, either version 3 of the License, or
+###  (at your option) any later version.
+###  
+###  Logster is distributed in the hope that it will be useful,
+###  but WITHOUT ANY WARRANTY; without even the implied warranty of
+###  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+###  GNU General Public License for more details.
+###  
+###  You should have received a copy of the GNU General Public License
+###  along with Logster. If not, see <http://www.gnu.org/licenses/>.
+###
+
+from time import time
+
+class MetricObject(object):
+    """General representation of a metric that can be used in many contexts"""
+    def __init__(self, name, value, units='', type='float'):
+        self.name = name
+        self.value = value
+        self.units = units
+        self.type = type
+        self.timestamp = int(time())
+
+class LogsterParser(object):
+    """Base class for logster parsers"""
+    def parse_line(self, line):
+        """Take a line and do any parsing we need to do. Required for parsers"""
+        raise RuntimeError, "Implement me!"
+
+    def get_state(self, duration):
+        """Run any calculations needed and return list of metric objects"""
+        raise RuntimeError, "Implement me!"
+
+
+class LogsterParsingException(Exception):
+    """Raise this exception if the parse_line function wants to
+        throw a 'recoverable' exception - i.e. you want parsing
+        to continue but want to skip this line and log a failure."""
+    pass
+
+class LockingError(Exception):
+    """ Exception raised for errors creating or destroying lockfiles. """
+    pass
+
