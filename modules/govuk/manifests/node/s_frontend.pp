604dc4486db5a79ffd70ce7e31dd55d1f247ca15
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2013-03-05 10:48:53 +0000

    Set up limelight web app config

--- modules/govuk/manifests/node/s_frontend.pp
+++ modules/govuk/manifests/node/s_frontend.pp
@@ -12,6 +12,7 @@ class govuk::node::s_frontend inherits govuk::node::s_base {
     'govuk::apps::feedback':              vhost_protected => $protect_fe;
     'govuk::apps::frontend':              vhost_protected => $protect_fe;
     'govuk::apps::licencefinder':         vhost_protected => $protect_fe;
+    'govuk::apps::limelight':             vhost_protected => true;
     'govuk::apps::smartanswers':          vhost_protected => $protect_fe;
     'govuk::apps::tariff':                vhost_protected => $protect_fe;
   }
