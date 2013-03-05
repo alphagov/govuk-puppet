064e643f33067d7ae2d5174935ddd8eab5bc3850
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2013-03-05 09:57:22 +0000

    Correct ports (3037 was already taken)

--- modules/performance_platform/manifests/backstage.pp
+++ modules/performance_platform/manifests/backstage.pp
@@ -4,13 +4,13 @@ class performance_platform::backstage {
 
   govuk::app { "read.backstage":
     app_type           => 'procfile',
-    port               => 3037,
+    port               => 3038,
     enable_nginx_vhost => false,
   }
 
   govuk::app { "write.backstage":
     app_type           => 'procfile',
-    port               => 3038,
+    port               => 3039,
     enable_nginx_vhost => false,
   }
 }
