32ede73ad3be783c48de3618f69c7db8a078866a
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-03-06 09:46:14 +0000

    Pass worldwide-org API requests to whitehall
    This part of the API is hosted by Whitehall, so requests intended for
    /api/worldwide-organisations should fall through to there (so basically
    just like specialist guidance).
    
    https://www.pivotaltracker.com/story/show/43754419

--- modules/govuk/manifests/apps/publicapi.pp
+++ modules/govuk/manifests/apps/publicapi.pp
@@ -25,7 +25,7 @@ class govuk::apps::publicapi {
       # requests made to that path without a trailing slash, *if* there is a
       # proxy_pass directive in the block.
 
-      location ~ ^/api/specialist(/|$) {
+      location ~ ^/api/(specialist|worldwide-organisations)(/|$) {
         proxy_set_header Host ${whitehallapi};
         proxy_pass http://${whitehallapi};
       }
