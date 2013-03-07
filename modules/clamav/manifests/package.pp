e6a67fd290156f57dcc92f9f5d5fa6d712185c54
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-03-07 11:29:56 +0000

    Create govuk_clamscan symlink
    Points as either clamscan or clamdscan depending on whether the daemon is enabled.

--- modules/clamav/manifests/package.pp
+++ modules/clamav/manifests/package.pp
@@ -2,4 +2,14 @@ class clamav::package {
   package { 'clamav':
     ensure => '0.97.5',
   }
+
+  $symlink_target = $::govuk_platform ? {
+    'development' => "/opt/clamav/bin/clamscan",
+    default       => "/opt/clamav/bin/clamdscan",
+  }
+  file { '/usr/local/bin/govuk_clamscan':
+    ensure  => symlink,
+    target  => $symlink_target,
+    require => Package['clamav'],
+  }
 }
