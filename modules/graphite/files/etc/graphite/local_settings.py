36e0bb9ff2ee9917b1b6b02f6a0c27dc84128ec4
Dan Carley <dan.carley@gmail.com>
2013-03-07 07:48:29 +0000

    UTC all of the Graphites!
    Set Graphite's timezone to UTC. This matches all of our servers, and thus
    the metric and log timestamps that they generate. It will ensure that we
    have a consistent view of data when BST rolls in again at the end of this
    month.

--- modules/graphite/files/etc/graphite/local_settings.py
+++ modules/graphite/files/etc/graphite/local_settings.py
@@ -8,7 +8,7 @@ from os.path import dirname, abspath
 # Set your local timezone (django will *try* to figure this out automatically)
 # If your graphs appear to be offset by a couple hours then this probably
 # needs to be explicitly set to your local timezone.
-TIME_ZONE = 'Europe/London'
+TIME_ZONE = 'UTC'
 
 # Override this if you need to provide documentation specific to your graphite deployment
 #DOCUMENTATION_URL = "http://wiki.mycompany.com/graphite"
