d2c040abb0b1f21a64b70cd6ea5b90ddde5b673f
Dan Carley <dan.carley@gmail.com>
2013-03-07 17:05:27 +0000

    [#45757679] Refactor postfix (SES smarthosting)
    This isolates the smarthosting functionality and will allow us to use the
    same class on all hosts. As an aside, it also makes it agnostic SES,
    allowing us to switch to another SMTP relay if we need to.
    
    Variable lookups in templates from `postfix::postmapfile` are now qualified
    back to `postfix::config` to prevent scope warnings. Tests should catch
    these ever being changed in the class.
    
    Should be a no-op on existing nodes.

--- modules/postfix/manifests/config.pp
+++ modules/postfix/manifests/config.pp
@@ -1,17 +1,24 @@
-class postfix::config {
-
-  $amazon_ses_key    = extlookup('amazon_ses_key','NO_SES_KEY')
-  $amazon_ses_secret = extlookup('amazon_ses_secret','NO_SES_SECRET')
-  $email_list        = extlookup('email_list','noemail')
-  $email_domain      = extlookup('email_domain','localhost')
+class postfix::config(
+  $smarthost,
+  $smarthost_user,
+  $smarthost_pass
+) {
 
   file { "/etc/postfix/main.cf":
     content => template("postfix/etc/postfix/main.cf.erb"),
     notify  => Service[postfix]
   }
 
-  postfix::postmapfile { 'sasl_passwd':           name => 'sasl_passwd' }
-  postfix::postmapfile { 'outbound_rewrites':     name => 'outbound_rewrites' }
-  postfix::postmapfile { 'local_remote_rewrites': name => 'local_remote_rewrites' }
+  if $smarthost {
+    $email_list   = extlookup('email_list', 'noemail')
+    $email_domain = extlookup('email_domain', 'localhost')
+
+    postfix::postmapfile { 'outbound_rewrites':     name => 'outbound_rewrites' }
+    postfix::postmapfile { 'local_remote_rewrites': name => 'local_remote_rewrites' }
+
+    if ($smarthost_user and $smarthost_pass) {
+      postfix::postmapfile { 'sasl_passwd': name => 'sasl_passwd' }
+    }
+  }
 
 }
