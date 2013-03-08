8cd383cd465a5f77b0f7766a99dd347c23d07d95
Dan Carley <dan.carley@gmail.com>
2013-03-08 16:03:22 +0000

    [#45755807] collectd::plugin::process wrapper
    Wrapper for monitoring additional processes. Intended as a replacement for
    Ganglia's procstat script.

---
+++ modules/collectd/spec/defines/collectd_plugin_process_spec.rb
@@ -0,0 +1,41 @@
+require_relative '../../../../spec_helper'
+
+describe 'collectd::plugin::process', :type => :define do
+  let(:pre_condition) { 'Collectd::Plugin <||>' }
+
+  describe 'when title is valid' do
+    let(:title) { 'giraffe' }
+
+    it {
+        should contain_collectd__plugin('process-giraffe').with_content(<<EOS
+<Plugin Processes>
+  Process "giraffe"
+</Plugin>
+EOS
+      )}
+
+    describe 'regex is provided' do
+      let(:params) {{
+        :regex => '^gi.*fe$',
+      }}
+
+      it {
+        should contain_collectd__plugin('process-giraffe').with_content(<<EOS
+<Plugin Processes>
+  ProcessMatch "giraffe" "^gi.*fe$"
+</Plugin>
+EOS
+      )}
+    end
+  end
+
+  describe 'when title is invalid' do
+    let(:title) { 'this makes for a bad filename' }
+
+    it do
+      expect {
+        should contain_collectd__plugin('process-giraffe')
+      }.to raise_error(Puppet::Error, /validate_re/)
+    end
+  end
+end
