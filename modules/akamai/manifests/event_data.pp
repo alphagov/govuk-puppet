8416799d3921b618926ea891cfe20ce4b7c8e69e
Garima Singh <igarimasingh@gmail.com>
2013-03-11 09:42:50 +0000

    Moved Akamai script to /usr/local/akamai/bin dir
    * Moved the python script to /usr/local/akamaibin folder
    * Added correct permissions

--- modules/akamai/manifests/event_data.pp
+++ modules/akamai/manifests/event_data.pp
@@ -1,21 +1,33 @@
+# == Class: akamai::event_data
+#
+# Used to log akamai event data, i.e.
+# EdgeControl actions, including login date and time, and session information
+#
 class akamai::event_data {
 
-  $akamai_webservice_username = extlookup("akamai_webservice_username")
-  $akamai_webservice_password = extlookup("akamai_webservice_password")
+  $akamai_webservice_username = extlookup('akamai_webservice_username')
+  $akamai_webservice_password = extlookup('akamai_webservice_password')
+  $akamai_script_dir = '/usr/local/akamai'
 
   package { 'suds':
-    ensure => present,
+    ensure   => present,
     provider => 'pip',
   }
 
-  file { "/var/lib/akamai":
+  file { $akamai_script_dir:
     ensure => directory,
   }
 
-  file { "/var/lib/akamai/pull_event_data_access_logs.py":
-    content => template("akamai/pull_event_data_access_logs.py.erb"),
-    ensure => present,
-    require => [Package['suds'], File['/var/lib/akamai']],
+  file { "${akamai_script_dir}/bin":
+    ensure  => directory,
+    require => File[$akamai_script_dir]
+  }
+
+  file { "${akamai_script_dir}/bin/pull_event_data_access_logs.py":
+    ensure  => present,
+    content => template('akamai/pull_event_data_access_logs.py.erb'),
+    require => [Package['suds'], File["${akamai_script_dir}/bin"]],
+    mode    => '0700',
   }
 
 }
