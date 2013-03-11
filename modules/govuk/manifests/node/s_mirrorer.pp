e777f9f5507c3180fe9a25643f1fb07653e4dbdd
Garima Singh <igarimasingh@gmail.com>
2013-03-11 16:53:49 +0000

    [#39459643] Feature toggle akamai logs
    - Jim/Garima

--- modules/govuk/manifests/node/s_mirrorer.pp
+++ modules/govuk/manifests/node/s_mirrorer.pp
@@ -1,4 +1,10 @@
 class govuk::node::s_mirrorer inherits govuk::node::s_base {
+  $enable_akamai_event_data_logging = str2bool(extlookup('enable_akamai_event_data_logging', "no"))
+
   include mirror
-  include akamai::event_data
+
+  if $enable_akamai_event_data_logging {
+    notice("Oops this is not what I EXPECT")
+    include akamai::event_data
+  }
 }
