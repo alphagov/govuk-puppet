ecf96e94f324c23191052a5af0d05bc18cc469fc
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-03-13 12:08:50 +0000

    [#45064809] output duration of the vius scan run

--- modules/clamav/files/usr/local/bin/virus_scan.sh
+++ modules/clamav/files/usr/local/bin/virus_scan.sh
@@ -4,6 +4,7 @@ INCOMING_DIR="$1"
 INFECTED_DIR="$2"
 CLEAN_DIR="$3"
 CLAMSCAN_CMD="/opt/clamav/bin/clamscan"
+START_TIME=`date +%s`
 
 usage() {
   echo "USAGE: $0 <incoming_dir> <infected_dir> [<clean_dir>]"
@@ -51,12 +52,14 @@ make_result_file
 $CLAMSCAN_CMD --no-summary --stdout -r . > "$RESULTFILE"
 set -o errexit # enable fail on errors
 
-logger -t virus_scan "`grep 'FOUND$' "$RESULTFILE" | wc -l` viruses found in $INCOMING_DIR"
-logger -t virus_scan "`grep ': OK$' "$RESULTFILE" | wc -l` clean found in $INCOMING_DIR"
-
 grep 'FOUND$' "$RESULTFILE" | sed 's/: [^:]* FOUND$//' | rsync --remove-source-files --files-from=- . "$INFECTED_DIR/."
 if [ -n "$CLEAN_DIR" ]; then
   grep ': OK$' "$RESULTFILE" | sed 's/: OK$//' | rsync --remove-source-files --files-from=- . "$CLEAN_DIR/."
 fi
 rm $RESULTFILE
+
+logger -t virus_scan "`grep 'FOUND$' "$RESULTFILE" | wc -l` viruses found in $INCOMING_DIR"
+logger -t virus_scan "`grep ': OK$' "$RESULTFILE" | wc -l` clean found in $INCOMING_DIR"
+logger -t virus_scan "virus scan took T:$((`date +%s`-START_TIME)) seconds"
+
 popd >/dev/null
