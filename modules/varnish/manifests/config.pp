46731b40ff3a743697b83e30236ff2e5db8c1a2e
Jordan Hatch <jordan@jordanh.net>
2013-03-13 14:17:43 +0000

    Fix transaction wrapper Varnish routes
    Use variable in manifest to determine whether 
    transaction wrapper routes should exist.

--- modules/varnish/manifests/config.pp
+++ modules/varnish/manifests/config.pp
@@ -74,6 +74,7 @@ class varnish::config {
   ]
 
   $app_domain = extlookup('app_domain')
+  $transaction_wrappers_enabled = str2bool(extlookup('govuk_enable_transaction_wrappers', 'no'))
 
   file { '/etc/default/varnish':
     ensure  => file,
