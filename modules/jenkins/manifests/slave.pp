d607feb87d9bad0402833af0aeee2679276d8930
Max Griffiths <maxgriff@thoughtworks.com>
2013-03-13 16:06:32 +0000

    Fixing licensify builds by increasing ulimit -u

--- modules/jenkins/manifests/slave.pp
+++ modules/jenkins/manifests/slave.pp
@@ -1,2 +1,10 @@
 class jenkins::slave inherits jenkins {
+
+    harden::limit { 'jenkins-slave-nproc':
+      domain => 'jenkins',
+      type   => '-', # set both hard and soft limits
+      item   => 'nproc',
+      value  => '1024',
+    }
+
 }
