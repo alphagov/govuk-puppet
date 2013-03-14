c13fd8946050805651604d5c2c1102ce24c7fbdf
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-03-14 18:22:11 +0000

    Add a govuk_node_clean script
    I keep forgetting how to remove a node's certs and stored configs from the
    environment. Add a govuk_node_clean script to wrap this up in one place. (And
    provide somewhere to add more hooks if necessary in future.)

--- modules/govuk/manifests/scripts.pp
+++ modules/govuk/manifests/scripts.pp
@@ -8,4 +8,12 @@ class govuk::scripts {
     mode    => '0755',
   }
 
+  # govuk_node_clean removes stale or decommissioned nodes from puppetdb and the
+  # puppetmaster
+  file { '/usr/local/bin/govuk_node_clean':
+    ensure  => present,
+    source  => 'puppet:///modules/govuk/bin/govuk_node_clean',
+    mode    => '0755',
+  }
+
 }
