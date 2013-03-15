c914c36ec64083d7255b27f161ccee4d8c516ac2
Garima Singh <igarimasingh@gmail.com>
2013-03-15 11:59:14 +0000

    [#46021167] used logstream to stream mysql error log

--- modules/mysql/manifests/server/service.pp
+++ modules/mysql/manifests/server/service.pp
@@ -3,4 +3,10 @@ class mysql::server::service {
     ensure     => running,
     status     => '/etc/init.d/mysql status | grep "mysql start"'
   }
+
+  govuk::logstream { 'mysql-error-logs':
+    logfile => '/var/log/mysql/error.log',
+    tags    => ['MYSQL', 'ERROR'],
+    enable  => true,
+  }
 }
