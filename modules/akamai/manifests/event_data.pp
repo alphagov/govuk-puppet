16f679877e985315c235a6edb252133e4546f117
Dan Carley <dan.carley@gmail.com>
2013-03-18 09:29:19 +0000

    [#46020681] Allow akamai::event_data toggle off
    Functionality for `akamai::event_data` to be feature toggled off and clear
    up after itself. The class will always be included on the mirrorer nodes,
    but the default feature toggle will disable it.

--- modules/akamai/manifests/event_data.pp
+++ modules/akamai/manifests/event_data.pp
@@ -3,21 +3,40 @@
 # Used to log akamai event data, i.e.
 # EdgeControl actions, including login date and time, and session information
 #
-class akamai::event_data {
+# === Parameters
+#
+# [*enable*]
+#   Whether the crontab should be enabled. It's not desireable to collect
+#   logs in/from all environments.
+#   Default: true
+#
+class akamai::event_data(
+  $enable = true
+) {
+  $ensure_real = $enable ? {
+    /^false$/ => absent,
+    default   => present,
+  }
+  $ensure_dir = $enable ? {
+    /^false$/ => undef,
+    default   => directory,
+  }
+
   include python::suds
   include python::pyyaml
 
-  $akamai_webservice_username = extlookup('akamai_webservice_username')
-  $akamai_webservice_password = extlookup('akamai_webservice_password')
+  $akamai_webservice_username = extlookup('akamai_webservice_username', 'UNSET')
+  $akamai_webservice_password = extlookup('akamai_webservice_password', 'UNSET')
 
   $akamai_script_dir = '/usr/local/akamai'
   $akamai_script = "${akamai_script_dir}/pull_akamai_event_data.py"
 
   file { $akamai_script_dir:
-    ensure => directory,
+    ensure => $ensure_dir,
   }
 
   file { "${akamai_script_dir}/akamai_logs.yaml":
+    ensure  => $ensure_real,
     content => template('akamai/akamai_logs.yaml.erb'),
     mode    => '0600',
   }
@@ -28,14 +47,14 @@ class akamai::event_data {
   }
 
   file { $akamai_script:
-    ensure  => present,
+    ensure  => $ensure_real,
     source  => 'puppet:///modules/akamai/pull_akamai_event_data.py',
     require => File[$akamai_script_dir],
     mode    => '0700',
   }
 
   cron { 'pull_akamai_event_data':
-    ensure  => present,
+    ensure  => $ensure_real,
     command => "cd ${akamai_script_dir}; ./pull_akamai_event_data.py | /usr/bin/logger -t akamai_event_data",
     hour    => '1',
     require => File[$akamai_script],
--- modules/govuk/manifests/node/s_mirrorer.pp
+++ modules/govuk/manifests/node/s_mirrorer.pp
@@ -3,7 +3,7 @@ class govuk::node::s_mirrorer inherits govuk::node::s_base {
 
   include mirror
 
-  if $enable_akamai_event_data_logging {
-    include akamai::event_data
+  class { 'akamai::event_data':
+    enable  => $enable_akamai_event_data_logging,
   }
 }
