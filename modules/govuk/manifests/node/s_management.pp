30da791fb8971ffbd2b114046374c26df6d2a861
Dan Carley <dan.carley@gmail.com>
2013-03-19 11:15:16 +0000

    [#46035427] Change default cert for ci.alphagov
    Change the certificate of default vhost on CI nodes to match that of
    `ci.alphagov.co.uk` so that non-SNI clients (such as
    `alphagov/github-jenkins-trigger`) are able to connect.

--- modules/govuk/manifests/node/s_management.pp
+++ modules/govuk/manifests/node/s_management.pp
@@ -4,10 +4,12 @@ class govuk::node::s_management inherits govuk::node::s_management_base {
 
   include apache::remove
 
-  # Close connection if vhost not known
   nginx::config::vhost::default { 'default':
+    # Close connection if vhost not known
     status         => '444',
     status_message => '',
+    # Match vhost below for non-SNI clients.
+    ssl_certtype   => 'ci_alphagov',
   }
 
   nginx::config::ssl { 'jenkins':
