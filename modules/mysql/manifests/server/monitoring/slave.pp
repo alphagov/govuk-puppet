4b7ab9389b85282eb2b3188f5ee8f3589c0edb54
Dan Carley <dan.carley@gmail.com>
2013-03-26 10:18:46 +0000

    [#46886367] Drop last val on MySQL repl check
    Drop the last value from `mysql.time_offset` when querying Graphite for
    Nagios checks. This prevents the very latest value from frequently being
    caught by `transformNull()` when each 10s time slice rolls over.

--- modules/mysql/manifests/server/monitoring/slave.pp
+++ modules/mysql/manifests/server/monitoring/slave.pp
@@ -4,7 +4,7 @@ class mysql::server::monitoring::slave inherits mysql::server::monitoring {
   }
 
   @@nagios::check { "check_mysql_replication_${::hostname}":
-    check_command       => "check_graphite_metric!transformNull(${::fqdn_underscore}.mysql.time_offset,86400)!300!600",
+    check_command       => "check_graphite_metric_args!transformNull(${::fqdn_underscore}.mysql.time_offset,86400)!300!600!--droplast 1",
     service_description => "mysql replication lag",
     host_name           => $::fqdn,
   }
