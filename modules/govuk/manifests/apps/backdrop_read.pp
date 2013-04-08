2e5610a6af137ccadaddfe0fefd9f80c284a8b04
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-04-08 10:06:22 +0100

    Fix style errors

--- modules/govuk/manifests/apps/backdrop_read.pp
+++ modules/govuk/manifests/apps/backdrop_read.pp
@@ -2,9 +2,9 @@ class govuk::apps::backdrop_read (
   $port = 3038,
   $vhost_protected
 ) {
-	govuk::app { 'read.backdrop':
-		app_type        => 'procfile',
-		port            => $port,
-		vhost_protected => $vhost_protected
-	}
+  govuk::app { 'read.backdrop':
+    app_type        => 'procfile',
+    port            => $port,
+    vhost_protected => $vhost_protected
+  }
 }
--- modules/govuk/manifests/apps/backdrop_write.pp
+++ modules/govuk/manifests/apps/backdrop_write.pp
@@ -2,9 +2,9 @@ class govuk::apps::backdrop_write (
   $port = 3039,
   $vhost_protected
 ) {
-	govuk::app { 'write.backdrop':
-		app_type        => 'procfile',
-		port            => $port,
-		vhost_protected => $vhost_protected
-	}
+  govuk::app { 'write.backdrop':
+    app_type        => 'procfile',
+    port            => $port,
+    vhost_protected => $vhost_protected
+  }
 }
