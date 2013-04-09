dfefc782b8803646a07f29f12588deca9a189f10
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-04-09 11:55:41 +0100

    Add healthcheck to govuk-delivery

--- modules/govuk/manifests/apps/govuk_delivery.pp
+++ modules/govuk/manifests/apps/govuk_delivery.pp
@@ -3,8 +3,9 @@ class govuk::apps::govuk_delivery( $port = 3042 ) {
   include virtualenv
 
   govuk::app { 'govuk-delivery':
-    app_type        => 'procfile',
-    port            => $port,
-    vhost_ssl_only  => true,
+    app_type          => 'procfile',
+    port              => $port,
+    vhost_ssl_only    => true,
+    health_check_path => '/_status',
   }
 }
