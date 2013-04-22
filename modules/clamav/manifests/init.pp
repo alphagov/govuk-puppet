8e5717cf35ccea8a0a156abd4ba588c46c01945f
Dan Carley <dan.carley@gmail.com>
2013-04-22 16:10:07 +0100

    Run freshclam before starting clamav-daemon
    clamav-daemon was failing to install and start within the same run because
    it relied on data which was later populated by freshclam. Triggering a run
    on package install ensures that the databases are fetched before the service
    starts.

--- modules/clamav/manifests/init.pp
+++ modules/clamav/manifests/init.pp
@@ -2,18 +2,32 @@ class clamav {
   anchor { 'clamav::begin':
     notify  => Class['clamav::service'],
   }
+
   class { 'clamav::package':
-    notify  => Class['clamav::config'],
+    notify  => Class[
+      'clamav::config',
+      'clamav::run_freshclam'
+    ],
     require => Anchor['clamav::begin'],
   }
+
   class { 'clamav::config':
     notify  => Class['clamav::service'],
     require => Class['clamav::package'],
   }
-  class { 'clamav::service': }
+
+  class { 'clamav::run_freshclam':
+    require => Class['clamav::config'],
+  }
+
+  class { 'clamav::service':
+    require => Class['clamav::run_freshclam'],
+  }
+
   class { 'clamav::monitoring':
     require => Class['clamav::config'],
   }
+
   anchor { 'clamav::end':
     require => Class[
       'clamav::service',
