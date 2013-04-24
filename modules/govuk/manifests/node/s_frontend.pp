3bace95d2830f5061a1ec97a35e4589b06e8b17a
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-04-24 15:35:14 +0100

    [#47792685] alert for high write connections on NGINX frontend

--- modules/govuk/manifests/node/s_frontend.pp
+++ modules/govuk/manifests/node/s_frontend.pp
@@ -39,4 +39,11 @@ class govuk::node::s_frontend inherits govuk::node::s_base {
 
   # If we miss all the apps, throw a 500 to be caught by the cache nginx
   nginx::config::vhost::default { 'default': }
+
+  @@nagios::check::graphite { "check_nginx_connections_writing_${::hostname}":
+    target   => "${::fqdn_underscore}.nginx.nginx_connections-writing",
+    desc     => 'nginx high conn writing - upstream indicator',
+    warning  => 150,
+    critical => 250,
+  }
 }
