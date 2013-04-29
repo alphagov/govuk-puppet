11aa18be5646f8e5ce24e09f23eeefebee24cdb8
Dan Carley <dan.carley@gmail.com>
2013-04-29 17:53:06 +0100

    [#48916131] Revert licensing maintenance hacks
    Single purpose. Not re-usable for anything else. Get rid of it.
    
    Reverts commits:
    
    - f0770a7
    - 409baae
    - 95d322e
    - 2e8c42e

--- modules/govuk/manifests/node/s_licensify_lb.pp
+++ modules/govuk/manifests/node/s_licensify_lb.pp
@@ -7,49 +7,25 @@ class govuk::node::s_licensify_lb {
   $licensify_backend_servers = ["licensify-backend-1", "licensify-backend-2"]
   $enable_feed_console = str2bool(extlookup('govuk_enable_licensify_feed_console','no'))
 
-  # FIXME: Revert this hack after Licensify maintenance (30/04/13)
-  $licensify_maintenance_mode = str2bool(extlookup('licensify_maintenance_mode','no'))
-  if $licensify_maintenance_mode {
-    $licensify_frontend_maintenance_file = 'licensify_frontend_maintenance.html'
-    $licensify_backend_maintenance_file  = 'licensify_backend_maintenance.html'
-  } else {
-    $licensify_frontend_maintenance_file = undef
-    $licensify_backend_maintenance_file  = undef
-  }
-
-  file {
-    '/usr/share/nginx/html/licensify_frontend_maintenance.html':
-        ensure => present,
-        source => 'puppet:///modules/govuk/node/s_licensify_lb/licensify_frontend_maintenance.html',
-        before => Loadbalancer::Balance['licensify', 'uploadlicence'];
-    '/usr/share/nginx/html/licensify_backend_maintenance.html':
-        ensure => present,
-        source => 'puppet:///modules/govuk/node/s_licensify_lb/licensify_backend_maintenance.html',
-        before => Loadbalancer::Balance['licensify-admin'];
-  }
-
   loadbalancer::balance {
     # Licensify frontend
     'licensify':
-      maintenance   => $licensify_frontend_maintenance_file,
       servers       => $licensify_frontend_servers,
       internal_only => true;
 
     # Licensify upload pdf public endpoint
     'uploadlicence':
-      maintenance   => $licensify_frontend_maintenance_file,
       https_only    => true,
       internal_only => true,
       servers       => $licensify_frontend_servers;
 
     # Licensify admin interface
     'licensify-admin':
-      maintenance   => $licensify_backend_maintenance_file,
       https_only    => true,
       internal_only => true,
       servers       => $licensify_backend_servers;
-  }
 
+  }
   if ($enable_feed_console) {
     loadbalancer::balance {
       # Licensify feed frontend
--- modules/loadbalancer/manifests/balance.pp
+++ modules/loadbalancer/manifests/balance.pp
@@ -22,13 +22,6 @@
 #   Limit access to the loadbalanced service to internal IP address only.
 #   Default: false
 #
-# [*maintenance*]
-#   File path under `/usr/share/nginx/html` which will be served with a 503
-#   response for all requests. Intended to be triggered with a feature flag
-#   by the module caller.
-#   FIXME: Revert this hack after Licensify maint (30/04/13)
-#   Default: ''
-#
 define loadbalancer::balance(
     $servers,
     $aliases = [],
@@ -36,8 +29,8 @@ define loadbalancer::balance(
     $internal_only = false,
     $vhost = $title,
     $read_timeout = 60,
-    $maintenance = ''
 ) {
+
   $vhost_suffix = extlookup('app_domain')
   $vhost_real = "${vhost}.${vhost_suffix}"
 
