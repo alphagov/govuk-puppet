3ab5f5679957efb8c302a9c55eaf8888b64893f5
Garima Singh <igarimasingh@gmail.com>
2013-04-29 16:38:04 +0100

    Added document url for nagios check for elasticsearch gc timings alert

--- modules/elasticsearch/manifests/monitoring.pp
+++ modules/elasticsearch/manifests/monitoring.pp
@@ -6,7 +6,7 @@ class elasticsearch::monitoring {
     warning          => 50,
     critical         => 250,
     host_name        => $::fqdn,
-    document_url     => '',
+    document_url     => 'https://sites.google.com/a/digital.cabinet-office.gov.uk/wiki/projects-and-processes/gov-uk/ops-manual/nagios-alerts-documentation-actions#TOC-Prolonged-GC-collection-times-Check',
   }
 
 }
