137230e73ca49ff88bdc4b76b38828d2369ffe1f
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-04-30 16:36:42 +0100

    [#47804029] Refactor and remove unnecessary globals

--- modules/nagios/files/usr/local/bin/check_pingdom.py
+++ modules/nagios/files/usr/local/bin/check_pingdom.py
@@ -4,13 +4,9 @@ import ConfigParser
 import json
 from urllib2 import *
 
-# other config
-pingdom_user     = ""
-pingdom_pass     = ""
-pingdom_key      = ""
+# General Config
 pingdom_api_host = "api.pingdom.com"
 config_file      = "/etc/pingdom.ini"
-check_id = 0
 
 def unknown(message):
     print "UNKNOWN: %s" % message
@@ -28,25 +24,29 @@ def ok(message):
     print "OK: %s" % message
     sys.exit(0)
 
-def read_config():
-    try:
-        config = ConfigParser.ConfigParser()
-        config.read(config_file)
-        global pingdom_pass
-        global pingdom_key
-        global pingdom_user
-        pingdom_pass = config.get('DEFAULT','pingdom_pass')
-        pingdom_key  = config.get('DEFAULT','pingdom_key')
-        pingdom_user = config.get('DEFAULT','pingdom_user')
-    except:
-        unknown("Could not read config file " % config_file)
+
+class Config :
+    pingdom_pass = None
+    pingdom_key  = None
+    pingdom_user = None
+
+    def __init__(self) :
+        try:
+            config = ConfigParser.ConfigParser()
+            config.read(config_file)
+            self.pingdom_pass = config.get('DEFAULT','pingdom_pass')
+            self.pingdom_key  = config.get('DEFAULT','pingdom_key')
+            self.pingdom_user = config.get('DEFAULT','pingdom_user')
+        except:
+            unknown("Could not read config file " % config_file)
+
 
 def check_arguments():
     if len(sys.argv) != 2:
         unknown("No check ID passed as a parameter")
     else:
-        global check_id
         check_id = sys.argv[1]
+        return check_id
 
 def check_pingdom_up():
     try:
@@ -56,12 +56,12 @@ def check_pingdom_up():
     except:
         unknown("Pingdom API Down")
 
-def get_pingdom_result():
+def get_pingdom_result(config,check_id):
     try:
-        basic_auth_token = "Basic " + (pingdom_user + ":" + pingdom_pass).encode("base64").rstrip()
+        basic_auth_token = "Basic " + (config.pingdom_user + ":" + config.pingdom_pass).encode("base64").rstrip()
         pingdom_url = "https://%s/api/2.0/checks/%s" % (pingdom_api_host, check_id)
         req = Request(pingdom_url)
-        req.add_header("App-Key", pingdom_key)
+        req.add_header("App-Key", config.pingdom_key)
         req.add_header("Authorization", basic_auth_token)
         try:
             result = urlopen(req)
@@ -83,7 +83,5 @@ def get_pingdom_result():
     except Exception, e:
         unknown("Unknown %s retrieving check status" % e)
 
-check_arguments()
-read_config()
 check_pingdom_up()
-get_pingdom_result()
+get_pingdom_result(Config(),check_arguments())
