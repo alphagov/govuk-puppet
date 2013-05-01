2e5a13b3b732cba20f2f1560f6f92f35e18bf2c3
Dan Carley <dan.carley@gmail.com>
2013-05-01 16:32:17 +0100

    [#48416887] unicorn check_client_connection
    Enable unicorn's `check_client_connection` option for versions of unicorn
    that support it (>= 4.5.0). Raise a warning on STDERR for those that don't.
    This will allow us to progressively upgrade all projects to make use of
    this.
    
    In short, the option will attempt to verify whether a request queued on the
    master process still has a client listening before passing it to a worker.
    This should reduce the load and recovery time when unicorn becomes swamped.
    You can read more about it here:
    
    - http://unicorn.bogomips.org/Unicorn/Configurator.html#method-i-check_client_connection
    - http://www.shopify.co.uk/technology/7535298-what-does-your-webserver-do-when-a-user-hits-refresh
    
    The effectiveness of this may be limited whilst we are using HTTPS for
    internally reverse-proxed connections because isn't able to detect the
    client disconnect so quickly:
    
    - http://nginx.2469901.n2.nabble.com/Dropped-https-client-connection-doesn-t-drop-backend-proxy-pass-connection-td7584346.html
    
    However this is still a step in the right direction and shouldn't cause any
    negative effects.

--- modules/govuk/files/etc/govuk/unicorn.rb
+++ modules/govuk/files/etc/govuk/unicorn.rb
@@ -4,3 +4,9 @@ stderr_path "#{ENV.fetch('GOVUK_APP_LOGROOT')}/app.err.log"
 before_exec do |server|
   ENV["BUNDLE_GEMFILE"] = "#{ENV.fetch('GOVUK_APP_ROOT')}/Gemfile"
 end
+
+if self.respond_to?('check_client_connection')
+  check_client_connection true
+else
+  warn "check_client_connection: will not enabled for Unicorn version < 4.5.0"
+end
