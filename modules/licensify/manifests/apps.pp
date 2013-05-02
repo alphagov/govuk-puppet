9c4473904cc8ca1de0e2d6d31f2ccba313ff2266
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-02 11:07:05 +0100

    [#47368223] Enable auth on licensify preview
    This is a rehash of a previous attempt to do this.
    
    The complication when enabling last time, was that while we want to
    protect /apply-for-a-license we do not want to protect /api as it breaks
    the application completely.
    
    We are using govuk:app to protect the vhost, but introducing additional
    config in an existing snippet file to unprotect /api

--- modules/licensify/manifests/apps.pp
+++ modules/licensify/manifests/apps.pp
@@ -16,9 +16,12 @@ class licensify::apps {
 
 class licensify::apps::licensify( $port = 9000 ) inherits licensify::apps::base {
 
+  $protect_licensify_frontend = str2bool(extlookup('protect_frontend_apps', 'no'))
+
   govuk::app { 'licensify':
     app_type           => 'procfile',
     port               => $port,
+    vhost_protected    => $protect_licensify_frontend,
     nginx_extra_config => template('licensify/nginx_extra'),
     health_check_path  => '/api/licences',
     require            => File['/etc/licensing'],
