a74af4744c7b298d75e2bbfcd1886121462c668d
Dan Carley <dan.carley@gmail.com>
2013-05-09 10:07:06 +0100

    Refactor condition in clamav::service
    Although the previous incantation appeared to work, I'm not sure if it's
    officially supported and it made my brain hurt. Replace it with a selector.

--- modules/clamav/manifests/service.pp
+++ modules/clamav/manifests/service.pp
@@ -1,6 +1,8 @@
 class clamav::service {
-
-  $enable_service = ($::govuk_platform != 'development')
+  $enable_service = $::govuk_platform ? {
+    'development' => stopped,
+    default       => running,
+  }
 
   service { ['clamav-freshclam', 'clamav-daemon']:
     ensure  => $enable_service,
