b38b8ade99d94c835067c3b9a21eab0c01d62806
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-05-10 17:21:59 +0100

    [#49061219] replace our usage of wget::fetch with curl::fetch

--- modules/govuk/manifests/node/s_mapit_server.pp
+++ modules/govuk/manifests/node/s_mapit_server.pp
@@ -6,7 +6,7 @@ class govuk::node::s_mapit_server inherits govuk::node::s_base {
 
   include mapit
 
-  wget::fetch { 'mapit_dbdump_download':
+  curl::fetch { 'mapit_dbdump_download':
     source      => 'http://gds-public-readable-tarballs.s3.amazonaws.com/mapit.sql.gz',
     destination => '/data/vhost/mapit/data/mapit.sql.gz',
     require     => File['/data/vhost/mapit/data'],
@@ -26,7 +26,7 @@ class govuk::node::s_mapit_server inherits govuk::node::s_base {
   postgres::exec { 'zcat -f /data/vhost/mapit/data/mapit.sql.gz | psql':
     database => 'mapit',
     unless   => "psql -Atc \"select count(*) from pg_catalog.pg_tables WHERE tablename='mapit_area'\" | grep -qvF 0",
-    require  => Wget::Fetch['mapit_dbdump_download'],
+    require  => Curl::Fetch['mapit_dbdump_download'],
   }
 
 }
