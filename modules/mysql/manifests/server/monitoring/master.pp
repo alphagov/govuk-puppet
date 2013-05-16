db20b68456bd86937d012d5b582ecf532a03e46b
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-05-16 16:39:23 +0100

    Add MySQL slave Nagios check
    Adds a MySQL user for Nagios. Uses the builtin check_mysql Nagios
    plugin to check that the slave is running. Using the existing collectd
    data for replication lag.

--- modules/mysql/manifests/server/monitoring/master.pp
+++ modules/mysql/manifests/server/monitoring/master.pp
@@ -1,5 +1,14 @@
-class mysql::server::monitoring::master inherits mysql::server::monitoring {
+class mysql::server::monitoring::master ($root_password) inherits mysql::server::monitoring {
   Collectd::Plugin::Mysql['lazy_eval_workaround'] {
     master => true,
+  }
+
+  $nagios_mysql_password = extlookup('mysql_nagios')
+
+  mysql::user { 'nagios':
+    root_password => $root_password,
+    user_password => $nagios_mysql_password,
+    remote_host   => 'localhost',
+    privileges    => 'REPLICATION CLIENT',
   }
 }
--- modules/mysql/manifests/server/monitoring/slave.pp
+++ modules/mysql/manifests/server/monitoring/slave.pp
@@ -12,4 +12,16 @@ class mysql::server::monitoring::slave inherits mysql::server::monitoring {
     args         => '--droplast 1',
     document_url => 'https://sites.google.com/a/digital.cabinet-office.gov.uk/wiki/projects-and-processes/gov-uk/ops-manual/nagios-alerts-documentation-actions#TOC-mysql-replication-lag-Check',
   }
+
+  $nagios_mysql_password = extlookup('mysql_nagios')
+
+  @nagios::nrpe_config { 'check_mysql_slave':
+    content => template('mysql/etc/nagios/nrpe.d/check_mysql_slave.cfg.erb'),
+  }
+
+  @@nagios::check { "check_mysql_slave_${::hostname}":
+    check_command       => 'check_nrpe_1arg!check_mysql_slave',
+    service_description => "check mysql slave is running",
+    host_name           => $::fqdn,
+  }
 }
