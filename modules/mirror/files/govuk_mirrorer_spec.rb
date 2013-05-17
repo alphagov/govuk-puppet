86c9dd83b8445c365c16fba907ee0e66f37821cc
Dan Carley <dan.carley@gmail.com>
2013-05-17 11:20:52 +0100

    [#49910675] Mirrorer handling of 429 responses
    We're moving response codes for rate limiting from 503 to 429. The mirrorer
    attempts to handle these kind of errors gracefully, by backing off for one
    second and then retrying. Maintain this behaviour for the new 429 response.
    
    Test for a handful of the specified codes. Broke handled codes out to a
    class constant because munging a range and an integer became a longer line
    of code.

--- modules/mirror/files/govuk_mirrorer_spec.rb
+++ modules/mirror/files/govuk_mirrorer_spec.rb
@@ -214,27 +214,35 @@ describe GovukMirrorer do
         @m.crawl
       end
 
-      context "5xx error" do
-        it "should sleep for a second, and then retry" do
-          error = Mechanize::ResponseCodeError.new(stub("Page", :code => 503), "Boom")
-          @m.send(:agent).should_receive(:get).with("https://www.example.com/1").ordered.and_raise(error)
-          @m.send(:agent).should_receive(:get).with("https://www.example.com/1").ordered.and_return("page_1")
+      context "error handling" do
+        [
+          [429, "Too Many Requests"],
+          [500, "Internal Server Error"],
+          [503, "Boom"],
+        ].each do |resp_code, resp_reason|
+          context "#{resp_code} #{resp_reason}" do
+            it "should sleep for a second, and then retry" do
+              error = Mechanize::ResponseCodeError.new(stub("Page", :code => resp_code), resp_reason)
+              @m.send(:agent).should_receive(:get).with("https://www.example.com/1").ordered.and_raise(error)
+              @m.send(:agent).should_receive(:get).with("https://www.example.com/1").ordered.and_return("page_1")
 
-          @m.should_not_receive(:add_log_warning)
-          @m.should_receive(:sleep).with(1) # Actually on kernel, but setting the expectation here works
-          @m.should_receive(:process_govuk_page).with("page_1", {})
+              @m.should_not_receive(:add_log_warning)
+              @m.should_receive(:sleep).with(1) # Actually on kernel, but setting the expectation here works
+              @m.should_receive(:process_govuk_page).with("page_1", {})
 
-          @m.crawl
-        end
+              @m.crawl
+            end
 
-        it "should only retry once" do
-          error = Mechanize::ResponseCodeError.new(stub("Page", :code => 503), "Boom")
-          @m.send(:agent).should_receive(:get).with("https://www.example.com/1").twice.and_raise(error)
+            it "should only retry once" do
+              error = Mechanize::ResponseCodeError.new(stub("Page", :code => resp_code), resp_reason)
+              @m.send(:agent).should_receive(:get).with("https://www.example.com/1").twice.and_raise(error)
 
-          @m.should_receive(:sleep).with(1) # Actually on kernel, but setting the expectation here works
-          @m.should_receive(:add_log_warning).with(:url => "https://www.example.com/1", :handler => :process_govuk_page, :error => error, :data => {}).once
+              @m.should_receive(:sleep).with(1) # Actually on kernel, but setting the expectation here works
+              @m.should_receive(:add_log_warning).with(:url => "https://www.example.com/1", :handler => :process_govuk_page, :error => error, :data => {}).once
 
-          @m.crawl
+              @m.crawl
+            end
+          end
         end
       end
     end
