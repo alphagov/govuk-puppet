7e16555be2b31c5e58e465e9a8fccc073b87b230
Dan Carley <dan.carley@gmail.com>
2013-05-17 11:50:07 +0100

    Remove unused variable from s_mysql_slave.

--- modules/govuk/manifests/node/s_mysql_slave.pp
+++ modules/govuk/manifests/node/s_mysql_slave.pp
@@ -1,6 +1,5 @@
 class govuk::node::s_mysql_slave inherits govuk::node::s_base {
   $root_password = extlookup('mysql_root', '')
-  $replica_password = extlookup('mysql_replica_password', '')
 
   include mysql::backup
 
