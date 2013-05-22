c7362d524c9d14c4e76274fb20539283ffdf95fd
Dan Carley <dan.carley@gmail.com>
2013-05-22 15:53:51 +0100

    [#48998005] govuk::deploy unnecessary app restarts
    Break apart `govuk::deploy` into two sub-classes:
    
    1. Setup: resources that are required by `govuk::app` resources, but when
       modified should not affect the operation of a running app. Examples would
       be file permissions on `/data` or new SSH keys.
    
    2. Config: resources which are also required, but when modified should
       trigger a restart of any `govuk::app` resources which called it. Examples
       would be global unicorn options or changes to the `govuk_setenv` script.
    
    This addresses an issue whereby the migration of `ssh_authorized_keys{}` to
    `file{}` resources in ccce1f2 caused *all* Ruby applications to restart
    unnecessarily. In an unfortunate sequence of events we ran Puppet over all
    nodes in a short period of time, which caused a brief outage.

--- modules/govuk/manifests/deploy.pp
+++ modules/govuk/manifests/deploy.pp
@@ -1,8 +1,5 @@
 class govuk::deploy {
-
-  include assets::user
   include bundler
-  include daemontools
   include fpm
   include govuk::logging
   include harden
@@ -11,157 +8,19 @@ class govuk::deploy {
   include virtualenv
   include unicornherder
 
-  group { 'deploy':
-    ensure  => 'present',
-    name    => 'deploy',
+  anchor { 'govuk::deploy::begin': }
+
+  # These resources are required, but should not refresh apps.
+  class { 'govuk::deploy::setup':
+    require => Anchor['govuk::deploy::begin'],
   }
 
-  user { 'deploy':
-    ensure      => present,
-    home        => '/home/deploy',
-    managehome  => true,
-    groups      => ['assets'],
-    shell       => '/bin/bash',
-    gid         => 'deploy',
-    require     => [Group['deploy'],Group['assets']];
+  # These resources should refresh apps.
+  class { 'govuk::deploy::config':
+    require => Class['govuk::deploy::setup'],
   }
 
-  harden::limit { 'deploy-nofile':
-    domain => 'deploy',
-    type   => '-', # set both hard and soft limits
-    item   => 'nofile',
-    value  => '16384',
-  }
-
-  file { '/etc/govuk':
-    ensure => directory,
-  }
-
-  file { '/var/lib/govuk':
-    ensure => directory,
-  }
-
-  file { '/data':
-    ensure  => directory,
-    owner   => 'deploy',
-    group   => 'deploy',
-    require => User['deploy'],
-  }
-
-  file { '/data/vhost':
-    ensure  => directory,
-    owner   => 'deploy',
-    group   => 'deploy',
-    require => File['/data'],
-  }
-
-  file { '/var/apps':
-    ensure => directory,
-  }
-
-  # Referenced by authorized_keys template below.
-  $deploy_ssh_keys = {
-    'jenkins_key'                     => extlookup('jenkins_key', 'NONE_IN_EXTDATA'),
-    'jenkins_skyscape_key'            => extlookup('jenkins_skyscape_key', 'NONE_IN_EXTDATA'),
-    # Production Jenkins sync to other environments.
-    'jenkins_skyscape_production_key' => extlookup('jenkins_skyscape_production_key', 'NONE_IN_EXTDATA'),
-    'deploy_user_data_sync_key'       => extlookup('deploy_user_data_sync_key', 'NONE_IN_EXTDATA'),
-  }
-  file { '/home/deploy/.ssh':
-    ensure => directory,
-    owner  => 'deploy',
-    group  => 'deploy',
-    mode   => '0700',
-  }
-  file { '/home/deploy/.ssh/authorized_keys':
-    ensure  => present,
-    owner   => 'deploy',
-    group   => 'deploy',
-    mode    => '0600',
-    content => template('govuk/home/deploy/.ssh/authorized_keys.erb'),
-  }
-
-  file { '/etc/govuk/unicorn.rb':
-    ensure  => present,
-    source  => 'puppet:///modules/govuk/etc/govuk/unicorn.rb',
-    require => File['/etc/govuk'],
-  }
-
-  # govuk_spinup is a wrapper script used to start up apps that form part of
-  # the GOV.UK stack. It exports various environment variables used by
-  # Rails/A. N. Other Application Framework, and starts up either
-  # Procfile-based or unicorn applications.
-  file { '/usr/local/bin/govuk_spinup':
-    ensure  => present,
-    source  => 'puppet:///modules/govuk/bin/govuk_spinup',
-    mode    => '0755',
-  }
-
-  # govuk_setenv is a simple script that loads the environment for a GOV.UK
-  # application and execs its arguments
-  # daemontools provides envdir, used by govuk_setenv
-  file { '/usr/local/bin/govuk_setenv':
-    ensure  => present,
-    source  => 'puppet:///modules/govuk/bin/govuk_setenv',
-    mode    => '0755',
-    require => Class['daemontools'],
-  }
-
-  # /etc/govuk/env.d is an envdir. Each file and its contents should denote
-  # the name and value of an environment variable that should be exported
-  file { '/etc/govuk/env.d':
-    ensure  => directory,
-    purge   => true,
-    recurse => true,
-    force   => true,
-    require => File['/etc/govuk'],
-  }
-
-  $app_domain = extlookup('app_domain')
-
-  govuk::envvar { 'GOVUK_APP_DOMAIN':
-    value => $app_domain,
-  }
-
-  $asset_root = extlookup('asset_root', "https://static.${app_domain}")
-
-  # This file is deprecated and will be removed once the transition from
-  # asset_host -> asset_root is completed.
-  file { '/etc/govuk/asset_host.conf':
-    ensure  => present,
-    content => $asset_root,
-    require => File['/etc/govuk'],
-  }
-
-  govuk::envvar { 'GOVUK_ASSET_ROOT':
-    value => $asset_root,
-  }
-
-  $website_root = extlookup('website_root')
-
-  govuk::envvar { 'GOVUK_WEBSITE_ROOT':
-    value => $website_root,
-  }
-
-  # I don't want to create the following with invalid content, which it would
-  # contain until the asset_host extdata variable is repurposed. Commented out
-  # until the transition from asset_host -> asset_root has occurred in
-  # extdata.
-
-  # $asset_host = extlookup('asset_host', "static.${app_domain}")
-
-  # govuk::envvar { 'GOVUK_ASSET_HOST':
-  #   value => $asset_host,
-  # }
-
-  govuk::envvar { 'GOVUK_ASSET_HOST':
-    value => $asset_root,
-  }
-
-  $govuk_env = extlookup('govuk_env', 'production')
-  govuk::envvar {
-    'GOVUK_ENV': value => $govuk_env;
-    'RAILS_ENV': value => $govuk_env;
-    'RACK_ENV':  value => $govuk_env;
+  anchor { 'govuk::deploy::end':
+    subscribe => Class['govuk::deploy::config'],
   }
 }

alphagov/govuk-puppet

commit ccce1f23fd5c8cf260d32d1a13065906c7471f11

[#48566865] SSH keys for deploy user as file{}
Manage SSH `authorized_keys` for the `deploy` user as a file resource.

This will enable us to reliably purge old or unmanaged keys, which the
`ssh_authorized_key{}` type doesn't allow. It should also be marginally
faster to process.

Keys that aren't present in extdata will appear as commented entries with
the text `NONE_IN_EXTDATA` to indicate to humans why it's disabled. Puppet
will autorequire the user and file hierachy resources correctly itself.

modules/govuk/manifests/deploy.pp
@@ -59,40 +59,26 @@ class govuk::deploy {
     ensure => directory,
   }
 
-  if $::govuk_platform != 'development' {
-    ssh_authorized_key { 'deploy_key_jenkins':
-      ensure  => present,
-      key     => extlookup('jenkins_key', 'NO_KEY_IN_EXTDATA'),
-      type    => 'ssh-rsa',
-      user    => 'deploy',
-      require => User['deploy'];
-    }
-
-    ssh_authorized_key { 'deploy_key_jenkins_skyscape':
-      ensure  => present,
-      key     => extlookup('jenkins_skyscape_key', 'NO_KEY_IN_EXTDATA'),
-      type    => 'ssh-rsa',
-      user    => 'deploy',
-    }
-    # This key is required to allow Production Jenkins to sync data to
-    # other environments.
-    ssh_authorized_key { 'deploy_key_jenkins_skyscape_production':
-      ensure  => present,
-      key     => extlookup('jenkins_skyscape_production_key', 'NO_KEY_IN_EXTDATA'),
-      type    => 'ssh-rsa',
-      user    => 'deploy',
-    }
+  # Referenced by authorized_keys template below.
+  $deploy_ssh_keys = {
+    'jenkins_key'                     => extlookup('jenkins_key', 'NONE_IN_EXTDATA'),
+    'jenkins_skyscape_key'            => extlookup('jenkins_skyscape_key', 'NONE_IN_EXTDATA'),
+    # Production Jenkins sync to other environments.
+    'jenkins_skyscape_production_key' => extlookup('jenkins_skyscape_production_key', 'NONE_IN_EXTDATA'),
+    'deploy_user_data_sync_key'       => extlookup('deploy_user_data_sync_key', 'NONE_IN_EXTDATA'),
   }
-
-  $deploy_user_data_sync_key = extlookup('deploy_user_data_sync_key', 'NONE')
-
-  if $deploy_user_data_sync_key != 'NONE' {
-    ssh_authorized_key { 'data_sync_key':
-      ensure  => present,
-      key     => $deploy_user_data_sync_key,
-      type    => 'ssh-rsa',
-      user    => 'deploy',
-    }
+  file { '/home/deploy/.ssh':
+    ensure => directory,
+    owner  => 'deploy',
+    group  => 'deploy',
+    mode   => '0700',
+  }
+  file { '/home/deploy/.ssh/authorized_keys':
+    ensure  => present,
+    owner   => 'deploy',
+    group   => 'deploy',
+    mode    => '0600',
+    content => template('govuk/home/deploy/.ssh/authorized_keys.erb'),
   }
 
   file { '/etc/govuk/unicorn.rb':

modules/govuk/spec/classes/govuk_deploy_spec.rb
@@ -1,0 +1,38 @@
+require_relative '../../../../spec_helper'
+
+describe 'govuk::deploy', :type => :class do
+  let(:file_path) { '/home/deploy/.ssh/authorized_keys' }
+
+  context 'with all keys present in extdata' do
+    before {
+      update_extdata({
+        'jenkins_key'                     => 'oneapple',
+        'jenkins_skyscape_key'            => 'twopears',
+        'jenkins_skyscape_production_key' => 'threeplums',
+        'deploy_user_data_sync_key'       => 'fourstrawberries',
+      })
+    }
+    # FIXME: Hack to refresh extdata.
+    let(:facts) {{ :cache_bust => Time.now }}
+
+    it 'authorized_keys should have all keys active and sorted by comment' do
+      should contain_file(file_path).with_content(<<EOS
+ssh-rsa fourstrawberries deploy_user_data_sync_key
+ssh-rsa oneapple jenkins_key
+ssh-rsa twopears jenkins_skyscape_key
+ssh-rsa threeplums jenkins_skyscape_production_key
+EOS
+      )
+    end
+  end
+
+  context 'with no keys present in extdata' do
+    # FIXME: Hack to refresh extdata.
+    let(:facts) {{ :cache_bust => Time.now }}
+
+    it 'authorized_keys should only contain commented keys' do
+      should contain_file(file_path).with_content(/ NONE_IN_EXTDATA /)
+      should_not contain_file(file_path).with_content(/^[^#]/)
+    end
+  end
+end

modules/govuk/templates/home/deploy/.ssh/authorized_keys.erb
@@ -1,0 +1,6 @@
+<%
+# Sort by key name/comment to ensure ordering on older Rubies.
+@deploy_ssh_keys.sort_by { |k,v| k }.each do |name, key|
+-%>
+<%= '#' if key == 'NONE_IN_EXTDATA' -%>ssh-rsa <%= key -%> <%= name %>
+<% end -%>

commit c7362d524c9d14c4e76274fb20539283ffdf95fd

[#48998005] govuk::deploy unnecessary app restarts
Break apart `govuk::deploy` into two sub-classes:

1. Setup: resources that are required by `govuk::app` resources, but when
   modified should not affect the operation of a running app. Examples would
   be file permissions on `/data` or new SSH keys.

2. Config: resources which are also required, but when modified should
   trigger a restart of any `govuk::app` resources which called it. Examples
   would be global unicorn options or changes to the `govuk_setenv` script.

This addresses an issue whereby the migration of `ssh_authorized_keys{}` to
`file{}` resources in ccce1f2 caused *all* Ruby applications to restart
unnecessarily. In an unfortunate sequence of events we ran Puppet over all
nodes in a short period of time, which caused a brief outage.

modules/govuk/manifests/deploy.pp
@@ -1,8 +1,5 @@
 class govuk::deploy {
-
-  include assets::user
   include bundler
-  include daemontools
   include fpm
   include govuk::logging
   include harden
@@ -11,157 +8,19 @@ class govuk::deploy {
   include virtualenv
   include unicornherder
 
-  group { 'deploy':
-    ensure  => 'present',
-    name    => 'deploy',
+  anchor { 'govuk::deploy::begin': }
+
+  # These resources are required, but should not refresh apps.
+  class { 'govuk::deploy::setup':
+    require => Anchor['govuk::deploy::begin'],
   }
 
-  user { 'deploy':
-    ensure      => present,
-    home        => '/home/deploy',
-    managehome  => true,
-    groups      => ['assets'],
-    shell       => '/bin/bash',
-    gid         => 'deploy',
-    require     => [Group['deploy'],Group['assets']];
+  # These resources should refresh apps.
+  class { 'govuk::deploy::config':
+    require => Class['govuk::deploy::setup'],
   }
 
-  harden::limit { 'deploy-nofile':
-    domain => 'deploy',
-    type   => '-', # set both hard and soft limits
-    item   => 'nofile',
-    value  => '16384',
-  }
-
-  file { '/etc/govuk':
-    ensure => directory,
-  }
-
-  file { '/var/lib/govuk':
-    ensure => directory,
-  }
-
-  file { '/data':
-    ensure  => directory,
-    owner   => 'deploy',
-    group   => 'deploy',
-    require => User['deploy'],
-  }
-
-  file { '/data/vhost':
-    ensure  => directory,
-    owner   => 'deploy',
-    group   => 'deploy',
-    require => File['/data'],
-  }
-
-  file { '/var/apps':
-    ensure => directory,
-  }
-
-  # Referenced by authorized_keys template below.
-  $deploy_ssh_keys = {
-    'jenkins_key'                     => extlookup('jenkins_key', 'NONE_IN_EXTDATA'),
-    'jenkins_skyscape_key'            => extlookup('jenkins_skyscape_key', 'NONE_IN_EXTDATA'),
-    # Production Jenkins sync to other environments.
-    'jenkins_skyscape_production_key' => extlookup('jenkins_skyscape_production_key', 'NONE_IN_EXTDATA'),
-    'deploy_user_data_sync_key'       => extlookup('deploy_user_data_sync_key', 'NONE_IN_EXTDATA'),
-  }
-  file { '/home/deploy/.ssh':
-    ensure => directory,
-    owner  => 'deploy',
-    group  => 'deploy',
-    mode   => '0700',
-  }
-  file { '/home/deploy/.ssh/authorized_keys':
-    ensure  => present,
-    owner   => 'deploy',
-    group   => 'deploy',
-    mode    => '0600',
-    content => template('govuk/home/deploy/.ssh/authorized_keys.erb'),
-  }
-
-  file { '/etc/govuk/unicorn.rb':
-    ensure  => present,
-    source  => 'puppet:///modules/govuk/etc/govuk/unicorn.rb',
-    require => File['/etc/govuk'],
-  }
-
-  # govuk_spinup is a wrapper script used to start up apps that form part of
-  # the GOV.UK stack. It exports various environment variables used by
-  # Rails/A. N. Other Application Framework, and starts up either
-  # Procfile-based or unicorn applications.
-  file { '/usr/local/bin/govuk_spinup':
-    ensure  => present,
-    source  => 'puppet:///modules/govuk/bin/govuk_spinup',
-    mode    => '0755',
-  }
-
-  # govuk_setenv is a simple script that loads the environment for a GOV.UK
-  # application and execs its arguments
-  # daemontools provides envdir, used by govuk_setenv
-  file { '/usr/local/bin/govuk_setenv':
-    ensure  => present,
-    source  => 'puppet:///modules/govuk/bin/govuk_setenv',
-    mode    => '0755',
-    require => Class['daemontools'],
-  }
-
-  # /etc/govuk/env.d is an envdir. Each file and its contents should denote
-  # the name and value of an environment variable that should be exported
-  file { '/etc/govuk/env.d':
-    ensure  => directory,
-    purge   => true,
-    recurse => true,
-    force   => true,
-    require => File['/etc/govuk'],
-  }
-
-  $app_domain = extlookup('app_domain')
-
-  govuk::envvar { 'GOVUK_APP_DOMAIN':
-    value => $app_domain,
-  }
-
-  $asset_root = extlookup('asset_root', "https://static.${app_domain}")
-
-  # This file is deprecated and will be removed once the transition from
-  # asset_host -> asset_root is completed.
-  file { '/etc/govuk/asset_host.conf':
-    ensure  => present,
-    content => $asset_root,
-    require => File['/etc/govuk'],
-  }
-
-  govuk::envvar { 'GOVUK_ASSET_ROOT':
-    value => $asset_root,
-  }
-
-  $website_root = extlookup('website_root')
-
-  govuk::envvar { 'GOVUK_WEBSITE_ROOT':
-    value => $website_root,
-  }
-
-  # I don't want to create the following with invalid content, which it would
-  # contain until the asset_host extdata variable is repurposed. Commented out
-  # until the transition from asset_host -> asset_root has occurred in
-  # extdata.
-
-  # $asset_host = extlookup('asset_host', "static.${app_domain}")
-
-  # govuk::envvar { 'GOVUK_ASSET_HOST':
-  #   value => $asset_host,
-  # }
-
-  govuk::envvar { 'GOVUK_ASSET_HOST':
-    value => $asset_root,
-  }
-
-  $govuk_env = extlookup('govuk_env', 'production')
-  govuk::envvar {
-    'GOVUK_ENV': value => $govuk_env;
-    'RAILS_ENV': value => $govuk_env;
-    'RACK_ENV':  value => $govuk_env;
+  anchor { 'govuk::deploy::end':
+    subscribe => Class['govuk::deploy::config'],
   }
 }

modules/govuk/manifests/deploy/config.pp
@@ -1,0 +1,100 @@
+# == Class: govuk::deploy::config
+#
+# Configuration resources for apps. Changes to the contents of these will
+# require an app to be restarted. For example, to pick up changes to
+# environment variables or centralised unicorn options.
+#
+class govuk::deploy::config {
+  include daemontools
+
+  harden::limit { 'deploy-nofile':
+    domain => 'deploy',
+    type   => '-', # set both hard and soft limits
+    item   => 'nofile',
+    value  => '16384',
+  }
+
+  file { '/etc/govuk/unicorn.rb':
+    ensure  => present,
+    source  => 'puppet:///modules/govuk/etc/govuk/unicorn.rb',
+    require => File['/etc/govuk'],
+  }
+
+  # govuk_spinup is a wrapper script used to start up apps that form part of
+  # the GOV.UK stack. It exports various environment variables used by
+  # Rails/A. N. Other Application Framework, and starts up either
+  # Procfile-based or unicorn applications.
+  file { '/usr/local/bin/govuk_spinup':
+    ensure  => present,
+    source  => 'puppet:///modules/govuk/bin/govuk_spinup',
+    mode    => '0755',
+  }
+
+  # govuk_setenv is a simple script that loads the environment for a GOV.UK
+  # application and execs its arguments
+  # daemontools provides envdir, used by govuk_setenv
+  file { '/usr/local/bin/govuk_setenv':
+    ensure  => present,
+    source  => 'puppet:///modules/govuk/bin/govuk_setenv',
+    mode    => '0755',
+    require => Class['daemontools'],
+  }
+
+  # /etc/govuk/env.d is an envdir. Each file and its contents should denote
+  # the name and value of an environment variable that should be exported
+  file { '/etc/govuk/env.d':
+    ensure  => directory,
+    purge   => true,
+    recurse => true,
+    force   => true,
+    require => File['/etc/govuk'],
+  }
+
+  $app_domain = extlookup('app_domain')
+
+  govuk::envvar { 'GOVUK_APP_DOMAIN':
+    value => $app_domain,
+  }
+
+  $asset_root = extlookup('asset_root', "https://static.${app_domain}")
+
+  # This file is deprecated and will be removed once the transition from
+  # asset_host -> asset_root is completed.
+  file { '/etc/govuk/asset_host.conf':
+    ensure  => present,
+    content => $asset_root,
+    require => File['/etc/govuk'],
+  }
+
+  govuk::envvar { 'GOVUK_ASSET_ROOT':
+    value => $asset_root,
+  }
+
+  $website_root = extlookup('website_root')
+
+  govuk::envvar { 'GOVUK_WEBSITE_ROOT':
+    value => $website_root,
+  }
+
+  # I don't want to create the following with invalid content, which it would
+  # contain until the asset_host extdata variable is repurposed. Commented out
+  # until the transition from asset_host -> asset_root has occurred in
+  # extdata.
+
+  # $asset_host = extlookup('asset_host', "static.${app_domain}")
+
+  # govuk::envvar { 'GOVUK_ASSET_HOST':
+  #   value => $asset_host,
+  # }
+
+  govuk::envvar { 'GOVUK_ASSET_HOST':
+    value => $asset_root,
+  }
+
+  $govuk_env = extlookup('govuk_env', 'production')
+  govuk::envvar {
+    'GOVUK_ENV': value => $govuk_env;
+    'RAILS_ENV': value => $govuk_env;
+    'RACK_ENV':  value => $govuk_env;
+  }
+}

modules/govuk/manifests/deploy/setup.pp
@@ -1,0 +1,71 @@
+# == Class: govuk::deploy::setup
+#
+# Setup resources required by apps. The variation of these resources should
+# not require an app to be restarted. Compared to `govuk::deploy::config`.
+#
+class govuk::deploy::setup {
+  include assets::user
+
+  group { 'deploy':
+    ensure  => 'present',
+    name    => 'deploy',
+  }
+
+  user { 'deploy':
+    ensure      => present,
+    home        => '/home/deploy',
+    managehome  => true,
+    groups      => ['assets'],
+    shell       => '/bin/bash',
+    gid         => 'deploy',
+    require     => [Group['deploy'],Group['assets']];
+  }
+
+  file { '/etc/govuk':
+    ensure => directory,
+  }
+
+  file { '/var/lib/govuk':
+    ensure => directory,
+  }
+
+  file { '/data':
+    ensure  => directory,
+    owner   => 'deploy',
+    group   => 'deploy',
+    require => User['deploy'],
+  }
+
+  file { '/data/vhost':
+    ensure  => directory,
+    owner   => 'deploy',
+    group   => 'deploy',
+    require => File['/data'],
+  }
+
+  file { '/var/apps':
+    ensure => directory,
+  }
+
+  # Referenced by authorized_keys template below.
+  $deploy_ssh_keys = {
+    'jenkins_key'                     => extlookup('jenkins_key', 'NONE_IN_EXTDATA'),
+    'jenkins_skyscape_key'            => extlookup('jenkins_skyscape_key', 'NONE_IN_EXTDATA'),
+    # Production Jenkins sync to other environments.
+    'jenkins_skyscape_production_key' => extlookup('jenkins_skyscape_production_key', 'NONE_IN_EXTDATA'),
+    'deploy_user_data_sync_key'       => extlookup('deploy_user_data_sync_key', 'NONE_IN_EXTDATA'),
+  }
+  file { '/home/deploy/.ssh':
+    ensure => directory,
+    owner  => 'deploy',
+    group  => 'deploy',
+    mode   => '0700',
+  }
+  file { '/home/deploy/.ssh/authorized_keys':
+    ensure  => present,
+    owner   => 'deploy',
+    group   => 'deploy',
+    mode    => '0600',
+    content => template('govuk/home/deploy/.ssh/authorized_keys.erb'),
+  }
+}

modules/govuk/spec/classes/govuk_deploy_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../../../spec_helper'
 
-describe 'govuk::deploy', :type => :class do
+describe 'govuk::deploy::setup', :type => :class do
   let(:file_path) { '/home/deploy/.ssh/authorized_keys' }
 
   context 'with all keys present in extdata' do