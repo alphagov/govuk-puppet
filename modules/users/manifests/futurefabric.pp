0beb66091b0e62de0fda5964f0c967373734f757
Guy Moorhouse <gmoorhouse@gmail.com>
2013-05-22 10:58:06 +0100

    Adding futurefabric user

---
+++ modules/users/manifests/futurefabric.pp
@@ -0,0 +1,7 @@
+class users::futurefabric {
+  govuk::user { 'futurefabric':
+    fullname => 'Guy Moorhouse',
+    email    => 'gmoorhouse@gmail.com',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSrzsrGcj8xByDK5DPaA43RhzQr8LMbHLN1LB6SynsH8YQDzM4/WU+tZQUhVXAmXZUR10RyAS9aUEP0d0GFL4esVmtCqqN6rBMKVUXQb/ctDC4LHE4LckstfbeW6/jXw7BqWnUqjkr1fy97oWZVm5FYXC0PE9V0ONRpZgY3Flp7q4QVLp/Mj5f32I1ntaOdHwZU/Lg46S6q59r0X+afcqinMIk5Bz31nqm3YYe9tmGzBKP1M3pmVG6nPT76ww7um9sKWJIncdjcM9+WHH37sBInBqNQs9SoiLwXWQl34qmc5H9j6lYorkvKpmtnZwe9tNevSNIujIbLI1gXR7O/1eP',
+  }
+}
