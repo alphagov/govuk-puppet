d87529fcf28621304c832dc0a3bad3c0d5621999
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-05-22 16:04:21 +0100

    Make nginx::logging require nginx::package
    Ensures ordering so that we dont end up doing multiple runs.

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -15,7 +15,9 @@ class nginx ($server_names_hash_max_size = 512, $variables_hash_max_size = 512) 
     notify                     => Class['nginx::service'];
   }
 
-  include nginx::logging
+  class { 'nginx::logging':
+    require => Class['nginx::package'];
+  }
 
   class { 'nginx::firewall':
     require => Class['nginx::config'],
