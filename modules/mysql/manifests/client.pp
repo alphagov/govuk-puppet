11acf8a2e2881aeb0b181083b553027bc8f41370
Dan Carley <dan.carley@gmail.com>
2013-05-22 14:30:45 +0100

    [#620] Document intentions of mysql::client class.

--- modules/mysql/manifests/client.pp
+++ modules/mysql/manifests/client.pp
@@ -1,3 +1,13 @@
+# == Class: mysql::client
+#
+# Installs packages for both the client binaries and development
+# libraries/headers. The latter of which are for compiling other clients
+# against MySQL, e.g. Ruby's `mysql2` gem.
+#
+# It is assumed that you will always want both. They both pull in
+# `mysql-common`. It is often useful to have the `mysql(1)` client to
+# quickly test connectivity or queries, for example.
+#
 class mysql::client {
   package { ['mysql-client','libmysqlclient-dev']:
     ensure => installed,
