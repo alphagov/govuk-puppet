3c2b8f4fe0ec4a7c655477b312fbd5f3f2d8f684
Dan Carley <dan.carley@gmail.com>
2013-05-23 16:41:09 +0100

    [#48998005] Don't restart mapit on Nginx changes
    Move all Nginx resources out of `::config` and into a new `::nginx`
    sub-class so that they don't trigger unwanted restarts of `::service`.
    
    The `::end` anchor hangs off this because we want a calling class to know
    that the service may not be completely operational if Nginx fails to
    configure.

--- modules/mapit/manifests/init.pp
+++ modules/mapit/manifests/init.pp
@@ -1,11 +1,11 @@
 class mapit {
 
-  include mapit::package, mapit::config, mapit::service
-  include nginx
+  include mapit::package, mapit::config, mapit::nginx, mapit::service
 
   anchor { ['mapit::start', 'mapit::end']: }
 
   Anchor['mapit::start'] -> Class['mapit::package'] -> Class['mapit::config']
   Class['mapit::config'] ~> Class['mapit::service'] ~> Anchor['mapit::end']
+  Class['mapit::nginx'] -> Anchor['mapit::end']
 
 }
