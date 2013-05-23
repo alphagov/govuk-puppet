8f459182eaf6f922a1479c95083b5218b4eecbed
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-05-23 16:18:48 +0100

    Remove old mirrorer script
    prevents collision with gem version which installs to /usr/bin

--- modules/mirror/manifests/init.pp
+++ modules/mirror/manifests/init.pp
@@ -34,6 +34,11 @@ class mirror {
     source => 'puppet:///modules/mirror/govuk_update_mirror',
   }
 
+  # TODO: Remove this once it's been rolled out to prod
+  file { '/usr/local/bin/govuk_mirrorer':
+    ensure => absent,
+  }
+
   include ruby::govuk_mirrorer
 
   # script that uploads the mirrored files to net storage
