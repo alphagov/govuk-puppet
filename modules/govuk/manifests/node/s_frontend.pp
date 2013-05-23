d1939b3cd23bd41f57da4daaefafc760ecb0f5f0
Garima Singh <igarimasingh@gmail.com>
2013-05-23 16:45:47 +0100

    [#42296389] Adding rbenv and ruby to frontend boxes

--- modules/govuk/manifests/node/s_frontend.pp
+++ modules/govuk/manifests/node/s_frontend.pp
@@ -1,8 +1,14 @@
 class govuk::node::s_frontend inherits govuk::node::s_base {
+  include rbenv
 
   $protect_fe = str2bool(extlookup('protect_frontend_apps', 'no'))
 
   include govuk::node::s_ruby_app_server
+
+  rbenv::version { '1.9.3-p392': }
+  rbenv::alias { '1.9.3':
+    to_version => '1.9.3-p392',
+  }
 
   class {
     'govuk::apps::businesssupportfinder': vhost_protected => $protect_fe;
