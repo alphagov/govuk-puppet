ea56dcd6b87baa4942410871e69ab2531341cb76
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-05-24 17:04:57 +0100

    Bump mirrorer gem to 1.0.0
    Requires explicitly configuring to use syslog (this was previously
    hardcoded, and the --logfile option was being ignored)
    
    The new version bring in a delay between requests that should prevent it
    hitting rate limiting.
    
    It will also support pagination in the content_api when that it turned
    on.

--- modules/ruby/manifests/govuk_mirrorer.pp
+++ modules/ruby/manifests/govuk_mirrorer.pp
@@ -1,6 +1,6 @@
 class ruby::govuk_mirrorer {
   package { 'govuk_mirrorer':
-    ensure   => '0.0.1',
+    ensure   => '1.0.0',
     provider => gem,
     source   => extlookup('govuk_gemfury_source_url'),
     require  => Package['libxml2-dev'],
