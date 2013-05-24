28abf0ad03650958f844c92beb5765264d4cdd12
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-05-24 15:57:41 +0100

    Specify version of mirrorer gem installed.
    So that we can control the rollout of newer versions

--- modules/ruby/manifests/govuk_mirrorer.pp
+++ modules/ruby/manifests/govuk_mirrorer.pp
@@ -1,6 +1,6 @@
 class ruby::govuk_mirrorer {
   package { 'govuk_mirrorer':
-    ensure   => present,
+    ensure   => '0.0.1',
     provider => gem,
     source   => extlookup('govuk_gemfury_source_url'),
     require  => Package['libxml2-dev'],
