caa86f534e22b2f0477ecc8d2ca16a0189358ebd
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-05-24 10:01:39 +0100

    Stub out extdata in mirror spec
    Fixes failing test.

--- modules/mirror/spec/classes/mirror_spec.rb
+++ modules/mirror/spec/classes/mirror_spec.rb
@@ -1,6 +1,10 @@
 require_relative '../../../../spec_helper'
 
 describe 'mirror', :type => :class do
+  before :each do
+    update_extdata({'govuk_gemfury_source_url' => 'https://some_token@gem.fury.io/govuk/',})
+  end
+
   it do
     should contain_file('/usr/local/bin/govuk_update_mirror')
     should_not raise_error(Puppet::ParseError)
