a9220424c328355d997d457d9c5b44720500e71e
Garima Singh <igarimasingh@gmail.com>
2013-05-24 14:50:10 +0100

    [#42296389] Specifying bundler version rbenv-ruby in whitehall boxes

--- modules/govuk/manifests/node/s_whitehall_frontend.pp
+++ modules/govuk/manifests/node/s_whitehall_frontend.pp
@@ -3,7 +3,9 @@ class govuk::node::s_whitehall_frontend inherits govuk::node::s_base {
   include nginx
   include rbenv
 
-  rbenv::version { '1.9.3-p392': }
+  rbenv::version { '1.9.3-p392':
+    bundler_version => '1.3.5'
+  }
   rbenv::alias { '1.9.3':
     to_version => '1.9.3-p392',
   }
