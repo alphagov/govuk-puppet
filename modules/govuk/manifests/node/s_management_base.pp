54c21aa8f1199a1f1211f96a72227723d88b9d31
Garima Singh <igarimasingh@gmail.com>
2013-05-24 14:54:43 +0100

    [#42296389] Specifying bundler version rbenv-ruby in aws ci boxes

--- modules/govuk/manifests/node/s_management_base.pp
+++ modules/govuk/manifests/node/s_management_base.pp
@@ -14,7 +14,9 @@ class govuk::node::s_management_base inherits govuk::node::s_base {
   include mongodb::server
   include nodejs
 
-  rbenv::version { '1.9.3-p392': }
+  rbenv::version { '1.9.3-p392':
+    bundler_version => '1.3.5'
+  }
   rbenv::alias { '1.9.3':
     to_version => '1.9.3-p392',
   }
