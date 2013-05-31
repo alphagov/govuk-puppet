ceffd1ce3dfc98788c4320b2f68443b2af45018f
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-05-31 15:08:49 +0100

    Tweak fact_cave app definition to match backend apps

--- modules/govuk/manifests/apps/fact_cave.pp
+++ modules/govuk/manifests/apps/fact_cave.pp
@@ -1,11 +1,8 @@
-class govuk::apps::fact_cave(
-  $port = 3048,
-  $vhost_protected
-) {
+class govuk::apps::fact_cave( $port = 3048 ) {
   govuk::app { 'fact-cave':
     app_type          => 'rack',
     port              => $port,
-    vhost_protected   => $vhost_protected,
+    vhost_ssl_only    => true,
     health_check_path => '/',
   }
 }
