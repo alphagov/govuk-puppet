35069cb7a05261e545bf3b6035ed9762ccc00832
Dan Carley <dan.carley@gmail.com>
2013-06-05 11:46:40 +0100

    Restart delayed worker jobs when script changes
    Required to rollout caceed5. Incidentally, these should be broken out to
    separate manifests. But I'm in a hurry.

--- modules/govuk/manifests/delayed_job.pp
+++ modules/govuk/manifests/delayed_job.pp
@@ -15,10 +15,10 @@ define govuk::delayed_job::worker (
   $enable_service = ($::govuk_platform != 'development')
 
   file { "/etc/init/${title}-delayed-job-worker.conf":
-    ensure  => present,
-    content => template('govuk/delayed-job-worker.conf.erb'),
-    require => Class['Govuk::Delayed_job'],
-    notify  => Service["${title}-delayed-job-worker"],
+    ensure    => present,
+    content   => template('govuk/delayed-job-worker.conf.erb'),
+    notify    => Service["${title}-delayed-job-worker"],
+    subscribe => Class['Govuk::Delayed_job'],
   }
 
   service { "${title}-delayed-job-worker":
