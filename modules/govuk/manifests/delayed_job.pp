b7dabb52d84d3f3984bd70960bb5b77ade3cab8c
Dan Carley <dan.carley@gmail.com>
2013-06-05 21:34:06 +0100

    Split out govuk::delayed_job::worker
    Satisfy lint check `autoloader_layout`. This also has some benefits in terms
    of error reporting and how the Puppet master falls back to cached
    catalogs/manifests. Discovered in 35069cb.

--- modules/govuk/manifests/delayed_job.pp
+++ modules/govuk/manifests/delayed_job.pp
@@ -5,23 +5,3 @@ class govuk::delayed_job {
     mode    => '0755',
   }
 }
-
-define govuk::delayed_job::worker (
-  $setenv_as = $title
-) {
-
-  include govuk::delayed_job
-
-  $enable_service = ($::govuk_platform != 'development')
-
-  file { "/etc/init/${title}-delayed-job-worker.conf":
-    ensure    => present,
-    content   => template('govuk/delayed-job-worker.conf.erb'),
-    notify    => Service["${title}-delayed-job-worker"],
-    subscribe => Class['Govuk::Delayed_job'],
-  }
-
-  service { "${title}-delayed-job-worker":
-    ensure  => $enable_service,
-  }
-}
---
+++ modules/govuk/manifests/delayed_job/worker.pp
@@ -0,0 +1,18 @@
+define govuk::delayed_job::worker (
+  $setenv_as = $title
+) {
+  include govuk::delayed_job
+
+  $enable_service = ($::govuk_platform != 'development')
+
+  file { "/etc/init/${title}-delayed-job-worker.conf":
+    ensure    => present,
+    content   => template('govuk/delayed-job-worker.conf.erb'),
+    notify    => Service["${title}-delayed-job-worker"],
+    subscribe => Class['Govuk::Delayed_job'],
+  }
+
+  service { "${title}-delayed-job-worker":
+    ensure  => $enable_service,
+  }
+}
