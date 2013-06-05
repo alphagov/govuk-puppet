4ef56db60004aef99007aa8a53789150468ee493
Philip Potter <philip.g.potter@gmail.com>
2013-06-05 15:15:28 +0100

    Add ext4mount for graphite-1 boxes

--- modules/govuk/manifests/node/s_graphite.pp
+++ modules/govuk/manifests/node/s_graphite.pp
@@ -1,4 +1,12 @@
 class govuk::node::s_graphite inherits govuk::node::s_base {
-  include graphite
+  class {'graphite':
+    require => Ext4mount['/opt/graphite'],
+  }
+
+  ext4mount {'/opt/graphite':
+    mountoptions => 'defaults',
+    disk         => '/dev/sdb1',
+  }
+
   include collectd::server
 }
