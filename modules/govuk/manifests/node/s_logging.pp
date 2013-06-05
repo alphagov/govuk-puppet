ad620c21b96daa6412007cda7a291332cecea81b
Philip Potter <philip.g.potter@gmail.com>
2013-06-05 15:23:53 +0100

    whitespace changes

--- modules/govuk/manifests/node/s_logging.pp
+++ modules/govuk/manifests/node/s_logging.pp
@@ -1,19 +1,19 @@
 class govuk::node::s_logging inherits govuk::node::s_base {
 
-# we want this to be a syslog server.
+  # we want this to be a syslog server.
   class { 'rsyslog::server': }
-# we also want it to send stuff to logstash
+  # we also want it to send stuff to logstash
   class { 'rsyslog::logstash': }
 
-# we want all the other machines to be able to send syslog on 514/tcp to this machine
+  # we want all the other machines to be able to send syslog on 514/tcp to this machine
   @ufw::allow {
     'allow-syslog-from-anywhere':
       from => '10.0.0.0/8',
       port => 514;
   }
 
-include java::oracle7::jre
-# TODO: this should really be done with a package.
+  include java::oracle7::jre
+  # TODO: this should really be done with a package.
 
   curl::fetch { 'logstash-monolithic':
     source      => 'https://logstash.objects.dreamhost.com/release/logstash-1.1.9-monolithic.jar',
@@ -28,7 +28,7 @@ include java::oracle7::jre
     initfile    => 'puppet:///modules/govuk/logstash.init.Debian',
   }
 
-#configure logstash inputs
+  #configure logstash inputs
   logstash::input::tcp {'syslog':
     type => 'syslog',
     port => '5544',
@@ -38,14 +38,14 @@ include java::oracle7::jre
     port => '5544',
   }
 
-#configure logstash filters
+  #configure logstash filters
   logstash::filter::grok {'syslog':
     type      => 'syslog',
     pattern   => [ '<%{POSINT:syslog_pri}>%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} %{PROG:syslog_program}(?:\[%{POSINT:syslog_pid}\])?: %{GREEDYDATA:syslog_message}' ],
     add_field => {
       'received_at'   => '%{@timestamp}',
       'received_from' => '%{@source_host}'
-      },
+    },
     order     => '11',
   }
   logstash::filter::syslog_pri {'syslog':
@@ -55,7 +55,7 @@ include java::oracle7::jre
   logstash::filter::date {'syslog':
     type  => 'syslog',
     match => [ 'syslog_timestamp', 'MMM dd HH:mm:ss',
-          'MMM  d HH:mm:ss' ],
+               'MMM  d HH:mm:ss' ],
     order => '13',
   }
   logstash::filter::mutate {'syslog-1':
@@ -65,7 +65,7 @@ include java::oracle7::jre
     replace      => {
       '@source_host' => '%{syslog_hostname}',
       '@message'     => '%{syslog_message}'
-      },
+    },
   }
   logstash::filter::mutate {'syslog-2':
     type   => 'syslog',
@@ -73,7 +73,7 @@ include java::oracle7::jre
     remove => [ 'syslog_hostname', 'syslog_message', 'syslog_timestamp' ],
   }
 
-#configure logstash outputs
+  #configure logstash outputs
 
   logstash::output::elasticsearch_http {'syslog':
     host  => 'logs-elasticsearch.cluster',
