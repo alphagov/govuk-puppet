981f7b2442f645ece77f4d57d67536c250f1a232
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-06-06 12:21:53 +0000

    Add Aspell for Rummager
    Aspell (http://aspell.net/) kicks ass at spelling and typo suggestions.
    We might switch from using Aspell to Elasticsearch for this one day, but that
    feature is only available in Elasticsearch 0.90 which is too bleeding edge for
    us.

--- modules/govuk/manifests/apps/search.pp
+++ modules/govuk/manifests/apps/search.pp
@@ -4,4 +4,9 @@ class govuk::apps::search( $port = 3009 ) {
     port               => $port,
     health_check_path  => '/mainstream/search?q=search_healthcheck',
   }
+
+  # For generating spelling suggestions
+  package { ['aspell', 'aspell-en', 'libaspell-dev']:
+    ensure => installed,
+  }
 }
