d2c715aa3522ca1b8024604e33a811f34c718aaa
Graham Ashton <graham@effectif.com>
2013-06-18 08:56:10 +0000

    Create shell account for gma

---
+++ modules/users/manifests/gma.pp
@@ -0,0 +1,7 @@
+class users::gma {
+  govuk::user { 'gma':
+    fullname => 'Graham Ashton',
+    email    => 'graham.ashton@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAyn4hs5YPGcfkZ0YWiPaP5puRzqvi1rd230Jg8m52AAa0O3z97xbPJG5QrF/RnQRAT5dVyIPEKrJZSBax4Q/GVw+DqYZ9u3UAzdvaINjjllY8SlDb66Cw5f00NGd8Gg0m9VgUYpjfn/iwopspmeMgV4VXJn/DyfwYr/ZUo3g50t8on+7elwbe0+aI8nlYXLGPYA1ijRDIntWN7wk6oH7YO5rLJLKTy1nFD0DgFgSEw4nVqCkteUiWjXL4t6Nfc0ZRaQmioyJtrk2diJtlTn0wBia5NtvxLnzk6zp9jv33XE66sp56uU5cToQlYe4SN1WPYXhcKrGTmpW+qEc9GU9R4w=='
+  }
+}
