954b4deb3303c26c5b41df778e3f991f18f66479
Dan Carley <dan.carley@gmail.com>
2013-06-20 16:10:21 +0100

    [#49133237] Move assets-origin to alphagov.co.uk
    Rename `assets-origin.digital.cabinet-office.gov.uk` ->
    `assets-origin.${environment}.alphagov.co.uk`.
    
    Where environment is {preview,staging,production}. This is inline with how
    we use `www-origin`, which Akamai edge references. This will facilitate us
    being able to use a CDN in front of staging or preview.

--- modules/router/manifests/assets_origin.pp
+++ modules/router/manifests/assets_origin.pp
@@ -4,23 +4,24 @@
 #
 class router::assets_origin {
   $app_domain = extlookup('app_domain')
+  $vhost_name = "assets-origin.${app_domain}"
 
   # suspect we want `protected => false` here
   # once appropriate firewalling is in place?
-  nginx::config::site { 'assets-origin.digital.cabinet-office.gov.uk':
+  nginx::config::site { $vhost_name:
     content => template('router/assets_origin.conf.erb'),
   }
 
-  nginx::config::ssl { 'assets-origin.digital.cabinet-office.gov.uk':
+  nginx::config::ssl { $vhost_name:
     certtype => 'wildcard_alphagov'
   }
 
   nginx::log {
-    'assets-origin.digital.cabinet-office.gov.uk-json.event.access.log':
+    "${vhost_name}-json.event.access.log":
       json          => true,
       logstream     => true,
       statsd_metric => "${::fqdn_underscore}.nginx_logs.assets-origin.http_%{@fields.status}";
-    'assets-origin.digital.cabinet-office.gov.uk-error.log':
+    "${vhost_name}-error.log":
       logstream => true;
   }
 }
