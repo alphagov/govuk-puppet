4feddb0da8698b7234b8ad6a67e36dee9ed42544
Garima Singh <igarimasingh@gmail.com>
2013-06-26 14:04:04 +0100

    [#52201615] Added MockExtdata to add ability to update extdata
    The earlier MockExtdata started using stubs from mocha after
    introduction of puppetlabs_spec_helper. "stubs" doesn't supports blocks
    because of which the hack to stub "File.exist?" started to fail.
    
    To have the ability of update extdata for the existing test we are now
    overriding extlookup function everytime we want to update extdata for
    tests. This is a hack and should be removed when we move to hiera.

--- spec_helper.rb
+++ spec_helper.rb
@@ -5,12 +5,37 @@ require 'puppet/parser/functions/extlookup'
 
 HERE = File.expand_path(File.dirname(__FILE__))
 
+module MockExtdata
+  def update_extdata(h)
+    # FIXME: Currently update_extdata on each call overrides puppet extlookup
+    # function, which is inefficient. This should be removed once we move to
+    # hiera
+    Puppet::Parser::Functions.newfunction(:extlookup, :type => :rvalue) do |args|
+      extdata = {
+        'app_domain'    => 'test.gov.uk',
+        'website_root'  => 'www.test.gov.uk',
+        'http_username' => 'test_username',
+        'http_password' => 'test_password',
+
+        'google_client_id_datainsight'               => 'example client id',
+        'google_client_secret_datainsight'           => 'example client secret',
+        'google_analytics_refresh_token_datainsight' => 'example refresh token',
+        'google_drive_refresh_token_datainsight'     => 'example refresh token'
+      }.merge!(h)
+
+      key, default = args
+      extdata[key] || default or raise Puppet::ParseError, "No match found for '#{key}' in any data file during extlookup()"
+    end
+  end
+end
+
 RSpec.configure do |c|
   c.manifest    = File.join(HERE, 'manifests', 'site.pp')
   c.module_path = [
     File.join(HERE, 'modules'),
     File.join(HERE, 'vendor', 'modules')
   ].join(':')
+  c.include MockExtdata
 
   c.before do
     # FIXME: We shouldn't need to do this. puppet/face should. See:
@@ -20,22 +45,6 @@ RSpec.configure do |c|
       Puppet.initialize_settings
     end
 
-    # Mock out extdata for the purposes of testing.
-    module Puppet::Parser::Functions
-      newfunction(:extlookup, :type => :rvalue) do |args|
-        key, default = args
-        {
-          'app_domain'    => 'test.gov.uk',
-          'website_root'  => 'www.test.gov.uk',
-          'http_username' => 'test_username',
-          'http_password' => 'test_password',
-
-          'google_client_id_datainsight'               => 'example client id',
-          'google_client_secret_datainsight'           => 'example client secret',
-          'google_analytics_refresh_token_datainsight' => 'example refresh token',
-          'google_drive_refresh_token_datainsight'     => 'example refresh token'
-        }[key] || default or raise Puppet::ParseError, "No match found for '#{key}' in any data file during extlookup()"
-      end
-    end
+    update_extdata({})
   end
 end
