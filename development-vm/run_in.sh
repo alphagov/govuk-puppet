b79c4c82f70f8b698eeb8476e34ea6d6de537635
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-06-26 13:32:18 +0100

    Expand comment about rbenv in run_in.sh

--- development-vm/run_in.sh
+++ development-vm/run_in.sh
@@ -3,6 +3,9 @@
 # Clear the rbenv setup that has been done in the development repo
 # Without this, rbenv won't do the setup again after changing directory,
 # and won't pick up the app's ruby version if defined.
+#
+# Note: rbenv sets RBENV_VERSION even when using the system ruby, in this
+# case it is set to the value 'system'.
 unset RBENV_VERSION
 
 cd $1
