83dd3d98c189dac0718b651f56094b6d56d338e3
Dan Carley <dan.carley@gmail.com>
2013-07-03 16:55:25 +0100

    Add class docs for collectd::plugin::processes
    The purpose of this wasn't clear. Even to me, who originally wrote it. So
    attempt to clarify why it exists.
    
    /cc @mazzmosley

--- modules/collectd/manifests/plugin/processes.pp
+++ modules/collectd/manifests/plugin/processes.pp
@@ -1,3 +1,12 @@
+# == Class: collectd::plugin::processes
+#
+# This class serves two purposes:
+#
+# 1. It tells collectd to always monitor basic process stats from a system.
+#    Like total number of running, blocked, zombies, etc.
+# 2. It ensures that the plugin is loaded before any instances of
+#    `collectd::plugin::process` are encountered.
+#
 class collectd::plugin::processes {
   @collectd::plugin { 'processes':
     prefix  => '00-',
