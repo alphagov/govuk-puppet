e4774d3dfe4863daf7d4fa8bcf88dfe1f62ad017
Anna Shipman <anna@annashipman.co.uk>
2013-07-04 14:59:32 +0100

    Making argument explicit
    It would have worked, mostly, before, but 'frontend' would have picked up
    'datainsight-frontend'.

--- modules/govuk/manifests/app/config.pp
+++ modules/govuk/manifests/app/config.pp
@@ -148,7 +148,7 @@ define govuk::app::config (
     }
   }
   @@nagios::check { "check_app_${title}_unicornherder_up_${::hostname}":
-    check_command       => "check_nrpe!check_proc_running_with_arg!unicornherder ${title}",
+    check_command       => "check_nrpe!check_proc_running_with_arg!unicornherder /var/run/${title}/app.pid",
     service_description => "${title} app unicornherder running",
     host_name           => $::fqdn,
     document_url        => 'https://github.gds/pages/gds/opsmanual/2nd-line/nagios.html#app-unicornherder-running',
