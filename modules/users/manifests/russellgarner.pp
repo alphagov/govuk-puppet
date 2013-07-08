c93a1be231e3ba7855d11636abd39f36bf59722b
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-07-08 11:53:07 +0100

    Add SSH keys for Russell Garner

---
+++ modules/users/manifests/russellgarner.pp
@@ -0,0 +1,7 @@
+class users::russellgarner {
+  govuk::user { 'russellgarner':
+    fullname => 'Russell Garner',
+    email    => russell.garner@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAuoIglteQmEwL5/T3maYcomkZs44x1Rx/YRn31vMEuAaBHqYyD5mLUFMXQbCyFO6mZcjx+uOTOT6QhzIiKa8dJhAs5K/KFtjG/6vuwqFdC/Tpggvx6My/vDROsZVmPz4RFrd9XhoL2ybAXaHq6hfo+j112J/n+rkemqzmR/gz2TGFni9jSWG8fEzEkMwQR0iHZnGSFJI2UUR8trxyk32Pq3hkyEiA+XdipR/U8uPnBB2C3+Ms5NUaI8kFmQKYEumqU093dUD7iuekw3vA3elD8oR6UeDGcoRbMomT8oaatPtZl2rSw343M+tEQTC4VsUtcYAiK46sCxIsSjCI0+k2eQ== rgarner@zephyros-systems.co.uk',
+  }
+}
