0950f7446096146025b5e8e9f3f458cb391731df
Mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2013-07-09 13:06:25 +0100

    Let's sanitise the title of the filename.

--- modules/collectd/manifests/plugin/raindrops.pp
+++ modules/collectd/manifests/plugin/raindrops.pp
@@ -3,6 +3,9 @@ define collectd::plugin::raindrops(
 ) {
   include collectd::plugin::curl
 
+  # Sanitise file and metric names.
+  validate_re($title, '^[\w\-]+$')
+
   @collectd::plugin { "raindrops-${title}":
     content => template('collectd/etc/collectd/conf.d/raindrops.conf.erb'),
     require => Class['collectd::plugin::curl'],
