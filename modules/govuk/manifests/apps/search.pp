6ca161ba3cbeeb3dc3b1d9772589fc4f07143fe4
Mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2013-07-09 11:03:56 +0100

    Include raindrops collectd for search only.

--- modules/govuk/manifests/apps/search.pp
+++ modules/govuk/manifests/apps/search.pp
@@ -1,6 +1,11 @@
 class govuk::apps::search( $port = 3009 ) {
   include aspell
 
+  # Enable raindrops monitoring
+  collectd::plugin::raindrops { 'search':
+  	port => $port,
+  }
+
   govuk::app { 'search':
     app_type           => 'rack',
     port               => $port,
