86846b914251f856e8a491deff14049b4df1e936
Anna Shipman <anna@annashipman.co.uk>
2013-07-10 10:46:43 +0100

    Remove file extension for consistency

--- modules/nagios/manifests/client/check_upstart_status.pp
+++ modules/nagios/manifests/client/check_upstart_status.pp
@@ -3,8 +3,8 @@
 # Install a Nagios plugin that can alert when upstart is down
 #
 class nagios::client::check_upstart_status {
-  @nagios::plugin { 'check_upstart_status.pl':
-    source  => 'puppet:///modules/nagios/usr/lib/nagios/plugins/check_upstart_status.pl',
+  @nagios::plugin { 'check_upstart_status':
+    source  => 'puppet:///modules/nagios/usr/lib/nagios/plugins/check_upstart_status',
   }
 
   @nagios::nrpe_config { 'check_upstart_status':
