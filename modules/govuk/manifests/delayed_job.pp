dd297ca02ebef508132b1bb8ecd0ff9a55d41d2c
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-07-10 12:39:40 +0100

    Clarify what a "delayed job worker" is and isn't.
    Ideally, I think this wouldn't be called a "delayed job worker" at all,
    because that implies it's only for delayed_job, rather than a generic
    worker that runs off a Rake task, but that would involve mass renaming
    of services and config files and executables, and is beyond my Puppetry
    ken.

--- modules/govuk/manifests/delayed_job.pp
+++ modules/govuk/manifests/delayed_job.pp
@@ -1,4 +1,7 @@
 class govuk::delayed_job {
+  # One word of caution: this is a generic worker that runs a Rake task,
+  # rather than necessarily being tied to the `delayed_job` library.
+
   file { '/usr/local/bin/govuk_run_delayed_job_worker':
     ensure  => present,
     source  => 'puppet:///modules/govuk/bin/govuk_run_delayed_job_worker',
