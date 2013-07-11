cff08567f325cbf74bd144fe565386989bc9309c
Guy Moorhouse <gmoorhouse@gmail.com>
2013-07-11 13:34:01 +0000

    Add key for new machine for user futurefabric

--- modules/users/manifests/futurefabric.pp
+++ modules/users/manifests/futurefabric.pp
@@ -2,6 +2,6 @@ class users::futurefabric {
   govuk::user { 'futurefabric':
     fullname => 'Guy Moorhouse',
     email    => 'gmoorhouse@gmail.com',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSrzsrGcj8xByDK5DPaA43RhzQr8LMbHLN1LB6SynsH8YQDzM4/WU+tZQUhVXAmXZUR10RyAS9aUEP0d0GFL4esVmtCqqN6rBMKVUXQb/ctDC4LHE4LckstfbeW6/jXw7BqWnUqjkr1fy97oWZVm5FYXC0PE9V0ONRpZgY3Flp7q4QVLp/Mj5f32I1ntaOdHwZU/Lg46S6q59r0X+afcqinMIk5Bz31nqm3YYe9tmGzBKP1M3pmVG6nPT76ww7um9sKWJIncdjcM9+WHH37sBInBqNQs9SoiLwXWQl34qmc5H9j6lYorkvKpmtnZwe9tNevSNIujIbLI1gXR7O/1eP',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyQ2gpI87R74Zn65+ID/60Ow0+tdXj6gk3Npp818OjL2OaWxkjo+oAJZdwh/IomwrtGL0urmijpXpNfgbdC/2yV73kPxpLkJjxQ03ThROhFA4miBYXKhaReR8GTYcW54IOeReDXFe3WiLJDQJ040W8Umpw4g1Dk/md9mqsyy4vUHl4JDTzoYxhSHmeoikduphTBSYmXhNuoag2CxlNdL7E44POY92j1ICc/6CSiJk6xK1eOswaj67FZDhqhtrBCxo8XtqEMqzqzKPGnN/0Wc+AnQhcS3Jxm4TQpM3281GR6mOmV4W6jq2Mpui+ALFsYlF/XdlSvOCpqqkix2V7DUYv',
   }
 }
