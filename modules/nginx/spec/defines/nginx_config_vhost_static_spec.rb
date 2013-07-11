e4fd31bc0fdf0afa5fd2df3b4d8f3a7c6a74a364
Garima Singh <igarimasingh@gmail.com>
2013-07-11 11:58:48 +0100

    [#52201683] Adding test around static-vhost.conf

--- modules/nginx/spec/defines/nginx_config_vhost_static_spec.rb
+++ modules/nginx/spec/defines/nginx_config_vhost_static_spec.rb
@@ -17,4 +17,20 @@ describe 'nginx::config::vhost::static', :type => :define do
     it { should contain_logster__cronjob('nginx-vhost-static.example.com')
       .with_prefix('nginx_logs.static_example_com') }
   end
+
+  context 'with aliases' do
+    let(:params) do
+      {
+        :to => 'giraffe:1234',
+        :aliases => ['foo', 'bar'],
+      }
+    end
+
+    it 'should proxy to server giraffe:1234' do
+      should contain_nginx__config__site('static.example.com').with_content(/location \/foo\//)
+      should contain_nginx__config__site('static.example.com').with_content(/alias \/data\/vhost\/foo\.test\.gov\.uk\/current\/public\/foo/)
+      should contain_nginx__config__site('static.example.com').with_content(/location \/bar\//)
+      should contain_nginx__config__site('static.example.com').with_content(/alias \/data\/vhost\/bar\.test\.gov\.uk\/current\/public\/bar/)
+    end
+  end
 end
