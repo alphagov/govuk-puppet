7ba24139465f60616ab0d2e46237e7051933a262
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-07-15 16:27:28 +0100

    Add extra env var for fco-services smokey tests.

--- modules/nagios/manifests/config/smokey.pp
+++ modules/nagios/manifests/config/smokey.pp
@@ -1,13 +1,14 @@
 class nagios::config::smokey {
   $smokey_vars = {
-    'AUTH_USERNAME'   => extlookup('http_username', 'UNSET'),
-    'AUTH_PASSWORD'   => extlookup('http_password', 'UNSET'),
-    'EFG_DOMAIN'      => extlookup('smokey_efg_domain', 'UNSET'),
-    'EFG_USERNAME'    => extlookup('smokey_efg_username', 'UNSET'),
-    'EFG_PASSWORD'    => extlookup('smokey_efg_password', 'UNSET'),
-    'SIGNON_EMAIL'    => extlookup('smokey_signon_email', 'UNSET'),
-    'SIGNON_PASSWORD' => extlookup('smokey_signon_password', 'UNSET'),
-    'BEARER_TOKEN'    => extlookup('smokey_bearer_token', 'UNSET'),
+    'AUTH_USERNAME'              => extlookup('http_username', 'UNSET'),
+    'AUTH_PASSWORD'              => extlookup('http_password', 'UNSET'),
+    'EFG_DOMAIN'                 => extlookup('smokey_efg_domain', 'UNSET'),
+    'EFG_USERNAME'               => extlookup('smokey_efg_username', 'UNSET'),
+    'EFG_PASSWORD'               => extlookup('smokey_efg_password', 'UNSET'),
+    'SIGNON_EMAIL'               => extlookup('smokey_signon_email', 'UNSET'),
+    'SIGNON_PASSWORD'            => extlookup('smokey_signon_password', 'UNSET'),
+    'BEARER_TOKEN'               => extlookup('smokey_bearer_token', 'UNSET'),
+    'FCO_SERVICES_DOMAIN_PREFIX' => extlookup('fco_services_domain_prefix', 'UNSET'),
   }
 
   file { '/etc/smokey.sh':
