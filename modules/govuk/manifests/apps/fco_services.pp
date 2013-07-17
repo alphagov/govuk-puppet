4795f4cdb2b693082a9d0d123fd3d745cca3cbc5
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-07-17 12:25:36 +0000

    Set cache headers for fco-services assets.
    (only when not on a dev VM)
    
    These are handled by the Rails asset pipeline, and therefore get a
    new filename each time the contents change.  We can therefore safely
    set a far-future expires header for them.

--- modules/govuk/manifests/apps/fco_services.pp
+++ modules/govuk/manifests/apps/fco_services.pp
@@ -3,11 +3,23 @@ class govuk::apps::fco_services(
   $vhost_protected,
   $vhost_aliases = []
 ) {
+
+  # Set expires headers for assets when not on a dev VM
+  $extra_config = $::govuk_platform ? {
+    'development' => '',
+    default       => '
+  location ^~ /assets/ {
+    expires max;
+    add_header Cache-Control public;
+  }',
+  }
+
   govuk::app { 'fco-services':
-    app_type          => 'rack',
-    port              => $port,
-    vhost_protected   => $vhost_protected,
-    health_check_path => '/healthcheck',
-    vhost_aliases     => $vhost_aliases,
+    app_type           => 'rack',
+    port               => $port,
+    vhost_protected    => $vhost_protected,
+    health_check_path  => '/healthcheck',
+    vhost_aliases      => $vhost_aliases,
+    nginx_extra_config => $extra_config,
   }
 }
