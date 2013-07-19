601098cd3fafcdf6409e866b895dbcd4e4371fab
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-07-19 12:50:46 +0100

    Enable Rummager queue worker in all environments.

--- modules/govuk/manifests/apps/search.pp
+++ modules/govuk/manifests/apps/search.pp
@@ -12,7 +12,5 @@ class govuk::apps::search( $port = 3009 ) {
     health_check_path  => '/mainstream/search?q=search_healthcheck',
   }
 
-  if str2bool(extlookup('govuk_enable_search_worker', 'no')) {
-    govuk::delayed_job::worker { 'search': }
-  }
+  govuk::delayed_job::worker { 'search': }
 }
