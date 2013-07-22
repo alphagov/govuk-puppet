143d3a710e8136bbac70c78503255be4090b32c7
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-07-22 13:06:02 +0100

    Add Ben Firshman and Tim Abell
    Contractors on Inside Government.

---
+++ modules/users/manifests/ben.pp
@@ -0,0 +1,7 @@
+class users::ben {
+  govuk::user { 'ben':
+    fullname => 'Ben Firshman',
+    email    => 'ben.firshman@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAvG7yXWQq/OznMVGDE05VD8aCG2fzai5XfNAdjv9CDAm9GwmUTZ1DHU/grXlGtC5MRuHROP4pZew3B2rR5y9XZou4ba8fhr/zlQhaw/OFsRr7SOwWrZ685Ix5SRCtiwGP0t2BT2CtU7xPE0PAToWhYJSH2Sjt62vu/h7RXk7POEtmzrydkakoeXCg1yB4r6ifEbMogBVzIgEnMQJC/RK1i1aBoNl12B2yg+OuJK1b1NN874UuHV+b62Kmii6XxISJr6z0uYybslvyQ6vvpZHAAsRyMJgmveirWBSe2bgFUeUJHVJpqOjkK2qEs3NdyXsfv+2ibK31fBGML/GStf+W3Q==',
+  }
+}
--- modules/users/manifests/groups/other.pp
+++ modules/users/manifests/groups/other.pp
@@ -1,8 +1,10 @@
 class users::groups::other {
 
+  include users::ben
   include users::jolyonpawlyn
   include users::russellgarner
-  include users::tomstuart
   include users::tekin
+  include users::timabell
+  include users::tomstuart
 
 }
