7de1859e808785536b9b302df9f7e12fb828b454
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-07-25 17:25:15 +0100

    Upgrade phantomjs to 1.9.1

--- modules/phantomjs/manifests/init.pp
+++ modules/phantomjs/manifests/init.pp
@@ -1,24 +1,24 @@
 class phantomjs {
 
   exec { 'download phantomjs':
-    command => '/usr/bin/curl -o phantomjs-1.8.1-linux-x86_64.tar.bz2 https://gds-public-readable-tarballs.s3.amazonaws.com/phantomjs-1.8.1-linux-x86_64.tar.bz2',
+    command => '/usr/bin/curl -o phantomjs-1.9.1-linux-x86_64.tar.bz2 https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2',
     cwd     => '/usr/local/src',
-    creates => '/usr/local/src/phantomjs-1.8.1-linux-x86_64.tar.bz2',
+    creates => '/usr/local/src/phantomjs-1.9.1-linux-x86_64.tar.bz2',
     require => Package['curl'],
     timeout => 3600,
-    unless  => '/usr/bin/test "`shasum phantomjs-1.8.1-linux-x86_64.tar.bz2`" = "8fb1420d633c24092d01a34eaa8c8c9e89d21d0a phantomjs-1.8.1-linux-x86_64.tar.bz2"'
+    unless  => '/usr/bin/test "`shasum phantomjs-1.9.1-linux-x86_64.tar.bz2`" = "8ab4753abd352eaed489709c6c7dd13dae67cd91 phantomjs-1.9.1-linux-x86_64.tar.bz2"'
   }
 
   exec { 'unpack phantomjs':
     require => Exec['download phantomjs'],
-    creates => '/usr/local/src/phantomjs-1.8.1-linux-x86_64',
+    creates => '/usr/local/src/phantomjs-1.9.1-linux-x86_64',
     cwd     => '/usr/local/src',
-    command => '/bin/tar -jxf ./phantomjs-1.8.1-linux-x86_64.tar.bz2'
+    command => '/bin/tar -jxf ./phantomjs-1.9.1-linux-x86_64.tar.bz2'
   }
 
   file { '/usr/local/bin/phantomjs':
     ensure  => link,
-    target  => '/usr/local/src/phantomjs-1.8.1-linux-x86_64/bin/phantomjs',
+    target  => '/usr/local/src/phantomjs-1.9.1-linux-x86_64/bin/phantomjs',
     require => Exec['unpack phantomjs'],
   }
 
