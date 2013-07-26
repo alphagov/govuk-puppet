11f7e3c431512bc3b30ed6533eaa16c3fbeb3df1
roc <ralph.cowling@digital.cabinet-office.gov.uk>
2013-07-26 16:03:02 +0000

    add in roc (ralph cowling) production user puppet file

---
+++ modules/users/manifests/roc.pp
@@ -0,0 +1,7 @@
+class users::roc {
+  govuk::user { 'roc':
+    fullname => 'Ralph Cowling',
+    email    => 'ralph.cowling@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZEAxcu70/IbytBYrTh6jDsouuH8mjBlRPoq8e3eEWryQWe6+wevh/c29hiG1J1iRVYfUzxnVV2lIlxh3YOPG4TIwMFHS1rMpmR8j0VTbNw89Jf2O90X+wZa5ERlBRpXWhV98EMLWPMXmSTo7bXAdIZvNolAkw0GaIV9ozbZHV69hcXLxXFgO7KSp6J5ByYcqPwHASmZhfSLEYNSmD8JnwU0Z9lJl+YrlT9x0OrY79lCfSMIfCJ+l8+nMsNi7YrPEP6RrtCR3mStKmBVtiLukclxKTkx4nDxxuYThlZwVz90VBiRQHerK62EKmxky9Wrmx3KPT5dQDOCQ8gTmfTPOl',
+  }
+}
