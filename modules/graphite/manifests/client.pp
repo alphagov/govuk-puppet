bd9a3765c25260edf927ac0ba6702e8606832ff2
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-07-30 11:44:32 +0100

    remove graphite::cronjob
    this was only ever used by the haproxy module to set statsd gauges
    from haproxy stats. As haproxy is no more, we should remove this too.

--- modules/graphite/manifests/client.pp
+++ modules/graphite/manifests/client.pp
@@ -20,21 +20,8 @@ class graphite::client {
     require => [Package['statsd'], File['/etc/statsd.conf']],
   }
 
-  file { '/etc/statsd/':
-    ensure => directory,
-  }
-
-  file { '/etc/statsd/scripts':
-    ensure  => directory,
-    recurse => true,
-    purge   => true,
-    force   => true,
-  }
-
   service { 'statsd':
     ensure    => running,
     subscribe => [File['/etc/init/statsd.conf'], File['/etc/statsd.conf']],
   }
-
-  Graphite::Cronjob <| |>
 }
