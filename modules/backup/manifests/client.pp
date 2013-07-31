73ac013716a7106201f66a6f05344a4e363651b2
Dan Carley <dan.carley@gmail.com>
2013-07-31 11:39:31 +0100

    [#54348212] govuk-backup pubkey from extdata
    This will allow us to use a different keypair in preview. The extdata
    entries were already there - apart from preview, which was just added.

--- modules/backup/manifests/client.pp
+++ modules/backup/manifests/client.pp
@@ -1,9 +1,11 @@
 class backup::client {
 
-    govuk::user { 'govuk-backup':
-      fullname  => 'Backup User',
-      email     => 'webops@digital.cabinet-office.gov.uk',
-      ssh_key   => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDM1YJkjk/n9JaiepLnDNqB3yBR7gNUITz3EenyRoNy1udi4MVr3m4FFbYEWGCMvyjF/rLdvZT7rE1cRQfDHn0VQcB9+eO7jjaCtk5nz/+mDrtQq+dWWidg2R4cBfnDmyY/JP44ZkKx/TcXzILBRfOuAE3JfYbcgXScl95OmeOdyw==',
-    }
+  $backup_pubkey = extlookup('govuk-backup_key', '')
+
+  govuk::user { 'govuk-backup':
+    fullname  => 'Backup User',
+    email     => 'webops@digital.cabinet-office.gov.uk',
+    ssh_key   => "ssh-rsa ${backup_pubkey}",
+  }
 
 }
