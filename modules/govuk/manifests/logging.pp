a26097c6a4a159d0f7cdccd0d7377378eb87d644
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-08-01 16:35:17 +0100

    [#49661665] update tagalog to 0.4.0
    This version provides support for statsd timers and so we could use it
    to ship nginx request_time data into statsd.

--- modules/govuk/manifests/logging.pp
+++ modules/govuk/manifests/logging.pp
@@ -1,7 +1,7 @@
 class govuk::logging {
   # tagalog provides logship, used by govuk::logstream
   package { 'tagalog':
-    ensure   => '0.3.2',
+    ensure   => '0.4.0',
     provider => 'pip',
   }
 }
