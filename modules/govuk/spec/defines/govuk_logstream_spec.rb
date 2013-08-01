c813d9510ce499e5b8281f2ffc0f294a87853c49
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-08-01 16:55:31 +0100

    better test descriptions

--- modules/govuk/spec/defines/govuk_logstream_spec.rb
+++ modules/govuk/spec/defines/govuk_logstream_spec.rb
@@ -74,7 +74,7 @@ describe 'govuk::logstream', :type => :define do
     end
   end
 
-  context 'with statsd shipper specified' do
+  context 'with statsd counter shipper specified' do
     let(:params) { {
       :logfile       => log_file,
       :enable        => true,
@@ -82,7 +82,7 @@ describe 'govuk::logstream', :type => :define do
       :statsd_metric => 'tom_jerry.foo.%{@fields.bar}',
     } }
 
-    it 'should pass --json arg' do
+    it 'should statsd_counter arg' do
       should contain_file(upstart_conf).with(
         :ensure  => 'present',
         :content => /\| logship -f init_json,add_timestamp,add_source_host -s #{default_shipper} statsd_counter,metric=tom_jerry.foo.%{@fields.bar}$/,
