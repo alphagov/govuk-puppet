56f45ea7794f24bbcf8d6a6c50b76d7da8e982e8
Dan Carley <dan.carley@gmail.com>
2013-08-02 14:31:50 +0100

    [#54279372] Split logs specific redis check
    Split the check for the "logs" list from general Redis health. This will
    stop checking the "logs" list on the old `redis-N.backend` nodes, which are
    no longer being used for this purpose. It will setup a new check on the new
    `logs-redis-N.management` nodes.

--- modules/govuk/manifests/node/s_logs_redis.pp
+++ modules/govuk/manifests/node/s_logs_redis.pp
@@ -36,8 +36,14 @@ class govuk::node::s_logs_redis {
     #   - warns if >5 blocked clients, critical if >10
     #   - warns if >800 connected_clients, critical if >1000 connected_clients
     #   - warns if >10000 list length of logs, critical if >30000 list length of logs
-    check_command       => "check_nrpe!check_redis!${redis_port} 1,2 80,90 blocked_clients,connected_clients 5,800 10,1000 LLEN,logs 10000 30000",
-    service_description => 'redis server',
+    check_command       => "check_nrpe!check_redis!${redis_port} 1,2 80,90 blocked_clients,connected_clients 5,800 10,1000",
+    service_description => 'redis server health',
+    host_name           => $::fqdn,
+    document_url        => 'https://github.gds/pages/gds/opsmanual/2nd-line/nagios.html#redis-server-check',
+  }
+  @@nagios::check { "check_redis_list_logs${::hostname}":
+    check_command       => "check_nrpe!check_redis_list!${redis_port} LLEN,logs 10000 30000",
+    service_description => 'redis list length for logs',
     host_name           => $::fqdn,
     document_url        => 'https://github.gds/pages/gds/opsmanual/2nd-line/nagios.html#redis-server-check',
   }
--- modules/govuk/manifests/node/s_redis.pp
+++ modules/govuk/manifests/node/s_redis.pp
@@ -35,9 +35,8 @@ class govuk::node::s_redis {
     #   - warns if using >80% of system memory, critical if >90%
     #   - warns if >5 blocked clients, critical if >10
     #   - warns if >800 connected_clients, critical if >1000 connected_clients
-    #   - warns if >10000 list length of logs, critical if >30000 list length of logs
-    check_command       => "check_nrpe!check_redis!${redis_port} 1,2 80,90 blocked_clients,connected_clients 5,800 10,1000 LLEN,logs 10000 30000",
-    service_description => 'redis server',
+    check_command       => "check_nrpe!check_redis!${redis_port} 1,2 80,90 blocked_clients,connected_clients 5,800 10,1000",
+    service_description => 'redis server health',
     host_name           => $::fqdn,
     document_url        => 'https://github.gds/pages/gds/opsmanual/2nd-line/nagios.html#redis-server-check',
   }
