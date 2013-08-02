d33083d338186b4859c87f27315b1d63bdc5aade
Dan Carley <dan.carley@gmail.com>
2013-08-02 15:23:42 +0100

    [#54213626] Passive check for offsite backups
    Same implementation as 32187dd.

--- modules/backup/manifests/offsite.pp
+++ modules/backup/manifests/offsite.pp
@@ -8,10 +8,19 @@ class backup::offsite {
     require => File['/usr/local/bin/offsite-backup'],
   }
 
+  $threshold_secs = 28 * (60 * 60)
+  # Also used in template.
+  $service_desc   = 'offsite backup'
+
   file { '/usr/local/bin/offsite-backup':
-    ensure => present,
-    source => 'puppet:///modules/backup/usr/local/bin/offsite-backup',
-    mode   => '0755',
+    ensure  => present,
+    content => template('backup/usr/local/bin/offsite-backup.erb'),
+    mode    => '0755',
   }
 
+  @@nagios::passive_check { "check_backup_offsite-${::hostname}":
+    service_description => $service_desc,
+    host_name           => $::fqdn,
+    freshness_threshold => $threshold_secs,
+  }
 }
