2d2e900b63df90079cbfc4cd29aade7945006626
Tim Abell <tim@timwise.co.uk>
2013-08-02 15:09:07 +0100

    Show the repo & process count when bundle-updating
    Also added an option to restrict to a single process for
    troubleshooting.
    I've made this change because as a new person I hadn't realized that the
    output is interleaved, and that makes troubleshooting harder.

--- development-vm/update-bundler.sh
+++ development-vm/update-bundler.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# To troubleshoot use the following invocation to avoid interleaved output:
+#  PROC_COUNT=1 ./update-bundler.sh
 set -e
 
 cd "$(dirname "$0")"
@@ -8,6 +10,11 @@ find_repos () {
   ls -d ../*/Gemfile | cut -d/ -f2
 }
 
+
 # Run in parallel with 4 processes. This is primarily network-bound, so even
 # with a single core -P4 is faster than -P<numcores>.
-find_repos | xargs -P4 -n1 ./single-update-bundler.sh
+: ${PROC_COUNT:=4}
+if [ $PROC_COUNT -gt 1 ]; then
+	echo "Running in $PROC_COUNT processes; output will be interleaved."
+fi
+find_repos | xargs -P$PROC_COUNT -n1 ./single-update-bundler.sh
