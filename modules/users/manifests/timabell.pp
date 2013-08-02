16f51bcb19e82f56a1ff2760ca1d82856fccf3fc
Tim Abell <tim@timwise.co.uk>
2013-08-02 16:14:56 +0100

    Tim Abell - ssh key for new laptop

--- modules/users/manifests/timabell.pp
+++ modules/users/manifests/timabell.pp
@@ -2,6 +2,6 @@ class users::timabell {
   govuk::user { 'timabell':
     fullname => 'Tim Abell',
     email    => 'tim.abell@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7PrWwqGfhXePnjab8yWX9ZkuH+DzNGb33gXfOdaZdF6+Dua1VKt02E9H5dfcTM3z+QADuK/MhYNDb4a4TKzIAf3Rywgzt8sJw43tAsmqsvU6ztVnjSLsCeLJ3atwL6371866/5hbmArliBWOdDR5mlrM0sE7VT0q7FkzVcH9FPSh49L5qyTpP4ZSWLbbgeL62Yx+bqDyAc0kiaCJkKXhWZQsF/e6gQCCqX1TTzJuTOrKho+a9ZWYuxvbDFpdfZPUI48WdNqjEARI3XuqueGibSMKL6A5wTJv9OGEm1fo2hmzh/s1Vv5YhGs9eu5RSumMWEuJE3/tDVp66VY+mmBtT',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDa3nwhv6kdAiQBqO8O4nKmwuIcfW3NzcUnfqfRggnNRVfWyuOj8b5ebpA51aModyqiin9gITFJcxWhlQkbefc2ED3r2QdwTCUpnUo3u1wDrVmlE/bX0xFhNzPrF9c8JGvqfgHEmFdd8Er6gpTD/pxFRPPRngjQTnTAfxXPCIxc/QPXqkDT7Yx4wZ/5E+zRklmz6XlA17eFKHx5Kly58zqBlYDnQhzJgiH3A0cWIxeeFohJa+U3FQ2E19JU/lhANgMMYC9ee+KbyPVmq7q0V9diIWCjXiPbvuHqL3mskgb3/9L4A4IeVzOFRznTDrx7bVmbTdp8mUA8tG0IdsEesMON tim@sammy',
   }
 }
