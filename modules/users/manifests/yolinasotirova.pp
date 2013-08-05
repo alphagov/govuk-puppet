88851a0b76abcf72c6d5b003aae0d4e41168ae1f
James Abley <james.abley@gmail.com>
2013-08-05 16:10:13 +0100

    Remove Yolina Sotirova (Thoughtworks)

--- modules/users/manifests/yolinasotirova.pp
+++ modules/users/manifests/yolinasotirova.pp
@@ -1,7 +1,3 @@
 class users::yolinasotirova {
-  govuk::user { 'yolinasotirova':
-    fullname => 'Yolina Sotirova',
-    email    => 'yolina.sotirova@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/xedNYf/OlOnimh6JRhpaSD+l9FUhH/2wLsbCB1eG2wbzCRyWqpAV/uFkQYam05TjohQ69E4k6ZAWC8YomXW+Y0V9O2nzRyNH8/AWkJNROXnof75Ojsv1BqIZwUCVWQ7ub+CTgZ5ix9XlcS/qShGkE/z3q40z1907iH1UxmS8IQ7uvIP5IzWkgsln2Uk2ccWKeBLcUYjaNcN81FfMZ3R8qNbB2iDrXflLvtO8DxJm8DH8thY6zdIiprbtNFXBJdTqBWG/GCgpMVEJEgTqBzywZR86Cohbb78LzCMn29bTmXjKLjhyBm6yrMULFvz3RCOjPlQq2o1MvmvP+NbyPcHv yolina.sotirova@gmail.com',
-  }
+  govuk::user { 'yolinasotirova': ensure => 'absent' }
 }
