695f99623736b4ea7e25197887cc6551ba22d202
Jacob Ashdown <jcbashdown@googlemail.com>
2013-08-06 12:01:15 +0100

    Add Jacob to puppet

---
+++ modules/users/manifests/jacobashdown.pp
@@ -0,0 +1,7 @@
+class users::jacobashdown {
+    govuk::user { 'jacobashdown':
+        fullname => 'Jacob Ashdown',
+            email    => 'jacob.ashdown@digital.cabinet-office.gov.uk',
+                ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5BrQ0S9MM4LNIoDZKMtFPqA99rQWaB29JHDobFbtt8nmoLXrrkED0UU6N+IQk4Mt9ewEo1xa1pMXsTTcHQbNCMX/3K+uYW2qYneVqJAKYl3m2M1amzSzJvUdnGE9S47brGy2Jq1MO8qZIr7vyHzo4FBwPOYW4vrRhHzmiDkMuAYfE0ubWSUzGWPKsyXHvsyCkYJ7jvw8aX0HW6n4Wyn3KXFfvPLYnB/dfAjAOylVBgA6vR9Dum91XXLW1xAnnAyHNCeLZMmVmYtPpjnybel2eP/BndY2JqnsSng5Q7GqoKvhfJqnPnT9hYwp4F8lIpxmnjfV22uqKnAP7KOJtxfS9'
+                  }
+}
