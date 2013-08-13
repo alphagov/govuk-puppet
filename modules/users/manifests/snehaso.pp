c7162a3b5834ea94cd07aa94ec2d7c16d54d2b46
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-08-13 13:57:16 +0100

    Added ssh key for my user

---
+++ modules/users/manifests/snehaso.pp
@@ -0,0 +1,7 @@
+class users::snehaso {
+  govuk::user { 'snehaso':
+    fullname => 'Sneha Somwanshi',
+    email    => 'sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwZvm5hbwOXMzMN+5xnWbasruTNe6BkzeyCeC9cJqO7vjZ51yAyoVizp8sqosbpwcrO601sGAvYM9+NzUC8mVnbbvcp1i6FvR2gdG6xASoVp4CkgTTy3OFGTmHJHh4P1gWWObTFSVH9ZhkQq3vtvHsewTJFve6dqDP7sYeJSw7ptnJ66EYdQmEstnTxp8RjSHpHi3DlpI/2x1svs/vHeyyhTMZJhYdxwb278PIdfukYsZsw5+WSFENnR15NO/Wh/8BPfICvvgIZrPAvuYKOlw+7o9uKoWMiM/hWRw670HykcZ0jRHc8dDIxq9uEzpOZnnbQx5D+Y+JvxWNXuT6DJrv sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk'
+  }
+}
