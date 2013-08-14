98c1a5cf0dcf974f7782ddcf1bee91895cf2c6a6
Jake Benilov <benilov@gmail.com>
2013-08-14 14:11:04 +0100

    enable json logs for support app

--- modules/govuk/manifests/apps/support.pp
+++ modules/govuk/manifests/apps/support.pp
@@ -5,6 +5,7 @@ class govuk::apps::support($port = 3031) {
     port               => $port,
     vhost_ssl_only     => true,
     health_check_path  => '/',
+    log_format_is_json => true,
     nginx_extra_config => '
       location /_status {
         allow   127.0.0.0/8;
