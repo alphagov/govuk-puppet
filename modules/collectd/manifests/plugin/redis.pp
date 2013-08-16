da4012c4e01969265171b900ebaa7bcc99867104
Kushal Pisavadia <kushi.p@gmail.com>
2013-08-16 14:58:36 +0100

    [#44291865] Report stats on redis through collectd
    We don't have the redis plugin included with collectd despite it being
    available since v5.0. Instead, using a little Python script that uses
    socket to make a connection.
    
    Chosen this script instead of one that uses the redis python lib to
    reduce the number of dependencies. At the moment we're just using this
    to gather information on number of keys and objects in redis. If that
    changes we _could_ move to using a script that uses the redis python
    library.

---
+++ modules/collectd/manifests/plugin/redis.pp
@@ -0,0 +1,19 @@
+define collectd::plugin::redis(
+  $host = 'localhost',
+  $port = 6379,
+) {
+  include collectd::plugin::python
+
+  # Attribution: https://github.com/powdahound/redis-collectd-plugin
+  file { '/usr/lib/collectd/python/redis_info.py':
+    ensure  => present,
+    source  => 'puppet:///modules/collectd/usr/lib/collectd/python/redis_info.py',
+    tag     => 'collectd::plugin',
+    notify  => File['/etc/collectd/conf.d/redis.conf'],
+  }
+
+  collectd::plugin { 'redis':
+    content => template('collectd/etc/collectd/conf.d/redis.conf.erb'),
+    require => Class['collectd::plugin::python'],
+  }
+}
