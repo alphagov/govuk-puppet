d21ebc80dff6ef755bb606cc4467b09271a949fb
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-08-21 10:56:25 +0100

    [Transactions Explorer] Reset basic auth
    * Mostly reverts 4bd9e551a2c6d12ebe531a519f5cd75ead047946
    * Pull #877

--- modules/govuk/manifests/apps/transactions_explorer.pp
+++ modules/govuk/manifests/apps/transactions_explorer.pp
@@ -1,6 +1,4 @@
 class govuk::apps::transactions_explorer {
-  include govuk::htpasswd
-
   $app_domain = extlookup('app_domain')
   $app_name = 'transactions-explorer'
   $vhost_full = "${app_name}.${app_domain}"
@@ -16,7 +14,7 @@ class govuk::apps::transactions_explorer {
   # Whether to enable SSL. Used by template.
   $enable_ssl = str2bool(extlookup('nginx_enable_ssl', 'yes'))
   # Whether to enable basic auth protection. Used by template.
-  $enable_basic_auth = str2bool(extlookup('transactions_explorer_enable_basic_auth', 'yes'))
+  $enable_basic_auth = str2bool(extlookup('nginx_enable_basic_auth', 'yes'))
 
   include govuk::deploy
 
