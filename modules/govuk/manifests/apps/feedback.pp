22c052d613839d1289fb078b2c7fd308e031ad57
Jake Benilov <benilov@gmail.com>
2013-08-22 19:47:42 +0100

    Revert "Merge pull request #899 from gds/feedback-dont-intercept-503-for-report-a-problem-endpoint"
    This reverts commit 48d87fc8ff66042a746906b96002d562fdb0ab81, reversing
    changes made to d8e47abb26d2c2bbfef5797666792cb44f1b2b11.

--- modules/govuk/manifests/apps/feedback.pp
+++ modules/govuk/manifests/apps/feedback.pp
@@ -8,9 +8,5 @@ class govuk::apps::feedback(
     log_format_is_json => true,
     vhost_protected    => $vhost_protected,
     health_check_path  => '/feedback',
-    nginx_extra_config => '
-      location /report-a-problem {
-        proxy_intercept_errors off;
-      }'
   }
 }
