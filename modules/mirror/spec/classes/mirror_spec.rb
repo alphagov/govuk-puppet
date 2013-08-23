0cf9c1f51688ff9ac70bbf46e96c88986203bf17
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2013-08-23 11:30:05 +0100

    [#54676742] tests for mirror targets -
    govuk_mirror_targets can be a string , empty or an array. Added tests
    for this.

--- modules/mirror/spec/classes/mirror_spec.rb
+++ modules/mirror/spec/classes/mirror_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../../../spec_helper'
 
 describe 'mirror', :type => :class do
   before :each do
-    update_extdata({'govuk_gemfury_source_url' => 'https://some_token@gem.fury.io/govuk/',})
+    update_extdata({'govuk_gemfury_source_url' => 'https://some_token@gem.fury.io/govuk/'})
   end
 
   it do
@@ -18,4 +18,31 @@ describe 'mirror', :type => :class do
   it {
     should contain_file('/home/govuk-netstorage/.ssh/id_rsa').with_ensure('file')
   }
+
+  context "govuk mirror targets" do
+    context "govuk empty mirror targets" do
+      let(:facts) {{ :cache_bust => Time.now }} 
+      it {
+        update_extdata({'govuk_mirror_targets' => [], 'govuk_gemfury_source_url' => 'https://some_token@gem.fury.io/govuk/' })
+        should contain_file('/usr/local/bin/govuk_upload_mirror').with_content(/^TARGETS=""$/)
+      }
+    end
+
+     context "should render single string mirror target" do
+      let(:facts) {{ :cache_bust => Time.now }}
+        it  {
+          update_extdata({'govuk_mirror_targets' => 'user102@mirror102', 'govuk_gemfury_source_url' => 'https://some_token@gem.fury.io/govuk/'})
+          should contain_file('/usr/local/bin/govuk_upload_mirror').with_content(/^TARGETS="user102@mirror102"$/)
+        }
+    end
+
+     context "render govuk array mirror targets" do
+      let(:facts) {{ :cache_bust => Time.now }}
+        it {
+          update_extdata({'govuk_mirror_targets' => ['user101@mirror101', 'user102@mirror102'], 'govuk_gemfury_source_url' => 'https://some_token@gem.fury.io/govuk/'})
+          should contain_file('/usr/local/bin/govuk_upload_mirror').with_content(/^TARGETS="user101@mirror101 user102@mirror102"$/)
+        }
+     end
+  end
+
 end
