ee4c983c090a4fa1b97311310574bd0c7d78f5e7
James Abley <james.abley@gmail.com>
2013-08-27 06:48:15 +0100

    Fix typo

--- modules/clamav/manifests/run_freshclam.pp
+++ modules/clamav/manifests/run_freshclam.pp
@@ -3,7 +3,7 @@
 # Run freshclam after a new install in order to populate the databases that
 # clamav-daemon needs to run. This enables the service to start in the same
 # Puppet run as the package install. If it fails, then the freshclam daemon
-# should have run by the time Puppet next attempts to start clamav-daeon.
+# should have run by the time Puppet next attempts to start clamav-daemon.
 #
 class clamav::run_freshclam {
   exec { 'freshclam --quiet':
