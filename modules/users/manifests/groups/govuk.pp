0bd841c17c7358f6a7f0d864d42837f0a882b24f
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-08-31 23:20:09 +0100

    [gds-leavers:1601] Max Fliri

--- modules/users/manifests/groups/govuk.pp
+++ modules/users/manifests/groups/govuk.pp
@@ -51,7 +51,6 @@ class users::groups::govuk {
   include users::joshua
   include users::kushalp
   include users::leelongmore
-  include users::maxf
   include users::maxgriff
   include users::mazz
   include users::minglis
