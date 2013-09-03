ddba7d2b51f5f5302a07746c245cd993a70a783a
Tom Booth <tombooth@gmail.com>
2013-09-03 17:26:31 +0100

    [#51816839] Switch fqdn to hostname
    Because bundle exec rake told me to...

--- modules/govuk/manifests/apps/publisher.pp
+++ modules/govuk/manifests/apps/publisher.pp
@@ -15,7 +15,7 @@ class govuk::apps::publisher( $port = 3000 ) {
     content => template('govuk/local_authority_import_check.erb'),
   }
 
-  @@nagios::passive_check { "check-local-authority-data-importer-${::fqdn}":
+  @@nagios::passive_check { "check-local-authority-data-importer-${::hostname}":
     service_description => $service_desc,
     host_name           => $::fqdn,
     freshness_threshold => 28 * (60 * 60),
