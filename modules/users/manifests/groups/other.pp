785c3e55961b69c626d9b93985c154fb4df5f3d8
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-09-09 10:36:54 +0100

    Add new contractor Elliot Crosby-McCullough to SSH

--- modules/users/manifests/groups/other.pp
+++ modules/users/manifests/groups/other.pp
@@ -1,6 +1,7 @@
 class users::groups::other {
   include users::ben
   include users::benparker
+  include users::elliot
   include users::jolyonpawlyn
   include users::russellgarner
   include users::tekin
