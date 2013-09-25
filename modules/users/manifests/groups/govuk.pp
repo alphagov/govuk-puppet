6db10a08d30f61eea91a47e90c5cf6b7136d25b0
Jordan Hatch <jordan@jordanh.net>
2013-09-25 17:08:42 +0100

    Move stuartgale to other.pp

--- modules/users/manifests/groups/govuk.pp
+++ modules/users/manifests/groups/govuk.pp
@@ -71,7 +71,6 @@ class users::groups::govuk {
   include users::snehaso
   include users::ssharpe
   include users::stevelaing
-  include users::stuartgale
   include users::timpaul
   include users::tombooth
   include users::tombyers
