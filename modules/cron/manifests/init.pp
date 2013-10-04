8b1433c1d1ef7755bc6e93a2b3c1af898ae8c1f4
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-10-04 12:21:50 +0100

    Manage /etc/crontab and splay times across boxes
    The /etc/crontab file controls the timings of the periodic jobs.
    
    The ubuntu defaults are:
    
     - hourly: 17 minutes past the hour
     - daily: 0625
     - weekly: 0647
     - monthly: 0642
    
    Unfortunately that means all machines will run their jobs at the same
    time, which is not desirable for performance reasons.
    
    By using fdqn_rand() we can choose a value which is randomised across
    machines but it related to the hostname so consistent across puppet
    runs (and indeed machine rebuilds). Under the hood, fqdn_rand uses
    the puppet
    [deterministic_rand](https://github.com/puppetlabs/puppet/blob/master/lib/puppet/util.rb#L503-L513)
    function.

--- modules/cron/manifests/init.pp
+++ modules/cron/manifests/init.pp
@@ -15,4 +15,24 @@ class cron {
   service { 'cron':
     ensure => running,
   }
+
+  # set the timings of the scheduled tasks at different times
+  # for each machine, but at predicable times for the various
+  # schedules.
+  # Hourly:  01 -> 15 minutes past the hour
+  $hourly_time  = fqdn_rand(15) + 1
+  # Daily:   11 -> 25 minutes past the hour
+  $daily_time   = $hourly_time + 10
+  # Weekly:  26 -> 40 minutes past the hour
+  $weekly_time  = $hourly_time + 25
+  # Monthly: 41 -> 55 minutes past the hour
+  $monthly_time = $hourly_time + 40
+  file {'/etc/crontab':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('cron/etc/crontab.erb'),
+    notify  => Service['cron'],
+  }
 }
