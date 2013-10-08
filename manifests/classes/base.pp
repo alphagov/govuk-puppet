f11032e7aa51bc31c12e787a29459fee72710935
Dan Carley <dan.carley@gmail.com>
2013-10-08 15:08:39 +0100

    Reinstate Class['apt::update'] -> Package <||>
    Ensure that `apt-get update` is run before package installations. Rather
    than just after a new source/ppa has been setup. This is a workaround for
    modules which don't depend on their `apt::source`/`apt::ppa` resource.
    
    This includes two additional conditions to workaround a dependency loop
    within `apt::ppa`. I had hoped to use `tags != 'apt::ppa'` but that
    doesn't work.

--- manifests/classes/base.pp
+++ manifests/classes/base.pp
@@ -28,4 +28,16 @@ class base {
     ],
   }
 
+  # Ensure update is always run before any package installs.
+  # title conditions prevent a dependency loop within apt module.
+  Class['apt::update'] -> Package <|
+    provider != pip and
+    provider != gem and
+    ensure != absent and
+    ensure != purged and
+    title != 'python-software-properties' and
+    title != 'software-properties-common' and
+    tag != 'no_require_apt_update'
+  |>
+
 }
