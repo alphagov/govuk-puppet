67a3295c94123166d0c248b3f45970e14eb8bfa1
Anna Shipman <anna@annashipman.co.uk>
2013-10-08 12:45:40 +0100

    Remove .conf files
    We intall fail2ban which includes the fail2ban.conf and jail.conf, so we do not
    need to include our own versions. All we need are the .local files, containing
    our custom config.

--- modules/fail2ban/manifests/config.pp
+++ modules/fail2ban/manifests/config.pp
@@ -1,15 +1,7 @@
 class fail2ban::config {
 
-  file { '/etc/fail2ban/fail2ban.conf':
-    source => 'puppet:///modules/fail2ban/etc/fail2ban/fail2ban.conf',
-  }
-
   file { '/etc/fail2ban/fail2ban.local':
     source => 'puppet:///modules/fail2ban/etc/fail2ban/fail2ban.local',
-  }
-
-  file { '/etc/fail2ban/jail.conf':
-    source => 'puppet:///modules/fail2ban/etc/fail2ban/jail.conf',
   }
 
   file { '/etc/fail2ban/jail.local':
--- modules/fail2ban/spec/classes/config_spec.rb
+++ modules/fail2ban/spec/classes/config_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../../../spec_helper'
 
 describe 'fail2ban::config', :type => :class do
   it do
-    should contain_file('/etc/fail2ban/fail2ban.conf')
-    should contain_file('/etc/fail2ban/jail.conf')
+    should contain_file('/etc/fail2ban/fail2ban.local')
+    should contain_file('/etc/fail2ban/jail.local')
   end
 end
