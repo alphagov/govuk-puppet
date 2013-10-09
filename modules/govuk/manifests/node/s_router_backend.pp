ec29deeb09ba99135c4d5d7952a2751dbafd05bb
Dan Carley <dan.carley@gmail.com>
2013-10-09 12:21:44 +0100

    [#58439020] MongoDB on router-backend-*.router
    Three node replSet. Includes standard monitoring checks, but no backups.

--- modules/govuk/manifests/node/s_router_backend.pp
+++ modules/govuk/manifests/node/s_router_backend.pp
@@ -1,2 +1,10 @@
 class govuk::node::s_router_backend inherits govuk::node::s_base {
+  include mongodb::server
+  class { 'mongodb::configure_replica_set':
+    members => [
+      'router-backend-1.router',
+      'router-backend-2.router',
+      'router-backend-3.router',
+    ],
+  }
 }
