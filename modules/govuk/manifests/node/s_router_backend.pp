7c9cabb7bfaa8c9ba7505c3ed06dea0de7085696
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-10-15 09:52:21 +0000

    Add router_api to router_backend nodes

--- modules/govuk/manifests/node/s_router_backend.pp
+++ modules/govuk/manifests/node/s_router_backend.pp
@@ -7,4 +7,15 @@ class govuk::node::s_router_backend inherits govuk::node::s_base {
       'router-backend-3.router',
     ],
   }
+
+  include govuk::node::s_ruby_app_server
+
+  if str2bool(extlookup('govuk_enable_router_api', 'no')) {
+    include govuk::apps::router_api
+  }
+
+  include nginx
+
+  # If we miss all the apps, throw a 500 to be caught by the cache nginx
+  nginx::config::vhost::default { 'default': }
 }
