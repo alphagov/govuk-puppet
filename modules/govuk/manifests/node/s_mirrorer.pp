2567e01f8e0dba1650c6127cb9de66866c9b22a3
Dan Carley <dan.carley@gmail.com>
2013-10-15 15:05:10 +0100

    Disable some Akamai stuff
    We are no longer an Akamai customer. So disable some stuff:
    
    - Access log collection (and backup).
    - Event log collection.
    - __testobj__ static file.
    - NetStorage uploads.
    - Comment referencing NetStorage failover.
    
    The `akamai-logs` and `akamai-logs-backup` nodes will remain with any
    existing data. They just won't copy new data.

--- modules/govuk/manifests/node/s_mirrorer.pp
+++ modules/govuk/manifests/node/s_mirrorer.pp
@@ -1,9 +1,7 @@
 class govuk::node::s_mirrorer inherits govuk::node::s_base {
-  $enable_akamai_event_data_logging = str2bool(extlookup('enable_akamai_event_data_logging', 'no'))
-
   include mirror
 
   class { 'akamai::event_data':
-    enable  => $enable_akamai_event_data_logging,
+    enable  => false,
   }
 }
