21974126c4bc47890c4628bbbf4976ef5f14c31d
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-10-28 15:04:36 +0000

    Add Logstream capturing to Imminence

--- modules/govuk/manifests/apps/imminence.pp
+++ modules/govuk/manifests/apps/imminence.pp
@@ -1,9 +1,11 @@
 class govuk::apps::imminence( $port = 3002 ) {
   govuk::app { 'imminence':
-    app_type          => 'rack',
-    port              => $port,
-    vhost_ssl_only    => true,
-    health_check_path => '/',
+    app_type           => 'rack',
+    port               => $port,
+    vhost_ssl_only     => true,
+    health_check_path  => '/',
+    logstream          => true,
+    log_format_is_json => true,
   }
 
   govuk::delayed_job::worker { 'imminence': }
