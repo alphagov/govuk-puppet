a08dbf27d5223e7d79a15bfab9cb6613f401c439
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-11-08 13:15:35 +0000

    Enable logstash JSON streaming for router-api
    Not to be merged until https://github.com/alphagov/router-api/pull/33 is
    merged and deployed.

--- modules/govuk/manifests/apps/router_api.pp
+++ modules/govuk/manifests/apps/router_api.pp
@@ -1,9 +1,10 @@
 class govuk::apps::router_api( $port = 3056 ) {
   govuk::app { 'router-api':
-    app_type          => 'rack',
-    port              => $port,
-    vhost_ssl_only    => true,
-    health_check_path => '/healthcheck',
+    app_type           => 'rack',
+    port               => $port,
+    vhost_ssl_only     => true,
+    health_check_path  => '/healthcheck',
+    log_format_is_json => true,
   }
 
   unless $::govuk_platform == 'development' {
