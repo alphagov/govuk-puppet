d64c938fb935678b19d23dcd8be789a385cba7c2
Dan Carley <dan.carley@gmail.com>
2013-11-08 17:11:42 +0000

    [#60420920] Feature flag Router redirects
    New feature flag to allow the Router to serve redirects instead of handling
    them in Nginx above. Needs to be used together with the Varnish feature
    flag, which is enabled in preview, otherwise the Router won't see the
    request.
    
    All this code can be removed a short while after launch.

--- modules/router/spec/classes/router__nginx_spec.rb
+++ modules/router/spec/classes/router__nginx_spec.rb
@@ -49,21 +49,48 @@ describe 'router::nginx', :type => :class do
       :vhost_protected => false,
       :real_ip_header  => 'X-Some-Address-Header',
     }}
-    it {
-      should contain_file(routes_path).with_content(/location \~ \^\/cloudstore/)
-      should contain_file(routes_path).with_content(/return 302 http:\/\/gcloud.civilservice\.gov\.uk\/cloudstore\//)
-    }
-    it {
-      should contain_file(routes_path).with_content(/location \~\* \^\/aaib/)
-      should contain_file(routes_path).with_content(/return 301 \/government\/organisations\/air-accidents-investigation-branch/)
-    }
-    it {
-      should contain_file(routes_path).with_content(/location \~ \^\/green-deal/)
-      should contain_file(routes_path).with_content(/return 301 \/green-deal-energy-saving-measures/)
-    }
-    it {
-      should contain_file(routes_path).with_content(/location \~ \^\/adoption-leave/)
-      should contain_file(routes_path).with_content(/return 301 \/adoption-pay-leave/)
-    }
+    let(:facts) {{
+      :cache_bust => Time.now,
+    }}
+
+    context 'enable_router true' do
+      before(:each) { update_extdata({
+        'fco_services_domain_prefix'    => 'www-test',
+        'govuk_enable_router_varnish'   => 'yes',
+        'govuk_enable_router_redirects' => 'yes',
+      })}
+
+      it { should_not contain_file(routes_path).with_content(/rewrite\s+/) }
+      it { should_not contain_file(routes_path).with_content(/return 30\d\s+/) }
+    end
+
+    context 'enable_router false (missing one feature flag)' do
+      before(:each) { update_extdata({
+        'fco_services_domain_prefix'    => 'www-test',
+        'govuk_enable_router_redirects' => 'yes',
+      })}
+
+      it { should contain_file(routes_path).with_content(/rewrite\s+/) }
+      it { should contain_file(routes_path).with_content(/return 30\d\s+/) }
+    end
+
+    context 'enable_router false (default)' do
+      it {
+        should contain_file(routes_path).with_content(/location \~ \^\/cloudstore/)
+        should contain_file(routes_path).with_content(/return 302 http:\/\/gcloud.civilservice\.gov\.uk\/cloudstore\//)
+      }
+      it {
+        should contain_file(routes_path).with_content(/location \~\* \^\/aaib/)
+        should contain_file(routes_path).with_content(/return 301 \/government\/organisations\/air-accidents-investigation-branch/)
+      }
+      it {
+        should contain_file(routes_path).with_content(/location \~ \^\/green-deal/)
+        should contain_file(routes_path).with_content(/return 301 \/green-deal-energy-saving-measures/)
+      }
+      it {
+        should contain_file(routes_path).with_content(/location \~ \^\/adoption-leave/)
+        should contain_file(routes_path).with_content(/return 301 \/adoption-pay-leave/)
+      }
+    end
   end
 end
