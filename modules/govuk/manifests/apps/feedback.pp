4e668b19dea2ff99371182f554170d95f7aaff10
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-11-15 14:30:14 +0000

    Fix feedback healthcheck (redirected by router)

--- modules/govuk/manifests/apps/feedback.pp
+++ modules/govuk/manifests/apps/feedback.pp
@@ -7,6 +7,6 @@ class govuk::apps::feedback(
     port               => $port,
     log_format_is_json => true,
     vhost_protected    => $vhost_protected,
-    health_check_path  => '/feedback',
+    health_check_path  => '/contact',
   }
 }
