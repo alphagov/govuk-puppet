528a2279fcc5c2892a8c94f4bb1f2f1547a2a063
Dan Carley <dan.carley@gmail.com>
2013-11-18 15:12:44 +0000

    Remove MySQL from MongoDB nodes
    EC2 has been deprecated. Thus no longer needed, per comment.
    
    We'll either need to manually uninstall it from existing nodes or just
    allow this to happen as part of the Platform1 migration - if the affected
    machines haven't already been provisioned there.

--- modules/govuk/manifests/node/s_mongo.pp
+++ modules/govuk/manifests/node/s_mongo.pp
@@ -1,8 +1,5 @@
 class govuk::node::s_mongo inherits govuk::node::s_base {
   include mongodb::server
-  #TODO: When Preview is no longer in EC2, the Mongo Servers
-  #should not include the s_mysql_master class
-  include govuk::node::s_mysql_master
   include mongodb::backup
 
   case $::govuk_provider {
