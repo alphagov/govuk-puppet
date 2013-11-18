bebddb769c95a3fe21b4f67304a0ccada0bc2065
Vinay Patel <mail@vinayvinay.com>
2013-11-18 16:03:19 +0000

    Adding a manifest for a new account for production/preview access for Vinay Patel.

---
+++ modules/users/manifests/vinayvinay.pp
@@ -0,0 +1,7 @@
+class users::vinayvinay {
+  govuk::user { 'vinayvinay':
+    fullname => 'Vinay Patel',
+    email    => 'vinay.patel@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLXxwMdahbk06KRxF0745rnHQ4JuIwDP+NOTZnLWaGyfIK5eBbPT9wP8M3g4jyAwSlFojd9JZFDOVkWRBU1UtLw9SmiTIGQc1xScMTb/YpgoLUReMxoJPgD4erpkwQz401iHMov+Qlfxi7zQtO/8XhjdILcY58Qnivq4g4IrwfLqIZc3hTPRArW9WTzHd/g2ATGhAuzQAZuzzrrT3UUsPoXOGIAANhw26mc1cFxuN7gbNyq2a3/epAO3U7xAmyUfq+cd0flQUoIYmVE94HjTtWS4fVZlP2kYqycm6XUjlPZMlIB4mRjQV12uC5Uinl+famPdn5exW3uegQC9PAD4bP vinay.patel@digital.cabinet-office.gov.uk',
+  }
+}
