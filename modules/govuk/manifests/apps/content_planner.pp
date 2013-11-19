7f58e152f0fb00b307e263e21e328d8d1a63fb5c
Matthew Ford <matt@bitzesty.com>
2013-11-19 16:05:37 +0000

    typo in content-planner subdomain

--- modules/govuk/manifests/apps/content_planner.pp
+++ modules/govuk/manifests/apps/content_planner.pp
@@ -1,5 +1,5 @@
 class govuk::apps::content_planner($port = 3058) {
-  govuk::app { 'content_planner':
+  govuk::app { 'content-planner':
     app_type          => 'rack',
     port              => $port,
     health_check_path => '/healthcheck',
