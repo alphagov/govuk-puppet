83753d2af495ed6003d2e669636e43cdbb09b00b
James Abley <james.abley@gmail.com>
2013-12-02 12:18:09 +0000

    Enable tariff-admin everywhere

--- modules/govuk/manifests/apps/tariff_admin/db.pp
+++ modules/govuk/manifests/apps/tariff_admin/db.pp
@@ -1,13 +1,11 @@
 class govuk::apps::tariff_admin::db {
 
-  if str2bool(extlookup('govuk_enable_tariff_admin', 'no')) {
-    $tariff_admin_password = extlookup('mysql_tariff_admin', '')
-    $mysql_password = extlookup('mysql_root', '')
+  $tariff_admin_password = extlookup('mysql_tariff_admin', '')
+  $mysql_password = extlookup('mysql_root', '')
 
-    mysql::server::db { 'tariff_admin_production':
-      user          => 'tariff_admin',
-      password      => $tariff_admin_password,
-      root_password => $mysql_password,
-    }
+  mysql::server::db { 'tariff_admin_production':
+    user          => 'tariff_admin',
+    password      => $tariff_admin_password,
+    root_password => $mysql_password,
   }
 }
