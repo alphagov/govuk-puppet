6567b81cc1d11edb1ab09954cac431a28411b437
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-12-05 09:02:05 +0000

    Remove www alias for frontend application.
    Nothing is using this, and it's only misleading.  www should always
    point at the cache layer, not at an individual application.

--- modules/govuk/manifests/apps/frontend.pp
+++ modules/govuk/manifests/apps/frontend.pp
@@ -9,17 +9,8 @@ class govuk::apps::frontend(
     app_type               => 'rack',
     port                   => $port,
     vhost_protected        => $vhost_protected,
-    vhost_aliases          => ['private-frontend', 'www'], # TODO: Remove the www alias once we're sure it's not being used.
+    vhost_aliases          => ['private-frontend'],
     health_check_path      => '/',
     log_format_is_json     => true,
-  }
-
-  # Frontend used to be deployed to /data/vhost/www.${app_domain}. Leave this
-  # symlink in place until we're sure that nothing needs it any more.
-  file { "/data/vhost/www.${app_domain}":
-    ensure => link,
-    target => "/data/vhost/frontend.${app_domain}",
-    owner  => 'deploy',
-    group  => 'deploy',
   }
 }
