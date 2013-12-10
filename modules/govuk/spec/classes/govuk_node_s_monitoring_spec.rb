e9605c1d1e4bf617c3cfb37cf49b73bbecdce688
Dan Carley <dan.carley@gmail.com>
2013-12-10 13:03:19 +0000

    [#61532234] Hiera for Nagios Campfire/PagerDuty
    Move the configuration of Nagios contact groups for Campfire and PagerDuty
    out to Hiera. This namespaces the keys correctly to this class, making it
    more obvious where they are used, as well as renaming them to better reflect
    their purpose.
    
    Simplified the logic in the manifest by using separate variables and
    puppetlabs-stdlib `flatten()` function to compose a list of lists. This
    means we no longer have nested conditions.
    
    The move to hiera and class params means that we can now test this as a
    normal class. I've added contexts for three common use cases. There is some
    missing coverage, such as absent Campfire credentials, but the tests are so
    slow (3s per context) that I don't think there's value covering them all.
    
    Depends on gds/deployment#90

---
+++ modules/govuk/spec/classes/govuk_node_s_monitoring_spec.rb
@@ -0,0 +1,64 @@
+require_relative '../../../../spec_helper'
+
+shared_examples 'configured contact groups' do |urgent, high, normal|
+  it { should contain_nagios__contact_group('urgent-priority').with(
+    :members => urgent
+  )}
+  it { should contain_nagios__contact_group('high-priority').with(
+    :members => high
+  )}
+  it { should contain_nagios__contact_group('normal-priority').with(
+    :members => normal
+  )}
+end
+
+describe 'govuk::node::s_monitoring', :type => :class do
+  let(:facts) {{
+    :osfamily        => 'Debian',
+    :operatingsystem => 'Ubuntu',
+    :lsbdistcodename => 'Precise',
+  }}
+
+  context 'param defaults' do
+    it { should_not contain_nagios__campfire_contact('campfire_notification') }
+
+    it_should_behave_like 'configured contact groups',
+      ['monitoring_google_group'],
+      ['monitoring_google_group'],
+      ['monitoring_google_group']
+  end
+
+  context 'notify_pager => true' do
+    let(:params) {{
+      :notify_pager => true,
+    }}
+
+    it { should_not contain_nagios__campfire_contact('campfire_notification') }
+
+    it_should_behave_like 'configured contact groups',
+      ['monitoring_google_group', 'pager_nonworkhours'],
+      ['monitoring_google_group'],
+      ['monitoring_google_group']
+  end
+
+  context 'notify_pager => true, notify_campfire => true, campfire creds' do
+    let(:params) {{
+      :notify_pager       => true,
+      :notify_campfire    => true,
+      :campfire_token     => 'peach',
+      :campfire_room      => 'pear',
+      :campfire_subdomain => 'plum',
+    }}
+
+    it { should contain_nagios__campfire_contact('campfire_notification').with(
+      :campfire_token     => 'peach',
+      :campfire_room      => 'pear',
+      :campfire_subdomain => 'plum'
+    )}
+
+    it_should_behave_like 'configured contact groups',
+      ['monitoring_google_group', 'campfire_notification', 'pager_nonworkhours'],
+      ['monitoring_google_group', 'campfire_notification'],
+      ['monitoring_google_group', 'campfire_notification']
+  end
+end
