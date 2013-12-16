a9d02c7f16fa0215ad4a75d26f75bbbaadbd4b5d
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-12-16 09:07:58 +0000

    Paulo and Oliver have left
    Zendesk ticket #2519 and #2520

--- modules/users/manifests/ole.pp
+++
@@ -1,7 +0,0 @@
-class users::ole {
-  govuk::user { 'ole':
-    fullname => 'Oliver Spindler',
-    email    => 'oliver.spindler@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwQOhkvbnwnMWYcOMkThvB+hnfwjXOxLFE25q6/f1cxsDWaeZyrjClLk9pqzrmIkn/Yh+BdbRtQbSMNQpcj9Om5uLxCwKemWvAji3ZbD+G8W8MPzook0rzAhUoMFWKaoPQUju0AjeRmbA4eq/Kn+FqIQQ66ZJ+WvnnYQjVZm17L/2m9gxE/mMbPStqkZgYYt6Sx8pm76U8l7+Vbw4CiBkDbtZorJERJn/7LJWLYhWE9kTwgrIKYVwD23tlBg1b+9ZQ+YCBU2MiPtXpTwzFx3CO/gpZ6584gQ1+cfe0BVS09iKfVkeRrUUoSuzAxPNc/1y7yb0oocWouhZJyj3fGb94w==',
-  }
-}
