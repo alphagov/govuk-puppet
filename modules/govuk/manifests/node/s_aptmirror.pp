a424de5d59bfc3d28d0204183983eb50ddd92cf0
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2013-12-18 17:24:16 +0000

    Added precise, created vhost

--- modules/govuk/manifests/node/s_aptmirror.pp
+++ modules/govuk/manifests/node/s_aptmirror.pp
@@ -1,9 +1,24 @@
 class govuk::node::s_aptmirror inherits govuk::node::s_base {
+  include nginx
   class { 'aptmirror':
-    servers => {
-    'http://apt.puppetlabs.com' => {
-      'lucid' => ['main', 'devel']
+      servers => {
+        'http://apt.puppetlabs.com' => {
+          'lucid'   => ['main', 'devel'],
+          'precise' => ['main', 'devel'],
+        }
       }
-      }
-    }
   }
+  nginx::config::site { 'puppet-aptmirror':
+    content => "
+      server {
+        listen   80;
+        root /var/spool/apt-mirror/mirror/apt.puppetlabs.com;
+        server_name aptmirror.production.alphagov.co.uk;
+        location / {
+          autoindex on;
+          try_files \$uri \$uri/ =404;
+        }
+      }
+    "
+  }
+}
