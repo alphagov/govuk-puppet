7e3400eac4943b383ad24a6ebebd2a8fc4461567
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-12-18 13:51:46 +0000

    Enable collection of contentapi json log

--- modules/govuk/manifests/apps/contentapi.pp
+++ modules/govuk/manifests/apps/contentapi.pp
@@ -7,6 +7,7 @@ class govuk::apps::contentapi (
     port               => $port,
     vhost_protected    => $vhost_protected,
     health_check_path  => '/search.json?q=tax',
+    log_format_is_json => hiera('govuk_enable_contentapi_json_log', false),
     nginx_extra_config => 'proxy_set_header API-PREFIX $http_api_prefix;',
   }
 }
