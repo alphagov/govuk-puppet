2f45511ec19cc660752e0893fe57bb3d6d2ef073
Dan Carley <dan.carley@gmail.com>
2014-01-01 12:22:16 +0000

    Replace Graphite module and upgrade
    Use newly released vendored gds-operations module. This will upgrade
    Graphite from 0.9.10 to 0.9.12, which includes features, security fixes, and
    will allow us to tune down logging. Configs copied out of old module so that
    the setup will remain the same, incl. aggregator
    
    There are some tidyup resources to remove key parts of the old Graphite
    installation. I'm not going to worry about other dependency packages though.
    These can be removed once deployed to prod.
    
    I've verified that this will preserve our existing metrics and saved
    dashboards. However temporary dashboards will probably be lost, which is OK.

--- modules/govuk/manifests/node/s_graphite.pp
+++ modules/govuk/manifests/node/s_graphite.pp
@@ -1,13 +1,36 @@
 class govuk::node::s_graphite inherits govuk::node::s_base {
-  class {'graphite':
-    require => Govuk::Mount['/opt/graphite'],
-  }
-
   govuk::mount { '/opt/graphite':
     nagios_warn  => 10,
     nagios_crit  => 5,
     mountoptions => 'defaults',
     disk         => '/dev/sdb1',
+  }
+
+  # FIXME: Remove tidyup resources.
+  exec { 'stop-old_carbon_aggregator':
+    command => 'stop carbon_aggregator && rm /etc/init/carbon_aggregator.conf',
+    onlyif  => 'test -f /etc/init/carbon_aggregator.conf',
+  } ->
+  exec { 'stop-old-carbon_cache':
+    command => 'stop carbon_cache && rm /etc/init/carbon_cache.conf',
+    onlyif  => 'test -f /etc/init/carbon_cache.conf',
+  } ->
+  exec { 'stop_old_graphite':
+    command => 'stop graphite && rm /etc/init/graphite.conf',
+    onlyif  => 'test -f /etc/init/graphite.conf',
+  } ->
+  package { ['python-whisper', 'python-carbon', 'python-graphite']:
+    ensure => absent,
+  } ->
+
+  class { 'graphite':
+    port                       => '33333',
+    carbon_aggregator          => true,
+    aggregation_rules_source   => 'puppet:///modules/govuk/node/s_graphite/aggregation-rules.conf',
+    storage_aggregation_source => 'puppet:///modules/govuk/node/s_graphite/storage-aggregation.conf',
+    storage_schemas_source     => 'puppet:///modules/govuk/node/s_graphite/storage-schemas.conf',
+    carbon_source              => 'puppet:///modules/govuk/node/s_graphite/carbon.conf',
+    require                    => Govuk::Mount['/opt/graphite'],
   }
 
   @@nagios::check { "check_carbon_cache_running_on_${::hostname}":
