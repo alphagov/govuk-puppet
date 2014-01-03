105740bb5767c4c4e6490dc7f6a40f9d2b1f27a2
Matt Bostock <matt.bostock@digital.cabinet-office.gov.uk>
2014-01-03 10:14:02 +0000

    [#60945276] Concern apt check only with security
    Changes the apt nagios check to only check for security updates.
    
    Also reduces the threshold at which point an alert is triggered; any
    more than zero pending security updates will trigger a critical alert.
    
    Waits 24 hours before triggering an alert, to allow unattended-upgrades
    time to run.

--- modules/monitoring/manifests/client/apt.pp
+++ modules/monitoring/manifests/client/apt.pp
@@ -4,23 +4,24 @@ class monitoring::client::apt {
   #   of some packages in order to request that the system be rebooted at a
   #   convenient time. Required by the `check_reboot_required` alert, but
   #   still beneficial to have on nodes that aren't monitored by Nagios.
-  # - `apt-check` which is called by the `check_apt_updates` alert. More
-  #   reliable than the builin Nagios plugin.
+  # - `apt-check` which is called by the `check_apt_security_updates` alert.
+  #   More reliable than the builtin Nagios plugin.
   package { 'update-notifier-common':
     ensure => present,
   }
 
-  @nagios::nrpe_config { 'check_apt_updates':
-    source => 'puppet:///modules/monitoring/etc/nagios/nrpe.d/check_apt_updates.cfg',
+  @nagios::nrpe_config { 'check_apt_security_updates':
+    source => 'puppet:///modules/monitoring/etc/nagios/nrpe.d/check_apt_security_updates.cfg',
   }
-  @nagios::plugin { 'check_apt_updates':
-    source  => 'puppet:///modules/monitoring/usr/lib/nagios/plugins/check_apt_updates',
+  @nagios::plugin { 'check_apt_security_updates':
+    source  => 'puppet:///modules/monitoring/usr/lib/nagios/plugins/check_apt_security_updates',
     require => Package['update-notifier-common'],
   }
-  @@nagios::check { "check_apt_updates_${::hostname}":
-    check_command       => 'check_nrpe!check_apt_updates!200 1000',
-    service_description => 'outstanding package updates',
-    host_name           => $::fqdn,
+  @@nagios::check { "check_apt_security_updates_${::hostname}":
+    check_command             => 'check_nrpe!check_apt_security_updates!0',
+    service_description       => 'outstanding security updates',
+    host_name                 => $::fqdn,
+    first_notification_delay  => 1440, # Wait 24hrs to allow unattended-upgrades to run first
   }
 
   @nagios::nrpe_config { 'check_reboot_required':
