41fd9df6251160bdfb68423efdd4c7263539da74
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-01-06 15:40:17 +0000

    Maslow is logging in JSON format as well.

--- modules/govuk/manifests/apps/maslow.pp
+++ modules/govuk/manifests/apps/maslow.pp
@@ -1,8 +1,9 @@
 class govuk::apps::maslow( $port = 3053 ) {
   govuk::app { 'maslow':
-    app_type          => 'rack',
-    port              => $port,
-    vhost_ssl_only    => true,
-    health_check_path => '/healthcheck',
+    app_type           => 'rack',
+    port               => $port,
+    vhost_ssl_only     => true,
+    health_check_path  => '/healthcheck',
+    log_format_is_json => true,
   }
 }
