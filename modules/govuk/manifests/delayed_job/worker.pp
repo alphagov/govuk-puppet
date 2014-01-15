8d93104e560ed3da7113c70b045cfdbfdd263c8a
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-01-15 11:01:47 +0000

    Add Nagios checks for workers' upstart status

--- modules/govuk/manifests/delayed_job/worker.pp
+++ modules/govuk/manifests/delayed_job/worker.pp
@@ -19,4 +19,12 @@ define govuk::delayed_job::worker (
   service { $service_name:
     ensure  => $enable_service,
   }
+
+  if $enable_service {
+    @@nagios::check { "check_app_${title}_delayed_job_worker_upstart_up_${::hostname}":
+      check_command       => "check_nrpe!check_upstart_status!${service_name}",
+      service_description => "${title} delayed job worker upstart up",
+      host_name           => $::fqdn,
+    }
+  }
 }
--- modules/govuk/manifests/procfile/worker.pp
+++ modules/govuk/manifests/procfile/worker.pp
@@ -19,4 +19,12 @@ define govuk::procfile::worker (
   service { $service_name:
     ensure  => $enable_service,
   }
+
+  if $enable_service {
+    @@nagios::check { "check_app_${title}_procfile_worker_upstart_up_${::hostname}":
+      check_command       => "check_nrpe!check_upstart_status!${service_name}",
+      service_description => "${title} procfile worker upstart up",
+      host_name           => $::fqdn,
+    }
+  }
 }
