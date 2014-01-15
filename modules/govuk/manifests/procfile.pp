4f9c9bfd16619d6db31996811046a01b74d933d6
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-01-15 10:53:45 +0000

    Remove non-applicable comment
    Was copy/pasted from delayed_job class.

--- modules/govuk/manifests/procfile.pp
+++ modules/govuk/manifests/procfile.pp
@@ -1,7 +1,4 @@
 class govuk::procfile {
-  # One word of caution: this is a generic worker that runs a Rake task,
-  # rather than necessarily being tied to the `delayed_job` library.
-
   file { '/usr/local/bin/govuk_run_procfile_worker':
     ensure  => present,
     source  => 'puppet:///modules/govuk/bin/govuk_run_procfile_worker',
