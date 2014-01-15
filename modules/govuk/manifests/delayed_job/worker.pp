eea2ea6a0656b8ce87e1fa1105111c9c5f8aaff6
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-01-15 10:32:24 +0000

    Refactor worker defines to avoid string repetition

--- modules/govuk/manifests/delayed_job/worker.pp
+++ modules/govuk/manifests/delayed_job/worker.pp
@@ -3,19 +3,20 @@ define govuk::delayed_job::worker (
 ) {
   include govuk::delayed_job
 
+  $service_name = "${title}-delayed-job-worker"
   $enable_service = $::govuk_platform ? {
     'development' => false,
     default       => true,
   }
 
-  file { "/etc/init/${title}-delayed-job-worker.conf":
+  file { "/etc/init/${service_name}.conf":
     ensure    => present,
     content   => template('govuk/delayed-job-worker.conf.erb'),
-    notify    => Service["${title}-delayed-job-worker"],
+    notify    => Service[$service_name],
     subscribe => Class['Govuk::Delayed_job'],
   }
 
-  service { "${title}-delayed-job-worker":
+  service { $service_name:
     ensure  => $enable_service,
   }
 }
--- modules/govuk/manifests/procfile/worker.pp
+++ modules/govuk/manifests/procfile/worker.pp
@@ -3,19 +3,20 @@ define govuk::procfile::worker (
 ) {
   include govuk::procfile
 
+  $service_name = "${title}-procfile-worker"
   $enable_service = $::govuk_platform ? {
     'development' => false,
     default       => true,
   }
 
-  file { "/etc/init/${title}-procfile-worker.conf":
+  file { "/etc/init/${service_name}.conf":
     ensure    => present,
     content   => template('govuk/procfile-worker.conf.erb'),
-    notify    => Service["${title}-procfile-worker"],
+    notify    => Service[$service_name],
     subscribe => Class['Govuk::Procfile'],
   }
 
-  service { "${title}-procfile-worker":
+  service { $service_name:
     ensure  => $enable_service,
   }
 }
