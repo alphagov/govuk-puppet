6b0c0c60c5188ebf3681dd43e63e545f23861636
Dan Carley <dan.carley@gmail.com>
2014-01-15 17:58:06 +0000

    [#59908614] Relocate gor s_cache->govuk::gor
    Split from the `s_cache` class because I'm about to add more logic for
    enabling/disabling Gor which will soon swap the node class. I also want to
    add some tests, independent of the node class.
    
    Hiera keys will need to be renamed. I think it makes sense that they
    reference `govuk::gor` directly rather than passing through
    `govuk::node::s_cache`, since configuration is per-environment.

---
+++ modules/govuk/manifests/gor.pp
@@ -0,0 +1,38 @@
+# == Class: govuk::gor
+#
+# Setup gor traffic replay for GOV.UK
+#
+class govuk::gor(
+  $enable_gor_to_staging = false,
+  $enable_gor_to_plat1prod = false,
+) {
+  # Hardcoded, rather than hiera, because I don't want to create the
+  # illusion that you can modify these on the fly. You will need to tidy
+  # up old `host{}` records.
+  $staging_ip     = '217.171.99.81'
+  $staging_host   = 'www-origin-staging.production.alphagov.co.uk'
+  $plat1prod_ip   = '37.26.90.220'
+  $plat1prod_host = 'www-origin-plat1.production.alphagov.co.uk'
+
+  if $enable_gor_to_staging or $enable_gor_to_plat1prod {
+    $gor_hosts_ensure = present
+  } else {
+    $gor_hosts_ensure = absent
+  }
+
+  # FIXME: These "fake" host entries serve two purposes:
+  #   1. Ensures that the SSL cert on staging, which thinks it is
+  #   production, matches the hostname that we connect to.
+  #   2. Prevents Gor/Go from performing DNS lookups, which occur once
+  #   for *every* request/goroutine, and can be quite overwhelming.
+  host {
+    $staging_host:
+      ensure  => $gor_hosts_ensure,
+      ip      => $staging_ip,
+      comment => 'Used by Gor. See comments in s_cache.';
+    $plat1prod_host:
+      ensure  => $gor_hosts_ensure,
+      ip      => $plat1prod_ip,
+      comment => 'Used by Gor. See comments in s_cache.';
+  }
+}
