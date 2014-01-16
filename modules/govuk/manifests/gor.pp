746a8e713d66e2aea2a492b50231a406e2b1e2d2
Dan Carley <dan.carley@gmail.com>
2014-01-16 09:33:07 +0000

    [#59908614] Manage Gor for s_cache
    Uses a vendored module to install Gor, create an upstart job, and start the
    service. Our `govuk::gor` module is responsible for enabling or disabling
    this based on certain feature flags. The service will be unconfigured
    (targets removed) and stopped if neither feature flag is enabled.
    
    The Gor arguments match what has been run manually on production in the past
    week or so. They differ from the previous way that we used Gor, which was a
    listener in production talking to a replayer in staging over an SSH tunnel.
    
    This method of performing the listen and replay in production means that we
    don't need to fiddle with SSH tunnels or VPNs, we just fire normal requests
    to the staging origin, which are encrypted by HTTPS. This comes at the small
    cost of increased load on the cache nodes -- which doesn't appear to be an
    issue, but we should keep an eye on.

--- modules/govuk/manifests/gor.pp
+++ modules/govuk/manifests/gor.pp
@@ -3,9 +3,12 @@
 # Setup gor traffic replay for GOV.UK
 #
 class govuk::gor(
+  $version = '0.7.0-9026155~ppa1~precise',
   $enable_staging = false,
   $enable_plat1prod = false,
 ) {
+  include govuk::ppa
+
   # Hardcoded, rather than hiera, because I don't want to create the
   # illusion that you can modify these on the fly. You will need to tidy
   # up old `host{}` records.
@@ -14,10 +17,23 @@ class govuk::gor(
   $plat1prod_ip   = '37.26.90.220'
   $plat1prod_host = 'www-origin-plat1.production.alphagov.co.uk'
 
-  if $enable_staging or $enable_plat1prod {
-    $gor_hosts_ensure = present
+  $gor_target0 = $enable_staging ? {
+    true    => [$staging_host],
+    default => [],
+  }
+  $gor_target1 = $enable_plat1prod ? {
+    true    => [$plat1prod_host],
+    default => [],
+  }
+
+  $gor_targets = flatten([$gor_target0, $gor_target1])
+
+  if $gor_targets == [] {
+    $gor_hosts_ensure   = absent
+    $gor_service_ensure = stopped
   } else {
-    $gor_hosts_ensure = absent
+    $gor_hosts_ensure   = present
+    $gor_service_ensure = running
   }
 
   # FIXME: These "fake" host entries serve two purposes:
@@ -34,5 +50,16 @@ class govuk::gor(
       ensure  => $gor_hosts_ensure,
       ip      => $plat1prod_ip,
       comment => 'Used by Gor. See comments in s_cache.';
+  } ->
+  class { '::gor':
+    package_ensure => $version,
+    service_ensure => $gor_service_ensure,
+    args           => {
+      '-input-raw'          => 'localhost:7999',
+      '-output-http'        => $gor_targets,
+      '-output-http-method' => [
+        'GET', 'HEAD', 'OPTIONS'
+      ],
+    },
   }
 }
