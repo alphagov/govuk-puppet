78f1b4b02fdbec2e3572a8d5b718602b62bb28c3
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-01-23 08:36:55 +0000

    Raise tariff-api memory warning

--- modules/govuk/manifests/apps/tariff_api.pp
+++ modules/govuk/manifests/apps/tariff_api.pp
@@ -1,9 +1,11 @@
 class govuk::apps::tariff_api($port = 3018) {
   govuk::app { 'tariff-api':
-    app_type           => 'rack',
-    port               => $port,
-    health_check_path  => '/healthcheck',
-    log_format_is_json => true,
+    app_type               => 'rack',
+    port                   => $port,
+    health_check_path      => '/healthcheck',
+    log_format_is_json     => true,
+    #2.5GB for a warning
+    nagios_memory_warning  => 2684354560,
   }
 
   govuk::procfile::worker {'tariff-api': }
