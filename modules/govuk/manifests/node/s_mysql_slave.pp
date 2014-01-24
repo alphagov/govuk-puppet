19f06e64ce3d756d654e575a7f636ace73a024b3
bob walker <bob@randomness.org.uk>
2014-01-24 11:15:51 +0000

    [#59676220] Remove automysqlbackup from mysql-slave-1
    Purge the automysqlbackup package. This will leave the backups in /var/lib/automysqlbackup.

--- modules/govuk/manifests/node/s_mysql_slave.pp
+++ modules/govuk/manifests/node/s_mysql_slave.pp
@@ -1,14 +1,15 @@
 class govuk::node::s_mysql_slave inherits govuk::node::s_base {
   $root_password = extlookup('mysql_root', '')
 
-  include mysql::backup
-
   class { 'mysql::server':
     root_password       => $root_password,
     tmp_table_size      => '256M',
     max_heap_table_size => '256M',
   }
   include mysql::server::slave
+
+  #REMOVE: once deployed to production can be removed.
+  package { 'automysqlbackup': ensure => purged }
 
   collectd::plugin::tcpconn { 'mysql':
     incoming => 3306,
