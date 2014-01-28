b2647a39da44370a16f5533c069311f1d33dde6b
Dan Carley <dan.carley@gmail.com>
2014-01-28 11:17:03 +0000

    [64143110] Undelete some icinga::contact tests
    Partial revert of 4fcbed9 which deleted these without explanation and the
    omitted `let(:title)` caused them to fail.

--- modules/icinga/spec/defines/contact_spec.rb
+++ modules/icinga/spec/defines/contact_spec.rb
@@ -1,6 +1,16 @@
 require_relative '../../../../spec_helper'
 
 describe 'icinga::contact', :type => :define do
+  let(:title) { 'jim' }
+  let(:params) {{ "email" => "jimisawesome@invalid.domain"}}
+  it { should contain_file('/etc/icinga/conf.d/contact_jim.cfg').
+    with_content(/contact_name\s+jim/).
+    with_content(/email\s+jimisawesome@invalid.domain/)
+  }
+end
+
+describe 'icinga::contact', :type => :define do
+  let(:title) { 'contact_notification_options' }
   let(:params) {{ "service_notification_options" => 'c', "email"=>'a@b.com'}}
   it { should contain_file('/etc/icinga/conf.d/contact_contact_notification_options.cfg').
     with_content(/service_notification_options\s+c/)
