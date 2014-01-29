alphagov/govuk-puppet

commit 0e6dfed2d61185b0fc5fe61fdd5fc20aab141c35

Migrate asset_root, website_host and website_root to hiera

modules/govuk/manifests/deploy/config.pp
@@ -69,7 +69,7 @@ class govuk::deploy::config {
     value => $asset_root,
   }
 
-  $website_root = extlookup('website_root')
+  $website_root = hiera('website_root')
 
   govuk::envvar { 'GOVUK_WEBSITE_ROOT':
     value => $website_root,

modules/govuk/spec/classes/govuk_node_s_monitoring_spec.rb
@@ -14,7 +14,8 @@ end
 
 describe 'govuk::node::s_monitoring', :type => :class do
   let (:hiera_data) {{
-      'app_domain' => 'giraffe.biz',
+      'app_domain'   => 'giraffe.biz',
+      'website_root' => 'www.giraffe.biz',
     }}
 
   context 'param defaults' do

modules/govuk/spec/defines/govuk_app_spec.rb
@@ -3,7 +3,8 @@ require_relative '../../../../spec_helper'
 describe 'govuk::app', :type => :define do
   let(:title) { 'giraffe' }
   let(:hiera_data) {{
-      'app_domain' => 'test.gov.uk',
+      'app_domain'   => 'test.gov.uk',
+      'website_root' => 'foo.test.gov.uk',
     }}
 
   context 'with no params' do

hieradata/development.yaml
@@ -1,1 +1,3 @@
---- {}
+---
+asset_root: 'http://static.dev.gov.uk'
+website_root: 'http://www.dev.gov.uk'

hieradata/vagrant.yaml
@@ -1,1 +1,4 @@
---- {}
+---
+asset_root: 'http://static.dev.gov.uk'
+website_host: 'www.dev.gov.uk'
+website_root: 'http://www.dev.gov.uk'

modules/govuk/manifests/apps/static.pp
@@ -3,7 +3,7 @@
 # The GOV.UK static application -- serves static assets and templates
 class govuk::apps::static( $port = 3013 ) {
   $app_domain = hiera('app_domain')
-  $website_root = extlookup('website_root')
+  $website_root = hiera('website_root')
   $asset_manager_host = "asset-manager.${app_domain}"
 
   govuk::app { 'static':

modules/govuk/manifests/apps/transactions_explorer.pp
@@ -5,7 +5,7 @@ class govuk::apps::transactions_explorer {
   $app_path = "/data/vhost/${app_name}"
 
   $te_root = "${app_name}.${app_domain}"
-  $website_root = extlookup('website_root')
+  $website_root = hiera('website_root')
 
   $logpath = '/var/log/nginx'
   $access_log = "${vhost_full}-access.log"

modules/hosts/manifests/skyscape/production_like.pp
@@ -132,7 +132,7 @@ class hosts::skyscape::production_like {
   }
 
   #router lb vhosts
-  $website_host = extlookup('website_host', 'www.gov.uk')
+  $website_host = hiera('website_host', 'www.gov.uk')
   govuk::host { 'cache':
     ip              => '10.1.1.1',
     vdc             => 'router',

commit fa9bafcb603188c005931569f3aee0ad513cfd25

Fix asset_root in deploy::config
We missed this out of 0e6dfed2 which caused Preview to revert to a crap
host. This fixes that.

modules/govuk/manifests/deploy/config.pp
@@ -63,7 +63,7 @@ class govuk::deploy::config {
     value => $app_domain,
   }
 
-  $asset_root = extlookup('asset_root', "https://static.${app_domain}")
+  $asset_root = hiera('asset_root')
 
   govuk::envvar { 'GOVUK_ASSET_ROOT':
     value => $asset_root,

modules/govuk/spec/classes/govuk_node_s_monitoring_spec.rb
@@ -15,6 +15,7 @@ end
 describe 'govuk::node::s_monitoring', :type => :class do
   let (:hiera_data) {{
       'app_domain'   => 'giraffe.biz',
+      'asset_root'   => 'https://static.test.gov.uk',
       'website_root' => 'www.giraffe.biz',
     }}
 

modules/govuk/spec/defines/govuk_app_spec.rb
@@ -4,6 +4,7 @@ describe 'govuk::app', :type => :define do
   let(:title) { 'giraffe' }
   let(:hiera_data) {{
       'app_domain'   => 'test.gov.uk',
+      'asset_root'     => 'https://static.test.gov.uk',
       'website_root' => 'foo.test.gov.uk',
     }}
 