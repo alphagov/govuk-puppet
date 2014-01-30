32b1829522c3d74a584ce6c8906ea6711863bd76
Dan Carley <dan.carley@gmail.com>
2014-01-30 12:25:03 +0000

    [64143110] Stop NSCA daemon on client machines
    This was mistakenly started on all `nsca::client` nodes and then omitted
    without stopping the daemon. This can be removed when deployed to platform1
    prod. Isolating the monitoring node because it already (rightly) manages the
    service in `nsca::server`. Offending commits:
    
    48e19156e918032b0500d82a62851c452f0d8c72...4bcf5af47451f85d1acb9111f63c5d994a4b1258

--- modules/nsca/manifests/client.pp
+++ modules/nsca/manifests/client.pp
@@ -1,4 +1,11 @@
 class nsca::client {
   include nsca
 
+  # FIXME: Remove when deployed to platform1 prod.
+  # A horrible thing to remove another horrible thing.
+  if !defined(Class['govuk::node::s_monitoring']) {
+    service { 'nsca':
+      ensure => stopped,
+    }
+  }
 }
