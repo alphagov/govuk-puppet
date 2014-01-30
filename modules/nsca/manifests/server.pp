5f3fd3ee0e3f97acd36b192a8832597fe2ce47e2
Dan Carley <dan.carley@gmail.com>
2014-01-30 15:08:51 +0000

    [64143110] Manage stock NSCA configuration
    Is the same on Lucid and Precise. Importing and managing for upcoming Icinga
    modifications. This is only used by the server portion.
    
    Modifying relationships to ensure that the NSCA service gets restarted on
    any package or config changes. Using `Class['nsca']` encapsulation instead
    of `Package['nsca']`. Updated Icinga/Nagios references in comments too.
    
    NB: monitoring-1.management in platform1 has `debug` turned on. This will
    subsequently disable it.

--- modules/nsca/manifests/server.pp
+++ modules/nsca/manifests/server.pp
@@ -6,17 +6,26 @@ class nsca::server {
     port => 5667,
   }
 
+  file { '/etc/nsca.cfg':
+    ensure    => file,
+    source    => 'puppet:///modules/nsca/etc/nsca.cfg',
+    subscribe => Class['nsca'],
+  }
   service {'nsca':
     ensure    => running,
     hasstatus => false,
+    subscribe => [
+      File['/etc/nsca.cfg'],
+      Class['nsca']
+    ],
     # ugh. cross-module dependency alert.
-    # we require Package['nagios3'] because it creates the 'nagios'
+    # we require Package['icinga'] because it creates the 'nagios'
     # user which the nsca process depends on. I don't want to depend
-    # on Class['nagios'] because that feels a bit too restrictive - we
-    # don't want to stop nsca starting before nagios3 has started.
+    # on Class['icinga'] because that feels a bit too restrictive - we
+    # don't want to stop nsca starting before icinga has started.
     #
     # if you can imagine a better way of doing this, please do it! :)
     # -- ppotter 2013-02-07
-    require   => [Package['icinga'],Package['nsca']],
+    require   => Package['icinga'],
   }
 }
