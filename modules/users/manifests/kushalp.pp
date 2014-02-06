9de595b6b3a2c7a35db7b76661538aa269a5d43c
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-02-06 15:34:34 +0000

    Update the SSH key for 'kushalp' user
    I'm rolling all of my keys and submitting new ones.

--- modules/users/manifests/kushalp.pp
+++ modules/users/manifests/kushalp.pp
@@ -2,6 +2,6 @@ class users::kushalp {
   govuk::user { 'kushalp':
     fullname => 'Kushal Pisavadia',
     email    => 'kushal.pisavadia@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAqQtrhEyrLg4B8Hj1hLFiFCs1DxTzeFaOUB5nbpUw/TP3CMAO7hNFtFVjrqiUethEH+YML6P1Y/eVWupEALazsFIErvO+pPrZPXrNOEcBCDh6Gkv6C0KXQsaj5w7b8p+nD0tLJcVfmWFInv4BUjKR9ApzTe5UmVAdqq5938zR4J2mNBCqmQwU0DBaQnd3GBSueMo1Mok4loGidliDL99Z8LQTtQf3Ln1U/ZOdT4SI9gnmK7a7yO5QqBMsdX1oclzMz14DsHVmAJhwLOgIwUV7T4W0fV/QX0sm6H9d3GPRnATZhxwXIlf61MkL3lxlUoZjIWvugD6nwbL/JjQ6F56gcQ== kushi.p@gmail.com',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC//mv5/siGq5+W61w626wzaRRciO3Cd1RHbgQbxpu3kYRQVnvexizf2OyscW0C/nT/+lTXMHF9dloIA3qgYIcGh7B69I53ZHqkmoS9V6hE2JUtZaYF1J3CRDh/u35MvgO2lbDOxdJO6l2dnn8bVPFE2KHZhq4w3O9MmSQ184MUnuJ4rj7vnBzaoKyhDWGE2CkngmL32S8EN/e0H4qRTUPenvmJjUgByEQNotwp4NGuoGYWPeeUqIOXbZ19EebSzvMznfTXsKe9+0/N6bmH6eUAyzqt84Fd8zct3kzLjTkPsIp0P1Nh+yu02qW5hhq6gxkMyLlGakXRLZTFH+CKqvdOw6CUZpnmIOJg/5Pi76vjMJXmhFNWBI4519wQlLU7ytqrK2yLDTe0Newjl8jquJj3OcmeKRoxI/7ZWf3pSnntkTLph+dUjV4cr1lAxiELxHBNIagIWDCtDYN5C84ku2SHlg0fHkgDR5RxAe6nr+ScxyhKV2H1NGfyzIt/xZ3u5P8D5Wxo2cWa15AGn1+PMJLsvnp4zYzJIQVp540kYT8Z3zP8VgQ5/yuqTJV5KrJmG/SrI8CDV8NaNmaXAtR7Z7ULvscsFgAhZs37r5js1tc8qgUJc7mJkqOATDSSaGqsl1THis1/++1bKiEvTh01VM2IHBUZQAoAM78i8BphOZWn+w== kushal.pisavadia@digital.cabinet-office.gov.uk',
   }
 }
