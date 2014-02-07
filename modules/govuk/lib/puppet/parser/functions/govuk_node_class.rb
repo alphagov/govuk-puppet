13646ac76da6c8da0b6b38eecb5d82929fcdf0de
Dan Carley <dan.carley@gmail.com>
2014-02-07 16:14:27 +0000

    [#65294674] $::clientcert IS spoofable
    Correct the comments in `govuk_node_class()` and add a FIXME to address when
    we upgrade to Puppet 3.4.x -- thanks @rolandpienaar and @philippotter

--- modules/govuk/lib/puppet/parser/functions/govuk_node_class.rb
+++ modules/govuk/lib/puppet/parser/functions/govuk_node_class.rb
@@ -4,8 +4,10 @@ Return the name to construct a `govuk::node::s_` class based on a client's
 `$::clientcert` fact. Takes no arguments.
 
 For `puppet agent` this fact will be the FQDN on the certificate signed by
-the master and thus unspoofable. For `puppet apply` the fact will just be
-the machine's FQDN, but it spoofable.
+the master. However it is currently spoofable; see FIXME below.
+
+For `puppet apply` the fact will just be the machine's FQDN, but will always
+be spoofable.
 
 It does so by taking the first part (the unqualified hostname), stripping
 any numeric suffixes, and replacing hyphens (valid for DNS) with underscores
@@ -18,6 +20,8 @@ EOS
       "govuk_node_class: Wrong number of arguments given #{args.size} for 0."
     ) unless args.size == 0
 
+    # FIXME: Use $trusted variable in Puppet 3.4 to prevent spoofing:
+    # http://docs.puppetlabs.com/puppet/latest/reference/lang_variables.html#trusted-node-data
     clientcert = lookupvar('::clientcert')
 
     raise(
