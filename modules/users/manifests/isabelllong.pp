7091c6ded894aac6c410dc895698f6830b37b5e0
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2014-02-10 16:12:49 +0000

    Add new starter Isabell Long

---
+++ modules/users/manifests/isabelllong.pp
@@ -0,0 +1,7 @@
+class users::isabelllong {
+  govuk::user { 'isabelllong':
+    fullname => 'Isabell Long',
+    email    => 'isabell.long@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTk24+wYy1IQjggldZLJ++mEwFKDPBTaR9+NRsHJwRArrS+1Fp1sPJclNTls8H6KZCwAAyJym9R27Qr6FJkwmwdZlaYGY/pC4CJrEKti27BQjDarPjQOZmuMTrxBAn6U52CVN+EgEPPXw8/aiz5zw81/u8jnEHrUpHlPowJ6rTZqfy51SvhZj3rE8/Z0RRffj2+qdPNtCldeLlMPig2Y097U16QVGQ3IuWberLN1alcatwZEeo+QNiKI/cq6qw2u5QPO/tVzQfGG7SNICyHvO0aWeweZjOaTGGxK+2nS3L7DjLJ7A6ecGiDER5tP0zoLJXEQnmYXWfh++X0J+X5cxF isabelllong@gds0207.local',
+  }
+}
