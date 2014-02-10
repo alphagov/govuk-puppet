bfbbd52fd95c9d6d3de44db4f7e5a9c02e7768a6
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-02-10 17:35:24 +0000

    Add new starter Daniel Roseman

---
+++ modules/users/manifests/danielroseman.pp
@@ -0,0 +1,7 @@
+class users::bradleyw {
+  govuk::user { 'danielroseman':
+    fullname => 'Daniel Roseman',
+    email    => 'daniel.roseman@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCbibke+QgHUIcq3GEyCWztjsp0g2JHaYi4ZLR2O/v12usHQQ2PtoynD+SC9fXfWxsjNChyXL9pBAx5sFo9iFZRWD6VeBh7pHkOGB1VN2/LDe9sjY/mgRrj3WtviLW6X4J3eXAthVh3462AZZ+xek/UFSWDgmFlfs94E2MtsTjd3HbCuXLGsUW1NtUgTYb7EloFTJuFfq0Ofohg3N3aR+sNDv64AAmMbSbiZol9GvwAHxPZDt85arG5JAP0Gd2sBwY39U4XlzeC7zPSzIbHFh4gKnqD5Pd3cqQdXCFkZyjlgpEuPfPaKos0HhiJcBSsGSHy1LWQmn6sx/NZPgCbRyKT daniel@roseman.org.uk',
+  }
+}
