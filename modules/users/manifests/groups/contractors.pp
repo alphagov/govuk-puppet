c33f8821feafe2b66881457b9902e33f37279ac0
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-02-12 09:14:55 +0000

    Remove Ben Firshman

--- modules/users/manifests/groups/contractors.pp
+++ modules/users/manifests/groups/contractors.pp
@@ -4,7 +4,6 @@
 # Please ensure that the list remains sorted alphabetically by username.
 #
 class users::groups::contractors {
-  include users::ben
   include users::benilovj
   include users::davidainslie
   include users::dominicbaggott
