2e03978d23fb536cc0b92b7a182f1291eef8601b
Dan Carley <dan.carley@gmail.com>
2014-02-17 11:03:23 +0000

    [#65887322] Remove govuk_provider
    References to this were killed off in ff6c17b. We no longer need to maintain
    this environment variable.

--- modules/govuk/spec/classes/govuk_envsys_spec.rb
+++ modules/govuk/spec/classes/govuk_envsys_spec.rb
@@ -13,11 +13,9 @@ describe 'govuk::envsys', :type => :class do
   context 'with govuk vars' do
     let(:facts) {{
       :govuk_platform => 'foo',
-      :govuk_provider => 'bar',
     }}
 
     it { should contain_file(file_path).with_content(/^FACTER_govuk_platform=foo$/) }
-    it { should contain_file(file_path).with_content(/^FACTER_govuk_provider=bar$/) }
 
     # We can't test for govuk_class because it's interpreted by manifests/site.pp
     it { should_not contain_file(file_path).with_content(/^FACTER_govuk_class=baz$/) }
