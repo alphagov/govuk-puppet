0ace90861b25752e698e17e67a7aeaf6a38700fd
Dan Carley <dan.carley@gmail.com>
2014-03-06 11:11:53 +0000

    [#66664898] Remove remaining check_redis alerts
    We no longer use this plugin for checking list lengths (c01343e) or memory
    usage (af91ab4). The remaining checks are:
    
    - Connection latency / response time
    
      Looking at the code it appears to be the elapsed time in which the `INFO`
      command returns. Rather than the avg/max response time for other clients
      talking to Redis. Which I can't see has ever fired an alert for us and
      isn't incredibly useful.
    
    - Connection clients
    
      The limit for Redis 2.4 (which we're using) is hard-coded to 10k clients.
    
      - http://redis.io/topics/clients
      - https://github.com/antirez/redis/blob/2.4.13/src/ae.h#L36
    
      Our stats in Graphite show that we hover at a couple of hundred. Again, we
      never appear to have fired this, so I don't think it's terribly useful.
    
    I think these were all added because we just wanted to monitor *something*
    when we first introduced Redis to our stack. But they aren't providing us
    much value and the plugin is incredibly difficult to use/read. So remove
    them.
    
    The `icinga::nrpe_config` and `icinga::check` resources should purge
    themselves. `icinga::plugin` won't because there are unmanaged plugins in
    that directory, so it's marked as `absent`. The `cpanm` provider doesn't
    allow us to remove the `Redis` package, but that's probably not a big deal.

--- modules/govuk/manifests/node/s_redis_base.pp
+++ modules/govuk/manifests/node/s_redis_base.pp
@@ -22,32 +22,9 @@ class govuk::node::s_redis_base {
     host_name => $::fqdn,
   }
 
-  # The check_redis nagios plugin (below) requires this perl module
-  package { 'Redis':
-    ensure   => present,
-    provider => 'cpanm',
-  }
-
+  # FIXME: Remove when deployed.
   @icinga::plugin { 'check_redis':
-    source  => 'puppet:///modules/govuk/node/s_redis/nagios/check_redis.pl',
-    require => Package['Redis'],
-  }
-
-  @icinga::nrpe_config { 'check_redis':
-    content => template('govuk/node/s_redis/nagios/check_redis.cfg.erb'),
-  }
-
-  @@icinga::check { "check_redis_${::hostname}":
-    # Full details of arguments to check_redis can be found in
-    #   modules/govuk/files/node/s_redis/nagios/check_redis.pl
-    #
-    # In summary, this:
-    #   - warns if connection latency >1s, critical if >2s
-    #   - warns if >800 connected_clients, critical if >1000 connected_clients
-    check_command       => "check_nrpe!check_redis!${redis_port} 1,2 connected_clients 800 1000",
-    service_description => 'redis server health',
-    host_name           => $::fqdn,
-    notes_url           => 'https://github.gds/pages/gds/opsmanual/2nd-line/nagios.html#redis-server-check',
+    ensure  => absent,
   }
 
   @logrotate::conf { 'redis':
