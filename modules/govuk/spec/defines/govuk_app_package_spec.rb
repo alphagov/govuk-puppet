509412126a8ad7d59ef3634b02380ed53d88a290
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-03-06 15:07:23 +0000

    Add ensure parameter to govuk::app::package
    Allows log and app directories to be removed.
    
    force => true is used here to ensure directories are actually removed.
    Without force the are left, see:
    http://docs.puppetlabs.com/references/latest/type.html#file-attribute-force

--- modules/govuk/spec/defines/govuk_app_package_spec.rb
+++ modules/govuk/spec/defines/govuk_app_package_spec.rb
@@ -24,4 +24,36 @@ describe 'govuk::app::package', :type => :define do
       should contain_file('/data/vhost/giraffe.example.com')
     end
   end
+
+  context 'ensure' do
+    context 'absent' do
+      let(:params) do
+        {
+          :vhost_full => 'giraffe.example.com',
+          :ensure     => 'absent',
+        }
+      end
+
+      it do
+        should contain_file('/var/apps/giraffe').with_ensure('absent')
+        should contain_file('/var/log/giraffe').with_ensure('absent')
+        should contain_file('/data/vhost/giraffe.example.com').with_ensure('absent')
+      end
+    end
+
+    context 'invalid' do
+      let(:params) do
+        {
+          :vhost_full => 'giraffe.example.com',
+          :ensure     => 'invalid',
+        }
+      end
+
+      it do
+        expect {
+          should contain_file('/var/apps/giraffe')
+        }.to raise_error(Puppet::Error, /Invalid ensure value/)
+      end
+    end
+  end
 end
