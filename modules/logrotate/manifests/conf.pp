fa6a0484d9dcdbdf819ae3fe6dedfdaadff6889d
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-03-06 19:13:51 +0000

    Add ensure to logrotate::conf
    This is towards adding ensure to govuk::app

--- modules/logrotate/manifests/conf.pp
+++ modules/logrotate/manifests/conf.pp
@@ -1,10 +1,11 @@
 define logrotate::conf (
   $matches,
-  $days_to_keep = '31'
+  $days_to_keep = '31',
+  $ensure = 'present',
 ) {
 
   file { "/etc/logrotate.d/${title}":
-    ensure  => present,
+    ensure  => $ensure,
     content => template('logrotate/logrotate.conf.erb'),
     require => Package['logrotate'],
   }
