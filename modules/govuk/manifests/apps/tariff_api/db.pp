7d6c4c54e8fdf1223479f4b785b9e47d9dd063de
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-03-10 13:54:06 +0000

    Remove tariff_production database from puppet
    Trade Tariff was changed to a temporal database model (whatever that means)
    and during that change was migrated to the
    `tariff_temporal_production` database.
    
    As per alphagov-deployment:
    ```
    [gds/alphagov-deployment:master●]$ ack tariff_production trade*
    [gds/alphagov-deployment:master●]$ ack tariff_temporal_production trade*
    trade-tariff-backend/to_upload/database.sky.yml 3:  database:
    tariff_temporal_production
    ```
    
    This commit removes the database from all the machines. It leaves a
    couple of FIXME's to be done at some point later.
    
    This is related to Pivotal [#67106956].

--- modules/govuk/manifests/apps/tariff_api/db.pp
+++ modules/govuk/manifests/apps/tariff_api/db.pp
@@ -1,7 +1,9 @@
 class govuk::apps::tariff_api::db {
+  # FIXME: This can be removed from s_mysql_master.pp and here once the DB has been has been removed
   $tariff_api_password = extlookup('mysql_tariff_api', '')
 
   mysql::db { 'tariff_production':
+    ensure   => 'absent',
     user     => 'tariff',
     host     => '%',
     password => $tariff_api_password,
