b68c877242734a156a811c3c7c77ef0735629b6b
Matthew Ford <matt@bitzesty.com>
2014-03-12 17:10:09 +0000

    Include the HMRC contacts application in the dev vm

--- modules/govuk/manifests/node/s_development.pp
+++ modules/govuk/manifests/node/s_development.pp
@@ -84,6 +84,7 @@ class govuk::node::s_development {
   include govuk::apps::business_support_api
   include govuk::apps::canary_backend
   include govuk::apps::canary_frontend
+  include govuk::apps::contacts
   include govuk::apps::content_planner
   include govuk::apps::efg
   include govuk::apps::errbit
