5b9819c5305d5bf5858537e49095d3d10854d832
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-03-12 16:14:01 +0000

    Add healthcheck paths for finders
    none for specialist frontend at present because it does not have any
    pages until a document has been published

--- modules/govuk/manifests/apps/finder_api.pp
+++ modules/govuk/manifests/apps/finder_api.pp
@@ -9,7 +9,7 @@ class govuk::apps::finder_api(
       app_type               => 'rack',
       port                   => $port,
       vhost_protected        => $vhost_protected,
-      health_check_path      => '/',
+      health_check_path      => '/cma-cases.json',
       log_format_is_json     => true,
     }
   }
