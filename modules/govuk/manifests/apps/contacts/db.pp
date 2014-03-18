458aad84bccb0d39bed89911e831811696a0eb2b
Dominic Baggott <dominic.baggott@gmail.com>
2014-03-18 16:47:09 +0000

    Add readonly MySQL user for contacts-frontend
    The contacts-frontend and contacts-admin apps share a MySQL database.
    Only contacts-admin should be able to write to it.

--- modules/govuk/manifests/apps/contacts/db.pp
+++ modules/govuk/manifests/apps/contacts/db.pp
@@ -1,9 +1,16 @@
 class govuk::apps::contacts::db {
-  $contacts_password = extlookup('mysql_contacts', '')
+  $contacts_admin_password = extlookup('mysql_contacts_admin', '')
+  $contacts_frontend_password = extlookup('mysql_contacts_frontend', '')
 
   mysql::db { 'contacts_production':
     user     => 'contacts',
     host     => '%',
-    password => $contacts_password,
+    password => $contacts_admin_password,
+  }
+
+  govuk_mysql::user { 'contacts_fe@%':
+    password_hash => mysql_password($contacts_frontend_password),
+    table         => 'contacts_production.*',
+    privileges    => ['SELECT'],
   }
 }
