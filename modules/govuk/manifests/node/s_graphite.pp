88e44970bdb804fe78856e61df268d20f8218a0c
Dan Carley <dan.carley@gmail.com>
2014-03-21 07:10:10 +0000

    [#67404250] Patch for Graphite->Carbon comms
    The upgrade to Graphite 0.9.12 broke one of the ways that the Graphite
    webapp obtains metric data for display. It was still reading the vast
    majority of metrics from Whisper files on disk. But it stopped reading the
    most recent data, which hadn't been persisted to disk yet, from
    carbon-cache's buffer.
    
    This wasn't apparent to us until we reduced the amount of disk I/O that
    Carbon was consuming in 49a9861. When we stopped writing to disk so
    aggresively, and the Whisper files were less up-to-date, we noticed that the
    graphs were incomplete for the most recent ~3 minutes. On closer
    inspection there were also lots of errors in `webapp/exception.log`.
    
    Besides making our graphs look Not Right, this will have a negative effect
    on our Icinga `check_graphite` alerts, because they no longer have access to
    accurate data for the last five minutes (the default check timeframe).
    
    This has been fixed in the Graphite 0.9.x branch but not yet released (as
    what will be 0.9.13). I'm backporting the fix because of how seriously it
    affects us. There's no nice way to do this, but this should at least:
    
    - Be obvious about what patches we've applied from upstream
    - Blow up if we change the version but don't remove the fix
    
    I've pinned the Graphite version to make this more obvious.

--- modules/govuk/manifests/node/s_graphite.pp
+++ modules/govuk/manifests/node/s_graphite.pp
@@ -15,6 +15,7 @@ class govuk::node::s_graphite inherits govuk::node::s_base {
   }
 
   class { 'graphite':
+    version                    => '0.9.12',
     port                       => '33333',
     carbon_aggregator          => true,
     aggregation_rules_source   => 'puppet:///modules/govuk/node/s_graphite/aggregation-rules.conf',
@@ -22,6 +23,25 @@ class govuk::node::s_graphite inherits govuk::node::s_base {
     storage_schemas_source     => 'puppet:///modules/govuk/node/s_graphite/storage-schemas.conf',
     carbon_source              => 'puppet:///modules/govuk/node/s_graphite/carbon.conf',
     require                    => Govuk::Mount['/opt/graphite'],
+  }
+
+  # Remove this fix when upgrading from 0.9.12:
+  # https://github.com/graphite-project/graphite-web/issues/423
+  $util_py          = '/opt/graphite/webapp/graphite/util.py'
+  $util_patch       = '/opt/graphite/graphite_util.patch'
+  $util_patched_md5 = 'aa6b5e9234dfc705fa01cb871f8eec38'
+  file { $util_patch:
+    ensure => file,
+    source => 'puppet:///modules/govuk/node/s_graphite/graphite_carbonlink.patch',
+  }
+  exec { 'patch graphite 0.9.12':
+    command => "/usr/bin/patch -b ${util_py} ${util_patch}",
+    unless  => "/usr/bin/md5sum ${util_py} | /bin/grep -qsw ${util_patched_md5}",
+    notify  => Class['graphite::service'],
+    require => [
+      Class['graphite::install'],
+      File[$util_patch],
+    ],
   }
 
   @@icinga::check { "check_carbon_cache_running_on_${::hostname}":
