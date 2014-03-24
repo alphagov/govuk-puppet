a53545d2904ddc3c1e41b496e871b1845c34d3e5
Harry Panayiotou <harry.panayiotou@digital.cabinet-office.gov.uk>
2014-03-24 14:47:43 +0000

    Migrate mysql_contacts_frontend extdata

--- modules/govuk/manifests/apps/contacts/db.pp
+++ modules/govuk/manifests/apps/contacts/db.pp
@@ -2,6 +2,10 @@ class govuk::apps::contacts::db (
   $mysql_contacts_admin = '',
 ){
   $contacts_frontend_password = extlookup('mysql_contacts_frontend', '')
+class govuk::apps::contacts::db (
+  $mysql_contacts_frontend =  '',
+){
+  $contacts_admin_password = extlookup('mysql_contacts_admin', '')
 
   mysql::db { 'contacts_production':
     user     => 'contacts',
@@ -10,7 +14,7 @@ class govuk::apps::contacts::db (
   }
 
   govuk_mysql::user { 'contacts_fe@%':
-    password_hash => mysql_password($contacts_frontend_password),
+    password_hash => mysql_password($mysql_contacts_frontend),
     table         => 'contacts_production.*',
     privileges    => ['SELECT'],
   }
