6335eba4e546651e855c08334297761e8e6e7eba
Harry Panayiotou <harry.panayiotou@digital.cabinet-office.gov.uk>
2014-03-24 15:15:58 +0000

    Migrate mysql_content_planner extdata

--- modules/govuk/manifests/apps/content_planner/db.pp
+++ modules/govuk/manifests/apps/content_planner/db.pp
@@ -1,9 +1,9 @@
-class govuk::apps::content_planner::db {
-  $content_planner_password = extlookup('mysql_content_planner', '')
-
+class govuk::apps::content_planner::db (
+  $mysql_content_planner = '',
+){
   mysql::db { 'content_planner_production':
     user     => 'content_planner',
     host     => '%',
-    password => $content_planner_password,
+    password => $mysql_content_planner,
   }
 }
