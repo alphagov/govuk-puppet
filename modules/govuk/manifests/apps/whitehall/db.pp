5cbd6dfadd589b6f500cb9067a683274f1ced1cf
Harry Panayiotou <harry.panayiotou@digital.cabinet-office.gov.uk>
2014-03-25 13:14:50 +0000

    Migrate whitehall_admin_password extdata

--- modules/govuk/manifests/apps/whitehall/db.pp
+++ modules/govuk/manifests/apps/whitehall/db.pp
@@ -1,9 +1,9 @@
-class govuk::apps::whitehall::db {
-  $whitehall_admin_password = extlookup('mysql_whitehall_admin', '')
-
+class govuk::apps::whitehall::db (
+  $mysql_whitehall_admin = '',
+){
   mysql::db { 'whitehall_production':
     user     => 'whitehall',
     host     => '%',
-    password => $whitehall_admin_password,
+    password => $mysql_whitehall_admin,
   }
 }
