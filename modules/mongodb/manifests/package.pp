2a8f42f72c1538a1304b41fc36599521067f4254
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-03-25 17:22:04 +0000

    Allow moving from mongodb20-10gen to mongodb-10gen
    This will allow us to transition development vms without a package
    conflict.

--- modules/mongodb/manifests/package.pp
+++ modules/mongodb/manifests/package.pp
@@ -4,6 +4,14 @@ class mongodb::package(
 ) {
   include mongodb::repository
 
+  # FIXME Remove this once everything's migrated to using the mongodb-10gen package
+  if $package_name != 'mongodb20-10gen' {
+    package { 'mongodb20-10gen':
+      ensure => absent,
+      before => Package[$package_name],
+    }
+  }
+
   package { $package_name:
     ensure => $version,
   }
