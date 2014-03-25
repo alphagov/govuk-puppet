4e40b1f7381f9c22cbd31293fcb0a138e76a326c
Harry Panayiotou <harry.panayiotou@digital.cabinet-office.gov.uk>
2014-03-25 12:17:53 +0000

    Migrate mysql_support_contacts extdata

--- modules/govuk/manifests/node/s_support_contacts.pp
+++ modules/govuk/manifests/node/s_support_contacts.pp
@@ -1,9 +1,9 @@
+class govuk::node::s_support_contacts ( $mysql_support_contacts )inherits govuk::node::s_base {
 class govuk::node::s_support_contacts ($mysql_root_support_contacts) inherits govuk::node::s_base {
 class govuk::node::s_support_contacts (
   $dump_password
 ) inherits govuk::node::s_base {
   $root_password = extlookup('mysql_root_support_contacts')
-  $support_password = extlookup('mysql_support_contacts')
 
   class { 'govuk_mysql::server':
     root_password => $mysql_root_support_contacts,
@@ -17,7 +17,7 @@ class govuk::node::s_support_contacts (
   mysql::db {'support_contacts_production':
     user     => 'support_contacts',
     host     => '%',
-    password => $support_password,
+    password => $mysql_support_contacts,
   }
 
   govuk_mysql::user { 'dump@localhost':
