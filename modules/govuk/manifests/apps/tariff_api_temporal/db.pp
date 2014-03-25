53d389f3adbff4ceb52eb8d2e96cc7d298d9e026
Harry Panayiotou <harry.panayiotou@digital.cabinet-office.gov.uk>
2014-03-25 13:03:32 +0000

    Migrate mysql_tariff_api_temporal extdata

--- modules/govuk/manifests/apps/tariff_api_temporal/db.pp
+++ modules/govuk/manifests/apps/tariff_api_temporal/db.pp
@@ -1,9 +1,9 @@
-class govuk::apps::tariff_api_temporal::db {
-  $tariff_api_temporal_password = extlookup('mysql_tariff_api_temporal', '')
-
+class govuk::apps::tariff_api_temporal::db (
+  $mysql_tariff_api_temporal = '',
+){
   mysql::db { 'tariff_temporal_production':
     user     => 'tariff_temporal',
     host     => '%',
-    password => $tariff_api_temporal_password,
+    password => $mysql_tariff_api_temporal,
   }
 }
