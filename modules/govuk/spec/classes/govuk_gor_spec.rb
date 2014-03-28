1514395ee0a65c40a290c339f4e5a9e558a1f65b
Dan Carley <dan.carley@gmail.com>
2014-03-28 15:40:28 +0000

    [#68276766] Change gor to Platform1 staging
    Now that we've migrated Production and Staging to Skyscape Platform1 the old
    targets of Interim Staging and Platform1 Production no longer make sense.
    
    Repoint "staging" to Platform1 Staging, so that we can enable traffic replay
    again. Remove the target and feature flag for "plat1prod", since this is no
    longer a thing (we don't want Prod to replay against itself).
    
    Make changes to host entries notify the gor class which will restart the
    service. We don't need this right now, because `enable_staging` isn't
    enabled anywhere. But it may be useful in the future.
    
    The old `host{}` entry for "p1prod" will get purged by 6da6025.
    
    I've left some of the logic for specifying multiple hosts in case we want to
    do something similar again in the future. I've had to remove the spec test
    though, since this no longer applies.

--- modules/govuk/spec/classes/govuk_gor_spec.rb
+++ modules/govuk/spec/classes/govuk_gor_spec.rb
@@ -2,13 +2,11 @@ require_relative '../../../../spec_helper'
 
 shared_examples 'hosts and logstream enabled' do
   it { should contain_host(host_staging).with_ensure('present') }
-  it { should contain_host(host_plat1prod).with_ensure('present') }
   it { should contain_govuk__logstream('gor_upstart_log').with_ensure('present') }
 end
 
 describe 'govuk::gor', :type => :class do
   let(:host_staging) { 'www-origin-staging.production.alphagov.co.uk' }
-  let(:host_plat1prod) { 'www-origin-plat1.production.alphagov.co.uk' }
   let(:args_default) {{
     '-input-raw'          => 'localhost:7999',
     '-output-http-method' => %w{GET HEAD OPTIONS},
@@ -18,7 +16,6 @@ describe 'govuk::gor', :type => :class do
     let(:params) {{ }}
 
     it { should contain_host(host_staging).with_ensure('absent') }
-    it { should contain_host(host_plat1prod).with_ensure('absent') }
     it { should contain_class('gor').with_service_ensure('stopped') }
     it { should contain_govuk__logstream('gor_upstart_log').with_ensure('absent') }
   end
@@ -35,41 +32,6 @@ describe 'govuk::gor', :type => :class do
         :service_ensure => 'running',
         :args           => args_default.merge({
           '-output-http' => ["https://#{host_staging}"],
-        }),
-      )
-    }
-  end
-
-  context '#enable_plat1prod' do
-    let(:params) {{
-      :enable_plat1prod => true,
-    }}
-
-    it_should_behave_like 'hosts and logstream enabled'
-
-    it {
-      should contain_class('gor').with(
-        :service_ensure => 'running',
-        :args           => args_default.merge({
-          '-output-http' => ["https://#{host_plat1prod}"],
-        }),
-      )
-    }
-  end
-
-  context '#enable_staging and #enable_plat1prod' do
-    let(:params) {{
-      :enable_staging   => true,
-      :enable_plat1prod => true,
-    }}
-
-    it_should_behave_like 'hosts and logstream enabled'
-
-    it {
-      should contain_class('gor').with(
-        :service_ensure => 'running',
-        :args           => args_default.merge({
-          '-output-http' => ["https://#{host_staging}","https://#{host_plat1prod}"],
         }),
       )
     }
