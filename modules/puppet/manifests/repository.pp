57bf1cbe5a4d2fd512c63c6f1e2645c7f6183bc7
Dan Carley <dan.carley@gmail.com>
2014-04-02 09:01:25 +0100

    [#54520168] Use mirror of apt.puppetlabs.com
    Use our internal mirror for Puppet packages. Depends on gds/deployment#201
    
    IMPORTANT: The firewall restrictions on `apt.production.alphagov` mean that
    you will no longer be able to provision Vagrant boxes when not in the office
    or on the `/ah` VPN profile.
    
    Includes a feature flag that will flip this back to the upstream repo. Which
    we might need to use if we were bringing up a new environment that replaced
    or couldn't access the current Production.

--- modules/puppet/manifests/repository.pp
+++ modules/puppet/manifests/repository.pp
@@ -1,7 +1,37 @@
-class puppet::repository {
-  apt::source {'puppetlabs-repo':
-    location => 'http://apt.puppetlabs.com',
-    repos    => 'main dependencies',
-    key      => '4BD6EC30', # Puppet Labs Release Key (Puppet Labs Release Key) <info@puppetlabs.com>
+# == Class: puppet::repository
+#
+# Setup the APT repo for Puppet packages.
+#
+# === Parameters
+#
+# [*use_mirror*]
+#   Whether to use our own mirror of the PuppetLabs repo. You may want to
+#   temporarily disable this if you're bringing up a new production
+#   environment.
+#   Default: true
+#
+class puppet::repository(
+  $use_mirror = true,
+) {
+  # This is installed by bootstrapping. `apt::source` takes its place.
+  package { 'puppetlabs-release':
+    ensure => purged,
+  }
+
+  validate_bool($use_mirror)
+  if $use_mirror {
+    apt::source { 'puppetlabs':
+      location     => 'http://apt.production.alphagov.co.uk/puppetlabs',
+      release      => 'stable',
+      architecture => $::architecture,
+      key          => '37E3ACBB',
+    }
+  } else {
+    apt::source {'puppetlabs':
+      location => 'http://apt.puppetlabs.com',
+      release  => 'stable',
+      repos    => 'main dependencies',
+      key      => '4BD6EC30', # Puppet Labs Release Key (Puppet Labs Release Key) <info@puppetlabs.com>
+    }
   }
 }
