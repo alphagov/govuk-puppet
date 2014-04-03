c144b4d8336eb1f3e0da93e498c062b642e3aa19
Dan Carley <dan.carley@gmail.com>
2014-04-03 15:28:11 +0100

    Really remove govuk-fabric dir from jumpboxes
    jumpbox-1 and jumpbox-2 in Preview are logging the following:
    
        (/Stage[main]/Govuk::Node::S_jumpbox/File[/usr/local/share/govuk-fabric]) Not removing directory; use 'force' to override
    
    Nuke this directory. Later we can nuke the FIXME.

--- modules/govuk/manifests/node/s_jumpbox.pp
+++ modules/govuk/manifests/node/s_jumpbox.pp
@@ -1,8 +1,11 @@
 class govuk::node::s_jumpbox inherits govuk::node::s_base {
 
-  #REMOVE: once its been pushed out.
+  # FIXME: When deployed to preview (platform1 never got this).
   file { '/usr/local/share/govuk-fabric':
-    ensure => 'absent',
+    ensure  => absent,
+    recurse => true,
+    force   => true,
+    backup  => false,
   }
 
 }
