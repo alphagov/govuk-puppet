c93a3003ad6978877ce30764f714634dfa745eaf
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-04-04 11:32:14 +0100

    Fix whitehall delayed_job cleanup.
    This was failing to do the cleanup if the worker wasn't running.

--- modules/govuk/manifests/apps/whitehall.pp
+++ modules/govuk/manifests/apps/whitehall.pp
@@ -125,7 +125,7 @@ class govuk::apps::whitehall(
     # Clean up old delayed-job worker and config.
     # These can be removed once this is pushed out to production
     exec {'stop_whitehall_admin_delayed_job_worker':
-      command => 'initctl stop whitehall-admin-delayed-job-worker',
+      command => 'initctl stop whitehall-admin-delayed-job-worker || /bin/true',
       onlyif  => 'test -f /etc/init/whitehall-admin-delayed-job-worker.conf',
     }
     file { '/etc/init/whitehall-admin-delayed-job-worker.conf':
