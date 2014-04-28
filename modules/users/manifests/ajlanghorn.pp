f1d6da9ab95b1d1acade5f476e70510a56a4f0fc
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-04-28 10:09:10 +0100

    Amend SSH key
    I have a new Mac. This commit amends my SSH key.

--- modules/users/manifests/ajlanghorn.pp
+++ modules/users/manifests/ajlanghorn.pp
@@ -2,6 +2,6 @@ class users::ajlanghorn {
   govuk::user { 'ajlanghorn':
     fullname => 'Andrew Langhorn',
     email    => 'andrew.langhorn@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIrL7kXMd9lAj5TgX81FY/zo6IvU4iWo4ajqW8F8FaMZ6zaBnWh4WBhS7/Pgh7RN8mN8nGtbF+UNOlcjgz1gdkI8VDXYtUqSxiffeEaIiTnjvt6EH5mLSbkTkuI8e4ar8Gu2Nutex3ZkmhfMxthN6IxfZkgOj4HDHUlc0+YSZBlX0miDQzdBdty+wSifDjpx0aV3UADKcGUHmUVOmAa6LqGd2YwODP+tZ30T2F2VONh7BW5BUKlNH5Dm+c+0e/DGVgXTaqT9/Pc3LLDUll5UJSNDzUjjCohJ8MrhaaFzyBhC3DyQIy1x55P6jDW2lvHNO7i8L0LmQOahAnyzPxP7KD',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNRVe5iVIwcWJ4vxv4e1EgxuXelMYMbYop0TnzHmI2Kj/olA7x0eZy2nXIMQovjteGEu14exuDnWxXsZZA0yIcNKQKgX+WIcqsZX1hnzfVtX6+asLsolVn6OE+WalY3tYD6h2O94BYzYDnazARebdDC9tlAPxPFZG+TJYmgdHn/atzr9A5iEjFyprMYvf8JRxg1eP+uB/Zty6oy2jXyKAuPDjNEikkIFrSov2kHYvLSY5k10cI1sTjvU47yp7J/KmXlkAO4wfr0L+ffpRibUf85FnvLq3HG7ruXoYDMEX4NEB7xzE8D2Dn58mOIUuqIQio2YDRah7FByZHxR/85ntT',
   }
 }
