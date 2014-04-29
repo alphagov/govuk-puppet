80c6da4d6388add88edbfa10de515d3fd21fe9f5
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-04-29 10:05:30 +0100

    [#70264760] Bump govuk_mirrorer version to 1.3.0
    This picks up the instrumentation for various parts of the mirrorer
    that have been added recently.

--- modules/ruby/manifests/govuk_mirrorer.pp
+++ modules/ruby/manifests/govuk_mirrorer.pp
@@ -1,6 +1,6 @@
 class ruby::govuk_mirrorer {
   package { 'govuk_mirrorer':
-    ensure   => '1.2.0',
+    ensure   => '1.3.0',
     provider => system_gem,
     source   => extlookup('govuk_gemfury_source_url'),
     require  => Package['libxml2-dev'],
