12f6c7385dcbb12a06d3e9df292917d3e960fd70
Cal Paterson <cal@calpaterson.com>
2014-04-29 11:39:38 +0100

    Use gds-nagios-plugins pip package
    - use gds-nagios-plugins in preference to our ones
      - use ensure => absent
    - correct arguments passed to checks

--- modules/monitoring/manifests/client.pp
+++ modules/monitoring/manifests/client.pp
@@ -6,6 +6,21 @@ class monitoring::client {
   include auditd
   include collectd
 
+  # Provides:
+  # - `notify-reboot-required` which is referenced in the `postinst`
+  #   of some packages in order to request that the system be rebooted at a
+  #   convenient time. Required by the `check_reboot_required` alert, but
+  #   still beneficial to have on nodes that aren't monitored by Nagios.
+  # - `apt-check` which is called by the `check_apt_security_updates` alert.
+  #   More reliable than the builtin Nagios plugin.
+  package {'update-notifier-common': }
+
+  package {'gds-nagios-plugins':
+    ensure   => '1.1.0',
+    provider => 'pip',
+    require  => Package['update-notifier-common']
+  }
+
   class { 'statsd':
     graphite_hostname => 'graphite.cluster',
   }
