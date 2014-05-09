ec640729e393fd67520e517250e78b11bcca0224
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-05-09 13:54:28 +0100

    Tests for existing behaviour when using hiera
    I am about to change the switches in this module such that instead of
    using `govuk_platform` then use a class parameter passed in from Hiera.
    
    The tests have been extended to confirm the existing behaviour but with
    a new class parameter passed in.

--- modules/puppet/spec/classes/puppet_spec.rb
+++ modules/puppet/spec/classes/puppet_spec.rb
@@ -4,11 +4,26 @@ describe 'puppet', :type => :class do
   let (:hiera_data) {{
     :app_domain => 'giraffe.example.com',
   }}
-  let(:facts) {{
-    :govuk_platform => 'production',
-  }}
 
-  it do
-    should contain_file('/etc/puppet/puppet.conf')
+  context 'with $use_puppetmaster = false' do
+    let (:params) {{
+      :use_puppetmaster => false,
+    }}
+
+    it do
+      should contain_file('/etc/puppet/puppet.conf').with_content(/manifestdir = \/var\/govuk\/puppet\/manifests/)
+      should contain_file('/usr/local/bin/govuk_puppet').with_source('puppet:///modules/puppet/govuk_puppet_development')
+    end
+  end
+
+  context 'with $use_puppetmaster = true' do
+    let (:params) {{
+      :use_puppetmaster => true,
+    }}
+
+    it do
+      should contain_file('/etc/puppet/puppet.conf').with_content(/manifestdir = \/usr\/share\/puppet\/production\/current\/manifests/)
+      should contain_file('/usr/local/bin/govuk_puppet').with_source('puppet:///modules/puppet/govuk_puppet')
+    end
   end
 end
