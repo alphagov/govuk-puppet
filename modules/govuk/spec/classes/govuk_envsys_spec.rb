c7826c93fd18a77bcb2d46683d0f85c073429b6e
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-05-22 10:43:52 +0100

    fix govuk::envsys test

--- modules/govuk/spec/classes/govuk_envsys_spec.rb
+++ modules/govuk/spec/classes/govuk_envsys_spec.rb
@@ -6,7 +6,6 @@ describe 'govuk::envsys', :type => :class do
     let(:facts) {{}}
 
     # with_content does not currently accept an argument.
-    it { should contain_file(file_path).with_content(/^(PATH|LC_ALL)/) }
-    it { should_not contain_file(file_path).with_content(/FACTER_/) }
+    it { should contain_file(file_path).with_source('puppet:///modules/govuk/etc/environment') }
   end
 end
