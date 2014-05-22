cf0d11c2ad8d1a15e846b78d800f11f4bbd7644b
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2014-05-22 14:29:10 +0100

    Checks for explicit disks on offsite backup machine.
    We now have more than one backup disk on the offsite backup machine.
    Explicitly check those disks and have a default one to catch all disks.

--- modules/backup/manifests/offsite/monitoring.pp
+++ modules/backup/manifests/offsite/monitoring.pp
@@ -15,6 +15,20 @@ class backup::offsite::monitoring {
     host_name           => $offsite_fqdn,
   }
 
+  icinga::check { "check_disk_backup-data_${offsite_hostname}":
+    check_command       => 'check_nrpe_1arg!check_disk_backup-data',
+    service_description => 'high disk usage',
+    use                 => 'govuk_high_priority',
+    host_name           => $offsite_fqdn,
+  }
+
+  icinga::check { "check_disk_logs-backup_${offsite_hostname}":
+    check_command       => 'check_nrpe_1arg!check_disk_logs-backup',
+    service_description => 'high disk usage',
+    use                 => 'govuk_high_priority',
+    host_name           => $offsite_fqdn,
+  }
+
   icinga::check { "check_ssh_${offsite_hostname}":
     check_command       => 'check_ssh',
     use                 => 'govuk_high_priority',
