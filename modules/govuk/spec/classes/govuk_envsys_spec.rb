162850fe95520611b298e4708d300219da12e6a1
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-05-22 08:32:55 +0100

    Remove FACTER_govuk_platform from /etc/environment

--- modules/govuk/spec/classes/govuk_envsys_spec.rb
+++ modules/govuk/spec/classes/govuk_envsys_spec.rb
@@ -2,19 +2,11 @@ require_relative '../../../../spec_helper'
 
 describe 'govuk::envsys', :type => :class do
   let(:file_path) { '/etc/environment' }
-  context 'with no govuk vars' do
+  context 'as a file not a template' do
     let(:facts) {{}}
 
     # with_content does not currently accept an argument.
     it { should contain_file(file_path).with_content(/^(PATH|LC_ALL)/) }
-    it { should_not contain_file(file_path).with_content(/FACTER_govuk/) }
-  end
-
-  context 'with govuk vars' do
-    let(:facts) {{
-      :govuk_platform => 'foo',
-    }}
-
-    it { should contain_file(file_path).with_content(/^FACTER_govuk_platform=foo$/) }
+    it { should_not contain_file(file_path).with_content(/FACTER_/) }
   end
 end
