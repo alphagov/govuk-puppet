18f0779cb88ece7073891afca0788d7dd06d0f11
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-05-28 12:03:47 +0100

    Remove basic auth for search-admin
    Also tweak some other things to bring it inline with other admin apps:
    
    * enable asset-pipeline nginx rules (caching etc.)
    * deny framing (Set X-Frame-Options header)
    * Make it SSL only.

--- modules/govuk/manifests/apps/search_admin.pp
+++ modules/govuk/manifests/apps/search_admin.pp
@@ -1,13 +1,11 @@
-class govuk::apps::search_admin(
-  $port = 3073,
-  $vhost_protected = true,
-) {
-
+class govuk::apps::search_admin( $port = 3073 ) {
   govuk::app { 'search-admin':
     app_type               => 'rack',
     port                   => $port,
-    vhost_protected        => $vhost_protected,
+    vhost_ssl_only         => true,
     health_check_path      => '/best-bets',
     log_format_is_json     => true,
+    asset_pipeline         => true,
+    deny_framing           => true,
   }
 }
