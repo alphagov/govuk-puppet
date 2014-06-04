5f04027b2af6daf3c954d12529b2201e1a587087
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2014-06-04 16:14:28 +0100

    Migrate backup ssh key extdata to hiera.

--- modules/backup/manifests/client.pp
+++ modules/backup/manifests/client.pp
@@ -1,11 +1,11 @@
-class backup::client {
-
-  $backup_pubkey = extlookup('govuk-backup_key', '')
+class backup::client (
+  $backup_public_key = '',
+) {
 
   govuk::user { 'govuk-backup':
     fullname  => 'Backup User',
     email     => 'webops@digital.cabinet-office.gov.uk',
-    ssh_key   => "ssh-rsa ${backup_pubkey}",
+    ssh_key   => "ssh-rsa ${backup_public_key}",
   }
 
 }
