8657b10979c13f4de35c20fc58114b0786c3498e
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-06-06 14:38:52 +0100

    Put exception_handler mongo db on separate disk

--- modules/govuk/manifests/node/s_exception_handler.pp
+++ modules/govuk/manifests/node/s_exception_handler.pp
@@ -9,4 +9,10 @@ class govuk::node::s_exception_handler inherits govuk::node::s_base {
   }
   include nginx
   include govuk::apps::errbit
+
+  #FIXME: remove if when we have moved to platform one
+  if hiera(use_hiera_disks,false) {
+    Govuk::Mount['/var/lib/mongodb'] -> Class['mongodb::server']
+    Govuk::Mount['/var/lib/automongodbbackup'] -> Class['mongodb::backup']
+  }
 }
