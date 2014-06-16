78edf26dc78d84989a517b326becfc0f891eb435
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-06-16 20:52:47 +0100

    Switch certs based on certtype with explicit lookups
    The former code relied on hiera lookups on an interpolated string which
    our linter fails to grasp. Rather than fix the linter, I've collapsed
    the code which switches certs with the code that checks for valid
    certtype parameter. This isn't pretty, but it's no more ugly than the
    former solution.

--- modules/nginx/manifests/config/ssl.pp
+++ modules/nginx/manifests/config/ssl.pp
@@ -19,26 +19,31 @@
 #   environments where the key is not present in extlookup.
 #
 define nginx::config::ssl( $certtype ) {
-  if ! ($certtype in [
-      'www',
-      'ci_alphagov',
-      'wildcard_alphagov',
-      'wildcard_alphagov_mgmt'
-    ]) {
-    fail "${certtype} is not a valid certtype"
-  }
-
-  if $certtype == 'wildcard_alphagov_mgmt' {
-    # The below two lines are hard to mentally parse, but if the certtype is set to
-    # 'wildcard_alphagov_mgmt' then it will first look for certs and keys for that.
-    # If they don't exist in hiera, it will fall back to looking for 'wildcard_alphagov'
-    # and if that fails, will return the empty string.
-    #FIXME: chomp() is to maintain md5 between previous extlookup - can be removed once deployed
-    $cert = chomp(hiera("${certtype}_crt", hiera('wildcard_alphagov_crt', '')))
-    $key = chomp(hiera("${certtype}_key", hiera('wildcard_alphagov_key', '')))
-  } else {
-    $cert = chomp(hiera("${certtype}_crt", ''))
-    $key = chomp(hiera("${certtype}_key", ''))
+  case $certtype {
+    'ci_alphagov': {
+        $cert = chomp(hiera('ci_alphagov_crt', ''))
+        $key = chomp(hiera('ci_alphagov_key', ''))
+    }
+    'wildcard_alphagov': {
+        $cert = chomp(hiera('wildcard_alphagov_crt', ''))
+        $key = chomp(hiera('wildcard_alphagov_key', ''))
+    }
+    'wildcard_alphagov_mgmt': {
+        # The below two lines are hard to mentally parse, but if the certtype is set to
+        # 'wildcard_alphagov_mgmt' then it will first look for certs and keys for that.
+        # If they don't exist in hiera, it will fall back to looking for 'wildcard_alphagov'
+        # and if that fails, will return the empty string.
+        #FIXME: chomp() is to maintain md5 between previous extlookup - can be removed once deployed
+        $cert = chomp(hiera('wildcard_alphagov_mgmt_crt', hiera('wildcard_alphagov_crt', '')))
+        $key = chomp(hiera('wildcard_alphagov_mgmt_key', hiera('wildcard_alphagov_key', '')))
+    }
+    'www': {
+        $cert = chomp(hiera('www_crt', ''))
+        $key = chomp(hiera('www_key', ''))
+    }
+    default: {
+        fail "${certtype} is not a valid certtype"
+    }
   }
 
   file { "/etc/nginx/ssl/${name}.crt":
