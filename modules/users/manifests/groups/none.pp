c50b1a3d87b6a190954330248c2922f5da675add
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-06-16 12:18:04 +0100

    Only purge users if user_groups is not empty
    This fixes the tests, although in a slightly dirty way. The new
    behaviour is that:
    
     - user_groups defaults to empty array
     - purging only happens when user_groups is non-empty
     - to have no users and to purge, user_groups => ['none']

--- modules/users/manifests/groups/none.pp
+++ modules/users/manifests/groups/none.pp
@@ -1,9 +1,9 @@
 # This class is empty. It only exists to pass an empty user list
 # to `users` via hiera and acts as a safety measure to prevent passing
-# a blank array of user groups.
+# a blank array of user groups and then removing user accounts
 
-# If you deliberately want to add no users, you must have the following
-# set in hiera to avoid a catalog compile error:
+# If you deliberately want to add no users and purge existing ones, you
+# must have the following set in hiera:
 #
 #   users::user_groups:
 #           - 'none'
--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -1,13 +1,20 @@
-# In order to have no user accounts defined, you *must* have user_groups => ['none']
-# set via hiera or otherwise during class instantiation. This is a safety check to
-# prevent accidentally passing a blank list of users.
 class users (
-    $user_groups
+    $user_groups = [],
   ) {
-  # Remove unmanaged UIDs >= 500 (users, not system accounts).
-  resources { 'user':
-    purge              => true,
-    unless_system_user => 500,
+  # Remove unmanaged UIDs >= 500 (users, not system accounts),
+  # In order to have no user accounts defined and purge existing accounts, you *must*
+  # have user_groups => ['none'] set via hiera or otherwise during class instantiation.
+  # This is a safety check to prevent accidentally passing a blank list of users and
+  # then removing all accounts
+  if !empty($user_groups) {
+    resources { 'user':
+      purge              => true,
+      unless_system_user => 500,
+    }
+  } else {
+    notify {'missing user::groups':
+        message => 'You have not set user::groups so users will not be purged',
+    }
   }
 
   group { 'admin':
