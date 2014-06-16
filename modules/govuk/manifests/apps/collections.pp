36bdbd2fa7acaf991d783747684c6a882aa73e9b
Edd Sowden <e@e26.co.uk>
2014-06-16 08:05:36 +0000

    Proxy content api on the collections app in dev
    On production the router routes requests to `/api` to the `publicapi`
    application which then forwards the requests to the correct application.
    This change lets us mock that in development so requests to `/api` go to
    the contentapi.

--- modules/govuk/manifests/apps/collections.pp
+++ modules/govuk/manifests/apps/collections.pp
@@ -1,7 +1,24 @@
 class govuk::apps::collections(
   $port = 3070,
-  $vhost_protected
+  $vhost_protected,
+  $proxy_content_api = false,
 ) {
+  $app_domain = hiera('app_domain')
+
+  validate_bool($proxy_content_api)
+  if ($proxy_content_api) {
+    # So that AJAX requests are handled by the same application in development as production
+    $nginx_extra_config = "
+      location /api {
+        rewrite ^/api/?(.*) /\$1 break;
+        proxy_set_header Host 'contentapi.${app_domain}';
+        proxy_pass http://contentapi.${app_domain};
+      }
+    "
+  } else {
+    $nginx_extra_config = ''
+  }
+
   govuk::app { 'collections':
     app_type              => 'rack',
     port                  => $port,
@@ -10,5 +27,6 @@ class govuk::apps::collections(
     log_format_is_json    => true,
     asset_pipeline        => true,
     asset_pipeline_prefix => 'collections',
+    nginx_extra_config    => $nginx_extra_config,
   }
 }
