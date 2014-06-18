67394a9330e6512a4341ec4f73d3f5dddd9df9c1
Andrew Mitchell <themitchell@me.com>
2014-06-18 12:11:43 +0100

    Make sure to include '==' at the end of Andrew's SSH key

--- modules/users/manifests/andrewmitchell.pp
+++ modules/users/manifests/andrewmitchell.pp
@@ -2,6 +2,6 @@ class users::andrewmitchell {
   govuk::user { 'andrewmitchell':
     fullname => 'Andrew Mitchell',
     email    => 'andrew.mitchell@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAopOOKxHm7ZwPinybuk4i8nVtZgJCZ8SfVfcvyoP/xSInTuY3B7YDQAmwg/TebHOziNo76AA/+Wqe/s1O8KVjxkRfC61OvbHk3zrEPEXSovRDPtSu+n0qBYDLMI3kXfqeUj9jxAtKF43J8mvzy3VwQ1iDD0jqAvK/1zPyQJZS1gWjQe9+D96zCUw4PjDEY24hMlJtAiMjwhDxSa02QwyA9DD0N4g0H960NtnMB46a7aMe23AncTzXRpNVmt4Fm3AeeZB+zT75EToKayCt0dN85/nJ+uE3Mwc6LlHbBScoJjukhDLucVUVFNaDfBdHQCG4EKNmcV8ey4Ngp8ZCbSHPyw',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAopOOKxHm7ZwPinybuk4i8nVtZgJCZ8SfVfcvyoP/xSInTuY3B7YDQAmwg/TebHOziNo76AA/+Wqe/s1O8KVjxkRfC61OvbHk3zrEPEXSovRDPtSu+n0qBYDLMI3kXfqeUj9jxAtKF43J8mvzy3VwQ1iDD0jqAvK/1zPyQJZS1gWjQe9+D96zCUw4PjDEY24hMlJtAiMjwhDxSa02QwyA9DD0N4g0H960NtnMB46a7aMe23AncTzXRpNVmt4Fm3AeeZB+zT75EToKayCt0dN85/nJ+uE3Mwc6LlHbBScoJjukhDLucVUVFNaDfBdHQCG4EKNmcV8ey4Ngp8ZCbSHPyw==',
   }
 }
