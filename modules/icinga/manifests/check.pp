47dc26aeb4a93cc3f41e950283dbe458fd437aff
Jacob Ashdown <jcbashdown@gmail.com>
2014-06-18 13:04:43 +0100

    Validate icinga service_description
    When deploying checks with invalid characters in the service description
    you kill icinga for the environment. This is bad. This change will stop
    puppet running in this case which is better than killing icinga.

--- modules/icinga/manifests/check.pp
+++ modules/icinga/manifests/check.pp
@@ -68,6 +68,8 @@ define icinga::check (
   $contact_groups             = undef,
 ) {
 
+  validate_re($service_description, '^(\w|\s|\-|:|\.)*$', "Icinga check ${service_description} contains invalid characters")
+
   $app_domain = hiera('app_domain')
   $graph_width = 600
   $graph_height = 300
