38e9a3df27cfe7ac2b7b9949a6a031181bcebe4b
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-06-25 09:17:52 +0100

    Enable contacts-frontend everywhere.
    This is now going to be deployed to staging and prod.

--- modules/govuk/manifests/apps/contacts_frontend.pp
+++ modules/govuk/manifests/apps/contacts_frontend.pp
@@ -1,17 +1,14 @@
 class govuk::apps::contacts_frontend (
   $port = 3074,
-  $vhost_protected,
-  $enabled = false
+  $vhost_protected
 ) {
-  if $enabled {
-    govuk::app { 'contacts-frontend':
-      app_type              => 'rack',
-      port                  => $port,
-      vhost_protected       => $vhost_protected,
-      health_check_path     => '/healthcheck',
-      log_format_is_json    => true,
-      asset_pipeline        => true,
-      asset_pipeline_prefix => 'contacts-frontend',
-    }
+  govuk::app { 'contacts-frontend':
+    app_type              => 'rack',
+    port                  => $port,
+    vhost_protected       => $vhost_protected,
+    health_check_path     => '/healthcheck',
+    log_format_is_json    => true,
+    asset_pipeline        => true,
+    asset_pipeline_prefix => 'contacts-frontend',
   }
 }
