f44ba9407a82c74750967ef5d9388fd2291111d2
Dan Carley <dan.carley@gmail.com>
2014-07-01 12:01:26 +0100

    [#70083992] Use MongoDB APT mirror
    Use our own mirror of the MongoDB repo to improve our availability. This
    contains all of the package versions that we need.
    
    Like the Puppet repo, it can be flagged off if we want to temporarily revert
    to upstream.

--- modules/mongodb/manifests/repository.pp
+++ modules/mongodb/manifests/repository.pp
@@ -1,8 +1,31 @@
-class mongodb::repository {
-  apt::source { '10gen':
-    location => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart',
-    key      => '7F0CEB10', # Richard Kreuter <richard@10gen.com>
-    release  => 'dist',
-    repos    => '10gen',
+# == Class: mongodb::repository
+#
+# Manage APT repo for MongoDB.
+#
+# === Parameters
+#
+# [*use_mirror*]
+#   Whether to use our mirror of the repo.
+#   Default: true
+#
+class mongodb::repository(
+  $use_mirror = true,
+) {
+  validate_bool($use_mirror)
+  if $use_mirror {
+    apt::source { 'mongodb':
+      location     => 'http://apt.production.alphagov.co.uk/mongodb',
+      release      => 'dist',
+      repos        => '10gen',
+      architecture => $::architecture,
+      key          => '37E3ACBB',
+    }
+  } else {
+    apt::source { 'mongodb':
+      location => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart',
+      release  => 'dist',
+      repos    => '10gen',
+      key      => '7F0CEB10', # Richard Kreuter <richard@10gen.com>
+    }
   }
 }
