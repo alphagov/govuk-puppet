7e027c7a67d07f556642a12bd36e212dba6f763a
Cal Paterson <cal@calpaterson.com>
2014-07-03 13:45:55 +0100

    Switch over to using new rabbitmq collectd plugin
    - remove the old one
    - change the config over
    - verbose logging for now

--- modules/govuk_rabbitmq/manifests/init.pp
+++ modules/govuk_rabbitmq/manifests/init.pp
@@ -1,4 +1,6 @@
-class govuk_rabbitmq {
+class govuk_rabbitmq (
+  $monitoring_password
+  ) {
   include govuk_rabbitmq::firewalls
   include govuk_rabbitmq::logging
   include govuk_rabbitmq::monitoring
@@ -9,9 +11,15 @@ class govuk_rabbitmq {
     ensure => present,
   }
 
-  rabbitmq_plugin {'rabbitmq_management':
-    ensure => present,
+  # FIXME: Consider using a normal user with a "monitoring" user tag.  Not
+  # configurable from puppet using the current puppetlabs/rabbitmq (4.0).
+  # Added here: https://github.com/puppetlabs/puppetlabs-rabbitmq/pull/193
+  rabbitmq_user {'monitoring':
+    admin    => true,
+    password => $monitoring_password
   }
 
-  class { 'collectd::plugin::rabbitmq': }
+  class { 'collectd::plugin::rabbitmq':
+    monitoring_password => $monitoring_password
+  }
 }
