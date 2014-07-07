eb883e8eb1e18d10a0024d7e90fd22722b8b135b
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-07-07 11:27:29 +0100

    Raise urgency of a varnish not-responding alert
    It is possible, when a machine is under high load, for the varnish child
    process to die, being replaced by another child process that also dies,
    leaving a parent process and no children. In this state, varnishd will
    not respond to connections.
    
    It will also cause 1/3 of the requests to our origin to fail.
    
    This check has a good history of not alerting unless there is a real
    problem. It alerted today on cache-3 for the first time ever and there
    was a real problem.
    
    I have:
    
    1. Raised the priority of this alert to urgent, which will cause a page
    2. Documented the resolution in the Ops Manual
    3. Linked to an appropriate graph that I used to diagnose the issue.

--- modules/varnish/manifests/monitoring.pp
+++ modules/varnish/manifests/monitoring.pp
@@ -14,9 +14,13 @@ class varnish::monitoring {
     source => 'puppet:///modules/varnish/nrpe_check_varnish.cfg',
   }
 
+  $monitoring_domain_suffix = hiera('monitoring_domain_suffix', '')
   @@icinga::check { "check_varnish_responding_${::hostname}":
     check_command       => 'check_nrpe_1arg!check_varnish_responding',
     service_description => 'varnishd port not responding',
     host_name           => $::fqdn,
+    use                 => 'govuk_urgent_priority',
+    notes_url           => 'https://github.gds/pages/gds/opsmanual/2nd-line/nagios.html#varnish-port-not-responding',
+    action_url          => "https://graphite.${monitoring_domain_suffix}/render?from=-8hours&until=now&width=800&height=600&target=alias%28stats.cache-1_router_production.nginx_logs.www-origin.http_5xx,%27cache-1%20HTTP%205xx%20Errors%27%29&target=alias%28stats.cache-2_router_production.nginx_logs.www-origin.http_5xx,%27cache-2%20HTTP%205xx%20Errors%27%29&target=alias%28stats.cache-3_router_production.nginx_logs.www-origin.http_5xx,%27cache-3%20HTTP%205xx%20Errors%27%29&title=Error%20Rates%20at%20Caches",
   }
 }
