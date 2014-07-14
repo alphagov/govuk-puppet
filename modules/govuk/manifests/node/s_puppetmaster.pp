06c0f0feecb5fb61567e9ce0fec909652a01a757
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-07-14 10:35:58 +0100

    [#71305918] Remove ruby::rubygems class
    This commit removes any reference to the `ruby::rubygems` class now that
    machines are no longer bodging system Ruby and RubyGems.

--- modules/govuk/manifests/node/s_puppetmaster.pp
+++ modules/govuk/manifests/node/s_puppetmaster.pp
@@ -1,7 +1,5 @@
 # == Class: govuk::node::s_puppetmaster
 #
 class govuk::node::s_puppetmaster inherits govuk::node::s_base {
-  class { 'puppet::master':
-    subscribe => Class['ruby::rubygems'],
-  }
+  include puppet::master
 }
