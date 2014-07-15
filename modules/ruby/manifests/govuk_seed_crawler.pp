84d5b47a4d210451239d8f276dd6ee6084f9b2ce
Dan Carley <dan.carley@gmail.com>
2014-07-15 13:27:45 +0100

    Bump govuk_seed_crawler to 0.1.0
    Bug fixes: https://github.gds/gds/govuk_seed_crawler/compare/v0.0.1...v0.1.0

--- modules/ruby/manifests/govuk_seed_crawler.pp
+++ modules/ruby/manifests/govuk_seed_crawler.pp
@@ -2,7 +2,7 @@ class ruby::govuk_seed_crawler (
   $govuk_gemfury_source_url
 ){
   package { 'govuk_seed_crawler':
-    ensure   => '0.0.1',
+    ensure   => '0.1.0',
     provider => system_gem,
     source   => $govuk_gemfury_source_url,
   }
