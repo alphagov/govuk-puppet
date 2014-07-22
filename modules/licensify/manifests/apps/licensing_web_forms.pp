2a50cdf429760869bb1dacf2b9b6489dc376238d
David Ainslie <dainslie@gmail.com>
2014-07-22 09:45:16 +0100

    This application is currently only for Preview

--- modules/licensify/manifests/apps/licensing_web_forms.pp
+++ modules/licensify/manifests/apps/licensing_web_forms.pp
@@ -1,9 +1,10 @@
-class licensify::apps::licensing_web_forms( $port = 9001 ) inherits licensify::apps::base {
-
-  govuk::app { 'licensing-web-forms':
-    app_type            => 'procfile',
-    port                => $port,
-    vhost_protected     => false,
-    # health_check_path => '/login', TODO
+class licensify::apps::licensing_web_forms( $port = 9001, $enabled = false ) inherits licensify::apps::base {
+  if str2bool($enabled) {
+    govuk::app { 'licensing-web-forms':
+      app_type            => 'procfile',
+      port                => $port,
+      vhost_protected     => false,
+      health_check_path => '/'
+    }
   }
 }
