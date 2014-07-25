a7c5b939d1adcc1579ab6942c9dfc8a2f8aa77b0
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-07-25 17:40:27 +0100

    Remove in-tree postgres module
    PRs #1960 #1961 #1965 migrate our usage to the puppetlabs postgresql
    module, which is much much better.
    
    This removes our in-tree module completely.
    
    NB: Tests will not pass until #1961 and #1965 are merged.

--- modules/postgres/manifests/package.pp
+++
@@ -1,9 +0,0 @@
-class postgres::package {
-
-  include govuk::ppa
-
-  package { 'postgresql-9.1':
-    ensure => present,
-  }
-
-}
--- modules/postgres/spec/classes/postgres_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../../../../spec_helper'
-
-describe 'postgres', :type => :class do
-  it do
-    should contain_package("postgresql-9.1")
-    should contain_file("/etc/postgresql/9.1/main/postgresql.conf")
-    should contain_service("postgresql")
-  end
-end
