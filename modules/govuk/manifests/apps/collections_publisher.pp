f0221bfaa3753e0e1ea66c965dca574392950ed4
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-08-04 09:24:36 +0000

    Remove feature flag for collections publisher

--- modules/govuk/manifests/apps/collections_publisher.pp
+++ modules/govuk/manifests/apps/collections_publisher.pp
@@ -1,18 +1,15 @@
 class govuk::apps::collections_publisher(
   $port = 3078,
-  $enabled = false,
 ) {
 
-  if $enabled {
-    govuk::app { 'collections-publisher':
-      app_type               => 'rack',
-      port                   => $port,
-      vhost_ssl_only         => true,
-      health_check_path      => '/',
-      log_format_is_json     => true,
-      asset_pipeline         => true,
-      deny_framing           => true,
-    }
+  govuk::app { 'collections-publisher':
+    app_type               => 'rack',
+    port                   => $port,
+    vhost_ssl_only         => true,
+    health_check_path      => '/',
+    log_format_is_json     => true,
+    asset_pipeline         => true,
+    deny_framing           => true,
   }
 
 }
