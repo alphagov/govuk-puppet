a65595bb5f51a2db3d1bc881c5948859379320d9
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2014-08-04 12:19:11 +0100

    Configure transition's postgres database

--- modules/govuk/manifests/node/s_transition_postgresql_master.pp
+++ modules/govuk/manifests/node/s_transition_postgresql_master.pp
@@ -1,5 +1,7 @@
 class govuk::node::s_transition_postgresql_master inherits govuk::node::s_base {
   include govuk_postgresql::server
 
+  include govuk::apps::transition::postgresql_db
+
   Govuk::Mount['/var/lib/postgresql'] -> Class['govuk_postgresql::server']
 }
