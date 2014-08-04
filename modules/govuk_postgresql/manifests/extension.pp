b66b63119618c28b77f8e68baa9e228df098d8f1
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-08-04 13:15:33 +0100

    Add a wrapper module for postgresql
    This wraps the postgresql module and applies our defaults of:
    
    1. Packages to install on clients (libpq-dev)
    2. Packages to install on servers (postgresql-server-contrib)
    3. Database defaults:
       - owner is the same as user
       - default encoding is UTF8
    4. Installing database extensions
    
    In general, it can be used as:
    ```
    include govuk_postgresql::server
    include govuk_postgresql::client
    govuk_postgresql::db {'db-name':
       user     => myuser,
       password => mypassword,
    }
    ```

---
+++ modules/govuk_postgresql/manifests/extension.pp
@@ -0,0 +1,29 @@
+# == Define: govuk_postgresql::extension
+#
+# A simple wrapper for an exec that will load a Postgres extension into
+# a database
+#
+# The title of this resource must be the name of the database and the
+# extension to load into it, colon separated
+#
+# === Parameters
+#
+# [*db*]
+# The database to install the extension for
+#
+define govuk_postgresql::extension {
+    include postgresql::server::contrib
+    $extracted_title = split($title,':')
+    if (size($extracted_title) < 2) {
+        fail("Could not extract db_name:extension from ${title}")
+    } else {
+        $db = $extracted_title[0]
+        $extension = $extracted_title[1]
+        exec {"Load ${extension} for Postgres DB: ${db}":
+            command => "psql -d ${db} -c 'CREATE EXTENSION IF NOT EXISTS ${extension}'",
+            unless  => "psql -d ${db} -c '\\dx' | grep -q ${extension}",
+            user    => 'postgres',
+            require => [Class['postgresql::server::contrib'], Postgresql::Server::Db[$db]],
+        }
+    }
+}
