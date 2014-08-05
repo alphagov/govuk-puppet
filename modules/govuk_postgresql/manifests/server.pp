d17330420d4c587a7e31f300ed1e437a1c05aace
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-08-05 11:01:56 +0100

    Backup postgres servers by default
    This:
     - adds the autopostgresqlbackup package
     - requires backup::client
     - sets it to:
       - keep a separate copy of latest backup
       - run a script after backup to allow govuk-backup to read files
    
    Separate commit coming up to disable backups in dev and vagrant

--- modules/govuk_postgresql/manifests/server.pp
+++ modules/govuk_postgresql/manifests/server.pp
@@ -1,10 +1,16 @@
 # Wrapper for all the things needed for a postgres server
-class govuk_postgresql::server {
+class govuk_postgresql::server (
+    $backup = true,
+) {
     class {'postgresql::server':
         listen_addresses => '*',
     }
 
     @ufw::allow { 'allow-postgresql-from-all':
         port => 5432,
+    }
+
+    if ($backup) {
+        include govuk_postgresql::backup
     }
 }
