069acfdef9f20b055e1018959c8a6437c7fd2363
Dominic Baggott <dominic.baggott@gmail.com>
2014-08-05 14:30:04 +0100

    Add Sidekiq to Specialist Publisher
    Enable the Procfile worker by default. Disable it in development.
    Follows the same pattern as all the other apps.

--- modules/govuk/manifests/apps/specialist_publisher.pp
+++ modules/govuk/manifests/apps/specialist_publisher.pp
@@ -2,6 +2,7 @@ class govuk::apps::specialist_publisher(
   $port = 3064,
   $vhost_protected = false,
   $enabled = false,
+  $enable_procfile_worker = true,
 ) {
 
   if str2bool($enabled) {
@@ -11,6 +12,10 @@ class govuk::apps::specialist_publisher(
       vhost_protected        => $vhost_protected,
       health_check_path      => '/specialist-documents',
       log_format_is_json     => true,
+    }
+
+    govuk::procfile::worker { 'specialist-publisher':
+      enable_service => $enable_procfile_worker,
     }
   }
 }
