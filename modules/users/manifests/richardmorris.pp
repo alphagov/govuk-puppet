93f8a0fdb54b0088817729a4559542bb97569aa8
Jake Benilov <benilov@gmail.com>
2014-08-12 16:32:35 +0100

    Revert "Merge pull request #2024 from gds/add-ricky"
    This reverts commit f9e89287ece5891abe2ec5121a1a39c0b210cb6e, reversing
    changes made to c5e93f365caf92e59116310d9dcff952de9d656d.
    
    This was added by mistake.

--- modules/users/manifests/richardmorris.pp
+++
@@ -1,7 +0,0 @@
-class users::richardmorris {
-  govuk::user { 'richardmorris':
-    fullname => 'Richard Morris',
-    email    => 'richard.morris@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDYiYpZD6wKLdmTK04ST6ZYGOfaPi3gbBl2inX3wkosCPLpg7bzcWQ+ysEgDEJWL+fsby5XX62Bq/97JhlIBdjLdsa17iF4U1cm1w0tRZPhCQsgyAHiqdhzDcIzSAiUqYjL7n2XafSYhhrk55JHA47wLfqZmX54dr/HyltyhoeJ/vyEP+kpZf1reCi+0U/6HoYzQ3WUjV4NA4xjc4qPZzMWISt4LZ0cEchH10lMVwXTGZFasxYBsORxhsesmLXbNZ3KW/pAD/I6ZRoicEbv5kV6Q3zJTwsdn+NOnVEUYhFHv3A0SOrVDbbWRKar/FzQWUHTzaR45YhvzFjyo5zbMgyt richard.morris@digital.cabinet-office.gov.uk',
-  }
-}
