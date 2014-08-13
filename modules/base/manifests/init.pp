94363096dc235653ce6cfd4e1d79450fb1c87bf5
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-08-13 07:47:55 +0100

    [#76786302] Reset version of rubygems for system ruby
    This properly resets the version of Ruby to how it should be. That is:
    
    - Gems in /var/lib/gems
    - No special rubygems.rb in /usr/local/lib/site_ruby
    - /usr/bin/gem1.9.1 as provided by the ruby1.9.1 package
    
    The reason for doing this is that when removing our force-upgrade of
    rubygems to 1.8.24, we failed to clean up. Nodes created since ~15th
    July have rubygems 1.8.11 (as they should), but all the nodes created
    prior to this have rubygems 1.8.24 still.

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -23,4 +23,27 @@ class base {
   include tmux
   include wget
   include curl
+
+  # FIXME: Fix to reset rubygems to Precise version
+  # Described in https://www.pivotaltracker.com/story/show/76786302
+  exec { 'reinstall correct ruby1.9.1 package':
+    user    => 'root',
+    command => 'apt-get --reinstall install ruby1.9.1=1.9.3.0-1ubuntu2.8',
+    unless  => 'echo "3afb14dbac5fb97eb505fa4f74469c19  /usr/bin/gem1.9.1" | /usr/bin/md5sum -c --status',
+    require => Exec['apt_update'],
+  }
+  exec { 'migrate /usr/lib/ruby/gems to /var/lib/gems':
+    user    => 'root',
+    command => 'mv /usr/lib/ruby/gems /var/lib/',
+    unless  => 'test -d /var/lib/gems',
+    require => Exec['reinstall correct ruby1.9.1 package'],
+  }
+  exec { 'remove weird rubygems in /usr/local/lib':
+    user    => 'root',
+    command => 'mv /usr/local/lib/site_ruby/1.9.1 /root/',
+    onlyif  => 'test -f /usr/local/lib/site_ruby/1.9.1/rubygems.rb',
+    require => Exec['migrate /usr/lib/ruby/gems to /var/lib/gems'],
+  }
+  # End Rubygems fix
+
 }
