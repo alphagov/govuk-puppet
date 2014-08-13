14053d0bea8b34a362b09e936765cf5fb5427303
Jake Benilov <benilov@gmail.com>
2014-08-13 09:40:38 +0100

    Remove 'support-api' enable/disable feature flag
    The 'support-api' is running happily in production, so the feature flag
    to enable/disable it is no longer needed.

--- modules/govuk/manifests/apps/support_api.pp
+++ modules/govuk/manifests/apps/support_api.pp
@@ -1,14 +1,9 @@
-class govuk::apps::support_api(
-  $port = 3075,
-  $enabled = false
-) {
-  if $enabled {
-    govuk::app { 'support-api':
-      app_type           => 'rack',
-      port               => $port,
-      vhost_ssl_only     => true,
-      health_check_path  => '/healthcheck',
-      log_format_is_json => true,
-    }
+class govuk::apps::support_api($port = 3075) {
+  govuk::app { 'support-api':
+    app_type           => 'rack',
+    port               => $port,
+    vhost_ssl_only     => true,
+    health_check_path  => '/healthcheck',
+    log_format_is_json => true,
   }
 }
