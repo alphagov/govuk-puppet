1dd93da86327c9a2f0e359dcad1217b187c0f5dc
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2014-08-14 16:14:08 +0100

    Start collecting stats for uploadlicence.*.alphagov.co.uk
    We weren't actually collecting statsd stats for this, even though we
    were trying to alert on them.  Actually create the stats.

--- modules/nginx/manifests/config/vhost/licensify_upload.pp
+++ modules/nginx/manifests/config/vhost/licensify_upload.pp
@@ -3,20 +3,27 @@ define nginx::config::vhost::licensify_upload($port='9000') {
   $vhost_name = "uploadlicence.${app_domain}"
   $vhost_escaped = regsubst($vhost_name, '\.', '_', 'G')
 
+  $counter_basename = "${::fqdn_underscore}.nginx_logs.${vhost_escaped}"
+
   nginx::config::ssl { $vhost_name: certtype => 'wildcard_alphagov' }
   nginx::config::site { $vhost_name: content => template('nginx/licensify-upload-vhost.conf') }
   nginx::log {
     "${vhost_name}-json.event.access.log":
-      json      => true,
-      logstream => present;
+      json          => true,
+      logstream     => present,
+      statsd_metric => "${counter_basename}.http_%{@fields.status}",
+      statsd_timers => [{metric => "${counter_basename}.time_request",
+                          value => '@fields.request_time'}];
     "${vhost_name}-access.log":
       logstream => absent;
     "${vhost_name}-error.log":
       logstream => present;
   }
 
+  statsd::counter { "${counter_basename}.http_500": }
+
   @@icinga::check::graphite { "check_nginx_5xx_${vhost_name}_on_${::hostname}":
-    target    => "transformNull(stats.${::fqdn_underscore}.nginx_logs.${vhost_escaped}.http_5xx,0)",
+    target    => "transformNull(stats.${counter_basename}.http_5xx,0)",
     warning   => 0.05,
     critical  => 0.1,
     from      => '3minutes',
