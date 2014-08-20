33df7abcaef0274935ceb0eaad6eb2e1d2e0b1bf
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-08-20 11:47:08 +0100

    Remove workaround for upstart service provider in puppet 2.7
    We added a workaround for an issue when running Puppet 2.7 and Ruby
    1.9.3 which broke the Upstart service provider.
    
    We don't use Puppet 2.7, we use Puppet 3.4 and that has been fixed in
    this commit:
       https://github.com/puppetlabs/puppet/commit/55505db1dff0972b8fdbb611ca59c5fd81a1ce4b
    
    This removes the workaround and enables the lint checking for
    inheritance across namespaces.
    
    I have tested that Puppet still works by bringing up a jumpbox with
    vagrant-govuk and it didn't barf, so I think this is good.

--- modules/govuk/manifests/firewall.pp
+++ modules/govuk/manifests/firewall.pp
@@ -1,4 +1,4 @@
-class govuk::firewall inherits ufw {
+class govuk::firewall {
 
   include ufw
 
@@ -6,17 +6,5 @@ class govuk::firewall inherits ufw {
   Ufw::Allow <| |>
   Ufw::Deny <| |>
   Ufw::Limit <| |>
-
-  # Temporary fix for http://projects.puppetlabs.com/issues/15534
-  #
-  #     err: /Stage[main]//Service[ufw]: Could not evaluate: undefined
-  #     method `each' for #<String:0x00000002e68d20>
-  #
-  # NB: when you remove this hack, please also remove the inheritance on the
-  # ufw module above, and remove the line in lib/tasks/lint.rake which
-  # disables the puppet-lint inherits_across_namespaces check.
-  Service['ufw'] {
-    enable => undef,
-  }
 
 }
