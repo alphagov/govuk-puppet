9f510cb223388c350272bcfd8f199ce9d5db5170
Matt Bostock <matt@mattbostock.com>
2014-08-22 10:24:30 +0100

    Remove unused files and directories
    Remove files and directories that were used by the `mirror` module,
    which was recently removed.
    
    The `govuk-netstorage` user created by that module has already been
    purged along with the cron jobs that belonged to it.

--- modules/govuk/manifests/node/s_mirrorer.pp
+++ modules/govuk/manifests/node/s_mirrorer.pp
@@ -2,4 +2,45 @@ class govuk::node::s_mirrorer inherits govuk::node::s_base {
   include govuk::apps::govuk_crawler_worker
   include govuk_crawler
   include nginx
+
+  # FIXME: Remove once absent
+  file { '/usr/local/bin/govuk_update_and_upload_mirror':
+    ensure => absent,
+    purge  => true,
+  }
+
+  # FIXME: Remove once absent
+  file { '/usr/local/bin/govuk_update_mirror':
+    ensure => absent,
+    purge  => true,
+  }
+
+  # FIXME: Remove once absent
+  file { '/usr/local/bin/govuk_upload_mirror':
+    ensure => absent,
+    purge  => true,
+  }
+
+  # FIXME: Remove once absent
+  file { '/var/run/govuk_update_and_upload_mirror.lock':
+    ensure => absent,
+    purge  => true,
+  }
+
+  # FIXME: Remove once absent
+  file { '/var/lib/govuk_mirror':
+    ensure  => absent,
+    force   => true,
+    purge   => true,
+    recurse => true,
+  }
+
+  # FIXME: Remove once absent
+  file { '/home/govuk-netstorage':
+    ensure  => absent,
+    force   => true,
+    purge   => true,
+    recurse => true,
+  }
+
 }
