e20625d2faafb3d611adeb8973f8ffed1d7d40b9
Dan Carley <dan.carley@gmail.com>
2014-08-22 15:49:21 +0100

    Create postgres replication user
    Creates a user on the postgres masters, which:
    
    - is authenticated by password which will come from gds/deployment
    - has REPLICATION privileges
    - is allowed to connect from the same subnet as the master

---
+++ modules/govuk_postgresql/manifests/server/master.pp
@@ -0,0 +1,32 @@
+# == Class: govuk_postgresql::server::master
+#
+# PostgreSQL binary replication master.
+#
+# === Parameters:
+#
+# [*slave_password*]
+#   Password the slave can authenticate with.
+#
+# [*slave_address*]
+#   Address (CIDR) the slave can connect from. Ideally this should be a /32
+#   to make access as restrictive as possible.
+#
+class govuk_postgresql::server::master (
+  $slave_password,
+  $slave_address,
+) {
+  $username = 'replication'
+
+  postgresql::server::role { $username:
+    replication   => true,
+    password_hash => postgresql_password($username, $slave_password),
+  }
+
+  postgresql::server::pg_hba_rule { $username:
+    type        => 'host',
+    database    => $username,
+    user        => $username,
+    address     => $slave_address,
+    auth_method => 'md5',
+  }
+}
