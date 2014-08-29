602cafb3de1657986e33a3e6252618bd8da9ea12
Dan Carley <dan.carley@gmail.com>
2014-08-29 12:27:37 +0100

    Record PostgreSQL XLOG position with collectd
    For all nodes - standalone, master, and slave. See inline comment for some
    details about the query. I'm going to use this for an alert with
    `icinga::check::graphite` in a following commit - where I'll explain why
    I've taken this approach.
    
    Using collectd's `bytes` type mostly so that we have a better idea of why
    unit this is. It's a gauge that is bounded at zero to infinity.
    
        vagrant@transition-postgresql-slave-1:~$ awk '$1 == "bytes"' /usr/share/collectd/types.db
        bytes                   value:GAUGE:0:U

--- modules/collectd/manifests/plugin/postgresql.pp
+++ modules/collectd/manifests/plugin/postgresql.pp
@@ -1,15 +1,19 @@
-# == Define: collectd::plugin::postgresql
+# == Class: collectd::plugin::postgresql
 #
-# Monitor a postgresql server.
+# Monitor a PostgreSQL server.
 #
 # This is a class to load the collectd plugin - to monitor a specific database
 # use collectd::plugin::postgresql_db
+#
 class collectd::plugin::postgresql {
-  @collectd::plugin { 'postgresql':
-    prefix  => '00-',
-  }
   $user = 'collectd'
   $password = 'collectd'
+
+  @collectd::plugin { 'postgresql':
+    prefix  => '00-',
+    content => template('collectd/etc/collectd/conf.d/postgresql.conf.erb'),
+  }
+
   @postgresql::server::role { $user:
     password_hash => postgresql_password($user, $password),
     tag           => 'govuk_postgresql::server::not_slave',
