4befc48b5e7e98beb615921eebabb8416af45bf4
Dan Carley <dan.carley@gmail.com>
2014-09-01 09:24:54 +0100

    Rename our internal sysctl module
    So that it doesn't conflict with thias/sysctl which was brought in at
    faa2b70. This will prevent us from using that third-party module (which we
    don't at the moment because we don't enable the flag in the redis module
    which makes use of it) and produces this warning in the dev VM:
    
        ==> default: INFO: modules/sysctl conflicts with vendor/modules/sysctl

--- modules/govuk_sysctl/manifests/init.pp
+++ modules/govuk_sysctl/manifests/init.pp
@@ -1,5 +1,5 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class sysctl {
+class govuk_sysctl {
   file { '/etc/sysctl.d/10-disable-timestamps.conf':
     source => 'puppet:///modules/sysctl/10-disable-timestamps.conf',
     owner  => root,
