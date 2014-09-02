3db05fd46d0d6a20389fb6d712c0c8528db61d58
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-09-02 12:24:12 +0100

    Fix really bad PR #2099

--- modules/govuk/manifests/apps/contentapi.pp
+++ modules/govuk/manifests/apps/contentapi.pp
@@ -1,17 +1,16 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::apps::contentapi (
-  $enabled = false,
   $port = 3022,
   $vhost_protected
 ) {
-  if ($enabled) {
-    govuk::app { 'contentapi':
-      app_type           => 'rack',
-      port               => $port,
-      vhost_protected    => $vhost_protected,
-      health_check_path  => '/vat-rates.json',
-      log_format_is_json => true,
-      nginx_extra_config => 'proxy_set_header API-PREFIX $http_api_prefix;',
-    }
+
+  govuk::app { 'contentapi':
+    app_type           => 'rack',
+    port               => $port,
+    vhost_protected    => $vhost_protected,
+    health_check_path  => '/vat-rates.json',
+    log_format_is_json => true,
+    nginx_extra_config => 'proxy_set_header API-PREFIX $http_api_prefix;',
   }
+
 }
