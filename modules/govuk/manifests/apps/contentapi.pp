696e15925104fbe4f943a56c7243d630735d3c86
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-09-02 11:57:25 +0100

    Feature flag contentapi on in development only
    This isn't deployed. It's been alerting in Production for weeks. No-one
    cared. I'm turning it off.

--- modules/govuk/manifests/apps/contentapi.pp
+++ modules/govuk/manifests/apps/contentapi.pp
@@ -1,14 +1,17 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::apps::contentapi (
+  $enabled = false,
   $port = 3022,
   $vhost_protected
 ) {
-  govuk::app { 'contentapi':
-    app_type           => 'rack',
-    port               => $port,
-    vhost_protected    => $vhost_protected,
-    health_check_path  => '/vat-rates.json',
-    log_format_is_json => true,
-    nginx_extra_config => 'proxy_set_header API-PREFIX $http_api_prefix;',
+  if ($enabled) {
+    govuk::app { 'contentapi':
+      app_type           => 'rack',
+      port               => $port,
+      vhost_protected    => $vhost_protected,
+      health_check_path  => '/vat-rates.json',
+      log_format_is_json => true,
+      nginx_extra_config => 'proxy_set_header API-PREFIX $http_api_prefix;',
+    }
   }
 }
