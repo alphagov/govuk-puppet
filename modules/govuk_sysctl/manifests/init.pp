c6c8d57271aa89eb8cbf4c3c10bdb77f80f88831
Dan Carley <dan.carley@gmail.com>
2014-09-04 17:12:38 +0100

    Remove sysctl net.ipv4.tcp_timestamps file
    Remove our custom file which we used to reinstate the actual default. Once
    this is deployed then we can really remove this class.

--- modules/govuk_sysctl/manifests/init.pp
+++ modules/govuk_sysctl/manifests/init.pp
@@ -3,12 +3,7 @@
 # FIXME: This entire class can be removed once deployed to production.
 #
 class govuk_sysctl {
-  sysctl {
-    'disable-timestamps':
-      ensure  => absent,
-      prefix  => 10;
-    'net.ipv4.tcp_timestamps':
-      value   => 1,
-      comment => ['Restore default Ubuntu setting of `1`'];
+  sysctl { 'net.ipv4.tcp_timestamps':
+    ensure  => absent,
   }
 }
