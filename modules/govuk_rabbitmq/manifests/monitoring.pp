c579ef6516f87240153e822b20af87d7bb5d906a
Matt Bostock <matt@mattbostock.com>
2014-09-04 14:09:30 +0100

    Add check_rabbitmq_network_partition NRPE check
    Add an Icinga NRPE check for RabbitMQ network partitions.
    
    Under normal operation, RabbitMQ will report no (nil) network
    partitions. Query the RabbitMQ admin HTTP API on the `/api/nodes`
    endpoint which will give us a JSON response.
    
    The check looks for for an empty `partitions` array in the JSON
    response.
    
    See also: https://www.rabbitmq.com/partitions.html

--- modules/govuk_rabbitmq/manifests/monitoring.pp
+++ modules/govuk_rabbitmq/manifests/monitoring.pp
@@ -1,8 +1,18 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk_rabbitmq::monitoring {
+  @icinga::nrpe_config { 'check_rabbitmq_network_partition':
+    content => template('govuk_rabbitmq/check_rabbitmq_network_partition.cfg.erb'),
+  }
+
   @@icinga::check { "check_rabbitmq_running_${::hostname}":
     check_command       => 'check_nrpe!check_proc_running!rabbitmq-server',
     service_description => 'rabbitmq-server not running',
+    host_name           => $::fqdn,
+  }
+
+  @@icinga::check { "check_rabbitmq_network_partitions_${::hostname}":
+    check_command       => 'check_nrpe!check_rabbitmq_network_partition',
+    service_description => 'RabbitMQ network partition has occurred',
     host_name           => $::fqdn,
   }
 }
