d722460f8f4d6b63828e8a89e195a3f219ab9c46
Dan Carley <dan.carley@gmail.com>
2014-09-08 10:54:53 +0100

    Make check_http_timeout_noncrit plugin virtual
    This resource gets applied to the development VM since refactoring in
    26d83f4. We don't install Icinga there, which causes this error:
    
        Error: Could not find dependency Class[Icinga::Client::Package] for File[/usr/lib/nagios/plugins/check_http_timeout_noncrit] at /var/govuk/puppet/modules/icinga/manifests/plugin.pp:32
    
    Fix this by making the `icinga::plugin` a virtual resource. This follows a
    convention we've used for other resources of this type. They get collected
    by `icinga::client`.

--- modules/icinga/manifests/plugin/check_http_timeout_noncrit.pp
+++ modules/icinga/manifests/plugin/check_http_timeout_noncrit.pp
@@ -1,6 +1,6 @@
 # Installs this Icinga plugin. Can be included multiple times.
 class icinga::plugin::check_http_timeout_noncrit {
-    icinga::plugin { 'check_http_timeout_noncrit':
+    @icinga::plugin { 'check_http_timeout_noncrit':
         source => 'puppet:///modules/monitoring/usr/lib/nagios/plugins/check_http_timeout_noncrit',
     }
 }
