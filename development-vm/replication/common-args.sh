74978c4a3d152839738d52ca344e313e43e5124e
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-09-18 11:36:50 +0100

    Add external-link-tracker to default ignore list.
    This database is getting very large.  It's also rarely used on the dev
    VM, so it doesn't make sense to replicate it by default.

--- development-vm/replication/common-args.sh
+++ development-vm/replication/common-args.sh
@@ -28,8 +28,8 @@ EOF
 DIR="backups/$(date +%Y-%m-%d)"
 SKIP_DOWNLOAD=false
 DRY_RUN=false
-# By default, ignore the trade tariff databases
-IGNORE="tariff tariff_temporal tariff_demo"
+# By default, ignore the trade tariff and external_link_tracker databases
+IGNORE="tariff tariff_temporal tariff_demo external_link_tracker"
 
 while getopts "hF:u:d:sri:n" OPTION
 do
