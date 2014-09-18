9479fba567458a5ff331b177d2acaa8ed9bc07d8
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-09-18 10:04:10 +0100

    Remove vhost_protected from Spotlight manifest
    There's no need to protect this vhost with basic auth, because
    `spotlight.preview.alphagov.co.uk` can't be accessed from anywhere.

--- modules/performance_platform/manifests/spotlight.pp
+++ modules/performance_platform/manifests/spotlight.pp
@@ -1,9 +1,6 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class performance_platform::spotlight (
   $perfplat_internal_app_domain = 'production.performance.service.gov.uk',
-  #FIXME #73421574: remove when we are off old preview and it is no longer possible
-  #       to access apps directly from the internet
-  $vhost_protected = false
 ) {
   $app_domain = hiera('app_domain')
 
@@ -22,7 +19,6 @@ class performance_platform::spotlight (
     certtype => 'wildcard_alphagov',
   }
   nginx::config::site { $vhost_full:
-    # This template is the consumer of vhost_protected
     content => template('performance_platform/spotlight-vhost.conf'),
   }
 }
