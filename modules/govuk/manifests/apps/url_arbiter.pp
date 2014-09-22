349e32e65b5bf2b1bd59186fdb78cb2d3ee1fbba
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-09-22 15:12:34 +0100

    Enable url-arbiter everywhere.
    This is now being deployed to staging and prod.

--- modules/govuk/manifests/apps/url_arbiter.pp
+++ modules/govuk/manifests/apps/url_arbiter.pp
@@ -1,18 +1,15 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::apps::url_arbiter(
   $port = 3034,
-  $enabled = false,
 ) {
-  if $enabled {
 
-    govuk::app { 'url-arbiter':
-      app_type           => 'rack',
-      port               => $port,
-      vhost_ssl_only     => true,
-      health_check_path  => '/healthcheck',
-      log_format_is_json => true,
-    }
-
-    include govuk_postgresql::client #installs libpq-dev package needed for pg gem
+  govuk::app { 'url-arbiter':
+    app_type           => 'rack',
+    port               => $port,
+    vhost_ssl_only     => true,
+    health_check_path  => '/healthcheck',
+    log_format_is_json => true,
   }
+
+  include govuk_postgresql::client #installs libpq-dev package needed for pg gem
 }
