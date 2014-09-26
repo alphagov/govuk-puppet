9801a24a70db5f09f21b770884ee44f47f614660
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-09-26 16:08:31 +0100

    Add email alert service (with RabbitMQ).

--- modules/govuk/manifests/node/s_backend.pp
+++ modules/govuk/manifests/node/s_backend.pp
@@ -42,6 +42,7 @@ class govuk::node::s_backend inherits govuk::node::s_base {
 
   include govuk::apps::content_planner
   include govuk::apps::email_alert_api
+  include govuk::apps::email_alert_service
   class { 'govuk::apps::external_link_tracker':
     mongodb_nodes => [
       'mongo-1.backend',
