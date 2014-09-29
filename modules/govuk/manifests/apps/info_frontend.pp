4aeee9c62d6013fd6a4f07caeb58cdd44e8d5512
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-09-29 17:32:28 +0100

    Revert "Merge pull request #2166 from gds/remove-enabled-flag-from-metadata-api-and-info-frontend"
    This reverts commit c187b836d898ed68ac67b5b2d9535c3747a058a7, reversing
    changes made to a74e16da55bbcb2737957edad44718d5841bd896.
    
    This shouldn't have been merged yet. It needs the respective puppet
    code to have been deployed first.

--- modules/govuk/manifests/apps/info_frontend.pp
+++ modules/govuk/manifests/apps/info_frontend.pp
@@ -6,14 +6,19 @@ class govuk::apps::info_frontend(
 
   # Should this vhost be protected with HTTP Basic auth?
   $vhost_protected = false,
+
+  # Whether the app should be present in a given environment.
+  $enabled = false,
 ) {
-  govuk::app { 'info-frontend':
-    app_type              => 'rack',
-    port                  => $port,
-    vhost_protected       => $vhost_protected,
-    vhost_aliases         => ['info-frontend'],
-    log_format_is_json    => true,
-    asset_pipeline        => true,
-    asset_pipeline_prefix => 'info-frontend',
+  if $enabled {
+    govuk::app { 'info-frontend':
+      app_type              => 'rack',
+      port                  => $port,
+      vhost_protected       => $vhost_protected,
+      vhost_aliases         => ['info-frontend'],
+      log_format_is_json    => true,
+      asset_pipeline        => true,
+      asset_pipeline_prefix => 'info-frontend',
+    }
   }
 }
