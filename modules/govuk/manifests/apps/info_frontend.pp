06186ebe738c6ced32836f55ca3790292987641c
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-09-29 17:26:20 +0100

    Remove the `enabled` flag from `info-frontend` and `metadata-api` apps
    Both of these applications are now deployed to production and we no
    longer need to feature flag where this application is expected to
    exist.

--- modules/govuk/manifests/apps/info_frontend.pp
+++ modules/govuk/manifests/apps/info_frontend.pp
@@ -6,19 +6,14 @@ class govuk::apps::info_frontend(
 
   # Should this vhost be protected with HTTP Basic auth?
   $vhost_protected = false,
-
-  # Whether the app should be present in a given environment.
-  $enabled = false,
 ) {
-  if $enabled {
-    govuk::app { 'info-frontend':
-      app_type              => 'rack',
-      port                  => $port,
-      vhost_protected       => $vhost_protected,
-      vhost_aliases         => ['info-frontend'],
-      log_format_is_json    => true,
-      asset_pipeline        => true,
-      asset_pipeline_prefix => 'info-frontend',
-    }
+  govuk::app { 'info-frontend':
+    app_type              => 'rack',
+    port                  => $port,
+    vhost_protected       => $vhost_protected,
+    vhost_aliases         => ['info-frontend'],
+    log_format_is_json    => true,
+    asset_pipeline        => true,
+    asset_pipeline_prefix => 'info-frontend',
   }
 }
