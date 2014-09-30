d59b7d5821b95eeed5c5003690ed9fb3b493408a
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-09-30 15:45:14 +0100

    Add new contractor Karl Entwistle to SSH

---
+++ modules/users/manifests/karlentwistle.pp
@@ -0,0 +1,7 @@
+class users::karlentwistle {
+  govuk::user { 'karlentwistle':
+    fullname => 'Karl Entwistle',
+    email    => 'karl.entwistle@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7i02O7ckwrwP18OIiZji4w7UTrpfxiFDNdQ6F/kAIZipaV3e2Ai6bYPVd8RUYIvEe3Qa04u+edYp2+gz5/zjLEy7t0xjz5AeB2Lp6re+jr3L3tTHI240ItBo4wK5Au702WbmDJLo3rZJfLo+1RCne/J5Z4eejkqWfMsWfF5Q67m1UDAQq0ubG0Boiu2xHBtxJstFdi7yOVngA8cojshAzYd4kCw6I6e+cVcGjeplKcSMNQAiBqoJAyTmHgX/6bUQK+SZyuO85lON0xgCmC/eLu57EwprhixWHr/OZFd2L2NCbhxGvlt0tbMxXF5mS51um9YMzbOb+Bf4UNMtcLEtP',
+  }
+}
