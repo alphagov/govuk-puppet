319232bbef9b20e89353315e039fe47aecfdf9e5
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-10-01 12:27:07 +0100

    Remove the `enabled` flag for info-frontend and metadata-api
    We want these applications to exist in all environments now.

--- modules/govuk/manifests/apps/info_frontend.pp
+++ modules/govuk/manifests/apps/info_frontend.pp
@@ -10,15 +10,13 @@ class govuk::apps::info_frontend(
   # Whether the app should be present in a given environment.
   $enabled = false,
 ) {
-  if $enabled {
-    govuk::app { 'info-frontend':
-      app_type              => 'rack',
-      port                  => $port,
-      vhost_protected       => $vhost_protected,
-      vhost_aliases         => ['info-frontend'],
-      log_format_is_json    => true,
-      asset_pipeline        => true,
-      asset_pipeline_prefix => 'info-frontend',
-    }
+  govuk::app { 'info-frontend':
+    app_type              => 'rack',
+    port                  => $port,
+    vhost_protected       => $vhost_protected,
+    vhost_aliases         => ['info-frontend'],
+    log_format_is_json    => true,
+    asset_pipeline        => true,
+    asset_pipeline_prefix => 'info-frontend',
   }
 }
