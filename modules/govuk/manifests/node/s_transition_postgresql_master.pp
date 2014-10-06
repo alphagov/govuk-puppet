fc9f183aed7ac43b4ee1837f5822417031b6bcc3
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2014-10-06 14:23:10 +0100

    Modify PostgreSQL settings on transition-postgresql-master-1
    - effective_cache_size: an estimate of how much space is available
    for the operating system's disk cache - see http://www.postgresql.org/docs/9.1/static/runtime-config-query.html#GUC-EFFECTIVE-CACHE-SIZE
    - shared_buffers: the amount of memory to allocate to postgresql for
    caching data - see http://www.postgresql.org/docs/9.1/static/runtime-config-resource.html#GUC-SHARED-BUFFERS
    
    By default these settings are very low for compatibility with old systems
    (128MB for effective_cache_size, 24MB for shared_buffers) but they need to
    be increased to get reasonable performance on modern dedicated servers.
    The values set for both are what we've found to increase performance
    in our testing on Preview, and they're in line with general recommendations.
    
    On PostgreSQL 9.1, shared_buffers is dependent on kernel.shmmax, so for now
    we need to increase kernel.shmmax so that it is equal to or greater than
    the amount of memory which postgresql will request on startup, which is
    partly determined by the value of shared_buffers.
    
    When we are on 9.3 we should be able to remove the setting of kernel.shmmax:
    
        https://wiki.postgresql.org/wiki/What's_new_in_PostgreSQL_9.3#Switch_to_Posix_shared_memory_and_mmap.28.29
    
    We're only modifying these settings on the master for now because we haven't
    yet started testing Bouncer's performance when reading from the slave.

--- modules/govuk/manifests/node/s_transition_postgresql_master.pp
+++ modules/govuk/manifests/node/s_transition_postgresql_master.pp
@@ -14,4 +14,33 @@ class govuk::node::s_transition_postgresql_master (
   class { 'govuk_postgresql::server::master':
     slave_password => $slave_password,
   }
+
+  $effective_cache_size_mb = floor($::memtotalmb * 0.5)
+  $shared_buffers_mb       = floor($::memtotalmb * 0.25)
+
+  # The calculation given in the following table for the approximate actual
+  # requested size for shared_buffers (to use for kernel.shmmax) doesn't give
+  # a number big enough for postgresql to be able to restart on a vagrant-govuk
+  # box with the default 363MB RAM:
+  #
+  #     http://www.postgresql.org/docs/9.1/static/kernel-resources.html#SHARED-MEMORY-PARAMETERS
+  #
+  # Instead, set kernel.shmmax to 120% of shared_buffers, which seems to be
+  # big enough (110% wasn't enough when testing with Vagrant):
+  $magical_mystery_multiplier = 1.2
+  $kernel_shmmax_bytes        = floor($shared_buffers_mb * 1024 * 1024
+                                      * $magical_mystery_multiplier)
+
+  postgresql::server::config_entry { 'effective_cache_size':
+    value => "${$effective_cache_size_mb}MB",
+  }
+  postgresql::server::config_entry { 'shared_buffers':
+    value   => "${$shared_buffers_mb}MB",
+    require => Sysctl['kernel.shmmax']
+  }
+
+  sysctl { 'kernel.shmmax':
+    value  => $kernel_shmmax_bytes,
+    notify => Class['postgresql::server::service']
+  }
 }
