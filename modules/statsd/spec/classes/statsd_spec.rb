f2f1e0fc5811f70a82c2b9eafb158f9420b03a2b
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-10-07 15:29:15 +0100

    Update statsd module to use newer package.
    Statsd comes from out PPA, and is not present in Ubuntu.  Setting it to
    ensure => latest is therefore safe and ensures the updated version in
    the PPA will be picked up.
    
    This also removes  the puppet-managed upstart script as this is now
    provided by the package.  The nodejs dependency is also provided by the
    package, so it's unnecessary to duplicate it in the module.

--- modules/statsd/spec/classes/statsd_spec.rb
+++ modules/statsd/spec/classes/statsd_spec.rb
@@ -6,9 +6,7 @@ describe 'statsd', :type => :class do
       :graphite_hostname => 'graphite.example.com',
     }}
 
-    it { should contain_class('nodejs') }
     it { should contain_package('statsd') }
-    it { should contain_file('/etc/init/statsd.conf') }
     it { should contain_service('statsd') }
 
     it { should contain_file('/etc/statsd.conf').with_content(/ graphiteHost: "graphite.example.com"$/) }
