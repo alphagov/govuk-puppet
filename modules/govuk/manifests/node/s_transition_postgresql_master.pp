b94eb55e182a68d2944ae4b3ab6024e7904c0441
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2014-10-07 18:59:43 +0100

    Move postgresql config settings away from kernel settings
    The kernel settings are only needed while we are still on PostgreSQL 9.1,
    but the config settings will be needed after that as well.

--- modules/govuk/manifests/node/s_transition_postgresql_master.pp
+++ modules/govuk/manifests/node/s_transition_postgresql_master.pp
@@ -18,6 +18,14 @@ class govuk::node::s_transition_postgresql_master (
   $effective_cache_size_mb = floor($::memtotalmb * 0.5)
   $shared_buffers_mb       = floor($::memtotalmb * 0.25)
 
+  postgresql::server::config_entry { 'effective_cache_size':
+    value => "${$effective_cache_size_mb}MB",
+  }
+  postgresql::server::config_entry { 'shared_buffers':
+    value   => "${$shared_buffers_mb}MB",
+    require => Sysctl['kernel.shmmax', 'kernel.shmall'],
+  }
+
   # The calculation given in the following table for the approximate actual
   # requested size for shared_buffers (to use for kernel.shmmax) doesn't give
   # a number big enough for postgresql to be able to restart on a vagrant-govuk
@@ -34,14 +42,6 @@ class govuk::node::s_transition_postgresql_master (
   # reported by `getconf PAGESIZE` in Preview is 4096. The `ceil` function
   # doesn't exist in Puppet, so use floor + 1 instead.
   $kernel_shmall_pages        = floor($kernel_shmmax_bytes / 4096 ) + 1
-
-  postgresql::server::config_entry { 'effective_cache_size':
-    value => "${$effective_cache_size_mb}MB",
-  }
-  postgresql::server::config_entry { 'shared_buffers':
-    value   => "${$shared_buffers_mb}MB",
-    require => Sysctl['kernel.shmmax', 'kernel.shmall'],
-  }
 
   sysctl { 'kernel.shmmax':
     value  => $kernel_shmmax_bytes,
