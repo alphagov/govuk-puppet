c62115d364b7e9a3cedd39f6ed47ac54a6674d79
Matt Bostock <matt@mattbostock.com>
2014-10-10 11:36:24 +0100

    Upgrade to Puppet version 3.6
    Upgrade to Puppet version 3.6, from version 3.4. Doing this now because
    version 3.6 is the minimum version of Puppet supported by Puppetlabs'
    Apt repository for Ubuntu Trusty.
    
    Note that we were using packages from the `stable` distribution provided
    by their apt repo, which is likely intended for Debian.
    
    When I tried installing Puppet 3.6 from the `stable` distribution, it
    failed due to missing dependencies. To avoid this and possible
    complications in future from using Debian packages, this commit changes
    to use the Ubuntu LTS distributions (`trusty` or `precise`).
    
    This also makes our Puppet code consistent with our [preamble] script,
    which already uses the Ubuntu distributions.
    
    Note that we encountered an issue in Aptly the last time we tried to
    mirror more than one distribution from Puppetlabs' apt repository
    (see d2ab45d), but I was unable to reproduce that error. On further
    investigation, it appears to have been been [fixed] by Aptly. We are
    currently using version 0.6.0 of Aptly, which includes that fix.
    
    [fixed]: https://github.com/smira/aptly/issues/60
    [preamble]: https://github.gds/gds/govuk-provisioning/blob/af40f2607fbe5d97556baedeb10ec5b61c276559/vcloud-launcher/preamble/preamble.sh.erb#L28
    
    Also update the Hiera version to the one installed by the Puppetlabs`
    `puppet` package.
    
    Choose version 3.6 rather than the latest, 3.7, because:
    
      - Puppet 3.6 is the minimum version supported by the Puppetlabs' Apt
        repository for Ubuntu Trusty so is the smallest possible change we
        can make to support Ubuntu Trusty (if we are to continue using
        Puppetlabs' repo, which I think is preferable)
    
      - Version 3.7 contains a number of deprecations in the Puppet DSL,
        ready for the release of version 4.0 which contains the 'future'
        parser; we should handle these deprecations separately
    
    Puppet 3.6 will give us the following benefits:
    
      - Allow us to enable `strict_variables` which, though not enabled
        here, cause Puppet to fail if a variable used in a manifest is empty
        (which I think is desirable to prevent unexpected behaviour)
    
      - Support for purging unmanaged `ssh_authorized_key` resources
    
      - Helps ready us for version 4.0
    
    I have tested this as follows:
    
      - Under vagrant on the development VM running Ubuntu Precise; four
        deprecation warnings but no errors:
    
            ==> default: Warning: Setting manifestdir is deprecated. See http://links.puppetlabs.com/env-settings-deprecations
            ==> default: Warning: Setting modulepath is deprecated in puppet.conf. See http://links.puppetlabs.com/env-settings-deprecations
            ==> default: Warning: Setting config_version is deprecated in puppet.conf. See http://links.puppetlabs.com/env-settings-deprecations
            ==> default: Warning: The use of 'import' is deprecated at /var/govuk/puppet/manifests/site.pp:39. See http://links.puppetlabs.com/puppet-import-deprecation
    
        We can fix these deprecation errors later by switching to 'directory
        environments', which are the default as of Puppet 3.5.1 and are
        always enabled in Puppet 4.0.
    
      - Under vagrant on a cache-1.router VM running Ubuntu Precise; the
        following error appears on the first Puppet run under version 3.6:
    
            Error: Report processor failed: undefined method `exist?' for Puppet::FileSystem:Module
    
        The error goes away on the second run; it appears to occur because
        Puppet has been upgraded on disk but version 3.4 (running in memory)
        expects that method to exist.
    
    I'm not expecting this change to deploy without issue but we can address
    any lingering issues in Preview before proceeding to Staging and
    Production.
    
    For more information, see the release notes:
    
      - https://docs.puppetlabs.com/puppet/3.5/reference/release_notes.html
      - https://docs.puppetlabs.com/puppet/3.6/reference/release_notes.html

--- modules/puppet/manifests/package.pp
+++ modules/puppet/manifests/package.pp
@@ -4,8 +4,8 @@
 #
 class puppet::package (
   $facter_version = '1.7.5-1puppetlabs1',
-  $hiera_version = '1.3.2-1puppetlabs1',
-  $puppet_version = '3.4.3-1puppetlabs1',
+  $hiera_version = '1.3.4-1puppetlabs1',
+  $puppet_version = '3.6.2-1puppetlabs1',
 ) {
   # Pin the desired Puppet version so that Puppet doesn't update
   # without us having tested the new version first. If Puppet breaks,
