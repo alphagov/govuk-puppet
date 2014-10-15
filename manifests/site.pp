226b966a9c2132ff19f6dd77e57368b819a2c05b
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-10-15 12:43:10 +0100

    Fix puppet-apply to work with puppet 3.4
    `puppet apply` doesn't support being given a directory instead of a file
    before puppet 3.5[1].  We need to support puppet 3.4 so that puppet can
    upgrade itself everywhere.
    
    This therefore reverts to specifying a manifest file in pupppet-apply,
    and inlines the contents of `nodes.pp` into `site.pp`.
    
    [1] https://docs.puppetlabs.com/puppet/3.5/reference/release_notes.html#auto-import-use-a-directory-as-main-manifest

--- manifests/site.pp
+++ manifests/site.pp
@@ -34,3 +34,26 @@ if chomp(hiera('HIERA_EYAML_GPG_CHECK')) != "It's all OK penguins" {
   fail("Hiera eYAML GPG encryption backend is not working; you should read: \
 https://github.gds/pages/gds/opsmanual/infrastructure/howto/encrypted-hiera-data.html?#puppet-fails-because-my-it-can-t-find-a-usable-gpg-key")
 }
+
+# Top-level variable also used by `hiera.yml`.
+# Cannot be spoofed by Facter from `puppet agent`.
+$govuk_node_class = govuk_node_class()
+
+node default {
+  if $::lsbdistcodename == 'lucid' {
+    fail('Ubuntu Lucid is no longer supported')
+  }
+
+  govuk_check_hostname_facts()
+
+  # This will fail with an error if the node class doesn't exist.
+  class { "govuk::node::s_${::govuk_node_class}": }
+
+
+  # Create logical volumes from hiera
+  $lv = hiera('lv',{})
+  create_resources('govuk::lvm', $lv)
+  # Create mounts from hiera
+  $mount = hiera('mount',{})
+  create_resources('govuk::mount', $mount)
+}
