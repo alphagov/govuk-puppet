c511cff7fbdace320c74c9d11a43399c2137c19a
Edd Sowden <e@e26.co.uk>
2014-10-21 09:34:23 +0000

    Remove leaver: stephenbest

--- modules/users/manifests/groups/contractors.pp
+++ modules/users/manifests/groups/contractors.pp
@@ -17,7 +17,6 @@ class users::groups::contractors {
   include users::paulhayes
   include users::pedromoreira
   include users::russellgarner
-  include users::stephenbest
   include users::stevelaing
   include users::tekin
   include users::tomrussell
