baa4e30102be812ee43557205dcccfe2d783fcdc
Edd Sowden <e@e26.co.uk>
2014-10-21 09:32:43 +0000

    Remove leaver: brandanbutler

--- modules/users/manifests/groups/contractors.pp
+++ modules/users/manifests/groups/contractors.pp
@@ -5,7 +5,6 @@
 #
 class users::groups::contractors {
   include users::andrewmitchell
-  include users::brendanbutler
   include users::camillebaldock
   include users::dominicbaggott
   include users::elliot
