ad9a3007d0eab9781e09e17cd1fa5ac613169bfd
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2014-10-29 12:39:28 +0000

    Automatically run email-alert-api Sidekiq worker

--- modules/govuk/manifests/apps/email_alert_api.pp
+++ modules/govuk/manifests/apps/email_alert_api.pp
@@ -12,10 +12,18 @@
 #   Should the application should be enabled. Set in hiera data for each
 #   environment.
 #
+# [*port*]
+#   What port should the app run on?
+#
+# [*enable_procfile_worker*]
+#   Should the Foreman-based background worker be enabled by default. Set in
+#   hiera.
+#
 class govuk::apps::email_alert_api(
   $port = 3088,
   $vhost_protected = false,
   $enabled = false,
+  $enable_procfile_worker = true
 ) {
 
   if $enabled {
@@ -24,6 +32,10 @@ class govuk::apps::email_alert_api(
       port               => $port,
       vhost_protected    => $vhost_protected,
       log_format_is_json => true,
+    }
+
+    govuk::procfile::worker {'email-alert-api':
+      enable_service => $enable_procfile_worker,
     }
   }
 }
