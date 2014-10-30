2b3251365365736111427a076fb1b8ba2a5d165f
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-10-30 09:35:20 +0000

    Add content-register to dev and backend
    www.agileplannerapp.com/boards/173808/cards/7897
    
    This is a follow-up to:
    
    https://github.gds/gds/puppet/pull/2285

--- modules/govuk/manifests/node/s_postgresql_base.pp
+++ modules/govuk/manifests/node/s_postgresql_base.pp
@@ -3,6 +3,7 @@
 # Base node for s_postgresql_{master,slave}
 #
 class govuk::node::s_postgresql_base inherits govuk::node::s_base {
+  include govuk::apps::content_register::db
   include govuk::apps::email_alert_api::db
   include govuk::apps::url_arbiter::db
 
