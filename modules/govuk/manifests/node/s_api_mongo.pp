74e7f0ada60aca49fd058b1daf49f405f5b4e96e
Anna Shipman <anna@annashipman.co.uk>
2014-11-04 10:58:36 +0000

    Update api-mongo machines to use hiera
    Rationale explained in 1a22b35b.
    
    This commit did not require a removal of legacy aliases.

--- modules/govuk/manifests/node/s_api_mongo.pp
+++ modules/govuk/manifests/node/s_api_mongo.pp
@@ -3,14 +3,6 @@ class govuk::node::s_api_mongo inherits govuk::node::s_base {
   include mongodb::server
   include mongodb::backup
 
-  class { 'mongodb::configure_replica_set':
-    members => [
-      'api-mongo-1.api',
-      'api-mongo-2.api',
-      'api-mongo-3.api',
-    ]
-  }
-
   collectd::plugin::tcpconn { 'mongo':
     incoming => 27017,
     outgoing => 27017,
