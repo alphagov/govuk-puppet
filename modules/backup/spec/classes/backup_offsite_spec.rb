d3d5195d2e6a03d3b9f6ac12ff2e14b8a84b3062
Dan Carley <dan.carley@gmail.com>
2014-11-04 17:06:34 +0000

    [#80824874] Replace offsite-backup with duplicity
    Use duplicity to do offsite backups instead of our custom script. This
    performs GPG encryption and remote sync as before, but with the added
    benefits that:
    
    - we can re-use the same simple Puppet resource instead of duplicating the
      original shell script (this has copied `backup::assets`)
    - it's less in-house code for us to maintain
    - we save bandwidth and storage by using differential updates
    - rotation of old backups is handled by the sender and not out-of-band by
      the receiver
    
    WRT the last point, we can remove `base::rotate` from the
    alphagov/govuk_offsitebackup-puppet once we've been running duplicity for
    30days. All of the old style backups will have been removed and we'll have
    the complete retention period for the new style.
    
    We have lost the additional syslog logging and statsd timers because they
    can't be implemented without making more changes to the duplicity module. We
    should be able to get some of this information from duplicity's own logs.
    
    The `enable` param is still supported but we don't setup the cronjob or the
    alert if it is false (default). This too would have required significant
    changes to the duplicity module to re-implement.
    
    I've surfaced the GPG key (which can now be shared with `backup::assets`)
    and the list of directories up to hiera, so that they are no longer hidden
    within the logic. The directory list now just specifies the parent directory
    for `archived` and `automongodbbackup` instead of a glob because those dirs
    don't contain anything that we don't want.

--- modules/backup/spec/classes/backup_offsite_spec.rb
+++ modules/backup/spec/classes/backup_offsite_spec.rb
@@ -1,55 +1,85 @@
 require_relative '../../../../spec_helper'
 
 describe 'backup::offsite', :type => :class do
-  context 'hostname => ice.cream, hostkey => pickle' do
-    let(:params) {{
+  let(:default_params) {{
+    :src_dirs      => ['/unused'],
+    :gpg_key_id    => 'unused',
+    :dest_host     => 'unused',
+    :dest_host_key => 'unused',
+  }}
+
+  describe 'enable' do
+    context 'false (default)' do
+      let(:params) { default_params }
+
+      it { should contain_duplicity('offsite-govuk-datastores').with(
+        :ensure => 'absent',
+      )}
+    end
+
+    context 'true' do
+      let(:params) { default_params.merge({
+        :enable        => true,
+      })}
+
+      it { should contain_duplicity('offsite-govuk-datastores').with(
+        :ensure => 'present',
+      )}
+    end
+  end
+
+  describe 'dest_folder' do
+    context 'relative path' do
+      let(:params) { default_params.merge({
+        :dest_folder   => 'some/path',
+        :dest_host     => 'backup.example.com',
+      })}
+
+      it 'should include a single slash between host and path' do
+        should contain_duplicity('offsite-govuk-datastores').with(
+          :target => 'rsync://backup.example.com/some/path',
+        )
+      end
+    end
+
+    context 'absolute path' do
+      let(:params) { default_params.merge({
+        :dest_folder   => '/srv/some/path',
+        :dest_host     => 'backup.example.com',
+      })}
+
+      it 'should include an "extra" slash between host and path' do
+        should contain_duplicity('offsite-govuk-datastores').with(
+          :target => 'rsync://backup.example.com//srv/some/path',
+        )
+      end
+    end
+  end
+
+  describe 'dest_host_key' do
+    let(:params) { default_params.merge({
       :dest_host     => 'ice.cream',
       :dest_host_key => 'pickle',
-    }}
+    })}
+
+    it {
+      should contain_sshkey('ice.cream').with_key('pickle')
+    }
+  end
+
+  describe 'misc' do
+    let(:params) { default_params }
+
+    it 'should include service description in NRPE command' do
+      should contain_duplicity('offsite-govuk-datastores').with(
+        :post_command => /\\toffsite backup govuk datastores\\t/,
+      )
+    end
 
     it {
       # Leaky abstraction? We need to know that govuk::user creates the
       # parent directory for our file.
       should contain_file('/home/govuk-backup/.ssh').with_ensure('directory')
-    }
-
-    it {
-      should contain_sshkey('ice.cream').with_key('pickle')
-    }
-
-    it {
-      should contain_file('/usr/local/bin/offsite-backup').with_content(/^scp \$SSH_OPTS \$TEMP_FILE \${DEST}:\${FILENAME}$/)
-    }
-
-    it {
-      should contain_file('/usr/local/bin/offsite-backup').with_content(/^DEST="ice.cream"$/)
-    }
-
-    it {
-      should contain_file('/usr/local/bin/offsite-backup').with_content(/^exit 0$/)
-    }
-  end
-  context 'hostname => ice.cream, hostkey => pickle' do
-    let(:params) {{
-      :dest_folder   => 'cheesecake/',
-      :dest_host     => 'ice.cream',
-      :dest_host_key => 'pickle',
-    }}
-
-    it {
-      should contain_file('/usr/local/bin/offsite-backup').with_content(/^scp \$SSH_OPTS \$TEMP_FILE \${DEST}:cheesecake\/\${FILENAME}$/)
-    }
-  end
-  context 'enable => true' do
-    let(:params) {{
-      :dest_folder   => 'lemon/tart',
-      :dest_host     => 'apple.turnover',
-      :dest_host_key => 'cinnamonbun',
-      :enable        => true,
-    }}
-
-    it {
-      should contain_file('/usr/local/bin/offsite-backup').without_content(/^exit 0$/)
     }
   end
 end
