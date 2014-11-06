7da92507514d33729b287dfc08067ffd5dacbb89
Anna Shipman <anna@annashipman.co.uk>
2014-11-06 08:52:36 +0000

    Update router machines to use hiera
    Rationale explained in 1a22b35b.
    
    This commit did not require a removal of legacy aliases.

--- modules/govuk/manifests/node/s_router_backend.pp
+++ modules/govuk/manifests/node/s_router_backend.pp
@@ -3,14 +3,6 @@ class govuk::node::s_router_backend inherits govuk::node::s_base {
   include mongodb::server
   include mongodb::backup
 
-  class { 'mongodb::configure_replica_set':
-    members => [
-      'router-backend-1.router',
-      'router-backend-2.router',
-      'router-backend-3.router',
-    ],
-  }
-
   include govuk::node::s_ruby_app_server
 
   include govuk::apps::router_api
