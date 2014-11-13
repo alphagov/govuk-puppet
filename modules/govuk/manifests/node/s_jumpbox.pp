ff42efd37a3a03d05b04821a6d4cc52ac865eee7
Matt Bostock <matt@mattbostock.com>
2014-11-13 11:42:11 +0000

    Remove FIXME for jumpbox directory

--- modules/govuk/manifests/node/s_jumpbox.pp
+++ modules/govuk/manifests/node/s_jumpbox.pp
@@ -1,12 +1,4 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::node::s_jumpbox inherits govuk::node::s_base {
 
-  # FIXME: When deployed to preview (platform1 never got this).
-  file { '/usr/local/share/govuk-fabric':
-    ensure  => absent,
-    recurse => true,
-    force   => true,
-    backup  => false,
-  }
-
 }
