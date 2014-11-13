54a8b154e474a445c99759ea9fab7057a816c3d4
Matt Bostock <matt@mattbostock.com>
2014-11-13 11:39:00 +0000

    Remove FIXME for Imminence delayed job worker

--- modules/govuk/manifests/apps/imminence.pp
+++ modules/govuk/manifests/apps/imminence.pp
@@ -12,15 +12,4 @@ class govuk::apps::imminence( $port = 3002, $enable_procfile_worker = true ) {
   govuk::procfile::worker { 'imminence':
     enable_service => $enable_procfile_worker,
   }
-
-  # Clean up old delayed-job worker.
-  # FIXME: These can be removed once they've run on production
-  exec {'stop_imminence_delayed_job_worker':
-    command => 'service imminence-delayed-job-worker stop || /bin/true',
-    onlyif  => 'test -f /etc/init/imminence-delayed-job-worker.conf',
-  }
-  file { '/etc/init/imminence-delayed-job-worker.conf':
-    ensure  => absent,
-    require => Exec['stop_imminence_delayed_job_worker'],
-  }
 }
