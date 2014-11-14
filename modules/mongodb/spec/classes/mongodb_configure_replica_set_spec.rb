141f5dcb1d18808fa6479bbf77960c55295d8678
Anna Shipman <anna@annashipman.co.uk>
2014-11-14 11:20:26 +0000

    Add test for replicaset named development
    Not in previous commit as that also reformatted this class, so this makes the change here clearer.

--- modules/mongodb/spec/classes/mongodb_configure_replica_set_spec.rb
+++ modules/mongodb/spec/classes/mongodb_configure_replica_set_spec.rb
@@ -1,13 +1,27 @@
 require_relative '../../../../spec_helper'
 
 describe 'mongodb::configure_replica_set', :type => :class do
-  let(:params) {{
-    'members' => ['123.456.789.123', '987.654.321.012', '432.434.454.454:457'],
-    'replicaset' => 'production',
-  }}
+  describe 'production' do
+    let(:params) {{
+      'members' => ['123.456.789.123', '987.654.321.012', '432.434.454.454:457'],
+      'replicaset' => 'production',
+    }}
 
-  it {
-    should contain_file('/etc/mongodb/configure-replica-set.js').with_content(
-      /_id: "production"/)
-  }
+    it {
+      should contain_file('/etc/mongodb/configure-replica-set.js').with_content(
+        /_id: "production"/)
+    }
+  end
+
+  describe 'development' do
+    let(:params) {{
+      'members' => ['123.456.789.123', '987.654.321.012', '432.434.454.454:457'],
+      'replicaset' => 'development',
+    }}
+
+    it {
+      should contain_file('/etc/mongodb/configure-replica-set.js').with_content(
+        /_id: "development"/)
+    }
+  end
 end
