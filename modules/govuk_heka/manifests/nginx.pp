49a84b809d77c7dff4340f97db7abbae1d53818c
Dan Carley <dan.carley@gmail.com>
2014-12-11 10:28:23 +0000

    Consume all Nginx access logs with Heka
    This is basically the same as errors logs two commit previous. The only real
    difference is that we're using the JSON event decoder and they have
    `@type:nginx.access`.
    
    These entries can be searched in Kibana using `server_name:` and
    `http_host:` as before. But you may wish to use `@logger:` instead if you
    want to see access and error logs for the same vhost.

--- modules/govuk_heka/manifests/nginx.pp
+++ modules/govuk_heka/manifests/nginx.pp
@@ -1,8 +1,10 @@
 # == Class: govuk_heka::nginx
 #
-# Consume all Nginx error entries.
+# Consume all Nginx access (JSON) and error (plaintext) entries.
 #
 class govuk_heka::nginx {
+  include heka::plugin::json_event_decoder
+
   # FIXME: Move this to a global config option in heka >=0.9
   if !$::fqdn_underscore {
     fail('Unable to load `fqdn_underscore` fact')
