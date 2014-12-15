9813c1b6e6148ed6953f57a0516e63c00fa735b2
Matt Bostock <matt@mattbostock.com>
2014-12-15 14:43:04 +0000

    Add module to orchestrate unattended reboots
    A new Puppet module, `govuk_unattended_reboot`, which orchestrates
    unattended reboots across an environment using a distributed
    mutually-exclusive lock ('mutex') backed by [etcd][].
    
    If security updates are applied by `unattended-upgrades(1)`, each
    machine should attempt to reboot overnight (if it considers it safe to
    do so) in turn.
    
    The module is disabled by default, and can be enabled separately for
    each environment.
    
    The module comprises of two key components:
    
    - A cron job that runs every minute overnight and assets that the
      following conditions are true before rebooting a machine:
    
        #. A reboot is necessary, as indicated by the presence of the
        `/var/run/reboot-required` flag generated by the
        `update-notifier-common` package following the application of
        security updates.
    
        #. The machine is safe to reboot, as determined by
        `/etc/govuk/env.d/SAFE_TO_REBOOT`.
    
        #. There are no `WARNING`, `CRITICAL`, `DOWN` or `UNREACHABLE`
        alerts in Icinga for any machine of the same node class.
    
        #. No other machines are currently undergoing a reboot, as
        determined by `locksmithctl`.
    
    - An Upstart task (note [task][], not service) that releases the reboot
      mutex on boot once networking is available. Both `0` and `1` exit
      codes are accepted as `locksmithctl(1)` may return an exit code of `1`
      if the reboot mutex is not held by any machine.
    
    Note that the TTL used for the reboot mutex is zero (0), meaning that
    the mutex should not expire (and thus prevent other machines from
    rebooting if not released by the last machine to grab the lock):
    https://github.com/coreos/locksmith/blob/7909adde5b9415f10c102cec9743303819ebe998/lock/etcd.go#L94
    https://github.com/coreos/locksmith/blob/7909adde5b9415f10c102cec9743303819ebe998/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd/compare_and_swap.go#L5
    
    [task]: http://upstart.ubuntu.com/cookbook/#task-job
    [accepted]: http://upstart.ubuntu.com/cookbook/#normal-exit
    
    - The reboot mutex uses a convoluted namespace for our purposes:
      `coreos.com/updateengine/rebootlock/semaphore`
    
      This is [hard-coded][] into `locksmith`; we could use a simpler key
      name if we were to implement our own locking tool at some point in
      future.
    
    - The lock/unlock mechanism currently only points to the first endpoint
      in the array passed to `govuk_unattended_reboot::etcd_endpoints`. In
      practical terms, this means that the locking mechanism reliant on
      `etcd-1.management` being up for it to work. The [go-etcd][] library
      that `locksmithctl(1)` uses allows multiple endpoints to be specified,
      so we could modify `locksmithctl(1)` to support this.
    
    - The errors returned by `locksmithctl(1)` are ambiguous and make it
      difficult to distinguish between a scenario where the etcd cluster is
      down and another where the reboot lock is already held by another
      machine. This may mean that the logs are confusing. This could be
      solved by contributing a pull request to `locksmithctl`.
    
    - We might want to write a more sophisticated script, for example for
      checking the Icinga status. I think this is sufficient for a first
      pass.
    
    [etcd]: https://github.com/coreos/etcd
    [go-etcd]: https://github.com/coreos/go-etcd/
    [hard-coded]: https://github.com/coreos/locksmith/blob/7909adde5b9415f10c102cec9743303819ebe998/lock/etcd.go#L29

---
+++ modules/govuk_unattended_reboot/spec/classes/unattended_reboot_spec.rb
@@ -0,0 +1,55 @@
+require_relative '../../../../spec_helper'
+
+describe 'govuk_unattended_reboot', :type => :class do
+  let(:facts) {{
+    :fqdn             => 'foo.example.com',
+    :govuk_node_class => 'chocolate_factory',
+  }}
+
+  describe "enabled" do
+    let(:params) {{
+      :enabled => true,
+      :etcd_endpoints => ['http://etcd-1.foo:4001', 'http://etcd-2.foo:4001'],
+    }}
+
+    it { should contain_file('/etc/init/post-reboot-unlock.conf').with_ensure('present') }
+    it { should contain_file('/usr/local/bin/unattended-reboot').with_ensure('present') }
+    it { should contain_file('/etc/logrotate.d/unattended_reboot').with_ensure('present') }
+
+    it { should contain_cron('unattended-reboot').with_ensure('present') }
+
+    it "passes the first endpoint only to locksmithctl" do
+      should contain_file('/usr/local/bin/unattended-reboot').with_content(/locksmithctl -endpoint='http:\/\/etcd-1\.foo:4001' lock/)
+    end
+
+    it "correctly formats the node class when querying Icinga" do
+      should contain_file('/usr/local/bin/unattended-reboot').with_content(/status.cgi\?search_string=chocolate-factory&/)
+    end
+
+    it "uses the correct FQDN when obtaining the reboot mutex" do
+      should contain_file('/usr/local/bin/unattended-reboot').with_content(/locksmithctl.*lock 'foo\.example\.com'/)
+    end
+  end
+
+  describe "disabled" do
+    let(:params) {{
+      :enabled => false,
+    }}
+
+    it { should contain_file('/etc/init/post-reboot-unlock.conf').with_ensure('absent') }
+    it { should contain_file('/usr/local/bin/unattended-reboot').with_ensure('absent') }
+    it { should contain_file('/etc/logrotate.d/unattended_reboot').with_ensure('absent') }
+
+    it { should contain_cron('unattended-reboot').with_ensure('absent') }
+  end
+
+  describe "enabled but empty array passed to etcd_endpoints" do
+    let(:params) {{
+      :enabled => true,
+      :etcd_endpoints => [],
+    }}
+
+    it { expect { should }.to raise_error(Puppet::Error, /^Must pass non-empty array/) }
+  end
+
+end
