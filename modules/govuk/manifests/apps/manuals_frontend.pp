d8c44b701199f541eb24694d44823c4bbfaf1774
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-12-15 15:00:37 +0000

    Manuals Frontend doesn't need to be optional
    It has been been deployed to every environment for a while now.

--- modules/govuk/manifests/apps/manuals_frontend.pp
+++ modules/govuk/manifests/apps/manuals_frontend.pp
@@ -1,14 +1,11 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::apps::manuals_frontend(
   $port = 3072,
-  $enabled = false,
 ) {
-  if $enabled {
-    govuk::app { 'manuals-frontend':
-      app_type              => 'rack',
-      port                  => $port,
-      asset_pipeline        => true,
-      asset_pipeline_prefix => 'manuals-frontend',
-    }
+  govuk::app { 'manuals-frontend':
+    app_type              => 'rack',
+    port                  => $port,
+    asset_pipeline        => true,
+    asset_pipeline_prefix => 'manuals-frontend',
   }
 }
