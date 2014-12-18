4ad94269d3da1b97c3181e8fb6bbdb1692cb2469
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-12-18 22:35:47 +0000

    Stub Facter facts; rename Elasticsearch class
    Since we stub our own Facter facts in Rspec tests, rather than rely on the
    true values from Facter, we need to ensure that we're passing some values
    correctly in the Rspec tests. Without these, tests will fail.
    
    To neaten these tests up, too, refactor the tests such that the Facter facts
    are stubbed outside the inner-most context, thus only requiring them to be
    set once. This works fine for the meantime, and a bit of re-factoring at a
    later stage will allow them to differ per inner context if so required.
    
    Finally, 'elasticsearch_old' is no more. Long live 'elasticsearch'.

--- modules/govuk_elasticsearch/spec/classes/govuk_elasticsearch_spec.rb
+++ modules/govuk_elasticsearch/spec/classes/govuk_elasticsearch_spec.rb
@@ -2,13 +2,23 @@ require_relative '../../../../spec_helper'
 
 describe 'govuk_elasticsearch', :type => :class do
   describe '#use_mirror' do
+    let(:facts) {{
+      :kernel      => 'Linux',
+      :configdir   => '/woody',
+      :package_dir => '/rex',
+      :installpath => '/slinky/dog',
+      :plugindir   => '/mr/potato/head',
+      :plugintool  => 'bopeep',
+      :datadir     => '/buzz/lightyear',
+    }}
+
     context 'true (default)' do
       let(:params) {{
         :version => '1.2.3',
       }}
 
       it { should contain_class('govuk_elasticsearch::repo') }
-      it { should contain_class('elasticsearch_old').with_manage_repo(false) }
+      it { should contain_class('elasticsearch').with_manage_repo(false) }
     end
 
     context 'false' do
@@ -18,7 +28,7 @@ describe 'govuk_elasticsearch', :type => :class do
       }}
 
       it { should_not contain_class('govuk_elasticsearch::repo') }
-      it { should contain_class('elasticsearch_old').with_manage_repo(true) }
+      it { should contain_class('elasticsearch').with_manage_repo(true) }
     end
   end
 end
