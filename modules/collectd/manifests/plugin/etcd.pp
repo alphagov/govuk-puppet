81b3adc76e1646976ae38f498480af6f4d78c297
Matt Bostock <matt@mattbostock.com>
2014-12-23 15:23:10 +0000

    Add collecd plugin for etcd
    Add a collectd plugin for etcd that uses the cURL-JSON plugin to query
    etcd's API.
    
    See this page for more information on the metrics queried:
    https://coreos.com/docs/distributed-configuration/etcd-api/
    
    With our current configuration, this will produce Graphite metrics such
    as:
    
        etcd-3_management_development.curl_json-etcd_self.recvBandwidthRate
    
    Some of the metrics queried are scoped to the cluster and so will be
    duplicated for each etcd node that collectd runs on; other metrics are
    local to each node. etcd exposes both types of metrics on the same
    endpoints, so you'll need to check the documentation (above) to see
    which is which.
    
    Alternatively, we could change the plugin or type
    instance to group metrics in a more intuitive way but the advantage of
    the grouping implemented here is that it mirrors etcd's API.
    
    Note that we don't query the `/v2/stats/leader` endpoint because it only
    returns statistics for follower nodes (not the leader node), and if we
    query the node local to collectd, etcd will respond with a HTTP
    `Location` header redirecting us to the leader, which the collectd
    cURL-JSON plugin does not support.
    
    The `/v2/stats/leader` endpoint provides some useful metrics such as
    failed RPC requests and node latency, so it might be worth trying to add
    this in future.

---
+++ modules/collectd/manifests/plugin/etcd.pp
@@ -0,0 +1,23 @@
+# == Class: collectd::plugin::etcd
+#
+# Collectd plugin for etcd using the cURL-JSON plugin.
+#
+# === Variables
+#
+# [*client_endpoint*]
+#   Determines the etcd client URL (typically on port 4001) to use when retrieving metrics.
+#
+# [*peer_endpoint*]
+#   Determines the etcd peer URL (typically on port 7001) to use when retrieving metrics.
+#
+class collectd::plugin::etcd {
+  include collectd::plugin::curl_json
+
+  $client_endpoint = "${::fqdn}:4001"
+  $peer_endpoint = "${::fqdn}:7001"
+
+  @collectd::plugin { 'etcd':
+    content => template('collectd/etc/collectd/conf.d/etcd.conf.erb'),
+    require => Class['collectd::plugin::curl_json'],
+  }
+}
--- modules/govuk_etcd/manifests/monitoring.pp
+++ modules/govuk_etcd/manifests/monitoring.pp
@@ -3,6 +3,9 @@
 # Monitoring checks for an etcd cluster.
 #
 class govuk_etcd::monitoring {
+
+  include collectd::plugin::etcd
+
   @@icinga::check { "check_etcd_running_${::hostname}":
     check_command       => 'check_nrpe!check_proc_running!etcd',
     service_description => 'etcd not running',
