d7c7d48c466615be47ebd2ebea348c69a900749c
Matt Bostock <matt@mattbostock.com>
2014-12-23 13:22:18 +0000

    Remove MongoDB FIXME
    This has been deployed to Production.

--- modules/mongodb/manifests/logging.pp
+++ modules/mongodb/manifests/logging.pp
@@ -1,20 +1,5 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class mongodb::logging {
-
-
-  # FIXME: Remove once deployed to Production
-  include logrotate
-  file { '/var/log/mongodb':
-    ensure  => absent,
-    force   => true,
-    recurse => true,
-  }
-  file { '/etc/logrotate.d/mongodb':
-    ensure  => absent,
-    require => Class['logrotate'],
-  }
-  # FIXME: End
-
   govuk::logstream { 'mongodb-logstream':
     logfile => '/var/log/upstart/mongodb.log',
     fields  => {'application' => 'mongodb'},
