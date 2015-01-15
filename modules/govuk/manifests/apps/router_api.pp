08b178fe15b2177ab7e8dfa3e17bdb914b9ca5cf
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-01-15 15:33:32 +0000

    Remove router-api ENABLE_ROUTER_RELOADING env var
    This hasn't been used since
    https://github.com/alphagov/router-api/pull/49

--- modules/govuk/manifests/apps/router_api.pp
+++ modules/govuk/manifests/apps/router_api.pp
@@ -1,21 +1,10 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::apps::router_api( $port = 3056, $enable_router_reloading = true ) {
+class govuk::apps::router_api( $port = 3056 ) {
   govuk::app { 'router-api':
     app_type           => 'rack',
     port               => $port,
     vhost_ssl_only     => true,
     health_check_path  => '/healthcheck',
     log_format_is_json => true,
-  }
-
-  validate_bool($enable_router_reloading)
-
-  if ($enable_router_reloading) {
-    govuk::app::envvar {
-      "${title}-ENABLE_ROUTER_RELOADING":
-        app     => 'router-api',
-        varname => 'ENABLE_ROUTER_RELOADING',
-        value   => '1';
-    }
   }
 }
