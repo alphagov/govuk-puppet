b33f5be5eaa580b4b9c52780b5b72361555f73ec
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-01-15 16:49:27 +0000

    Pass MONGODB_URI to router-api as ENV var.
    So that we don't need to overwrite config files at deploy time.

--- modules/govuk/manifests/apps/router_api.pp
+++ modules/govuk/manifests/apps/router_api.pp
@@ -1,8 +1,12 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::apps::router_api(
   $port = 3056,
+  $mongodb_nodes,
   $secret_key_base = undef,
 ) {
+
+  validate_array($mongodb_nodes)
+
   govuk::app { 'router-api':
     app_type           => 'rack',
     port               => $port,
@@ -11,11 +15,22 @@ class govuk::apps::router_api(
     log_format_is_json => true,
   }
 
+  Govuk::App::Envvar {
+    app => 'router-api',
+  }
+
   if $secret_key_base != undef {
     govuk::app::envvar { "${title}-SECRET_KEY_BASE":
-      app     => 'router-api',
       varname => 'SECRET_KEY_BASE',
       value   => $secret_key_base,
+    }
+  }
+
+  if $mongodb_nodes != [] {
+    $mongodb_nodes_string = join($mongodb_nodes, ',')
+    govuk::app::envvar { "${title}-MONGODB_URI":
+      varname => 'MONGODB_URI',
+      value   => "mongodb://${mongodb_nodes_string}/router",
     }
   }
 }
