2fc7d6dd5ba1a74df9704c4ff562c5b26fd35b2a
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-01-15 15:43:59 +0000

    Add router-api SECRET_KEY_BASE env var.
    To replace the config that currently comes from alphagov-deployment.

--- modules/govuk/manifests/apps/router_api.pp
+++ modules/govuk/manifests/apps/router_api.pp
@@ -1,10 +1,21 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::apps::router_api( $port = 3056 ) {
+class govuk::apps::router_api(
+  $port = 3056,
+  $secret_key_base = undef,
+) {
   govuk::app { 'router-api':
     app_type           => 'rack',
     port               => $port,
     vhost_ssl_only     => true,
     health_check_path  => '/healthcheck',
     log_format_is_json => true,
+  }
+
+  if $secret_key_base != undef {
+    govuk::app::envvar { "${title}-SECRET_KEY_BASE":
+      app     => 'router-api',
+      varname => 'SECRET_KEY_BASE',
+      value   => $secret_key_base,
+    }
   }
 }
