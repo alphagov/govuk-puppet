9bd37e53aa1c594bb1323d606eb8bb496a66a52d
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-01-16 15:28:45 +0000

    Update puppetdocs for router-api

--- modules/govuk/manifests/apps/router_api.pp
+++ modules/govuk/manifests/apps/router_api.pp
@@ -1,4 +1,23 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: govuk::apps::router_api
+#
+# Configure the router-api app on a node.
+#
+# === Parameters
+#
+# [*port*]
+#   The port that router-api listens on.
+#   Default: 3056
+#
+# [*mongodb_nodes*]
+#   Array of hostnames for the mongo cluster to use.
+#
+# [*router_nodes*]
+#   Array of hostname:port pairs for the router instances.  These will be used
+#   when reloading routes in the router.
+#
+# [*secret_key_base*]
+#   The key for Rails to use when signing/encrypting sessions.
+#
 class govuk::apps::router_api(
   $port = 3056,
   $mongodb_nodes,
