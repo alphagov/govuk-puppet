ad8a19f291cd6d8a8e14781a3c7de8b0bcf0117c
Dan Carley <dan.carley@gmail.com>
2015-01-21 14:43:36 +0000

    Remove nginx::log/logstream for MHRA reverse proxy
    To match d240cc82def41caf657e771cac1d718320c3ee05. These logs are now
    automatically picked up by Heka. This will prevent duplicate entries with
    `@tags:nginx` and `@type:nginx.*` from appearing in Kibana.

--- modules/govuk/manifests/apps/bouncer.pp
+++ modules/govuk/manifests/apps/bouncer.pp
@@ -127,11 +127,11 @@ class govuk::apps::bouncer(
   nginx::log {
     'www.mhra.gov.uk-json.event.access.log':
       json          => true,
-      logstream     => present,
+      logstream     => absent,
       statsd_metric => "${::fqdn_underscore}.nginx_logs.mhra_proxy.http_%{@fields.status}",
       statsd_timers => [{metric => "${::fqdn_underscore}.nginx_logs.mhra_proxy.time_request",
                           value => '@fields.request_time'}];
     'www.mhra.gov.uk-error.log':
-      logstream     => present;
+      logstream     => absent;
   }
 }
