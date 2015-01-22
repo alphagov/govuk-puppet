623b1e26cb45c12d7de655950b77a7049b2a2c8b
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-01-22 16:43:10 +0000

    Configure entity extractor database connection
    We pass the database credentials to the application via environment
    variables.
    
    I've used the `hiera()` function in the `development_credentials.yml` in
    order to avoid repeating the database password.

--- modules/govuk/manifests/apps/entity_extractor.pp
+++ modules/govuk/manifests/apps/entity_extractor.pp
@@ -11,6 +11,7 @@
 class govuk::apps::entity_extractor(
   $enabled = false,
   $port = 3096,
+  $db_password = '',
 ) {
   if $enabled {
     $app_name = 'entity-extractor'
@@ -21,6 +22,21 @@ class govuk::apps::entity_extractor(
       command            => "./${app_name}",
       health_check_path  => '/healthcheck',
       vhost_ssl_only     => true,
+    }
+
+    govuk::app::envvar {'EXTRACTOR_EXTRACT_ADDR':
+      app   => 'entity-extractor',
+      value => ":${port}",
+    }
+
+    govuk::app::envvar {'EXTRACTOR_DB_CONNECTION_STRING':
+      app   => 'entity-extractor',
+      value => "host=postgresql-master-1 username=${app_name} password=${db_password} dbname=entity-extractor_production sslmode=disable"
+    }
+
+    govuk::app::envvar {'EXTRACTOR_LOG_PATH':
+      app   => 'entity-extractor',
+      value => 'STDERR'
     }
   }
 }
