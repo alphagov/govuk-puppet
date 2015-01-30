1dcd9785593852855cff379f5d8e36e3ffab4809
Dan Carley <dan.carley@gmail.com>
2015-01-30 15:12:36 +0000

    De-dupe automysqlbackup resources
    These resources were duplicated in four places where the only per-class
    difference was the password being passed in. I'm about to make some changes
    to the params and may add a Nagios passive check, so now seems like a good
    time to de-dupe them.
    
    Depends on gds/deployment#493 for name changes to hiera keys.

--- modules/govuk/manifests/node/s_support_contacts.pp
+++ modules/govuk/manifests/node/s_support_contacts.pp
@@ -2,9 +2,6 @@
 class govuk::node::s_support_contacts (
   $mysql_root_support_contacts,
   $mysql_support_contacts,
-  $rotation_daily   = '6',
-  $rotation_monthly = '95',
-  $rotation_weekly  = '28',
 ) inherits govuk::node::s_base {
 
   class { 'govuk_mysql::server':
@@ -22,39 +19,10 @@ class govuk::node::s_support_contacts (
     password => $mysql_support_contacts,
   }
 
-  file { '/etc/automysqlbackup/prebackup':
-    source => 'puppet:///modules/govuk/etc/automysqlbackup/prebackup',
-  }
-  file { '/etc/automysqlbackup/postbackup':
-    source => 'puppet:///modules/govuk/etc/automysqlbackup/postbackup',
-  }
-  ensure_packages(['mailutils'])
-
-  include automysqlbackup
-  automysqlbackup::backup { 'automysqlbackup':
-    backup_dir                   => '/var/lib',
-    backup_dir_perms             => '0755',
-    backup_file_perms            => '0444',
-    db_exclude                   => ['mysql', 'information_schema', 'performance_schema', 'test'],
-    do_weekly                    => '6',
-    mail_address                 => 'zd-alrt-normal@digital.cabinet-office.gov.uk',
-    mailcontent                  => 'quiet',
-    mail_maxattsize              => '4000',
-    mysql_dump_username          => 'root',
-    mysql_dump_password          => $mysql_root_support_contacts,
-    mysql_dump_host              => 'localhost',
-    mysql_dump_create_database   => 'yes',
-    mysql_dump_use_separate_dirs => 'yes',
-    mysql_dump_compression       => 'bzip2',
-    mysql_dump_commcomp          => 'no',
-    mysql_dump_latest            => 'yes',
-    prebackup                    => '/etc/automysqlbackup/prebackup',
-    postbackup                   => '/etc/automysqlbackup/postbackup',
-    rotation_daily               => $rotation_daily,
-    rotation_weekly              => $rotation_weekly,
-    rotation_monthly             => $rotation_monthly,
+  class { 'backup::mysql':
+    mysql_dump_password => $mysql_root_support_contacts,
+    require             => Govuk::Mount['/var/lib/automysqlbackup'],
   }
 
   Govuk::Mount['/var/lib/mysql'] -> Class['govuk_mysql::server']
-  Govuk::Mount['/var/lib/automysqlbackup'] -> Automysqlbackup::Backup['automysqlbackup']
 }
