ee7404055b103cdb39c4c3a3961dd650af5c7ed7
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-02-03 15:27:13 +0000

    Move autopostgresqlbackup to a template
    - Add a trap function which sends a passive check to Nagios
    - If the script reaches the end and still has $STATUS of 0,
      assume that it has succeeded

--- modules/govuk_postgresql/manifests/backup.pp
+++ modules/govuk_postgresql/manifests/backup.pp
@@ -1,7 +1,17 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk_postgresql::backup {
-    file {'/usr/local/sbin/autopostgresqlbackup':
-        source  => 'puppet:///modules/govuk_postgresql/usr/local/sbin/autopostgresqlbackup',
+    $threshold_secs = 28 * 3600
+    $service_desc = 'AutoPostgreSQL backup'
+
+    file {'/etc/cron.daily/autopostgresqlbackup':
+        mode    => '0755',
+        content => template('govuk_postgresql/etc/cron.daily/autopostgresqlbackup.erb'),
+    }
+
+    @@icinga::passive_check { "check_autopostgresqlbackup-${::hostname}":
+        service_description => $service_desc,
+        freshness_threshold => $threshold_secs,
+        host_name           => $::fqdn,
     }
 
     # FIXME: Remove when the autopostgresqlbackup has been removed from all environments
@@ -15,7 +25,7 @@ class govuk_postgresql::backup {
     #    PREBACKUP="/etc/postgresql-backup-pre"
     file {'/etc/default/autopostgresqlbackup':
         source  => 'puppet:///modules/govuk_postgresql/etc/default/autopostgresqlbackup',
-        require => File['/usr/local/sbin/autopostgresqlbackup'],
+        require => File['/etc/cron.daily/autopostgresqlbackup'],
     }
 
     include backup::client
