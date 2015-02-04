3f992023e1bdca6653210ba1a5aa8a0abb69c871
Sven Agnew <sven@kangohammer.com>
2015-02-04 15:00:13 +0000

    Add user svenagnew to contractors.pp
    As per follow up instruction, I've added the user svenagnew to the list
    of contractors in the contractors.pp group file.

--- modules/users/manifests/groups/contractors.pp
+++ modules/users/manifests/groups/contractors.pp
@@ -9,6 +9,7 @@ class users::groups::contractors {
   include users::elliot
   include users::futurefabric
   include users::paulhayes
+  include users::svenagnew
   include users::tekin
   include users::vinayvinay
 }
