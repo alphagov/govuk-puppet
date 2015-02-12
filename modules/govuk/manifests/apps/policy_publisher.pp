95e2972a64e8a5810430147aa0bc8fe6db3844d2
David Singleton <david@dsingleton.co.uk>
2015-02-12 15:21:44 +0000

    No need to feature flag Policy Publisher anymore
    It's setup in prod/staging now.

--- modules/govuk/manifests/apps/policy_publisher.pp
+++ modules/govuk/manifests/apps/policy_publisher.pp
@@ -11,24 +11,17 @@
 #   What port should the app run on?
 #   Default: 3098
 #
-# [*enabled*]
-#   Feature flag to allow the app to be deployed to an environment.
-#   Default: false
-#
 class govuk::apps::policy_publisher(
   $port = 3098,
-  $enabled = false
 ) {
-  if $enabled {
-    include govuk_postgresql::client #installs libpq-dev package needed for pg gem
-    govuk::app { 'policy-publisher':
-      app_type           => 'rack',
-      port               => $port,
-      vhost_ssl_only     => true,
-      health_check_path  => '/healthcheck',
-      log_format_is_json => true,
-      asset_pipeline     => true,
-      deny_framing       => true,
-    }
+  include govuk_postgresql::client #installs libpq-dev package needed for pg gem
+  govuk::app { 'policy-publisher':
+    app_type           => 'rack',
+    port               => $port,
+    vhost_ssl_only     => true,
+    health_check_path  => '/healthcheck',
+    log_format_is_json => true,
+    asset_pipeline     => true,
+    deny_framing       => true,
   }
 }
