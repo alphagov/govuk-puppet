46e17a6f13421da74e607203aef17a399d9fe740
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-02-18 12:42:57 +0000

    Add test for unattended reboot unlocking
    I missed this when updating locksmithctl to use all the endpoints,
    so add a test for it.

--- modules/govuk_unattended_reboot/spec/classes/unattended_reboot_spec.rb
+++ modules/govuk_unattended_reboot/spec/classes/unattended_reboot_spec.rb
@@ -30,6 +30,10 @@ describe 'govuk_unattended_reboot', :type => :class do
     it "uses the correct FQDN when obtaining the reboot mutex" do
       should contain_file('/usr/local/bin/unattended-reboot').with_content(/locksmithctl.*lock 'foo\.example\.com'/)
     end
+
+    it "includes all etcd endpoints when unlocking after a reboot" do
+      should contain_file('/etc/init/post-reboot-unlock.conf').with_content(/locksmithctl -endpoint='http:\/\/etcd-1\.foo:4001,http:\/\/etcd-2\.foo:4001' unlock/)
+    end
   end
 
   describe "disabled" do
