0e516dae24551b44cebe3eb656c132ecbae5857c
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-03-02 14:06:52 +0000

    Update test for node classes
    This updates the optional spec for all node classes to work with current
    versions of everything. This tests all the govuk::node::s_* classes to
    ensure that they complie. They run with the real hieradata for the
    vagrant environment (except for the s_development class which uses the
    development data).
    
    This will provide 2 things:
    
    * It will ensure that all the node classes compile, and should therefore
      catch issues like the one fixed in 3c7e05a.
    * It will ensure that the necessary hieradata is populated for the
      vagrant environment, which should ensure that all machines can be
      brought up in vagrant-govuk
    
    This test takes around 12 minutes to run on my machine, and is therefore
    left out of the main test run.  It can be run by the separate test:nodes
    rake task.

--- modules/govuk/spec/classes/govuk_nodes_spec_optional.rb
+++ modules/govuk/spec/classes/govuk_nodes_spec_optional.rb
@@ -5,31 +5,54 @@ require_relative '../../../../spec_helper'
 # filtering was not appropriate because the glob and loop below get
 # eagerly evaluated.
 
-def class_list
-  if ENV["classes"]
-    ENV["classes"].split(",")
-  else
-    class_dir = File.expand_path("../../../manifests/node", __FILE__)
-    Dir.glob("#{class_dir}/*.pp").collect { |dir|
-      dir.gsub(/^#{class_dir}\/s_(.+)\.pp$/, '\1')
+# Don't attempt to instantiate these classes as they aren't concrete
+# machine classes, and therefore aren't intended to be instantiated directly.
+blacklist_classes = %w(
+  asset_base
+  base
+  postgresql_base
+  redis_base
+  ruby_app_server
+  test
+  transition_postgresql_base
+)
+
+hiera_conf = YAML.load_file(File.expand_path("../../../../../hiera.yml", __FILE__))
+hiera_conf[:yaml][:datadir] = File.expand_path("../../../../../hieradata", __FILE__)
+hiera_conf[:eyaml][:datadir] = hiera_conf[:yaml][:datadir]
+hiera_conf[:eyaml][:gpg_gnupghome] = File.expand_path("../../../../../gpg", __FILE__)
+
+Dir.glob(File.expand_path("../../../manifests/node/s_*.pp", __FILE__)).each do |filepath|
+  class_name = File.basename(filepath, ".pp")[2..-1] # Strip leading s_
+  next if blacklist_classes.include?(class_name)
+
+  node_hostname = class_name.tr("_", "-")
+
+  describe "govuk::node::s_#{class_name}", :type => :class do
+    let(:node) { "#{node_hostname}-1.example.com" }
+    let(:facts) {{
+      :environment => (class_name == "development" ? "development" : 'vagrant'),
+      :concat_basedir => '/var/lib/puppet/concat/',
+      :kernel => 'Linux',
+      :memorysize =>  '3.86 GB',
+      :memtotalmb => 3953,
+    }}
+
+    let(:hiera_config) { hiera_conf }
+
+    # Pull in some required bits from top-level site.pp
+    let(:pre_condition) { <<-EOT
+$govuk_node_class = "#{class_name}"
+
+$lv = hiera('lv',{})
+create_resources('govuk::lvm', $lv)
+$mount = hiera('mount',{})
+create_resources('govuk::mount', $mount)
+      EOT
     }
-  end
-end
 
-describe "govuk::node classes" do
-  pending "FIXME: This was broken by the upgrade to rspec-puppet 1.x"
-
-  class_list.each do |govuk_class|
-    fqdn = "#{govuk_class.gsub('_', '-')}-1.example.com"
-
-    describe fqdn, :type => :host do
-      let(:node) { fqdn }
-      let(:facts) {{
-        :clientcert     => fqdn,
-        :memtotalmb     => "1024",
-      }}
-
-      it { should contain_class("govuk::node::s_#{govuk_class}") }
+    it "should compile" do
+      expect { subject }.not_to raise_error
     end
   end
 end
