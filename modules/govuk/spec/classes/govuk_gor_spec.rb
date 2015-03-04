8d3257024cba6893c1ac99b9cfefa9fd1f7d05e9
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2015-03-04 11:20:55 +0000

    Disable gor if the data sync is in progress
    If a facter fact of data_sync_in_progress is set to true ensure that gor
    isnt running. Also remove icinga checks and the log streaming.

--- modules/govuk/spec/classes/govuk_gor_spec.rb
+++ modules/govuk/spec/classes/govuk_gor_spec.rb
@@ -31,4 +31,40 @@ describe 'govuk::gor', :type => :class do
       )
     }
   end
+
+  context 'enabled but datasync in progress' do
+    let(:params) {{
+      'args' => args_default,
+      :enable => true,
+    }}
+    let(:facts) {{
+      'data_sync_in_progress' => true,
+    }}
+
+    it { should contain_govuk__logstream('gor_upstart_log').with_ensure('absent') }
+
+    it {
+      should contain_class('gor').with(
+        :service_ensure => 'stopped',
+      )
+    }
+  end
+
+  context 'enabled but datasync not in progress' do
+    let(:params) {{
+      'args' => args_default,
+      :enable => true,
+    }}
+    let(:facts) {{
+      'data_sync_in_progress' => false,
+    }}
+
+    it { should contain_govuk__logstream('gor_upstart_log').with_ensure('present') }
+
+    it {
+      should contain_class('gor').with(
+        :service_ensure => 'running',
+      )
+    }
+  end
 end
