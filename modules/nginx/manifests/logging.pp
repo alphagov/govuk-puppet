d53750dedfbd03b8f51541e08cbebde3d68c6142
Matt Bostock <matt@mattbostock.com>
2015-03-16 13:57:28 +0000

    Add back class documentation for logging.pp
    This class documentation was added when Heka was introduced. It was
    removed when we removed Heka in bd1cb5, but I'm adding it back here as
    the class documentation is worth keeping.

--- modules/nginx/manifests/logging.pp
+++ modules/nginx/manifests/logging.pp
@@ -1,4 +1,7 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: nginx::logging
+#
+# Nginx log centralisation and rotation.
+#
 class nginx::logging {
   file { '/etc/logrotate.d/nginx':
     ensure => present,
