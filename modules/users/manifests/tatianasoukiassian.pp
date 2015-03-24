40d5ef1eb8b26940ec82fe50f7386225d785b997
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-03-24 14:12:40 +0000

    added Tatiana's manifest file

---
+++ modules/users/manifests/tatianasoukiassian.pp
@@ -0,0 +1,8 @@
+# Creates the tatianasoukiassian user
+class users::tatianasoukiassian {
+  govuk::user { 'tatianasoukiassian':
+    fullname => 'Tatiana Soukiassian',
+    email    => 'tatiana.soukiassian@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOAP8SYTwqBym+ZOibaISjNeDp/kxmyn/LbyGMJR5ekLpWeQUX7nP5IiPqjwUZu3n5f5KJ6w79jDxNxmO2mXvQ+ZBQ1ZIAKE9QV0FvpOD9Vvs2wT12NEROpb4nLchj5tvM81cydYBka3vqy++a6xcpA92iKCp6fH0q0pzmymWpeLz3/RZvp/y1X58Kqd3VOfHOtl4sPqjEJHY3oojHPeXNQlmndWq7OUDX5C08TFeo9WHO1eGZ+gY+6TGsRdQCIU/wf01XcfFOGC+hBdJ+nbh9H2vcQ/PZovjCLz2bfNdxpkjfY+aCIg7Yj4X/BRgWbdoYcJa0Ej5QQFGLbKizpW5F tatiana.soukiassian@digital.cabinet-office.gov.uk'
+  }
+}
