799a6b10c524bae6c1ca4c542f6d3f45b8d78eb8
Vinay Patel <mail@vinayvinay.com>
2015-03-31 13:46:31 +0530

    Enable running content store in draft mode
    https://trello.com/c/9IjHVbeJ
    
    Enables running content-store for storing draft content.
    In draft mode content-store:
    
    1. runs on a different port
    2. writes to a different mongo database
    3. is served via a different nginx vhost
    4. prepends [DRAFT] to rails application log entries

--- modules/govuk/manifests/apps/content_store.pp
+++ modules/govuk/manifests/apps/content_store.pp
@@ -1,6 +1,25 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class govuk::apps::content_store
+#
+# The central storage of published content on GOV.UK
+#
+# === Parameters
+#
+# [*port*]
+#   The port the app will listen on.
+#
+# [*enable_running_in_draft_mode*]
+#   Intended to be used only during development.
+#   Enables running content-store for storing draft content.
+#   In draft mode content-store:
+#
+#   1. runs on a different port: 3100
+#   2. writes to a different mongo database: draft_content_store_development
+#   3. is served via a different nginx vhost: draft-content-store.dev.gov.uk
+#   4. prepends [DRAFT] to rails application log entries
+#
 class govuk::apps::content_store(
-  $port = 3068
+  $port = 3068,
+  $enable_running_in_draft_mode = false,
 ) {
   govuk::app { 'content-store':
     app_type           => 'rack',
@@ -8,5 +27,34 @@ class govuk::apps::content_store(
     vhost_ssl_only     => true,
     health_check_path  => '/healthcheck',
     log_format_is_json => true,
+  }
+
+  if $enable_running_in_draft_mode {
+    $draft_content_store_port = 3100
+    $app_name = 'draft-content-store'
+    $app_domain = hiera('app_domain')
+
+    govuk::app::nginx_vhost { $app_name:
+      vhost    => "${app_name}.${app_domain}",
+      app_port => $draft_content_store_port,
+    }
+
+    Govuk::App::Envvar {
+      app            => $app_name,
+      notify_service => false,
+    }
+
+    govuk::app::envvar {
+      'GOVUK_APP_NAME':
+        value => $app_name;
+      'GOVUK_APP_LOGROOT':
+        value => "/var/log/${app_name}";
+      'DRAFT':
+        value => '1';
+      'PORT':
+        value => $draft_content_store_port;
+      'MONGO_DB_NAME':
+        value => 'draft_content_store_development';
+    }
   }
 }
