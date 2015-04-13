78631d3c705eab582106c19e49b44e465b9b0eb6
Vinay Patel <mail@vinayvinay.com>
2015-04-13 13:27:02 +0530

    Move errbit configuration to ENV
    https://trello.com/c/HfNnOLFJ
    https://github.com/errbit/errbit/blob/master/docs/configuration.md
    
    As part of syncing alphagov Errbit with upstream,
    we're moving custom configuration to ENV vars.

--- modules/govuk/manifests/apps/errbit.pp
+++ modules/govuk/manifests/apps/errbit.pp
@@ -1,5 +1,20 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::apps::errbit( $port = 3029 ) {
+# == Class: govuk::apps::errbit
+#
+# Configures Errbit, the open source error catcher that's Airbrake API compliant
+#
+# === Parameters
+#
+# [*port*]
+#   The port that Errbit is served on.
+#   Default: 3029
+#
+# [*errbit_email_from*]
+#   The from address used when errbit notifes us about an error
+#
+class govuk::apps::errbit(
+  $port = 3029,
+  $errbit_email_from = 'errbit@example.com',
+) {
   govuk::app { 'errbit':
     app_type           => 'rack',
     port               => $port,
@@ -7,5 +22,14 @@ class govuk::apps::errbit( $port = 3029 ) {
     health_check_path  => '/healthcheck',
     log_format_is_json => true,
     asset_pipeline     => true,
+  }
+
+  Govuk::App::Envvar {
+    app => 'errbit',
+  }
+
+  govuk::app::envvar {
+    'ERRBIT_EMAIL_FROM':
+      value => $errbit_email_from;
   }
 }
