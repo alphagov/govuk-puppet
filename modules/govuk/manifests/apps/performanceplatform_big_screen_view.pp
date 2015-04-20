c627bdbd9b3e05381ff1d53bd7d234fd861fc4aa
Timothy Mower <timothy.mower@gmail.com>
2015-04-20 14:24:50 +0100

    Add performance platform bigscreen view
    This is a purely static app used for displaying big screen views of
    performance platform dashboards. Following the approach taken for
    performance platform notifier, just the parts of govuk::app that are
    required for a static vhost are used.
    
    The port number we specify is not actually used - however the current
    nginx vhost define requires it is specified, and the logic requiring an
    app_proxy to be present is very intertwined with a standard vhost.
    
    The reason static hosting works like this is because the proxy vhost
    first tries looking for files in the document root, if these do not
    exist it will send to the upstream proxy

---
+++ modules/govuk/manifests/apps/performanceplatform_big_screen_view.pp
@@ -0,0 +1,27 @@
+# == Class: govuk::apps::performanceplatform_big_screen_view
+#
+# Big screen view displays a view of dashboards on the performance platform
+# optimised for big screens
+#
+# === Parameters
+class govuk::apps::performanceplatform_big_screen_view (
+  $enabled = false,
+) {
+  include govuk::deploy
+
+  if $enabled {
+    $app_domain = hiera('app_domain')
+    $app_name = 'performanceplatform-big-screen-view'
+    $vhost_full = "${app_name}.${app_domain}"
+    govuk::app::package { $app_name:
+      ensure     => present,
+      vhost_full => $vhost_full,
+    }
+    govuk::app::nginx_vhost { $app_name:
+      ensure   => present,
+      vhost    => $vhost_full,
+      ssl_only => true,
+      app_port => 3058,
+    }
+  }
+}
