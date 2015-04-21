354d62bc2bc795781717d5cfee716e590642d2d4
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-04-21 15:30:15 +0100

    Remove ruby cleanup FIXME

--- modules/govuk_rbenv/manifests/cleanup.pp
+++ modules/govuk_rbenv/manifests/cleanup.pp
@@ -19,12 +19,6 @@ class govuk_rbenv::cleanup {
       ensure => absent,
     }
   }
-  # FIXME: remove this version once cleaned up from all servers.
-  if ! defined(Rbenv::Version['2.0.0-p451']) {
-    rbenv::version { '2.0.0-p451':
-      ensure => absent,
-    }
-  }
   if ! defined(Rbenv::Version['2.0.0-p594']) {
     rbenv::version { '2.0.0-p594':
       ensure => absent,
