2348e08f3da7a2de8ebbc4a7164805ec9606c6cc
Vinay Patel <mail@vinayvinay.com>
2015-04-22 16:40:44 +0530

    Extract out draft content store setup into a class
    https://trello.com/c/2ZsPnGKo
    
    setting up `Govuk::App::Envvar` defaults was becoming confusing
    when this logic was in `govuk::apps::content_store`. a separate
    class ensures that we're setting up the correct defaults for that
    class's scope.
    
    this change enables us to remove a parameter, and selectively
    enable draft mode in development by simply including that class
    only in s_development.pp instead.

--- modules/govuk/manifests/apps/content_store.pp
+++ modules/govuk/manifests/apps/content_store.pp
@@ -15,21 +15,10 @@
 #   to be 'content_store_development'.
 #   Default: 'content_store_production'
 #
-# [*enable_running_in_draft_mode*]
-#   Intended to be used only during development.
-#   Enables running content-store for storing draft content.
-#   In draft mode content-store:
-#
-#   1. runs on a different port: 3100
-#   2. writes to a different mongo database: draft_content_store_development
-#   3. is served via a different nginx vhost: draft-content-store.dev.gov.uk
-#   4. prepends [DRAFT] to rails application log entries
-#
 class govuk::apps::content_store(
   $port = 3068,
   $mongodb_nodes,
   $mongodb_name = 'content_store_production',
-  $enable_running_in_draft_mode = false,
 ) {
   govuk::app { 'content-store':
     app_type           => 'rack',
@@ -47,44 +36,6 @@ class govuk::apps::content_store(
       app     => 'content-store',
       varname => 'MONGODB_URI',
       value   => "mongodb://${mongodb_nodes_string}/${mongodb_name}",
-    }
-  }
-
-  if $enable_running_in_draft_mode {
-    $draft_content_store_port = 3100
-    $app_name = 'draft-content-store'
-    $app_domain = hiera('app_domain')
-
-    govuk::app::nginx_vhost { $app_name:
-      vhost    => "${app_name}.${app_domain}",
-      app_port => $draft_content_store_port,
-    }
-
-    # Necessary because we're not creating a govuk::app instance for this.
-    file { ["/etc/govuk/${app_name}", "/etc/govuk/${app_name}/env.d"]:
-      ensure  => directory,
-      purge   => true,
-      recurse => true,
-      force   => true,
-    }
-
-    Govuk::App::Envvar {
-      app            => $app_name,
-      notify_service => false,
-      require        => File["/etc/govuk/${app_name}/env.d"],
-    }
-
-    govuk::app::envvar {
-      'GOVUK_APP_NAME':
-        value => $app_name;
-      'GOVUK_APP_LOGROOT':
-        value => "/var/log/${app_name}";
-      'DRAFT':
-        value => '1';
-      'PORT':
-        value => $draft_content_store_port;
-      'MONGODB_URI':
-        value => 'mongodb://localhost/draft_content_store_development';
     }
   }
 }
---
+++ modules/govuk/manifests/apps/content_store/enable_running_in_draft_mode.pp
@@ -0,0 +1,49 @@
+# == Class govuk::apps::content_store::enable_running_in_draft_mode
+#
+# Enables running content-store for storing draft content.
+#
+# Intended to be used only in development.
+# In draft mode content-store:
+#
+# 1. runs on a different port: 3100
+# 2. writes to a different mongo database: draft_content_store_development
+# 3. is served via a different nginx vhost: draft-content-store.dev.gov.uk
+# 4. prepends [DRAFT] to rails application log entries
+#
+class govuk::apps::content_store::enable_running_in_draft_mode() {
+  $draft_content_store_port = 3100
+  $app_name = 'draft-content-store'
+  $app_domain = hiera('app_domain')
+
+  govuk::app::nginx_vhost { $app_name:
+    vhost    => "${app_name}.${app_domain}",
+    app_port => $draft_content_store_port,
+  }
+
+  # Necessary because we're not creating a govuk::app instance for this.
+  file { ["/etc/govuk/${app_name}", "/etc/govuk/${app_name}/env.d"]:
+    ensure  => directory,
+    purge   => true,
+    recurse => true,
+    force   => true,
+  }
+
+  Govuk::App::Envvar {
+    app            => $app_name,
+    notify_service => false,
+    require        => File["/etc/govuk/${app_name}/env.d"],
+  }
+
+  govuk::app::envvar {
+    'GOVUK_APP_NAME':
+      value => $app_name;
+    'GOVUK_APP_LOGROOT':
+      value => "/var/log/${app_name}";
+    'DRAFT':
+      value => '1';
+    'PORT':
+      value => $draft_content_store_port;
+    'MONGODB_URI':
+      value => 'mongodb://localhost/draft_content_store_development';
+  }
+}
