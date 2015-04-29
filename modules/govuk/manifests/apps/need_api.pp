cadf2e3e1c8a10654f576433d24a1e5e5fb9aa6b
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-04-29 14:45:12 +0100

    Add errbit environment variables for need-api

--- modules/govuk/manifests/apps/need_api.pp
+++ modules/govuk/manifests/apps/need_api.pp
@@ -1,5 +1,27 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::apps::need_api( $port = 3052 ) {
+# == Class: govuk::apps::need_api
+#
+# An application to maintain all the user needs for GOV.UK.  This presents an
+# API for managing them.  See maslow for the UI that consumes this.
+#
+# === Parameters
+#
+# [*port*]
+#   The port that publishing API is served on.
+#   Default: 3093
+#
+# [*errbit_api_key*]
+#   Errbit API key used by airbrake
+#   Default: ''
+#
+# [*errbit_environment_name*]
+#   Errbit environment name, one of preview, staging or production
+#   Default: ''
+#
+class govuk::apps::need_api(
+  $port = 3052,
+  $errbit_api_key = '',
+  $errbit_environment_name = '',
+) {
   govuk::app { 'need-api':
     app_type           => 'rack',
     port               => $port,
@@ -12,5 +34,15 @@ class govuk::apps::need_api( $port = 3052 ) {
     logfile => '/var/apps/need-api/log/organisation_import.json.log',
     fields  => {'application' => 'need-api'},
     json    => true,
+  }
+
+  Govuk::App::Envvar {
+    app => 'need-api',
+  }
+  govuk::app::envvar {
+    'ERRBIT_API_KEY':
+      value => $errbit_api_key;
+    'ERRBIT_ENVIRONMENT_NAME':
+      value => $errbit_environment_name;
   }
 }
