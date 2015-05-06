c946a5bc41c6cfef8f35c73174a9ba3f74f76203
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2015-05-06 12:50:37 +0100

    Change a double quote to a single quote to fix the build
    - This was my fault for not checking thoroughly enough when merging
      (and tests not running on forks).

--- modules/users/manifests/stuartgale.pp
+++ modules/users/manifests/stuartgale.pp
@@ -3,6 +3,6 @@ class users::stuartgale {
   govuk::user { 'stuartgale':
     fullname => 'Stuart Gale',
     email    => 'stuart.gale@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMlIGdH3hOQ8kz+gZtIsp93DDdrd3iQFlVJc/cAd5i7+TLnJ+jQjb7A7TGttBa8IVtTgxOtZGm6ER4dJPUR55/yGU24naHTafGp/OfTiKvRg7DpN4SQFqNy2g8jesnZdV7e9NnUGfFnqduf9UcreumIYHPSALvFbAaiyTzbUrD3B3kCnYPp3NZjVRur5PKZSHhf/3Mr4vWWqp8wr6JS/HHK+UMBdLqE456uDPbRnX+/TF42BNDZlVHHNwWdZ4xrnQLIl9cfamYOjPwYb13tnSFf8+gsf9nXZcchBARYPannxOCq7MxfbropLN//AMOn/vc1cCGUNlVcL4FKkcSlwIr",
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMlIGdH3hOQ8kz+gZtIsp93DDdrd3iQFlVJc/cAd5i7+TLnJ+jQjb7A7TGttBa8IVtTgxOtZGm6ER4dJPUR55/yGU24naHTafGp/OfTiKvRg7DpN4SQFqNy2g8jesnZdV7e9NnUGfFnqduf9UcreumIYHPSALvFbAaiyTzbUrD3B3kCnYPp3NZjVRur5PKZSHhf/3Mr4vWWqp8wr6JS/HHK+UMBdLqE456uDPbRnX+/TF42BNDZlVHHNwWdZ4xrnQLIl9cfamYOjPwYb13tnSFf8+gsf9nXZcchBARYPannxOCq7MxfbropLN//AMOn/vc1cCGUNlVcL4FKkcSlwIr',
   }
 }
