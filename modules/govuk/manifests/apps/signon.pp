alphagov/govuk-puppet

commit 95c500f03e296df9816c5a73da6faf6dda3fb923

Add DEVISE_SECRET_KEY envvar for Signon

modules/govuk/manifests/apps/signon.pp
@@ -1,5 +1,9 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::apps::signon( $port = 3016, $enable_procfile_worker = true) {
+class govuk::apps::signon(
+  $port = 3016,
+  $enable_procfile_worker = true,
+  $devise_secret_key = undef,
+) {
   govuk::app { 'signon':
     app_type           => 'rack',
     port               => $port,
@@ -14,5 +18,12 @@ class govuk::apps::signon( $port = 3016, $enable_procfile_worker = true) {
 
   govuk::procfile::worker {'signon':
     enable_service => $enable_procfile_worker,
+  }
+
+  if $devise_secret_key != undef {
+    govuk::app::envvar { "${title}-DEVISE_SECRET_KEY":
+      varname => 'DEVISE_SECRET_KEY',
+      value   => $devise_secret_key,
+    }
   }
 }

hieradata/vagrant_credentials.yaml
@@ -41,6 +41,7 @@ govuk::apps::policy_publisher::db::password: 'ub5r2hgmtWQp5ESJmJUIjQ4V2CYMF2ev9x
 govuk::apps::release::db::mysql_release: 'AlxLdicrdDEx20Yekkn4AXGAG2ksIQU1'
 govuk::apps::router_api::secret_key_base: '05ddf7c150a3c850705c86b270895d92a71779cdee752077c8d76d89fb657b018043e918bfc5abe8b914c1094158d9ffbb2264aa1301da5d2e6dde0995afd65d'
 govuk::apps::search_admin::db::mysql_search_admin: 'KrafvoginjuxwycjiwegFaicsyecvi'
+govuk::apps::signon::devise_secret_key: '1e0310b9f18fc6ba34145829addcfd3f100c91f12e31ce071bd582981d44fc6ee1bd9a899e05e7cea9189aa54b6132eb36f4ade9c17b5535ba13bf5909b2a79e'
 govuk::apps::signon::db::mysql_signonotron: 'Yo68brC26TBdCf2NzCmwszv9UHtqbH8G'
 govuk::apps::stagecraft::postgresql_db::password: 'stagecraft_postgresql_password'
 govuk::apps::tariff_api::db::mysql_tariff_api: 'K6dyqfAJoXyUu5l2EQOu3ZAnqnVBEc6H'

commit 2e1e06905c369e2a08e9ab5d1ca4d6a84bffaef1

Fix test errors introduced in 95c500f

modules/govuk/manifests/apps/signon.pp
@@ -22,6 +22,7 @@ class govuk::apps::signon(
 
   if $devise_secret_key != undef {
     govuk::app::envvar { "${title}-DEVISE_SECRET_KEY":
+      app     => 'signon',
       varname => 'DEVISE_SECRET_KEY',
       value   => $devise_secret_key,
     }