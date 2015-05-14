0d8c4ef7681dd194e7bf61208078116e2729bde6
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-05-14 17:47:39 +0100

    Run shellcheck on `sync-assets.sh`
    - Use double quotes to prevent potential weird behaviour
    - Use $((..)) rather than $(expr foo)
    - Use double quotes around $@ to prevent * from being expanded to
      wildcard

--- modules/govuk/files/node/s_asset_base/sync-assets.sh
+++ modules/govuk/files/node/s_asset_base/sync-assets.sh
@@ -2,23 +2,24 @@
 # script to rsync files locally
 set -eu
 
-APPNAME=$(basename $0)
+APPNAME=$(basename "$0")
 REMOTEDIR=$1
 LOCALDIR=$2
-DIRECTORIES=${@:3}
+DIRECTORIES="${@:3}"
 RSYNCOPTS="--delete -a"
 STARTEPOCH=$(date +'%s')
+
 for DIR in $DIRECTORIES
 do
   STARTRSYNC=$(date +'%s')
-  rsync $RSYNCOPTS $REMOTEDIR/$DIR/. $LOCALDIR/$DIR
+  rsync "$RSYNCOPTS" "$REMOTEDIR/$DIR/." "$LOCALDIR/$DIR"
   FINISHRSYNC=$(date +'%s')
-  TOTALRSYNC=$(expr $FINISHRSYNC - $STARTRSYNC)
+  TOTALRSYNC=$((FINISHRSYNC-STARTRSYNC))
   RSYNCMESSAGE="$APPNAME: $DIR rsynced in T:$TOTALRSYNC"
-  logger -i -t $APPNAME $RSYNCMESSAGE
+  logger -i -t "$APPNAME" "$RSYNCMESSAGE"
 done
-FINISHEPOCH=$(date +'%s')
-TOTALTIME=$(expr $FINISHEPOCH - $STARTEPOCH)
-MESSAGE="$APPNAME: $DIRECTORIES rsynced in T:$TOTALTIME"
-logger -i -t $APPNAME $MESSAGE
 
+FINISHEPOCH=$(date +'%s')
+TOTALTIME=$((FINISHEPOCH-STARTEPOCH))
+MESSAGE="$APPNAME: $DIRECTORIES rsynced in T:$TOTALTIME"
+logger -i -t "$APPNAME" "$MESSAGE"
