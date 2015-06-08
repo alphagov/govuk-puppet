6557db9ee02e86b1b6f2a251a6391145ee3581f3
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2015-06-08 12:55:36 +0100

    Allow passing `$ensure` to collecd::plugin::raindrops
    This lets us remove the plugin using a parameter.

--- modules/collectd/manifests/plugin/raindrops.pp
+++ modules/collectd/manifests/plugin/raindrops.pp
@@ -1,6 +1,7 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 define collectd::plugin::raindrops(
-  $port
+  $port,
+  $ensure = 'present',
 ) {
   include collectd::plugin::curl
 
@@ -8,6 +9,7 @@ define collectd::plugin::raindrops(
   validate_re($title, '^[\w\-]+$')
 
   @collectd::plugin { "raindrops-${title}":
+    ensure  => $ensure,
     content => template('collectd/etc/collectd/conf.d/raindrops.conf.erb'),
     require => Class['collectd::plugin::curl'],
   }
