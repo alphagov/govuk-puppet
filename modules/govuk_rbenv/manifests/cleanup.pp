b51a5bd7422aeaffd22299dd964a311bb4f0e7a6
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-06-08 12:25:14 +0100

    Remove rbenv ruby 2.0.0-p594 and 2.2.1 everywhere
    They're no longer in use.

--- modules/govuk_rbenv/manifests/cleanup.pp
+++ modules/govuk_rbenv/manifests/cleanup.pp
@@ -19,6 +19,7 @@ class govuk_rbenv::cleanup {
       ensure => absent,
     }
   }
+  # FIXME: remove this version once cleaned up from all servers.
   if ! defined(Rbenv::Version['2.0.0-p594']) {
     rbenv::version { '2.0.0-p594':
       ensure => absent,
@@ -56,6 +57,7 @@ class govuk_rbenv::cleanup {
     }
   }
 
+  # FIXME: remove this version once cleaned up from all servers.
   if ! defined(Rbenv::Version['2.2.1']) {
     rbenv::version { '2.2.1':
       ensure => absent,
