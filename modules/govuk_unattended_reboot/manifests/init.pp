b697be00a996cc30e2a813384d36dcf47a885da4
Matt Bostock <matt@mattbostock.com>
2015-06-17 16:41:57 +0100

    Use require to specify dependencies
    The dependency chain, specified using `->`, was causing a dependency
    cycle:
    
        ==> jumpbox-1.management: Error: Could not apply complete catalog: Found 1 dependency cycle:
        ==> jumpbox-1.management: (Cron[unattended-reboot] => Class[Unattended_reboot] => File[/etc/unattended-reboot/check] => File[/etc/unattended-reboot/check/01_alerts] => Class[Unattended_reboot] => Cron[unattended-reboot])
    
    To make the dependencies easier to reason about and to fix the
    dependency cycle, use 'require' instead of the `->` arrow.

--- modules/govuk_unattended_reboot/manifests/init.pp
+++ modules/govuk_unattended_reboot/manifests/init.pp
@@ -29,35 +29,42 @@ class govuk_unattended_reboot (
   $node_class_search_phrase = regsubst($::govuk_node_class, '_', '-')
   $icinga_url = "https://nagios.cluster/cgi-bin/icinga/status.cgi?search_string=%5E${node_class_search_phrase}-[0-9]&allunhandledproblems&jsonoutput"
 
-  file { '/usr/local/bin/check_icinga':
-    ensure => $file_ensure,
-    mode   => '0755',
-    owner  => 'root',
-    group  => 'root',
-    source => 'puppet:///modules/govuk_unattended_reboot/usr/local/bin/check_icinga.rb',
-  } ->
   file { [ $config_directory, $check_scripts_directory ]:
     ensure => $directory_ensure,
     mode   => '0755',
     owner  => 'root',
     group  => 'root',
-  } ->
+  }
+
   file { "${check_scripts_directory}/00_safety":
     ensure  => $file_ensure,
     mode    => '0755',
     owner   => 'root',
     group   => 'root',
     content => template("govuk_unattended_reboot${check_scripts_directory}/00_safety.erb"),
-  } ->
+    require => File[$check_scripts_directory],
+  }
+
   file { "${check_scripts_directory}/01_alerts":
     ensure  => $file_ensure,
     mode    => '0755',
     owner   => 'root',
     group   => 'root',
     content => template("govuk_unattended_reboot${check_scripts_directory}/01_alerts.erb"),
-  } ->
+    require => [ File[$check_scripts_directory], File['/usr/local/bin/check_icinga'] ],
+  }
+
+  file { '/usr/local/bin/check_icinga':
+    ensure => $file_ensure,
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/govuk_unattended_reboot/usr/local/bin/check_icinga.rb',
+  }
+
   class { '::unattended_reboot':
     enabled                 => $enabled,
     check_scripts_directory => $check_scripts_directory,
+    require                 => [ File["${check_scripts_directory}/00_safety"], File["${check_scripts_directory}/01_alerts"] ],
   }
 }
