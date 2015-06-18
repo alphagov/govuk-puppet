287c51d10abdb1c6a22cd397b0955fb43b2bc5f5
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-06-18 09:12:01 +0100

    Move rkhunter config to ERB template
    It's easier to manage a list of disabled checks when it's
    an array.

--- modules/rkhunter/manifests/config.pp
+++ modules/rkhunter/manifests/config.pp
@@ -1,14 +1,25 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class rkhunter::config {
 
+  $disabled_tests = [
+    'apps',
+    'deleted_files',
+    'filesystem',
+    'hidden_procs',
+    'packet_cap_apps',
+    'properties',
+    'running_procs',
+    'suspscan',
+  ]
+
   file { '/etc/default/rkhunter':
     ensure => 'present',
     source => 'puppet:///modules/rkhunter/etc/default/rkhunter',
   }
 
   file { '/etc/rkhunter.conf.local':
-    ensure => 'present',
-    source => 'puppet:///modules/rkhunter/etc/rkhunter.conf.local',
+    ensure  => 'present',
+    content => template('rkhunter/rkhunter.conf.local.erb'),
   }
 
 }
