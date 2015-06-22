f06a03b6503d97d66be27553cb23a7caf74a0e26
Matt Bostock <matt@mattbostock.com>
2015-06-22 10:36:07 +0100

    Don't refer to backend in the API vDC
    Remove mentions of 'backend' in the s_api_lb node definition. Since this
    class of machines lives in the API vDC, I think it's unhelpful to refer
    to 'backend' given that we also have a Backend vDC.

--- modules/govuk/manifests/node/s_api_lb.pp
+++ modules/govuk/manifests/node/s_api_lb.pp
@@ -1,15 +1,15 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::node::s_api_lb (
-  $api_backend_servers,
-  $content_store_backend_servers,
-  $search_backend_servers,
+  $api_servers,
+  $content_store_servers,
+  $search_servers,
 ) {
   include govuk::node::s_base
   include loadbalancer
 
-  validate_array($api_backend_servers)
-  validate_array($content_store_backend_servers)
-  validate_array($search_backend_servers)
+  validate_array($api_servers)
+  validate_array($content_store_servers)
+  validate_array($search_servers)
 
   loadbalancer::balance {
     [
@@ -18,7 +18,7 @@ class govuk::node::s_api_lb (
       'metadata-api',
       'stagecraft',
     ]:
-      servers       => $api_backend_servers,
+      servers       => $api_servers,
       internal_only => true;
   }
 
@@ -26,7 +26,7 @@ class govuk::node::s_api_lb (
     [
       'content-store',
     ]:
-      servers       => $content_store_backend_servers,
+      servers       => $content_store_servers,
       internal_only => true;
   }
 
@@ -38,7 +38,7 @@ class govuk::node::s_api_lb (
       # cluster running in backend VDC.
       'search',
     ]:
-      servers       => $search_backend_servers,
+      servers       => $search_servers,
       https_only    => false, # Necessary for the router to fetch sitemaps.
       internal_only => true;
   }
