7115906aadfd718dafc05242f7cbeb4e43a87b70
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-06-22 16:00:01 +0100

    Remove collections-api everywhere.
    This app is no longer used, and is being retired.

--- modules/govuk/manifests/apps/collections_api.pp
+++ modules/govuk/manifests/apps/collections_api.pp
@@ -5,6 +5,7 @@ class govuk::apps::collections_api(
 ) {
   if $enabled {
     govuk::app { 'collections-api':
+      ensure             => absent,
       app_type           => 'rack',
       port               => $port,
       vhost_ssl_only     => true,
