743c96c4a83116db89539cf40a533d5d9d3f3e2e
Matt Bostock <matt@mattbostock.com>
2015-06-23 09:12:04 +0100

    Prefix contacts-frontend vhost on draft_frontend
    Prefix the virtual host for contacts-frontend with 'draft-' on the
    draft_frontend machines.

--- modules/govuk/manifests/apps/contacts_frontend.pp
+++ modules/govuk/manifests/apps/contacts_frontend.pp
@@ -1,5 +1,6 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::apps::contacts_frontend (
+  $vhost,
   $port = 3074,
 ) {
   govuk::app { 'contacts-frontend':
@@ -9,5 +10,6 @@ class govuk::apps::contacts_frontend (
     log_format_is_json    => true,
     asset_pipeline        => true,
     asset_pipeline_prefix => 'contacts-frontend',
+    vhost                 => $vhost,
   }
 }
