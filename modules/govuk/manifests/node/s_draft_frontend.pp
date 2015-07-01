964fb55caf06a5178d23eec7284d66077d9a36c2
Matt Bostock <matt@mattbostock.com>
2015-07-01 16:56:43 +0100

    Override location of static on draft_frontend
    Set the location of the static application on the draft_frontend machine
    class by setting an environment variable that will be picked up by Plek,
    which our applications use to discover the URIs of other applications.
    
    We run a separate instance of the static application on the
    draft_frontend machines because we set some header text when serving
    draft content, which is rendered by static:
    
    gds/puppet@d5a72ead8d

--- modules/govuk/manifests/node/s_draft_frontend.pp
+++ modules/govuk/manifests/node/s_draft_frontend.pp
@@ -10,12 +10,20 @@
 # [*draft_content_store_uri*]
 #   URI of the content-store instance used to serve draft content.
 #
+# [*draft_static_uri*]
+#   URI of the static application used when serving draft content.
+#
 class govuk::node::s_draft_frontend(
   $draft_content_store_uri,
+  $draft_static_uri,
 ) inherits govuk::node::s_base {
   include govuk::node::s_frontend
 
   govuk::envvar { 'PLEK_SERVICE_CONTENT_STORE_URI':
     value => $draft_content_store_uri,
+  }
+
+  govuk::envvar { 'PLEK_SERVICE_STATIC_URI':
+    value => $draft_static_uri,
   }
 }
