daf56ff5f417c73e53a53db27454317d0b02e6a8
Matt Bostock <matt@mattbostock.com>
2015-07-02 12:05:52 +0100

    Allow connections to port 8443 on API LB
    Open port 8443 for HTTPS connections from the internal vSE Draft API
    load balancer on 10.7.2.2.
    
    This was omitted in 86ad023.

--- modules/govuk/manifests/node/s_api_lb.pp
+++ modules/govuk/manifests/node/s_api_lb.pp
@@ -41,6 +41,10 @@ class govuk::node::s_api_lb (
       https_only    => true,
       https_port    => 8443,
   }
+  @ufw::allow { 'allow-https-8443-from-draft-api-internal-lb':
+    port => 8443,
+    from => '10.7.2.2',
+  }
 
   loadbalancer::balance {
     [
