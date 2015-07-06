0c54de3374f598be1322c7c6d2d16f4623fab960
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-07-06 13:42:09 +0100

    Remove unnecessary puppet resource
    This was added in May 2014 with a note saying it could be
    removed when run everywhere.

--- modules/phantomjs/manifests/init.pp
+++ modules/phantomjs/manifests/init.pp
@@ -6,9 +6,4 @@ class phantomjs {
     ensure  => '1.9.7-0~ppa1',
     require => Class['govuk::ppa'],
   }
-
-  # FIXME: remove when this has been run everywhere
-  file { '/usr/local/bin/phantomjs':
-    ensure  => absent,
-  }
 }
