1bc0290e65e013a3962d162e6a2aef7c1e8a0a2d
Matt Bostock <matt@mattbostock.com>
2015-07-06 22:18:00 +0100

    Remove s_draft_content_store::draft_router_api_uri
    release_140 of content-store has been deployed to Production, so the
    `PLEK_SERVICE_ROUTER_API_URI` environment variable can be removed.
    
    content-store will instead rely on Plek version 1.11.0 or above to pick
    up the `PLEK_HOSTNAME_PREFIX` environment variable when communicating
    with draft applications.

--- modules/govuk/manifests/node/s_draft_content_store.pp
+++ modules/govuk/manifests/node/s_draft_content_store.pp
@@ -6,25 +6,11 @@
 #
 # Includes `govuk::node::s_content_store`, upon which this node definition is based.
 #
-# === Parameters
-#
-# [*draft_router_api_uri*]
-#   URI of the router-api application used to register routes for draft
-#   content.
-#
-class govuk::node::s_draft_content_store(
-  $draft_router_api_uri,
-) inherits govuk::node::s_base {
+class govuk::node::s_draft_content_store() inherits govuk::node::s_base {
   include govuk::node::s_content_store
 
   govuk::envvar { 'PLEK_HOSTNAME_PREFIX':
     value => 'draft-',
-  }
-
-  # FIXME Remove this once content-store release_140 is deployed to Production
-  # See: https://github.com/alphagov/plek/pull/34
-  govuk::envvar { 'PLEK_SERVICE_ROUTER_API_URI':
-    value => $draft_router_api_uri,
   }
 }
 
