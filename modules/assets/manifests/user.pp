b6a53af917ad11ef8f2946a1fb30f2e19f23244f
Matt Bostock <matt@mattbostock.com>
2015-07-06 23:12:53 +0100

    Add documentation for assets::user class

--- modules/assets/manifests/user.pp
+++ modules/assets/manifests/user.pp
@@ -1,4 +1,7 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: assets::user
+#
+# Configures the 'assets' user and group.
+#
 class assets::user {
   group { 'assets':
     ensure => 'present',
