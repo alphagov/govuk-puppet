49adc677e48dd35255e502aa6b5862a6a329e407
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-07-07 13:23:32 +0100

    Remove leaver mattcottingham
    Leaver ticket https://www.pivotaltracker.com/story/show/97278710

--- modules/users/manifests/mattcottingham.pp
+++
@@ -1,8 +0,0 @@
-# Creates the mattcottingham user
-class users::mattcottingham {
-  govuk::user { 'mattcottingham':
-    fullname => 'Matt Cottingham',
-    email    => 'matt.cottingham@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNf2T4vI0Fq1DK/hrv54fN7WR8IM32ItDsiRcOmsq807LXZkxn0Hbua8NP1KjtUt1Ks7kxQiXYp32S/LNi0G/Sr48BqFhCPFpEtOUexrX+wK+ndXNwP3XygTPKbgoPV/IXyDdjSvqOVmQKbWFGYCMn6+nGA+JaCbvt8Ink7lpRGLrf8W73rbcFcPCNuOtWvMT+WSspcqs9IifvwWaHDFX2Uj8DsE3rZcaeoxXPXNxc2mZ9/tKee6Ceg0Acmsywn/YsEwAv55ng73WMUE5y5KximZwGKBIQjjILTryPd7euztCFr5Y4UMEDbDz52ovmD+jtrI2hxRygAimTshJ5jeCb matt.cottingham@digital.cabinet-office.gov.uk',
-  }
-}
