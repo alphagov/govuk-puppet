28f4a8ae88865948b490caffab5529be25945a30
Matt Bostock <matt@mattbostock.com>
2015-07-08 23:26:43 +0100

    Rely on PLEK_HOSTNAME_PREFIX for draft_frontend
    On draft_frontend machines, remove the `PLEK_SERVICE_XYZ_URI`
    environment variables in favour of `PLEK_HOSTNAME_PREFIX`, which has the
    same desired effect of prefixing `draft-` to application hostnames
    returned by Plek.
    
    `PLEK_SERVICE_STATIC_URI` was problematic because when using it, Plek
    does not respect the `force_http` parameter [1] that Static uses when
    registering itself as a backend with router-api at deploy time. Since
    router-api only accepts HTTP URLs as backends and not HTTPS as defined
    by `PLEK_SERVICE_STATIC_URI` here, Static fails to deploy as router-api
    rejects the HTTPS URL:
    
    https://deploy.preview.alphagov.co.uk/job/Preview_Deploy_Matt_Draft_Applications/25/console
    
        *** [err :: draft-frontend-1.frontend.production] rake aborted!
        *** [err :: draft-frontend-1.frontend.production] GdsApi::HTTPClientError: url: https://draft-router-api.preview.alphagov.co.uk/backends/static
        *** [err :: draft-frontend-1.frontend.production] {"backend_id":"static","backend_url":"https://draft-static.preview.alphagov.co.uk/","errors":{"backend_url":["is not a valid HTTP URL"]}}
        <snip>
        *** [err :: draft-frontend-1.frontend.production] RestClient::UnprocessableEntity: 422 Unprocessable Entity
    
    All applications currently deployed to draft_frontend support Plek
    version 1.11.0 or later, which recognises `PLEK_HOSTNAME_PREFIX`.
    
    [1]: https://github.com/alphagov/plek/pull/38

--- modules/govuk/manifests/node/s_draft_frontend.pp
+++ modules/govuk/manifests/node/s_draft_frontend.pp
@@ -5,31 +5,10 @@
 #
 # Includes `govuk::node::s_frontend`, upon which this node definition is based.
 #
-# === Parameters
-#
-# [*draft_content_store_uri*]
-#   URI of the content-store instance used to serve draft content.
-#
-# [*draft_static_uri*]
-#   URI of the static application used when serving draft content.
-#
-class govuk::node::s_draft_frontend(
-  $draft_content_store_uri,
-  $draft_static_uri,
-) inherits govuk::node::s_base {
+class govuk::node::s_draft_frontend() inherits govuk::node::s_base {
   include govuk::node::s_frontend
 
   govuk::envvar { 'PLEK_HOSTNAME_PREFIX':
     value => 'draft-',
-  }
-
-  # FIXME Remove these once all apps deployed to these nodes support Plek
-  # v1.11.0 or later
-  # See: https://github.com/alphagov/plek/pull/34
-  govuk::envvar { 'PLEK_SERVICE_CONTENT_STORE_URI':
-    value => $draft_content_store_uri,
-  }
-  govuk::envvar { 'PLEK_SERVICE_STATIC_URI':
-    value => $draft_static_uri,
   }
 }
