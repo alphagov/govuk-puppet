e06fb1a7a6566668d15c4b80482bb1d612494dd2
Paul Martin <Paul.martin@digital.cabinet-office.gov.uk>
2015-07-10 15:04:42 +0100

    Remove unused parameter
    the assets_uploads_disk parameter is no longer used on s_asset_base.

--- modules/govuk/manifests/node/s_asset_base.pp
+++ modules/govuk/manifests/node/s_asset_base.pp
@@ -1,5 +1,5 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::node::s_asset_base ( $assets_uploads_disk = undef ) inherits govuk::node::s_base{
+class govuk::node::s_asset_base inherits govuk::node::s_base{
   include assets::user
   include clamav
 
