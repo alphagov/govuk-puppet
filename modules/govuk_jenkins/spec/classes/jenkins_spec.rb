24a257f4583c7718f617d3c3ebbbc2962af62825
Paul Martin <Paul.martin@digital.cabinet-office.gov.uk>
2015-07-16 17:47:44 +0100

    Template Jenkins config for Puppet
    This class is an attempt manage Jenkins through Puppet and Hiera. It's
    basic function is to generate the main Jenkins configuration file
    (config.xml) and restart the Jenkins service if there are any changes.
    
     - Sets up the oauth plugin so that Jenkins can authenticate with Github,
    and when a user logs in their permissions should already be set in the main
    config file so one does not need to go and manually set up their user in Jenkins.
    Unfortunately this has created yet another list of users to manage in Hiera.
    
     - Adds a bunch of standard plugin XML configuration files
    
    I also note there seems to be quite a few variables between Preview and
    Staging/Production Jenkins instances which will need to be factored in
    somehow.

--- modules/govuk_jenkins/spec/classes/jenkins_spec.rb
+++ modules/govuk_jenkins/spec/classes/jenkins_spec.rb
@@ -2,13 +2,22 @@ require_relative '../../../../spec_helper'
 
 describe 'govuk_jenkins', :type => :class do
   let(:ssh_dir) { '/var/lib/jenkins/.ssh' }
+  let(:github_client_id) { 'foo' }
+  let(:github_client_secret) { 'bar' }
 
   let(:params) {{
     :github_enterprise_cert => 'certcertcert',
   }}
 
+  let(:hiera_data) {{
+    'govuk_jenkins::config::github_client_id' => %w{ foo },
+    'govuk_jenkins::config::github_client_secret' => %w{ bar },
+  }}
+
   it { should contain_class('govuk_jenkins::ssh_key') }
+  it { should contain_class('govuk_jenkins::config') }
   it { should contain_file(ssh_dir).with_ensure('directory') }
+  it { should contain_file('/var/lib/jenkins/config.xml').with_content(/foo/) }
 
   it { should contain_user('jenkins').with_ensure('present') }
 end
