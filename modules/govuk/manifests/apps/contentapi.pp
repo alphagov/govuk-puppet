a8031c0280955ba3a197d3523ab872a784647de0
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-07-17 09:00:16 +0100

    Increase contentapi memory alerting thresholds.
    contentapi currently stabilises at a memory usage of around 3G. This
    means that the memory alerts are constantly firing. To prevent these
    simply being ignored, this raises the limits to reflect typcial usage.

--- modules/govuk/manifests/apps/contentapi.pp
+++ modules/govuk/manifests/apps/contentapi.pp
@@ -1,14 +1,19 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: govuk::apps::contentapi
+#
+# Configure the contentapi application - https://github.com/alphagov/govuk_content_api
+#
 class govuk::apps::contentapi (
   $port = 3022,
 ) {
 
   govuk::app { 'contentapi':
-    app_type           => 'rack',
-    port               => $port,
-    health_check_path  => '/vat-rates.json',
-    log_format_is_json => true,
-    nginx_extra_config => 'proxy_set_header API-PREFIX $http_api_prefix;',
+    app_type               => 'rack',
+    port                   => $port,
+    health_check_path      => '/vat-rates.json',
+    log_format_is_json     => true,
+    nginx_extra_config     => 'proxy_set_header API-PREFIX $http_api_prefix;',
+    nagios_memory_warning  => 3000000000,
+    nagios_memory_critical => 3500000000,
   }
 
 }
