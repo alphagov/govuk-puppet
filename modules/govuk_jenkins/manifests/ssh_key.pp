260b9eb88d97067a74d23a69da13cba8c7659f76
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-07-20 17:31:02 +0100

    Allow specifying an SSH keypair for jenkins user
    We're trying to automate as much as we can when creating an environment.
    Instead of having Puppet create the keypair which we then add to
    GitHub Enterprise, it's easier if we create the keypair beforehand
    and add it to Puppet and GitHub Enterprise at the same time.
    
    This allows us to bring up an environment in an automated way without
    needing to stop half way through to deal with SSH keys.
    
    If an SSH keypair is provided in hieradata Puppet will use that,
    otherwise it will create one as normal.

--- modules/govuk_jenkins/manifests/ssh_key.pp
+++ modules/govuk_jenkins/manifests/ssh_key.pp
@@ -1,9 +1,23 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk_jenkins::ssh_key {
+# == Class: govuk_jenkins::ssh_key
+#
+# Sets up an SSH key pair for the jenkins user.
+#
+# === Parameters:
+#
+# [*private_key*]
+#   A passwordless RSA private key generated with `ssh-keygen -t rsa`.
+#
+# [*public_key*]
+#   The public key that matches `$private_key`.
+#
+class govuk_jenkins::ssh_key (
+  $private_key = undef,
+  $public_key = undef,
+) {
   $home_dir = '/var/lib/jenkins'
   $ssh_dir = "${home_dir}/.ssh"
-  $private_key = "${ssh_dir}/id_rsa"
-  $public_key = "${ssh_dir}/id_rsa.pub"
+  $private_key_filename = "${ssh_dir}/id_rsa"
+  $public_key_filename = "${ssh_dir}/id_rsa.pub"
 
   file { $ssh_dir:
     ensure => directory,
@@ -12,14 +26,32 @@ class govuk_jenkins::ssh_key {
     group  => 'jenkins',
   }
 
-  exec { 'Creating key pair for jenkins':
-    command => "ssh-keygen -t rsa -C 'Provided by Puppet for jenkins' -N '' -f ${private_key}",
-    creates => $private_key,
-    require => [
-      User['jenkins'],
-      File[$ssh_dir],
-    ],
-    user    => 'jenkins',
+  if $private_key and $public_key {
+    file { $public_key_filename:
+      content => $public_key,
+      mode    => '0644',
+      owner   => 'jenkins',
+      group   => 'jenkins',
+      require => User['jenkins'],
+    }
+
+    file { $private_key_filename:
+      content => $private_key,
+      mode    => '0600',
+      owner   => 'jenkins',
+      group   => 'jenkins',
+      require => User['jenkins'],
+    }
+  } else {
+    exec { 'Creating key pair for jenkins':
+      command => "ssh-keygen -t rsa -C 'Provided by Puppet for jenkins' -N '' -f ${private_key_filename}",
+      creates => $private_key_filename,
+      require => [
+        User['jenkins'],
+        File[$ssh_dir],
+      ],
+      user    => 'jenkins',
+    }
   }
 
   package { 'keychain':
