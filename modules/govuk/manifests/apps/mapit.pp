b6e1be378792c78cff81dffb91fbdda3daa95d24
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-07-22 14:42:42 +0100

    Add govuk::apps::mapit class
    We're moving to deploying MapIt as a normal application. This change follows
    on from acaa35f14fcba1bc65a9c987ba34b240890b96d2 which added a new machine for
    mapit which will run in parallel with the existing mapit-server-* machines
    until we've finished the migration.
    
    Port 3108 has already been reserved in the development repo for MapIt:
    
        https://github.gds/gds/development/commit/e7587351f1fed1800872b036ffa467054aa26f9e
    
    We're defining the database in the same class as the application config
    because MapIt's database is on the same machine as the app. For most apps the
    database needs to be created on a different machine from the application
    config so needs to be in a separate class.
    
    MapIt is a Python/Django app so the app type is 'procfile'. We'll add a
    Procfile to our fork of the application repo.

---
+++ modules/govuk/manifests/apps/mapit.pp
@@ -0,0 +1,33 @@
+# == Class: govuk::apps::mapit
+##
+# === Parameters
+#
+# [*enabled*]
+#   Should the app exist?
+#
+# [*port*]
+#   What port should the app run on?
+#
+# [*db_password*]
+#   The password for the mapit postgresql role
+#
+class govuk::apps::mapit (
+  $enabled = false,
+  $port    = 3108,
+  $db_password,
+) {
+  if $enabled {
+    govuk::app { 'mapit':
+      app_type           => 'procfile',
+      port               => $port,
+      vhost_ssl_only     => true,
+      health_check_path  => '/',
+      log_format_is_json => false;
+    }
+
+    govuk_postgresql::db { 'mapit':
+      user       => 'mapit',
+      password   => $db_password,
+    }
+  }
+}
