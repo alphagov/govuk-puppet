85c0b7e204b2219f677b42f704fce5d732dd3c04
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-07-29 10:03:52 +0100

    Add auth-token to allow remote trigger of jobs
    These 2 Jenkins jobs need to be triggered remotely so that we can
    chain deployment jobs from existing staging.

--- modules/govuk_jenkins/manifests/job/deploy_app.pp
+++ modules/govuk_jenkins/manifests/job/deploy_app.pp
@@ -2,7 +2,16 @@
 #
 # Create a file on disk that can be parsed by jenkins-job-builder
 #
+# === Parameters
+#
+# [*auth_token*]
+#   Token which will allow this job to be triggered remotely
+#
+# [*ci_deploy_jenkins_api_key*]
+#   API key to download build artefacts from CI servers
+#
 class govuk_jenkins::job::deploy_app (
+  $auth_token = undef,
   $ci_deploy_jenkins_api_key = undef,
 ) {
   file { '/etc/jenkins_jobs/jobs/deploy_app.yaml':
--- modules/govuk_jenkins/manifests/job/deploy_puppet.pp
+++ modules/govuk_jenkins/manifests/job/deploy_puppet.pp
@@ -2,7 +2,14 @@
 #
 # Create a file on disk that can be parsed by jenkins-job-builder
 #
-class govuk_jenkins::job::deploy_puppet {
+# === Parameters
+#
+# [*auth_token*]
+#   Token which will allow this job to be triggered remotely
+#
+class govuk_jenkins::job::deploy_puppet (
+  $auth_token = undef,
+) {
   file { '/etc/jenkins_jobs/jobs/deploy_puppet.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/deploy_puppet.yaml.erb'),
