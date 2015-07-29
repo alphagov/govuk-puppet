d2f9754c309b0b252e940200a8f4725751b4f386
Matt Bostock <matt@mattbostock.com>
2015-07-29 10:32:03 +0100

    Allow govuk::gor to accept multiple targets
    Allow the `govuk::gor` class to accept multiple targets for traffic
    replay.
    
    We'll use this to replay traffic against the new Carrenza Staging
    environment.

--- modules/govuk/spec/classes/router_gor_spec.rb
+++ modules/govuk/spec/classes/router_gor_spec.rb
@@ -1,41 +1,51 @@
 require_relative '../../../../spec_helper'
 
 describe 'router::gor', :type => :class do
-  let(:staging_host) { 'replay-target' }
-  let(:staging_ip) { '127.0.0.1' }
+  let(:params) {{
+    :replay_targets => replay_targets,
+  }}
   let(:args_default) {{
     '-input-raw'          => 'localhost:7999',
     '-output-http-method' => %w{GET HEAD OPTIONS},
   }}
 
-  context 'default (disabled)' do
-    let(:params) {{
-      :staging_host => staging_host,
-      :staging_ip => staging_ip,
-    }}
+  context 'no targets defined (disabled)' do
+    let(:replay_targets) {{}}
 
-    it { should contain_host(staging_host).with_ensure('absent') }
-    it {
-      should contain_class('govuk::gor').with({
-        :enable => false,
-      })
-    }
+    it { should contain_class('govuk::gor').with({ :enable => false, }) }
   end
 
-  context '#enable_staging' do
-    let(:params) {{
-      :enable_staging => true,
-      :staging_host => staging_host,
-      :staging_ip => staging_ip,
+  context 'a target defined (enabled)' do
+    let(:target_host) { 'replay-target' }
+    let(:replay_targets) {{
+      target_host => { 'ip' => '127.0.0.1' }
     }}
 
-    it { should contain_host(staging_host).with_ensure('present') }
+    it { should contain_host(target_host).with_ensure('present') }
 
     it {
       should contain_class('govuk::gor').with(
         :enable => true,
-        :args           => args_default.merge({
-          '-output-http' => ["https://#{staging_host}"],
+        :args   => args_default.merge({
+          '-output-http' => ["https://#{target_host}"],
+        }),
+      )
+    }
+  end
+
+  context 'a multiple targets defined (enabled)' do
+    let(:target_host_a) { 'replay-target-a' }
+    let(:target_host_b) { 'replay-target-b' }
+    let(:replay_targets) {{
+      target_host_a => { 'ip' => '127.0.0.1' },
+      target_host_b => { 'ip' => '127.0.0.2' },
+    }}
+
+    it {
+      should contain_class('govuk::gor').with(
+        :enable => true,
+        :args   => args_default.merge({
+          '-output-http' => ["https://#{target_host_a}", "https://#{target_host_b}"],
         }),
       )
     }
--- modules/router/manifests/gor.pp
+++ modules/router/manifests/gor.pp
@@ -4,34 +4,20 @@
 #
 # === Parameters
 #
-# [*staging_host*]
-#   Hostname to replay traffic against
-#
-# [*staging_ip*]
-#   IP address for `staging_host` hostname
-#
-# [*enable_staging*]
-#   Boolean to determine if traffic will be replayed against Staging.
+# [*replay_targets*]
+#   Hash of targets to replay traffic against.
 #
 class router::gor (
-  $staging_host,
-  $staging_ip,
-  $enable_staging = false,
+  $replay_targets = {},
 ) {
-  validate_bool($enable_staging)
-  validate_string($staging_host)
-  validate_string($staging_ip)
+  validate_hash($replay_targets)
 
-  if $enable_staging and !is_ip_address($staging_ip) {
-    fail("Invalid IP address: ${staging_ip}")
-  }
-
-  if $enable_staging {
-    $gor_targets        = ["https://${staging_host}"]
-    $gor_hosts_ensure   = present
+  if $replay_targets == {} {
+    $enabled = false
+    $gor_hosts_ensure = absent
   } else {
-    $gor_targets        = []
-    $gor_hosts_ensure   = absent
+    $enabled = true
+    $gor_hosts_ensure = present
   }
 
   # FIXME: These "fake" host entries serve two purposes:
@@ -39,20 +25,22 @@ class router::gor (
   #   production, matches the hostname that we connect to.
   #   2. Prevents Gor/Go from performing DNS lookups, which occur once
   #   for *every* request/goroutine, and can be quite overwhelming.
-  host {
-    $staging_host:
-      ensure  => $gor_hosts_ensure,
-      ip      => $staging_ip,
-      comment => 'Used by Gor. See comments in router::gor.';
-  } ~>
+  $host_defaults = {
+    'ensure'  => $gor_hosts_ensure,
+    'before'  => 'Class[Govuk::Gor]',
+    'comment' => 'Used by Gor. See comments in router::gor.',
+  }
+
+  create_resources('host', $replay_targets, $host_defaults)
+
   class { 'govuk::gor':
     args   => {
       '-input-raw'          => 'localhost:7999',
-      '-output-http'        => $gor_targets,
+      '-output-http'        => prefix(keys($replay_targets), 'https://'),
       '-output-http-method' => [
         'GET', 'HEAD', 'OPTIONS'
       ],
     },
-    enable => $enable_staging,
+    enable => $enabled,
   }
 }
