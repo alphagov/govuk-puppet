8f90e637b73a18dd13cf59d99b0732e69ca7da28
Matt Bostock <matt@mattbostock.com>
2015-07-30 14:48:12 +0100

    Explicitly set shortened hostname for collectd
    Use the `fqdn_metrics` facter fact to explicitly set the hostname for
    collectd.
    
    Do this to avoid exceeding the 63-byte limit that collectd imposes on
    hostnames.

--- modules/collectd/manifests/config.pp
+++ modules/collectd/manifests/config.pp
@@ -4,8 +4,8 @@
 #
 class collectd::config {
   file { '/etc/collectd/collectd.conf':
-    ensure => present,
-    source => 'puppet:///modules/collectd/etc/collectd/collectd.conf',
+    ensure  => present,
+    content => template('collectd/etc/collectd/collectd.conf'),
   }
 
   file { '/etc/collectd/conf.d':
