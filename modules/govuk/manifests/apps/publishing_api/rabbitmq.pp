b7c03993b44a6f4f0766caf2f1f67a536adf114d
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-07-31 15:02:39 +0100

    Add rabbitmq permissions for publishing API.
    This adds permissions to the publishing API
    while leaving the content store able to write until
    we're done with the deploy.
    
    It moves responsibility for creating the exchanges
    over to the publishing API's configuration.

---
+++ modules/govuk/manifests/apps/publishing_api/rabbitmq.pp
@@ -0,0 +1,54 @@
+# == Class: govuk::apps::publishing_api::rabbitmq
+#
+# Creates an AMQP exchange and sets up permissions for the publishing_api
+# to read and write messages.
+#
+# This sets up the user, and permits read/write/configure
+# to the queue and exchange(s).
+#
+# === Parameters
+#
+# [*amqp_user*]
+#   The RabbitMQ username (default: 'publishing_api')
+#
+# [*amqp_pass*]
+#   The RabbitMQ password (default: 'publishing_api')
+#
+# [*amqp_exchange*]
+#   The RabbitMQ exchange to read from (default: 'published_documents')
+#
+# [*configure_test_exchange*]
+#   Whether or not to set up a test exchange for development (default: false)
+#
+class govuk::apps::publishing_api::rabbitmq (
+  $amqp_user  = 'publishing_api',
+  $amqp_pass  = 'publishing_api',
+  $amqp_exchange = 'published_documents',
+  $configure_test_exchange = false,
+) {
+  rabbitmq_user { $amqp_user:
+    password => $amqp_pass,
+  }
+
+  $permissions_regex_fragment = $configure_test_exchange ? {
+    true    => "${amqp_exchange}|${amqp_exchange}_test",
+    default => $amqp_exchange
+  }
+
+  rabbitmq_user_permissions { "${amqp_user}@/":
+    configure_permission => '^amq\.gen.*$',
+    read_permission      => "^(amq\\.gen.*|${permissions_regex_fragment})$",
+    write_permission     => "^(amq\\.gen.*|${permissions_regex_fragment})$",
+  }
+
+  govuk_rabbitmq::exchange { "${amqp_exchange}@/":
+    type     => 'topic',
+  }
+
+  if $configure_test_exchange {
+    # Used for running integration tests.
+    govuk_rabbitmq::exchange { "${amqp_exchange}_test@/":
+      type     => 'topic',
+    }
+  }
+}
