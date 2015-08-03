6e9314c8d20844691d657c91b717c210d89b827d
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-08-03 09:07:20 +0100

    Parameterise creds for CDN IP ranges Jenkins jobs
    This commit allows these parameters to be passed in from
    the deployment repository.

--- modules/govuk_jenkins/manifests/job/production/check_cdn_ip_ranges.pp
+++ modules/govuk_jenkins/manifests/job/production/check_cdn_ip_ranges.pp
@@ -1,8 +1,24 @@
 # == Class: govuk_jenkins::job::production::check_cdn_ip_ranges
 #
-# Create a file on disk that can be parsed by jenkins-job-builder
+# Create a jenkins-job-builder config file for checking that CDN
+# IP ranges are configured correctly.
 #
-class govuk_jenkins::job::production::check_cdn_ip_ranges {
+# === Parameters
+#
+# [*cdn_username*]
+#   Username for an account with our CDN provider.
+#
+# [*cdn_password_encrypted*]
+#   Password for the `$cdn_username` account. This password
+#   must be encrypted by Jenkins before passing it in as a
+#   parameter. You can do this by taking the plaintext password,
+#   adding it as a password parameter in Jenkins and taking the
+#   result from the `config.xml` file.
+#
+class govuk_jenkins::job::production::check_cdn_ip_ranges (
+  $cdn_username = undef,
+  $cdn_password_encrypted = undef,
+) {
   file { '/etc/jenkins_jobs/jobs/check_cdn_ip_ranges.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/production/check_cdn_ip_ranges.yaml.erb'),
