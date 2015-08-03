64abaf4023630d6618f2dbd2a71051ce6c167da1
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-08-03 13:59:37 +0100

    Parameterise Smokey Jenkins job even more
    Move some of these configuration settings and credentials to be
    parameters of this class so that they can be passed from the
    deployment repo.

--- modules/govuk_jenkins/manifests/job/smokey.pp
+++ modules/govuk_jenkins/manifests/job/smokey.pp
@@ -2,9 +2,39 @@
 #
 # Create a file on disk that can be parsed by jenkins-job-builder
 #
+# === Parameters
+#
+# [*auth_username*]
+#   HTTP basic auth username
+#
+# [*auth_password*]
+#   HTTP basic auth password
+#
+# [*efg_username*]
+#   Username to sign in to EFG
+#
+# [*efg_password*]
+#   Password to sign in to EFG
+#
+# [*signon_email*]
+#   Username to sign in to Signon
+#
+# [*signon_password*]
+#   Password to sign in to Signon
+#
+# [*smokey_bearer_token*]
+#   Bearer token to pass as part of an Authorization HTTP header
+#
+# [*smokey_rate_limit_token*]
+#   Token to pass as an HTTP header to bypass rate limiting
+#
 class govuk_jenkins::job::smokey (
+  $auth_username = undef,
+  $auth_password = undef,
+  $efg_username = undef,
   $efg_password = undef,
   $signon_email = undef,
+  $signon_password = undef,
   $smokey_bearer_token = undef,
   $smokey_rate_limit_token = undef,
 ) {
