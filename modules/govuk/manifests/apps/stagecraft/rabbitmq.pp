59a58d2ad1d09a624c66f9134639102c6559c87a
Tom Booth <tombooth@gmail.com>
2015-08-04 16:15:38 +0100

    Add in rabbitmq configuration for stagecraft
    We are going to be integrating celery with stagecraft so that we can
    dispatch jobs. This requires a new rabbitmq vhost that celery will have
    complete control of. This has been isolated to just stagecraft and not
    using backdrop_write's so that they don't clobber each other.
    
    Signed-off-by: Tom Booth <tombooth@gmail.com>

---
+++ modules/govuk/manifests/apps/stagecraft/rabbitmq.pp
@@ -0,0 +1,41 @@
+# == Class: govuk::apps::stagecraft::rabbitmq
+#
+# Permissions for the stagecraft application to write messages to and the
+# Procfile worker read from Stagecraft's AMQP exchange.
+#
+# === Parameters
+#
+# [*amqp_user*]
+#   The username for the RabbitMQ user
+#   Default: stagecraft
+#
+# [*amqp_pass*]
+#   The password for the RabbitMQ user
+#   Default: stagecraft
+#
+# [*amqp_vhost*]
+#   The virtual host that this application is restricted to
+#   Default: stagecraft
+#
+class govuk::apps::stagecraft::rabbitmq (
+  $amqp_user  = 'stagecraft',
+  $amqp_pass  = 'stagecraft',
+  $amqp_vhost = 'stagecraft',
+) {
+
+  rabbitmq_vhost { "/${amqp_vhost}":
+    ensure   => present,
+  }
+
+  rabbitmq_user { $amqp_user:
+    admin    => false,
+    password => $amqp_pass,
+  }
+
+  rabbitmq_user_permissions { "${amqp_user}@/${amqp_vhost}":
+    configure_permission => '.*',
+    read_permission      => '.*',
+    write_permission     => '.*',
+  }
+
+}
