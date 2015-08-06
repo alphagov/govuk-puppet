ae896d7429951e86f7a4d7145d1dd5063b43ed80
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-08-06 15:10:19 +0100

    Parameterise Performance Platform token
    This is a secret and needs to be moved to the deployment repository
    before being rotated.

--- modules/govuk_jenkins/manifests/job/production/run_govuk_complaint_rate_report.pp
+++ modules/govuk_jenkins/manifests/job/production/run_govuk_complaint_rate_report.pp
@@ -2,7 +2,14 @@
 #
 # Create a file on disk that can be parsed by jenkins-job-builder
 #
-class govuk_jenkins::job::production::run_govuk_complaint_rate_report {
+# === Parameters
+#
+# [*performanceplatform_dataset_token*]
+#   Token to POST data to the Performance Platform
+#
+class govuk_jenkins::job::production::run_govuk_complaint_rate_report (
+  $performanceplatform_dataset_token = undef,
+) {
   file { '/etc/jenkins_jobs/jobs/run_govuk_complaint_rate_report.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/production/run_govuk_complaint_rate_report.yaml.erb'),
