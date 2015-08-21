efba26576da0906aadbaf5524bb42cf59eea2735
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-08-21 15:38:35 +0100

    Add Icinga alert for errors at the CDN edge
    We had a prolonged incident recently where our CDN provider was serving
    with an elevated error rate because they had an issue with a cache node.
    
    Our lack of monitoring meant that it took us a week to bring this to
    our provider's attention, which in turn meant that we served far more
    errors to users than we should have done.
    
    This commit uses the metrics we are collecting about our CDN behaviour
    and creates an Icinga alert if the error rate rises.
    
    The warning and critical thresholds are deliberately low to begin with.
    I don't believe that these values will alert excessively, but I have
    absolutely no problem with raising them. The important thing is that
    we have some visibility over how our CDN is behaving which we will
    be able to act on quickly.
    
    The check sets a `from` parameter to pull metrics from the last 30 minutes
    because statistics from the CDN provider are delayed by several minutes.
    This check is not intended to catch immediate issues - our internal alerting
    will be far better at that. It's intended to catch subtle long-term issues
    which we would not otherwise see.

---
+++ modules/monitoring/manifests/edge.pp
@@ -0,0 +1,24 @@
+# == Class: monitoring::edge
+#
+# This class sets up monitoring at the edge of the Content
+# Delivery Network which serves GOV.UK.
+#
+# === Parameters
+#
+# [*enabled*]
+#   Should monitoring of the CDN edge be enabled?
+#
+class monitoring::edge (
+  $enabled = false,
+) {
+
+  icinga::check::graphite { "fastly_5xx_rate_on_${::hostname}":
+    target    => "${::fqdn_metrics}.cdn_fastly-govuk.requests-status_5xx",
+    desc      => 'Fastly error rate for GOV.UK',
+    warning   => 0.05,
+    critical  => 0.1,
+    host_name => $::fqdn,
+    from      => '30minutes',
+  }
+
+}
--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -1,4 +1,7 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: monitoring
+#
+# Sets up monitoring.
+#
 class monitoring {
 
   package { 'python-rrdtool':
@@ -13,6 +16,7 @@ class monitoring {
   # Monitoring server only.
   include monitoring::contacts
   include monitoring::checks
+  include monitoring::edge
   include monitoring::pagerduty_drill
 
 }
