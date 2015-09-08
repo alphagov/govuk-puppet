450ecd58fb3b4fb0bbaa99316ceeb5e331925e5f
Tadas Tamosauskas <tadas@pdfcv.com>
2015-09-08 15:39:48 +0100

    Set EXPOSE_GOVSPEAK for Smart Answers
    To simplify coordination of content change requests between content designers
    and developers we're going to expose Smart Answer outcomes as govspeak by
    appending .txt to Smart Answer outcome URLs. This will allow content designers
    to copy, paste and modify (in their text editor) govspeak. Currently they are
    'reverse-engineering' it from HTML outputs.
    
    We do not want to expose this feature to the wide public, but we want to enable
    it on preview. The main concerns for not exposing it in production are:
    - a fear of needing to support it in a long term
    - it is an internal 'tool' and it feels clunky to have it available in
      production
    
    Note that the values are strings 'true' or 'false', because it is an
    environment variable and environment variables can only be strings.

--- modules/govuk/manifests/apps/smartanswers.pp
+++ modules/govuk/manifests/apps/smartanswers.pp
@@ -1,7 +1,32 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: govuk::apps::smartanswers
+#
+# Configures Smart Answers application
+#
+# === Parameters
+#
+# [*port*]
+#   The port that Smart Answers is served on.
+#   Default: 3010
+#
+# [*expose_govspeak*]
+#   Either 'true' or 'false' indicating if govspeak format should be made
+#   available alongside other formats (e.g. html, json)
+#   Default: 'false'
+
 class govuk::apps::smartanswers(
   $port = 3010,
+  $expose_govspeak = 'false',
 ) {
+
+  Govuk::App::Envvar {
+    app => 'smartanswers',
+  }
+
+  govuk::app::envvar {
+    'EXPOSE_GOVSPEAK':
+      value => $expose_govspeak;
+  }
+
   govuk::app { 'smartanswers':
     app_type              => 'rack',
     port                  => $port,
