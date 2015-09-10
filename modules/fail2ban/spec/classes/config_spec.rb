0182b568a7c99ed4837c8191d4698db99056039b
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-09-10 10:26:00 +0100

    Add fail2ban::config::jail_snippet defined type.
    This is intended to be used as a virtual resource in other modules that
    need to provide fail2ban jail config. It's realised then the main
    fail2ban module is included.

--- modules/fail2ban/spec/classes/config_spec.rb
+++ modules/fail2ban/spec/classes/config_spec.rb
@@ -1,8 +1,33 @@
 require_relative '../../../../spec_helper'
 
 describe 'fail2ban::config', :type => :class do
+  let(:facts) {{
+    :lsbdistcodename => 'trusty',
+  }}
+
   it do
     should contain_file('/etc/fail2ban/fail2ban.local')
     should contain_file('/etc/fail2ban/jail.local')
+  end
+
+  context "with some jail config snippets" do
+    let(:pre_condition) {
+      <<-EOT
+      @fail2ban::config::jail_snippet { 'test':
+        content => "foo",
+      }
+      EOT
+    }
+
+    it "should realise the jail config snippets" do
+      expect(subject).to contain_file("/etc/fail2ban/jail.d/01_test.local")
+        .with_content("foo")
+    end
+
+    it "should skip the snippet on precise" do
+      facts[:lsbdistcodename] = "precise"
+
+      expect(subject).not_to contain_file("/etc/fail2ban/jail.d/01_test.local")
+    end
   end
 end
