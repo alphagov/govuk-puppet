ceb034037d0aaf3be028a2a77011922806e3e2ec
Jake Benilov <benilov@gmail.com>
2015-09-22 15:41:55 +0100

    Feedback app: configure secret key base as an envvar
    This config is needed for Rails 4, which is what the app is being upgraded
    to. This change brings the app closer to the 12factor way of configuration.

--- modules/govuk/manifests/apps/feedback.pp
+++ modules/govuk/manifests/apps/feedback.pp
@@ -1,13 +1,35 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# govuk::apps::feedback
+#
+# The GOV.UK Feedback application -- handles user feedback and support submissions
+#
+# === Parameters
+#
+# [*secret_key_base*]
+#   The key for Rails to use when signing/encrypting sessions.
+#
 class govuk::apps::feedback(
   $port = 3028,
+  $secret_key_base = undef,
 ) {
-  govuk::app { 'feedback':
+  $app_name = 'feedback'
+
+  govuk::app { $app_name:
     app_type              => 'rack',
     port                  => $port,
     health_check_path     => '/contact',
     log_format_is_json    => true,
     asset_pipeline        => true,
-    asset_pipeline_prefix => 'feedback',
+    asset_pipeline_prefix => $app_name,
+  }
+
+  Govuk::App::Envvar {
+    app => $app_name,
+  }
+
+  if $secret_key_base != undef {
+    govuk::app::envvar { "${title}-SECRET_KEY_BASE":
+      varname => 'SECRET_KEY_BASE',
+      value   => $secret_key_base,
+    }
   }
 }
