a3f49bf42c2966c2bd483b1e07c2efa820907f5f
Paul Bowsher <paul.bowsher@gmail.com>
2015-09-23 12:11:27 +0100

    Remove env var and icing check resources
    These are managed by puppet - if not explicitly declared they will be
    removed.

--- modules/govuk/manifests/apps/external_link_tracker.pp
+++ modules/govuk/manifests/apps/external_link_tracker.pp
@@ -8,20 +8,6 @@ class govuk::apps::external_link_tracker (
   $error_log = '/var/log/external-link-tracker/errors.json.log',
 ) {
   if $enabled {
-    Govuk::App::Envvar {
-      ensure => absent,
-      app    => 'external-link-tracker',
-    }
-
-    govuk::app::envvar {
-      'LINK_TRACKER_PUBADDR':
-        value   => "localhost:${port}";
-      'LINK_TRACKER_APIADDR':
-        value   => "localhost:${api_port}";
-      'LINK_TRACKER_MONGO_URL':
-        value   => join($mongodb_nodes, ',');
-    }
-
     govuk::app { 'external-link-tracker':
       ensure             => absent,
       app_type           => 'bare',
@@ -38,16 +24,6 @@ class govuk::apps::external_link_tracker (
       to        => ["localhost:${api_port}"],
       protected => false,
       ssl_only  => false,
-    }
-
-    # We can't pass `health_check_path` to `govuk::app` because it has the
-    # reverse proxy port, not the API port. Changing the port would lose us
-    # TCP connection stats.
-    @@icinga::check { "check_app_external_link_tracker_up_on_${::hostname}":
-      ensure              => absent,
-      check_command       => "check_nrpe!check_app_up!${api_port} ${api_healthcheck}",
-      service_description => 'external-link-tracker app not running',
-      host_name           => $::fqdn,
     }
 
     govuk::logstream { 'external-link-tracker-error-json-log':
