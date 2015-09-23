ffe638289d6f385cd3aebb837e5fc7fa6629623c
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2015-09-23 11:29:21 +0100

    Set "PUBLISH_TOPICS" for HMRC Manuals API to publish topics to Rummager
    - Adapted from https://github.gds/gds/puppet/pull/3358.
    - Related to
      https://github.com/alphagov/hmrc-manuals-api/pull/110, from
      https://trello.com/c/XshMIRhW/55-send-hmrc-manuals-sub-topics-to-publishing-api-and-rummager-feature-flagged.

--- modules/govuk/manifests/apps/hmrc_manuals_api.pp
+++ modules/govuk/manifests/apps/hmrc_manuals_api.pp
@@ -17,10 +17,29 @@
 #   Whether to enable the procfile worker
 #   Default: true
 #
+# [*publish_topics*]
+#   Feature flag to allow publishing of manual topics to the Publishing
+#   API and Rummager.
+#   Default: false
+#
 class govuk::apps::hmrc_manuals_api(
   $port = 3071,
   $enable_procfile_worker = true,
+  $publish_topics = false,
 ) {
+
+  Govuk::App::Envvar {
+    app => 'hmrc_manuals_api',
+  }
+
+  if $publish_topics {
+    govuk::app::envvar {
+      "${title}-PUBLISH_TOPICS":
+        varname => 'PUBLISH_TOPICS',
+        value => '1';
+    }
+  }
+
   govuk::app { 'hmrc-manuals-api':
     app_type           => 'rack',
     port               => $port,
