2c2ecd8bb0dc8858bf6911ce4306dc04d5a36a51
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-09-26 17:49:37 +0100

    Move deployment SSH host keys to hieradata
    These are configuration so they should be specified in hieradata
    rather than in the Puppet manifest.

--- modules/govuk/manifests/sshkeys.pp
+++ modules/govuk/manifests/sshkeys.pp
@@ -5,31 +5,25 @@
 #
 # === Parameters
 #
+# [*deployment_keys*]
+#   A hash of host keys that are used for deploying software from source control.
+#
 # [*development_keys*]
 #   A hash of host keys to seed the development VM. For example keys from preview to
 #   allow the data replication script to work without prompting to connect
 #   to hosts.
 #
 class govuk::sshkeys (
+  $deployment_keys = {},
   $development_keys = {},
 ) {
-  validate_hash($development_keys)
+  validate_hash($deployment_keys, $development_keys)
 
   $ssh_key_defaults = {
     'type' => 'ssh-rsa',
   }
 
+  create_resources(sshkey, $deployment_keys, $ssh_key_defaults)
   create_resources(sshkey, $development_keys, $ssh_key_defaults)
 
-  sshkey { 'github.com':
-    ensure => present,
-    type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==',
-  }
-
-  sshkey { 'github.gds':
-    ensure => present,
-    type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDJsR5gu4+LPnomBEO37hY0l1chnD6U3eA1EHUg/o5op95dal49ZEvVEGtDCWyzwb2AF82/+APwCEHmAGF9l0suG5mU/VvtH4ne+S1Kji0TY+67t5rDDmckC0hqSkBxBrDyHROkXtRIyc/dyyuRhQBgW6zY1bEgM+eobxskWqBbx8bbUhPqH61Bm8fUCegvbgta8YHLKRF2fJ7EMkSXB8ghHQiiWTh1qj7Sz5lUNVGlOwwvXGiVMTLNaTLM+yO/I4Z8+94VkMTkdF4GVP7mn0jx3o84hZ3ZfcKgdD3bWl+e5vLboKb5F4mxMBto85+0F7iI0vnko9mAVHkGKpJjDwf5',
-  }
 }
