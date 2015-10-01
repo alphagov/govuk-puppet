85d8d6c080b4192a5770f0af944685447ae7a120
Tom Booth <tombooth@gmail.com>
2015-10-01 12:14:18 +0100

    API class needs to support ruby
    We are setting up a rails app that will be running on the api machines
    and will require the usual GOV.UK setup for ruby.

--- modules/govuk/manifests/node/s_api.pp
+++ modules/govuk/manifests/node/s_api.pp
@@ -3,6 +3,8 @@
 class govuk::node::s_api inherits govuk::node::s_base {
   include nginx
 
+  include govuk::node::s_ruby_app_server
+
   include govuk_postgresql::client
 
   include govuk::apps::backdrop_read
