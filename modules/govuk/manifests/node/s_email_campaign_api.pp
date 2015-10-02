755daa4f2e72dcfe444e3cc64b919cd66c8f5a61
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-10-02 12:25:55 +0100

    Add email-campaign apps to email-campaign machines.
    Once the app is deployed here, the load balancers will be updated, and
    the apps removed from the existing servers.

--- modules/govuk/manifests/node/s_email_campaign_api.pp
+++ modules/govuk/manifests/node/s_email_campaign_api.pp
@@ -3,6 +3,12 @@
 # App server used for email-campaign app
 #
 class govuk::node::s_email_campaign_api inherits govuk::node::s_base {
-  include nginx
   include govuk::node::s_ruby_app_server
+
+  include govuk::apps::email_campaign_api
+
+  include nginx
+
+  # If we miss all the apps, throw a 500 to be caught by the cache nginx
+  nginx::config::vhost::default { 'default': }
 }
