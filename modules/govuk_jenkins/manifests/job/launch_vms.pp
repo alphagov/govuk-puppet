26087ababb32a3dd489734ef48977a94f7dcb382
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-10-05 11:43:14 +0100

    Refactor launch_vms Jenkins job properties
    We're going to add disaster recovery properties to this class so
    it makes sense to keep the organisation and environment together.

--- modules/govuk_jenkins/manifests/job/launch_vms.pp
+++ modules/govuk_jenkins/manifests/job/launch_vms.pp
@@ -2,11 +2,16 @@
 #
 # Create a file on disk that can be parsed by jenkins-job-builder
 #
+# [*vcloud_properties*]
+#   A hash containing the vCloud organisation name and the environment
+#   identifier to launch VMs.
+#
 class govuk_jenkins::job::launch_vms (
-  $vcloud_org = undef,
-  $vcloud_env = undef,
+  $vcloud_properties = {},
   $vcloud_host = undef,
 ) {
+  validate_hash($vcloud_properties)
+
   file { '/etc/jenkins_jobs/jobs/launch_vms.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/launch_vms.yaml.erb'),
