a94fb29c526c4f5f597c9ace45b1cea88245e9a2
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-10-05 12:57:21 +0100

    Add disaster recovery launch VMs job
    This commit adds a Jenkins job to launch VMs in the disaster recovery
    organisation.
    
    Ideally we'd like to have this as a parameter in one Jenkins job:
    
    - normal
    - disaster recovery
    
    But that actually ends up being quite complex to implement because the job
    has to look up the value that's been selected and translate it to the
    vCloud organisation and environment parameters.

--- modules/govuk_jenkins/manifests/job/launch_vms.pp
+++ modules/govuk_jenkins/manifests/job/launch_vms.pp
@@ -6,15 +6,26 @@
 #   A hash containing the vCloud organisation name and the environment
 #   identifier to launch VMs.
 #
+# [*vcloud_properties_dr*]
+#   A hash containing the vCloud organisation name and the environment
+#   identifier to launch VMs for disaster recovery.
+#
 class govuk_jenkins::job::launch_vms (
   $vcloud_properties = {},
+  $vcloud_properties_dr = {},
   $vcloud_host = undef,
 ) {
-  validate_hash($vcloud_properties)
+  validate_hash($vcloud_properties, $vcloud_properties_dr)
 
   file { '/etc/jenkins_jobs/jobs/launch_vms.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/launch_vms.yaml.erb'),
+    notify  => Exec['jenkins_jobs_update'],
+  }
+
+  file { '/etc/jenkins_jobs/jobs/launch_vms_dr.yaml':
+    ensure  => present,
+    content => template('govuk_jenkins/jobs/launch_vms_dr.yaml.erb'),
     notify  => Exec['jenkins_jobs_update'],
   }
 }
