e23d83de9b116ecd7570db1825e250c33ab44d2b
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-10-05 15:13:24 +0100

    Remove duplicate test
    The development test is testing the same thing as the production
    test.

--- modules/mongodb/spec/classes/mongodb_configure_replica_set_spec.rb
+++ modules/mongodb/spec/classes/mongodb_configure_replica_set_spec.rb
@@ -1,35 +1,21 @@
 require_relative '../../../../spec_helper'
 
 describe 'mongodb::configure_replica_set', :type => :class do
-  describe 'production' do
-    let(:params) {{
-      'members'         => {
-        '123.456.789.123' => {},
-        '987.654.321.012' => {},
-        '432.434.454.454:457' => {},
-      },
-      'replicaset_name' => 'production',
-    }}
+  describe 'configure-replica-set' do
+    describe 'sets replica set name' do
+      let(:params) {{
+        'members'         => {
+          '123.456.789.123' => {},
+          '987.654.321.012' => {},
+          '432.434.454.454:457' => {},
+        },
+        'replicaset_name' => 'production',
+      }}
 
-    it {
-      is_expected.to contain_file('/etc/mongodb/configure-replica-set.js').with_content(
-        /_id: "production"/)
-    }
-  end
-
-  describe 'development' do
-    let(:params) {{
-      'members'         => {
-        '123.456.789.123' => {},
-        '987.654.321.012' => {},
-        '432.434.454.454:457' => {},
-      },
-      'replicaset_name' => 'development',
-    }}
-
-    it {
-      is_expected.to contain_file('/etc/mongodb/configure-replica-set.js').with_content(
-        /_id: "development"/)
-    }
+      it {
+        is_expected.to contain_file('/etc/mongodb/configure-replica-set.js').with_content(
+          /_id: "production"/)
+      }
+    end
   end
 end
