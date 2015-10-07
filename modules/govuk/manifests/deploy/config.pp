98ff1dc259fdfd06b3e9fce0d9aa1e41cbe02ad9
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-10-07 09:35:59 +0100

    Update Licensify post-migration
    The hostname for Licensify has changed to `licensify.publishing.service.gov.uk`
    so we need to:
    
    - update the hosts entries in production for Skyscape to point to the new hostname
    - remove the Plek environment variable override so that other apps look in the right place

--- modules/govuk/manifests/deploy/config.pp
+++ modules/govuk/manifests/deploy/config.pp
@@ -9,17 +9,12 @@
 # [*errbit_environment_name*]
 #   Name of the environment to be included in Errbit error reports.
 #
-# [*external_licensify*]
-#   Boolean indicating whether or not Licensify is hosted externally
-#   to this environment.
-#
 # [*govuk_env*]
 #   GOV.UK environment
 #   Default: 'production'
 #
 class govuk::deploy::config(
   $errbit_environment_name = '',
-  $external_licensify = false,
   $govuk_env = 'production',
 ){
   include daemontools
@@ -89,12 +84,5 @@ class govuk::deploy::config(
     'GOVUK_ASSET_HOST': value          => $asset_root;
     'GOVUK_ASSET_ROOT': value          => $asset_root;
     'GOVUK_WEBSITE_ROOT': value        => $website_root;
-  }
-
-  # FIXME: Remove this when Skyscape uses Carrenza Puppetmaster
-  if $external_licensify {
-    govuk::envvar { 'PLEK_SERVICE_LICENSIFY_URI':
-      value => 'https://licensify.production.alphagov.co.uk/',
-    }
   }
 }
