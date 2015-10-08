a7e35892386857e8b2761ec6d97e011cab2b43fe
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-10-08 10:28:00 +0100

    Add service_manual_publisher class docs
    Not having class documentation causes lint to fail.

--- modules/govuk/manifests/apps/service_manual_publisher.pp
+++ modules/govuk/manifests/apps/service_manual_publisher.pp
@@ -1,3 +1,15 @@
+# == Class: govuk::apps::service_manual_publisher
+#
+# Publisher for the service manual
+#
+# === Parameters
+#
+# [*port*]
+#   The port that the app is served on.
+#
+# [*enabled*]
+#   Whether to enable the app
+#
 class govuk::apps::service_manual_publisher(
   $port = 3111,
   $enabled = false,
