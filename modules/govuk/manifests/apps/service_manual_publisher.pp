8caa7e70771eade2c2f6b1450f47abe2e35f7884
Kushal Pisavadia <kushi.p@gmail.com>
2015-10-12 14:22:46 +0100

    Service Manual Publisher: Set Errbit API key as ENV var
    Set the Errbit API key as an environment variable.

--- modules/govuk/manifests/apps/service_manual_publisher.pp
+++ modules/govuk/manifests/apps/service_manual_publisher.pp
@@ -4,25 +4,42 @@
 #
 # === Parameters
 #
-# [*port*]
-#   The port that the app is served on.
-#
 # [*enabled*]
 #   Whether to enable the app
 #
+# [*errbit_api_key*]
+#   Errbit API key used by airbrake
+#   Default: ''
+#
+# [*port*]
+#   The port that the app is served on.
+#
 class govuk::apps::service_manual_publisher(
-  $port = 3111,
   $enabled = false,
+  $errbit_api_key = '',
+  $port = 3111,
 ) {
 
   if $enabled {
     include govuk_postgresql::client #installs libpq-dev package needed for pg gem
 
-    govuk::app { 'service-manual-publisher':
+    $app_name = 'service-manual-publisher'
+
+    govuk::app { $app_name:
       app_type          => 'rack',
       port              => $port,
       vhost_ssl_only    => true,
       health_check_path => '/healthcheck',
+    }
+
+    Govuk::App::Envvar {
+      app => $app_name,
+    }
+
+    govuk::app::envvar {
+      "${title}-ERRBIT_API_KEY":
+        varname => 'ERRBIT_API_KEY',
+        value   => $errbit_api_key;
     }
   }
 }
