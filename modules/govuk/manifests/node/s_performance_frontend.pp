1b4037597fbd5a65181f2d4173e32f614c97f3b6
Timothy Mower <timothy.mower@gmail.com>
2015-10-16 15:10:24 +0100

    Remove performance platform notifier completely
    Puppet has been deployed with the app ensured absent, so we can now
    remove all references to it

--- modules/govuk/manifests/node/s_performance_frontend.pp
+++ modules/govuk/manifests/node/s_performance_frontend.pp
@@ -7,7 +7,6 @@ class govuk::node::s_performance_frontend inherits govuk::node::s_base {
   include nodejs
 
   include govuk::apps::performanceplatform_big_screen_view
-  include govuk::apps::performanceplatform_notifier
   include govuk::apps::screenshot_as_a_service
   include govuk::apps::spotlight
 }
