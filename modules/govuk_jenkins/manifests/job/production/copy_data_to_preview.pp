ddf7d9f0a921638a0fd3e9b69121e766722ddacf
Paul Martin <Paul.martin@digital.cabinet-office.gov.uk>
2015-10-19 16:49:24 +0100

    Add Icinga passive checks to jobs
    Previously the required triggers were added to jobs, but because the
    Icinga defined services didn't exist they were not able to actually
    report on the passive check, throwing the following error:
    
    ```
    /var/log/icinga/nagios.log:[1445257363] EXTERNAL COMMAND:
    PROCESS_SERVICE_CHECK_RESULT;10.3.0.3;"Data sync to Staging";0;"Data
    sync to Staging successful"
    /var/log/icinga/nagios.log:[1445257363] Warning:  Passive check result
    was received for service '"Data sync to Staging"' on host '10.3.0.3',
    but the service could not be found!
    /var/log/icinga/nagios.log:[1445257363] Error: External command failed
    -> PROCESS_SERVICE_CHECK_RESULT;10.3.0.3;"Data sync to Staging";0;"Data
    sync to Staging successful"
    ```
    
    This commit looks to add the Icinga passive check per job that you want
    to be alerted on, and also attempt to template in the service
    description within the template.

--- modules/govuk_jenkins/manifests/job/production/copy_data_to_preview.pp
+++ modules/govuk_jenkins/manifests/job/production/copy_data_to_preview.pp
@@ -9,10 +9,21 @@ class govuk_jenkins::job::production::copy_data_to_preview (
   $pg_dst_env_sync_pw = undef,
   $ci_alphagov_api_key = undef,
   $ci_alphagov_api_token = undef,
+  $app_domain = hiera('app_domain')
 ) {
   file { '/etc/jenkins_jobs/jobs/copy_data_to_preview.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/production/copy_data_to_preview.yaml.erb'),
     notify  => Exec['jenkins_jobs_update'],
+  }
+
+  $service_description = regsubst($title,'_',' ')
+  $job_url = "https://deploy.${app_domain}/job/${title}"
+
+  @@icinga::passive_check { "${title}_${::hostname}":
+    service_description => $service_description,
+    host_name           => $::fqdn,
+    freshness_threshold => 7200,
+    action_url          => $job_url,
   }
 }
