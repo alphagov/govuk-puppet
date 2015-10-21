fd394c51be9b8aa264527287745a7c3e2c2a3b57
Ben Lovell <benjamin.lovell@gmail.com>
2015-10-21 14:46:21 +0100

    Configure the `SECRET_KEY_BASE` for Maslow
    Due to the recent Rails 4 upgrade Maslow now requires the
    `SECRET_KEY_BASE` environment variable to be exported for signing
    secrets. This change adds the required configuration and the actual key
    for the vagrant based development environment.

--- modules/govuk/manifests/apps/maslow.pp
+++ modules/govuk/manifests/apps/maslow.pp
@@ -1,10 +1,32 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::apps::maslow( $port = '3053' ) {
-  govuk::app { 'maslow':
+# The GOV.UK Maslow application -- handles user needs submissions
+#
+# === Parameters
+#
+# [*secret_key_base*]
+#   The key for Rails to use when signing/encrypting sessions.
+#
+class govuk::apps::maslow(
+  $port = '3053',
+  $secret_key_base = undef,
+) {
+  $app_name = 'maslow'
+
+  govuk::app { $app_name:
     app_type           => 'rack',
     port               => $port,
     vhost_ssl_only     => true,
     health_check_path  => '/healthcheck',
     log_format_is_json => true,
+  }
+
+  Govuk::App::Envvar {
+    app => $app_name,
+  }
+
+  if $secret_key_base != undef {
+    govuk::app::envvar { "${title}-SECRET_KEY_BASE":
+      varname => 'SECRET_KEY_BASE',
+      value   => $secret_key_base,
+    }
   }
 }
