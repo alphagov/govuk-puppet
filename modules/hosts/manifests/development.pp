b789d5ca7f820c400e43212136a0661a8e3559f5
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-10-23 16:12:43 +0100

    Move development host entries to hieradata
    To be consistent with the way that we manage host entries in other
    environments, and because YAML is nicer than Puppet.

--- modules/hosts/manifests/development.pp
+++ modules/hosts/manifests/development.pp
@@ -2,100 +2,28 @@
 #
 # Host entries to setup on the development virtual machine.
 #
-class hosts::development {
-  host { 'alert.cluster':       ip => '127.0.0.1' }
-  host { 'backend.cluster':     ip => '127.0.0.1' }
-  host { 'cache.cluster':       ip => '127.0.0.1' }
-  host { 'data.cluster':        ip => '127.0.0.1' }
-  host { 'frontend.cluster':    ip => '127.0.0.1' }
-  host { 'mongodb.cluster':     ip => '127.0.0.1' }
-  host { 'mysql.cluster':       ip => '127.0.0.1' }
-  host { 'puppet.cluster':      ip => '127.0.0.1' }
-  host { 'router.cluster':      ip => '127.0.0.1' }
-  host { 'support.cluster':     ip => '127.0.0.1' }
-  host { 'whitehall.cluster':   ip => '127.0.0.1' }
+# === Parameters
+#
+# [*apps*]
+#   An array of apps to set up host entries for on the dev VM.
+#
+# [*services*]
+#   An array of hosts to set up on the development VM as services.
+#
+class hosts::development (
+  $apps = [],
+  $services = [],
+) {
+  validate_array($apps, $services)
 
-  host { 'asset-manager.dev.gov.uk':                        ip => '127.0.0.1' }
-  host { 'bouncer.dev.gov.uk':                              ip => '127.0.0.1' }
-  host { 'backdrop-write.dev.gov.uk':                       ip => '127.0.0.1' }
-  host { 'backdrop-read.dev.gov.uk':                        ip => '127.0.0.1' }
-  host { 'business-support-api.dev.gov.uk':                 ip => '127.0.0.1' }
-  host { 'businesssupportfinder.dev.gov.uk':                ip => '127.0.0.1' }
-  host { 'calculators.dev.gov.uk':                          ip => '127.0.0.1' }
-  host { 'calendars.dev.gov.uk':                            ip => '127.0.0.1' }
-  host { 'canary-backend.dev.gov.uk':                       ip => '127.0.0.1' }
-  host { 'canary-frontend.dev.gov.uk':                      ip => '127.0.0.1' }
-  host { 'collections.dev.gov.uk':                          ip => '127.0.0.1' }
-  host { 'collections-publisher.dev.gov.uk':                ip => '127.0.0.1' }
-  host { 'contacts-admin.dev.gov.uk':                       ip => '127.0.0.1' }
-  host { 'contacts-frontend.dev.gov.uk':                    ip => '127.0.0.1' }
-  host { 'contacts-frontend-old.dev.gov.uk':                ip => '127.0.0.1' }
-  host { 'contentapi.dev.gov.uk':                           ip => '127.0.0.1' }
-  host { 'content-register.dev.gov.uk':                     ip => '127.0.0.1' }
-  host { 'content-store.dev.gov.uk':                        ip => '127.0.0.1' }
-  host { 'courts-api.dev.gov.uk':                           ip => '127.0.0.1' }
-  host { 'courts-frontend.dev.gov.uk':                      ip => '127.0.0.1' }
-  host { 'designprinciples.dev.gov.uk':                     ip => '127.0.0.1' }
-  host { 'draft-content-store.dev.gov.uk':                  ip => '127.0.0.1' }
-  host { 'efg.dev.gov.uk':                                  ip => '127.0.0.1' }
-  host { 'email-alert-api.dev.gov.uk':                      ip => '127.0.0.1' }
-  host { 'email-alert-monitor.dev.gov.uk':                  ip => '127.0.0.1' }
-  host { 'email-alert-frontend.dev.gov.uk':                 ip => '127.0.0.1' }
-  host { 'email-campaign-api.dev.gov.uk':                   ip => '127.0.0.1' }
-  host { 'email-campaign-frontend.dev.gov.uk':              ip => '127.0.0.1' }
-  host { 'errbit.dev.gov.uk':                               ip => '127.0.0.1' }
-  host { 'event-store.dev.gov.uk':                          ip => '127.0.0.1' }
-  host { 'feedback.dev.gov.uk':                             ip => '127.0.0.1' }
-  host { 'finder-frontend.dev.gov.uk':                      ip => '127.0.0.1' }
-  host { 'frontend.dev.gov.uk':                             ip => '127.0.0.1' }
-  host { 'govuk-delivery.dev.gov.uk':                       ip => '127.0.0.1' }
-  host { 'government-frontend.dev.gov.uk':                  ip => '127.0.0.1' }
-  host { 'hmrc-manuals-api.dev.gov.uk':                     ip => '127.0.0.1' }
-  host { 'imminence.dev.gov.uk':                            ip => '127.0.0.1' }
-  host { 'info-frontend.dev.gov.uk':                        ip => '127.0.0.1' }
-  host { 'kibana.dev.gov.uk':                               ip => '127.0.0.1' }
-  host { 'licencefinder.dev.gov.uk':                        ip => '127.0.0.1' }
-  host { 'manuals-frontend.dev.gov.uk':                     ip => '127.0.0.1' }
-  host { 'maslow.dev.gov.uk':                               ip => '127.0.0.1' }
-  host { 'metadata-api.dev.gov.uk':                         ip => '127.0.0.1' }
-  host { 'need-api.dev.gov.uk':                             ip => '127.0.0.1' }
-  host { 'panopticon.dev.gov.uk':                           ip => '127.0.0.1' }
-  host { 'performanceplatform-admin.dev.gov.uk':            ip => '127.0.0.1' }
-  host { 'policy-publisher.dev.gov.uk':                     ip => '127.0.0.1' }
-  host { 'private-frontend.dev.gov.uk':                     ip => '127.0.0.1' }
-  host { 'publicapi.dev.gov.uk':                            ip => '127.0.0.1' }
-  host { 'public-event-store.dev.gov.uk':                   ip => '127.0.0.1' }
-  host { 'publisher.dev.gov.uk':                            ip => '127.0.0.1' }
-  host { 'publishing-api.dev.gov.uk':                       ip => '127.0.0.1' }
-  host { 'release.dev.gov.uk':                              ip => '127.0.0.1' }
-  host { 'router-api.dev.gov.uk':                           ip => '127.0.0.1' }
-  host { 'router.dev.gov.uk':                               ip => '127.0.0.1' }
-  host { 'rummager.dev.gov.uk':                             ip => '127.0.0.1' }
-  host { 'search.dev.gov.uk':                               ip => '127.0.0.1' }
-  host { 'search-admin.dev.gov.uk':                         ip => '127.0.0.1' }
-  host { 'service-manual.dev.gov.uk':                       ip => '127.0.0.1' }
-  host { 'service-manual-publisher.dev.gov.uk':             ip => '127.0.0.1' }
-  host { 'short-url-manager.dev.gov.uk':                    ip => '127.0.0.1' }
-  host { 'signon.dev.gov.uk':                               ip => '127.0.0.1' }
-  host { 'smartanswers.dev.gov.uk':                         ip => '127.0.0.1' }
-  host { 'specialist-frontend.dev.gov.uk':                  ip => '127.0.0.1' }
-  host { 'specialist-publisher.dev.gov.uk':                 ip => '127.0.0.1' }
-  host { 'spotlight.dev.gov.uk':                            ip => '127.0.0.1' }
-  host { 'stagecraft.dev.gov.uk':                           ip => '127.0.0.1' }
-  host { 'static.dev.gov.uk':                               ip => '127.0.0.1' }
-  host { 'support.dev.gov.uk':                              ip => '127.0.0.1' }
-  host { 'support-api.dev.gov.uk':                          ip => '127.0.0.1' }
-  host { 'tariff-admin.dev.gov.uk':                         ip => '127.0.0.1' }
-  host { 'tariff-api.dev.gov.uk':                           ip => '127.0.0.1' }
-  host { 'tariff.dev.gov.uk':                               ip => '127.0.0.1' }
-  host { 'transition.dev.gov.uk':                           ip => '127.0.0.1' }
-  host { 'travel-advice-publisher.dev.gov.uk':              ip => '127.0.0.1' }
-  host { 'url-arbiter.dev.gov.uk':                          ip => '127.0.0.1' }
-  host { 'whitehall-admin.dev.gov.uk':                      ip => '127.0.0.1' }
-  host { 'whitehall-frontend.dev.gov.uk':                   ip => '127.0.0.1' }
-  host { 'whitehall.dev.gov.uk':                            ip => '127.0.0.1' }
-  host { 'www.dev.gov.uk':                                  ip => '127.0.0.1' }
+  $app_domain = hiera('app_domain')
+  $default_host_properties = {
+    ip => '127.0.0.1',
+  }
 
-  # The one Performance Platform host, required by publicapi in development only
-  host { 'read.backdrop.dev.gov.uk':                        ip => '127.0.0.1' }
+  $app_hostnames = regsubst($apps, '$', ".${app_domain}")
+  ensure_resource('host', $app_hostnames, $default_host_properties)
+
+  $service_hostnames = regsubst($services, '$', '.cluster')
+  ensure_resource('host', $service_hostnames, $default_host_properties)
 }
