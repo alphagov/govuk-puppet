04bd7ec96f158ee0641f101c71f568880da0b705
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2015-10-26 10:45:50 +0000

    12 factor Imminence to add secret_key_base in the new way
    - This commit updates the manifest for Imminence to follow the 12
      factor app methodology in order to add the SECRET_KEY_BASE environment
      config which is used in Rails 4.
    - Reorganise this file a bit so that it looks more like ones updated
      more recently (eg. asset-manager).

--- modules/govuk/manifests/apps/imminence.pp
+++ modules/govuk/manifests/apps/imminence.pp
@@ -1,6 +1,33 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::apps::imminence( $port = '3002', $enable_procfile_worker = true ) {
-  govuk::app { 'imminence':
+# class govuk::apps::imminence
+#
+# Imminence manages sets of (somewhat) structured data for use
+# elsewhere on GOV.UK. It's primarily used for geographical data such
+# as lists of registry offices, test centres, and the like. There is a
+# simple JSON API for integrating the data with other applications.
+#
+# === Parameters
+#
+# [*port*]
+#   The port that Imminence is served on.
+#   Default: 3002
+# [*enable_procfile_worker*]
+#   Whether to enable the Procfile worker.
+# [*secret_key_base*]
+#   The key for Rails to use when signing/encrypting sessions.
+#
+class govuk::apps::imminence(
+  $port = '3002',
+  $enable_procfile_worker = true,
+  $secret_key_base = undef,
+) {
+
+  $app_name = 'imminence'
+
+  Govuk::App::Envvar {
+    app => $app_name,
+  }
+
+  govuk::app { $app_name:
     app_type           => 'rack',
     port               => $port,
     vhost_ssl_only     => true,
@@ -9,7 +36,15 @@ class govuk::apps::imminence( $port = '3002', $enable_procfile_worker = true ) {
     asset_pipeline     => true,
   }
 
-  govuk::procfile::worker { 'imminence':
+  if $secret_key_base {
+    govuk::app::envvar {
+      "${title}-SECRET_KEY_BASE":
+      varname => 'SECRET_KEY_BASE',
+      value   => $secret_key_base;
+    }
+  }
+
+  govuk::procfile::worker { $app_name:
     enable_service => $enable_procfile_worker,
   }
 }
