0876e132d6cff11ffa61bfda4be5c4fced98daae
Tadas Tamosauskas <tadas@pdfcv.com>
2015-10-29 15:29:56 +0000

    Enable Service Manual formats rendering in preview and dev environments
    Now that we've got https://github.com/alphagov/government-frontend/pull/61 and
    https://github.com/alphagov/government-frontend/pull/58 merged, enable
    government-frontend to render Service Manual formats by setting the
    FLAG_ENABLE_SERVICE_MANUAL=1 environment variable.

--- modules/govuk/manifests/apps/government_frontend.pp
+++ modules/govuk/manifests/apps/government_frontend.pp
@@ -10,10 +10,25 @@
 # [*port*]
 #   What port should the app run on?
 #
+# [*enable_service_manual*]
+#   A feature flag to enable rendering Service Manual formats
+#
 class govuk::apps::government_frontend(
   $vhost,
   $port = '3090',
+  $enable_service_manual = false,
 ) {
+  Govuk::App::Envvar {
+    app => 'government-frontend',
+  }
+
+  if $enable_service_manual {
+    govuk::app::envvar {
+      "${title}-FLAG_ENABLE_SERVICE_MANUAL":
+        varname => 'FLAG_ENABLE_SERVICE_MANUAL',
+        value   => '1';
+    }
+  }
   govuk::app { 'government-frontend':
     app_type              => 'rack',
     port                  => $port,
