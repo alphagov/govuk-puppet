c1bf5f467b50a42011aed11b36728d7be72e4cd9
Timothy Mower <timothy.mower@gmail.com>
2015-10-30 14:53:53 +0000

    Add a logstream for perf platform collectors
    The collectors log to their own file, as this was the easiest way to
    setup logging. It is useful to search for collector errors in kibana, to
    do this we need a logstream setup.

--- modules/govuk/manifests/apps/stagecraft.pp
+++ modules/govuk/manifests/apps/stagecraft.pp
@@ -24,5 +24,10 @@ class govuk::apps::stagecraft (
       health_check_path  => '/_status',
       log_format_is_json => true;
     }
+    govuk::logstream { 'performanceplatform_collectors_log':
+      logfile => '/var/apps/stagecraft/log/collectors.json.log',
+      fields  => {'application' => 'performanceplatform-collectors'},
+      json    => true,
+    }
   }
 }
