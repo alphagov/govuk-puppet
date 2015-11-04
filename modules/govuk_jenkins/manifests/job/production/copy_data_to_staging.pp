2df9b093e6207f678d45c9256a80f676cf11381a
Josh Myers <joshuajmyers@gmail.com>
2015-11-04 15:06:22 +0000

    Make sure variable is in scope for JJB template
    We are templating the JJB job yaml file and using
    a variable that we have not declared. Fix this.

--- modules/govuk_jenkins/manifests/job/production/copy_data_to_staging.pp
+++ modules/govuk_jenkins/manifests/job/production/copy_data_to_staging.pp
@@ -11,15 +11,16 @@ class govuk_jenkins::job::production::copy_data_to_staging (
   $auth_token = undef,
   $app_domain = hiera('app_domain'),
 ) {
+
+  $check_name = 'copy_data_to_staging'
+  $service_description = 'Copy Data to Staging'
+  $job_url = "https://deploy.${app_domain}/job/copy_data_to_staging/"
+
   file { '/etc/jenkins_jobs/jobs/copy_data_to_staging.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/production/copy_data_to_staging.yaml.erb'),
     notify  => Exec['jenkins_jobs_update'],
   }
-
-  $check_name = 'copy_data_to_staging'
-  $service_description = 'Copy Data to Staging'
-  $job_url = "https://deploy.${app_domain}/job/copy_data_to_staging/"
 
   @@icinga::passive_check { "${check_name}_${::hostname}":
     service_description => $service_description,
--- modules/govuk_jenkins/manifests/job/production/copy_licensify_data_to_staging.pp
+++ modules/govuk_jenkins/manifests/job/production/copy_licensify_data_to_staging.pp
@@ -5,15 +5,16 @@
 class govuk_jenkins::job::production::copy_licensify_data_to_staging (
   $app_domain = hiera('app_domain'),
 ) {
+
+  $check_name = 'copy_licensify_data_to_staging'
+  $service_description = 'Copy Licensify Data to Staging'
+  $job_url = "https://deploy.${app_domain}/job/copy_licensify_data_to_staging"
+
   file { '/etc/jenkins_jobs/jobs/copy_licensify_data_to_staging.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/production/copy_licensify_data_to_staging.yaml.erb'),
     notify  => Exec['jenkins_jobs_update'],
   }
-
-  $check_name = 'copy_licensify_data_to_staging'
-  $service_description = 'Copy Licensify Data to Staging'
-  $job_url = "https://deploy.${app_domain}/job/copy_licensify_data_to_staging"
 
   @@icinga::passive_check { "${check_name}_${::hostname}":
     service_description => $service_description,
