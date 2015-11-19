02ca13f434190d0e2a79ee2c4eeae32fe8308ad2
Josh Myers <joshuajmyers@gmail.com>
2015-11-19 10:40:06 +0000

    Add Integration specific `govuk_delivery` job

---
+++ modules/govuk_jenkins/manifests/job/integration/govuk_delivery_configure_integration_catchall_feed.pp
@@ -0,0 +1,17 @@
+# == Class:
+# govuk_jenkins::job::integration::govuk_delivery_configure_integration_catchall_feed
+#
+# === Parameters
+#
+# [*auth_token*]
+#   Token to allow this job to be triggered remotely
+#
+class govuk_jenkins::job::integration::govuk_delivery_configure_integration_catchall_feed (
+  $auth_token = undef,
+) {
+  file { '/etc/jenkins_jobs/jobs/govuk_delivery_configure_integration_catchall_feed.yaml':
+    ensure  => present,
+    content => template('govuk_jenkins/jobs/integration/govuk_delivery_configure_integration_catchall_feed.yaml.erb'),
+    notify  => Exec['jenkins_jobs_update'],
+  }
+}
