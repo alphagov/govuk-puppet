0c5e68e4fa02fdd76ff8eef4ccbc6865acecd7b2
Tijmen Brommet <tijmen@gmail.com>
2015-12-01 14:16:51 +0000

    Add content-tagger to postgres node
    This will set up the database for content-tagger.

--- modules/govuk/manifests/node/s_postgresql_base.pp
+++ modules/govuk/manifests/node/s_postgresql_base.pp
@@ -4,6 +4,7 @@
 #
 class govuk::node::s_postgresql_base inherits govuk::node::s_base {
   include govuk::apps::content_register::db
+  include govuk::apps::content_tagger::db
   include govuk::apps::email_alert_api::db
   include govuk::apps::email_alert_monitor::db
   include govuk::apps::policy_publisher::db
