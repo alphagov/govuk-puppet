alphagov/govuk-puppet

commit 3fb566eb81fdfa0ed345dc8b6764ffae3eb8a265

Configures `SECRET_KEY_BASE` for Needs API

modules/govuk/manifests/apps/need_api.pp
@@ -19,11 +19,15 @@
 # [*mongodb_name*]
 #   The name of the MongoDB database to use
 #
+# [*secret_key_base*]
+#   The key for Rails to use when signing/encrypting sessions.
+#
 class govuk::apps::need_api(
   $port = '3052',
   $errbit_api_key = '',
   $mongodb_nodes,
   $mongodb_name = 'govuk_needs_production',
+  $secret_key_base = undef,
 ) {
   govuk::app { 'need-api':
     app_type           => 'rack',
@@ -54,6 +58,11 @@ class govuk::apps::need_api(
     govuk::app::envvar { "${title}-MONGODB_URI":
       varname => 'MONGODB_URI',
       value   => "mongodb://${mongodb_nodes_string}/${mongodb_name}",
+
+  if $secret_key_base != undef {
+    govuk::app::envvar { "${title}-SECRET_KEY_BASE":
+      varname => 'SECRET_KEY_BASE',
+      value   => $secret_key_base,
     }
   }
 }

hieradata/vagrant_credentials.yaml
@@ -54,6 +54,7 @@ govuk::apps::tariff_api_temporal::db::mysql_tariff_api_temporal: 'e8UppeCFQw,yg&
 govuk::apps::transition::postgresql_db::password: 'ood8Michej4phaquuchi'
 govuk::apps::whitehall::db::mysql_whitehall_admin: 'lB2gKNzWLBt75P8qBEtKkkCapePqAvd2'
 govuk::apps::maslow::secret_key_base: 'a06f64da7e2fa4289722d21a8cbc9fdbc26510be9be2a48803d6dc95459a1a176f70425e6fb6597549b0b951b6a0cca5abe2f0155929e912ad251064eea267c8'
+govuk::apps::need_api::secret_key_base: '46c79ea3cd5429fb8295d5ec3cc8e255685ac7409feb51bc10c1c1ab24c53a7de61dc078fa5601112a2d1f13389a46d43542d1be6521382be76d8c079d878ac6'
 
 govuk_cdnlogs::server_key: |
   -----BEGIN RSA PRIVATE KEY-----

commit 24ddc41bf017a8d80991f7800947d687b27b139e

Fix syntax error: missing parentheses
Fix this error:

    Could not parse for environment production: Syntax error at 'secret_key_base'; expected '}' at /Users/matt/govuk/puppet/modules/govuk/manifests/apps/need_api.pp:62

The error was introduced in 3fb566eb because we don't currently run CI
tests against pull requests originating from forks of the Puppet repo.

modules/govuk/manifests/apps/need_api.pp
@@ -58,6 +58,8 @@ class govuk::apps::need_api(
     govuk::app::envvar { "${title}-MONGODB_URI":
       varname => 'MONGODB_URI',
       value   => "mongodb://${mongodb_nodes_string}/${mongodb_name}",
+    }
+  }
 
   if $secret_key_base != undef {
     govuk::app::envvar { "${title}-SECRET_KEY_BASE":