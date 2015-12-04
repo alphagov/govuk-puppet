9371d93632c49b37070696ab88213f80d0df6e17
Matt Bostock <matt@mattbostock.com>
2015-12-04 15:34:44 +0000

    Kibana dashboard showing top 10 most erroring apps
    Add a Kibana dashboard that uses a terms panel to show the 10
    applications that generate the most HTTP 5XX errors over the past 7
    days.

--- modules/govuk/manifests/apps/kibana.pp
+++ modules/govuk/manifests/apps/kibana.pp
@@ -1,7 +1,22 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: govuk::apps::kibana
+#
+# Set up the Kibana application
+#
+# [*port*]
+#  Port that Kibana listens on
+#
 class govuk::apps::kibana( $port = '3202' ) {
   govuk::app { 'kibana':
     app_type => 'rack',
     port     => $port,
+  }
+
+  $dashboard_directory = '/var/apps/kibana/dashboards'
+
+  file { $dashboard_directory:
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+    source  => 'puppet:///modules/govuk/apps/kibana/dashboards',
   }
 }
