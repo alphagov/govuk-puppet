81c176cd7971589bf366855ba76e615902366e80
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-12-11 17:48:20 +0000

    Configure disaster recovery MongoDB as hidden
    > Hidden members are part of a replica set but cannot become primary
    > and are invisible to client applications.
    
    When we added priority 0 nodes across the disaster recovery VPN we discovered
    that by default some MongoDB drivers will autodiscover all MongoDB replica
    set members, including those that aren't specified in the application
    configuration.
    
    This led to application outages because applications would attempt to connect
    to a replica set member which was unavailable because the VPN was down.
    
    The second part of [the above quote][1] is important: we want the disaster recovery
    members to not be used by the applications while the site is behaving normally.
    
    This commit introduces a bit of duplication in the JavaScript but I'm not sure
    it's worth refactoring now.
    
    [1]: https://docs.mongodb.org/v2.4/core/replica-set-hidden-member/

--- modules/mongodb/spec/classes/mongodb__configure_replica_set_spec.rb
+++ modules/mongodb/spec/classes/mongodb__configure_replica_set_spec.rb
@@ -20,48 +20,6 @@ describe 'mongodb::configure_replica_set', :type => :class do
   end
 
   describe 'configure-node-priority' do
-    context 'should default to 1 when no params are set' do
-      let(:params) {{
-        'members' => {
-          '1.2.3.4' => {},
-          '5.6.7.8' => {},
-        },
-        'replicaset_name' => 'production',
-      }}
-
-      it { is_expected.to contain_file('/etc/mongodb/configure-node-priority.js').with_content(
-        /\{\n      host: '1.2.3.4:27017',\n      priority: 1,\n    \},/)
-      }
-    end
-
-    context 'should default to 1 when the priority param is not set' do
-      let(:params) {{
-        'members' => {
-          '1.2.3.4' => {'another_param' => 'some_value'},
-          '5.6.7.8' => {},
-        },
-        'replicaset_name' => 'production',
-      }}
-
-      it { is_expected.to contain_file('/etc/mongodb/configure-node-priority.js').with_content(
-        /\{\n      host: '1.2.3.4:27017',\n      priority: 1,\n    \},/)
-      }
-    end
-
-    context 'should use the priority param when it is set' do
-      let(:params) {{
-        'members' => {
-          '1.2.3.4' => {'priority' => 0.5},
-          '5.6.7.8' => {},
-        },
-        'replicaset_name' => 'production',
-      }}
-
-      it { is_expected.to contain_file('/etc/mongodb/configure-node-priority.js').with_content(
-        /\{\n      host: '1.2.3.4:27017',\n      priority: 0.5,\n    \},/)
-      }
-    end
-
     context 'should run after the replica set is bootstrapped' do
       let(:params) {{
         'members' => {
@@ -75,6 +33,94 @@ describe 'mongodb::configure_replica_set', :type => :class do
         is_expected.to contain_exec('configure-node-priority')
           .that_requires('Exec[configure-replica-set]')
       }
+    end
+
+    context 'priority' do
+      context 'should default to 1 when no params are set' do
+        let(:params) {{
+          'members' => {
+            '1.2.3.4' => {},
+            '5.6.7.8' => {},
+          },
+          'replicaset_name' => 'production',
+        }}
+
+        it { is_expected.to contain_file('/etc/mongodb/configure-node-priority.js').with_content(
+          /\{\s+host: '1.2.3.4:27017',\s+priority: 1,\n/)
+        }
+      end
+
+      context 'should default to 1 when the priority param is not set' do
+        let(:params) {{
+          'members' => {
+            '1.2.3.4' => {'another_param' => 'some_value'},
+            '5.6.7.8' => {},
+          },
+          'replicaset_name' => 'production',
+        }}
+
+        it { is_expected.to contain_file('/etc/mongodb/configure-node-priority.js').with_content(
+          /\{\s+host: '1.2.3.4:27017',\s+priority: 1,\n/)
+        }
+      end
+
+      context 'should use the priority param when it is set' do
+        let(:params) {{
+          'members' => {
+            '1.2.3.4' => {'priority' => 0.5},
+            '5.6.7.8' => {},
+          },
+          'replicaset_name' => 'production',
+        }}
+
+        it { is_expected.to contain_file('/etc/mongodb/configure-node-priority.js').with_content(
+          /\{\s+host: '1.2.3.4:27017',\s+priority: 0.5,\n/)
+        }
+      end
+    end
+
+    context 'hidden nodes' do
+      context 'should default not hidden when no params are set' do
+        let(:params) {{
+          'members' => {
+            '1.2.3.4' => {},
+            '5.6.7.8' => {},
+          },
+          'replicaset_name' => 'production',
+        }}
+
+        it { is_expected.to contain_file('/etc/mongodb/configure-node-priority.js').with_content(
+          /\{\s+host: '1.2.3.4:27017',\s+[a-z: \d]+,\s+hidden: false,\n/)
+        }
+      end
+
+      context 'should default to not hidden when the hidden param is not set' do
+        let(:params) {{
+          'members' => {
+            '1.2.3.4' => {'another_param' => 'some_value'},
+            '5.6.7.8' => {},
+          },
+          'replicaset_name' => 'production',
+        }}
+
+        it { is_expected.to contain_file('/etc/mongodb/configure-node-priority.js').with_content(
+          /\{\s+host: '1.2.3.4:27017',\s+[a-z: \d]+,\s+hidden: false,\n/)
+        }
+      end
+
+      context 'should use the hidden param when it is set' do
+        let(:params) {{
+          'members' => {
+            '1.2.3.4' => {'hidden' => true},
+            '5.6.7.8' => {},
+          },
+          'replicaset_name' => 'production',
+        }}
+
+        it { is_expected.to contain_file('/etc/mongodb/configure-node-priority.js').with_content(
+          /\{\s+host: '1.2.3.4:27017',\s+[a-z: \d]+,\s+hidden: true,\n/)
+        }
+      end
     end
   end
 end
