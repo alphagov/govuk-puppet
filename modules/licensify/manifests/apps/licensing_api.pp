03e4f5a8373d36d54abc7efb23d68738316f8eb9
Paul Martin <surminus@gmail.com>
2015-12-30 15:59:35 +0000

    Remove licensing-api entirely
    This app never made it out of development and should be removed from
    puppet.

--- modules/licensify/manifests/apps/licensing_api.pp
+++
@@ -1,9 +0,0 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class licensify::apps::licensing_api( $port = 9100 ) inherits licensify::apps::base {
-
-  govuk::app { 'licensing-api':
-    app_type => 'procfile',
-    port     => $port,
-    # health_check_path => '/login', TODO
-  }
-}
