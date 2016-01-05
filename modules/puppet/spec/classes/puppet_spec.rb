040c080d82410976834883774216be3045d33de7
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-01-05 15:41:48 +0000

    Fix Puppet duration stats in Graphite
    We used to send stats on the amount of time Puppet is taking to run
    to Graphite.
    
    This broke when we changed machine hostnames because we're not using
    the correct facter fact in `govuk_puppet`. The machine hostnames are
    too long so we created `fqdn_metrics` fact to handle the long hostnames.
    
    This needs to be a template rather than a file because we're using
    ERB to insert the value of a facter fact.

--- modules/puppet/spec/classes/puppet_spec.rb
+++ modules/puppet/spec/classes/puppet_spec.rb
@@ -8,7 +8,7 @@ describe 'puppet', :type => :class do
 
     it do
       is_expected.to contain_file('/etc/puppet/puppet.conf').with_content(/environmentpath = \/var\/govuk\/govuk-puppet\/environments/)
-      is_expected.to contain_file('/usr/local/bin/govuk_puppet').with_source('puppet:///modules/puppet/govuk_puppet_development')
+      is_expected.to contain_file('/usr/local/bin/govuk_puppet').with_content(/puppet-apply-dev/)
     end
   end
 
@@ -19,7 +19,7 @@ describe 'puppet', :type => :class do
 
     it do
       is_expected.to contain_file('/etc/puppet/puppet.conf').with_content(/environmentpath = \/usr\/share\/puppet\/production\/current\/environments/)
-      is_expected.to contain_file('/usr/local/bin/govuk_puppet').with_source('puppet:///modules/puppet/govuk_puppet')
+      is_expected.to contain_file('/usr/local/bin/govuk_puppet').with_content(/puppet agent --onetime --no-daemonize/)
     end
   end
 end
