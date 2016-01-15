e63fe7838bded80371594003a6c5f820c5dfd64c
Matt Bostock <matt@mattbostock.com>
2016-01-15 17:02:43 +0000

    Remove users that have left GOV.UK
    These users no longer work on GOV.UK; remove their access.

--- modules/users/manifests/timothymower.pp
+++
@@ -1,8 +0,0 @@
-# Creates the timothymower user
-class users::timothymower {
-  govuk::user { 'timothymower':
-    fullname => 'Timothy Mower',
-    email    => 'timothy.mower@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa REPLACE ME',
-  }
-}
