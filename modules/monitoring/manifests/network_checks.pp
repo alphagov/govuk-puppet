6ece8959c3e8677b0cddede1a2acb019da615ac6
Paul Martin <Paul.martin@digital.cabinet-office.gov.uk>
2016-01-22 15:18:23 +0000

    Create network_checks defined type
    This creates a defined type which creates a host and a ping check on top
    of that host. This can be used to create simple monitoring for any
    networking devices. The immediate requirement was to be able to monitor
    vDC gateways, and create them as hosts so they could be used as parents
    for any "children" nodes behind them.

---
+++ modules/monitoring/manifests/network_checks.pp
@@ -0,0 +1,28 @@
+# == Class: monitoring::network_checks
+#
+# Create a host with specified IP address and name, and include a basic ping
+# check.
+#
+# ===Parameters:
+#
+# [*address*]
+#   The IP address of the host.
+#
+define monitoring::network_checks (
+  $address,
+) {
+    icinga::host { $title:
+      host_name    => $title,
+      hostalias    => $title,
+      address      => $address,
+      display_name => $title,
+    }
+
+    icinga::check { "check_ping_${title}":
+      check_command       => 'check_ping!100.0,20%!500.0,60%',
+      notification_period => '24x7',
+      use                 => 'govuk_high_priority',
+      service_description => 'unable to ping',
+      host_name           => $title,
+    }
+}
