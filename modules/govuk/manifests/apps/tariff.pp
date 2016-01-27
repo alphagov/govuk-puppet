12c793e3a23b426853ee777d9aafd7b7a3cf5dc3
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-01-27 11:18:03 +0000

    Publishing API bearer token for Tariff app.

--- modules/govuk/manifests/apps/tariff.pp
+++ modules/govuk/manifests/apps/tariff.pp
@@ -1,6 +1,20 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: govuk::apps::tariff
+#
+# App details at: https://github.com/alphagov/trade-tariff-frontend
+#
+# === Parameters
+#
+# [*port*]
+#   The port that it is served on.
+#   Default: 3017
+#
+# [*publishing_api_bearer_token*]
+#   The bearer token to use when communicating with Publishing API.
+#   Default: undef
+#
 class govuk::apps::tariff(
   $port = '3017',
+  $publishing_api_bearer_token = undef,
 ) {
   govuk::app { 'tariff':
     app_type              => 'rack',
@@ -11,5 +25,11 @@ class govuk::apps::tariff(
     asset_pipeline_prefix => 'tariff',
     # The tariff frontend levels off at around 2.7G memory used.
     nagios_memory_warning => 2800000000,
+  }
+
+  govuk::app::envvar { "${title}-PUBLISHING_API_BEARER_TOKEN":
+    app     => 'tariff',
+    varname => 'PUBLISHING_API_BEARER_TOKEN',
+    value   => $publishing_api_bearer_token,
   }
 }
