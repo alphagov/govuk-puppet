71b62fb15bf6aa50855a004fc173862ffeb59f66
Lee Longmore <lee.longmore@gmail.com>
2016-02-03 11:27:11 +0000

    Replace ssh key for leelongmore

--- modules/users/manifests/leelongmore.pp
+++ modules/users/manifests/leelongmore.pp
@@ -3,6 +3,6 @@ class users::leelongmore {
   govuk::user { 'leelongmore':
     fullname => 'Lee Longmore',
     email    => 'lee.longmore@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+sUY2dMRxzAkLDF7O7xf3AabdYY66dFYXKAfgh6ITZJm31U3XYn27hCBw9Sn3FdoRcegIFEVD1Kh5XipUxN/sAiptCU+o4HKf/0Yj8qPtwQ4OHCj7aKmfiAE/COqPM7BbcL2Vgag9BMNXtwCcpAC3W5305PDnLHqpCFPj8J5dBeGMmoMyCZ06ywdSrGVTU8U7YaVwArKUsG9b8DS0xociGPaRQVnR5vfY1xKOCrV/6TvwjC9gcHRCBjReVqFj1C9tqKki+Z70O6DoYx/XGV6LuSm+feApXxS0Yi+Bu0Rp1MW7kO5mUeI+LD9EFF6L3d5xDYciN8k2vnVW7IQvExTb lee@turkeyred.co.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdIpPIrJNN9ZfhiXeTinWB1uHFzVM18cVRBazlTg7P8+kJVReHUfrHmLRIFZGuCXSp1ZCXo4hvOZ4Xlt1kJYIiP4LVrq7w5VECUHVr8s9FyzjszHT8h7+ExiltFfgpnXjymnssQ8tKZk+31+nGNHVICx8gtTv7HHgh9MEZrufneBQOc1OzqsG71lZTqyDCqDYLW83qMR5Kr+JfRXNTn9tKbzXzbheyyw8fK/rX4uaGg/dW4bMT9WuHkzgonjFXTxVLxgCLrvj9gM6fZJCqlcARj5Cjj7uEWe9a1uqA5WdhcfxpUS4NKKckvnd+KJQzeRQ6AUBWTZyZ0grbDILt8RmF lee@turkeyred.co.uk',
   }
 }
