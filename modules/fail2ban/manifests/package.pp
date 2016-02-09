c11b00ad231e0784c64c15f56f2272d7a442a356
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2016-02-09 15:02:15 +0000

    Remove fail2ban.
    After a discussion we no longer want to run Fail2Ban.
    
    From the original ticket (https://trello.com/c/7lnrxg1a)
    
    "We have decided as a team that fail2ban does not provide us with enough value for us to keep it. We only use it for banning IP addresses that generate a number of failed SSH logins, and we can protect ourselves against this by firewall off SSH access to Integration.
    
    We've also seen two incidents recently where fail2ban has banned our own office IP or internal IP addresses."

--- modules/fail2ban/manifests/package.pp
+++
@@ -1,8 +0,0 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class fail2ban::package {
-
-  package { 'fail2ban':
-    ensure => present,
-  }
-
-}
--- modules/fail2ban/spec/classes/fail2ban__service_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../../../../spec_helper'
-
-describe 'fail2ban::service', :type => :class do
-  it do
-    is_expected.to contain_service('fail2ban')
-  end
-end
