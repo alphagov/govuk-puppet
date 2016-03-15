f1553ef77691eb31ef4feaf5ef7659add8cc1f08
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-03-15 16:01:06 +0000

    Improve ClamAV freshness monitoring
    Instead of monitoring the age of the file (which is frequently broken for example
    because the file isn't updated) we should check the value that ClamAV publish over
    DNS and make sure it isn't too out-of-date.
    
    Being a version behind should be ok.

--- modules/clamav/manifests/monitoring.pp
+++ modules/clamav/manifests/monitoring.pp
@@ -7,9 +7,6 @@
 # There are other definition files but this is a good canary.
 #
 class clamav::monitoring {
-  # Convert days to seconds.
-  $warning_age = 2 * (24 * 60 * 60)
-
   file { '/usr/local/bin/check_clamav_definitions':
     ensure => present,
     source => 'puppet:///modules/clamav/usr/local/bin/check_clamav_definitions',
@@ -24,7 +21,7 @@ class clamav::monitoring {
   }
 
   @@icinga::check { "check_clamav_definitions_${::hostname}":
-    check_command       => "check_nrpe!check_clamav_definitions!${warning_age} 0",
+    check_command       => 'check_nrpe_1arg!check_clamav_definitions',
     service_description => 'clamav definitions out of date',
     host_name           => $::fqdn,
     notes_url           => monitoring_docs_url(clamav-definitions-out-of-date),
