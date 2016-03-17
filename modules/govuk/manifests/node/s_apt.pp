ba9c13ccdd4c8fb4117529bf8de827420bfaac55
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-03-17 18:28:21 +0000

    Upgrade logs-elasticsearch to 1.5.2 in integration
    Our logs-elasticsearch cluster is running a version of Elasticsearch
    which is quite old. I'd like to try upgrading to 1.5.2 first because
    I don't have much experience with Elasticsearch.
    
    Upgrading instructions: https://www.elastic.co/guide/en/elasticsearch/reference/current/setup-upgrade.html
    
    Breaking changes doc: https://www.elastic.co/guide/en/elasticsearch/reference/current/breaking-changes.html
    (there are no breaking changes from 1.4 to 1.5).
    
    This commit also creates an aptly mirror which we'll use to install
    the package.

--- modules/govuk/manifests/node/s_apt.pp
+++ modules/govuk/manifests/node/s_apt.pp
@@ -62,6 +62,10 @@ class govuk::node::s_apt (
       location => 'http://packages.elasticsearch.org/elasticsearch/1.4/debian',
       release  => 'stable',
       key      => 'D88E42B4';
+    'elasticsearch-1.5':
+      location => 'http://packages.elastic.co/elasticsearch/1.5/debian',
+      release  => 'stable',
+      key      => '46095ACC8548582C1A2699A9D27D666CD88E42B4';
     'rabbitmq':
       location => 'http://www.rabbitmq.com/debian',
       release  => 'testing',
