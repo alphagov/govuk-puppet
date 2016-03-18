78f3b9274c93a70c2ff016c084ecb723a02346d5
Matt Bostock <matt@mattbostock.com>
2016-03-18 13:21:56 +0000

    Make govuk_keepalived::auth_pass mandatory
    I was complicating things by allowing `auth_pass` to be undefined.
    Make it mandatory.

--- modules/govuk_keepalived/spec/classes/govuk_keepalived_spec.rb
+++ modules/govuk_keepalived/spec/classes/govuk_keepalived_spec.rb
@@ -4,36 +4,6 @@ describe 'govuk_keepalived', :type => :class do
   let(:facts) {{
     :concat_basedir => '/tmp/concat',
   }}
-  let(:params) {{
-    'instances' => {},
-  }}
-
-  describe "if instances specified but no auth_pass set" do
-    let(:params) {{
-      'instances' => {'foo' => {}},
-    }}
-
-    it do
-      is_expected.to raise_error(Puppet::Error, /^VRRP instances defined but auth_pass parameter not set/)
-    end
-  end
-
-  describe "instances hash is empty and auth_pass not specified" do
-    it do
-      is_expected.to_not raise_error
-    end
-  end
-
-  describe "auth_pass is set to an empty string" do
-    let(:params) {{
-      'auth_pass' => '',
-      'instances' => {'foo' => {}},
-    }}
-
-    it do
-      is_expected.to raise_error(Puppet::Error, /^VRRP instances defined but auth_pass parameter not set/)
-    end
-  end
 
   describe "auth_pass is too short" do
     let(:params) {{
