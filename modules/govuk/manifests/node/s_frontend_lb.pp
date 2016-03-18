4d0ad29ca2d0b37a68df7cbc2a55d868f76bffa9
Matt Bostock <matt@mattbostock.com>
2016-03-18 12:33:02 +0000

    Remove floating IP from frontend_lb machines
    We're not ready to use it yet.

--- modules/govuk/manifests/node/s_frontend_lb.pp
+++ modules/govuk/manifests/node/s_frontend_lb.pp
@@ -12,7 +12,6 @@ class govuk::node::s_frontend_lb (
 ){
   include govuk::node::s_base
   include loadbalancer
-  include govuk_keepalived
 
   Loadbalancer::Balance {
     https_redirect => false, # Varnish/Router can't speak HTTPS.
