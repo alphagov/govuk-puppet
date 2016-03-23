1308ada193606424f4b865e55644bb633915e5f5
Matt Bostock <matt@mattbostock.com>
2016-03-23 13:35:54 +0000

    Monitor /srv/backup-cdn-logs on offsite backup box
    Monitor the disk used to backup processed CDN logs on the provider0
    (Skyscape) offsite backup machine.

--- modules/backup/manifests/offsite/monitoring.pp
+++ modules/backup/manifests/offsite/monitoring.pp
@@ -10,10 +10,15 @@
 # [*offsite_hostname*]
 #   Descriptive hostname for use in Icinga check - eg "backup0.provider0"
 #
+# [*monitor_cdn_logs_disk*]
+#   If true, monitor disk space on /srv/backup-cdn-logs
+#
 class backup::offsite::monitoring(
   $offsite_fqdn,
   $offsite_hostname,
+  $monitor_cdn_logs_disk = false,
 ){
+  validate_bool($monitor_cdn_logs_disk)
 
   icinga::host { $offsite_fqdn:
     hostalias => $offsite_fqdn,
@@ -46,6 +51,15 @@ class backup::offsite::monitoring(
     service_description => 'low available disk space /srv/backup-logs',
     use                 => 'govuk_high_priority',
     host_name           => $offsite_fqdn,
+  }
+
+  if $monitor_cdn_logs_disk {
+    icinga::check { "check_disk_backup-cdn-logs_${offsite_hostname}":
+      check_command       => 'check_nrpe_1arg!check_disk_backup-cdn-logs',
+      service_description => 'low available disk space /srv/backup-cdn-logs',
+      use                 => 'govuk_high_priority',
+      host_name           => $offsite_fqdn,
+    }
   }
 
   icinga::check { "check_ssh_${offsite_hostname}":
