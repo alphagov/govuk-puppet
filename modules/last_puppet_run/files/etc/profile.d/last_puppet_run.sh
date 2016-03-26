a81f1a4933c2f5165ce61888abc78874ada1e4ce
Paul Bowsher <paul.bowsher@gmail.com>
2016-03-26 14:17:42 +0000

    More accurate puppet run reporting
    Now reports how long since you ran puppet, for added shaming.

--- modules/last_puppet_run/files/etc/profile.d/last_puppet_run.sh
+++ modules/last_puppet_run/files/etc/profile.d/last_puppet_run.sh
@@ -2,12 +2,11 @@
 
 last_run=`sudo stat -c %Y /var/lib/puppet/state/last_run_report.yaml`
 time_now=`date +%s`
-difference=$((time_now - last_run))
-three_days=259200
+difference=$(((time_now - last_run) / 86400))
 
-if [ "$difference" -gt "$three_days" ]; then
+if [[ $difference -ge 3 ]]; then
   yellow="\e[0;33m"
   reset="\e[0m"
 
-  echo -e "${yellow}It has been more than 3 days since you last ran govuk_puppet.${reset}"
+  echo -e "${yellow}It has been more than ${difference} days since you last ran govuk_puppet.${reset}"
 fi
