67e39b7714aa05f69c590b30c8981ad813714e09
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-03-31 14:04:56 +0000

    Add env vars for contentapi.
    This adds MONGODB_URI and SECRET_KEY_BASE, which are required in the upgrade
    to the latest versions of Mongoid/govuk_content_models. The latter will be
    populated by the deployment hieradata.

--- modules/govuk/manifests/apps/contentapi.pp
+++ modules/govuk/manifests/apps/contentapi.pp
@@ -18,11 +18,23 @@
 # [*nagios_memory_critical*]
 #   Memory use at which Nagios should generate a critical alert.
 #
+# [*secret_key_base*]
+#   The key for Rails to use when signing/encrypting sessions.
+#
+# [*mongodb_name*]
+#   The Mongo database to be used.
+#
+# [*mongodb_nodes*]
+#   Array of hostnames for the mongo cluster to use.
+#
 class govuk::apps::contentapi (
   $port = '3022',
   $publishing_api_bearer_token = undef,
   $nagios_memory_warning = undef,
   $nagios_memory_critical = undef,
+  $secret_key_base = undef,
+  $mongodb_name = undef,
+  $mongodb_nodes = undef,
 ) {
 
   govuk::app { 'contentapi':
@@ -35,9 +47,21 @@ class govuk::apps::contentapi (
     nagios_memory_critical => $nagios_memory_critical,
   }
 
-  govuk::app::envvar { "${title}-PUBLISHING_API_BEARER_TOKEN":
-    app     => 'contentapi',
-    varname => 'PUBLISHING_API_BEARER_TOKEN',
-    value   => $publishing_api_bearer_token,
+  if $mongodb_nodes != undef {
+    govuk::app::envvar::mongodb_uri { 'contentapi':
+      hosts    => $mongodb_nodes,
+      database => $mongodb_name,
+    }
+  }
+
+  govuk::app::envvar {
+    "${title}-PUBLISHING_API_BEARER_TOKEN":
+      app     => 'contentapi',
+      varname => 'PUBLISHING_API_BEARER_TOKEN',
+      value   => $publishing_api_bearer_token;
+    "${title}-SECRET_KEY_BASE":
+      app     => 'contentapi',
+      varname => 'SECRET_KEY_BASE',
+      value   => $secret_key_base;
   }
 }
