f5f370dbb1fbcc691680f897080bd8dc3d644045
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-04-01 16:03:06 +0100

    Remove basic auth for monitoring
    Graphite and Icinga both have basic authentication enabled in integration
    but they are also restricted to only be available from the GDS office,
    so there's no need for this basic auth at all.

--- modules/icinga/manifests/nginx.pp
+++ modules/icinga/manifests/nginx.pp
@@ -2,16 +2,7 @@
 #
 # Sets up nginx for Icinga
 #
-# === Parameters:
-#
-# [*enable_basic_auth*]
-#   Boolean. Whether basic auth should be enabled or disabled.
-#
-class icinga::nginx (
-  $enable_basic_auth = true,
-) {
-  validate_bool($enable_basic_auth)
-
+class icinga::nginx {
   include ::nginx
 
   nginx::config::ssl { 'nagios':
