be38eaea103f71fc42f98f7359fc6c3106eab15a
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-04-08 14:45:36 +0000

    Add env vars for service-manual-frontend secrets.

--- modules/govuk/manifests/apps/service_manual_frontend.pp
+++ modules/govuk/manifests/apps/service_manual_frontend.pp
@@ -11,10 +11,25 @@
 # [*port*]
 #   What port should the app run on?
 #
+# [*enabled*]
+#   Is the app enabled
+#
+# [*errbit_api_key*]
+#   Errbit API key used by airbrake
+#   Default: ''
+#
+# [*secret_key_base*]
+#   Used to set the app ENV var SECRET_KEY_BASE which is used to configure
+#   rails 4.x signed cookie mechanism. If unset the app will be unable to
+#   start.
+#   Default: undef
+#
 class govuk::apps::service_manual_frontend(
   $vhost = 'service-manual-frontend',
   $port = 3122,
   $enabled = false,
+  $errbit_api_key = undef,
+  $secret_key_base = undef,
 ) {
   if $enabled {
     Govuk::App::Envvar {
@@ -30,6 +45,15 @@ class govuk::apps::service_manual_frontend(
       asset_pipeline        => true,
       asset_pipeline_prefix => 'service-manual-frontend',
       vhost                 => $vhost,
+    }
+
+    govuk::app::envvar {
+      "${title}-ERRBIT_API_KEY":
+        varname => 'ERRBIT_API_KEY',
+        value   => $errbit_api_key;
+      "${title}-SECRET_KEY_BASE":
+        varname => 'SECRET_KEY_BASE',
+        value   => $secret_key_base;
     }
   }
 }
