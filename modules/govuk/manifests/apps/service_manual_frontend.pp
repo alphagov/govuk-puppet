0e980568074797c57d66011e295b2eb59862a118
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-04-08 15:00:50 +0100

    An enabled flag for service-manual-frontend
    We don't want service-manual-frontend to be deployed to staging and production until we've had time to test in integration.

--- modules/govuk/manifests/apps/service_manual_frontend.pp
+++ modules/govuk/manifests/apps/service_manual_frontend.pp
@@ -14,19 +14,22 @@
 class govuk::apps::service_manual_frontend(
   $vhost = 'service-manual-frontend',
   $port = 3122,
+  $enabled = false,
 ) {
-  Govuk::App::Envvar {
-    app => 'service-manual-frontend',
-  }
+  if $enabled {
+    Govuk::App::Envvar {
+      app => 'service-manual-frontend',
+    }
 
-  govuk::app { 'service-manual-frontend':
-    app_type              => 'rack',
-    port                  => $port,
-    vhost_ssl_only        => true,
-    health_check_path     => '/healthcheck',
-    legacy_logging        => false,
-    asset_pipeline        => true,
-    asset_pipeline_prefix => 'service-manual-frontend',
-    vhost                 => $vhost,
+    govuk::app { 'service-manual-frontend':
+      app_type              => 'rack',
+      port                  => $port,
+      vhost_ssl_only        => true,
+      health_check_path     => '/healthcheck',
+      legacy_logging        => false,
+      asset_pipeline        => true,
+      asset_pipeline_prefix => 'service-manual-frontend',
+      vhost                 => $vhost,
+    }
   }
 }
