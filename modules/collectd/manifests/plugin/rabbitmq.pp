b3b46fa595619293dd0ca314b309e493edf986be
benhyland <mail@bhyland.co.uk>
2016-04-12 12:21:38 +0100

    Revert "Add per-queue stats for rabbitmq"

--- modules/collectd/manifests/plugin/rabbitmq.pp
+++ modules/collectd/manifests/plugin/rabbitmq.pp
@@ -1,31 +1,31 @@
 # == Class: collectd::plugin::rabbitmq
 #
-# Sets up the NYTimes collectd-rabbitmq collectd plugin to monitor RabbitMQ.
-# See https://github.com/NYTimes/collectd-rabbitmq
+# Sets up a collectd plugin to monitor RabbitMQ.
 #
 # === Parameters
 #
 # [*monitoring_password*]
-#   The password to the 'monitoring' user which can access monitoring information from RabbitMQ.
+#   The password to a user which can access monitoring information from RabbitMQ.
 #
 class collectd::plugin::rabbitmq (
   $monitoring_password,
   ){
+  include collectd::plugin::python
 
-  package { 'collectd-rabbitmq':
-    ensure   => '1.3.0',
-    provider => pip,
-  }
-
-  file { '/usr/share/collectd/types.db.rabbitmq':
+  @file { '/usr/lib/collectd/python/rabbitmq.py':
     ensure => present,
-    source => 'puppet:///modules/collectd/usr/share/collectd/types.db.rabbitmq',
+    source => 'puppet:///modules/collectd/usr/lib/collectd/python/rabbitmq.py',
     tag    => 'collectd::plugin',
     notify => File['/etc/collectd/conf.d/rabbitmq.conf'],
   }
 
+  @file { '/usr/lib/collectd/python/rabbitmq.pyc':
+    ensure => undef,
+    tag    => 'collectd::plugin',
+  }
+
   @collectd::plugin { 'rabbitmq':
     content => template('collectd/etc/collectd/conf.d/rabbitmq.conf.erb'),
-    require => Package['collectd-rabbitmq'],
+    require => Class['collectd::plugin::python'],
   }
 }
