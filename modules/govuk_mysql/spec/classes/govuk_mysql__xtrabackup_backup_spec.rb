3ece5e9f367ff501e393fdb64f4b3f908a67ef7b
Laura Martin <surminus@gmail.com>
2016-04-13 12:27:25 +0100

    Change gof3r path to full
    Got the following error on the cron run:
    
    `envdir: fatal: unable to run gof3r: file does not exist`
    
    Changed from just "gof3r" to "/usr/local/bin/gof3r" in the command.

--- modules/govuk_mysql/spec/classes/govuk_mysql__xtrabackup_backup_spec.rb
+++ modules/govuk_mysql/spec/classes/govuk_mysql__xtrabackup_backup_spec.rb
@@ -18,6 +18,6 @@ describe 'govuk_mysql::xtrabackup::backup', :type => :class do
     it { is_expected.to contain_file('/etc/mysql/xtrabackup/env.d').with_ensure('directory') }
     it { is_expected.to contain_file('/etc/mysql/xtrabackup/env.d/AWS_SECRET_ACCESS_KEY').with_content(/walk-on-the-wild-side/) }
     it { is_expected.to contain_file('/etc/mysql/xtrabackup/env.d/AWS_ACCESS_KEY_ID').with_content(/lou-reed/) }
-    it { is_expected.to contain_file('/usr/local/bin/xtrabackup_s3_base').with_content(/.*innobackupex\ --extra-lsndir='\/var\/lib\/mysql\/'\ --encrypt=AES256\ --encrypt-key="thisisnotarealkey"\ --stream=xbstream\ --compress\ \.\ \|\ envdir \/etc\/mysql\/xtrabackup\/env\.d\ gof3r\ put\ --endpoint s3-eu-west-1\.amazonaws\.com\ -b\ foo\ -k\ \$\(cat \/var\/lib\/mysql\/xtrabackup_date\)\/base\.xbcrypt.*/) }
+    it { is_expected.to contain_file('/usr/local/bin/xtrabackup_s3_base').with_content(/.*innobackupex\ --extra-lsndir='\/var\/lib\/mysql\/'\ --encrypt=AES256\ --encrypt-key="thisisnotarealkey"\ --stream=xbstream\ --compress\ \.\ \|\ envdir \/etc\/mysql\/xtrabackup\/env\.d\ \/usr\/local\/bin\/gof3r\ put\ --endpoint s3-eu-west-1\.amazonaws\.com\ -b\ foo\ -k\ \$\(cat \/var\/lib\/mysql\/xtrabackup_date\)\/base\.xbcrypt.*/) }
   end
 end
