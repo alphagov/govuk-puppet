08a99784522ef384740ec53163999fd2e8190e6f
Tom Hipkin <tomhipkin@gmail.com>
2016-04-13 16:36:57 +0100

    Add a more secure SSH key
    2048 bit keys are deemed not strong enough by the CI so here is a
    stronger one.

--- modules/users/manifests/tomhipkin.pp
+++ modules/users/manifests/tomhipkin.pp
@@ -3,6 +3,6 @@ class users::tomhipkin {
   govuk_user { 'tomhipkin':
       fullname => 'Tom Hipkin',
       email    => 'tomhipkin@gmail.com',
-      ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvAK56Pn2Ml6KBJOlXZ7VTZ9PuNOXwgu1lMa46bSWve//tfojAJEL7jx+b4In7Znr//AXz1UZQovFOvns1HrKoA+p6YlyVKXb+7qwcHpt2exHUV00jWBUuVeFqt+CRDIiXnTc0P3Udzy4RDo9c1DoN2O61eerZhdfACAEu/i+2e+GBdGnK3ANL9xdoQPfhlz9cRIfCksBvgDGHkbZa+25nxQz9MLLep9unbPbAjYU9bsWX6ARh4qfq6R9hO7ZeXxC0FbfK7jaSpEDsj/HLOV6iurWFvUXQbqLoft7tGqMU3TnEbVzkXpSS8t1jNOWAtkaKnkga2uPkSBBGR++M3QnH dxw',
+      ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC7sZPOQY+vKJBdyWJqsio+9sSFyAfzvMxlnKCt/FC3qQhRzrvqQsyPf0yl01XcVv7C5hrJCaOIL+WBonp/Ce2G5Q4oN5ZPrbA2on30rIm177V1Lz50TnwrcU0A51S7fmXjcoMNwRqpdYjf/yiwwn0MnLwTpjHU5OISgabsgIytPJ0HV+by7mj2cqdpiiuirSqm7nFo4Ng75ROGeoiGtzr5OvYRVGemmD//uML7fTwfFbbTkvxtAP1HxkyMKIblO8vakFboQKrkujUQSpHWSNznEljhKLD+hv6jnJ1kx0anIKQY/F8kkf/koRqjUfjv52GU2veRLmdDZJxxma5sRI2++CruFleFswSLzDdRDAQ9pf546afEPu8xgKFuGHxiVCykp1oRkXdC0O2CPxXMUMRx/22bfqHWipEXDdKXUUrUpU4ZFViyaB9Icz8vrgE2OAwTBZ0vWJaFDEXpmZsWluDfx2tcf0kfB8y7Xt0sbpEmyx3qZCDyaqdHcST+cAl87YnDYrilEZHR3q+xXPhRl/PxqWKo0+ExqFSU8u2RT+OqYA1r87YSdmMm10NIS2Czp5pZBcDoKhtV1vcljHvzeej98hQDmuayLDqowrbSjg33U0m1fbZevY36GimSFruWsVnodbH1kGpD+Fso2FILyGS8io/7eJ+Im7dKF8b9bxWlXw== dxw',
     }
 }
