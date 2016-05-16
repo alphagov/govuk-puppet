4d33c965abeccabe1b807b255de824f4593ff53e
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2016-05-16 15:49:28 +0100

    Puppet deploy a collectd file handle plugin

---
+++ modules/collectd/manifests/plugin/file_handles.pp
@@ -0,0 +1,22 @@
+# == Class: collectd::plugin::file_handles
+#
+# Monitors the number of file handles in use on a system
+#
+class collectd::plugin::file_handles(
+) {
+
+  @file { '/usr/lib/collectd/file_handles.sh':
+    ensure  => 'present',
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    content => file('collectd/usr/lib/collectd/file_handles.sh'),
+    tag     => 'collectd::plugin',
+  }
+
+  @collectd::plugin { 'file_handles':
+    content => template('collectd/etc/collectd/conf.d/file_handles.conf.erb'),
+    require => File['/usr/lib/collectd/file_handles.sh'],
+  }
+
+}
