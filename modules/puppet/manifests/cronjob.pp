d3bb39c317e6eb683ed711f9d403953c90ba9d06
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-05-24 17:45:14 +0100

    Add enabled parameter to Puppet cronjob

--- modules/puppet/manifests/cronjob.pp
+++ modules/puppet/manifests/cronjob.pp
@@ -2,16 +2,25 @@
 #
 # Sets up a cronjob that runs Puppet periodically.
 #
-class puppet::cronjob {
+# === Parameters
+#
+# [*enabled*]
+#   Boolean, whether or not the cronjob should be created.
+#
+class puppet::cronjob (
+  $enabled = true,
+) {
 
-  $first = fqdn_rand(30)
-  $second = $first + 30
+  if $enabled {
+    $first = fqdn_rand(30)
+    $second = $first + 30
 
-  cron { 'puppet':
-    ensure  => present,
-    user    => 'root',
-    minute  => [$first, $second],
-    command => '/usr/local/bin/govuk_puppet',
-    require => File['/usr/local/bin/govuk_puppet'],
+    cron { 'puppet':
+      ensure  => present,
+      user    => 'root',
+      minute  => [$first, $second],
+      command => '/usr/local/bin/govuk_puppet',
+      require => File['/usr/local/bin/govuk_puppet'],
+    }
   }
 }
