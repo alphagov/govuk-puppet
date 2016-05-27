8a8338be33dd5782f349e7ff9d0fc71ba4a7f4e4
Mikael <mikellallison@gmail.com>
2016-05-27 10:52:45 +0100

    Adding Stronger ssh key

--- modules/users/manifests/mikaelallison.pp
+++ modules/users/manifests/mikaelallison.pp
@@ -3,6 +3,6 @@ class users::mikaelallison {
   govuk_user { 'mikaelallison':
     fullname => 'Mikael Allison',
     email    => 'mikael.allison@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6T5vb0lt6T8yhcAGr0d2VKlhN4r+f7K6cSITOp/3s9YPZSehhAKo65bdt3lSXo/NqB4xN9NJIn/zVMfDLLmSgLhmVLJe2d86HCQ8lW9m+nxZuHNamQYNxhpzK3H+3WD82Gu66rc+123SrQMA0rydCQJw0D69Z1C72S0ngsRhNwevA7ouTDu+dbOlioANa9fKskPmmXHQAM2FfgDCQui0m7LC6tCc9uj/FSsl2HtX0SxBxB2o9AC4b6cT0tR5DJfjR+/wJk+VJMHKnJKwKHY+1mUIY831UJ4P9X8+ykJJqtTbunRE/Z1eNTCBlDycvxK98YH+08MmDnMpHKEOoHkmp mikaela@MacBook-Pro.local',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCb6Tz5ZaPAph0TT7Jh1CIQ+iTYuS/TxeMVqj3+DeEYORRbGBAG12lX1Ma95q+M+iuFj7m3bbYiMDtSzflIKhqN1ymlv/A2xyCqsiNSGx0Zl978Q85oSzzeh0//nzet+pn0QxEpb1d2e7gcHT1hakp802PuPVSMZycQqZa2VvNKC+OEEpPJKJ98U1XxYR4vR9hCqXhnq7uwLWr5QNUxSlsrZd60RawRitOBqSU6bMZCGKjA+HTz3agmcVa5C9cISAxx34NP7UHDEyEp6825y+IJtcXigIDutzR7kzBkoXLz8IV1ccgpLCjTQyRQjtV4q51oifs0NkIey84yykdUAshyY4g8YG+hv2NU1rLBTtG5MM4yYAdMAiNRz6dW+C+3zM5fZvOmk/MW6lcFIX9mRr0MSp7Z0sd0inVyj5nkug2NVsGrTK8Hsgm3jIHw0nFRBDuVd1jybLuy6a/yf7epfMz6Idhh7JDeu+96u9j7Yhux0q7a4R2Y1O5xGpUthUJmgzr4mJ8UfxQgCWBP+u+ZWUjV3eGK+JfbXCWwSGdGwugVjxfJB/VGbUVr5vBThyuDXh4OQp4GkOUAl1810BbsTltF+ly0W+fYK5npWnu8EsavLqzsIiXcd4YIoQ2hYa7/xMcgwTu26K7C8svKcYvsapD3ZMGvNHlhHgcjhuu32Kb4Gw== mikaela@MacBook-Pro.local',
   }
 }
