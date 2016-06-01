d824aea729a6bee5fde55a6ca9ad3e151542dc78
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-06-01 18:32:57 +0100

    Install cloud-aws Elasticsearch plugin everywhere
    This is already on api-elasticsearch and we want to start using it
    on logs-elasticsearch, so it makes sense to install it everywhere.

--- modules/govuk_elasticsearch/manifests/plugins.pp
+++ modules/govuk_elasticsearch/manifests/plugins.pp
@@ -9,4 +9,17 @@ class govuk_elasticsearch::plugins {
     instances  => $::fqdn,
   }
 
+  case $govuk_elasticsearch::version {
+    /^0.90/: { $cloud_aws_version = '1.16.0' }
+    /^1.4/:  { $cloud_aws_version = '2.4.2' }
+    /^1.5/:  { $cloud_aws_version = '2.5.1' }
+    /^1.7/:  { $cloud_aws_version = '2.7.1' }
+    default: { fail('Not able to select a version for cloud-aws, see https://github.com/elastic/elasticsearch-cloud-aws') }
+  }
+
+  elasticsearch::plugin { "elasticsearch/elasticsearch-cloud-aws/${cloud_aws_version}":
+    module_dir => 'cloud-aws',
+    instances  => $::fqdn,
+  }
+
 }
