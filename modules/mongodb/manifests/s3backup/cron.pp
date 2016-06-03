705b0fbaea2a52baffb22dd9b3ab62a46601c4d6
Mikael <mikellallison@gmail.com>
2016-06-03 13:58:28 +0100

    This commit adds the script /usr/local/bin/{mongodb-restore-s3,mongodb-restore-s3-wrapper}. All the script does is downloads the most recent backup, decrypts it, uncompresses it and restores the backup into mongo. We're able to get the latest backup because amazon s3 stores files in alphabetical,chronological order. Note: this only works when the filenames are timestamped in 'yyyy-mm-dd' format.
    Added quiet flags to commands.  Referenced User variable from Backup.

--- modules/mongodb/manifests/s3backup/cron.pp
+++ modules/mongodb/manifests/s3backup/cron.pp
@@ -1,11 +1,23 @@
 #
-class mongodb::s3backup::cron {
+# Runs a backup of MongoDB to Amazon S3 as a cron job.
+#
+# [*user*]
+#   The user to run the cronjob as.
+#
+class mongodb::s3backup::cron(
+  $user = mongodb::s3backup::backup::user
+) {
 
   cron { 'mongodb-s3backup':
     command => '/usr/local/bin/mongodb-backup-s3-wrapper',
-    user    => $mongodb::s3backup::user,
+    user    => $user,
     minute  => '*/15',
-    require => [Class['mongodb::s3backup::package'],Class['mongodb::s3backup::backup']],
+  }
+
+  cron { 'mongodb-s3restore':
+    command => '/usr/local/bin/mongodb-restore-s3-wrapper',
+    user    =>  $user,
+    hour    => 3,
   }
 
 }
---
+++ modules/mongodb/manifests/s3backup/restore.pp
@@ -0,0 +1,66 @@
+# == Class: mongodb::s3backup::restore
+#
+# Restore a MongoDB backup to a server from s3
+#
+# === Parameters:
+#
+# [*aws_access_key_id*]
+#
+# [*aws_secret_access_key*]
+#
+# [*backup_dir*]
+#   Defines the directory to restore the backups
+#
+# [*env_dir*]
+#   Defines directory for the environment
+#   variables
+#
+# [*private_gpg_key*]
+#   Defines the ascii exported private gpg to
+#   use for encrypting backups. This key should
+#   be created by the user and encrypted with eyaml
+#
+# [*private_gpg_key_fingerprint*]
+#   Defines the fingerprint of the gpg private
+#   key to encrypt the backups. The fingerprint
+#   should be 40 characters without spaces
+#
+# [*s3_bucket*]
+#   Defines the AWS S3 bucket where the backups
+#   will be uploaded. It should be created by the
+#   user
+#
+# [*cron*]
+#   Defines whether to enable the cron job. Value
+#   should be true or false
+class mongodb::s3backup::restore(
+  $aws_access_key_id = undef,
+  $aws_secret_access_key = undef,
+  $env_dir = '/etc/mongo_s3backup',
+  $s3_bucket  = 'govuk-mongodb-backup-s3',
+  $backup_dir = '/var/lib/s3backup',
+  $cron = false
+){
+  include backup::client
+  $backup_user = 'govuk-backup'
+
+  File {
+    owner => $backup_user,
+    group => $backup_user,
+    mode  => '0660',
+  }
+
+  file { '/usr/local/bin/mongodb-restore-s3':
+    ensure  => file,
+    content => template('mongodb/mongodb-restore-s3.erb'),
+    mode    => '0750',
+    require => User[$backup_user],
+  }
+
+  file { '/usr/local/bin/mongodb-restore-s3-wrapper':
+    ensure  => file,
+    content => template('mongodb/mongodb-restore-s3-wrapper.erb'),
+    mode    => '0755',
+    require => User[$backup_user],
+  }
+}
