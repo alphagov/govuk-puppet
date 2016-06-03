8b09f2372627b4209a9945897a3873817ce41325
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-06-03 11:13:42 +0100

    Upgrade Licensify app servers to Oracle Java 8
    This is part of the work that the new Licensify team are doing.
    
    It will let them test Java 8 in integration.
    
    The setup is purposefully more simple than with Java 7. We're not
    caching the Java download anywhere. This may prove to be flaky and
    we might have to revisit it.

--- modules/govuk/manifests/node/s_licensify_backend.pp
+++ modules/govuk/manifests/node/s_licensify_backend.pp
@@ -1,8 +1,24 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::node::s_licensify_backend inherits govuk::node::s_base {
+# == Class: govuk::node::s_licensify_backend
+#
+# Sets up app servers for the Licensify backend
+#
+# === Parameters
+#
+# [*java8*]
+#   Boolean, whether or not to use new upgraded Java. This feature flag
+#   should be removed at some point.
+#
+class govuk::node::s_licensify_backend (
+  $java8 = false,
+) inherits govuk::node::s_base {
+
   include clamav
 
-  include govuk_java::oracle7::jdk
+  if $java8 {
+    include govuk_java::oracle8
+  } else {
+    include govuk_java::oracle7::jdk
+  }
 
   class { 'nginx': }
   class { 'licensify::apps::licensify_admin': }
