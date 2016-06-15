c7c4a2f38bd32dee19f937f96739909a6ae429e4
Mikael <mikellallison@gmail.com>
2016-06-15 11:01:38 +0100

    Modified user variable to reference the correct user for backups.
    Assigned govuk-backup user permissions to write to backup directory. Removed govuk-backups user and wapper script. Removed quiet flag because it is not supported by mongo client version 2.4.9.
    
    setting  user to govuk-backup.
    
    Removing the redirect from mongodump.
    
    Adding FIXME comment to remove resources with ensure absent after this is deployed
    
    Added missing double quote. Specified minute on nightly cron job. Added missing parameter descriptions. Adding bucket suffix.
    
    Cron
    Amended cron resources to use the setlock command.  This will ensure that we do not run this task while another is still in progress.
    Added minute attribute to daily cron job.

--- modules/mongodb/manifests/s3backup/cron.pp
+++ modules/mongodb/manifests/s3backup/cron.pp
@@ -5,23 +5,30 @@
 # [*user*]
 #   The user to run the cronjob as.
 #
+# [*backup_dir*]
+#   The directory where backups are managed
+#
 class mongodb::s3backup::cron(
-  $user = mongodb::s3backup::backup::user
+  $user = 'govuk-backup',
 ) {
 
+  include backup::client
   require mongodb::s3backup::package
   require mongodb::s3backup::backup
 
   cron { 'mongodb-s3backup':
-    command => '/usr/local/bin/mongodb-backup-s3',
+    command => '/usr/bin/setlock -n /var/lock/mongodb-s3backup \
+    /usr/local/bin/mongodb-backup-s3',
     user    => $user,
     minute  => '*/15',
   }
 
   cron { 'mongodb-s3-night-backup':
-    command => '/usr/local/bin/mongodb-backup-s3',
+    command => '/usr/bin/setlock /var/lock/mongodb-s3backup \
+    /usr/local/bin/mongodb-backup-s3 daily',
     user    => $user,
     hour    => '0',
+    minute  => '0',
   }
 
 }
