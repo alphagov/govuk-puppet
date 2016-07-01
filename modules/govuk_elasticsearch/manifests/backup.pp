14f8721d3c1a01d498dc18681aa0161d41bf2ae8
Mikael <mikellallison@gmail.com>
2016-07-01 12:39:27 +0100

    Cron wrapped cron job to prevent this task running where it is not enabled. Indices and Repos Have removed 'ignore unavailable' option from script. To prevent the backups failing I have moved defined indices to node classifier. Renamed local ES repo to something generic. Passive check Added Icinga passive check to script S3_Bucket Renamed to something more generic.
    removing un needed comments

--- modules/govuk_elasticsearch/manifests/backup.pp
+++ modules/govuk_elasticsearch/manifests/backup.pp
@@ -5,8 +5,10 @@
 # === Parameters
 #
 # [*aws_access_key_id*]
+#   AWS key to authenticate requests
 #
 # [*aws_secret_access_key*]
+#   AWS key to authenticate requests
 #
 # [*aws_region*]
 #   AWS region for the S3 bucket
@@ -31,6 +33,9 @@
 # [*es_indices*]
 #   Elaticsearch indexes
 #
+# [*enabled*]
+#   Determines whether this job will be scheduled
+#
 class govuk_elasticsearch::backup(
   $aws_access_key_id = undef,
   $aws_secret_access_key = undef,
@@ -38,60 +43,70 @@ class govuk_elasticsearch::backup(
   $s3_bucket = undef,
   $cron = true,
   $env_dir = '/etc/es_s3backup',
-  $user = backup::client::govuk-backup,
+  $user = 'govuk-backup',
   $es_repo = undef,
-  $housekeeping_enabled = fasle,
+  $enabled = false,
   $es_indices = [
     join(regsubst($govuk_elasticsearch::backup::es_indices, '.*', '"\1"'), ',')]
   ){
 
   include backup::client
+  include govuk_elasticsearch::housekeeping
 
-  if $housekeeping_enabled {
-    include govuk_elasticsearch::housekeeping
-  }
 
-  # push env files
-  file { [$env_dir,"${env_dir}/env.d"]:
-    ensure => directory,
-    owner  => $user,
-    group  => $user,
-    mode   => '0770',
-  }
+    # push env files
+    file { [$env_dir,"${env_dir}/env.d"]:
+      ensure => directory,
+      owner  => $user,
+      group  => $user,
+      mode   => '0770',
+    }
 
-  file { "${env_dir}/env.d/AWS_SECRET_ACCESS_KEY":
-    content => $aws_secret_access_key,
-    owner   => $user,
-    group   => $user,
-    mode    => '0640',
-  }
+    file { "${env_dir}/env.d/AWS_SECRET_ACCESS_KEY":
+      content => $aws_secret_access_key,
+      owner   => $user,
+      group   => $user,
+      mode    => '0640',
+    }
 
-  file { "${env_dir}/env.d/AWS_ACCESS_KEY_ID":
-    content => $aws_access_key_id,
-    owner   => $user,
-    group   => $user,
-    mode    => '0640',
-  }
+    file { "${env_dir}/env.d/AWS_ACCESS_KEY_ID":
+      content => $aws_access_key_id,
+      owner   => $user,
+      group   => $user,
+      mode    => '0640',
+    }
 
-  file { "${env_dir}/env.d/AWS_REGION":
-    content => $aws_region,
-    owner   => $user,
-    group   => $user,
-    mode    => '0640',
-  }
+    file { "${env_dir}/env.d/AWS_REGION":
+      content => $aws_region,
+      owner   => $user,
+      group   => $user,
+      mode    => '0640',
+    }
 
-  file { 'es-backup-s3':
-    path    => '/usr/local/bin',
-    owner   => $user,
-    group   => $user,
-    mode    => '0550',
-    content => template('govuk_elasticsearch/es-backup-s3.erb'),
-  }
+    file { 'es-backup-s3':
+      path    => '/usr/local/bin',
+      owner   => $user,
+      group   => $user,
+      mode    => '0550',
+      content => template('govuk_elasticsearch/es-backup-s3.erb'),
+    }
 
-  cron { 's3-elasticsearch-indexes':
-    command => '/usr/local/bin/es-backup-s3',
-    user    => $user,
-    hour    => 0,
-  }
+  # Remove cron job if backup is not enabled
+    if $enabled {
 
+      cron { 's3-elasticsearch-indexes':
+        ensure  => present,
+        command => '/usr/local/bin/es-backup-s3',
+        user    => $user,
+        hour    => 0,
+        minute  => 0,
+      }
+    }
+    else {
+
+      cron { 's3-elasticsearch-indexes':
+        ensure  => absent,
+      }
+    }
 }
+
