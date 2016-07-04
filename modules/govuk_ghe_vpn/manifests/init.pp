eeb3efea5272cce65a2b989387cb439b15812f31
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-07-04 11:15:20 +0200

    Add notes_url to GitHub Enterprise alert

--- modules/govuk_ghe_vpn/manifests/init.pp
+++ modules/govuk_ghe_vpn/manifests/init.pp
@@ -45,6 +45,7 @@ class govuk_ghe_vpn (
 
   @@icinga::check { "check_ghe_connection_on_${::hostname}":
     check_command       => 'check_nrpe_1arg!check_ghe_responding',
+    notes_url           => monitoring_docs_url('github-enterprise-connectivity', true),
     service_description => 'Connection to GitHub Enterprise over HTTPS',
     host_name           => $::fqdn,
   }
