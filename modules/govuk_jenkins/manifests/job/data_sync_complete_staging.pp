ef8f6a8ae7825f49dbe02ea120492b2d7e17209d
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-07-06 12:53:52 +0200

    Refactor signon domain migration after data sync
    We're going to be migrating more domains so this commit uses an
    array of hashes rather than loads of parameters.

--- modules/govuk_jenkins/manifests/job/data_sync_complete_staging.pp
+++ modules/govuk_jenkins/manifests/job/data_sync_complete_staging.pp
@@ -8,26 +8,13 @@
 # [*auth_token*]
 #   Token to allow this job to be triggered remotely
 #
-# [*signon_domain_old*]
-#   Optional param which will run signon's rake task for
-#   applications:migrate_domain if both this and `$signon_domain_new` are set.
-#
-# [*signon_domain_new*]
-#   See docs for `$signon_domain_old`.
-#
-# [*pp_signon_domain_old*]
-#   Optional param which will run signon's rake task for Performance Platform
-#   applications:migrate_domain if both this and `$pp_signon_domain_new` are set.
-#
-# [*pp_signon_domain_new*]
-#   See docs for `$pp_signon_domain_old`.
+# [*signon_domains_to_migrate*]
+#   An array of hashes containing `old` (the existing Signon domain) and `new`,
+#   (what that domain should be changed to when synced).
 #
 class govuk_jenkins::job::data_sync_complete_staging (
   $auth_token = undef,
-  $signon_domain_old = undef,
-  $signon_domain_new = undef,
-  $pp_signon_domain_old = undef,
-  $pp_signon_domain_new = undef,
+  $signon_domains_to_migrate = [],
 ) {
   file { '/etc/jenkins_jobs/jobs/data_sync_complete.yaml':
     ensure  => present,
