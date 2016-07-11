092926a8ce2944f50a8a6a9a7037392586722892
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-07-11 18:17:58 +0200

    Add alert for minimum number of bouncer requests
    We've previously broken traffic replay for bouncer and not
    noticed.
    
    The metric is prefixed with an at sign (@) because that's Graphite
    for "inverted" (ie alert when it's beneath this value).

--- modules/govuk/manifests/node/s_bouncer.pp
+++ modules/govuk/manifests/node/s_bouncer.pp
@@ -1,5 +1,17 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::node::s_bouncer inherits govuk::node::s_base {
+# == Class: govuk::node::s_bouncer
+#
+# Sets up a machine to run the bouncer app and redirect old domains
+# to GOV.UK.
+#
+# === Parameters
+#
+# [*minimum_request_rate*]
+#   The Graphite metric value for minimum number of requests that
+#   we would expect to see.
+#
+class govuk::node::s_bouncer (
+  $minimum_request_rate = 5,
+) inherits govuk::node::s_base {
 
   include govuk_bouncer::gor
   include govuk::node::s_app_server
@@ -12,5 +24,15 @@ class govuk::node::s_bouncer inherits govuk::node::s_base {
     desc      => 'nginx high conn writing - upstream indicator',
     host_name => $::fqdn,
     notes_url => monitoring_docs_url(nginx-high-conn-writing-upstream-indicator-check),
+  }
+
+  @@icinga::check::graphite { "check_nginx_requests_${::hostname}":
+    target              => "${::fqdn_metrics}.nginx.nginx_requests",
+    warning             => "@${minimum_request_rate * 1.2}",
+    critical            => "@${minimum_request_rate}",
+    desc                => 'Minimum HTTP request rate for bouncer',
+    host_name           => $::fqdn,
+    notes_url           => monitoring_docs_url(nginx-requests-too-low),
+    notification_period => 'inoffice',
   }
 }
