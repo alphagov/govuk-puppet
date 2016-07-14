9edaab6a581e5d578224e43d98584b5ad8cf6f43
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2016-07-14 15:07:15 +0100

    Force some ordering with requires.
    Also manage the service in the right part of the manifest to ensure it
    starts on boot and stays running

--- modules/grafana/manifests/init.pp
+++ modules/grafana/manifests/init.pp
@@ -7,23 +7,29 @@ class grafana {
   include grafana::dashboards
 
   package { 'grafana':
-    ensure => latest,
+    ensure  => latest,
+    require => Class['grafana::repo'],
   }
 
-  file { '/etc/grafana':
-    ensure => directory,
-  }
-
-  file { '/etc/grafana/config.js':
-    ensure => absent,
+  service { 'grafana-server':
+    ensure  => 'running',
+    enable  => true,
+    require => File['/etc/grafana/grafana.ini'],
   }
 
   file { '/etc/grafana/grafana.ini':
-    ensure => file,
-    source => 'puppet:///modules/grafana/grafana.ini',
+    ensure  => file,
+    source  => 'puppet:///modules/grafana/grafana.ini',
+    require => Package['grafana'],
   }
 
   nginx::config::site { 'grafana':
     source => 'puppet:///modules/grafana/vhost.conf',
   }
+
+  # FIXME: remove this code once all grafanasa are upgraded from 1.9
+  file { '/etc/grafana/config.js':
+    ensure => absent,
+  }
+
 }
