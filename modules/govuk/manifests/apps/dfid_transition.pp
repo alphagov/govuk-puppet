f2b4a24d5d0c80614f325338dcf4a0a1b4085809
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-07-18 17:52:04 +0100

    Add a dfid-transition app
    A procfile-only app that starts sidekiq based on dfid-transition's
    Procfile. Runs only in integration. The workers are temporary helpers to
    assist in the transition of DFID research outputs, and as such have a
    shelf life.

---
+++ modules/govuk/manifests/apps/dfid_transition.pp
@@ -0,0 +1,78 @@
+# == Class: govuk::apps::dfid_transition
+#
+# Helps in managing the DFID research outputs import by allowing
+# us to enqueue transition operations (such as downloading the
+# outputs' PDFs). This 'app' has a shelf life which expires
+# when dfid-transition does.
+#
+# Read more: https://github.com/alphagov/dfid-transition
+#
+# === Parameters
+#
+# [*port*]
+#   Not used.
+#   Default: 9999
+#
+# [*enabled*]
+#   Whether the app should exist
+#   Default: false
+#
+# [*enabled_procfile_worker*]
+#   Whether the procfile worker is enabled
+#
+# [*asset_manager_bearer_token*]
+#   The bearer token to use when communicating with Asset Manager.
+#   Default: undef
+#
+# [*publishing_api_bearer_token*]
+#   The bearer token to use when communicating with Publishing API.
+#   Default: undef
+#
+# [*redis_host*]
+#   Redis host for sidekiq and AttachmentIndex.
+#
+# [*redis_port*]
+#   Redis port for sidekiq and AttachmentIndex.
+#   Default: 6379
+class govuk::apps::dfid_transition (
+  $port = 9999,
+  $enabled = true,
+  $enable_procfile_worker = undef,
+  $asset_manager_bearer_token = undef,
+  $publishing_api_bearer_token = undef,
+  $redis_host = undef,
+  $redis_port = undef,
+) {
+  $app_name = 'dfid-transition'
+
+  if $enabled {
+    Govuk::App::Envvar {
+      app => $app_name,
+    }
+
+    govuk::procfile::worker {$app_name:
+      enable_service => $enable_procfile_worker,
+    }
+
+    govuk::app::envvar {
+      "${title}-ASSET_MANAGER_BEARER_TOKEN":
+        varname => 'ASSET_MANAGER_BEARER_TOKEN',
+        value   => $asset_manager_bearer_token;
+      "${title}-PUBLISHING_API_BEARER_TOKEN":
+        varname => 'PUBLISHING_API_BEARER_TOKEN',
+        value   => $publishing_api_bearer_token;
+      "${title}-REDIS_HOST":
+        varname => 'REDIS_HOST',
+        value   => $redis_host;
+      "${title}-REDIS_PORT":
+        varname => 'REDIS_PORT',
+        value   => $redis_port;
+    }
+
+    govuk::app { $app_name:
+      app_type           => 'procfile',
+      port               => $port,
+      enable_nginx_vhost => false,
+    }
+  }
+}
