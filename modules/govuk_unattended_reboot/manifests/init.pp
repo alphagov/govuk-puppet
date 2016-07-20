6521b3f236397eaf8598ea1a57a6020424ccd1f5
Mikael <mikellallison@gmail.com>
2016-07-20 18:03:31 +0100

    What Unattended reboots when invoked, interupt backups. We don't want to reboot while a backup is in progress.
    How
    While running backups we create a lock file under /var/lock.
    The lock file only exists for the lifetime of the backup job.  While the lock file exists,
    the machine doesn't get rebooted.

--- modules/govuk_unattended_reboot/manifests/init.pp
+++ modules/govuk_unattended_reboot/manifests/init.pp
@@ -13,10 +13,21 @@
 #   A hash containing a `username` and a `password` to access monitoring
 #   which is protected by basic authentication.
 #
+# [*lock_files*]
+#   An array of lock files owned by jobs that are in progress.
+#   Add full path of the lock file to hiera.
+#
+# === Variables
+#
+# [check_lockfiles*]
+#   Formatted array of lockfiles. Wrapped in doubles quotes and space separated.
+#
 class govuk_unattended_reboot (
   $enabled = false,
   $monitoring_basic_auth = {},
+  $lock_files = []
 ) {
+
 
   validate_bool($enabled)
   validate_hash($monitoring_basic_auth)
@@ -31,6 +42,7 @@ class govuk_unattended_reboot (
 
   $config_directory = '/etc/unattended-reboot'
   $check_scripts_directory = "${config_directory}/check"
+  $check_lockfiles = join(regsubst($lock_files,'(.*)','"\1"'), ' ')
 
   $node_class_search_phrase = regsubst($::govuk_node_class, '_', '-')
   $icinga_url = "https://alert.cluster/cgi-bin/icinga/status.cgi?search_string=%5E${node_class_search_phrase}-[0-9]&allunhandledproblems&jsonoutput"
@@ -65,6 +77,16 @@ class govuk_unattended_reboot (
     owner  => 'root',
     group  => 'root',
     source => 'puppet:///modules/govuk_unattended_reboot/usr/local/bin/check_icinga.rb',
+  }
+
+  # This script will check for the existence of any lock files relating to backups in progress as not to interrupt them
+
+  file { "${check_scripts_directory}/03_backups":
+    ensure  => $file_ensure,
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    content => template("govuk_unattended_reboot${check_scripts_directory}/backups.erb"),
   }
 
   class { '::unattended_reboot':
