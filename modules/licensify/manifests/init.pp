c1abb0fb6123178922a927d969e8df83e36aa040
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2016-07-28 10:07:40 +0100

    Add docs, remove FIXME:

--- modules/licensify/manifests/init.pp
+++ modules/licensify/manifests/init.pp
@@ -1,4 +1,7 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: licensify
+#
+# Installs and manages the licensify application and dependant resources
+#
 class licensify {
   include licensify::apps
 }
