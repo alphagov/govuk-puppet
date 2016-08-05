32ae75f2104172b5b99e69086956e3b89bd27f54
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-08-05 13:24:47 +0100

    Reminder to delete dfid_transition_import.pp

--- modules/govuk_jenkins/manifests/job/dfid_transition_import.pp
+++ modules/govuk_jenkins/manifests/job/dfid_transition_import.pp
@@ -1,11 +1,11 @@
 # == Class: govuk_jenkins::job::dfid_transition_import
 #
 # Remove the file that was previously parsed by jenkins-job-builder
+# TODO: delete this file when the job is gone from integration
 #
 class govuk_jenkins::job::dfid_transition_import () {
-  file {
-    '/etc/jenkins_jobs/jobs/dfid_transition_import.yaml':
-      ensure  => absent,
-      notify  => Exec['jenkins_jobs_update'];
+  file { '/etc/jenkins_jobs/jobs/dfid_transition_import.yaml':
+    ensure => absent,
+    notify => Exec['jenkins_jobs_update'];
   }
 }
