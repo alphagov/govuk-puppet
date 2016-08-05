d44d1591588686775ae443fb3d619bc7b5191750
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-08-05 13:21:57 +0100

    Ensure the dfid_transition_import job is removed
    This only ever ran on integration and was for samples of data. Now we've
    gone live we're part of Data_Sync and see production data every day.

--- modules/govuk_jenkins/manifests/job/dfid_transition_import.pp
+++ modules/govuk_jenkins/manifests/job/dfid_transition_import.pp
@@ -1,27 +1,11 @@
 # == Class: govuk_jenkins::job::dfid_transition_import
 #
-# Create a file on disk that can be parsed by jenkins-job-builder
+# Remove the file that was previously parsed by jenkins-job-builder
 #
-# === Parameters
-#
-# [*publishing_api_bearer_token*]
-#   The bearer token to use when communicating with Publishing API.
-#   Default: undef
-#
-# [*asset_manager_bearer_token*]
-#   The bearer token to use when communicating with Asset Manager.
-#   Default: undef
-class govuk_jenkins::job::dfid_transition_import (
-  $publishing_api_bearer_token = undef,
-  $asset_manager_bearer_token = undef,
-  $redis_host = undef,
-  $redis_port = 6379
-) {
-
+class govuk_jenkins::job::dfid_transition_import () {
   file {
     '/etc/jenkins_jobs/jobs/dfid_transition_import.yaml':
-      ensure  => present,
-      content => template('govuk_jenkins/jobs/dfid_transition_import.yaml.erb'),
+      ensure  => absent,
       notify  => Exec['jenkins_jobs_update'];
   }
 }
