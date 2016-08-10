414fb31d49e62c349b0be2a957b86d435b7a050d
Tim Blair <tim@bla.ir>
2016-08-10 17:43:41 +0100

    Ensure Go apps are run from the correct path
    Because we build Go apps when running them (rather than running a
    pre-built binary), the source needs to be in the correct location
    (within a workspace at $GOPATH) to build correctly.
    
    This commit checks for the existence of the relevant directory in
    the "wrong" place and moves it to the correct location if necessary.
    It also creates a symlink from the new location to the old one and
    notifies the user that this has happened.

---
+++ development-vm/run_go.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Ensure that the source of a Go app is within the $GOPATH workspace, and then
+# run it.
+
+app_name=$1
+gopath_dir=$(basename $GOPATH)
+org_path="$gopath_dir/src/github.com/alphagov"
+repo_path="$org_path/$app_name"
+
+if [ -d "../$app_name" ] && [ ! -d "../$repo_path" ]; then
+  (
+    cd ..
+    mkdir -p "$org_path"
+    mv "$app_name" "$org_path"
+    ln -s "$repo_path" "$app_name"
+
+    full_path="$GOPATH/src/github.com/alphagov/$app_name"
+    echo "Note: $app_name has been moved to $full_path"
+    echo "Go apps need to be built from within the \$GOPATH ($GOPATH)"
+    echo "A symlink has been created from the original location ($(pwd)/$app_name)"
+  )
+fi
+
+shift
+./run_in.sh "../$repo_path" "$@"
