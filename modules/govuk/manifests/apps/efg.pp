176c6a2fbf989ec7549fc855a06dac9aa55dc225
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-09-19 11:11:49 +0100

    Add environment variables for EFG
    These will help configure the app.

--- modules/govuk/manifests/apps/efg.pp
+++ modules/govuk/manifests/apps/efg.pp
@@ -19,11 +19,21 @@
 # [*devise_secret_key*]
 #   The secret_key setting for Devise, passed in as an environment variable
 #
+# [*exception_recipients*]
+#   App config: a string containing comma-separated email addresses that will
+#   receive exception notifications
+#
+# [*lender_support_email*]
+#   App config: an email address for support requests
+#
 # [*nagios_memory_warning*]
 #   Memory use at which Nagios should generate a warning.
 #
 # [*nagios_memory_critical*]
 #   Memory use at which Nagios should generate a critical alert.
+#
+# [*secret_token*]
+#   The secret token Rails uses to encrypt cookies and stuff like that.
 #
 class govuk::apps::efg (
   $port = '3019',
@@ -31,8 +41,11 @@ class govuk::apps::efg (
   $vhost_name,
   $devise_pepper = undef,
   $devise_secret_key = undef,
+  $exception_recipients = undef,
+  $lender_support_email = undef,
   $nagios_memory_warning = undef,
   $nagios_memory_critical = undef,
+  $secret_token = undef,
 ) {
   validate_string($vhost_name)
 
@@ -59,6 +72,15 @@ class govuk::apps::efg (
     "${title}-DEVISE_SECRET_KEY":
       varname => 'DEVISE_SECRET_KEY',
       value   => $devise_secret_key;
+    "${title}-EXCEPTION_RECIPIENTS":
+      varname => 'EXCEPTION_RECIPIENTS',
+      value   => $exception_recipients;
+    "${title}-LENDER_SUPPORT_EMAIL":
+      varname => 'LENDER_SUPPORT_EMAIL',
+      value   => $lender_support_email;
+    "${title}-SECRET_TOKEN":
+      varname => 'SECRET_TOKEN',
+      value   => $secret_token;
   }
 
   nginx::config::vhost::proxy { $vhost_name:
