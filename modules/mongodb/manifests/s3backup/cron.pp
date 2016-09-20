e708cab63285b95acf294f398c96ce2ada35104b
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2016-09-20 18:05:20 +0100

    Rewrite the class as a define we call once for each type of scheduled backup
    Some hosts will only have one type of backup.

--- modules/mongodb/manifests/s3backup/cron.pp
+++ modules/mongodb/manifests/s3backup/cron.pp
@@ -5,32 +5,43 @@
 # [*user*]
 #   The user to run the cronjob as.
 #
+# [*backup_type*]
+#   Type of backup to run, daily or incremental.
+#
+# [*hour*]
+#   The hour specifier the cornjob should run at
+#
+# [*minute*]
+#   The minute specifier the cornjob should run at
 #
 class mongodb::s3backup::cron(
-  $user = 'govuk-backup',
-  $realtime_hour = '*',
-  $realtime_minute = '*/15',
-  $daily_hour = 0,
-  $daily_minute = 0,
+  $backup_type = 'daily',
+  $user        = 'govuk-backup',
+  $hour        = '*',
+  $minute      = '*/15',
 ) {
 
   include ::backup::client
   require mongodb::s3backup::package
   require mongodb::s3backup::backup
 
-  # Here we use setlock to prevent the jobs from running asynchronously
-  cron { 'mongodb-s3backup-realtime':
-    command => '/usr/bin/ionice -c 2 -n 6 /usr/bin/setlock -n /etc/unattended-reboot/no-reboot/mongodb-s3backup /usr/local/bin/mongodb-backup-s3',
+  cron { "mongodb-s3backup-${backup_type}":
+    command => "/usr/bin/ionice -c 2 -n 6 /usr/bin/setlock -n /etc/unattended-reboot/no-reboot/mongodb-s3backup /usr/local/bin/mongodb-backup-s3 ${backup_type}",
     user    => $user,
-    hour    => $realtime_hour,
-    minute  => $realtime_minute,
+    hour    => $hour,
+    minute  => $minute,
+  }
+
+
+  # remove old cronjobs
+  cron { 'mongodb-s3backup-realtime':
+    ensure => 'absent',
+    user   => $user,
   }
 
   cron { 'mongodb-s3-night-backup':
-    command => '/usr/bin/ionice -c 2 -n 6 /usr/bin/setlock /etc/unattended-reboot/no-reboot/mongodb-s3backup /usr/local/bin/mongodb-backup-s3 daily',
-    user    => $user,
-    hour    => $daily_hour,
-    minute  => $daily_minute,
+    ensure => 'absent',
+    user   => $user,
   }
 
 }
