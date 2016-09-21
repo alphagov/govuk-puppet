29da7448b80e3a64e968e8cfe1d696af6df104b5
Laura Martin <surminus@gmail.com>
2016-09-21 14:02:47 +0100

    Add Whitehall MySQL S3 backups
    This adds all the configuration to allow backups and restores for MySQL
    S3 backups for the s_whitehall_mysql_backup and s_whitehall_mysql_master
    machines as applicable.

--- modules/govuk/manifests/node/s_whitehall_mysql_backup.pp
+++ modules/govuk/manifests/node/s_whitehall_mysql_backup.pp
@@ -1,7 +1,38 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::node::s_whitehall_mysql_backup inherits govuk::node::s_whitehall_mysql_slave {
+# == Class: Govuk::Node::S_whitehall_mysql_backup
+#
+#  Set up a GOV.UK Whitehall MySQL Backup machine
+#
+# === Parameters:
+#
+#  [*aws_access_key_id*]
+#    AWS access key ID for the S3 bucket
+#
+#  [*aws_secret_access_key*]
+#    AWS secret key for the S3 bucket
+#
+#  [*s3_bucket_name*]
+#    Name of the S3 bucket
+#
+#  [*encryption_key*]
+#    Encryption key that encrypts the backup for S3
+#
+class govuk::node::s_whitehall_mysql_backup (
+  $aws_access_key_id = undef,
+  $aws_secret_access_key = undef,
+  $s3_bucket_name = undef,
+  $encryption_key = undef,
+) inherits govuk::node::s_whitehall_mysql_slave {
   class { 'backup::mysql':
     mysql_dump_password => hiera('mysql_root',''),
     require             => Govuk_mount['/var/lib/automysqlbackup'],
+  }
+
+  if $s3_bucket_name and $aws_access_key_id and $aws_secret_access_key {
+    govuk_mysql::xtrabackup::backup { $::hostname:
+      aws_access_key_id     => $aws_access_key_id,
+      aws_secret_access_key => $aws_secret_access_key,
+      s3_bucket_name        => $s3_bucket_name,
+      encryption_key        => $encryption_key,
+    }
   }
 }
--- modules/govuk/manifests/node/s_whitehall_mysql_master.pp
+++ modules/govuk/manifests/node/s_whitehall_mysql_master.pp
@@ -4,11 +4,27 @@
 #
 # === Parameters
 #
-# [*whitehall_fe_password*]
-#   Password for a MySQL account called `whitehall_fe`.
+#  [*whitehall_fe_password*]
+#    Password for a MySQL account called `whitehall_fe`.
+#
+#  [*aws_access_key_id*]
+#    AWS access key ID for the S3 bucket
+#
+#  [*aws_secret_access_key*]
+#    AWS secret key for the S3 bucket
+#
+#  [*s3_bucket_name*]
+#    Name of the S3 bucket
+#
+#  [*encryption_key*]
+#    Encryption key that decrypts the backup
 #
 class govuk::node::s_whitehall_mysql_master (
   $whitehall_fe_password = '',
+  $aws_access_key_id = undef,
+  $aws_secret_access_key = undef,
+  $s3_bucket_name = undef,
+  $encryption_key = undef,
 ) inherits govuk::node::s_base {
   $replica_password = hiera('mysql_replica_password', '')
   $root_password = hiera('mysql_root', '')
@@ -36,4 +52,13 @@ class govuk::node::s_whitehall_mysql_master (
   }
 
   Govuk_mount['/var/lib/mysql'] -> Class['govuk_mysql::server']
+
+  if $s3_bucket_name and $aws_access_key_id and $aws_secret_access_key {
+    govuk_mysql::xtrabackup::restore { $::hostname:
+      aws_access_key_id     => $aws_access_key_id,
+      aws_secret_access_key => $aws_secret_access_key,
+      s3_bucket_name        => $s3_bucket_name,
+      encryption_key        => $encryption_key,
+    }
+  }
 }
