289347346507c52b2070bbf1b494aef428478462
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2016-09-23 15:25:15 +0100

    Upgrade the ruby version used by puppet to 2.1.8
    We're currently running a very old, unsupported, ruby version and
    we should upgrade to a newer, still security patch supported one.
    
    2.1.8 was picked as it's quite new (comparatively speaking) and we have
    packages built for it.
    
    Install ruby 2.1.8 everywhere as we're going to use it for puppet
    
    We're leaving 1.9.3 in for now while we upgrade the puppet used ruby.
    We're also unsure if we have any other scripts running that use this version.
    
    Bump the puppet version

--- modules/govuk_rbenv/manifests/init.pp
+++ modules/govuk_rbenv/manifests/init.pp
@@ -6,13 +6,20 @@
 class govuk_rbenv {
   include rbenv
 
-  unless $::lsbdistcodename == 'xenial' {
-    rbenv::version { '1.9.3-p550':
-      bundler_version => '1.7.4',
-    }
-    rbenv::alias { '1.9.3':
-      to_version => '1.9.3-p550',
-    }
+  rbenv::version { '2.1.8':
+    bundler_version => '1.10.6',
+  }
+  rbenv::alias { '2.1':
+    to_version => '2.1.8',
+  }
+
+  # FIXME: we're leaving this in for now while we upgrade the puppet used ruby. 
+  # We're also unsure if we have any other scripts running.
+  rbenv::version { '1.9.3-p550':
+    bundler_version => '1.7.4',
+  }
+  rbenv::alias { '1.9.3':
+    to_version => '1.9.3-p550',
   }
 
 }
