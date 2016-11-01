c23bc1052470ca00e9dca6c6b6bd1b928478bca6
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-11-01 14:32:07 +0000

    Panopticon env vars
    Related to: https://github.gds/gds/deployment/pull/1135

--- modules/govuk/manifests/apps/panopticon.pp
+++ modules/govuk/manifests/apps/panopticon.pp
@@ -9,6 +9,28 @@
 #   The port that panopticon API is served on.
 #   Default: 3003
 #
+# [*enable_procfile_worker*]
+#   Whether to enable the procfile worker
+#   Default: true
+#
+# [*errbit_api_key*]
+#   Errbit API key used by airbrake
+#
+# [*mongodb_name*]
+#   The Mongo database to be used.
+#
+# [*mongodb_nodes*]
+#   Array of hostnames for the mongo cluster to use.
+#
+# [*oauth_id*]
+#   Sets the OAuth ID
+#
+# [*oauth_secret*]
+#   Sets the OAuth Secret Key
+#
+# [*publishing_api_bearer_token*]
+#   The bearer token to use when communicating with Publishing API.
+#
 # [*rabbitmq_hosts*]
 #   RabbitMQ hosts to connect to.
 #   Default: localhost
@@ -21,33 +43,22 @@
 #   RabbitMQ password.
 #   Default: panopticon
 #
-# [*enable_procfile_worker*]
-#   Whether to enable the procfile worker
-#   Default: true
-#
-# [*publishing_api_bearer_token*]
-#   The bearer token to use when communicating with Publishing API.
-#   Default: undef
-#
 # [*secret_key_base*]
 #   The key for Rails to use when signing/encrypting sessions.
 #
-# [*mongodb_name*]
-#   The Mongo database to be used.
-#
-# [*mongodb_nodes*]
-#   Array of hostnames for the mongo cluster to use.
-#
 class govuk::apps::panopticon(
   $port = '3003',
+  $enable_procfile_worker = true,
+  $errbit_api_key = undef,
+  $mongodb_name = undef,
+  $mongodb_nodes = undef,
+  $oauth_id = undef,
+  $oauth_secret = undef,
+  $publishing_api_bearer_token = undef,
   $rabbitmq_hosts = ['localhost'],
   $rabbitmq_user = 'panopticon',
   $rabbitmq_password = 'panopticon',
-  $enable_procfile_worker = true,
-  $publishing_api_bearer_token = undef,
   $secret_key_base = undef,
-  $mongodb_name = undef,
-  $mongodb_nodes = undef,
 ) {
   govuk::app { 'panopticon':
     app_type           => 'rack',
@@ -64,6 +75,15 @@ class govuk::apps::panopticon(
   }
 
   govuk::app::envvar {
+    "${title}-ERRBIT_API_KEY":
+      varname => 'ERRBIT_API_KEY',
+      value   => $errbit_api_key;
+    "${title}-OAUTH_ID":
+      varname => 'OAUTH_ID',
+      value   => $oauth_id;
+    "${title}-OAUTH_SECRET":
+      varname => 'OAUTH_SECRET',
+      value   => $oauth_secret;
     "${title}-PUBLISHING_API_BEARER_TOKEN":
       varname => 'PUBLISHING_API_BEARER_TOKEN',
       value   => $publishing_api_bearer_token;
