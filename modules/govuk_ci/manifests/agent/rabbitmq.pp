26680751e12a7477f7e2792bad0133fca081c4be
Mikael <mikellallison@gmail.com>
2016-11-04 15:05:05 +0000

    Adding Jenkins Job configuration for RabbitMQ.
    This class provisions users, user permissions and an exchange.

--- modules/govuk_ci/manifests/agent/rabbitmq.pp
+++ modules/govuk_ci/manifests/agent/rabbitmq.pp
@@ -3,5 +3,52 @@
 # Installs and configures rabbitmq-server
 #
 class govuk_ci::agent::rabbitmq {
-  include ::govuk_rabbitmq
+  contain ::govuk_rabbitmq
+
+  rabbitmq_user {
+    'content_register_test':
+      password => 'content_register';
+    'email_alert_service_test':
+      password => 'email_alert_service_test';
+    'govuk_seed_crawler':
+      password => 'govuk_seed_crawler';
+    'govuk_crawler_worker':
+      password => 'govuk_crawler_worker',
+      tags     => ['monitoring'];
+    'publishing_api':
+      password => 'publishing_api';
+  }
+
+  rabbitmq_user_permissions {
+    'content_register_test@/':
+      configure_permission => '^(amq\.gen.*|content_register_test)$',
+      read_permission      => '^(amq\.gen.*|content_register_test|content_register_published_documents_test_exchange)$',
+      write_permission     => '^(amq\.gen.*|content_register_test|content_register_published_documents_test_exchange)$';
+    'email_alert_service_test@/':
+      configure_permission => '^(amq\.gen.*|email_alert_service_published_documents_test_exchange|email_alert_service_published_documents_test_queue)$',
+      read_permission      => '^(amq\.gen.*|email_alert_service_published_documents_test_exchange|email_alert_service_published_documents_test_queue)$',
+      write_permission     => '^(amq\.gen.*|email_alert_service_published_documents_test_exchange|email_alert_service_published_documents_test_queue)$';
+    'govuk_seed_crawler@/':
+      configure_permission => '^govuk_seed_crawler.*',
+      read_permission      => '^govuk_seed_crawler.*',
+      write_permission     => '^govuk_seed_crawler.*';
+    'govuk_crawler_worker@/':
+      configure_permission => '^govuk_crawler_worker.*',
+      read_permission      => '^govuk_crawler_worker.*',
+      write_permission     => '^govuk_crawler_worker.*';
+    'publishing_api@/':
+      configure_permission => '^amq\.gen.*$',
+      read_permission      => '^(amq\.gen.*|published_documents_test)$',
+      write_permission     => '^(amq\.gen.*|published_documents_test)$';
+  }
+
+  govuk_rabbitmq::exchange {
+    [
+      'content_register_published_documents_test_exchange@/',
+      'email_alert_service_published_documents_test_exchange@/',
+      'published_documents_test@/',
+    ]:
+      type     => 'topic';
+  }
+
 }
