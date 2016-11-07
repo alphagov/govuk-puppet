b0818009e321022410d87977aea42ec2f0d151ae
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2016-11-07 13:46:01 +0000

    Upgrade Terraform package to 0.7.9
    Upgrade Terraform package in the Jenkins deploy job to 0.7.9

--- modules/govuk_jenkins/manifests/job/deploy_terraform_project.pp
+++ modules/govuk_jenkins/manifests/job/deploy_terraform_project.pp
@@ -27,7 +27,7 @@ class govuk_jenkins::job::deploy_terraform_project (
   }
 
   package { 'terraform':
-    ensure  => '0.7.7',
+    ensure  => '0.7.9',
     require => Apt::Source['terraform'],
   }
 
