5a351617041c41b0fa3b5a3d82f9a98a778cc143
Mikael <mikellallison@gmail.com>
2016-11-07 15:06:52 +0000

    Creating master class for CI.
    Create the base master class for CI.

--- modules/govuk/manifests/node/s_ci_master.pp
+++ modules/govuk/manifests/node/s_ci_master.pp
@@ -3,5 +3,23 @@
 # Class to manage the continuous deployment job controller
 #
 class govuk::node::s_ci_master inherits govuk::node::s_base {
+  include ::nginx
+  include ::govuk_ghe_vpn
+  include ::govuk_rbenv::all
+  include ::govuk_ci::master
 
+  # Close connection if vhost not known
+  nginx::config::vhost::default { 'default':
+    status         => '444',
+    status_message => '',
+  }
+
+  nginx::config::ssl { 'jenkins':
+    certtype => 'wildcard_publishing',
+  }
+
+  nginx::config::site { 'jenkins':
+    content => template('govuk/node/s_jenkins/jenkins.conf.erb'),
+    require => Nginx::Config::Ssl['jenkins'],
+  }
 }
---
+++ modules/govuk_ci/manifests/master.pp
@@ -0,0 +1,10 @@
+# == Class: govuk_ci::master
+#
+# Class to manage continuous deployment master
+#
+class govuk_ci::master {
+
+  include ::govuk_jenkins
+
+}
+
