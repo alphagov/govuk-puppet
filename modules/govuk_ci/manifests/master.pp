ed481c812ccff1ec1fd588401860cf4cab786852
Mikael <mikellallison@gmail.com>
2016-11-10 11:06:41 +0000

    Jenkins firewall rules for CI Master Allows network access for specified services on the jenkins master.

--- modules/govuk_ci/manifests/master.pp
+++ modules/govuk_ci/manifests/master.pp
@@ -7,4 +7,16 @@ class govuk_ci::master {
   include ::govuk_ci::credentials
   include ::govuk_jenkins
 
+  ufw::allow {'jenkins-slave-to-jenkins-master-on-tcp':
+    port  => '32768:65535',
+    proto => 'tcp',
+    ip    => 'any',
+  }
+
+  ufw::allow {'jenkins-slave-to-jenkins-master-on-udp':
+    port  => '33848',
+    proto => 'udp',
+    ip    => 'any',
+  }
+
 }
