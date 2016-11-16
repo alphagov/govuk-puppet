336343d5198a62e76ab1da5515a6e40cbaf9fc0b
Murray Steele <muz@h-lame.com>
2016-11-16 17:12:44 +0000

    Let govuk::app::envvar handle multiline values
    We use envdir (from daemontools) to read the files we write out with
    govuk::app::envvar, and this tool only reads the first line of the file.
    It does however support multiline values - you have to replace newlines
    in the value with null characters.  Envdir will then convert those nulls
    back into newlines before setting the value in the environment.
    
    We use an inlinetemplate command to normalize the newlines and then
    convert all `\n` into `\0`s.

--- modules/govuk/manifests/app/envvar.pp
+++ modules/govuk/manifests/app/envvar.pp
@@ -6,6 +6,11 @@
 # Unlike govuk_envvar, the dependency between the govuk::app::envvar and the
 # application is explicitly encoded, so changing a govuk::app::envvar should
 # automatically restart the relevant application.
+#
+# We use envdir to read the files we place in env.d and this only reads the
+# first line of the file.  To deliver multiline values we have to convert
+# newlines into null characters.  Envdir will then do the reverse when it
+# reads the value out of the file and makes it available in the environment.
 
 define govuk::app::envvar (
   # Name of the application with which this env var is associated
@@ -18,16 +23,21 @@ define govuk::app::envvar (
 ) {
   validate_string($value)
 
+  $nulls_for_newlines_value = inline_template('<%= @value.encode(
+    @value.encoding,
+    universal_newline: true
+  ).gsub(/\n/,"\0") unless @value.nil? %>')
+
   if $notify_service {
     file { "${envdir}/${varname}":
       ensure  => present,
-      content => $value,
+      content => $nulls_for_newlines_value,
       notify  => Govuk::App::Service[$app],
     }
   } else {
     file { "${envdir}/${varname}":
       ensure  => present,
-      content => $value,
+      content => $nulls_for_newlines_value,
     }
   }
 }
