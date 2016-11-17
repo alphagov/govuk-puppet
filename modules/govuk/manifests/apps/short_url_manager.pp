9d0d9a113934ecf47ca79fc8e336cca3bb69d77d
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-11-17 16:53:58 +0000

    Add short-url-manager env vars

--- modules/govuk/manifests/apps/short_url_manager.pp
+++ modules/govuk/manifests/apps/short_url_manager.pp
@@ -4,19 +4,51 @@
 #
 # === Parameters
 #
-# [*port*]
-#   The port that publishing API is served on.
-#   Default: 3078
+# [*errbit_api_key*]
+#   Errbit API key for sending errors.
+#   Default: undef
+#
+# [*mongodb_nodes_string*]
+#   List of mongo hostnames and ports.
+#   Default: undef
+#
+# [*oauth_id*]
+#   Sets the OAuth ID for using GDS-SSO
+#   Default: undef
+#
+# [*oauth_secret*]
+#   Sets the OAuth Secret Key for using GDS-SSO
+#   Default: undef
 #
 # [*publishing_api_bearer_token*]
 #   The bearer token to use when communicating with Publishing API.
 #   Default: undef
 #
+# [*port*]
+#   The port that publishing API is served on.
+#   Default: 3078
+#
+# [*redis_host*]
+#   Redis host for sidekiq.
+#
+# [*redis_port*]
+#   Redis port for sidekiq.
+#   Default: 6379
+#
 class govuk::apps::short_url_manager(
-  $port = '3076',
+  $errbit_api_key = undef,
+  $mongodb_nodes_string = undef,
+  $oauth_id = undef,
+  $oauth_secret = undef,
   $publishing_api_bearer_token = undef,
+  $port = '3076',
+  $redis_host = undef,
+  $redis_port = '6379',
 ) {
-  govuk::app { 'short-url-manager':
+
+  $app_name = 'short-url-manager'
+
+  govuk::app { $app_name:
     app_type           => 'rack',
     port               => $port,
     vhost_ssl_only     => true,
@@ -24,9 +56,28 @@ class govuk::apps::short_url_manager(
     log_format_is_json => true,
   }
 
-  govuk::app::envvar { "${title}-PUBLISHING_API_BEARER_TOKEN":
-    app     => 'short-url-manager',
-    varname => 'PUBLISHING_API_BEARER_TOKEN',
-    value   => $publishing_api_bearer_token,
+  govuk::app::envvar {
+    "${title}-ERRBIT_API_KEY":
+      varname => 'ERRBIT_API_KEY',
+      value   => $errbit_api_key;
+    "${title}-MONGODB_NODES":
+      varname => 'MONGODB_NODES',
+      value   => $mongodb_nodes_string;
+    "${title}-OAUTH_ID":
+      varname => 'OAUTH_ID',
+      value   => $oauth_id;
+    "${title}-OAUTH_SECRET":
+      varname => 'OAUTH_SECRET',
+      value   => $oauth_secret;
+    "${title}-PUBLISHING_API_BEARER_TOKEN":
+      app     => 'short-url-manager',
+      varname => 'PUBLISHING_API_BEARER_TOKEN',
+      value   => $publishing_api_bearer_token;
+  }
+
+  govuk::app::envvar::redis { $app_name:
+    host      => $redis_host,
+    port      => $redis_port,
+    namespace => $app_name,
   }
 }
