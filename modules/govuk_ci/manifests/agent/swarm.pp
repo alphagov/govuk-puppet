1f28da3793033628eadb27aa4d7c04a6019ebefe
Mikael <mikellallison@gmail.com>
2016-11-23 15:53:20 +0000

    Jenkins swarm client
    This commit adds the functionality to connect a Jenkins agent to a Jenkins master with the swarm client.
    
    The swarm client will run on Jenkins agents as an upstart service.
    
    The discovery address is hard coded as a temporary measure until we have a fact to resolve.
    
    The swarm client is a jar file. The jar has been packaged so we can use native package management tools to fetch it.
    see: https://github.com/alphagov/packager/pull/121
    
    There is one manual step where we have to retrieve the api_token for the user we're authenticating against the Jenkins master.
    To automate this would mean opening up a potential security hole.
    reference: [jenkins.security.ApiTokenProperty.showTokenToAdmins](https://wiki.jenkins-ci.org/display/JENKINS/Features+controlled+by+system+properties)
    
    see: https://github.com/alphagov/govuk-puppet/pull/5131

---
+++ modules/govuk_ci/manifests/agent/swarm.pp
@@ -0,0 +1,94 @@
+# Class govuk_ci::agent::swarm
+#
+# Joins a jenkins agent machine to a jenkins master/cluster by downloading and invoking the swarm client.
+#
+# note: The jenkins master requires the swarm plugin before an agent can be assigned with this class.
+#       This class requires an associated API user to be created on the jenkins master. After the user
+#       has been created, you'll need to retrieve the user's API token. The API token is used in place of a
+#       a password to authenticate a jenkins user with the jenkins master. An API token can be
+#       found in the jenkins UI under the user's profile 'http://<JENKINS HOST>:<PORT>/user/<USERNAME>/configure'
+#
+# === Parameters
+#
+# [*swarm_user*]
+#   The user that invokes the swarm client
+#
+# [*agent_labels*]
+#   The agent labels advertise what resources a particular jenkins agent has available.
+#   This is useful if you wanted to isolate a job/task to a jenkins agent to
+#   make use of any unique resouces/dependencies a particular jenkins agent may offer.
+#
+# [*discovery_addr*]
+#   The broadcast IP address of an interface.  The swarm client uses udp to discovery to
+#   discover a jenkins master on the network.
+#
+# [*agent_user_api_token*]
+#   Used to authenticate a jenkins user with the jenkisn master.  The user is defined in:
+#   'govuk_ci::master' as an 'govuk_jenkins::api_user' type.
+#
+#   By default, in newer versions of Jenkins you will be unable to view another
+#   users token. To get past this, temporarily set the following as an argument under
+#   JAVA_ARGS in the init.d script and restart the Jenkins service:
+#
+#   "-Djenkins.security.ApiTokenProperty.showTokenToAdmins=true"
+#
+#   Ref: https://wiki.jenkins-ci.org/display/JENKINS/Features+controlled+by+system+properties
+#
+# [*swarm_client_package*]
+#   The name of the package in our ppa (aptly repo). It also represents the name of the ppa.
+#
+# [*swarm_client_dest*]
+#   Path to the swarm client binary.
+#
+# [*apt_mirror_hostname*]
+#   The base url of the ppa. The value can be found in hiera
+#
+class govuk_ci::agent::swarm(
+  $swarm_user           = 'jenkins',
+  $agent_labels         = [],
+  $discovery_addr       = '10.1.6.255',
+  $agent_user_api_token = undef,     # Corresponding user: 'jenkins_agent'
+  $swarm_client_package = 'jenkins-agent',
+  $swarm_client_dest    = '/usr/local/bin/jenkins-agent',
+  $apt_mirror_hostname  = undef,
+) {
+
+  validate_array($agent_labels)
+  $labels = join($agent_labels, ' ') # Convert the Hiera array to a space separated list
+
+  user { $swarm_user :
+    comment    => 'I run the jenkins swarm client',
+    managehome => true,
+    shell      => '/bin/sh',
+  }
+
+  # The apt source which hosts the package
+  apt::source { $swarm_client_package :
+    location => "http://${apt_mirror_hostname}/${$swarm_client_package}",
+    release  => $::lsbdistcodename,
+    key      => '3803E444EB0235822AA36A66EC5FE1A937E3ACBB',
+  }
+
+  package { $swarm_client_package :
+    ensure  => '2.2',
+    require => Apt::Source[$swarm_client_package],
+  }
+
+  # Upstart script to manage process
+  file {'jenkins-agent.conf':
+    ensure  => file,
+    path    => '/etc/init/jenkins-agent.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    content => template('govuk_ci/jenkins-agent.conf.erb'),
+    require => Package[$swarm_client_package],
+  }
+
+  service { $swarm_client_package :
+    ensure   => running,
+    provider => upstart,
+    require  => File['jenkins-agent.conf'],
+  }
+
+}
