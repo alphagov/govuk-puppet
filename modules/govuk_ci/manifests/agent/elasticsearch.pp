1684e7ac741cacec35c8618900092985579cd68b
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-11-25 12:17:59 +0000

    Remove params for CI agent Elasticsearch
    We can specify these in hieradata.

--- modules/govuk_ci/manifests/agent/elasticsearch.pp
+++ modules/govuk_ci/manifests/agent/elasticsearch.pp
@@ -2,18 +2,7 @@
 #
 # Installs and configures elasticsearch
 #
-# === Parameters
-#
-# [*heap_size*]
-#   Sets the heap size of the jvm
-#
-# [*elasticsearch_version*]
-#   The version of elasticsearch to install
-#
-class govuk_ci::agent::elasticsearch(
-  $heap_size = '64m',
-  $elasticsearch_version = '1.7.5',
-) {
+class govuk_ci::agent::elasticsearch {
   include ::govuk_java::openjdk7::jre
   include ::govuk_java::openjdk7::jdk
 
@@ -25,10 +14,8 @@ class govuk_ci::agent::elasticsearch(
   class { 'govuk_elasticsearch':
     cluster_hosts          => ["${::hostname}:9300"],
     cluster_name           => 'elasticsearch',
-    heap_size              => $heap_size,
     host                   => $::fqdn,
     open_firewall_from_all => true,
-    version                => $elasticsearch_version,
     require                => [Class['govuk_java::openjdk7::jre'],Class['govuk_java::set_defaults']],
   }
 
