70273d6ca411258b2751bb66e95ec0f08bba0c87
Laura Martin <surminus@gmail.com>
2016-11-29 13:44:50 +0000

    Explicitly pass in parameters from s_jenkins class
    To make it less confusing, explicitly pass in parameters for the
    s_jenkins node class in the same way that we are for the
    govuk_ci::master class.

--- modules/govuk/manifests/node/s_jenkins.pp
+++ modules/govuk/manifests/node/s_jenkins.pp
@@ -1,9 +1,37 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::node::s_jenkins inherits govuk::node::s_base {
+# == Class: Govuk::Node::S_jenkins
+#
+# Sets up the deployment Jenkins standalone instance
+#
+# === Parameters:
+#
+# [*github_client_id*]
+#   The Github client ID is used as the user to authenticate against Github.
+#
+# [*github_client_secret*]
+#   The Github client secret is used to authenticate against Github.
+#
+# [*jenkins_api_token*]
+#   The API token for Jenkins user.
+#
+# [*environment_variables*]
+#   A hash of environment variables that should be set for all Jenkins jobs.
+#
+class govuk::node::s_jenkins (
+  $github_client_id = undef,
+  $github_client_secret = undef,
+  $jenkins_api_token = undef,
+  $environment_variables = {},
+) inherits govuk::node::s_base {
   include nginx
-  include govuk_jenkins
   include govuk_ghe_vpn
   include govuk_rbenv::all
+
+  class { 'govuk_jenkins':
+    github_client_id      => $github_client_id,
+    github_client_secret  => $github_client_secret,
+    jenkins_api_token     => $jenkins_api_token,
+    environment_variables => $environment_variables,
+  }
 
   # Close connection if vhost not known
   nginx::config::vhost::default { 'default':
