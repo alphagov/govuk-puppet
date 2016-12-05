c430443fedbafede190536530e8ead1baf0bc093
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2016-12-05 10:22:06 +0000

    Add user suzannehamilton

---
+++ modules/users/manifests/suzannehamilton.pp
@@ -0,0 +1,8 @@
+# Creates the suzannehamilton user
+class users::suzannehamilton {
+  govuk_user { 'suzannehamilton':
+    fullname => 'Suzanne Hamilton',
+    email    => 'suzanne.hamilton@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDW/4fNKxXw+WhcyswZ9gKVbJueQRh/wNy0c40pkUyffFV9+5QZoLz1aMjqur+NWT2HyBk3taAtCMcSb3/9AxoDBkyxGt73VbtFngVv3kXcErpZxwenDj9E7O+0ycsPlpbVWlP0ZokZgh305E3tjAk7PzhKsMB7iTMANJyrJ89BHVLuCB77DlUOYvR6yaeONJ7CXdKS4NZaaxN9NeQ/gKTUicD4t4IaaAFHu4y7v4vnb3obW5IZwzElWhr6qxGPnv6e22MRAtGgDXtPGWF3uh5rwJbW4tiRA7khDZNU7UzixYUCuGira3gfH1mmNxcbW/Opadc/3T45jqYPmZTSyOUPx1IUshVVG7hFXOjgLX7wlW7taK77JbXF+iHPGgEMsRnTfnUley5V4KW+NhIJAfR9YBEwyp1Wd0psJRkELumJto7ZOUKxlQpxL+IkOrIZlx5o960NRRWn/yxPRZyhdkss14aNBYZ68+iXSaUrjroQqJlBgDMPsngTAKLsEuPznxMs9Sj6Yfgz9ZhRErrPFziz4YtgvD8a+Q5LGS6mk62XmxgaHu4Jn5c2shtW2vw4vepmimykWdCgvKPT69FxzuqWtDFkl8he1YNsSk20Myc41qCF9SJp5kPTpdOHuCriWUqgIkQzXvcGbznV+rcTI2G2MQiTIiaaXhCtibcP7IAn9Q== suzanne.hamilton@digital.cabinet-office.gov.uk',
+  }
+}
