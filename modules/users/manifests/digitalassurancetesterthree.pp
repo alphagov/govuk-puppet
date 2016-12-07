f76a8bba11b401495e662f263a9e2ab53f728ce8
Tim Blair <tim@bla.ir>
2016-12-07 14:44:59 +0000

    Make pen-tester names pass linting

--- modules/users/manifests/digitalassurancetesterthree.pp
+++ modules/users/manifests/digitalassurancetesterthree.pp
@@ -1,8 +1,8 @@
-# Creates the digitalassurancetester3 user
-class users::digitalassurancetester3 {
-  govuk_user { 'digitalassurancetester3':
-    fullname => 'digitalassurancetester3',
-    email    => 'digitalassurancetester3@digital.cabinet-office.gov.uk',
+# Creates the digitalassurancetesterthree user
+class users::digitalassurancetesterthree {
+  govuk_user { 'digitalassurancetesterthree':
+    fullname => 'digitalassurancetester three',
+    email    => 'digitalassurancetester.three@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDznvuU2Izj0HSMCg1qOs/7ySGdK1t69JROLJ9bbo+n7q+Ckwgc5DZOlpxLA95GpJvBjxtsyBxejIqK+Wi4ZSZK1i+hDt4595864vAUPBwOqfdih7pnsoxhaVvtzlICUpl0hZr7F6DGc9IYTDIGtrMyMS4TVKclDyuGlfY5lh3nnLI4EHAtcdPPzQIlaVIz9+MNUes7Jm0pO7fn7TC5mRgLuRDIx2GK/AzOpKrttzKDSBGXQwlm85lqy0H4M3RouqC7Vt8wzgXc3wSnjEg2o23BNnDHzbh6PJcFs8HcNiMx5bfpLk2238WYqK6jZcWSJ8rL9k6sS6w2Rgyo4wiVookN8H3YRMI+/MZ6cjvDgUsXQCq8DjqJbw09D7YKzEjFksDt1MGMXwH0A9kNd5aPF+/gORtghLiAsDRZHJ8zZmP6MAkGNfXa3b1VqBTYzKvAaDuzkEHJNEGV5I2r2C5x8FXZjI+Fk8kv2dL/fcKoqBRb1jPekZb7LayvdvGnHLKTcTmuo27q7Am8HsnjFM3sZ8uZsj3uc0kmgr6H8VIVHeFzJItAkoIiTlKgA1oZwJ2Ku3D+GRptbjpO81dY00skVUCw+yCmyaTUVACDyAE19L42Uh9jdefSZY68DGrh0kI6H98aEH1m/Z2ia6DjkyzZTAjQ+saod123LBuhMw0R5zdCbQ== mike@dawk42',
   }
 }
