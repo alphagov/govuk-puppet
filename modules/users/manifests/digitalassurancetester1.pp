b8fa52fbb71f877ed7bc1453e1ad38e295008345
Tim Blair <tim@bla.ir>
2016-12-07 12:14:17 +0000

    Fix pen-test user types

--- modules/users/manifests/digitalassurancetester1.pp
+++ modules/users/manifests/digitalassurancetester1.pp
@@ -1,6 +1,6 @@
 # Creates the digitalassurancetester1 user
 class users::digitalassurancetester1 {
-  govuk::user { 'digitalassurancetester1':
+  govuk_user { 'digitalassurancetester1':
     fullname => 'digitalassurancetester1',
     email    => 'digitalassurancetester1@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDeIl2Sb58lTghd1hok5z8CO9nfU1MRNuz7nsNnsGexuqXw/SjswL66sWHsr0FdrivrCO2aukt3528w2F7Q8pd3Ew4GddEZgGPirmjuY+ntZWbayUPI0RKxoCDEVOSI0ij1HGSOW13FVFd4iHpH+zb3F3RR+hjBZ0vyRsWqxrMfjeEanprZiFYzGC+nz0E6eDIZVB0HaMoWF+Hrht929YxqToN0gvmBpdkAYoA38lqTo5yMUPuV63KEbJpMgc7XoKcZTay59QkTfLVH66SadWVkVySj4Y/pb/PeCD3i3wKYz/VEqeLeUQ3Y5WGlGjaVD2PRDBXnC6YvH9uuze3typSVB4WuD3nXZUd5i3+T642PLd2NK6RP8zEd6+Qxe+Hp0Qp5IeSthKuzZhABkTg09zFjwpIBZ/XRvWVlrP9OnEqxm9k/0Z6hcPsRlwHupGTAVNoO0tAgPD56m18IGxMeXRmBa8y0bZ4qU3Pd1n1k4CpVqafEUsS5Lxsv/HTqih2rEzpMhVwGt1yXIBLr/Hwjcb+EqOGO4GuogAOQcdBTi20QI05GYwIHlQcG8FT+e6B8HBR4O/NWenBzYlrTpn+aFPi8H15McNXoQHV2GSQ79SyI4zBiqQVmB9jRb0vjTPW1f7/dAKqU3Z7RfkkUxCoNef6OiK4lspymCJ7UFpTU0a04qQ== root@mykali',
--- modules/users/manifests/digitalassurancetester2.pp
+++ modules/users/manifests/digitalassurancetester2.pp
@@ -1,6 +1,6 @@
 # Creates the digitalassurancetester2 user
 class users::digitalassurancetester2 {
-  govuk::user { 'digitalassurancetester2':
+  govuk_user { 'digitalassurancetester2':
     fullname => 'digitalassurancetester2',
     email    => 'digitalassurancetester2@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCipoo+H7d+3WSsdoqXWuXYkLTl5qbVHP3FRnRS0mnWG4VY59nuwRaPlxQe9TkWDr4nYptdbjxTFshCYXYGSaMqcgPAP+EUmuakUCzb4uqAv2kD4V/9weI1pQTAKk9ARzA6XDKCYhSizboOC71EfA3Yfy/RcWFbRIgMMg4fSh5ATw9xQCFG9HgB+EgHsqzETg7t3MyJU8Qixd9ljtRD+r1//GEnn9JNduCu5K2ZUgyWAe6NCFDfurYnAX8cjSzva7Deky9dsJukE4o1dQ5OOKwftucqEDIObcp6eVc+/1uuW5X9zQGR/BeXN1kUn4gQWDx3vXmKflJjz2dmdTA84Q/4aDB+GQxZFWs0aSdc9wyhhIo3IQHRwSU4aZTrkvrywOx8HEIaU1egcxizsl+61nqvgg6+/4F39EudaFYhKek8EaEuY2UXaLYZZ0kLLyjvedTAFh1K2CDzkuPtl+KA6OYp9OzQW0YBfhGJCzpFXgWXdOo1DTOjHqiOYIJsUqZp/YnTJFGz9tKQ2d2L+bW/65pPdLLDhZPliJGY7CoY/cyUjhO/l/4uaqSiNi1aq/FNVbveEM1rmxpyiZd8875RrksLudu4U/lf5jkCp5w+nf+Bnfn56kDpxutWzAxLeN2wX+5e5tcKN9lL0IXhvNgE+P3ouq1ojGhBV4PANEW0wOBKKQ== user@computer',
