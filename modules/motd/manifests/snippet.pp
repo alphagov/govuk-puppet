d158b44ceee2388d43d3b7f002b55339ffae872c
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-12-12 13:36:39 +0000

    Remove unused ecryptfs and motd modules
    These were used for the old encrypted licensify MongoDB machines,
    but they're no longer needed.

--- modules/motd/manifests/snippet.pp
+++
@@ -1,9 +0,0 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-define motd::snippet {
-    file { "/etc/update-motd.d/${title}":
-        ensure  => present,
-        source  => "puppet:///modules/motd/etc/update-motd.d/${title}",
-        mode    => '0755',
-        require => File['/etc/update-motd.d'],
-    }
-}
