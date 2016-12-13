553743f59863ba223053774f922e0a45d18e9eb5
Laura Martin <surminus@gmail.com>
2016-12-13 16:45:21 +0000

    Add ssh_slave defined type
    This commit creates a defined type that should be used on a Jenkins
    master that creates the node config to enable adding agents using the
    ssh_slave plugin. This is a replacement of the Jenkins swarm method.
    
    One prerequisite is to generate the "credentials_id" on the Jenkins UI
    as this is directly mapped to the Jenkins instance we are unable to
    programtically generate this.

--- modules/govuk_ci/manifests/agent.pp
+++ modules/govuk_ci/manifests/agent.pp
@@ -2,10 +2,18 @@
 #
 # Class to manage continuous deployment agents
 #
+# === Parameters:
+#
+# [*docker_enabled*]
+#   Set to true to install the Docker service on this agent
+#
+# [*master_ssh_key*]
+#   The public SSH key of the CI master to enable SSH based agent builds
+#
 class govuk_ci::agent(
   $docker_enabled = false,
+  $master_ssh_key = undef,
 ) {
-
   include ::govuk_ci::agent::redis
   if $docker_enabled {
     include ::govuk_ci::agent::docker
@@ -15,10 +23,11 @@ class govuk_ci::agent(
   include ::govuk_ci::agent::mongodb
   include ::govuk_ci::agent::postgresql
   include ::govuk_ci::agent::mysql
-  include ::govuk_ci::agent::swarm
   include ::govuk_ci::vpn
   include ::govuk_java::oracle8
   include ::govuk_jenkins::github_enterprise_cert
+  include ::govuk_jenkins::user
+  include ::govuk_jenkins::pipeline
   include ::govuk_rbenv::all
   include ::golang
 
@@ -39,6 +48,14 @@ class govuk_ci::agent(
   # Override sudoers.d resource (managed by sudo module) to enable Jenkins user to run sudo tests
   File<|title == '/etc/sudoers.d/'|> {
     mode => '0555',
+  }
+
+  ssh_authorized_key { 'ci-master-1.ci':
+    ensure  => present,
+    user    => 'jenkins',
+    type    => 'ssh-rsa',
+    key     => $master_ssh_key,
+    require => Class['::govuk_jenkins::user'],
   }
 
 }
