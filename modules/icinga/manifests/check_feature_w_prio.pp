909e1dea2e02725169dfc5ac59ab3aba3690b483
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2016-12-20 13:45:49 +0000

    Add some puppetdoc for the icinga::check_feature_w_prio define.

--- modules/icinga/manifests/check_feature_w_prio.pp
+++ modules/icinga/manifests/check_feature_w_prio.pp
@@ -1,5 +1,23 @@
+# == Define: icinga::check
+#
 # Checks a feature at a certain priority
-define icinga::check_feature_w_prio ($feature, $prio, $notes_url) {
+#
+# === Parameters:
+#
+# [*feature*]
+#
+# [*prio*]
+#
+# [*notes_url*]
+#   Documentation URL for more information about how to diagnose the service
+#   and why the alert might exist. Should link to a section of the
+#   "opsmanual". This will be included in the Nagios UI and email alerts.
+#
+define icinga::check_feature_w_prio (
+  $feature,
+  $prio,
+  $notes_url,
+) {
   icinga::check { "check_feature_${feature}_${prio}_checker":
     check_command       => "run_smokey_tests!${feature}!${prio}",
     use                 => "govuk_${prio}_priority",
