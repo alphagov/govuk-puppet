128b92a52caebe6aebb39a5c84272576f1a9456f
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2016-12-20 13:31:52 +0000

    Add missing class doc comment in govuk_testing_tools

--- modules/govuk_testing_tools/manifests/init.pp
+++ modules/govuk_testing_tools/manifests/init.pp
@@ -1,4 +1,7 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+# == Class: govuk_testing_tools
+#
+# Installs packages required by testing environments such as CI agents
+#
 class govuk_testing_tools {
   include imagemagick
   include phantomjs
