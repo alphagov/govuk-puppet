1b8b83f5c0f785b1793c383c83cfb39e896b2b16
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-01-27 12:26:01 +0000

    Ensure content-performance-manager absent in prod
    The content-performance-manager is a new app, and is not ready to be
    deployed in production.
    
    When the original puppet code was added for the application, the feature
    flag was missing, so all of the alerts and checks were set up even though
    the application does not exist.
    
    I tried rectifying this by adding the feature flag, but this didn’t
    remove the framework that was already put in place.
    
    Setting `ensure` to absent is one of the first steps in
    [retiring an application](https://github.gds/pages/gds/opsmanual/infrastructure/howto/retiring-an-application.html). I’m hoping that this will remove all of
    the alerts for content-performance-manager
    
    I’ve removed the feature flag because the value for `ensure` can’t be set
    while it exists.

--- modules/govuk/manifests/apps/content_performance_manager.pp
+++ modules/govuk/manifests/apps/content_performance_manager.pp
@@ -8,8 +8,8 @@
 #   The port that it is served on.
 #   Default: 3206
 #
-# [*enabled*]
-#   Whether the application should be enabled. Can be specified for each
+# [*ensure*]
+#   Whether the application should be exist. Can be specified for each
 #   environment using deployment hieradata.
 #
 # [*secret_key_base*]
@@ -44,7 +44,7 @@
 #
 class govuk::apps::content_performance_manager(
   $port = '3206',
-  $enabled = false,
+  $ensure = 'present',
   $secret_key_base = undef,
   $publishing_api_bearer_token = undef,
   $google_analytics_govuk_view_id = undef,
@@ -57,13 +57,12 @@ class govuk::apps::content_performance_manager(
 ) {
   $app_name = 'content-performance-manager'
 
-  if $enabled {
-    govuk::app { $app_name:
-      app_type          => 'rack',
-      port              => $port,
-      health_check_path => '/',
-      asset_pipeline    => true,
-    }
+  govuk::app { $app_name:
+    ensure            => $ensure,
+    app_type          => 'rack',
+    port              => $port,
+    health_check_path => '/',
+    asset_pipeline    => true,
   }
 
   Govuk::App::Envvar {
