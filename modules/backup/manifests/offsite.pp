4ebd386bb33d7fb709394362a0db98f0b12fd7d3
Laura Martin <surminus@gmail.com>
2017-03-14 13:45:12 +0000

    Remove non-S3 backups
    We're now pushing offsite backups to Amazon S3 rather than a separate
    offsite backup machine. This removes the code associated with pushing
    backups to these machines.

--- modules/backup/manifests/offsite.pp
+++ modules/backup/manifests/offsite.pp
@@ -6,38 +6,20 @@
 #
 # === Parameters
 #
-# [*enable*]
-#   Whether to enable the generation and sending of encrypted backups
-#   Default: False
-#
 # [*jobs*]
 #   Hash of `backup::offsite::job` resources. `ensure` parameter will be
 #   added.
-#
-# [*dest_host*]
-#    Hostname or IP of the machine to send the backups to.
-#
-# [*dest_host_key*]
-#    The SSH key to use to authenticate to the destination machine.
 #
 # [*archive_directory*]
 #    Place to store the Duplicity cache - the default is ~/.cache/duplicity
 #
 class backup::offsite(
   $jobs,
-  $dest_host,
-  $dest_host_key,
   $archive_directory,
 ) {
   validate_hash($jobs)
   include backup::client
   include backup::repo
-
-  sshkey { $dest_host:
-    ensure => present,
-    type   => 'ssh-rsa',
-    key    => $dest_host_key,
-  }
 
   create_resources('backup::offsite::job', $jobs, {
     'archive_directory' => $archive_directory,
