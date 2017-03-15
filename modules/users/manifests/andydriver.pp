332587b0a727d0397ff219a0992a716ad73e9a13
Andy Driver <andy@pagezero.net>
2017-03-15 17:21:20 +0000

    Stronger key

--- modules/users/manifests/andydriver.pp
+++ modules/users/manifests/andydriver.pp
@@ -3,6 +3,6 @@ class users::andydriver {
   govuk_user { 'andydriver':
     fullname => 'Andy Driver',
     email    => 'andy.driver@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPVp4escReOGBiKil4wYDm/Gyw6nQjhHSHuytLsBC+RFdVv93MlhXPZmNvB6bcFIwWrykDNfm6fArnxnllB79NhpU/3SnB803kIYZxMl6uBkvC/oVdzNHizhTjho5u+A8D1OZ7sc5c8xLdq6VVjIVAAmrScyEDP4ZA64gtGANfove7ctHZ6WzH/BKl6gh4w6z5rRo1vKVxSEQFWRMh7XW3fXqc5BzaulHGs2D3ThzIC8/R6yAGXZH//nbr+8VK0oRHMGasGeTeXI4rW3lWFZqfnzTKIUo8m6HrYqYvRAYiM8vz4A4X5tIj8ymeTNK8bGNjLJlUGM87TONQR2ohv8JF andy@MacBook-Pro-4.local',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDfAMf+4udHD6k8H8O3XLWZ3MKT7iRkDf1tpMc+kTeLHHKM0xcCDlU9NikQ+fkS9WifPRS51ojFVv2J7xKAajSX0UCsChZxMsUdbwYQPBEx1r/vAUn58amcNqdOpXkppOhxcdfkqIcotbOqux9bEjSLwet/q2s/aY7X5qDDagxfNlZ/ki1libg0tGPzmgD2Sq90EEzlyYjnlbFcyCQRi0+zGDnFu5ztVbPfbAepeNUaFO74KWxHh2XJAiAqQEUFT01Xt+ND40JGdB/t6pD7EtA4KpMWbTzazKF1rcjvGwNp4T05o8Emh9Pcem7nYwPm+/whNerBB8bFXeceSc6rZBEFCItLo+zC+ACY11GJmNqllHT3bzZ2jm28AdzF5aVX4rZ9hXjgG0pTNu8S6u1VOtxEOtMnZzUgNvX8skD/0fKB886IOr7RQ8+N3T92sV5zyzUOqMDUh1whQxM0ycW7kxdLuimbK+bgd72DcE79l64o4xWy3XKlp8INQRF8VC0JtC37GNFlOWXBINj9Ak+SL7YMHYeyBNbEuDPAWoKr/e/XEe2O/hKfH2Lu9QU14QCYvAgbmZfMtzUVEV1Ea8Wst21bZs2BtDDuKdbSdvzoduQ1QqUua5hX1zopcPfqr/huuZOifi/p07geLTXWosms4iEeIBNemyBusa1pKieOgzkMCw== andy.driver@digital.cabinet-office.gov.uk',
   }
 }
