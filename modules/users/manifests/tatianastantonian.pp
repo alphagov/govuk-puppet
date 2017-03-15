b6ee9a93a7319327a8f6d819205c593146627b9f
Tatiana Stantonian <tatianastantonian@gds3822.local>
2017-03-15 11:37:04 +0000

    Update LDAP name and SSH key for tatiana
    My LDAP name has been updated to my married name, Tatiana Stantonian. I also have a new laptop, hence a new SSH key.

---
+++ modules/users/manifests/tatianastantonian.pp
@@ -0,0 +1,8 @@
+# Creates the tatianastantonian user
+class users::tatianastantonian {
+  govuk_user { 'tatianastantonian':
+    fullname => 'Tatiana Stantonian',
+    email    => 'tatiana.stantonian@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDUYPIl+WU5CWwIKn99yRHmyzmk1UDnFJOITxP31xy+CYr2jdOaI2yow7AbPUXKPCa7uOLBfi+a2CgDZMuy3BhEBUWwx/bF1WqBNpRw+XYjr9Khg+d7F9lRP/iMp6JYmsXiYm+ZxIsAnH9zt2H8J0CzmrorDNF4ej6NHnD9zGgRMt55UZcoFQZpYMFYqQJbzQGh3ghdWSVo0xXGPL05p7im0gnv/gHz2bUGyFKONQHr1WWBkrUJhQIMgNYz0RlLHYMvDUDt4Sa4vjubhEFx1KG13dWkKioEyN5Swli/YNo4tJqS9cOdhaes1FdRV2gihhP9gnS5rh4glEtFJtxUq8QXZ5UdifUyPQZ7qtqdRvC4qCvfu5Y0VNTwNCpNf21S54D/JzgYgXOuC5oMSAmkr8oz94p7O47KelismdPxtGCf5Kdkb8YlUrHNbBOcPO0iiHu/+mkLoZlCm0+eUVvZTGn5z6AHNU/svzDekYXFFXkKfOe5vdBD6jOXtQ6sHgU0V8fVarDL3vpfUiFfa3viU5FI7jaerfxRHB8Ba0yVvlCY3vxHgjt8yw2W176wQeUyR9B8PzdS5OLTNXRPJWfwhA7oCtqwLjIhAeCbE2YfVwnaTBol/jrdJLzucFCEkm86jyWmSoQVnum1Zm+ihOuNWY0J8kwTzi08ceyVZP9EPTMwaQ== tatiana.stantonian@digital.cabinet-office.gov.uk',
+  }
+}
