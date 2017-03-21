61541dc423654bdbdca6d58f53ba02aeba61e416
Carlos Vilhena <carlosvilhena@gmail.com>
2017-03-21 11:42:21 +0000

    Slack notifications for Deploy_CDN
    This commit adds slack notifications to the Deploy_CDN job. The changes
    are similar to previous commits like
    4b906fd0a42ac88426f7d53c87f70881a87ecedc.

--- modules/govuk_jenkins/manifests/job/deploy_cdn.pp
+++ modules/govuk_jenkins/manifests/job/deploy_cdn.pp
@@ -2,7 +2,14 @@
 #
 # Create a file on disk that can be parsed by jenkins-job-builder
 #
-class govuk_jenkins::job::deploy_cdn {
+class govuk_jenkins::job::deploy_cdn(
+  $app_domain = hiera('app_domain'),
+) {
+
+  $slack_team_domain = 'govuk'
+  $slack_room = '2ndline'
+  $slack_build_server_url = "https://deploy.${app_domain}/"
+
   file { '/etc/jenkins_jobs/jobs/deploy_cdn.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/deploy_cdn.yaml.erb'),
