1d2de09194aad19d9f6a6af07b6e4bc3e8f6dba3
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2017-04-06 13:07:37 +0000

    Added Andrien Ricketts as a user

---
+++ modules/users/manifests/andrienricketts.pp
@@ -0,0 +1,8 @@
+# Creates andrienricketts user
+class users::andrienricketts {
+  govuk_user { 'andrienricketts':
+    fullname => 'Andrien Ricketts',
+    email    => 'andrien.ricketts@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCZLr9u1qd5rGOR/BxLTAIX0mFfuQ5vnrXGlj3yPFREM1bzu1uQGHNRpH89P6z97n8UNlX2BZMT4JAsWPwglxNfXBsYdBbpXU4kgqsmTHDJktrTTJskSudVZu0zMtOYcGA1DTKjcZLR469obvKh61SxkTOVepwS5AwWia3opp1yIxZbAKrFqvdL8DEUh1tqMtIRrP6hQu0EUnAQpbZRZDNbY5T5e8IDMbx/z5/Y7xBBc3wBRD8iDw1Ol9o5gISdkg4gUxLr5Slc+LwmZFqmce6zx0W2hMWuRRVD5h3o22cqgDVMiPpnh6YVLBlAYqjDB6yLDincPYOLP19vyhHEOV2mApWOM51jsCEZ6795QUtz7cMHPGRR3nxP/K7WhsDFFmaJm+fRTofzBTT1rHo+rbjRfE8KvDwVAvwrWICE6/0XWQ179INRtHZ+hsuI407uWls5RjI5wV4yh6Jc3FyiPCmKCITD9ZvuZ5HVUu2+pSCistSwuCqCz7WFZVUrJ+7evFTNePndYx6eC5b4d7nO4oC5ZXEgpsx1eY1brqiA3cC+G8QJP7x3eLLKAA2LNH8Qeec+cF/91ITOnjX3MOmbgRUv1GuoFLyjVAOltcvsAhnVHRfwlk3R+bk/K3JMXwoLXxuSnzOs+ZmrBLYuju9zCuf+F8CplCRRdYa71ScjtoElqQ== andrien.ricketts@digital.cabinet-office.gov.uk',
+  }
+}
