a46f1ea5192b7df01d241a28c034c687fdd16872
Brenetic <b@brenetic.com>
2017-04-10 10:24:19 +0100

    Move Grafana dashboard panel logic from template
    This logic for deciding which panels are shown on a dashboard now lives
    in the dashboard class itself. This should make it easier to extend and
    customise in the future

--- modules/grafana/manifests/dashboards/deployment_dashboard.pp
+++ modules/grafana/manifests/dashboards/deployment_dashboard.pp
@@ -23,6 +23,24 @@ define grafana::dashboards::deployment_dashboard (
   $app_domain = undef,
   $has_workers = true,
 ) {
+  if $has_workers {
+    $worker_row = [['worker_failures', 'worker_successes']]
+  } else {
+    $worker_row = []
+  }
+
+  $panel_partials = concat(
+    [
+      ['processor_count', '5xx_rate'],
+      ['error_deploy_counts', 'recent_500_count', 'period_500_count', 'links']
+    ],
+    $worker_row,
+    [
+      ['errors_by_controller_action'],
+      ['response_times_by_controller']
+    ]
+  )
+
   file {
     "${dashboard_directory}/deployment_${app_name}.json": content => template('grafana/dashboards/deployment_dashboard_template.json.erb');
   }
