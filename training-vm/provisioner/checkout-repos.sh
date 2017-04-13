10f85d1c51442e8d308fd3f0cafa29cfdd366387
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2017-04-13 11:33:15 +0100

    Update checkout-repos.sh
    Bring `checkout-repos.sh` up-to-date with the development VM version.

--- training-vm/provisioner/checkout-repos.sh
+++ training-vm/provisioner/checkout-repos.sh
@@ -15,14 +15,21 @@ shift $((OPTIND-1))
 
 cd ${REPOS_DIR}
 
-# Takes either a file of repo names, or from stdin
+# Takes either a file of repo names, or from stdin.
+# If the DEPLOYED_TO_PRODUCTION environment variable
+# is set then the "deployed-to-production" branch is
+# checked out rather than "master".
+
+if [ "${DEPLOYED_TO_PRODUCTION}" == "true" ]; then
+  branch="-b deployed-to-production"
+else
+  branch=""
+fi
 
 while read repo
 do
-  OPTION_BRANCH=""
-  if [[ $repo == "govuk-puppet" ]]
-  then
-    OPTION_BRANCH="-b add-training-environment "
+  if [ $repo == "govuk-puppet" ]; then
+    branch="-b add-training-environment"
   fi
-  git clone ${OPTION_BRANCH} https://github.com/alphagov/$repo.git $repo
+  git clone $branch https://github.com/alphagov/$repo.git $repo
 done < "${1:-/dev/stdin}"
