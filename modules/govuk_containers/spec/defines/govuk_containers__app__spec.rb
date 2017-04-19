7e852d5cedb219f7d4caf0f85d3fb5586d73778b
Laura Martin <surminus@gmail.com>
2017-04-19 19:36:37 +0100

    First draft at managing apps as containers
    This commit adds a defined type to pull and run a specified container.
    
    It's function is meant to be the beginning of a construction similar to
    the govuk::app class, but does the minimum neccessary to pull an image
    with the given tag, and run that image.
    
    It allows adding environment variables using encrypted hieradata. These
    will be placed in an init upstart script for the time-being before we
    use proper secret management in docker.
    
    We're using the release app for testing our processes.

---
+++ modules/govuk_containers/spec/defines/govuk_containers__app__spec.rb
@@ -0,0 +1,88 @@
+require_relative '../../../../spec_helper'
+
+describe 'govuk_containers::app', :type => :define do
+  let(:title) { 'bella' }
+
+  let :default_params do
+    {
+      :image => 'cat',
+      :image_tag => 'v1',
+      :port => '1234',
+    }
+  end
+
+  context 'with minimum params' do
+    let(:params) { default_params }
+
+    it do
+      is_expected.to contain_docker__image('cat').with(
+        'ensure' => 'present',
+        'image_tag' => 'v1',
+      )
+      is_expected.to contain_docker__run('bella').with(
+        'image' => 'cat:v1',
+        'ports' => '1234:1234',
+        'extra_parameters' => '--restart=on-failure:3',
+      )
+    end
+  end
+
+  context 'setting environment variables' do
+    let(:params) do
+      {
+        :envvars => { 'cheese' => 'milk', 'wheat' => 'bread' },
+      }.merge(default_params)
+    end
+
+    it do
+      is_expected.to contain_docker__run('bella').with(
+        'image' => 'cat:v1',
+        'ports' => '1234:1234',
+        'env' => {"cheese"=>"milk", "wheat"=>"bread"},
+        'extra_parameters' => '--restart=on-failure:3',
+      )
+    end
+  end
+
+  context 'setting restart_attempts to "never"' do
+    let(:params) do
+      {
+        :restart_attempts => 'never',
+      }.merge(default_params)
+    end
+
+    it do
+      is_expected.to contain_docker__run('bella').with(
+        'image' => 'cat:v1',
+        'ports' => '1234:1234',
+        'extra_parameters' => '--restart=no',
+      )
+    end
+  end
+
+  context 'setting restart_attempts to "always"' do
+    let(:params) do
+      {
+        :restart_attempts => 'always',
+      }.merge(default_params)
+    end
+
+    it do
+      is_expected.to contain_docker__run('bella').with(
+        'image' => 'cat:v1',
+        'ports' => '1234:1234',
+        'extra_parameters' => '--restart=always',
+      )
+    end
+  end
+
+  context 'setting restart_attempts to something other than "always", "never" or a number' do
+    let(:params) do
+      {
+        :restart_attempts => 'foo',
+      }.merge(default_params)
+    end
+
+    it { is_expected.to raise_error(Puppet::Error, /validate_re/) }
+  end
+end
