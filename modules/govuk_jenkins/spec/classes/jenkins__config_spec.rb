d5eb8cf2d65aae5aa1de0a3f8a31c4c9ad2fad88
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2017-05-12 12:08:11 +0100

    Use a different authorisation strategy for CI
    We need to move to using public GitHub for authentication rather than
    GitHub Enterprise.
    
    This commit lets us use teams on GitHub Enterprise to authenticate instead
    of a list of usernames. We can then easily switch to teams on public
    GitHub once this is working.

--- modules/govuk_jenkins/spec/classes/jenkins__config_spec.rb
+++ modules/govuk_jenkins/spec/classes/jenkins__config_spec.rb
@@ -22,5 +22,18 @@ describe 'govuk_jenkins::config', :type => :class do
 
       it { is_expected.to contain_file('/var/lib/jenkins/config.xml').with_content(/<githubWebUri>wibble/) }
     end
+
+    context 'authentication' do
+      let (:params) { default_params.merge({
+        :manage_permissions_github_teams => true,
+        :user_permissions                => [{
+          'user'        => 'admin_user',
+          'permissions' => ['jenkins.perm.admin', 'jenkins.perm.edit'],
+        }],
+      })}
+
+      it { is_expected.to contain_file('/var/lib/jenkins/config.xml').with_content(/<authorizationStrategy class="hudson.security.GlobalMatrixAuthorizationStrategy">/) }
+      it { is_expected.to contain_file('/var/lib/jenkins/config.xml').with_content(/<permission>jenkins.perm.admin:admin_user<\/permission>\n\s+<permission>jenkins.perm.edit:admin_user<\/permission>/) }
+    end
   end
 end
