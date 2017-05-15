cc1c7329b6067522aa99d0212940716e5c268fce
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-05-15 10:26:14 +0100

    Add class and template to create a import Jenkins job for the CPM
    This Jenkins job will be run to load data on a daily basis in to the
    Content Performance Manager (CPM).

---
+++ modules/govuk_jenkins/manifests/job/content_performance_manager.pp
@@ -0,0 +1,22 @@
+# == Class: govuk_jenkins::job::content_performance_manager
+#
+# Create a jenkins job to periodically run rake for the following tasks:
+# - import:all_content_items
+#
+# === Parameters:
+#
+# [*rake_import_all_content_items_frequency *]
+#   The cron timings for the import:all_content_items rake task
+#   Default: undef
+#
+class govuk_jenkins::job::content_performance_manager (
+  $rake_import_all_content_items_frequency = undef,
+) {
+  if $rake_import_all_content_items_frequency != undef {
+    file { '/etc/jenkins_jobs/jobs/content_performance_manager.yaml':
+      ensure  => present,
+      content => template('govuk_jenkins/jobs/content_performance_manager.yaml.erb'),
+      notify  => Exec['jenkins_jobs_update'],
+    }
+  }
+}
