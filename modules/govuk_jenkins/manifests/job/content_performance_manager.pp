97f2852ace00fd7a484ae9f47aa7c8840e268674
Pablo Manrubia <pmanrubia@gmail.com>
2017-05-16 17:36:46 +0100

    Add Jenkins job to collect GA stats

--- modules/govuk_jenkins/manifests/job/content_performance_manager.pp
+++ modules/govuk_jenkins/manifests/job/content_performance_manager.pp
@@ -9,8 +9,13 @@
 #   The cron timings for the import:all_inventory rake task
 #   Default: undef
 #
+# [*rake_import_all_ga_metrics_frequency *]
+#   The cron timings for the import:all_inventory rake task
+#   Default: undef
+#
 class govuk_jenkins::job::content_performance_manager (
   $rake_import_all_inventory_frequency = undef,
+  $rake_import_all_ga_metrics_frequency = undef,
 ) {
   file { '/etc/jenkins_jobs/jobs/content_performance_manager.yaml':
     ensure  => present,
