66067c1cdadfe0ba25a75ad458a02797d72849f5
Pablo Manrubia <pmanrubia@gmail.com>
2017-05-16 17:05:18 +0100

    Rename rake task to run the daily import
    - Improves the display name
    - Updates the rake task that will be invoked

--- modules/govuk_jenkins/manifests/job/content_performance_manager.pp
+++ modules/govuk_jenkins/manifests/job/content_performance_manager.pp
@@ -1,16 +1,16 @@
 # == Class: govuk_jenkins::job::content_performance_manager
 #
 # Create a jenkins job to periodically run rake for the following tasks:
-# - import:all_content_items
+# - import:all_inventory
 #
 # === Parameters:
 #
-# [*rake_import_all_content_items_frequency *]
-#   The cron timings for the import:all_content_items rake task
+# [*rake_import_all_inventory_frequency *]
+#   The cron timings for the import:all_inventory rake task
 #   Default: undef
 #
 class govuk_jenkins::job::content_performance_manager (
-  $rake_import_all_content_items_frequency = undef,
+  $rake_import_all_inventory_frequency = undef,
 ) {
   file { '/etc/jenkins_jobs/jobs/content_performance_manager.yaml':
     ensure  => present,
