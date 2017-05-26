19bdf7a66d9f77c61d526f4d3c397d0ac7642517
Tijmen Brommet <tijmen@gmail.com>
2017-05-26 14:03:36 +0100

    Add environment variables for policy-publisher
    We're making policy-publisher a 12 factor application. This adds the
    necessary environment variables.

--- modules/govuk/manifests/apps/policy_publisher.pp
+++ modules/govuk/manifests/apps/policy_publisher.pp
@@ -15,14 +15,47 @@
 #   The bearer token to use when communicating with Publishing API.
 #   Default: undef
 #
+# [*secret_key_base*]
+#   The key for Rails to use when signing/encrypting sessions.
+#
+# [*errbit_api_key*]
+#   Errbit API key used by airbrake
+#
+# [*db_hostname*]
+#   The hostname of the database server to use in the DATABASE_URL.
+#
+# [*db_username*]
+#   The username to use in the DATABASE_URL.
+#
+# [*db_password*]
+#   The password for the database.
+#
+# [*db_name*]
+#   The database name to use in the DATABASE_URL.
+#
+# [*oauth_id*]
+#   Sets the OAuth ID
+#
+# [*oauth_secret*]
+#   Sets the OAuth Secret Key
+#
 class govuk::apps::policy_publisher(
   $port = '3098',
   $publishing_api_bearer_token = undef,
+  $secret_key_base = undef,
+  $errbit_api_key = '',
+  $db_hostname = undef,
+  $db_username = 'policy_publisher',
+  $db_password = undef,
+  $db_name = 'policy-publisher_production',
+  $oauth_id = '',
+  $oauth_secret = '',
 ) {
+  $app_name = 'policy-publisher'
 
   include govuk_postgresql::client #installs libpq-dev package needed for pg gem
 
-  govuk::app { 'policy-publisher':
+  govuk::app { $app_name:
     app_type           => 'rack',
     port               => $port,
     vhost_ssl_only     => true,
@@ -32,9 +65,39 @@ class govuk::apps::policy_publisher(
     deny_framing       => true,
   }
 
-  govuk::app::envvar { "${title}-PUBLISHING_API_BEARER_TOKEN":
-    app     => 'policy-publisher',
-    varname => 'PUBLISHING_API_BEARER_TOKEN',
-    value   => $publishing_api_bearer_token,
+  Govuk::App::Envvar {
+    app => $app_name,
+  }
+
+  if $secret_key_base {
+    govuk::app::envvar { "${title}-SECRET_KEY_BASE":
+      varname => 'SECRET_KEY_BASE',
+      value   => $secret_key_base;
+    }
+  }
+
+  govuk::app::envvar {
+    "${title}-ERRBIT_API_KEY":
+      varname => 'ERRBIT_API_KEY',
+      value   => $errbit_api_key;
+    "${title}-OAUTH_ID":
+      varname => 'OAUTH_ID',
+      value   => $oauth_id;
+    "${title}-OAUTH_SECRET":
+      varname => 'OAUTH_SECRET',
+      value   => $oauth_secret;
+    "${title}-PUBLISHING_API_BEARER_TOKEN":
+      varname => 'PUBLISHING_API_BEARER_TOKEN',
+      value   => $publishing_api_bearer_token;
+  }
+
+  if $::govuk_node_class !~ /^(development|training)$/ {
+    govuk::app::envvar::database_url { $app_name:
+      type     => 'postgresql',
+      username => $db_username,
+      password => $db_password,
+      host     => $db_hostname,
+      database => $db_name,
+    }
   }
 }
