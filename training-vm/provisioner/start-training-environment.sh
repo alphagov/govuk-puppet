1fbba723f5400f7ff10dec1e202a7e3ae11f56bc
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2017-07-12 14:29:24 +0100

    Set environment variables for training VM apps
    This commit sets some global environment variables for all apps in the training VM. The variables enable the use of signon for authentication, displaying images from production where they don’t exist in the VM, and sending emails to the local Mailhog instance.

--- training-vm/provisioner/start-training-environment.sh
+++ training-vm/provisioner/start-training-environment.sh
@@ -2,15 +2,8 @@
 
 cd "$(dirname "$0")"
 
-# Start all apps that are required in the training environment along with
-# setting the relevant environment variables to use real signon, display
-# images from production where they don't exist locally, and use the local
-# MailHog SMTP port to capture emails. Takes either a file of app names,
-# or from stdin.
-
-export GDS_SSO_STRATEGY=real
-export SHOW_PRODUCTION_IMAGES=true
-export SMTP_PORT=1025
+# Start all apps that are required in the training environment.
+# Takes either a file of app names, or from stdin.
 
 while read app
 do
