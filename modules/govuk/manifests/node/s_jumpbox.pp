cdc25d83b4e4b4303f16b956da3a40f163c141a8
Laura Martin <surminus@gmail.com>
2017-07-25 12:22:56 +0100

    Add ssh-proxy command to jumpboxes in AWS
    The usage would be:
    
    `ssh-proxy delana jenkins`
    
    This looks up the instance IP address using tags so you can jump
    straight to it from the jumpbox. It includes the stackname so that it's
    a generic script that can be called from a client machine on different
    stacks.
    
    If more than one host exists in a role (ie in an auto-scaling group),
    then it will select the first one.

--- modules/govuk/manifests/node/s_jumpbox.pp
+++ modules/govuk/manifests/node/s_jumpbox.pp
@@ -1,4 +1,12 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::node::s_jumpbox inherits govuk::node::s_base {
 
+  if $::aws_migration {
+    file { '/usr/local/bin/ssh-proxy':
+      ensure => 'present',
+      mode   => '0775',
+      source => 'puppet:///modules/govuk/node/s_jumpbox/ssh-proxy',
+    }
+  }
+
 }
