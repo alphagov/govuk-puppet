46136b5987e2942f8b51245179a378cb99aae565
Laura Martin <surminus@gmail.com>
2017-07-28 15:23:24 +0100

    Revert "Point lbs to container "release" in Integration"
    This reverts commit 09915f359f016ea56e6e2c63f507f9405422f297.
    
    We did this for a discovery but will never be used in Production, so
    revert the change so we have real parity across environments.

--- modules/govuk/manifests/node/s_backend_lb.pp
+++ modules/govuk/manifests/node/s_backend_lb.pp
@@ -10,9 +10,6 @@
 # [*backend_servers*]
 #   An array of backend app servers
 #
-# [*docker_backend_servers*]
-#   An array of Docker backend app servers
-#
 # [*performance_backend_servers*]
 #   An array of Performance Platform backend app servers
 #
@@ -28,7 +25,6 @@
 class govuk::node::s_backend_lb (
   $perfplat_public_app_domain = 'performance.service.gov.uk',
   $backend_servers,
-  $docker_backend_servers = [],
   $performance_backend_servers = [],
   $whitehall_backend_servers,
   $publishing_api_backend_servers,
@@ -62,6 +58,7 @@ class govuk::node::s_backend_lb (
       'maslow',
       'policy-publisher',
       'publisher',
+      'release',
       'search-admin',
       'service-manual-publisher',
       'signon',
@@ -110,19 +107,6 @@ class govuk::node::s_backend_lb (
       servers       => $performance_backend_servers,
       internal_only => false,
     }
-  }
-
-  if !empty($docker_backend_servers) {
-    $_backend_servers = $docker_backend_servers
-  } else {
-    $_backend_servers = $backend_servers
-  }
-
-  loadbalancer::balance {
-    [
-      'release',
-    ]:
-      servers => $_backend_servers;
   }
 
   nginx::config::vhost::redirect { "backdrop-admin.${app_domain}" :
