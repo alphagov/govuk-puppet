f781721c1eab83ee4fa1c644779e46741adc828f
Isabell Long <isabell@issyl0.co.uk>
2017-07-28 11:43:27 +0100

    Add ElasticSearch machines to AWS
    - Ignore firewall rules for Carrenza if we're on AWS, as that's all done
      through security groups in Terraform.

--- modules/govuk/manifests/node/s_api_elasticsearch.pp
+++ modules/govuk/manifests/node/s_api_elasticsearch.pp
@@ -7,6 +7,10 @@ class govuk::node::s_api_elasticsearch inherits govuk::node::s_base {
 
   $es_heap_size = floor($::memorysize_mb / 2)
 
+  if $::aws_migration {
+    $aws_cluster_name = "elasticsearch-${::aws_stackname}"
+  }
+
   class { 'govuk_elasticsearch::dump':
     require => Class['govuk_elasticsearch'], # required for elasticsearch user to exist
   }
@@ -19,57 +23,60 @@ class govuk::node::s_api_elasticsearch inherits govuk::node::s_base {
     host                   => $::fqdn,
     open_firewall_from_all => false,
     require                => Class['govuk_java::openjdk7::jre'],
+    aws_cluster_name       => $aws_cluster_name,
   }
 
-  @ufw::allow { 'allow-elasticsearch-http-9200-from-search-1':
-    port    => 9200,
-    from    => getparam(Govuk_host['search-1'], 'ip'),
-    require => Govuk_host['search-1'],
-  }
-  @ufw::allow { 'allow-elasticsearch-http-9200-from-search-2':
-    port    => 9200,
-    from    => getparam(Govuk_host['search-2'], 'ip'),
-    require => Govuk_host['search-2'],
-  }
-  @ufw::allow { 'allow-elasticsearch-http-9200-from-search-3':
-    port    => 9200,
-    from    => getparam(Govuk_host['search-3'], 'ip'),
-    require => Govuk_host['search-3'],
-  }
+  unless $::aws_migration {
+    @ufw::allow { 'allow-elasticsearch-http-9200-from-search-1':
+      port    => 9200,
+      from    => getparam(Govuk_host['search-1'], 'ip'),
+      require => Govuk_host['search-1'],
+    }
+    @ufw::allow { 'allow-elasticsearch-http-9200-from-search-2':
+      port    => 9200,
+      from    => getparam(Govuk_host['search-2'], 'ip'),
+      require => Govuk_host['search-2'],
+    }
+    @ufw::allow { 'allow-elasticsearch-http-9200-from-search-3':
+      port    => 9200,
+      from    => getparam(Govuk_host['search-3'], 'ip'),
+      require => Govuk_host['search-3'],
+    }
 
-  @ufw::allow { 'allow-elasticsearch-http-9200-from-calculators-frontend-1':
-    port    => 9200,
-    from    => getparam(Govuk_host['calculators-frontend-1'], 'ip'),
-    require => Govuk_host['calculators-frontend-1'],
-  }
-  @ufw::allow { 'allow-elasticsearch-http-9200-from-calculators-frontend-2':
-    port    => 9200,
-    from    => getparam(Govuk_host['calculators-frontend-2'], 'ip'),
-    require => Govuk_host['calculators-frontend-2'],
-  }
-  @ufw::allow { 'allow-elasticsearch-http-9200-from-calculators-frontend-3':
-    port    => 9200,
-    from    => getparam(Govuk_host['calculators-frontend-3'], 'ip'),
-    require => Govuk_host['calculators-frontend-3'],
-  }
+    @ufw::allow { 'allow-elasticsearch-http-9200-from-calculators-frontend-1':
+      port    => 9200,
+      from    => getparam(Govuk_host['calculators-frontend-1'], 'ip'),
+      require => Govuk_host['calculators-frontend-1'],
+    }
+    @ufw::allow { 'allow-elasticsearch-http-9200-from-calculators-frontend-2':
+      port    => 9200,
+      from    => getparam(Govuk_host['calculators-frontend-2'], 'ip'),
+      require => Govuk_host['calculators-frontend-2'],
+    }
+    @ufw::allow { 'allow-elasticsearch-http-9200-from-calculators-frontend-3':
+      port    => 9200,
+      from    => getparam(Govuk_host['calculators-frontend-3'], 'ip'),
+      require => Govuk_host['calculators-frontend-3'],
+    }
 
-  # FIXME: Remove these firewall rules by moving the need-api app from the backend
-  # machines to the API machines. The MongoDB database will need to move too.
-  # There are more firewall rules in govuk-provisioning that can also be removed.
-  @ufw::allow { 'allow-elasticsearch-http-9200-from-backend-1':
-    port    => 9200,
-    from    => getparam(Govuk_host['backend-1'], 'ip'),
-    require => Govuk_host['backend-1'],
-  }
-  @ufw::allow { 'allow-elasticsearch-http-9200-from-backend-2':
-    port    => 9200,
-    from    => getparam(Govuk_host['backend-2'], 'ip'),
-    require => Govuk_host['backend-2'],
-  }
-  @ufw::allow { 'allow-elasticsearch-http-9200-from-backend-3':
-    port    => 9200,
-    from    => getparam(Govuk_host['backend-3'], 'ip'),
-    require => Govuk_host['backend-3'],
+    # FIXME: Remove these firewall rules by moving the need-api app from the backend
+    # machines to the API machines. The MongoDB database will need to move too.
+    # There are more firewall rules in govuk-provisioning that can also be removed.
+    @ufw::allow { 'allow-elasticsearch-http-9200-from-backend-1':
+      port    => 9200,
+      from    => getparam(Govuk_host['backend-1'], 'ip'),
+      require => Govuk_host['backend-1'],
+    }
+    @ufw::allow { 'allow-elasticsearch-http-9200-from-backend-2':
+      port    => 9200,
+      from    => getparam(Govuk_host['backend-2'], 'ip'),
+      require => Govuk_host['backend-2'],
+    }
+    @ufw::allow { 'allow-elasticsearch-http-9200-from-backend-3':
+      port    => 9200,
+      from    => getparam(Govuk_host['backend-3'], 'ip'),
+      require => Govuk_host['backend-3'],
+    }
   }
 
   collectd::plugin::tcpconn { 'es-9200':
