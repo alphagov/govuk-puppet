e712b39a99def02a2d3301fac9b7c398f01d7e11
Laura Martin <surminus@gmail.com>
2017-07-31 16:45:18 +0100

    Update plugin conditional and add tests
    I updated the conditional to install the correct cloud-aws plugin for
    the right versions of Elasticsearch, including future proofing for above
    ES5.
    
    I added a parameter so that it was easier to amend the version of tests,
    and added puppet-rspec tests to ensure the conditionals worked as
    expected.

--- modules/govuk_elasticsearch/manifests/plugins.pp
+++ modules/govuk_elasticsearch/manifests/plugins.pp
@@ -2,7 +2,9 @@
 #
 # Install plugins on an Elasticsearch node
 #
-class govuk_elasticsearch::plugins {
+class govuk_elasticsearch::plugins (
+  $elasticsearch_version = $::govuk_elasticsearch::version,
+){
 
   elasticsearch::plugin { 'mobz/elasticsearch-head':
     module_dir => 'head',
@@ -15,16 +17,38 @@ class govuk_elasticsearch::plugins {
     instances  => $::fqdn,
   }
 
-  case $govuk_elasticsearch::version {
-    /^1.4/:  { $cloud_aws_version = '2.4.2' }
-    /^1.5/:  { $cloud_aws_version = '2.5.1' }
-    /^1.7/:  { $cloud_aws_version = '2.7.1' }
-    default: { fail('Not able to select a version for cloud-aws, see https://github.com/elastic/elasticsearch-cloud-aws') }
-  }
+  if versioncmp($elasticsearch_version, '2.0') < 0 {
+    # If version is older than 2.0.0, then install an older plugin
+    case $elasticsearch_version {
+      /^1.4/:  { $cloud_aws_version = '2.4.2' }
+      /^1.5/:  { $cloud_aws_version = '2.5.1' }
+      /^1.7/:  { $cloud_aws_version = '2.7.1' }
+      default: { fail('Not able to select a version for cloud-aws, see https://github.com/elastic/elasticsearch-cloud-aws') }
+    }
 
-  elasticsearch::plugin { "elasticsearch/elasticsearch-cloud-aws/${cloud_aws_version}":
-    module_dir => 'cloud-aws',
-    instances  => $::fqdn,
+    elasticsearch::plugin { "elasticsearch/elasticsearch-cloud-aws/${cloud_aws_version}":
+      module_dir => 'cloud-aws',
+      instances  => $::fqdn,
+    }
   }
+  # If the version is 2.4, then install the plugin the 2.4 way
+  elsif versioncmp($elasticsearch_version, '2.4') == 0 {
+    elasticsearch::plugin { 'cloud-aws':
+      module_dir => 'cloud-aws',
+      instances  => $::fqdn,
+    }
+  }
+  # If the version is 5.0 or newer, then install the two plugins it got split into:
+  # https://www.elastic.co/guide/en/elasticsearch/plugins/5.0/cloud-aws.html
+  elsif versioncmp($elasticsearch_version, '5.0') >= 0 {
+    elasticsearch::plugin { 'discovery-ec2':
+      module_dir => 'discovery-ec2',
+      instances  => $::fqdn,
+    }
 
+    elasticsearch::plugin { 'repository-s3':
+      module_dir => 'repository-s3',
+      instances  => $::fqdn,
+    }
+  }
 }
