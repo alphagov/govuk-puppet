420b52e4fda8a9941b636488081afe64e6f3f9d8
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-08-02 16:06:43 +0100

    Add AWS router-backend configuration
    Update hieradata and node configuration for router-backend
    on AWS.

--- modules/govuk/manifests/node/s_router_backend.pp
+++ modules/govuk/manifests/node/s_router_backend.pp
@@ -12,7 +12,9 @@ class govuk::node::s_router_backend inherits govuk::node::s_base {
   # If we miss all the apps, throw a 500 to be caught by the cache nginx
   nginx::config::vhost::default { 'default': }
 
-  Govuk_mount['/var/lib/mongodb'] -> Class['mongodb::server']
-  Govuk_mount['/var/lib/automongodbbackup'] -> Class['mongodb::backup']
-  Govuk_mount['/var/lib/s3backup'] -> Class['mongodb::backup']
+  if ! $::aws_migration {
+    Govuk_mount['/var/lib/mongodb'] -> Class['mongodb::server']
+    Govuk_mount['/var/lib/automongodbbackup'] -> Class['mongodb::backup']
+    Govuk_mount['/var/lib/s3backup'] -> Class['mongodb::backup']
+  }
 }
