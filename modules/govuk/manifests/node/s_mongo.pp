b1ad106c91420d058fb7311748d99a3e49ddc756
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-08-02 17:42:17 +0100

    Add AWS mongo configuration
    Update 'mongo' node class to avoid volume configuration for now, and add
    hieradata configuration.

--- modules/govuk/manifests/node/s_mongo.pp
+++ modules/govuk/manifests/node/s_mongo.pp
@@ -10,8 +10,10 @@ class govuk::node::s_mongo inherits govuk::node::s_base {
     outgoing => 27017,
   }
 
-  Govuk_mount['/var/lib/mongodb'] -> Class['mongodb::server']
-  Govuk_mount['/var/lib/automongodbbackup'] -> Class['mongodb::backup']
-  Govuk_mount['/var/lib/s3backup'] -> Class['mongodb::backup']
+  if ! $::aws_migration {
+    Govuk_mount['/var/lib/mongodb'] -> Class['mongodb::server']
+    Govuk_mount['/var/lib/automongodbbackup'] -> Class['mongodb::backup']
+    Govuk_mount['/var/lib/s3backup'] -> Class['mongodb::backup']
+  }
 
 }
