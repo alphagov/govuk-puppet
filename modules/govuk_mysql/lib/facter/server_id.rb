a569300d22e231f41f1dd6212d877a25e1ad2cbc
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-08-07 11:43:31 +0100

    Override mysql_id function in custom fact
    Do not use same function name as upstream mysql_server_id fact.

--- modules/govuk_mysql/lib/facter/server_id.rb
+++ modules/govuk_mysql/lib/facter/server_id.rb
@@ -1,10 +1,10 @@
 # Converts IP address to integer for MySQL server id
-def get_mysql_id
+def get_govuk_mysql_id
   Facter.value(:ipaddress).split('.').inject(0) { |total,value| (total << 8) + value.to_i }
 end
 
 Facter.add("govuk_mysql_server_id") do
   setcode do
-    get_mysql_id
+    get_govuk_mysql_id
   end
 end
