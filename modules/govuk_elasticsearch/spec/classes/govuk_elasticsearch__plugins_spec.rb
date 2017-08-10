bd5958586b552bb8550cc29c56cdbba692e81f52
David Henry <david@decoybecoy.com>
2017-08-10 14:00:29 +0100

    Add head plugin for elasticsearch 2.4

--- modules/govuk_elasticsearch/spec/classes/govuk_elasticsearch__plugins_spec.rb
+++ modules/govuk_elasticsearch/spec/classes/govuk_elasticsearch__plugins_spec.rb
@@ -26,9 +26,16 @@ describe 'govuk_elasticsearch::plugins', :type => :class do
 
   context 'Elasticsearch version 2.4.6 should install cloud-aws plugin' do
     let(:params) {{
-      :elasticsearch_version => '2.4',
+      :elasticsearch_version => '2.4.6',
     }}
     it { is_expected.to contain_elasticsearch__plugin('cloud-aws') }
+  end
+
+  context 'Elasticsearch version 2.4.6 should install head plugin' do
+    let(:params) {{
+      :elasticsearch_version => '2.4.6',
+    }}
+    it { is_expected.to contain_elasticsearch__plugin('mobz/elasticsearch-head') }
   end
 
   context 'Elasticsearch version 5.x.x should install the discovery-ec2 and repository-s3 plugins' do
