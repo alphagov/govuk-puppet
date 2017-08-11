7f32288d7892354f6359c1dc40b133612d31dc43
Laura Martin <surminus@gmail.com>
2017-08-11 14:45:06 +0100

    Add templates for lbs for no SSL
    We're terminating on ELBs in AWS.

--- modules/loadbalancer/manifests/balance.pp
+++ modules/loadbalancer/manifests/balance.pp
@@ -53,12 +53,18 @@ define loadbalancer::balance(
   $vhost_suffix = hiera('app_domain')
   $vhost_real = "${vhost}.${vhost_suffix}"
 
-  nginx::config::ssl { $vhost_real:
-    certtype => 'wildcard_publishing',
-  }
+  if ! $::aws_migration {
+    nginx::config::ssl { $vhost_real:
+      certtype => 'wildcard_publishing',
+    }
 
-  nginx::config::site { $vhost_real:
-    content => template('loadbalancer/nginx_balance.conf.erb'),
+    nginx::config::site { $vhost_real:
+      content => template('loadbalancer/nginx_balance.conf.erb'),
+    }
+  } else {
+    nginx::config::site { $vhost_real:
+      content => template('loadbalancer/nginx_balance_no_ssl.conf.erb'),
+    }
   }
 
   nginx::log {
