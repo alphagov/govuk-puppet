d1016ef6e37295f9462fcbf52c4096b59290247e
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-08-17 14:38:26 +0100

    s_logging logstash package
    At the moment we are downloding a very old version of Logstash from a site that
    has an expired certificate, so the download is failing on new installations.
    
    The Logstash jar has been packaged and uploaded to an internal repository, so
    this commit is updating the s_logging class to get the jar file from the package
    instead of downloading it. This should work for both VCloud and AWS. The package
    is just going to put the jar file in the same location as the previous `curl`
    resource, so the rest of the class remains as it was.
    
    s_logging will need to be refactored when we move to the elastic logstash Puppet module
    and upgrade the Logstash version.

--- modules/govuk/manifests/node/s_logging.pp
+++ modules/govuk/manifests/node/s_logging.pp
@@ -4,6 +4,7 @@
 #
 class govuk::node::s_logging (
   $compress_srv_logs_hour = '1',
+  $apt_mirror_hostname = undef,
 ) inherits govuk::node::s_base {
 
   # we want this to be a syslog server which also forwards to logstash
@@ -21,18 +22,25 @@ class govuk::node::s_logging (
   include govuk_java::oracle7::jre
   # TODO: this should really be done with a package.
 
-  if $::aws_migration {
-    $logstash_jar_url = 'https://s3-eu-west-1.amazonaws.com/govuk-logstash-package-integration/logstash-1.1.9-monolithic.jar'
-  } else {
-    $logstash_jar_url = 'https://logstash.objects.dreamhost.com/release/logstash-1.1.9-monolithic.jar'
-    Govuk_mount['/srv'] -> Class['logstash']
-    Govuk_mount['/srv'] -> Class['rsyslog::server']
+  apt::source { 'logstash':
+    location     => "http://${apt_mirror_hostname}/logstash",
+    release      => $::lsbdistcodename,
+    architecture => $::architecture,
+    key          => '3803E444EB0235822AA36A66EC5FE1A937E3ACBB',
   }
 
-  curl::fetch { 'logstash-monolithic':
-    source      => $logstash_jar_url,
-    destination => '/var/tmp/logstash-1.1.9-monolithic.jar',
-    require     => Class['govuk_java::oracle7::jre'],
+  package { 'logstash':
+    ensure  => installed,
+    require => [
+      Apt::Source['logstash'],
+      Class['govuk_java::oracle7::jre'],
+    ],
+    before  => Class['logstash'],
+  }
+
+  if ! $::aws_migration {
+    Govuk_mount['/srv'] -> Class['logstash']
+    Govuk_mount['/srv'] -> Class['rsyslog::server']
   }
 
   # FIXME 20130605 @philippotter: the current version of the
@@ -46,12 +54,9 @@ class govuk::node::s_logging (
   # provisioning a new machine.
   class { 'logstash':
     provider    => 'custom',
-    jarfile     => 'file:///var/tmp/logstash-1.1.9-monolithic.jar',
+    jarfile     => '/var/tmp/logstash-1.1.9-monolithic.jar',
     installpath => '/srv/logstash',
     initfile    => 'puppet:///modules/govuk/node/s_logging/logstash.init.Debian',
-    require     => [
-      Curl::Fetch['logstash-monolithic'],
-    ],
   }
 
   #configure logstash inputs
