7d87e25c2fc3efecd9e5e0f1de354688f84daff5
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2017-08-21 16:17:18 +0100

    Add router script to training VM
    This commit adds a script to move the router to the gopath and build it, so it can be run by upstart.

---
+++ training-vm/provisioner/make-router.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Move `router` to $GOPATH
+org_path="/var/govuk/gopath/src/github.com/alphagov"
+mkdir -p $org_path
+mv /var/govuk/router $org_path
+ln -s "$org_path/router" /var/govuk/router
+
+# Build `router` and `draft-router`
+cd "$org_path/router"
+make
