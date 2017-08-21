b06e653d8a4b7d349ea4a87c40ee7debf3de19ad
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2017-08-21 16:14:24 +0100

    Fix ruby versions and permissions in training VM
    This commit makes a number of small tweaks to the training VM setup script:
    
    * Run the ruby installation and bundler scripts under the deploy user
    * Install ruby under the root user
    * Add code to initialise rbenv for the deploy user so it works correctly
    * Navigate to the `/var/govuk/signon` folder before running the setup script so that relative paths in there work correctly

--- training-vm/provisioner/install-rubies.sh
+++ training-vm/provisioner/install-rubies.sh
@@ -4,9 +4,12 @@
 cd ~
 git clone https://github.com/rbenv/ruby-build.git
 cd ruby-build
-./install.sh
+sudo -i -u root ./install.sh
 
 # Install all ruby versions that are currently used by our apps
 for ruby in /var/govuk/*/.ruby-version; do
   rbenv install "$(cat $ruby)" -s
 done
+
+# Automatically initialise rbenv
+echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' > /home/deploy/.bash_profile
