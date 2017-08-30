1bbf976e0ac583aa5610052b993841168ebd6faa
Laura Martin <surminus@gmail.com>
2017-08-30 11:08:05 +0100

    Update default RDS root user

--- modules/govuk_postgresql/manifests/db.pp
+++ modules/govuk_postgresql/manifests/db.pp
@@ -67,7 +67,7 @@ define govuk_postgresql::db (
     $backend_ip_range        = undef,
     $ssl_only                = false,
     $rds                     = false,
-    $rds_root_user           = 'changeme2',
+    $rds_root_user           = 'aws_db_admin',
 ) {
 
     if ($database == undef) {
