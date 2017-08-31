3fb08497b145ed68049270b289490ef336b86848
Chris Roos <chris@seagul.co.uk>
2017-08-31 16:44:50 +0100

    Add X-Frame-Options header when proxying requests to S3
    This ensures that we're sending the `X-Frame-Options` header with a
    value of `DENY` irrespective of whether we serve the asset file from the
    filesystem or from S3.
    
    This address issue 166 in asset-manager[1].
    
    [1]: https://github.com/alphagov/asset-manager/issues/166

--- modules/govuk/manifests/apps/asset_manager.pp
+++ modules/govuk/manifests/apps/asset_manager.pp
@@ -140,6 +140,14 @@ class govuk::apps::asset_manager(
         add_header Last-Modified $last_modified_from_rails;
         add_header Content-Type $content_type_from_rails;
 
+        # Avoid the asset being embedded in other pages[1]
+        # This header is already set in the outer virtual host config but the
+        # presence of additional `add_header` calls in this `location` block
+        # mean that the value from the outer block is not being inherited[2].
+        # [1]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+        # [2]: http://nginx.org/en/docs/http/ngx_http_headers_module.html#add_header
+        add_header X-Frame-Options DENY;
+
         # Remove S3 HTTP headers listed in:
         # http://docs.aws.amazon.com/AmazonS3/latest/API/RESTCommonResponseHeaders.html
         # This keeps this HTTP response as similar as possible to the response
