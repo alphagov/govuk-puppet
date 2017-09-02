da5dd0b35de257f775e1e19d50c2f9cf76d4a970
Laura Martin <surminus@gmail.com>
2017-09-02 15:55:46 +0100

    Move whitehall_fe user creation to db class
    I feel like I'm missing why this does not live with the rest of the
    whitehall application db settings, as this seems like the sensible place
    for it to live.

--- modules/govuk/manifests/apps/whitehall/db.pp
+++ modules/govuk/manifests/apps/whitehall/db.pp
@@ -1,10 +1,18 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::apps::whitehall::db (
   $mysql_whitehall_admin = '',
+  $whitehall_fe_password = '',
 ){
   mysql::db { 'whitehall_production':
     user     => 'whitehall',
     host     => '%',
     password => $mysql_whitehall_admin,
+  }
+
+  govuk_mysql::user { 'whitehall_fe@%':
+    password_hash => mysql_password($whitehall_fe_password),
+    table         => 'whitehall_production.*',
+    privileges    => ['SELECT'],
+    require       => Mysql::Db['whitehall_production'],
   }
 }
