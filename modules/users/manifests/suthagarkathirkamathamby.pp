e0c752ed52e99eba74b3f4eaf01cbcf21620c35a
suthagarht <suthagar.kathirkamathamby@digital.cabinet-office.gov.uk>
2017-09-12 12:27:16 +0100

    Added user suthagarkathirkamathamby

---
+++ modules/users/manifests/suthagarkathirkamathamby.pp
@@ -0,0 +1,8 @@
+# Creates suthagarkathirkamathamby user
+class users::suthagarkathirkamathamby {
+  govuk_user { 'suthagarkathirkamathamby':
+    fullname => 'Suthagar Kathirkamathamby',
+    email    => 'suthagar.kathirkamathamby@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCueIeKHHQg9H4EmzFXE0PtLG/7vSjyX8FG21kOcs6BPmSUt6xkVILGxyPNayfNKFJe/afD0A+mswOD6g4in8/dHVD/yW3kXbVngNPktiaXLWw9iLprDGUTIqsA5XAXrnoRwOAeP3h8Zd0R144FtTRzqLvZmMn6yvlBmGi5yPSupLd5Kg3vPQLpq2c+tSBf3JnUdiBW1hX9DUhLVBZxgvnd81RRwwtezeMkCqI0o4luSr4rVXCzrUt706Zmu9MwGcjbLeoyrqPoSdD0LKuHTmB4YyH60/hbJA4XBI1CqbeXDzWfxtQKg6jc2B8jSjFfyNXJoy0a77l7FzEB/LvFWcHz suthagarkathirkamathamby@gds6260.local'
+  }
+}
