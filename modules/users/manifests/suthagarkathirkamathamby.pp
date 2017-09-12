64bb699a8f694cc8c4ae11f121b8b1c584e1d21a
suthagarht <suthagar.kathirkamathamby@digital.cabinet-office.gov.uk>
2017-09-12 13:54:41 +0100

    Changed ssh key to 4096

--- modules/users/manifests/suthagarkathirkamathamby.pp
+++ modules/users/manifests/suthagarkathirkamathamby.pp
@@ -3,6 +3,6 @@ class users::suthagarkathirkamathamby {
   govuk_user { 'suthagarkathirkamathamby':
     fullname => 'Suthagar Kathirkamathamby',
     email    => 'suthagar.kathirkamathamby@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCueIeKHHQg9H4EmzFXE0PtLG/7vSjyX8FG21kOcs6BPmSUt6xkVILGxyPNayfNKFJe/afD0A+mswOD6g4in8/dHVD/yW3kXbVngNPktiaXLWw9iLprDGUTIqsA5XAXrnoRwOAeP3h8Zd0R144FtTRzqLvZmMn6yvlBmGi5yPSupLd5Kg3vPQLpq2c+tSBf3JnUdiBW1hX9DUhLVBZxgvnd81RRwwtezeMkCqI0o4luSr4rVXCzrUt706Zmu9MwGcjbLeoyrqPoSdD0LKuHTmB4YyH60/hbJA4XBI1CqbeXDzWfxtQKg6jc2B8jSjFfyNXJoy0a77l7FzEB/LvFWcHz suthagarkathirkamathamby@gds6260.local'
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDsS46+XurgDRSZBg8lHQg+5KD1rqT55+/zNG21dw41v8ZRkkTEt4zPENnJnlBVQ+pUcEyWDK7n2Rztw8V9GFsJsNwwnAGJJ/W6YTTbukZhZdncCnH6tHQmNGOVGLWYXg1YCDKdfd+w3lWU3RrTIW1vN6jbZTRG9NJZ+beBp+6nmpjHw4/8x5415ds7p0TDFvcewogAoosJ6m+jV7uvF6rhpVK2AmuyRqzgDK+IItHB5OkCy3I6OrtaMXiI+/OeoguPoeJJJmoF9FqfMGfwzxi1wgr7U6CPoN/djbdw9Uq1lj5WagcVVVkgjBfj0VU6jq/5xAhSGc21Pl26QLADrweh2pJJrtKGUqj/saS0qCOHeyhpRiCt+SIQlvDoe10IKh8aRS+WWZYaesHEH1eBOIVMco86Wja8WfF40bTJNyrTgHFBZS5XiP6CMei12h2HFTdQR/B2S34nnXUJEQem/zpB/wc3paaCJyX2QN4ibzKHqns+4QwQde0RyPdnC9OcC8M5m6Ts+bIFFLcqMVK6RtQ05Yn+kvJ4DfyYMSbu7vJ4f5CxB9pOCZ1FWv0mA3Au7Zv4MQZxpAhv9WIm0lyM4wozEqwPD35f+keQoB/ORb1uCqC02PuPMLxbH+wfw2BJFh0GfmfA2wsIj72TMse42q5G4TgjNZrnhyYAHfZfOQYfZw== suthagarkathirkamathamby@gds6260.local'
   }
 }
