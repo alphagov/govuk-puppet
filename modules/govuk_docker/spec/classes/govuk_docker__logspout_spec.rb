f351cf369447ab2d997f1d12dd851bdb81f8b945
Philip Potter <philip.g.potter@gmail.com>
2017-09-14 10:26:28 +0100

    Revert "revert to upstream logspout"

--- modules/govuk_docker/spec/classes/govuk_docker__logspout_spec.rb
+++ modules/govuk_docker/spec/classes/govuk_docker__logspout_spec.rb
@@ -11,17 +11,17 @@ describe 'govuk_docker::logspout', :type => :class do
   context {
     let(:params){{ :endpoint => 'example.com' }}
     it {
-      is_expected.to contain_docker__image('gliderlabs/logspout').with(
+      is_expected.to contain_docker__image('govuk/logspout-alpine').with(
         'ensure'    => 'present',
         'image_tag' => 'latest',
       )
 
       is_expected.to contain_docker__run('logspout').with(
-        'image'   => 'gliderlabs/logspout:latest',
+        'image'   => 'govuk/logspout-alpine:latest',
         'env'     => ['LOGSTASH_TAGS=docker,json_log'],
         'volumes' => ['/var/run/docker.sock:/var/run/docker.sock'],
         'command' => 'logstash+tls://example.com',
-        'require' => 'Docker::Image[gliderlabs/logspout]',
+        'require' => 'Docker::Image[govuk/logspout-alpine]',
       )
     }
   }
