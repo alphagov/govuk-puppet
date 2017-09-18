e45c8e75908898b7f498cce1156457e2039f85bf
Laura Martin <surminus@gmail.com>
2017-09-18 19:07:12 +0100

    Update router docker healthcheck
    The healthcheck is actually on the API, which is on a different port, so
    we need to explicitly add this in.
    
    This was on the the `govuk::apps::router` manifest, but not copied
    across.

--- modules/govuk_containers/manifests/apps/router.pp
+++ modules/govuk_containers/manifests/apps/router.pp
@@ -16,10 +16,18 @@ class govuk_containers::apps::router (
   }
 
   govuk_containers::app { 'router':
-    image            => $image,
-    port             => $port,
-    envvars          => $envvars,
-    healthcheck_path => $healthcheck_path,
+    image   => $image,
+    port    => $port,
+    envvars => $envvars,
+  }
+
+  # While we pass the port to the govuk_containers::app class, the container
+  # appears to also expose the API port so we can do the healthcheck against
+  # the API
+  @@icinga::check { "check_app_router_up_on_${::hostname}":
+    check_command       => "check_nrpe!check_app_up!${api_port} ${healthcheck_path}",
+    service_description => 'router app healthcheck',
+    host_name           => $::fqdn,
   }
 
 }
