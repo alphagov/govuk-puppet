650caca623448bb387fff9c5026a84adf251bd59
Laura Martin <surminus@gmail.com>
2017-09-25 18:08:13 +0100

    Automated app deployments into AWS
    We're finding apps are getting out of date because we're not involved in
    deployments, so an automated deployment mechanism is now required to
    keep us up to date.

--- modules/govuk_jenkins/manifests/job/integration_deploy.pp
+++ modules/govuk_jenkins/manifests/job/integration_deploy.pp
@@ -13,6 +13,16 @@
 # [*jenkins_integration_api_password*]
 #   The password used to authenticate with the deployment Jenkins in Integration.
 #
+# [*jenkins_integration_aws_api_user*]
+#   The username used to authenticate with the deployment Jenkins in Integration AWS.
+#
+# [*jenkins_integration_aws_api_password*]
+#   The password used to authenticate with the deployment Jenkins in Integration AWS.
+#
+# [*aws_deploy_url*]
+#   The URL of the AWS Integration stack. This may potentially change in the future
+#   so it is added as a parameter.
+#
 # [*puppet_auth_token*]
 #   This token is used to authenticate with the deploy job in Integration.
 #
@@ -20,6 +30,9 @@ class govuk_jenkins::job::integration_deploy (
   $applications = undef,
   $jenkins_integration_api_user = undef,
   $jenkins_integration_api_password = undef,
+  $jenkins_integration_aws_api_user = undef,
+  $jenkins_integration_aws_api_password = undef,
+  $aws_deploy_url = 'deploy.blue.integration.govuk.digital',
   $puppet_auth_token = undef,
 ) {
   file { '/etc/jenkins_jobs/jobs/integration_app_deploy.yaml':
