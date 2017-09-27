024536446aa339eca09e500142d9dade1f372e54
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2017-09-27 13:50:16 +0100

    Remove the nsca client class
    Call the nsca class directly.

--- modules/monitoring/manifests/client.pp
+++ modules/monitoring/manifests/client.pp
@@ -6,7 +6,7 @@ class monitoring::client (
 
   include monitoring::client::apt
   include icinga::client
-  include nsca::client
+  include nsca
   include auditd
   include collectd
   include collectd::plugin::tcp
