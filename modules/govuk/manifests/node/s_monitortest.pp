a87d06014ac581aa6da233183ee4c495f5a125f3
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2017-09-28 12:09:01 +0100

    Move Prometheus configs to Hieradata
    The prometheus `scrape_configs` and `alerts` parameters are getting
    a little bit big. Configure these parameters in Hieradata to make them
    easier to read and update.
    
    Add relabeling configuration to the prometheus server.

--- modules/govuk/manifests/node/s_monitortest.pp
+++ modules/govuk/manifests/node/s_monitortest.pp
@@ -14,59 +14,11 @@ class govuk::node::s_monitortest {
 
   include ::govuk::node::s_base
 
-  $numeric_port = scanf('9091', "%i")
-
+  # scrape_configs and alerts parameters are configured in Hieradata
   class { 'prometheus':
     version        => '1.7.1',
     init_style     => 'debian',
-    scrape_configs => [
-      {
-        'job_name'        => 'prometheus',
-        'scrape_interval' => '30s',
-        'scrape_timeout'  => '30s',
-        'ec2_sd_configs'  => [
-          {
-            'region'  => $::aws_region,
-            'profile' => "${::aws_stackname}-${::aws_migration}",
-            'port'    => $numeric_port[0],
-          },
-        ],
-        'static_configs'  => [
-          {
-            'targets' => [
-              'localhost:9090',
-            ],
-            'labels'  => {
-              'alias' => 'Prometheus',
-            },
-          },
-        ],
-      },
-    ],
     extra_options  => '-alertmanager.url http://localhost:9093 -web.console.templates=/opt/prometheus-1.7.1.linux-amd64/consoles -web.console.libraries=/opt/prometheus-1.7.1.linux-amd64/console_libraries',
-    alerts         => [
-      {
-        'name'         => 'InstanceDown',
-        'condition'    => 'up == 0',
-        'timeduration' => '5m',
-        labels         => [
-          {
-            'name'    => 'severity',
-            'content' => 'page'
-          }
-        ],
-        'annotations'  => [
-          {
-            'name'  => 'summary',
-            content => 'Instance {{ $labels.instance }} down'
-          },
-          {
-            'name'  => 'description',
-            content => '{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes.'
-          },
-        ],
-      },
-    ],
   }
 
   @ufw::allow { 'allow-prometheus-http-9090':
