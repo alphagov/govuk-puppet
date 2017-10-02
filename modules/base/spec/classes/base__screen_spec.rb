04153a6c35160d17152583bc9b05bd8430d13f1b
Dean Wilson <dean.wilson@gmail.com>
2017-10-02 17:13:30 +0100

    Inline the screen module
    It's always in base and doesn't need to be a fully isolated module

--- modules/base/spec/classes/base__screen_spec.rb
+++ modules/base/spec/classes/base__screen_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../../../../spec_helper'
 
-describe 'screen', :type => :class do
+describe 'base::screen', :type => :class do
 
   it { is_expected.to compile }
 
   it { is_expected.to compile.with_all_deps }
 
-  it { is_expected.to contain_class('screen') }
+  it { is_expected.to contain_class('base::screen') }
 end
