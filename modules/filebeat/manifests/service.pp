533dae26c46e9a598559fc10110a4a70b167418f
Laura Martin <surminus@gmail.com>
2017-10-02 18:13:19 +0100

    Add service check for filebeat
    This service is vital to shipping logs, so we will want to know if it
    stops working for any reason.

--- modules/filebeat/manifests/service.pp
+++ modules/filebeat/manifests/service.pp
@@ -6,4 +6,11 @@ class filebeat::service {
     enable => $filebeat::service_enable,
   }
 
+  @@icinga::check { "check_filebeat_running_${::hostname}":
+    ensure              => $filebeat::service_ensure,
+    check_command       => 'check_nrpe!check_proc_running!filebeat',
+    service_description => 'filebeat running',
+    host_name           => $::fqdn,
+  }
+
 }
