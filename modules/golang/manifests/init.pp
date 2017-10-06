d7bd90103ebfd790f24738f06ab33da89a1a0b70
Graham Pengelly <graham@three-tier.com>
2017-10-06 11:32:07 +0100

    Add golang 1.9.1
    This adds the golang 1.9.1 version and sets it as the global default.

--- modules/golang/manifests/init.pp
+++ modules/golang/manifests/init.pp
@@ -7,11 +7,11 @@ class golang {
   include govuk_ppa
 
   class { 'goenv':
-    global_version => '1.7.1',
+    global_version => '1.9.1',
     require        => Class['govuk_ppa'],
   }
 
-  goenv::version { ['1.6.3', '1.7', '1.7.1']: }
+  goenv::version { ['1.6.3', '1.7', '1.7.1', '1.9.1']: }
 
   goenv::version { ['1.3.3', '1.4.2', '1.4.3', '1.5.1', '1.5.3', '1.6.2']:
     ensure => absent,
