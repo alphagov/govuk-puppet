1ab83692a64e548e1a388a75d4b84d754ce103a5
Laura Martin <surminus@gmail.com>
2017-10-19 17:07:36 +0100

    Add streaming RDS backup scripts
    Add backup scripts that do not do anything too clever: take a list of
    databases, excluding some specific ones, take a dump, and stream the
    output to a file in an S3 bucket. All backups will go into the same
    bucket, which we'll enable with expiration dates to manage storage.
    
    Ensure there are passive checks that alert us if a backup fails.
    
    In theory these scripts could also be run on demand if developers wanted
    more up to date data to import.
    
    We are relying on the connection during transfer being encrypted (which
    is by default with gof3r), and the bucket being encrypted at rest.
    
    We will use IAM roles to allow access to the bucket from the instance.

--- modules/govuk/manifests/node/s_transition_db_admin.pp
+++ modules/govuk/manifests/node/s_transition_db_admin.pp
@@ -4,14 +4,33 @@
 #
 # === Parameters
 #
-
 class govuk::node::s_transition_db_admin(
-  $postgres_host     = undef,
-  $postgres_user     = undef,
-  $postgres_password = undef,
-  $postgres_port     = '5432',
+  $apt_mirror_hostname  = undef,
+  $backup_s3_bucket     = undef,
+  $postgres_host        = undef,
+  $postgres_user        = undef,
+  $postgres_password    = undef,
+  $postgres_port        = '5432',
+  $postgres_backup_hour = 7,
+  $postgres_backup_min  = 30,
 ) {
   include ::govuk::node::s_base
+
+  apt::source { 'gof3r':
+    location     => "http://${apt_mirror_hostname}/gof3r",
+    release      => $::lsbdistcodename,
+    architecture => $::architecture,
+    key          => '3803E444EB0235822AA36A66EC5FE1A937E3ACBB',
+  }
+
+  package { 'gof3r':
+    ensure  => '0.5.0',
+    require => Apt::Source['gof3r'],
+  }
+
+  $alert_hostname = 'alert'
+
+  ### PostgreSQL ###
 
   $default_connect_settings = {
     'PGUSER'     => $postgres_user,
@@ -43,4 +62,31 @@ class govuk::node::s_transition_db_admin(
 
   # include all PostgreSQL classes that create databases and users
   class { '::govuk::apps::transition::postgresql_db': }
+
+  $postgres_backup_desc = 'RDS Transition PostgreSQL backup to S3'
+
+  @@icinga::passive_check { "check_rds_postgres_s3_backup-${::hostname}":
+    service_description => $postgres_backup_desc,
+    freshness_threshold => 28 * 3600,
+    host_name           => $::fqdn,
+  }
+
+  file { '/usr/local/bin/rds-postgres-to-s3':
+    ensure  => 'present',
+    content => template('govuk/node/s_db_admin/rds-postgres-to-s3.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0775',
+    require => [
+      Package['gof3r'],
+      File['/root/.pgpass'],
+    ],
+  }
+
+  cron::crondotdee { 'rds-transition-postgres-to-s3':
+    hour    => $postgres_backup_hour,
+    minute  => $postgres_backup_min,
+    command => '/usr/local/bin/rds-postgres-to-s3',
+    require => File['/usr/local/bin/rds-postgres-to-s3'],
+  }
 }
