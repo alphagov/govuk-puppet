d24d619c5d0a378b5373887fbd5ff04290a53291
Laura Martin <surminus@gmail.com>
2017-10-26 17:34:50 +0100

    Remove openconnect resources from all machines
    Since the upstream openconnect module now supports an "ensure"
    parameter, remove the resources from all machines, including alerts.
    
    Add an ensure parameter to an Icinga alert, so that can also be removed.

--- modules/icinga/manifests/nrpe_config.pp
+++ modules/icinga/manifests/nrpe_config.pp
@@ -1,11 +1,12 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 define icinga::nrpe_config (
+  $ensure = 'present',
   $content = undef,
   $source = undef
 ) {
 
   file { "/etc/nagios/nrpe.d/${title}.cfg":
-    ensure  => 'file',
+    ensure  => $ensure,
     content => $content,
     source  => $source,
     require => Class['icinga::client::package'],
