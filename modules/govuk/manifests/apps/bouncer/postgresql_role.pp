4acfbc1d82f0f600d5780b864cf41e35c31812d2
Isabell Long <isabell@issyl0.co.uk>
2017-10-26 14:18:03 +0100

    Make bouncer::postgresql_role work on RDS
    - This improves the experience for Carrenza too, as it's less complicated with
      less raw SQL. This uses the postgresql module's `::grant` class to GRANT
      SELECT ON ALL TABLES IN SCHEMA for the bouncer user, to make it a read-only
      user.
    
    Rework the postgresql_role stuff to use grants instead of execs

--- modules/govuk/manifests/apps/bouncer/postgresql_role.pp
+++ modules/govuk/manifests/apps/bouncer/postgresql_role.pp
@@ -1,10 +1,14 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk::apps::bouncer::postgresql_role ( $password = '' ){
+class govuk::apps::bouncer::postgresql_role (
+  $password = '',
+  $rds = undef,
+  ){
   $bouncer_role = 'bouncer'
   $db           = 'transition_production'
 
   postgresql::server::role { $bouncer_role:
     password_hash => postgresql_password($bouncer_role, $password),
+    rds           => $rds,
   }
 
   postgresql::server::database_grant { "${bouncer_role} can connect to ${db}":
@@ -14,21 +18,10 @@ class govuk::apps::bouncer::postgresql_role ( $password = '' ){
     require   => [Postgresql::Server::Role[$bouncer_role], Class['govuk::apps::transition::postgresql_db']],
   }
 
-  exec { "${bouncer_role} will be able to SELECT from all yet-to-be-created tables in ${db}":
-    command => "psql -d ${db} -c 'ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO ${bouncer_role}'",
-    unless  => "psql -d ${db} -c '\\ddp' | grep -q 'bouncer=r/'",
-    user    => 'postgres',
-    require => Postgresql::Server::Database_grant["${bouncer_role} can connect to ${db}"],
-  }
-
-  # FIXME: We will be able to do this via the postgres puppet module from version 4.2.0:
-  # https://github.com/puppetlabs/puppetlabs-postgresql/blob/master/CHANGELOG.md#2015-03-10---supported-release-420
-  # https://github.com/puppetlabs/puppetlabs-postgresql/pull/405
-  exec { "${bouncer_role} can SELECT from all existing tables in ${db}":
-    command => "psql -d ${db} -c 'GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${bouncer_role}'",
-    # This only tests that the privilege is set for at least one table, not all of them:
-    unless  => "psql -d ${db} -c '\\dp' | grep -q 'bouncer=r/'",
-    user    => 'postgres',
-    require => Exec["${bouncer_role} will be able to SELECT from all yet-to-be-created tables in ${db}"],
+  postgresql::server::grant { "${bouncer_role} can SELECT from all tables in ${db}":
+    privilege   => 'SELECT',
+    object_type => 'ALL TABLES IN SCHEMA',
+    db          => $db,
+    role        => $bouncer_role,
   }
 }
