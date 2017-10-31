e065a2791f93622d83561790693e2b08dd7c22f9
Laura Martin <surminus@gmail.com>
2017-10-31 17:24:45 +0000

    Big tidy of Elasticsearch to S3 snapshots
    This simplifies the manifest and script for creating Elasticsearch
    snapshots in S3.
    
     - Remove the user variable and just run it as root as there is no
     reason to create a separate user for this task
     - Rather than creating a separate "awspasswd" file, pass the
     credentials straight into the script.
     - Set an "ensure" parameter
     - Add a conditional if in AWS to set a base_path, but not set AWS
     access keys
     - Remove the housekeeping class and inline it into the main class

--- modules/govuk_elasticsearch/manifests/backup.pp
+++ modules/govuk_elasticsearch/manifests/backup.pp
@@ -4,106 +4,102 @@
 #
 # === Parameters
 #
+# [*s3_bucket*]
+#   Defines the AWS S3 bucket where the backups will be uploaded.
+#
+# [*ensure*]
+#   Whether to create the resources.
+#
 # [*aws_access_key_id*]
-#   AWS key to authenticate requests
+#   AWS key to authenticate requests.
 #
 # [*aws_secret_access_key*]
-#   AWS key to authenticate requests
+#   AWS key to authenticate requests.
 #
 # [*aws_region*]
-#   AWS region for the S3 bucket
+#   AWS region for the S3 bucket.
 #
-# [*env_dir*]
-#   Defines directory for the environment
-#   variables
-#
-# [*s3_bucket*]
-#   Defines the AWS S3 bucket where the backups
-#   will be uploaded. It should be created by the
-#   user
-#
-# [*user*]
-#   Defines the system user that will be created
-#   to run the backups
-#
-# [*es_repos*]
-#   The repositories within elasticsearch
-#   where backups will be stored
+# [*es_repo*]
+#   The repositories within elasticsearch where backups will be stored.
 #
 # [*es_indices*]
-#   Elaticsearch indexes
+#   Which Elasticsearch indices to back-up.
 #
-# [*cron_hour*]
-#   The hour that the job should run
+# [*base_path*]
+#   The base path (prefix) set for the object.
 #
-# [*cron_minute*]
-#   The minute that the job should run
+# [*backup_cron_hour*]
+#   The hour at which the backups run.
 #
-# === Variables
+# [*backup_cron_minute*]
+#   The minute at which the backup runs.
 #
-# [*json_es_indices*]
-#   Formatted indexes to be backed up when script is invoked
+# [*prune_cron_hour*]
+#   The hour at which the prune runs.
 #
-class govuk_elasticsearch::backup(
-  $aws_access_key_id,
-  $aws_secret_access_key,
-  $aws_region = 'eu-west-1',
+# [*prune_cron_minute*]
+#   The minute at which the prune runs.
+#
+class govuk_elasticsearch::backup (
   $s3_bucket,
-  $aws_source_file = '/etc/awspasswd',
-  $user = 'govuk-backup',
-  $es_repo = undef,
+  $ensure = 'present',
+  $aws_access_key_id = undef,
+  $aws_secret_access_key = undef,
+  $aws_region = 'eu-west-1',
+  $es_repo = 'snapshots',
   $es_indices = [],
-  $cron_hour = 0,
-  $cron_minute = 0,
+  $base_path = 'elasticsearch',
+  $backup_cron_hour = 0,
+  $backup_cron_minute = 0,
+  $prune_cron_hour = 4,
+  $prune_cron_minute = 0,
+  $prune_snapshots_to_keep = 7,
   $alert_hostname = 'alert.cluster',
 ){
 
-    $threshold_secs = 28 * 3600
-    $service_desc = 'Elasticsearch-Index_Backup'
-    $json_es_indices = join(regsubst($es_indices, '(.*)', '"\1"'), ',')
+  $threshold_secs = 28 * 3600
+  $service_desc = 'Elasticsearch index snapshots'
+  $json_es_indices = join(regsubst($es_indices, '(.*)', '"\1"'), ',')
 
-    include ::backup::client
-    include govuk_elasticsearch::housekeeping
+  ensure_packages(['jq'])
 
-    file { $aws_source_file:
-      ensure  => present,
-      owner   => $user,
-      group   => $user,
-      mode    => '0640',
-      content => template('govuk_elasticsearch/awspasswd.erb'),
-    }
+  @@icinga::passive_check { "check_es-backup-s3-${::hostname}":
+    ensure              => $ensure,
+    service_description => $service_desc,
+    freshness_threshold => $threshold_secs,
+    host_name           => $::fqdn,
+    notes_url           => monitoring_docs_url(backup-passive-checks),
+  }
 
-    @@icinga::passive_check { "check_esindexbackup-${::hostname}":
-      service_description => $service_desc,
-      freshness_threshold => $threshold_secs,
-      host_name           => $::fqdn,
-      notes_url           => monitoring_docs_url(backup-passive-checks),
-    }
+  file { '/usr/local/bin/es-backup-s3':
+    ensure  => $ensure,
+    mode    => '0755',
+    content => template('govuk_elasticsearch/es-backup-s3.erb'),
+  }
 
-    file { 'es-backup-s3':
-      path    => '/usr/local/bin/es-backup-s3',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755',
-      content => template('govuk_elasticsearch/es-backup-s3.erb'),
-    }
+  cron::crondotdee { 'es-backup-s3':
+    ensure  => $ensure,
+    command => '/usr/local/bin/es-backup-s3',
+    hour    => $backup_cron_hour,
+    minute  => $backup_cron_minute,
+  }
 
+  file { '/usr/local/bin/es-restore-s3':
+    ensure  => $ensure,
+    mode    => '0755',
+    content => template('govuk_elasticsearch/es-restore-s3.erb'),
+  }
 
-    cron { 's3-elasticsearch-indexes':
-      ensure  => present,
-      command => '/usr/local/bin/es-backup-s3',
-      user    => $user,
-      hour    => $cron_hour,
-      minute  => $cron_minute,
-    }
+  file { '/usr/local/bin/es-prune-snapshots':
+    ensure  => $ensure,
+    content => template('govuk_elasticsearch/es-prune-snapshots.erb'),
+    mode    => '0755',
+  }
 
-
-    file { 'es-restore-s3':
-      path    => '/usr/local/bin/es-restore-s3',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755',
-      content => template('govuk_elasticsearch/es-restore-s3.erb'),
-    }
-
+  cron::crondotdee { 'es-prune-snapshots':
+    ensure  => $ensure,
+    command => '/usr/local/bin/es-prune-snapshots',
+    hour    => $prune_cron_hour,
+    minute  => $prune_cron_minute,
+  }
 }
