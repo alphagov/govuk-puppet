2617c6428aa3d9d1c125428cdf4942b84e65cc43
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2017-11-17 16:34:48 +0000

    Add a separate jenkins job for recreating the search indexes
    This process
    - uses rummager's configuration to generate the settings/mappings for a
      new set of search indexes
    - creates the new indexes
    - triggers a reindex of content from the old index to the new one
    - while this is happening, a write lock is placed on the old index so
      data isn't lost
    - after completing the reindex, run some checks on the new index, and
      switch over to it if successful (by moving aliases)
    
    This used to be part of the search_fetch_analytics job, and ran every
    night but we split it out, and now only need to run it when changing
    the mappings or updating elasticsearch.
    
    This is a very slow process, so we don't want to hook it into our CI
    pipeline, but we do want to test it periodically on integration.

---
+++ modules/govuk_jenkins/manifests/job/search_reindex_with_new_schema.pp
@@ -0,0 +1,27 @@
+# == Class: govuk_jenkins::job::search-reindex-with-new-schema
+#
+# Test rebuilding the search indexes and reindexing all content
+#
+class govuk_jenkins::job::search_reindex_with_new_schema (
+  $app_domain = hiera('app_domain'),
+) {
+
+  $check_name = 'search-reindex-with-new-schema'
+  $service_description = 'Rebuild new search indexes with up to date settings and mappings, and reindex all content from the existing indexes.'
+  $job_url = "https://deploy.${app_domain}/job/search-reindex-with-new-schema/"
+
+  file { '/etc/jenkins_jobs/jobs/search_reindex_with_new_schema.yaml':
+    ensure  => present,
+    content => template('govuk_jenkins/jobs/search_reindex_with_new_schema.yaml.erb'),
+    notify  => Exec['jenkins_jobs_update'],
+  }
+
+  @@icinga::passive_check { "${check_name}_${::hostname}":
+    service_description => $service_description,
+    host_name           => $::fqdn,
+    freshness_threshold => 619200,
+    action_url          => $job_url,
+    # TODO: add this to the developer docs
+    # notes_url           => monitoring_docs_url(search-reindex-failed),
+  }
+}
