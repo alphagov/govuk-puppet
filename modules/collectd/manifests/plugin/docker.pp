7535a5a783386e1a81c0bfac8b9d84e81dea245e
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-23 15:25:06 +0000

    Use docker pip dependency rather than docker-py
    docker-py has been renamed to just docker. The docker-py
    dependency had compatibility issues with docker-compose so this now uses
    docker as suggested by docker-compose:
    https://github.com/docker/compose/issues/4569
    
    We've had to use the exec method to install the dependency as we hit a
    duplicate namespace issue if we install it via package. This issue is
    outlined here: https://tickets.puppetlabs.com/browse/PUP-1073 and seems
    to be fixed in Puppet 4.

--- modules/collectd/manifests/plugin/docker.pp
+++ modules/collectd/manifests/plugin/docker.pp
@@ -6,11 +6,23 @@ class collectd::plugin::docker {
 
   $dependencies = [
     'py-dateutil',
-    'docker-py',
   ]
 
   package { $dependencies:
     ensure   => 'present',
+    provider => 'pip',
+  }
+
+  # This has to be installed via exec rather than a package as the namespace
+  # conflicts with the different (but same name) docker package. This issue
+  # seems to be fixed in puppet 4: https://tickets.puppetlabs.com/browse/PUP-1073
+  exec { 'pip install docker':
+    path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+    command => 'pip install docker',
+  }
+
+  package { 'docker-py':
+    ensure   => 'absent',
     provider => 'pip',
   }
 
