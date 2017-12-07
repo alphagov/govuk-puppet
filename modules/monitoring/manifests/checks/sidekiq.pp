1545bd92c283de2d88e3b33ae7facfe1ffda4f62
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-12-07 14:46:59 +0000

    Improve the rummager queue size check
    Remove the movingAverage, as this duplicates what the check_graphite
    script does. Increase the from argument to 24 hours to account for
    missing data, and use dropfirst to focus on the most recent ~3 minutes
    worth of data.
    
    This change is a result of changing the statsd configuration to delete
    gauges. As statsd isn't sending old values anymore, this check needed
    adjusting to not fail on missing data.

--- modules/monitoring/manifests/checks/sidekiq.pp
+++ modules/monitoring/manifests/checks/sidekiq.pp
@@ -4,15 +4,17 @@
 #
 #
 class monitoring::checks::sidekiq {
-
-  $graphite_target = 'movingAverage(keepLastValue(stats.gauges.govuk.app.rummager.workers.queues.default.latency),"-600s")'
-
   icinga::check::graphite { 'check_rummager_queue_latency':
-    target              => $graphite_target,
+    target              => 'keepLastValue(stats.gauges.govuk.app.rummager.workers.queues.default.latency)',
     warning             => 0.3,
     critical            => 15,
     use                 => 'govuk_normal_priority',
-    from                => '3minutes',
+    # Use data from the last 24 hours, to avoid not getting any
+    # datapoints back.
+    from                => '24hours',
+    # Take an average over the most recent 36 datapoints, which at 5
+    # seconds per datapoint is the last 3 minutes
+    args                => '--dropfirst -36',
     host_name           => $::fqdn,
     desc                => 'check rummager queue latency [in office hours]',
     notification_period => 'inoffice',
