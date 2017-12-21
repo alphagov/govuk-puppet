682e5beb1bb218afc3fd10fb8984cc90086693de
Chris Patuzzo <chris@patuzzo.co.uk>
2017-12-21 15:25:14 +0000

    Redirect kibana.*.publishing.service.gov.uk to Logit
    This might be more efficient than going through
    google apps. The environment is set in the session
    from a UUID in the URL.

--- modules/govuk/manifests/apps/kibana.pp
+++ modules/govuk/manifests/apps/kibana.pp
@@ -1,20 +1,17 @@
 # == Class: govuk::apps::kibana
 #
-# A redirect to point people using the legacy Kibana domain to
-# docs for how to check Kibana on our ELK SaaS provider.
+# A redirect to point people to Kibana provided by Logit.
+# The URL sets the account and environment in a session.
 #
-class govuk::apps::kibana {
-  $app_name = 'kibana'
+class govuk::apps::kibana(
+  $logit_account = undef,
+  $logit_environment = undef,
+) {
 
+  $app_name = 'kibana'
   $app_domain = hiera('app_domain')
 
-  if $::aws_migration {
-    nginx::config::vhost::redirect { $app_name:
-      to => 'https://docs.publishing.service.gov.uk/manual/logit.html',
-    }
-  } else {
-    nginx::config::vhost::redirect { "${app_name}.${app_domain}":
-      to => 'https://docs.publishing.service.gov.uk/manual/logit.html',
-    }
+  nginx::config::vhost::redirect { "${app_name}.${app_domain}":
+    to => "https://logit.io/a/${logit_account}/s/${logit_environment}/kibana/access",
   }
 }
