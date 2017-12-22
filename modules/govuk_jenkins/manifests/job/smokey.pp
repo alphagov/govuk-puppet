0c3263985d5eaa05414c1fb3c80ffa09ac941ce4
Laura Martin <surminus@gmail.com>
2017-12-22 12:22:40 +0000

    Rework Smokey environment variables
    When trying to understand how we deploy Smokey, I was a little bemused
    by the confusing way how we set environment variables in two different
    ways, even though we generally run the app using `govuk_setenv`.
    
    It would be more straight forward to understand using Smokey both in
    Icinga and Jenkins if we were to set the environment variables in the
    same way they are set for all other applications, and in a single place
    that both the tasks can consume.
    
    This also sets GOVUK_APP_DOMAIN specifically for Smokey in AWS. Smokey
    relies on correctly authing with Signon for many of the publisher
    application tests, and if we make a request using an internal domain,
    then it will send the callback to the wrong place. Ensuring the
    GOVUK_APP_DOMAIN environment variable is overwritten resolves this
    issue.
    
    This relies on the change in Smokey itself: https://github.com/alphagov/smokey/commit/964f88bad7fe0134645396dc9fc7275e3c2fb340

--- modules/govuk_jenkins/manifests/job/smokey.pp
+++ modules/govuk_jenkins/manifests/job/smokey.pp
@@ -4,49 +4,24 @@
 #
 # === Parameters
 #
-# [*auth_username*]
-#   HTTP basic auth username
-#
-# [*auth_password*]
-#   HTTP basic auth password
-#
-# [*signon_email*]
-#   Username to sign in to Signon
-#
-# [*signon_password*]
-#   Password to sign in to Signon
-#
-# [*smokey_bearer_token*]
-#   Bearer token to pass as part of an Authorization HTTP header
-#
-# [*smokey_rate_limit_token*]
-#   Token to pass as an HTTP header to bypass rate limiting
-#
-# [*spoof_target_domain*]
-#   If you wish to run tests against an environment that is different to the
-#   main app_domain parameter.
-#
 # [*smokey_task*]
 #   The rake task to run for the tests
 #
 class govuk_jenkins::job::smokey (
-  $auth_username = undef,
-  $auth_password = undef,
-  $signon_email = undef,
-  $signon_password = undef,
-  $smokey_bearer_token = undef,
-  $smokey_rate_limit_token = undef,
   $smokey_task = 'test:production',
-  $spoof_target_domain = undef,
-  $app_domain = hiera('app_domain'),
 ) {
+  $app_domain = hiera('app_domain')
+
   $slack_team_domain = 'govuk'
   $slack_room = '2ndline'
   $slack_build_server_url = "https://deploy.${app_domain}/"
+
+  include govuk::apps::smokey
 
   file { '/etc/jenkins_jobs/jobs/smokey.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/smokey.yaml.erb'),
     notify  => Exec['jenkins_jobs_update'],
+    require => Class['govuk::apps::smokey'],
   }
 }
