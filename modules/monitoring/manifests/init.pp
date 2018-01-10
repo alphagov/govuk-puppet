c004843f0fda1aa1a26daa1b1cc5832a4c16118b
Laura Martin <surminus@gmail.com>
2018-01-10 16:30:27 +0000

    Fix monitoring in CI again
    I'm trying again, since 53fb3364a2502c75dcf941ef5b0a2b3fdc77f872 failed.
    
    Licensify has a dependency on event_handlers which restarts apps if they
    trigger memory alerts.

--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -20,12 +20,12 @@ class monitoring (
 
   include govuk_htpasswd
   include monitoring::contacts
+  include monitoring::event_handlers
 
   unless $ci_environment {
     # Monitoring server only.
     include monitoring::checks
     include monitoring::edge
-    include monitoring::event_handlers
     include monitoring::pagerduty_drill
     include monitoring::uptime_collector
   }
