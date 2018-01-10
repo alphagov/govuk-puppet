e09d2523715677e053f647c91bd5b90f7819190a
Laura Martin <surminus@gmail.com>
2018-01-10 11:48:41 +0000

    Disable default alerts in CI environment
    If this is the CI environment then we should disable all the default
    alerts that get created on the monitoring instance.
    
    Carrenza Integration is now essentially a full CI environment, with a
    smattering of Licensify.
    
    Host specific alerts should still be present.

--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -2,7 +2,14 @@
 #
 # Sets up monitoring.
 #
-class monitoring {
+# === Parameters:
+#
+# [*ci_environment*]
+#   Disables lots of default alerts if this is the CI environment.
+#
+class monitoring (
+  $ci_environment = false,
+) {
 
   package { 'python-rrdtool':
     ensure => 'installed',
@@ -13,13 +20,15 @@ class monitoring {
 
   include govuk_htpasswd
 
-  # Monitoring server only.
-  include monitoring::contacts
-  include monitoring::checks
-  include monitoring::edge
-  include monitoring::event_handlers
-  include monitoring::pagerduty_drill
-  include monitoring::uptime_collector
+  unless $ci_environment {
+    # Monitoring server only.
+    include monitoring::contacts
+    include monitoring::checks
+    include monitoring::edge
+    include monitoring::event_handlers
+    include monitoring::pagerduty_drill
+    include monitoring::uptime_collector
+  }
 
   if ! $::aws_migration {
     include monitoring::vpn_gateways
