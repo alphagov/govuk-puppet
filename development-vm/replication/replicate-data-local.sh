b6cb7ca6101c781b879ae8cb18ece75bf6f131ce
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2018-01-16 11:32:55 +0000

    AWS mysql development replication
    The replicate-data-local.sh is updated to only test mysql for now.
    
    sync-aws-mysql changes:
    - We don't need information about the Mysql SRC instance
    - The databases are stored in individual files
    - File names are different
    - Compression format is different

--- development-vm/replication/replicate-data-local.sh
+++ development-vm/replication/replicate-data-local.sh
@@ -10,44 +10,43 @@ set -eu
 status "Running bundle install"
 bundle install --quiet
 
-$(dirname $0)/sync-mysql.sh "$@" mysql-backup-1.backend.integration
-$(dirname $0)/sync-mysql.sh "$@" whitehall-mysql-backup-1.backend.integration
+$(dirname $0)/sync-aws-mysql.sh "$@" mysql-master
 
-$(dirname $0)/sync-mongo.sh "$@" mongo-1.backend.integration
-$(dirname $0)/sync-mongo.sh "$@" api-mongo-1.api.integration
-$(dirname $0)/sync-mongo.sh "$@" router-backend-1.router.integration
+#$(dirname $0)/sync-mongo.sh "$@" mongo-1.backend.integration
+#$(dirname $0)/sync-mongo.sh "$@" api-mongo-1.api.integration
+#$(dirname $0)/sync-mongo.sh "$@" router-backend-1.router.integration
 
-if ! ($SKIP_MONGO || $DRY_RUN); then
-  status "Munging router backend hostnames for dev VM"
-  mongo --quiet --eval 'db = db.getSiblingDB("router"); db.backends.find().forEach( function(b) { b.backend_url = b.backend_url.replace(".integration.publishing.service.gov.uk", ".dev.gov.uk").replace("https","http"); db.backends.save(b); } );'
-  mongo --quiet --eval 'db = db.getSiblingDB("draft_router"); db.backends.find().forEach( function(b) { b.backend_url = b.backend_url.replace(".integration.publishing.service.gov.uk", ".dev.gov.uk").replace("https","http"); db.backends.save(b); } );'
-fi
+#if ! ($SKIP_MONGO || $DRY_RUN); then
+#  status "Munging router backend hostnames for dev VM"
+#  mongo --quiet --eval 'db = db.getSiblingDB("router"); db.backends.find().forEach( function(b) { b.backend_url = b.backend_url.replace(".integration.publishing.service.gov.uk", ".dev.gov.uk").replace("https","http"); db.backends.save(b); } );'
+#  mongo --quiet --eval 'db = db.getSiblingDB("draft_router"); db.backends.find().forEach( function(b) { b.backend_url = b.backend_url.replace(".integration.publishing.service.gov.uk", ".dev.gov.uk").replace("https","http"); db.backends.save(b); } );'
+#fi
 
-$(dirname $0)/sync-postgresql.sh "$@" postgresql-primary-1.backend.integration
-if ignored "transition"; then
-  status "Skipping transition"
-else
-  $(dirname $0)/sync-postgresql.sh "$@" transition-postgresql-master-1.backend.integration
-fi
+#$(dirname $0)/sync-postgresql.sh "$@" postgresql-primary-1.backend.integration
+#if ignored "transition"; then
+#  status "Skipping transition"
+#else
+#  $(dirname $0)/sync-postgresql.sh "$@" transition-postgresql-master-1.backend.integration
+#fi
 
-if ignored "mapit"; then
-  status "Skipping mapit"
-else
-  $(dirname $0)/sync-mapit.sh
-fi
+#if ignored "mapit"; then
+#  status "Skipping mapit"
+#else
+#  $(dirname $0)/sync-mapit.sh
+#fi
 
-$(dirname $0)/sync-elasticsearch.sh "$@" rummager-elasticsearch-1.api.integration
+#$(dirname $0)/sync-elasticsearch.sh "$@" rummager-elasticsearch-1.api.integration
 
-if ! ($SKIP_ELASTIC || $DRY_RUN); then
-  status "Deleting old elasticsearch indexes"
-  ruby $(dirname $0)/delete_closed_indices.rb
-fi
+#if ! ($SKIP_ELASTIC || $DRY_RUN); then
+#  status "Deleting old elasticsearch indexes"
+#  ruby $(dirname $0)/delete_closed_indices.rb
+#fi
 
-if ! $DRY_RUN; then
-  status "Munging Signon db tokens for dev VM"
-  if [[ -d $(dirname $0)/../../../signon ]]; then
-    cd $(dirname $0)/../../../signon && bundle install && bundle exec ruby script/make_oauth_work_in_dev
-  fi
-fi
+#if ! $DRY_RUN; then
+#  status "Munging Signon db tokens for dev VM"
+#  if [[ -d $(dirname $0)/../../../signon ]]; then
+#    cd $(dirname $0)/../../../signon && bundle install && bundle exec ruby script/make_oauth_work_in_dev
+#  fi
+#fi
 
 ok "Data replication complete"
