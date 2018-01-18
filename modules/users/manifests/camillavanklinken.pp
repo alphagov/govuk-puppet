0c247d2fda126b6ecd124eb209518737dbca87cb
Tom Sabin <tom.sabin@digital.cabinet-office.gov.uk>
2018-01-18 11:18:46 +0000

    Remove developers that were working on Q3 project
    17th January was the last day that they have access to things
    
    Related PR: https://github.com/alphagov/govuk-user-reviewer/pull/62

--- modules/users/manifests/camillavanklinken.pp
+++
@@ -1,8 +0,0 @@
-# Creates the camillavanklinken user
-class users::camillavanklinken {
-  govuk_user { 'camillavanklinken':
-    fullname => 'Camilla van Klinken',
-    email    => 'camilla.vanklinken@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDEVs2q59o3m1t8DAmjsUFQVZGnFpH70uWGhXO9cVBBVVReX96vXAURHjMJYGU1XB8KCQXls50UaRrFGXGOxK3PRe6ZIqPo8K1pMZlIZ0wxPEQZzdV3RY9zmYB4qQ8WodmiF51rvHbu5W+QskNLxGpIMA1i/uum3NdG6iZdGh7+4m0H9bUWfDOSVmGJhbsjlvLjJ9f0VHLoReNvKlVAD85vGbPfr0ymwe8Vc32JmgdPkBdDONNXkGBBM7tDUD0CBFvGyftT1euoR/RsA1Z3AHVAr9Kkqj0AU+VIACsrjhCYWZKGb9AulTS0Qy/ZpXUgpKjAT9mNJozFb9FNBAMf3mdoj/xajAuxZi3wYuVhmUi0yJYL5vBmeVhoB566bIw5zwXbv/Uw0XtLmJBcN287oE12szlvHwpb4Jh6zvEkD/xrHI5rQzaiqfyIE5qW4+XLKLaRMESfGNOxWnOvQ5J+7ImfW0c+nBt8XLv+SxqHxIRJE9IJseKP9D/NKBMcGF8NApVwrlb9oQelaLszLqClZXhfiNwjckJmTIv8F+mWHTV5b25TVlj+Y5nmMm3yuoBnWgI4PQCBoeHVHmupKxMW4Gxnuf/+4TNorCL4LUXdig9iA+d7ZS5RIwNvOY8c/OLX4r84W8n2xw//2CC6xwkm4eqFRKRIN9Rb4BbE331uD7AYdQ== camilla.vanklinken@digital.cabinet-office.gov.uk',
-  }
-}
