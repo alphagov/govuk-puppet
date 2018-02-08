78733c100cc071b313cf8591f3bdc0e0db9d66e4
Chris Roos <chris@seagul.co.uk>
2018-02-08 16:24:15 +0000

    Store Whitehall asset paths as an array
    These paths are all proxied to whitehall-frontend so we can remove some
    duplication by storing them as an array instead of a hash.

--- modules/router/manifests/assets_origin.pp
+++ modules/router/manifests/assets_origin.pp
@@ -11,7 +11,7 @@
 #   Hash of paths => vhost_names.  Each entry will be added as a route in the vhost
 #
 # [*whitehall_asset_routes*]
-#   Hash of paths => vhost_names.  Each entry will be added as a route in the vhost
+#   Array of paths to proxy to whitehall-frontend.  Each entry will be added as a route in the vhost
 #
 # [*real_ip_header*]
 #   Uses the Nginx realip module (http://nginx.org/en/docs/http/ngx_http_realip_module.html)
@@ -26,7 +26,7 @@
 class router::assets_origin(
   $app_specific_asset_routes = {},
   $asset_manager_routes = [],
-  $whitehall_asset_routes = {},
+  $whitehall_asset_routes = [],
   $real_ip_header = '',
   $vhost_aliases = [],
   $vhost_name = 'assets-origin',
