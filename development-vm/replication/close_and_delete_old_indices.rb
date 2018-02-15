6e523fef19cba8010b59d472204b27bbc6efa77a
David Henry <david@decoybecoy.com>
2018-02-15 12:44:28 +0000

    Deal with edge case when index doesn't exist

--- development-vm/replication/close_and_delete_old_indices.rb
+++ development-vm/replication/close_and_delete_old_indices.rb
@@ -12,7 +12,7 @@ Net::HTTP.start(list_uri.host, list_uri.port) do |http|
   resp = http.get(list_uri.path)
   data = JSON.parse(resp.body)
 
-  used_aliases = valid_indices.flat_map { |index_name| data[index_name]['aliases'].keys }
+  used_aliases = valid_indices.flat_map { |index_name| data[index_name] ? data[index_name]['aliases'].keys : [] }
   data.each do |index_name, aliases|
     next if valid_indices.include?(index_name)
     if (aliases['aliases'].keys & used_aliases).any?
