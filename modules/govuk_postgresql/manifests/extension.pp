e1c751f86c79dd988d965f59c033e5707aa590b8
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-02-28 10:17:08 +0000

    Create an extension with quotes around the name
    When trying to install the extension 'uuid-ossp' this failed
    because of the dash in the name, but if we use quotes this should
    succeed.

--- modules/govuk_postgresql/manifests/extension.pp
+++ modules/govuk_postgresql/manifests/extension.pp
@@ -19,7 +19,7 @@ define govuk_postgresql::extension {
         $db = $extracted_title[0]
         $extension = $extracted_title[1]
         exec {"Load ${extension} for Postgres DB: ${db}":
-            command => "psql -d ${db} -c 'CREATE EXTENSION IF NOT EXISTS ${extension}'",
+            command => "psql -d ${db} -c 'CREATE EXTENSION IF NOT EXISTS \"${extension}\"'",
             unless  => "psql -d ${db} -c '\\dx' | grep -q ${extension}",
             user    => 'postgres',
             require => [Class['postgresql::server::contrib'], Postgresql::Server::Db[$db]],
