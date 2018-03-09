780c7888f72412b3f6888722f3536bf3400c3d25
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2018-03-09 12:05:11 +0000

    Increase cpu warning threshold for gov frontend
    We've increased `government-frontend` machines from 2 cores to 4 cores, so we need
    to change the cpu warning threshold for high CPU usage.

--- modules/govuk/manifests/apps/government_frontend.pp
+++ modules/govuk/manifests/apps/government_frontend.pp
@@ -21,12 +21,21 @@
 #   The number of unicorn worker processes to run
 #   Default: undef
 #
+# [*cpu_warning*]
+#   CPU usage percentage that alerts are sounded at
+#   Default: undef
+#
+# [*cpu_critical*]
+#   CPU usage percentage that alerts are sounded at
+#
 class govuk::apps::government_frontend(
   $vhost = 'government-frontend',
   $port = '3090',
   $sentry_dsn = undef,
   $secret_key_base = undef,
   $unicorn_worker_processes = undef,
+  $cpu_warning = 150,
+  $cpu_critical = 200,
 ) {
   Govuk::App::Envvar {
     app => 'government-frontend',
@@ -48,5 +57,7 @@ class govuk::apps::government_frontend(
     asset_pipeline_prefix    => 'government-frontend',
     vhost                    => $vhost,
     unicorn_worker_processes => $unicorn_worker_processes,
+    cpu_warning              => $cpu_warning,
+    cpu_critical             => $cpu_critical,
   }
 }
