6bb09ddbe137d993fd8bff6f7b989cd1dcb1d60e
Pablo Manrubia <pmanrubia@gmail.com>
2018-03-15 16:16:06 +0000

    Remove CPM jenkins processes to retrieve Items and Metrics

--- modules/govuk_jenkins/manifests/jobs/content_performance_manager.pp
+++ modules/govuk_jenkins/manifests/jobs/content_performance_manager.pp
@@ -5,17 +5,7 @@
 #
 # === Parameters:
 #
-# [*rake_import_all_content_items_frequency *]
-#   The cron timings for the import:all_content_items rake task
-#   Default: undef
-#
-# [*rake_import_all_ga_metrics_frequency *]
-#   The cron timings for the import:all_content_items rake task
-#   Default: undef
-#
 class govuk_jenkins::jobs::content_performance_manager (
-  $rake_import_all_content_items_frequency = undef,
-  $rake_import_all_ga_metrics_frequency = undef,
 ) {
   file { '/etc/jenkins_jobs/jobs/content_performance_manager.yaml':
     ensure  => present,
