caf6d3a41b83cba59740b1b743fbaffc32738c90
mahmudh <mahmud.hussain@digital.cabinet-office.gov.uk>
2018-04-09 11:34:24 +0100

    Add user mahmudhussain

---
+++ modules/users/manifests/mahmudhussain.pp
@@ -0,0 +1,8 @@
+# Creates mahmudhussain user
+class users::mahmudhussain {
+  govuk_user { 'mahmudhussain':
+    fullname => 'Mahmud Hussain',
+    email    => 'mahmud.hussain@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDZgqKzBeTdYQt4dgD1NhDtwiKtTaR7HTPd4MiDkVRppgrnO9bPBZ49bUNyudraeCFWR3FphaW/XSxBTyCUH2c2F4AbL8evJwgREtxZr4RD7tjohBuYhz+16h4hFLRMhVBoZNEbqatr9LpTRvuhXmtVNfD/CKWujuWnzZOPrEiuUXCLmRnSUg/d/WcRMfSl0ih06d7lszNKcguIsvhIS8ps+7eSWneEAHa90wyzSSIJQZ92NKAGwL4X/24QM/ySEwOKzr+e0j7c312PJZOzYg0qfPLh/f1FKhIz8BuHWFDCQxk3UCvQO2e59oQbKVP4xAj/Hl1bJhfhDxvYC5da7HgXro4nutgnPnpk4mwLyWycBKO0adPhEmFNsnL0TGtvjjDDHSewBqzJN75KW84A41vHn1rK/uINuaQo5IZNM29/sxtLXMVHd7YSOR7PFUswARk6WPk6dF/6u/te/9DjIlhVeYIl82OE+4ZlHq3sNV8RL9fJuLk99sVkEBLmAE6IXVVy8wOI38mqJzDjv2lwmORt9q7z7vYJ9S7ZrkHsX5gpbouUF5LJIrG7mKyXqQvA3mA5YJns1SIROp8JC6FTIAuV6fCcN6UY+YIrJNNQLEXxeMurKJtui1ppwbqLKvp04bo3/ad9JVv0NAKOSEKX7NYMyrlWl3rkVY1WYDqI/IIJdQ== mahmud.hussain@digital.cabinet-office.gov.uk',
+  }
+}
