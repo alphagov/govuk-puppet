1ffbf0eeb7b338a94e5e15fec94216febf28bc46
Erin Raj-Staniland <erin.raj-staniland@digital.cabinet-office.gov.uk>
2018-04-12 11:18:11 +0100

    Add erinrajstaniland user

---
+++ modules/users/manifests/erinrajstaniland.pp
@@ -0,0 +1,8 @@
+# Creates erinrajstaniland user
+class users::erinrajstaniland {
+  govuk_user { 'erinrajstaniland':
+    fullname => 'Erin Raj-Staniland',
+    email    => 'erin.raj-staniland@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCiysxqr+fXT6xAbvU0mwQaccFCLSQpPUAvCoAftKHccysoBPWILCb2fcBJR9sWMwP9DmbLXuqXWC1S+0XNh3GHZQlkvLgm74wk7zzugwsYiyFPOOiKBApMNJOlNLd+r6WmD1kMFxUzzTP5T6/qP4qII4P77RCj3/7fHpMH9Cq6iWQxWVa4l6r0B3RB7SrBtHGMrweyTb3YgbIBC+KfebkGyQWJF/IcOjixS+zOci3b+Ob6o+G9z+CUaDUZedzFsCO9f9mPQFyr+EbpIKrHAewTr4unkMXHD4SxcQLn2aTsjU7DuDtWVZ+UvVO0uUDNRTqSDkOrU0j6AhcPDLL5xun6VoaQN4Y1sOwLpOD6J1d5+saYzINfloduuVfXlPW6bWgffSn/3WK6pW0N6mnbHmgqXE2esG3CXZJQyu3MGUzfKNuaiE79oC5KZCHnMgShr8yhzOVO4qIDtu0f/8/5ISy8IJ0yKgbZEm+kvKu7c+h7KYzZTGCLzHu2cf216jhvz0vinm8sloJvchKNocMBle6VDx5UlFf9P6qpp/4BEyQyjD1ajhl5CN3+GPyNXtVjZi0y54aMAfa5JEYVg6ADsLNIk8KRcWuiktyYXi0glDArvvVUSh9wbRVW4M9n2Q3CVlnDgDeJkmIMpz+hL0uvbfypLJN57m8UFvh5j8lLgNmmbw== erin.raj-staniland@digital.cabinet-office.gov.uk',
+  }
+}
