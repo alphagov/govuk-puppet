3fb3bf33649211522670701d6c6fecef5a5ac568
Kevin Dew <kevindew@me.com>
2018-04-12 10:28:03 +0100

    Switch to Java 8 on CI Agents
    It's a bit weird that this is done through Elasticsearch I'll try sort
    this in a subsequent commit.

--- modules/govuk_ci/manifests/agent/elasticsearch.pp
+++ modules/govuk_ci/manifests/agent/elasticsearch.pp
@@ -3,19 +3,19 @@
 # Installs and configures elasticsearch
 #
 class govuk_ci::agent::elasticsearch {
-  include ::govuk_java::openjdk7::jre
-  include ::govuk_java::openjdk7::jdk
+  include ::govuk_java::openjdk8::jre
+  include ::govuk_java::openjdk8::jdk
 
   class { 'govuk_java::set_defaults':
-    jdk => 'openjdk7',
-    jre => 'openjdk7',
+    jdk => 'openjdk8',
+    jre => 'openjdk8',
   }
 
   class { 'govuk_elasticsearch':
     cluster_hosts => ["${::hostname}:9300"],
     cluster_name  => 'elasticsearch',
     host          => $::fqdn,
-    require       => [Class['govuk_java::openjdk7::jre'],Class['govuk_java::set_defaults']],
+    require       => [Class['govuk_java::openjdk8::jre'],Class['govuk_java::set_defaults']],
   }
 
 }
