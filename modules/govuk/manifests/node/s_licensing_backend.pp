83987d45a9bee8d27f21801e7470ee5a64f7d0dc
Kevin Dew <kevindew@me.com>
2018-04-16 12:17:26 +0100

    Switch Licensing to use OpenJDK rather than Oracle for Java
    This is because our Oracle 8 package will no longer install so we can't
    actually build this on new machines.

--- modules/govuk/manifests/node/s_licensing_backend.pp
+++ modules/govuk/manifests/node/s_licensing_backend.pp
@@ -5,10 +5,27 @@
 class govuk::node::s_licensing_backend inherits govuk::node::s_base {
   include clamav
 
-  include govuk_java::oracle8
+  class { 'govuk_java::oracle8':
+    ensure => absent,
+  }
 
+  include govuk_java::openjdk8::jdk
+  include govuk_java::openjdk8::jre
   include licensify::apps::licensify_admin
   include licensify::apps::licensify_feed
+
+  class { 'govuk_java::set_defaults':
+    jdk     => 'openjdk8',
+    jre     => 'openjdk8',
+    require => [
+                Class['govuk_java::openjdk8::jdk'],
+                Class['govuk_java::openjdk8::jre'],
+              ],
+    notify  => [
+                Class['licensify::apps::licensify_admin'],
+                Class['licensify::apps::licensify_feed'],
+              ],
+  }
 
   include nginx
 }
