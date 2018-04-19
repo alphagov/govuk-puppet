9cc87f09d1561c408a40b787605c31e7672aa1bc
suthagarht <suthagar.kathirkamathamby@digital.cabinet-office.gov.uk>
2018-04-19 14:14:21 +0100

    Data-sync Changes
    - These changes will enable data-sync from Production Carranza to AWS.
    
    Solo: @suthagarht

--- modules/govuk_jenkins/manifests/jobs/copy_data_from_production_to_aws.pp
+++ modules/govuk_jenkins/manifests/jobs/copy_data_from_production_to_aws.pp
@@ -3,11 +3,12 @@
 # Create a file on disk that can be parsed by jenkins-job-builder
 #
 class govuk_jenkins::jobs::copy_data_from_production_to_aws (
-  $mysql_src_root_pw = undef,
+  $mysql_src_root_pw = hiera('mysql_root'),
   $mysql_dst_root_pw = undef,
   $pg_src_env_sync_pw = undef,
   $pg_dst_env_sync_pw = undef,
   $pg_tr_dst_env_sync_pw = undef,
+  $pg_tr_src_env_sync_pw = undef,
   $whitehall_mysql_password = undef,
   $app_domain = hiera('app_domain'),
 ) {
