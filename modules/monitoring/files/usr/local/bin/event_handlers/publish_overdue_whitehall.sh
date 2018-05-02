8246d8b4c3edc5943cf1c5e5e7e34b507a93cc11
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-05-02 14:43:53 +0100

    Trigger overdue publications task on a soft status

--- modules/monitoring/files/usr/local/bin/event_handlers/publish_overdue_whitehall.sh
+++ modules/monitoring/files/usr/local/bin/event_handlers/publish_overdue_whitehall.sh
@@ -8,9 +8,9 @@ SERVICESTATETYPE=$2
 HOSTADDRESS=$3
 
 case "${SERVICESTATE}" in
-  CRITICAL)
+  WARNING|CRITICAL)
     case "${SERVICESTATETYPE}" in
-      HARD)
+      SOFT)
         logger --tag govuk_icinga_event_handler "Publishing overdue Whitehall documents on ${HOSTADDRESS}"
         /usr/lib/nagios/plugins/check_nrpe -H ${HOSTADDRESS} -c publish_overdue_whitehall
         ;;
