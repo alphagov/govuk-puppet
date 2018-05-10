f0eb4ea189bae1bf655f2a8fce8b7b9834e4e652
Paul Bowsher <paul.bowsher@gmail.com>
2018-05-10 13:26:33 +0100

    Enable storing wal segments in Standalone mode
    WAL backups using WAL-E aren't possible if we don't store the WAL segments in the first place. For this case, it's simplest to just pretend we're a primary server to be replicated from.

--- modules/govuk_postgresql/manifests/server/standalone.pp
+++ modules/govuk_postgresql/manifests/server/standalone.pp
@@ -5,4 +5,13 @@
 class govuk_postgresql::server::standalone {
   include govuk_postgresql::server
   include govuk_postgresql::server::not_slave
+
+  postgresql::server::config_entry {
+    'wal_level':
+      value => 'replica';
+    'max_wal_senders':
+      value => 3;
+    'wal_keep_segments':
+      value => 256;
+  }
 }
