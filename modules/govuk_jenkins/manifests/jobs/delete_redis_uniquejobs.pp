d6fd7a0af4eedff7660a48baf9f7de86ffe79379
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2018-05-23 15:31:03 +0100

    Add a job to clear the redis uniquejobs hash
    The redis uniquejobs hash has the propensity to grow and use all of the
    available redis memory. We want to regularly clear it.

---
+++ modules/govuk_jenkins/manifests/jobs/delete_redis_uniquejobs.pp
@@ -0,0 +1,12 @@
+# == Class: govuk_jenkins::jobs::whitehall_publisher_notifications
+#
+# Create a jenkins job to periodically remove redis uniquejobs key
+#
+class govuk_jenkins::jobs::delete_redis_uniquejobs {
+  file { '/etc/jenkins_jobs/jobs/delete_redis_uniquejobs.yaml':
+    ensure  => present,
+    content => template('govuk_jenkins/jobs/delete_redis_uniquejobs.yaml.erb'),
+    notify  => Exec['jenkins_jobs_update'],
+  }
+}
+
