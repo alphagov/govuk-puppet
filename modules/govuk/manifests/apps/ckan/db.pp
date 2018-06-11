7afd0a447eccad04dafeb337312a1f38aef88376
Paul Bowsher <paul.bowsher@digital.cabinet-office.gov.uk>
2018-06-11 18:02:57 +0100

    Enable PostGIS on main postgresql server
    This is only needed outside of RDS

--- modules/govuk/manifests/apps/ckan/db.pp
+++ modules/govuk/manifests/apps/ckan/db.pp
@@ -13,11 +13,16 @@ class govuk::apps::ckan::db (
   $backend_ip_range = '10.3.0.0/16',
   $rds = false,
 ) {
+  if !$rds {
+    class { 'postgresql::server::postgis': }
+  }
+
   govuk_postgresql::db { 'ckan_production':
     user                    => 'ckan',
     password                => $password,
     allow_auth_from_backend => true,
     backend_ip_range        => $backend_ip_range,
     rds                     => $rds,
+    extensions              => ['plpgsql', 'postgis'],
   }
 }
