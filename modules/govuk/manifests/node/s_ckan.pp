8f85d4b23280c65540bfcee89d9796a86c638f42
Paul Bowsher <paul.bowsher@digital.cabinet-office.gov.uk>
2018-06-12 09:03:01 +0100

    Add puppet class for the ckan machine class

---
+++ modules/govuk/manifests/node/s_ckan.pp
@@ -0,0 +1,28 @@
+# == Class: govuk::node::s_ckan
+#
+# Backend machine definition. Backend machines are used for running
+# publishing, administration and management applications.
+#
+class govuk::node::s_ckan inherits govuk::node::s_base {
+  limits::limits { 'root_nofile':
+    ensure     => present,
+    user       => 'root',
+    limit_type => 'nofile',
+    both       => 16384,
+  }
+
+  limits::limits { 'root_nproc':
+    ensure     => present,
+    user       => 'root',
+    limit_type => 'nproc',
+    both       => 1024,
+  }
+
+  include govuk_python
+  include nginx
+  include postgresql::lib::devel
+
+  package { 'libgeos-c1':
+    ensure => latest,
+  }
+}
