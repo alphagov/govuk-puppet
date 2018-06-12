5e9a9d409e84324878c06baa82ef409bb4ddf34b
Paul Bowsher <paul.bowsher@digital.cabinet-office.gov.uk>
2018-06-12 08:58:30 +0100

    Add puppet class to configure the CKAN application
    Most of the heavy lifting for this deployment will
    be done in the govuk-app-deployment script, including
    installing python dependencies. This simply sets up
    homes for things and how we'll eventually run the app
    as a service.

---
+++ modules/govuk/manifests/apps/ckan/db.pp
@@ -0,0 +1,23 @@
+# == Class: govuk::apps::ckan::db
+#
+# === Parameters
+#
+# [*password*]
+#   The DB instance password.
+#
+# [*backend_ip_range*]
+#   Backend IP addresses to allow access to the database.
+#
+class govuk::apps::ckan::db (
+  $password,
+  $backend_ip_range = '10.3.0.0/16',
+  $rds = false,
+) {
+  govuk_postgresql::db { 'ckan_production':
+    user                    => 'ckan',
+    password                => $password,
+    allow_auth_from_backend => true,
+    backend_ip_range        => $backend_ip_range,
+    rds                     => $rds,
+  }
+}
