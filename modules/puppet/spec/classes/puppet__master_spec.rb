1ac147fe77b7a9328e73d04909321d2e94913a81
Doug Neal <doug@neal.me.uk>
2018-07-17 16:46:11 +0100

    Update rspec for puppet::master
    Test for resources created by govuk_puppetdb, rather than the
    govuk_puppetdb class.

--- modules/puppet/spec/classes/puppet__master_spec.rb
+++ modules/puppet/spec/classes/puppet__master_spec.rb
@@ -14,7 +14,21 @@ describe 'puppet::master', :type => :class do
     is_expected.to contain_service('puppetmaster').with_provider('upstart').with_ensure('running')
   end
 
-  it { is_expected.to contain_class('govuk_puppetdb') }
+  it {
+    is_expected.to contain_package('puppetdb')
+    is_expected.to contain_package('puppetdb-terminus')
+    is_expected.to contain_exec('puppetdb ssl-setup')
+      .with_command('/usr/sbin/puppetdb-ssl-setup')
+      .that_requires('Class[puppet::master::generate_cert]')
+    is_expected.to contain_exec('disable-default-puppetdb')
+    is_expected.to contain_file('/etc/init/puppetdb.conf')
+    is_expected.to contain_govuk_postgresql__db('puppetdb')
+    is_expected.to contain_file('/etc/init/puppetdb.conf').with_content(/-Xmx1024m/)
+    is_expected.to contain_file('/etc/puppetdb/conf.d/database.ini')
+    is_expected.to contain_file('/etc/puppetdb/conf.d/config.ini').with_content(/logback.xml/)
+    is_expected.to contain_file('/etc/puppetdb/conf.d/repl.ini')
+  }
+
   it { is_expected.to contain_class('puppet::master::nginx') }
 
   it do
