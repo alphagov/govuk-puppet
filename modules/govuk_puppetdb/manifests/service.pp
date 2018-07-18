a8de48d558bf82b521b0882f82400fbfc8728250
Doug Neal <doug@neal.me.uk>
2018-07-18 11:10:12 +0100

    Delete govuk_puppetdb module
    Code for managing PuppetDB now lives entirely within `puppet::puppetdb`.

--- modules/govuk_puppetdb/manifests/service.pp
+++
@@ -1,8 +0,0 @@
-# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk_puppetdb::service {
-
-  service { 'puppetdb':
-    ensure  => running,
-  }
-
-}
