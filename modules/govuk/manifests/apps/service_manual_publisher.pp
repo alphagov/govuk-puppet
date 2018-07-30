391fc9c9810104fe7f0e16a46f5c53a79b465220
Michael Walker <mike@barrucadu.co.uk>
2018-07-30 10:36:36 +0100

    Use pgbouncer for service-manual-publisher in AWS

--- modules/govuk/manifests/apps/service_manual_publisher.pp
+++ modules/govuk/manifests/apps/service_manual_publisher.pp
@@ -13,6 +13,14 @@
 # [*db_password*]
 #   The password for the database.
 #
+# [*db_port*]
+#   The port of the database server to use in the DATABASE_URL.
+#   Default: undef
+#
+# [*db_allow_prepared_statements*]
+#   The ?prepared_statements= parameter to use in the DATABASE_URL.
+#   Default: undef
+#
 # [*db_name*]
 #   The database name to use in the DATABASE_URL.
 #
@@ -49,6 +57,8 @@
 #
 class govuk::apps::service_manual_publisher(
   $db_hostname = 'postgresql-primary-1.backend',
+  $db_port = undef,
+  $db_allow_prepared_statements = undef,
   $db_name = 'service-manual-publisher_production',
   $db_password = undef,
   $db_username = 'service_manual_publisher',
@@ -110,11 +120,13 @@ class govuk::apps::service_manual_publisher(
 
   if $::govuk_node_class !~ /^development$/ {
     govuk::app::envvar::database_url { $app_name:
-      type     => 'postgresql',
-      username => $db_username,
-      password => $db_password,
-      host     => $db_hostname,
-      database => $db_name,
+      type                      => 'postgresql',
+      username                  => $db_username,
+      password                  => $db_password,
+      host                      => $db_hostname,
+      port                      => $db_port,
+      allow_prepared_statements => $db_allow_prepared_statements,
+      database                  => $db_name,
     }
   }
 }
