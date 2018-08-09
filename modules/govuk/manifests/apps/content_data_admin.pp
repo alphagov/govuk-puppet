39fa51e033df4b04835b7dee140f34759962a0d4
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-08-09 09:39:52 +0000

    Remove the unessessary if statement

--- modules/govuk/manifests/apps/content_data_admin.pp
+++ modules/govuk/manifests/apps/content_data_admin.pp
@@ -38,18 +38,18 @@
 #   The database name to use for the DATABASE_URL environment variable
 #
 class govuk::apps::content_data_admin (
-  $port = '3230',
-  $enabled = false,
-  $secret_key_base = undef,
-  $sentry_dsn = undef,
-  $oauth_id = undef,
-  $oauth_secret = undef,
-  $db_username = 'content_data_admin',
-  $db_hostname = undef,
-  $db_port = undef,
+  $port                         = '3230',
+  $enabled                      = false,
+  $secret_key_base              = undef,
+  $sentry_dsn                   = undef,
+  $oauth_id                     = undef,
+  $oauth_secret                 = undef,
+  $db_username                  = 'content_data_admin',
+  $db_hostname                  = undef,
+  $db_port                      = undef,
   $db_allow_prepared_statements = undef,
-  $db_password = undef,
-  $db_name = 'content_data_admin_production',
+  $db_password                  = undef,
+  $db_name                      = 'content_data_admin_production',
 ) {
   $app_name = 'content-data-admin'
 
@@ -59,45 +59,43 @@ class govuk::apps::content_data_admin (
   }
 
   # see modules/govuk/manifests/app.pp for more options
-  if $enabled {
-    govuk::app { $app_name:
-      ensure            => $ensure,
-      app_type          => 'rack',
-      port              => $port,
-      sentry_dsn        => $sentry_dsn,
-      vhost_ssl_only    => true,
-      health_check_path => '/healthcheck', # must return HTTP 200 for an unauthenticated request
-      deny_framing      => true,
-      asset_pipeline    => true,
-    }
+  govuk::app { $app_name:
+    ensure            => $ensure,
+    app_type          => 'rack',
+    port              => $port,
+    sentry_dsn        => $sentry_dsn,
+    vhost_ssl_only    => true,
+    health_check_path => '/healthcheck', # must return HTTP 200 for an unauthenticated request
+    deny_framing      => true,
+    asset_pipeline    => true,
+  }
 
-    Govuk::App::Envvar {
-      app            => $app_name,
-      ensure         => $ensure,
-      notify_service => $enabled,
-    }
+  Govuk::App::Envvar {
+    app            => $app_name,
+    ensure         => $ensure,
+    notify_service => $enabled,
+  }
 
-    govuk::app::envvar {
-      "${title}-SECRET_KEY_BASE":
-        varname => 'SECRET_KEY_BASE',
-        value   => $secret_key_base;
-      "${title}-OAUTH_ID":
-        varname => 'OAUTH_ID',
-        value   => $oauth_id;
-      "${title}-OAUTH_SECRET":
-        varname => 'OAUTH_SECRET',
-        value   => $oauth_secret;
-    }
+  govuk::app::envvar {
+    "${title}-SECRET_KEY_BASE":
+      varname => 'SECRET_KEY_BASE',
+      value   => $secret_key_base;
+    "${title}-OAUTH_ID":
+      varname => 'OAUTH_ID',
+      value   => $oauth_id;
+    "${title}-OAUTH_SECRET":
+      varname => 'OAUTH_SECRET',
+      value   => $oauth_secret;
+  }
 
-    if $::govuk_node_class !~ /^development$/ {
-      govuk::app::envvar::database_url { $app_name:
-        type     => 'postgresql',
-        username => $db_username,
-        password => $db_password,
-        host     => $db_hostname,
-        port     => $db_port,
-        database => $db_name,
-      }
+  if $::govuk_node_class !~ /^development$/ {
+    govuk::app::envvar::database_url { $app_name:
+      type     => 'postgresql',
+      username => $db_username,
+      password => $db_password,
+      host     => $db_hostname,
+      port     => $db_port,
+      database => $db_name,
     }
   }
 }
