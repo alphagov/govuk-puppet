4349f64eb8a8c0eadbf8dc0ab0ab26e00701c5e8
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-09-03 18:03:52 +0100

    Add import job for content publisher
    This commit adds a Jenkins job to stream documents from Whitehall into
    Content Publisher over an SSH pipe, on integration only. This means we
    can regularly test importing data as part of the lifecycle of the
    application, and also preview the app with realistic data.
    
    For some reason the combination of commands on the Whitehall machine
    makes all the output go to STDERR. That and the unwanted logging sniffer
    output means we use grep to filter for the lines which contain a valid
    document export. Although somewhat hacky, this is only a temporary job.

---
+++ modules/govuk_jenkins/manifests/jobs/content_publisher_whitehall_import.pp
@@ -0,0 +1,18 @@
+# == Class: govuk_jenkins::jobs::content_publisher_whitehall_import
+#
+# Create a file on disk that can be parsed by jenkins-job-builder
+#
+class govuk_jenkins::jobs::content_publisher_whitehall_import (
+  $enable_slack_notifications = false,
+  $app_domain = hiera('app_domain'),
+) {
+  $slack_team_domain = 'gds'
+  $slack_room = 'govuk-pubworkflow-dev'
+  $slack_build_server_url = "https://deploy.${app_domain}/"
+
+  file { '/etc/jenkins_jobs/jobs/content_publisher_whitehall_import.yaml':
+    ensure  => present,
+    content => template('govuk_jenkins/jobs/content_publisher_whitehall_import.yaml.erb'),
+    notify  => Exec['jenkins_jobs_update'],
+  }
+}
