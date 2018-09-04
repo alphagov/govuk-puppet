14406d91b8e100c8bebd89106fef755dd55a653e
Doug Neal <doug@neal.me.uk>
2018-09-04 16:45:07 +0100

    Puppetise the data scrubber
    A minimalist Puppet class to install and configure the data scrubber.

---
+++ modules/govuk_datascrubber/manifests/init.pp
@@ -0,0 +1,79 @@
+# == Class: govuk_datascrubber
+#
+# Install the data scrubber and configure cron to run it
+# https://github.com/alphagov/govuk-datascrubber
+#
+# === Parameters:
+#
+# [*ensure*]
+#   Specify whether to install or remove the tool and its cron job(s)
+#   Default is 'latest'
+#
+# [*mysql_hosts*]
+#   List of hostnames of MySQL servers to look for snapshots of
+#   Default is 'mysql-primary'
+#
+# [*postgresql_hosts*]
+#   List of hostnames of Postgres servers to look for snapshots of
+#   Default is 'postgresql-primary'
+#
+# [*cron_user*]
+#   The account to run the cron job as (default: 'deploy')
+#
+# [*cron_hour*]
+#   To control the schedule.
+#   Passed through to the `hour` parameter of the `cron` resource.
+#   Default is 20 (8pm)
+#
+# [*cron_minute*]
+#   To control the schedule.
+#   Passed through to the `minute` parameter of the `cron` resource
+#   Default is 0 (first minute of the hour)
+#
+# [*share_with*]
+#   List of AWS accounts to share the scrubbed snapshots with.
+#   Defaults to empty list.
+#
+class govuk_datascrubber (
+  $ensure           = 'latest',
+  $mysql_hosts      = ['mysql-primary'],
+  $postgresql_hosts = ['postgresql-primary'],
+  $cron_user        = 'deploy',
+  $cron_hour        = 20,
+  $cron_minute      = 0,
+  $share_with       = [],
+) {
+
+  package { 'govuk-datascrubber':
+    ensure => $ensure,
+  }
+
+  $cron_command = shellquote(
+    flatten([
+      'datascrubber',
+
+      '--mysql-hosts', $mysql_hosts,
+      '--postgresql-hosts', $postgresql_hosts,
+
+      size($share_with) ? {
+        0       => [],
+        default => ['--share-with', $share_with],
+      }
+
+    ])
+  )
+
+  $cron_ensure = $ensure ? {
+    'absent' => 'absent',
+    default  => 'present',
+  }
+
+  cron { 'datascrubber' :
+    ensure  => $cron_ensure,
+    user    => $cron_user,
+    hour    => $cron_hour,
+    minute  => $cron_minute,
+    command => $cron_command,
+  }
+
+}
