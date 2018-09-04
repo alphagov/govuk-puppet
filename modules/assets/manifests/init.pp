ec8131bb3755e28f708f95a40d0177f16d53adfd
Michael Walker <mike@barrucadu.co.uk>
2018-09-04 10:54:55 +0100

    Remove asset-master and asset-slave boxes

--- modules/assets/manifests/init.pp
+++ modules/assets/manifests/init.pp
@@ -2,14 +2,7 @@
 #
 # Configures a machine ready to serve assets.
 #
-# === Parameters
-#
-# [*mount_asset_master*]
-#   Determines whether to mount the asset master's NFS share.
-#   Default: true
-#
 class assets (
-  $mount_asset_master = true,
   ) {
 
   unless $::aws_migration {
@@ -32,27 +25,5 @@ class assets (
     owner  => undef,
     group  => undef,
     mode   => undef,
-  }
-
-  validate_bool($mount_asset_master)
-  if $mount_asset_master {
-    $app_domain = hiera('app_domain')
-
-    if $::aws_migration {
-      $app_domain_internal = hiera('app_domain_internal')
-      $mount_point = "assets.${app_domain_internal}:/"
-    } else {
-      $mount_point = "asset-master.${app_domain}:/mnt/uploads"
-    }
-
-    mount { '/data/uploads':
-      ensure   => 'mounted',
-      device   => $mount_point,
-      fstype   => 'nfs',
-      options  => 'rw,soft',
-      remounts => false,
-      atboot   => true,
-      require  => [File['/data/uploads'], Package['nfs-common']],
-    }
   }
 }
