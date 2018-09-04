3be78659ffd70d1f52d0ae35fe3c0a865ebe761b
Michael Walker <mike@barrucadu.co.uk>
2018-09-04 11:09:58 +0100

    Ensure the asset NFS mounts are unmounted

--- modules/assets/manifests/init.pp
+++ modules/assets/manifests/init.pp
@@ -26,4 +26,25 @@ class assets (
     group  => undef,
     mode   => undef,
   }
+
+  # ensure the mounts are gone: todo, remove this after it's been
+  # deployed
+  $app_domain = hiera('app_domain')
+
+  if $::aws_migration {
+    $app_domain_internal = hiera('app_domain_internal')
+    $mount_point = "assets.${app_domain_internal}:/"
+  } else {
+    $mount_point = "asset-master.${app_domain}:/mnt/uploads"
+  }
+
+  mount { '/data/uploads':
+    ensure   => 'absent',
+    device   => $mount_point,
+    fstype   => 'nfs',
+    options  => 'rw,soft',
+    remounts => false,
+    atboot   => true,
+    require  => [File['/data/uploads'], Package['nfs-common']],
+  }
 }
