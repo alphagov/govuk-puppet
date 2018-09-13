e5ffffbbdd953ce6020a146d6fb969d8cdc537ee
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-09-13 10:49:43 +0100

    Add AWS X-Ray daemon configuration
    This commit adds configuration for the AWS X-Ray daemon. This includes configuring the correct AWS region and providing credentials.

--- modules/govuk_aws_xray_daemon/manifests/init.pp
+++ modules/govuk_aws_xray_daemon/manifests/init.pp
@@ -7,8 +7,16 @@
 # [*apt_mirror_hostname*]
 #   The hostname of the Apt mirror containing the aws-xray-daemon repo
 #
+# [*aws_access_key_id*]
+#   The AWS access key for the IAM role that has permissions to upload traces to AWS X-Ray
+#
+# [*aws_secret_access_key*]
+#   The AWS secret access key for the IAM role that has permissions to upload traces to AWS X-Ray
+#
 class govuk_aws_xray_daemon (
-  $apt_mirror_hostname,
+  $apt_mirror_hostname = undef,
+  $aws_access_key_id = undef,
+  $aws_secret_access_key = undef,
 )
 {
   apt::source { 'aws-xray-daemon':
@@ -20,14 +28,48 @@ class govuk_aws_xray_daemon (
   }
 
   user { 'xray':
-    ensure => present,
-    name   => 'xray',
-    shell  => '/bin/false',
-    system => true,
+    ensure     => present,
+    name       => 'xray',
+    home       => '/home/xray',
+    managehome => true,
+    shell      => '/bin/false',
+    system     => true,
+  }
+
+  file { '/home/xray':
+    ensure  => directory,
+    owner   => 'xray',
+    group   => 'xray',
+    mode    => '0750',
+    require => User['xray'],
+  }
+
+  file { '/home/xray/.aws':
+    ensure => directory,
+    owner  => 'xray',
+    group  => 'xray',
+    mode   => '0700',
+  }
+
+  file { '/home/xray/.aws/credentials':
+    ensure  => present,
+    owner   => 'xray',
+    group   => 'xray',
+    mode    => '0600',
+    content => template('govuk_aws_xray_daemon/credentials.erb'),
   }
 
   package { 'xray':
     ensure  => latest,
     require => Apt::Source['aws-xray-daemon'],
+  }
+
+  file { '/etc/amazon/xray/cfg.yaml':
+    ensure  => present,
+    owner   => 'xray',
+    group   => 'xray',
+    mode    => '0644',
+    source  => 'puppet:///modules/govuk_aws_xray_daemon/etc/amazon/xray/cfg.yaml',
+    require => Package['xray'],
   }
 }
