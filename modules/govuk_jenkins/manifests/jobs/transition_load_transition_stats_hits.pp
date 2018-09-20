457ea69650ce9a0a2f50c19b09a062f798000d16
Michael Walker <mike@barrucadu.co.uk>
2018-09-20 16:01:49 +0100

    Remove hourly transition hit stats job

--- modules/govuk_jenkins/manifests/jobs/transition_load_transition_stats_hits.pp
+++ modules/govuk_jenkins/manifests/jobs/transition_load_transition_stats_hits.pp
@@ -4,8 +4,6 @@
 #
 class govuk_jenkins::jobs::transition_load_transition_stats_hits {
   file { '/etc/jenkins_jobs/jobs/transition_load_transition_stats_hits.yaml':
-    ensure  => present,
-    content => template('govuk_jenkins/jobs/transition_load_transition_stats_hits.yaml.erb'),
-    notify  => Exec['jenkins_jobs_update'],
+    ensure => absent,
   }
 }
