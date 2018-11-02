9e199b7a54a180803afef019ff4ccded892417ed
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-11-02 14:47:44 +0000

    Upgrade nginx
    This commit replaces the default Ubuntu repos with a mirrored version of the nginx master repo for nginx packages.
    
    It also remodels the `nginx::package` class to reflect the different package layout.
    
    `nginx-module-perl` is installed since this is the only module includes in `nginx-extras` that we actually use, for tracking request IDs across the stack.

--- modules/nginx/spec/classes/nginx__package_spec.rb
+++ modules/nginx/spec/classes/nginx__package_spec.rb
@@ -3,46 +3,27 @@ require_relative '../../../../spec_helper'
 describe 'nginx::package', :type => :class do
   context 'defaults' do
     it do
-      is_expected.to contain_package('nginx-full').with(
+      is_expected.to contain_package('nginx-module-perl').with(
         'ensure'  => 'present',
         'notify'  => 'Class[Nginx::Restart]',
-        'require' => 'Package[nginx-common]',
+        'require' => 'Package[nginx]',
       )
-      is_expected.to contain_package('nginx-common').with(
-        'ensure'  => 'present',
+      is_expected.to contain_package('nginx').with(
+        'ensure'  => '1.14.0-1~trusty',
         'notify'  => 'Class[Nginx::Restart]',
       )
-      is_expected.to contain_package('nginx').with_ensure('purged')
-      is_expected.to contain_class('govuk_ppa')
     end
   end
-  context 'version set' do
-    let (:params) { {:version => '1.4.4-precise1'} }
-    it do
-      is_expected.to contain_package('nginx-full').with_ensure('1.4.4-precise1')
-      is_expected.to contain_package('nginx-common').with_ensure('1.4.4-precise1')
-      is_expected.to contain_package('nginx').with_ensure('purged')
-    end
-  end
-  context 'nginx_package set' do
-    let (:params) { {:nginx_package => 'nginx-extras'} }
-    it do
-      is_expected.to contain_package('nginx-extras').with_ensure('present')
-      is_expected.to contain_package('nginx-common').with_ensure('present')
-      is_expected.to contain_package('nginx').with_ensure('purged')
-    end
-  end
-  context 'nginx_package and version set' do
+  context 'nginx_version and nginx_module_perl_version set' do
     let (:params) {
       {
-        :nginx_package => 'nginx-light',
-        :version       => '1.7-trusty1',
+        :nginx_version => '1.12.0-1~trusty',
+        :nginx_module_perl_version => '1.10.0-1~trusty'
       }
     }
     it do
-      is_expected.to contain_package('nginx-light').with_ensure('1.7-trusty1')
-      is_expected.to contain_package('nginx-common').with_ensure('1.7-trusty1')
-      is_expected.to contain_package('nginx').with_ensure('purged')
+      is_expected.to contain_package('nginx').with_ensure('1.12.0-1~trusty')
+      is_expected.to contain_package('nginx-module-perl').with_ensure('1.10.0-1~trusty')
     end
   end
 end
--- modules/nginx/spec/classes/nginx_spec.rb
+++ modules/nginx/spec/classes/nginx_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../../../spec_helper'
 
 describe 'nginx', :type => :class do
   it do
-    is_expected.to contain_package('nginx').with_ensure('purged')
-    is_expected.to contain_package('nginx-full').with_ensure('present')
+    is_expected.to contain_package('nginx').with_ensure('1.14.0-1~trusty')
+    is_expected.to contain_package('nginx-module-perl').with_ensure('present')
   end
 end
