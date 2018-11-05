05e7854dc5dc7c3118ab7968e9f6671b0ab3a56c
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-11-05 16:38:05 +0000

    Tatiana has left
    Part of https://trello.com/c/UYKblFuF/332-gds-leaver-tatiana-stantonian

--- modules/users/manifests/tatianastantonian.pp
+++
@@ -1,9 +0,0 @@
-# Creates the tatianastantonian user
-class users::tatianastantonian {
-  govuk_user { 'tatianastantonian':
-    ensure   => absent,
-    fullname => 'Tatiana Stantonian',
-    email    => 'tatiana.stantonian@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDTfU50j/uwwu762wZPpix2jDvwBCQodfeqJzqIZ80PtKNjgSuDRvbD5mXTGsh//FgMr0z+S10rTXpaztsUFhuFuUQ3+zY2hN4s2itQtPxvBdBwDZ4UMqsc8LY9CEzn/Ss1t1iQW9J8N//NsTm7vvj0t4OfwYClpiMjX+w1ZnDZRdVZjBnliLL1C+2aLrPuxsdeHRcOL5zoYElsTYyB5Cw2qZQrY5J2D5cqjxPSd6xx9BBJ5ip8Ls4Ik8HBJtlYoUzoNRqltexIjvsKODlsytfpPCoKWyilbXBuQsblcQLIzdNiE0z0FpK1qY69hZ/zKjQ73h9qkKYrAfM9KVgzZzH1UklN0FjMKpVG1cOqyGsczaKCHk81B90mI3i9ANcVmI3SU3F1NEcIJSubDPw1D8e8+/ZQ1NGz8ZKKJacso0UGqJXsRXzyqJWPacf6ck68nSHkpTB3DcWM9MVDpnu/Q3gJrMEZj/gQR9cMBVEX8gniOmojjRWBFqwBZfC8RlTacOSzqQRlISGIZ7S+hgpS+InrDiRPQE4shSfObJ1qesBqL1EXNnOCgFh+bJ8aawpH+UbwkC3opQrZEARyhcrzMqWSruHVLDiA7jONz2WdozFeYuvx77bLyS/iKmb18DhkzNYVWjRvydOLv8xeozLoHty4u7P41FmMAf9Vm+sZWlWQaQ== tatiana.stantonian@digital.cabinet-office.gov.uk',
-  }
-}
