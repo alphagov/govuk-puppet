2226e22173f09e6809ffe68d13cdec7ec4e17725
David Jeche <m3rl1n@out3rheaven.io>
2018-11-07 12:27:38 +0000

    Add try catch in python scripts & try fix puppet execution order to ensure python is installed first

--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -24,7 +24,7 @@ class monitoring (
 
   unless $ci_environment {
     # Monitoring server only.
-    include ::govuk_jenkins::packages::govuk_python
+    include govuk_jenkins::packages::govuk_python
     include monitoring::checks
     include monitoring::edge
     include monitoring::pagerduty_drill
