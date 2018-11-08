0600fdd58c8377ab46aa8d9160a419b7af24c58b
Edward Kerry <edwardkerry@gds7308.local>
2018-11-08 13:00:36 +0000

    Add user edwardkerry

---
+++ modules/users/manifests/edwardkerry.pp
@@ -0,0 +1,8 @@
+# Creates the user edwardkerry
+class users::edwardkerry {
+  govuk_user { 'edwardkerry':
+    fullname => 'Edward Kerry',
+    email    => 'edward.kerry@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC7jBVz4z6BrhddvmRxxaYFPndUsrusaqzQARHRWWEVCGgLgmODjr66mIYTA7refk1zkJCmpvZobG5MuUEF/O3uYkml1YmRIrE/cyAUptiJKNWPds6YsCnX+4efJFdUfVQUvUH0uXs7I6shrPz6dAJwZjFy6y2u42RFliX6HhXtI3KHAt9Xb/hrbjM29lTQiVB8lHCdM6hdesyIK5WlYzGM/33DlqtXywr8WhOOlYGhix0FgWoiWAt3WX8hRDZaa0KuRKDaWuQ9nJRAFzINb+EvYwephSrpXWO/BtCoLLTb1emJl7ehKwb8DDmcNKV9eTIlW9iL4Xf8ZxB1GncQDu/bpfuLUzmbVMp0aJJMZFPwG+4mS8VFxd1yDAC4NucjbgekVrp2KLoUzMPlG9JTQ31/h7KTH6KP0hYGpKwY0FoTMafC6n5CQv3I00Fvl+zjh2g8xZbfllVJAcweEHwCcAQNI36+zusvKW6ptepyNcJiJB6MQzHhT+nCkUNnjYkoyLtyCyG+a9muiCFDDkVDHxapnNNXJF6ikWFHLGb0WHeLvjZ894JJjAbC7xuMYDTfYoKgPKCwfuGM3LRayPNc4faYqyo97qk7D0CvrnS6AXZylNVeeeTxkAoyq4AlTe7Pj1n478Dsq4jkxY637y6FOEnmPTJ2t9XPrPxncAaPyajBvQ== edward.kerry@digital.cabinet-office.gov.uk',
+  }
+}
