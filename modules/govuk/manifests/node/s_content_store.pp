fe8f3ccec1a436b7234f5052e2fc9e696880424c
Conor Glynn <conor.glynn@digital.cabinet-office.gov.uk>
2018-11-08 10:18:51 +0000

    GovUK-Migr-Trello-1682: enable gor replay
    Enabling GOR traffic replay from carrenza content-store staging into AWS
    staging content-store. This will help with testing during our migration
    of the services.
    
    Also enabling name caching service, as this will reduce the number of
    dns lookups externally made.
    
    Pair: @ronocg @fredericfran-gds

--- modules/govuk/manifests/node/s_content_store.pp
+++ modules/govuk/manifests/node/s_content_store.pp
@@ -1,10 +1,10 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::node::s_content_store inherits govuk::node::s_base {
   include govuk::node::s_app_server
-
   include govuk_aws_xray_daemon
-
   include nginx
+  include nscd
+  include router::gor
 
   # If we miss all the apps, throw a 500 to be caught by the cache nginx
   nginx::config::vhost::default { 'default': }
