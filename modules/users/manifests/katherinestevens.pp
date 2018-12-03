d8ef9d5f496313da823696b07dd7972d36abb4ab
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-12-03 10:53:05 +0000

    Add username and public key for katherinestevens

---
+++ modules/users/manifests/katherinestevens.pp
@@ -0,0 +1,7 @@
+# Creates the katherinestevens user
+class users::katherinestevens{ govuk_user { 'katherinestevens':
+    fullname => 'Katherine Stevens',
+    email    => 'katherine.stevens@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCWZ3rhrDvNp6FgDn3CnJAxKHtsQQckorU0I8qHNvgEZxqOqVaR/qjVwXwhX4MCcxu5xWP2J3wpTsET4mmKwz/HUqXrQZrTwX7vLjVSMP8uDmrdvkPfTlEk+zQtRndBqfEldRYAWsG7tlC2I5VWeFQD1coqEqykR9PxclDT56pMUTW+U1/T1F7ozaagDsBpa5tXo0w8bmF5nnQ5KDtqlXdKM0HMFi+mpk+34T2LF29awUngq8lltxYRhscI0CKbehhHRE7JL/OERkpVxmCMCM7QtPk9Wkw9alZTRR1SCe/GLXcJ2EhuvshEQDKrvdrq4ApmSlzjBEtU+CAFP3QVPM1bK7rUDf996hGMlhRPVRzLxIKTWlDIrv5gcmzoy93UwiXnSs/xmjbkz2Yp57xR+xhK+Bdg2ZJxTtHgTgVcu3GFM9Fzz0ClREXziwHZwMd+wuh/ZOartv2gBDZgq+4p4sZv2hIpo/iA8MTWYwQSKjs0M3Ym6E/ptpcOSOZtsLpEaVsHAi49lp079lRvIxryGx4QWws87/KYgOVtIqw8SUJn9MMdpWar2T4G9ZOb8MeBgwz2/2CqhQIdHv/ILJlyVJSznAf/QXAipDtMev5UKtUi2Ny5A63T4rO4KLFVE31FAeIhSxwT02h7yOVppwt9qg3G8YGTVw/ce0ECEWv86vCFNw== katherine.stevens@digital.cabinet-office.gov.uk',
+  }
+}
