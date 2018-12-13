6de842816544e64b2795072ad796b59b19275cc4
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2018-12-13 11:58:09 +0000

    add production hiera and include alert in content_store

--- modules/govuk/manifests/node/s_content_store.pp
+++ modules/govuk/manifests/node/s_content_store.pp
@@ -12,5 +12,6 @@ class govuk::node::s_content_store inherits govuk::node::s_base {
   if ($::aws_environment == 'staging') or ($::aws_environment == 'production') {
     include ::hosts::default
     include ::hosts::backend_migration
+    include icinga::client::check_pings
   }
 }
