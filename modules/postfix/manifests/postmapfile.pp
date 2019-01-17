dbf83518cb984b092d6c3fdee57bd7f4ff320347
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-01-17 17:22:41 +0000

    Tidy whitespace in postfix::postmapfile

--- modules/postfix/manifests/postmapfile.pp
+++ modules/postfix/manifests/postmapfile.pp
@@ -1,17 +1,19 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 define postfix::postmapfile() {
-        exec { "postmap_${title}":
-                command     => "/usr/sbin/postmap /etc/postfix/${title}",
-                refreshonly => true,
-                require     => [
-                            File["/etc/postfix/${title}"],
-                            Package['postfix']],
-        }
-        file { "/etc/postfix/${title}":
-                ensure  => present,
-                mode    => '0644',
-                content => template("postfix/etc/postfix/${title}.erb"),
-                notify  => Exec["postmap_${title}"],
-        }
+  exec { "postmap_${title}":
+    command     => "/usr/sbin/postmap /etc/postfix/${title}",
+    refreshonly => true,
+    require     => [
+      File["/etc/postfix/${title}"],
+      Package['postfix']
+    ],
+  }
+
+  file { "/etc/postfix/${title}":
+    ensure  => present,
+    mode    => '0644',
+    content => template("postfix/etc/postfix/${title}.erb"),
+    notify  => Exec["postmap_${title}"],
+  }
 }
 
