fa94d7ff244ae18c082288bf2eee2b49fa3e680e
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-01-17 17:25:58 +0000

    Refactor postfix::postmapfile - use variable for filename

--- modules/postfix/manifests/postmapfile.pp
+++ modules/postfix/manifests/postmapfile.pp
@@ -1,12 +1,14 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 define postfix::postmapfile() {
-  file { "/etc/postfix/${title}":
+  $filename = "/etc/postfix/${title}"
+
+  file { $filename:
     ensure  => present,
     mode    => '0644',
     content => template("postfix/etc/postfix/${title}.erb"),
   }
   ~> exec { "postmap_${title}":
-    command     => "/usr/sbin/postmap /etc/postfix/${title}",
+    command     => "/usr/sbin/postmap ${filename}",
     refreshonly => true,
     require     => Package['postfix'],
   }
