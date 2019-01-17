a91dde61fd0ffedbb9fc755025c9a3e5912997fc
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-01-17 17:24:48 +0000

    Reorder resources and use chaining arrows in postfix::postmapfile

--- modules/postfix/manifests/postmapfile.pp
+++ modules/postfix/manifests/postmapfile.pp
@@ -1,19 +1,14 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 define postfix::postmapfile() {
-  exec { "postmap_${title}":
-    command     => "/usr/sbin/postmap /etc/postfix/${title}",
-    refreshonly => true,
-    require     => [
-      File["/etc/postfix/${title}"],
-      Package['postfix']
-    ],
-  }
-
   file { "/etc/postfix/${title}":
     ensure  => present,
     mode    => '0644',
     content => template("postfix/etc/postfix/${title}.erb"),
-    notify  => Exec["postmap_${title}"],
+  }
+  ~> exec { "postmap_${title}":
+    command     => "/usr/sbin/postmap /etc/postfix/${title}",
+    refreshonly => true,
+    require     => Package['postfix'],
   }
 }
 
