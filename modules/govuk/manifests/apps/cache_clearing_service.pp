4b905096a67b0dbf7741e91065ec84182a89d6d6
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2019-01-21 12:42:48 +0000

    Remove output piping from cache clearing service command
    This will break kibana logging but will fix an issue where child processes are not killed if the foreman process is killed outside of the upstart commands.

--- modules/govuk/manifests/apps/cache_clearing_service.pp
+++ modules/govuk/manifests/apps/cache_clearing_service.pp
@@ -60,7 +60,7 @@ class govuk::apps::cache_clearing_service (
     app_type               => 'bare',
     enable_nginx_vhost     => false,
     sentry_dsn             => $sentry_dsn,
-    command                => './bin/cache_clearing_service',
+    command                => 'bundle exec foreman start',
     collectd_process_regex => 'cache-clearing-service/.*rake message_queue:consumer',
     nagios_memory_warning  => $nagios_memory_warning,
     nagios_memory_critical => $nagios_memory_critical,
