06dea0304e198f6bbead2100ee9ca0668cb86bb1
Roch Trinque <roch.trinque@digital.cabinet-office.gov.uk>
2019-01-29 17:32:39 +0000

    Remove the generation of the default nginx conf for staging AWS
      We generate the default nginx conf for content-store in another
    part of the code. This is because we want the ELB on amazon not
    to think that a node is up when just nginx is running and not the
    app.

--- modules/govuk/manifests/node/s_content_store.pp
+++ modules/govuk/manifests/node/s_content_store.pp
@@ -6,9 +6,11 @@ class govuk::node::s_content_store inherits govuk::node::s_base {
   include nscd
   include router::gor
 
-  # If we miss all the apps, throw a 500 to be caught by the cache nginx
-  nginx::config::vhost::default { 'default': }
 
+  if ($::aws_environment != 'staging') {
+    # If we miss all the apps, throw a 500 to be caught by the cache nginx
+    nginx::config::vhost::default { 'default': }
+  }
   if ($::aws_environment == 'staging') or ($::aws_environment == 'production') {
     include ::hosts::default
     include ::hosts::backend_migration
