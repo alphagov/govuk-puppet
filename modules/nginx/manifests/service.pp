530303203fabfb0210ac92fc1993d36c2c7e3115
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2019-01-30 16:02:04 +0000

    make sure nginx can be restarted/started even if PID was not stored in file

--- modules/nginx/manifests/service.pp
+++ modules/nginx/manifests/service.pp
@@ -3,6 +3,7 @@ class nginx::service {
 
   service { 'nginx':
     ensure    => running,
+    start     => '/etc/init.d/nginx start || { nginx -s quit; /etc/init.d/nginx start;}',
     hasstatus => true,
     restart   => '/etc/init.d/nginx configtest && /etc/init.d/nginx reload',
   }
