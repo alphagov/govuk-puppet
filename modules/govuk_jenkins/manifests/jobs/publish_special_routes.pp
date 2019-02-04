1d04552f536b91af1e4f0d344fb88f0d0731a62e
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2019-02-04 15:09:43 +0000

    Add GOVUK_APP_DOMAIN env var for publish special routes
    This commit adds the `GOVUK_APP_DOMAIN` environment variable for the “Publish special routes” Jenkins job, so Plek can find the Publishing API correctly.

--- modules/govuk_jenkins/manifests/jobs/publish_special_routes.pp
+++ modules/govuk_jenkins/manifests/jobs/publish_special_routes.pp
@@ -5,6 +5,12 @@
 class govuk_jenkins::jobs::publish_special_routes(
   $publishing_api_bearer_token = undef,
 ) {
+  if $::aws_migration {
+    $app_domain = hiera('app_domain_internal')
+  } else {
+    $app_domain = hiera('app_domain')
+  }
+
   file { '/etc/jenkins_jobs/jobs/publish_special_routes.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/publish_special_routes.yaml.erb'),
