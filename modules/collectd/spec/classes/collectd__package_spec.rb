ac83bf8444e64e8e8a73228843385eeeb3ac25a4
Kevin Dew <kevindew@me.com>
2019-02-11 19:33:50 +0000

    Collect file descriptors per process for Collectd
    This allows us to see how many files a particular process has open over
    time.

--- modules/collectd/spec/classes/collectd__package_spec.rb
+++ modules/collectd/spec/classes/collectd__package_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../../../spec_helper'
 
 describe 'collectd::package', :type => :class do
-  it { is_expected.to contain_package('collectd-core').with_ensure('5.4.0-3ubuntu2') }
+  it { is_expected.to contain_package('collectd-core').with_ensure('5.8.0.145.gca1cb27-1~trusty') }
 
   it { is_expected.to contain_package('libyajl2') }
 
