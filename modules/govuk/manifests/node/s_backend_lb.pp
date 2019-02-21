2b58e5f84ccdb2d2f2dd5c09f26586af6c1e7036
Sebastian Schmieschek <sebastian@schmie.net>
2019-02-21 16:41:48 +0000

    Add additional nginx config to route asset traffic
    - Mid-migration we serve assets both from Carrenza and AWS at the same
    time.
    
    - For this we introduce an assets-carrenza and draft-assets-carrenza
    proxy configuration in use on the backend LB
    
    solo: @schmie

--- modules/govuk/manifests/node/s_backend_lb.pp
+++ modules/govuk/manifests/node/s_backend_lb.pp
@@ -42,6 +42,13 @@ class govuk::node::s_backend_lb (
   $search_servers = [],
   $ckan_backend_servers = [],
   $maintenance_mode = false,
+  $app_specific_static_asset_routes = {},
+  $asset_manager_uploaded_assets_routes = [],
+  $whitehall_uploaded_assets_routes = [],
+  $assets_carrenza_real_ip_header = '',
+  $assets_carrenza_vhost_aliases = [],
+  $assets_carrenza_vhost_name = 'assets-carrenza',
+  $draft_assets_carrenza_vhost_name = 'draft-assets-carrenza',
 ){
   include govuk::node::s_base
   include loadbalancer
@@ -162,5 +169,56 @@ class govuk::node::s_backend_lb (
   nginx::config::vhost::proxy { "content-performance-api.${app_domain}" :
     to        => ['alphagov.github.io'],
     protected => false,
+  }
+
+  if ! $::aws_migration {
+    # Custom vhost to proxy assets-origin to asset-manager and whitehall in Carrenza
+    validate_array($assets_carrenza_vhost_aliases)
+
+    $enable_ssl = hiera('nginx_enable_ssl', true)
+
+    $upstream_ssl = $enable_ssl
+
+    # suspect we want `protected => false` here
+    # once appropriate firewalling is in place?
+    nginx::config::site { $assets_carrenza_vhost_name:
+      content => template('govuk/node/s_backend_lb/assets-carrenza.conf.erb'),
+    }
+
+    nginx::config::ssl { $assets_carrenza_vhost_name:
+      certtype => 'wildcard_publishing',
+    }
+
+    nginx::log {
+      "${vhost_name}-json.event.access.log":
+        json          => true,
+        logstream     => present,
+        statsd_metric => "${::fqdn_metrics}.nginx_logs.assets-carrenza.http_%{status}",
+        statsd_timers => [{metric => "${::fqdn_metrics}.nginx_logs.assets-carrenza.time_request",
+                            value => 'request_time'}];
+      "${vhost_name}-error.log":
+        logstream => present;
+    }
+
+    $graphite_429_target = "transformNull(stats.${::fqdn_metrics}.nginx_logs.assets-carrenza.http_429,0)"
+
+    @@icinga::check::graphite { "check_nginx_429_assets_on_${::hostname}":
+      target              => $graphite_429_target,
+      warning             => 3,
+      critical            => 5,
+      from                => '5minutes',
+      desc                => '429 rate for assets-carrenza [in office hours]',
+      host_name           => $::fqdn,
+      notes_url           => monitoring_docs_url(nginx-429-too-many-requests),
+      notification_period => 'inoffice',
+    }
+    # Custom vhost to proxy draft-assets-origin to asset-manager and whitehall in Carrenza
+    nginx::config::site { $draft_assets_carrenza_vhost_name:
+      content => template('govuk/node/s_backend_lb/draft-assets-carrenza.conf.erb'),
+    }
+
+    nginx::config::ssl { $draft_assets_carrenza_vhost_name:
+      certtype => 'wildcard_publishing',
+    }
   }
 }
