6a7bdd8262b394e25242a44d78d40cf7b9e104ed
Michael Walker <mike@barrucadu.co.uk>
2019-02-27 12:39:42 +0000

    Abstract target application in search index checks job

--- modules/govuk_jenkins/manifests/jobs/search_index_checks.pp
+++ modules/govuk_jenkins/manifests/jobs/search_index_checks.pp
@@ -3,6 +3,9 @@
 # Monitor the GOV.UK search indexes and report data to statsd
 #
 class govuk_jenkins::jobs::search_index_checks {
+  $job_name = 'search_index_checks'
+  $job_display_name = 'Search index checks'
+  $target_application = 'rummager'
 
   file { '/etc/jenkins_jobs/jobs/search_index_checks.yaml':
     ensure  => present,
