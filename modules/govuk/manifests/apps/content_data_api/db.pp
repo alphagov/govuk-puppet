136cafde41f6d46916264a26811423ff1c08a3ef
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-03-04 13:34:07 +0000

    Remove the backend_ip_range Content Data API configuration
    This configuration is only going to be used in the context of AWS, and
    this parameter only applies in non-AWS environments.

--- modules/govuk/manifests/apps/content_data_api/db.pp
+++ modules/govuk/manifests/apps/content_data_api/db.pp
@@ -5,19 +5,14 @@
 # [*password*]
 #   The DB instance password.
 #
-# [*backend_ip_range*]
-#   Backend IP addresses to allow access to the database.
-#
 class govuk::apps::content_data_api::db (
   $password,
-  $backend_ip_range = undef,
   $rds = false,
 ) {
   govuk_postgresql::db { 'content_performance_manager_production':
     user                    => 'content_performance_manager',
     password                => $password,
     allow_auth_from_backend => true,
-    backend_ip_range        => $backend_ip_range,
     rds                     => $rds,
     extensions              => ['plpgsql'],
     enable_in_pgbouncer     => false,
