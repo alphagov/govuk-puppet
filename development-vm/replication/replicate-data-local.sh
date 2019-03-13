5a331e5130e614d2d3ab5e01cd37506e026dc704
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2019-03-13 09:29:08 +0000

    Shellcheck correction

--- development-vm/replication/replicate-data-local.sh
+++ development-vm/replication/replicate-data-local.sh
@@ -1,24 +1,23 @@
-#!/usr/bin/env bash
-#
+# !/usr/bin/env bash
 # Fetch the most recent database dump files from production and restore locally.
 #
 
 set -eu
 
-. $(dirname $0)/common-args.sh
-. $(dirname $0)/aws.sh
+. "$(dirname "$0")"/common-args.sh
+. "$(dirname "$0")"/aws.sh
 
 status "Running bundle install"
 bundle install --quiet
 
-$(dirname $0)/sync-aws-mysql.sh "$@" mysql-master
+"$(dirname "$0")"/sync-aws-mysql.sh "$@" mysql-master
 
-$(dirname $0)/sync-aws-mongo.sh "$@" mongo
-$(dirname $0)/sync-aws-mongo.sh "$@" router_backend
+"$(dirname "$0")"/sync-aws-mongo.sh "$@" mongo
+"$(dirname "$0")"/sync-aws-mongo.sh "$@" router_backend
 
-$(dirname $0)/sync-aws-postgresql.sh "$@" postgresql-primary-1
+"$(dirname "$0")"/sync-aws-postgresql.sh "$@" postgresql-primary-1
 
-$(dirname $0)/sync-aws-elasticsearch.sh "$@"
+"$(dirname "$0")"/sync-aws-elasticsearch.sh "$@"
 
 if ! ($SKIP_MONGO || $DRY_RUN); then
   status "Munging router backend hostnames for dev VM"
@@ -29,14 +28,14 @@ fi
 if ignored "mapit"; then
   status "Skipping mapit"
 else
-  $(dirname $0)/sync-mapit.sh
+  "$(dirname "$0")"/sync-mapit.sh
 fi
 
 
 if ! $DRY_RUN; then
   status "Munging Signon db tokens for dev VM"
-  if [[ -d $(dirname $0)/../../../signon ]]; then
-    cd $(dirname $0)/../../../signon && bundle install && bundle exec ruby script/make_oauth_work_in_dev
+  if [[ -d "$(dirname "$0")"/../../../signon ]]; then
+    cd "$(dirname "$0")"/../../../signon && bundle install && bundle exec ruby script/make_oauth_work_in_dev
   fi
 fi
 
