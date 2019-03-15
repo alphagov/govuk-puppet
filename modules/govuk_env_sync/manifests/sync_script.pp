f1398e65fdf8fb6d4fa48289ac8c9d86a8ae327f
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-03-15 14:11:23 +0000

    Add sudoers config to the sync script class
    The sync script runs as the govuk-backup user, and uses sudo to run
    several commands. This configuration gives it permission to run those
    commands through sudo.

--- modules/govuk_env_sync/manifests/sync_script.pp
+++ modules/govuk_env_sync/manifests/sync_script.pp
@@ -3,6 +3,11 @@
 # Install the sync script
 #
 class govuk_env_sync::sync_script {
+  sudo::conf {
+    'govuk-env-sync-commands':
+      ensure  => 'present',
+      content => 'govuk-backup ALL=NOPASSWD:/usr/bin/psql,/usr/bin/dropdb,/usr/bin/dropuser,/usr/bin/pg_restore,/usr/bin/mysql,/usr/bin/mysqldump,/usr/bin/pg_dump';
+  }
 
   # sync script
   file { '/usr/local/bin/govuk_env_sync.sh':
