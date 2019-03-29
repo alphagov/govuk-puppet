4eb42877cd450a1b4ce8cd80757e06592ca67111
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-03-29 15:34:37 +0000

    Restore access to the postgres user for the govuk_env_sync.sh script
    The sudoers config was changed, but access to the postgres user was
    missed, so restore access so commands like pg_dump can be run.

--- modules/govuk_env_sync/manifests/sync_script.pp
+++ modules/govuk_env_sync/manifests/sync_script.pp
@@ -6,7 +6,7 @@ class govuk_env_sync::sync_script {
   sudo::conf {
     'govuk-env-sync-commands':
       ensure  => 'present',
-      content => 'govuk-backup ALL=NOPASSWD:/usr/bin/psql,/usr/bin/createdb,/usr/bin/dropdb,/usr/bin/dropuser,/usr/bin/pg_restore,/usr/bin/mysql,/usr/bin/mysqldump,/usr/bin/pg_dump';
+      content => 'govuk-backup ALL=(root,postgres) NOPASSWD:/usr/bin/psql,/usr/bin/createdb,/usr/bin/dropdb,/usr/bin/dropuser,/usr/bin/pg_restore,/usr/bin/mysql,/usr/bin/mysqldump,/usr/bin/pg_dump';
   }
 
   # sync script
