c0795335809738e472c0778fbafe26df8764a03c
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-04-12 05:23:33 +0100

    Support configuring the local tcpconns monitoring in govuk::app

--- modules/govuk/manifests/app.pp
+++ modules/govuk/manifests/app.pp
@@ -254,6 +254,16 @@
 #   which runs unicornherder.
 #   Default: true if app_type is 'rack' else false
 #
+# [*local_tcpconns_established_warning*]
+#   Warning value to use for the Icinga check on the number of
+#   established TCP connections to $port.
+#   Default: undef
+#
+# [*local_tcpconns_established_critical*]
+#   Critical value to use for the Icinga check on the number of
+#   established TCP connections to $port.
+#   Default: undef
+#
 define govuk::app (
   $app_type,
   $port = 0,
@@ -294,6 +304,8 @@ define govuk::app (
   $override_search_location = undef,
   $create_default_nginx_config = false,
   $monitor_unicornherder = undef,
+  $local_tcpconns_established_warning = undef,
+  $local_tcpconns_established_critical = undef,
 ) {
 
   if ! ($app_type in ['procfile', 'rack', 'bare']) {
@@ -335,44 +347,46 @@ define govuk::app (
   }
 
   govuk::app::config { $title:
-    ensure                           => $ensure,
-    require                          => Govuk::App::Package[$title],
-    app_type                         => $app_type,
-    command                          => $command,
-    create_pidfile                   => $create_pidfile,
-    domain                           => $app_domain,
-    port                             => $port,
-    vhost_aliases                    => $vhost_aliases,
-    vhost_full                       => $vhost_full,
-    vhost_protected                  => $vhost_protected,
-    vhost_ssl_only                   => $vhost_ssl_only,
-    nginx_extra_config               => $nginx_extra_config,
-    health_check_path                => $health_check_path,
-    expose_health_check              => $expose_health_check,
-    json_health_check                => $json_health_check,
-    health_check_service_template    => $health_check_service_template,
-    health_check_notification_period => $health_check_notification_period,
-    deny_framing                     => $deny_framing,
-    enable_nginx_vhost               => $enable_nginx_vhost,
-    nagios_memory_warning            => $nagios_memory_warning,
-    nagios_memory_critical           => $nagios_memory_critical,
-    alert_5xx_warning_rate           => $alert_5xx_warning_rate,
-    alert_5xx_critical_rate          => $alert_5xx_critical_rate,
-    unicorn_herder_timeout           => $unicorn_herder_timeout,
-    asset_pipeline                   => $asset_pipeline,
-    asset_pipeline_prefix            => $asset_pipeline_prefix,
-    depends_on_nfs                   => $depends_on_nfs,
-    read_timeout                     => $read_timeout,
-    sentry_dsn                       => $sentry_dsn,
-    proxy_http_version_1_1_enabled   => $proxy_http_version_1_1_enabled,
-    unicorn_worker_processes         => $unicorn_worker_processes,
-    cpu_warning                      => $cpu_warning,
-    cpu_critical                     => $cpu_critical,
-    collectd_process_regex           => $collectd_process_regex,
-    alert_when_threads_exceed        => $alert_when_threads_exceed,
-    override_search_location         => $override_search_location,
-    create_default_nginx_config      => $create_default_nginx_config,
-    monitor_unicornherder            => $monitor_unicornherder,
+    ensure                              => $ensure,
+    require                             => Govuk::App::Package[$title],
+    app_type                            => $app_type,
+    command                             => $command,
+    create_pidfile                      => $create_pidfile,
+    domain                              => $app_domain,
+    port                                => $port,
+    vhost_aliases                       => $vhost_aliases,
+    vhost_full                          => $vhost_full,
+    vhost_protected                     => $vhost_protected,
+    vhost_ssl_only                      => $vhost_ssl_only,
+    nginx_extra_config                  => $nginx_extra_config,
+    health_check_path                   => $health_check_path,
+    expose_health_check                 => $expose_health_check,
+    json_health_check                   => $json_health_check,
+    health_check_service_template       => $health_check_service_template,
+    health_check_notification_period    => $health_check_notification_period,
+    deny_framing                        => $deny_framing,
+    enable_nginx_vhost                  => $enable_nginx_vhost,
+    nagios_memory_warning               => $nagios_memory_warning,
+    nagios_memory_critical              => $nagios_memory_critical,
+    alert_5xx_warning_rate              => $alert_5xx_warning_rate,
+    alert_5xx_critical_rate             => $alert_5xx_critical_rate,
+    unicorn_herder_timeout              => $unicorn_herder_timeout,
+    asset_pipeline                      => $asset_pipeline,
+    asset_pipeline_prefix               => $asset_pipeline_prefix,
+    depends_on_nfs                      => $depends_on_nfs,
+    read_timeout                        => $read_timeout,
+    sentry_dsn                          => $sentry_dsn,
+    proxy_http_version_1_1_enabled      => $proxy_http_version_1_1_enabled,
+    unicorn_worker_processes            => $unicorn_worker_processes,
+    cpu_warning                         => $cpu_warning,
+    cpu_critical                        => $cpu_critical,
+    collectd_process_regex              => $collectd_process_regex,
+    alert_when_threads_exceed           => $alert_when_threads_exceed,
+    override_search_location            => $override_search_location,
+    create_default_nginx_config         => $create_default_nginx_config,
+    monitor_unicornherder               => $monitor_unicornherder,
+    local_tcpconns_established_warning  => $local_tcpconns_established_warning,
+    local_tcpconns_established_critical => $local_tcpconns_established_critical,
   }
 
   govuk::app::service { $title:
