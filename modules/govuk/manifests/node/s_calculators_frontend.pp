205f5d9f8b2fcb627b25e1cccaf69fdc346fa0ff
Michael Walker <mike@barrucadu.co.uk>
2019-04-18 12:42:52 +0100

    Remove node-specific seach plek overrides
    There are app-specific overrides in plac so this logic isn't doing
    anything.

--- modules/govuk/manifests/node/s_calculators_frontend.pp
+++ modules/govuk/manifests/node/s_calculators_frontend.pp
@@ -37,8 +37,6 @@ class govuk::node::s_calculators_frontend inherits govuk::node::s_base {
     $app_domain = hiera('app_domain')
 
     govuk_envvar {
-      'PLEK_SERVICE_SEARCH_URI': value    => "https://search.${app_domain}";
-      'PLEK_SERVICE_RUMMAGER_URI': value  => "https://rummager.${app_domain}";
       'PLEK_SERVICE_EMAIL_ALERT_API_URI': value  => "https://email-alert-api.${app_domain}";
       'PLEK_SERVICE_PUBLISHING_API_URI': value  => "https://publishing-api.${app_domain}";
       'PLEK_SERVICE_WHITEHALL_ADMIN_URI': value  => "https://whitehall-admin.${app_domain}";
