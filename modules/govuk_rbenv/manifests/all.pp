14289d855ccdd97442792e5b503649adf7d9c608
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-04-23 14:47:29 +0100

    Install Ruby 2.6.3
    This follows on from 2c4d5ae4e3e2440cc8888acae8fc66bfed3ad5ef as we've
    discovered that a newer version of Ruby has already been released.

--- modules/govuk_rbenv/manifests/all.pp
+++ modules/govuk_rbenv/manifests/all.pp
@@ -27,7 +27,7 @@ class govuk_rbenv::all (
     '2.5.1',
     '2.5.3',
     '2.6.1',
-    '2.6.2',
+    '2.6.3',
   ]
 
   govuk_rbenv::install_ruby_version { $ruby_versions:
@@ -42,6 +42,6 @@ class govuk_rbenv::all (
     to_version => '2.5.3',
   }
   rbenv::alias { '2.6':
-    to_version => '2.6.2',
+    to_version => '2.6.3',
   }
 }
