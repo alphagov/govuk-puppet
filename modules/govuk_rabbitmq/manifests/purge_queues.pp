3cee2d2198015f87b05a9c41b019e5394bd59ed3
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-05-07 10:22:10 +0100

    Remove the content_performance_manager queue purging cron task
    As the queues should no longer exist.

--- modules/govuk_rabbitmq/manifests/purge_queues.pp
+++ modules/govuk_rabbitmq/manifests/purge_queues.pp
@@ -4,10 +4,4 @@
 #
 
 class govuk_rabbitmq::purge_queues {
-  cron::crondotdee { 'purge_content_performance_manager':
-    command => '/usr/sbin/rabbitmqctl purge_queue content_performance_manager > /dev/null 2>&1',
-    hour    => '*',
-    minute  => '0',
-    user    => 'root',
-  }
 }
