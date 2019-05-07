c453f8995a9135df29304cca0a8a659fe05090bc
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-05-07 10:34:52 +0100

    Stop purging queues for Rummager
    Rummager has been renamed to the Search API, and the queues have also
    been renamed, so remove this now redundant configuration.

--- modules/govuk_rabbitmq/manifests/purge_queues.pp
+++ modules/govuk_rabbitmq/manifests/purge_queues.pp
@@ -16,22 +16,10 @@ class govuk_rabbitmq::purge_queues {
     minute  => '3',
     user    => 'root',
   }
-  cron::crondotdee { 'purge_rummager_govuk_index':
-    ensure  => 'absent',
-    command => undef,
-    hour    => undef,
-    minute  => undef,
-  }
   cron::crondotdee { 'purge_search_api_to_be_indexed':
     command => '/usr/sbin/rabbitmqctl purge_queue search_api_to_be_indexed > /dev/null 2>&1',
     hour    => '*',
     minute  => '4',
     user    => 'root',
-  }
-  cron::crondotdee { 'purge_rummager_to_be_indexed':
-    ensure  => 'absent',
-    command => undef,
-    hour    => undef,
-    minute  => undef,
   }
 }
