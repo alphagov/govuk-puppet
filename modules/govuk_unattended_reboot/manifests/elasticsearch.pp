9aaee118232c68ee51d43d67cf6b9aa2cb7ac8dc
Michael Walker <mike@barrucadu.co.uk>
2019-05-07 13:21:14 +0100

    Ensure absent govuk_unattended_reboot::elasticsearch

--- modules/govuk_unattended_reboot/manifests/elasticsearch.pp
+++ modules/govuk_unattended_reboot/manifests/elasticsearch.pp
@@ -3,26 +3,14 @@
 # Installs a script which ensures that an elasticsearch server
 # can be rebooted.
 #
-# === Parameters
-#
-# [*enabled*]
-#   Whether to enable the check for unattended reboots.
-#
 class govuk_unattended_reboot::elasticsearch (
-  $enabled = false
 ) {
 
   $config_directory = '/etc/unattended-reboot'
   $check_scripts_directory = "${config_directory}/check"
 
-  if ($enabled) {
-    $file_ensure = present
-  } else {
-    $file_ensure = absent
-  }
-
   file { "${check_scripts_directory}/02_elasticsearch":
-    ensure  => $file_ensure,
+    ensure  => 'absent',
     mode    => '0755',
     owner   => 'root',
     group   => 'root',
