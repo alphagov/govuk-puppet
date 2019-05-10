6b04c56d4371ae399cf80b8ce039c35e337b15cd
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2019-05-10 17:54:48 +0100

    Update service.pp

--- modules/collectd/manifests/service.pp
+++ modules/collectd/manifests/service.pp
@@ -5,6 +5,6 @@
 class collectd::service {
   service { 'collectd':
     ensure  => running,
-    restart => "/etc/init.d/collectd restart || { ps aux | grep collectd | awk '{{print $$2}}' | xargs kill -9 ; /etc/init.d/collectd start; }",
+    restart => "/etc/init.d/collectd restart || { ps aux | grep collectd | awk '{{print \$2}}' | xargs kill -9 ; /etc/init.d/collectd start; }",
   }
 }
