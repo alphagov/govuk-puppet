dd1ff696376615da3fa6b2a057e1289c96fbd6d1
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-05-21 11:46:29 +0100

    Update to the 2.1.1 release of icinga-slack-webbook
    This includes improved output and display for the Slack messages. The
    Slack Icinga contact configuration is updated accordingly.
    
    The $HOSTNAME$ as well as the $HOSTDISPLAYNAME$ is passed to the
    icinga_slack_webhook_notify script, so that it can construct the URL
    to the extinfo.cgi page for a service alert.

--- modules/icinga/manifests/config/slack.pp
+++ modules/icinga/manifests/config/slack.pp
@@ -1,7 +1,7 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class icinga::config::slack {
   package {'icinga-slack-webhook':
-    ensure   => '2.0.0',
+    ensure   => '2.1.1',
     provider => pip3,
   }
 }
