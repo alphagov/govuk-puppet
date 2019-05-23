39c18ca12d5c529fe0cf32cd83ae4f9ac78119e1
Ana Fernandez <ana.fernandez@digital.cabinet-office.gov.uk>
2019-05-23 12:02:28 +0100

    Update and fix configuration for LB Nginx health checks
    Fix 1f6eec5
    
    The upstream proxy does not exist in certain cases, for instance, contacts-admin
    is being configured as contacts-proxy. We need to pass the correct variable.
    
    Add missing `;`

--- modules/govuk/manifests/app/config.pp
+++ modules/govuk/manifests/app/config.pp
@@ -299,7 +299,7 @@ define govuk::app::config (
     if defined(Concat['/etc/nginx/lb_healthchecks.conf']) and $health_check_path != 'NOTSET' {
       concat::fragment { "${title}_lb_healthcheck":
         target  => '/etc/nginx/lb_healthchecks.conf',
-        content => "  location /_healthcheck_${title} {\n    proxy_pass http://${title}-proxy${health_check_path};\n  }\n",
+        content => "location /_healthcheck_${vhost_full} {\n  proxy_pass http://${vhost_full}-proxy${health_check_path};\n}\n",
       }
     }
   }
