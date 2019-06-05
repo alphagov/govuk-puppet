346d271952d95276fbaa9c0ccb68ab668cf2964e
Michael Walker <mike@barrucadu.co.uk>
2019-06-05 12:27:32 +0100

    Install ES "primary" and ES "secondary" in dev VM and CI

--- modules/govuk/manifests/node/s_development.pp
+++ modules/govuk/manifests/node/s_development.pp
@@ -81,6 +81,7 @@ class govuk::node::s_development (
 
   include ::govuk_docker
   include ::govuk_containers::elasticsearch::primary
+  include ::govuk_containers::elasticsearch::secondary
 
   include nginx
 
