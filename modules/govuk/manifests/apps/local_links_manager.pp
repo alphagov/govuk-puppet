d66954b8a05e1d6e4bc8f6dbed97486b0c3e07b4
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-06-06 17:32:18 +0100

    Replace the $enabled parameter with $ensure for Local Links Manager
    A large if statement just removes Puppet from managing these
    resources, and in some cases this will result in them being removed,
    but not for all the things managed through govuk::app.
    
    Instead, use $ensure, and pass this through to each resource so that
    it's managed correctly.

--- modules/govuk/manifests/apps/local_links_manager.pp
+++ modules/govuk/manifests/apps/local_links_manager.pp
@@ -4,12 +4,11 @@
 # they can be used for local transactions.
 # https://github.com/alphagov/local-links-manager
 #
+# [*ensure*]
+#   Whether Local Links Manager should be present or absent.
+#
 # [*port*]
 #   What port should the app run on?
-#
-# [*enabled*]
-#   Should the app exist?
-#   Default: true
 #
 # [*sentry_dsn*]
 #   The URL used by Sentry to report exceptions
@@ -100,8 +99,8 @@
 #   Default: undef
 #
 class govuk::apps::local_links_manager(
+  $ensure = 'present',
   $port = 3121,
-  $enabled = true,
   $sentry_dsn = undef,
   $secret_key_base = undef,
   $oauth_id = undef,
@@ -130,110 +129,112 @@ class govuk::apps::local_links_manager(
 ) {
   $app_name = 'local-links-manager'
 
-  if $enabled {
-    govuk::app { $app_name:
-      app_type                 => 'rack',
-      log_format_is_json       => true,
-      port                     => $port,
-      sentry_dsn               => $sentry_dsn,
-      vhost_ssl_only           => true,
-      health_check_path        => '/healthcheck',
-      unicorn_worker_processes =>  $unicorn_worker_processes,
-    }
+  govuk::app { $app_name:
+    ensure                   => $ensure,
+    app_type                 => 'rack',
+    log_format_is_json       => true,
+    port                     => $port,
+    sentry_dsn               => $sentry_dsn,
+    vhost_ssl_only           => true,
+    health_check_path        => '/healthcheck',
+    unicorn_worker_processes =>  $unicorn_worker_processes,
+  }
 
-    Govuk::App::Envvar {
-      app    => $app_name,
-    }
+  Govuk::App::Envvar {
+    ensure => $ensure,
+    app    => $app_name,
+  }
 
-    govuk::app::envvar::redis { $app_name:
-      host => $redis_host,
-      port => $redis_port,
-    }
+  govuk::app::envvar::redis { $app_name:
+    host => $redis_host,
+    port => $redis_port,
+  }
 
+  govuk::app::envvar {
+    "${title}-GOOGLE_ANALYTICS_GOVUK_VIEW_ID":
+      varname => 'GOOGLE_ANALYTICS_GOVUK_VIEW_ID',
+      value   => $google_analytics_govuk_view_id;
+    "${title}-GOOGLE_CLIENT_EMAIL":
+      varname => 'GOOGLE_CLIENT_EMAIL',
+      value   => $google_client_email;
+    "${title}-GOOGLE_PRIVATE_KEY":
+      varname => 'GOOGLE_PRIVATE_KEY',
+      value   => $google_private_key;
+    "${title}-GOOGLE_EXPORT_ACCOUNT_ID":
+      varname => 'GOOGLE_EXPORT_ACCOUNT_ID',
+      value   => $google_export_account_id;
+    "${title}-GOOGLE_EXPORT_CUSTOM_DATA_IMPORT_SOURCE_ID":
+      varname => 'GOOGLE_EXPORT_CUSTOM_DATA_IMPORT_SOURCE_ID',
+      value   => $google_export_custom_data_import_source_id;
+    "${title}-GOOGLE_EXPORT_TRACKER_ID":
+      varname => 'GOOGLE_EXPORT_TRACKER_ID',
+      value   => $google_export_tracker_id;
+    "${title}-SECRET_KEY_BASE":
+      varname => 'SECRET_KEY_BASE',
+      value   => $secret_key_base;
+    "${title}-OAUTH_ID":
+      varname => 'OAUTH_ID',
+      value   => $oauth_id;
+    "${title}-OAUTH_SECRET":
+      varname => 'OAUTH_SECRET',
+      value   => $oauth_secret;
+    "${title}-PUBLISHING_API_BEARER_TOKEN":
+      varname => 'PUBLISHING_API_BEARER_TOKEN',
+      value   => $publishing_api_bearer_token;
+    "${title}-LINK_CHECKER_API_SECRET_TOKEN":
+      varname => 'LINK_CHECKER_API_SECRET_TOKEN',
+      value   => $link_checker_api_secret_token;
+    "${title}-LINK_CHECKER_API_BEARER_TOKEN":
+      varname => 'LINK_CHECKER_API_BEARER_TOKEN',
+      value   => $link_checker_api_bearer_token;
+    "${title}-RUN_LINK_GA_EXPORT":
+      varname => 'RUN_LINK_GA_EXPORT',
+      value   => bool2str($run_links_ga_export);
+  }
+
+  if $app_domain {
     govuk::app::envvar {
-      "${title}-GOOGLE_ANALYTICS_GOVUK_VIEW_ID":
-        varname => 'GOOGLE_ANALYTICS_GOVUK_VIEW_ID',
-        value   => $google_analytics_govuk_view_id;
-      "${title}-GOOGLE_CLIENT_EMAIL":
-        varname => 'GOOGLE_CLIENT_EMAIL',
-        value   => $google_client_email;
-      "${title}-GOOGLE_PRIVATE_KEY":
-        varname => 'GOOGLE_PRIVATE_KEY',
-        value   => $google_private_key;
-      "${title}-GOOGLE_EXPORT_ACCOUNT_ID":
-        varname => 'GOOGLE_EXPORT_ACCOUNT_ID',
-        value   => $google_export_account_id;
-      "${title}-GOOGLE_EXPORT_CUSTOM_DATA_IMPORT_SOURCE_ID":
-        varname => 'GOOGLE_EXPORT_CUSTOM_DATA_IMPORT_SOURCE_ID',
-        value   => $google_export_custom_data_import_source_id;
-      "${title}-GOOGLE_EXPORT_TRACKER_ID":
-        varname => 'GOOGLE_EXPORT_TRACKER_ID',
-        value   => $google_export_tracker_id;
-      "${title}-SECRET_KEY_BASE":
-        varname => 'SECRET_KEY_BASE',
-        value   => $secret_key_base;
-      "${title}-OAUTH_ID":
-        varname => 'OAUTH_ID',
-        value   => $oauth_id;
-      "${title}-OAUTH_SECRET":
-        varname => 'OAUTH_SECRET',
-        value   => $oauth_secret;
-      "${title}-PUBLISHING_API_BEARER_TOKEN":
-        varname => 'PUBLISHING_API_BEARER_TOKEN',
-        value   => $publishing_api_bearer_token;
-      "${title}-LINK_CHECKER_API_SECRET_TOKEN":
-        varname => 'LINK_CHECKER_API_SECRET_TOKEN',
-        value   => $link_checker_api_secret_token;
-      "${title}-LINK_CHECKER_API_BEARER_TOKEN":
-          varname => 'LINK_CHECKER_API_BEARER_TOKEN',
-          value   => $link_checker_api_bearer_token;
-      "${title}-RUN_LINK_GA_EXPORT":
-        varname => 'RUN_LINK_GA_EXPORT',
-        value   => bool2str($run_links_ga_export);
-    }
-
-    if $app_domain {
-        govuk::app::envvar {
-        "${title}-GOVUK_APP_DOMAIN":
+      "${title}-GOVUK_APP_DOMAIN":
         varname => 'GOVUK_APP_DOMAIN',
         value   => $app_domain;
-        "${title}-GOVUK_APP_DOMAIN_EXTERNAL":
+      "${title}-GOVUK_APP_DOMAIN_EXTERNAL":
         varname => 'GOVUK_APP_DOMAIN_EXTERNAL',
         value   => $app_domain;
-      }
+    }
+  }
+
+  if $local_links_manager_passive_checks {
+    @@icinga::passive_check { "link-checker-${::hostname}":
+      ensure              => $ensure,
+      service_description => 'Local Links Manager link checker rake task',
+      host_name           => $::fqdn,
+      freshness_threshold => 26 * (60 * 60),
     }
 
-    if $local_links_manager_passive_checks {
-      @@icinga::passive_check { "link-checker-${::hostname}":
-        service_description => 'Local Links Manager link checker rake task',
-        host_name           => $::fqdn,
-        freshness_threshold => 26 * (60 * 60),
-      }
-
-      @@icinga::passive_check { "local-links-manager-import-service-interactions_${::hostname}":
-        service_description => 'Import services and interactions into local-links-manager',
-        host_name           => $::fqdn,
-        freshness_threshold => (32 * 24 * 60 * 60), #the job runs monthly on the 1st
-      }
-
-      @@icinga::passive_check { "local-links-manager-export-links_${::hostname}":
-        service_description => 'Export links to CSV from local-links-manager',
-        host_name           => $::fqdn,
-        freshness_threshold => (25 * 60 * 60), # 25 hrs (e.g. just over a day)
-      }
+    @@icinga::passive_check { "local-links-manager-import-service-interactions_${::hostname}":
+      ensure              => $ensure,
+      service_description => 'Import services and interactions into local-links-manager',
+      host_name           => $::fqdn,
+      freshness_threshold => (32 * 24 * 60 * 60), #the job runs monthly on the 1st
     }
 
-    if $::govuk_node_class !~ /^development$/ {
-      govuk::app::envvar::database_url { $app_name:
-        type                      => 'postgresql',
-        username                  => $db_username,
-        password                  => $db_password,
-        host                      => $db_hostname,
-        port                      => $db_port,
-        allow_prepared_statements => $db_allow_prepared_statements,
-        database                  => $db_name,
-      }
+    @@icinga::passive_check { "local-links-manager-export-links_${::hostname}":
+      ensure              => $ensure,
+      service_description => 'Export links to CSV from local-links-manager',
+      host_name           => $::fqdn,
+      freshness_threshold => (25 * 60 * 60), # 25 hrs (e.g. just over a day)
     }
+  }
 
+  if $::govuk_node_class !~ /^development$/ {
+    govuk::app::envvar::database_url { $app_name:
+      type                      => 'postgresql',
+      username                  => $db_username,
+      password                  => $db_password,
+      host                      => $db_hostname,
+      port                      => $db_port,
+      allow_prepared_statements => $db_allow_prepared_statements,
+      database                  => $db_name,
+    }
   }
 }
