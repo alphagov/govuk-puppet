73fc57369bac9d57596574c4d37114fe5b036b76
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-06-07 10:25:45 +0100

    Increase memory monitoring thresholds for licensify_admin
    The CollectD process plugin has only just been configured correctly,
    so this is something that's only now appearing.
    
    Levels seem to be around the 600 MB - 800 MB mark for the past two days.
    Think it's sensible to set a warning at 900 MB and a critical warning
    at 1000 MB. We can revisit these later if things change.

--- modules/licensify/manifests/apps/licensify_admin.pp
+++ modules/licensify/manifests/apps/licensify_admin.pp
@@ -17,6 +17,8 @@ class licensify::apps::licensify_admin(
     proxy_http_version_1_1_enabled => true,
     log_format_is_json             => true,
     collectd_process_regex         => 'java -Duser.dir=\/data\/vhost\/licensify-admin\..*publishing\.service\.gov\.uk\/licensify-admin-.*',
+    nagios_memory_warning          => 900,
+    nagios_memory_critical         => 1000,
   }
 
   licensify::apps::envvars { 'licensify-admin':
