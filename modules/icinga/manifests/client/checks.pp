0c10a2b735df29500804b1296c2414bafdfcd71d
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-06-12 11:14:03 +0100

    Fix the machine high load check linking to Grafana
    Previously, it would link to the Carrenza side Grafana instance from
    the AWS side Icinga instance. This change should get Icinga to link to
    the relevant Grafana instance.

--- modules/icinga/manifests/client/checks.pp
+++ modules/icinga/manifests/client/checks.pp
@@ -102,12 +102,18 @@ class icinga::client::checks (
     host_name           => $::fqdn,
   }
 
-  $app_domain = hiera('app_domain')
+  if $::aws_migration or ($::aws_environment == 'integration'){
+    $grafana = "grafana.${::aws_environment}.govuk.digital"
+  } else {
+    $app_domain = hiera('app_domain')
+    $grafana = "grafana.${app_domain}"
+  }
+
   @@icinga::check { "check_load_${::hostname}":
     check_command       => 'check_nrpe_1arg!check_load',
     service_description => 'high load on',
     host_name           => $::fqdn,
-    action_url          => "https://grafana.${app_domain}/dashboard/file/machine.json?refresh=1m&orgId=1&var-hostname=${::fqdn_metrics}",
+    action_url          => "https://${grafana}/dashboard/file/machine.json?refresh=1m&orgId=1&var-hostname=${::fqdn_metrics}",
   }
 
   if $::aws_migration {
