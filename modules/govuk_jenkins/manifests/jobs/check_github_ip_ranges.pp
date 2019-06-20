6cfd55be03b6989dcba2689acb13dbe870f6dc1b
Tijmen Brommet <tijmen@gmail.com>
2019-06-20 10:43:49 +0100

    Raise Icinga alerts for GitHub IP range check

--- modules/govuk_jenkins/manifests/jobs/check_github_ip_ranges.pp
+++ modules/govuk_jenkins/manifests/jobs/check_github_ip_ranges.pp
@@ -9,4 +9,15 @@ class govuk_jenkins::jobs::check_github_ip_ranges {
     content => template('govuk_jenkins/jobs/check_github_ip_ranges.yaml.erb'),
     notify  => Exec['jenkins_jobs_update'],
   }
+
+  $check_name = 'check_github_ip_ranges'
+  $service_description = 'Compare the IP ranges that GitHub publishes against the ranges configured in govuk-provisioning'
+  $job_url = "https://deploy.${app_domain}/job/Check_GitHub_IP_Ranges/"
+
+  @@icinga::passive_check { "${check_name}_${::hostname}":
+    service_description => $service_description,
+    host_name           => $::fqdn,
+    freshness_threshold => 104400,
+    action_url          => $job_url,
+  }
 }
