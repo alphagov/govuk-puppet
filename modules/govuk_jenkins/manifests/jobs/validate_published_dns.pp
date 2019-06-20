41308d4265afbf0556aadb4eebee803017b5bb31
Tijmen Brommet <tijmen@gmail.com>
2019-06-20 11:14:07 +0100

    Icinga alert for DNS validation job

--- modules/govuk_jenkins/manifests/jobs/validate_published_dns.pp
+++ modules/govuk_jenkins/manifests/jobs/validate_published_dns.pp
@@ -14,4 +14,15 @@ class govuk_jenkins::jobs::validate_published_dns (
     content => template('govuk_jenkins/jobs/validate_published_dns.yaml.erb'),
     notify  => Exec['jenkins_jobs_update'],
   }
+
+  $check_name = 'validate_published_dns'
+  $service_description = 'Check that the published DNS records match those in the govuk-dns-config repo'
+  $job_url = "https://deploy.${app_domain}/job/Validate_published_DNS/"
+
+  @@icinga::passive_check { "${check_name}_${::hostname}":
+    service_description => $service_description,
+    host_name           => $::fqdn,
+    freshness_threshold => 104400,
+    action_url          => $job_url,
+  }
 }
