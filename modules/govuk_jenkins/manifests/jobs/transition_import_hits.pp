dcf43ca7dc9ea461cf0e141ab72abeaa639ab95a
Kevin Dew <kevindew@me.com>
2019-07-01 11:25:26 +0100

    Set transition s3 bucket via parameter
    This was previously hardcoded to the production bucket as this task was
    only run in the production environment.

--- modules/govuk_jenkins/manifests/jobs/transition_import_hits.pp
+++ modules/govuk_jenkins/manifests/jobs/transition_import_hits.pp
@@ -2,7 +2,14 @@
 #
 # Create a file on disk that can be parsed by jenkins-job-builder
 #
-class govuk_jenkins::jobs::transition_import_hits {
+# === Parameters
+#
+# [*s3_bucket*]
+#   The S3 bucket to pull hits from
+#
+class govuk_jenkins::jobs::transition_import_hits(
+  $s3_bucket = '',
+) {
   file { '/etc/jenkins_jobs/jobs/transition_import_hits.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/transition_import_hits.yaml.erb'),
