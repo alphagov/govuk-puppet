0c00d072ac760ab9efb801aa6513b4b2cc525697
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-07-30 08:40:07 +0100

    Stop sending Smokey failures to Slack
    Now that we have error reporting in Icinga, it doesn't make as much
    sense to send the failures to Slack. It can be quite disruptive to see
    lots of Smokey failures in the channel while conversations are
    happening.

--- modules/govuk_jenkins/manifests/jobs/smokey.pp
+++ modules/govuk_jenkins/manifests/jobs/smokey.pp
@@ -4,22 +4,14 @@
 #
 # === Parameters
 #
-# [*enable_slack_notifications*]
-#   Whether slack should be notified about this job
-#
 # [*environment*]
 #   The environment which is running Smokey. Used to build the rake task.
 #
 class govuk_jenkins::jobs::smokey (
-  $enable_slack_notifications = false,
   $environment = 'production',
 ) {
   $smokey_task = "test:${environment}"
   $app_domain = hiera('app_domain')
-
-  $slack_team_domain = 'gds'
-  $slack_room = 'govuk-2ndline'
-  $slack_build_server_url = "https://deploy.${app_domain}/"
 
   include govuk::apps::smokey
 
