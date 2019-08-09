3faa5131b9ca9aea9334dfc2133b32927f8978f0
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2019-08-09 12:38:03 +0100

    Add csw endpoint to ckan publisher stack

--- modules/govuk/manifests/apps/ckan/cronjobs.pp
+++ modules/govuk/manifests/apps/ckan/cronjobs.pp
@@ -1,14 +1,21 @@
 # == Class: govuk::apps::ckan::cronjobs
 #
+# [*ckan_port*]
+#   The port CKAN is running on.
+#
+# [*pycsw_config*]
+#   The location of the PyCSW configuration file.
+#
 # [*enable_solr_reindex*]
 #   Enable automatic Solr reindexing, this is useful to run after the data
 #   sync.
 #   Default: false
 #
 class govuk::apps::ckan::cronjobs(
+  $ckan_port,
+  $pycsw_config,
   $enable_solr_reindex = false,
 ) {
-
   govuk::apps::ckan::paster_cronjob { 'harvester run':
     paster_command => 'harvester run',
     plugin         => 'ckanext-harvest',
@@ -34,4 +41,11 @@ class govuk::apps::ckan::cronjobs(
     minute         => '0',
   }
 
+  govuk::apps::ckan::paster_cronjob { 'pycsw load':
+    paster_command => "ckan-pycsw load -p ${pycsw_config} -u http://localhost:${ckan_port}",
+    plugin         => 'ckanext-spatial',
+    hour           => '6',
+    minute         => '0',
+    ckan_ini       => undef,
+  }
 }
