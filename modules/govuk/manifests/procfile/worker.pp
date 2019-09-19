58f18ba697f8715d05bca22f67ff49028c701cb0
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-09-19 10:03:23 +0100

    Disable the procfile worker memory check
    The intent of adding the check was to get Icinga to restart the
    service when lots of memory was being used, therefore avoiding failing
    checks. However, the event handler doesn't currently work, as the
    service sitting above the procfile workers doesn't support the reload
    action.
    
    Therefore, disable this check until the event handler can be fixed.

--- modules/govuk/manifests/procfile/worker.pp
+++ modules/govuk/manifests/procfile/worker.pp
@@ -120,6 +120,7 @@ define govuk::procfile::worker (
       $memory_critical_threshold_bytes = $memory_critical_threshold * $si_megabyte
 
       @@icinga::check::graphite { "check_${title_underscore}_app_worker_mem_usage${::hostname}":
+        ensure                     => absent, # TODO The event handler isn't working properly yet
         target                     => "${::fqdn_metrics}.processes-app-worker-${title_underscore}.ps_rss",
         warning                    => $memory_warning_threshold_bytes,
         critical                   => $memory_critical_threshold_bytes,
