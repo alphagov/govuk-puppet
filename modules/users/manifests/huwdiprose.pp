4b7ed3a1d94acb06ae925925b45331afe5b19eb0
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2019-09-27 12:30:11 +0100

    Add: Huw Diprose to integration
    Huw was following docs here: https://docs.publishing.service.gov.uk/manual/get-ssh-access.html

---
+++ modules/users/manifests/huwdiprose.pp
@@ -0,0 +1,10 @@
+# Creates the user
+class users::huwdiprose {
+  govuk_user { 'huwdiprose':
+    fullname => 'Huw Diprose',
+    email    => 'huw.diprose@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCUoWiQkPHE2ptM6J9dOr8MugIFN0A0cOXqgJ+8SQL+6S5sGOQ86Qnn3kxPQCkwzmQuzC1sTnZSEDmrFRGz354CufnYkYsf4i4dAB43Fk+jnowClM/q3c8QhZSllGjx3LANz6vhjahiJplWsuZtHwbGx/tHzJk7dTXuZ/H6AJ1fuULzQtC2WW6n/zN/E6Knr7lxbeOfbhTmL2UNRz+DaGrsxbPLXNwEoo7MrZwoSWeYE2czdZED7fN4yViMurOZapNdckuML6NTHEoJOjQJwKhtt+JaKWhz6eXiNWXHjb3QIZiDRyjCiwx5ZgZ2up2HhftZapsFJoOVDIt7RdY+E6KSM2Iix3RCQu1NEd6Gv8FSilX0XM9/fK5UNHrgHiLdIRmEE/qColQwgIuAwhxirKptzjCoo4mw74yf3fG5pLQg8xWkUFoMu6kyQVNfprvwr0YR412CcIhmhAoBvqhTT9aDwgbI0D37DV5eh6E41OKfQqRt2W3GsgCtDhrf+RVIKliuEJckn9fdGoqpCWPmGiGjtbSGTYvc1ev7EbvdnPsfVf0NaTnqWIYsuPLHcFyHsDOp6rod+QdSQhT/6kAh+LtqAhBHXIWgfifijKYOgcxE4tGzJeo/xJ2f5ffSufewcPf7paXYd15Z9JAwpxYMSG64tM1rQNZGF1wWEYbcG8a9Vw== huw.diprose@digital.cabinet-office.gov.uk',
+  }
+}
+
+
