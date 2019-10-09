92f17d61e8021e8d4fae25069fe8003a891af344
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2019-10-09 13:16:21 +0100

    Add Duncan to integration and CI

---
+++ modules/users/manifests/duncangarmonsway.pp
@@ -0,0 +1,7 @@
+class users::duncangarmonsway {
+  govuk_user { 'duncangarmonsway':
+    fullname => 'Duncan Garmonsway',
+    email    => 'duncan.garmonsway@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC77d30YoXpAluaF5hYEPiMQvs7FCa4i2HZoAzKnpKVABYtKioAqrQ5zWg6oB77WBidKU1+0igNIlAzR/zVo9ld8YwDjex78hSql9jsVBdITKuArU7K4WdjcMIhmZwNyNZzMpgQN3PFgETOj2JIF/bGebTmAGHx7W1/nSwWz/BZO665O2cYG1eCr6XCB4Cl9aao77V5INQzSDkP51yBMBPWXXFRcTI7X8nyyim68Fs7VSh8rLHUSqgprvAq+1Lae10Lb050c+J7Gxg8GDjKwWD9WAET6JYcjLYbIpeLSROZfwF6uob8CLF8ojoNTyTJk7vX/o/tIGIWlED6/W0l1WUjnGcuArgd14v6cHekWA9dWWZGVIp+SkiGi2qsu2eO+Q8du92QH1aneKGyJewRWf3f4XB86W/p4DpRmAYnrhlEis3i0rbBxdwNGNHyF4blsPG5iy3b5tcs3y/zlyxcldWpZUeois2Z4SLTEY3YXts96JyHI0Gd0Efn0ekuNSm/1KR5zUrakgSDixayP9g4HH5kxsOiq1Q3E1Pc7ouinjcv88/ogzUw5XDjfWB62sEW9Zw7Ec7cTHLY8IK7Wrd1Wkaj0Gyuq7d+LTEcT5SwzYIls/z8CxwDfEKGm3G6EjNFTttbrVDaEGeEDAgeEYlt3gOEQvIvlj6etoG9xLwVgpvAgQ== duncan.garmonsway@digital.cabinet-office.gov.uk',
+  }
+}
