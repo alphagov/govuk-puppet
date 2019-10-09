0bf3649a07737ee0a1d709520911dfdb1aa09af8
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-10-09 10:32:48 +0100

    Remove the link-checker-api from the PostgreSQL base class
    As on AWS, the database is managed through the db_admin node class.

--- modules/govuk/manifests/node/s_postgresql_base.pp
+++ modules/govuk/manifests/node/s_postgresql_base.pp
@@ -11,7 +11,6 @@ class govuk::node::s_postgresql_base inherits govuk::node::s_base {
   include govuk::apps::content_publisher::db
   include govuk::apps::content_tagger::db
   include govuk::apps::email_alert_api::db
-  include govuk::apps::link_checker_api::db
   include govuk::apps::local_links_manager::db
   include govuk::apps::publishing_api::db
   include govuk::apps::service_manual_publisher::db
