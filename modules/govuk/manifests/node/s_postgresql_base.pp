77a143357a3fa888ea9ad6dcfd263249db00cefd
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-10-11 10:39:53 +0100

    Remove support_api::db from s_postgresql_base
    As it's not running in AWS, and the database is managed through the
    db_admin machine.

--- modules/govuk/manifests/node/s_postgresql_base.pp
+++ modules/govuk/manifests/node/s_postgresql_base.pp
@@ -13,7 +13,6 @@ class govuk::node::s_postgresql_base inherits govuk::node::s_base {
   include govuk::apps::email_alert_api::db
   include govuk::apps::publishing_api::db
   include govuk::apps::service_manual_publisher::db
-  include govuk::apps::support_api::db
 
   include govuk_postgresql::tuning
 
