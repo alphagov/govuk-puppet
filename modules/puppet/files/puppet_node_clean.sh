8467584f9b8b75d3c81554cf22ab6623c8f76260
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-10-11 11:45:19 +0100

    Double quote variables in puppet_node_clean.sh to prevent splitting
    As suggested by shellcheck.

--- modules/puppet/files/puppet_node_clean.sh
+++ modules/puppet/files/puppet_node_clean.sh
@@ -19,9 +19,9 @@ print $1}
 ' <(aws ec2 describe-instances --filters "Name=tag:aws_stackname,Values=${STACKNAME}" "Name=instance-state-name,Values=pending,running,shutting-down,stopping,stopped" --output=json | jq -r '.Reservations[] | .Instances[] | .InstanceId ') <(curl 'http://localhost:8080/v3/nodes' 2>/dev/null | grep name | awk '{print $3}' | tr -d '",') | while read NODE
 do
   echo "[$(date '+%H:%M:%S %d-%m-%Y')] Preparing to delete: ${NODE}:"
-  curl http://localhost:8080/v3/nodes/${NODE}/facts/aws_migration
+  curl "http://localhost:8080/v3/nodes/${NODE}/facts/aws_migration"
   echo
-  puppet node clean ${NODE}
-  puppet node deactivate ${NODE}
+  puppet node clean "${NODE}"
+  puppet node deactivate "${NODE}"
 done
 
