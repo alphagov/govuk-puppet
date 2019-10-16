99b72fe27b66ef15d0ea3c69c6f89573509af411
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-10-16 13:22:41 +0100

    Add ensure parameter to check_feature
    This allows it to be disabled and enabled as necessary.

--- modules/icinga/manifests/check_feature_w_prio.pp
+++ modules/icinga/manifests/check_feature_w_prio.pp
@@ -16,9 +16,11 @@
 define icinga::check_feature_w_prio (
   $feature,
   $prio,
+  $ensure = present,
   $notes_url = undef,
 ) {
   icinga::check { "check_feature_${feature}_${prio}_checker":
+    ensure              => $ensure,
     check_command       => "run_smokey_tests!${feature}!${prio}",
     use                 => "govuk_${prio}_priority",
     service_description => "Run ${feature} ${prio} priority tests",
