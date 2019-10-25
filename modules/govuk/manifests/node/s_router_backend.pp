b1892df5fc0c8f5914d0e76756b739f806a766b4
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-10-25 10:03:17 +0100

    Remove unnecessary conditional from the router_backend node
    As these nodes are just present in AWS now.

--- modules/govuk/manifests/node/s_router_backend.pp
+++ modules/govuk/manifests/node/s_router_backend.pp
@@ -11,10 +11,4 @@ class govuk::node::s_router_backend inherits govuk::node::s_base {
 
   # If we miss all the apps, throw a 500 to be caught by the cache nginx
   nginx::config::vhost::default { 'default': }
-
-  if ! $::aws_migration {
-    Govuk_mount['/var/lib/mongodb'] -> Class['mongodb::server']
-    Govuk_mount['/var/lib/automongodbbackup'] -> Class['mongodb::backup']
-    Govuk_mount['/var/lib/s3backup'] -> Class['mongodb::backup']
-  }
 }
