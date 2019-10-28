95959a1da03e5e0f6ef74a488d814ccce6c6960a
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-10-28 08:20:37 +0000

    Update office start and end times
    We've gone back to GMT, so we need to change the times to match.
    
    See 1f9c585 for more details.

--- modules/monitoring/manifests/contacts.pp
+++ modules/monitoring/manifests/contacts.pp
@@ -50,9 +50,9 @@ class monitoring::contacts (
   validate_bool($notify_pager, $notify_slack, $notify_graphite)
 
   $midnight_day_start = '00:00'
-  $office_day_start = '08:30'
-  $midday = '11:00'
-  $office_day_end = '16:30'
+  $office_day_start = '09:30'
+  $midday = '12:00'
+  $office_day_end = '17:30'
   $midnight_day_end = '24:00'
 
   $all_day = "${midnight_day_start}-${midnight_day_end}"
