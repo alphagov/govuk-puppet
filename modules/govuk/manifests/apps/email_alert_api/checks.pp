88e740fda4b450926a48030e8e7966d46485f1f5
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2020-01-02 12:52:22 +0000

    Refactor current checks
    These parameters were used to disable the checks until the app had
    migrated to AWS. Now the migration is complete they are no longer
    needed.

--- modules/govuk/manifests/apps/email_alert_api/checks.pp
+++ modules/govuk/manifests/apps/email_alert_api/checks.pp
@@ -3,20 +3,15 @@
 # Various Icinga checks for Email Alert API.
 #
 class govuk::apps::email_alert_api::checks(
-  $internal_failure = true,
-  $technical_failure = true,
   $ensure = present,
 ) {
-  $internal_failure_ensure = $internal_failure ? { true => present, false => absent }
 
   delivery_attempt_status_check { 'internal_failure':
-    ensure => $internal_failure_ensure,
+    ensure => $ensure,
   }
 
-  $technical_failure_ensure = $technical_failure ? { true => present, false => absent }
-
   delivery_attempt_status_check { 'technical_failure':
-    ensure => $technical_failure_ensure,
+    ensure => $ensure,
   }
 
   @@icinga::check::graphite { 'email-alert-api-notify-email-send-request-success':
