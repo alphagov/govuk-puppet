b2b15c5aa331892bb0ab22711f3a154339981da5
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-04-22 11:24:22 +0100

    Add Richard Towers' SSH key to integration
    We'll think about production later, once we've tested this works.

---
+++ modules/users/manifests/richardtowers.pp
@@ -0,0 +1,8 @@
+# Creates the user richardtowers
+class users::richardtowers {
+  govuk_user { 'richardtowers':
+    fullname => 'Richard Towers',
+    email    => 'richard.towers@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXmBraPYRMsCCj0Hw1q73SW8d4hBXcoZMxdifRqVYs4/4GZqaJJbWKRaQ2eRAN6yeHgkAWdsrPx6NBbq12QGRIVpNF6C3eaIVQaU+fdOWepFi9dDbFWymPFspWC1pSwXBl/A3k4JbwyCsaUO7TfjzL4XB3CYjVkopmlvjjQTHBB8sdSm922A/czEK6DAqmDXrU5iDaiauN/dXx2qk9El6dfTyXDFXADgFXN3rMvPGY+V8UPSGUDSkB5K+SjyrtbkP+QaKvavoyHF/k+Tvib7Wp3fZgcBF9vQZ9tTQ22CBd4YJbrUSqMCVNFMpc09WDHw/o15acmUtKdfvbbowdPuyt2mUzkJsj5brfHTs22uHln5Bojs3RcnO74mO8aEIJ0WmbExM/Um4wuBrSOSAxxBsktnVAKkEfb2XtbYLEruyp9z/ycPELBf4iaES3pgbBVogvX+cq8TPSOSu7x8hM3+Cvkw7j07GhZsmTQEHERKPnHMa+LeaXkKAQOecHPUQZ6HhVDHuEI9agQ0GVufsUs3asOcwIZwXSLvXGymbsgnhogpxU9Cy5CXZtsPfZWSbq5n+OuRDKjT19Eks/8fSenvSuIaDkKZbCuhEwIutvTO3rZmzm/7vFnGA0YTkJX5qKNDgvtPDJJkn2MuEwyRbSCX2CH66WOEfkCE4MJqDthjERBw== richard.towers@digital.cabinet-office.gov.uk',
+  }
+}
