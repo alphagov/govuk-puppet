0eb2251270d5a9005de824bad8f7d270d2a2c597
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-04-29 14:37:41 +0100

    Make 'integration-app-deploy' job generic
    Previously we had an 'integration-app-deploy' job that was triggered by
    a successful build of the master branch for each app on CI. This renames
    the job to 'Deploy_App_Downstream' and extracts it so that it can be run
    in other environments, as part of our move to Continuous Deployment.
    
    In order for this job to run safely in other environments, this adds
    prepends a new step to ensure that the commit being deployed really is
    the latest release of the master branch for the repo in question. Due to
    historical issues with the naming of some repos, we need to do a lookup.
    
    Note that, although the job is configured with all apps in the Carrenza
    environment, it will only be provisioned in the CI environment for that
    provider. For all other environments, it will only be provisioned in
    AWS, since most apps are now deployed in AWS.

---
+++ modules/govuk_jenkins/manifests/jobs/deploy_app_downstream.pp
@@ -0,0 +1,30 @@
+# == Class: govuk_jenkins::jobs::deploy_app_downstream
+#
+# Creates jobs that enables kicking off deploys in a downstream environment.
+#
+# === Parameters
+#
+# [*applications*]
+#   Array of applications that are available to kick off downstream deploys.
+#
+# [*jenkins_downstream_api_user*]
+#   The username used to authenticate with the downstream deployment Jenkins.
+#
+# [*jenkins_downstream_api_password*]
+#   The password used to authenticate with the downstream deployment Jenkins.
+#
+# [*deploy_url*]
+#   The URL of the downstream Jenkins.
+#
+class govuk_jenkins::jobs::deploy_app_downstream (
+  $applications = undef,
+  $jenkins_downstream_api_user = undef,
+  $jenkins_downstream_api_password = undef,
+  $deploy_url = undef,
+) {
+  file { '/etc/jenkins_jobs/jobs/deploy_app_downstream.yaml':
+    ensure  => present,
+    content => template('govuk_jenkins/jobs/deploy_app_downstream.yaml.erb'),
+    notify  => Exec['jenkins_jobs_update'],
+  }
+}
