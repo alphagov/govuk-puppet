31832a79d59b70e4bf05478b0c2e471ff4fc86b2
Michael Walker <mike@barrucadu.co.uk>
2020-05-05 09:43:26 +0100

    Retire calendars (step 1)
    After this has been deployed and run, tearing down the app on all the
    machines, the remaining config can be removed.

--- modules/govuk/manifests/apps/calendars.pp
+++ modules/govuk/manifests/apps/calendars.pp
@@ -29,6 +29,7 @@ class govuk::apps::calendars(
   $app_name = 'calendars'
 
   govuk::app { $app_name:
+    ensure                => 'absent',
     app_type              => 'rack',
     port                  => $port,
     sentry_dsn            => $sentry_dsn,
@@ -36,23 +37,5 @@ class govuk::apps::calendars(
     log_format_is_json    => true,
     asset_pipeline        => true,
     asset_pipeline_prefix => 'calendars',
-  }
-
-  Govuk::App::Envvar {
-    app => $app_name,
-  }
-
-  govuk::app::envvar {
-    "${title}-PUBLISHING_API_BEARER_TOKEN":
-        varname => 'PUBLISHING_API_BEARER_TOKEN',
-        value   => $publishing_api_bearer_token;
-  }
-
-  if $secret_key_base {
-    govuk::app::envvar {
-      "${title}-SECRET_KEY_BASE":
-        varname => 'SECRET_KEY_BASE',
-        value   => $secret_key_base;
-    }
   }
 }
