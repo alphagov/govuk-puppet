e2cf17c501ebc01b83654d15893941fb1402b10d
Kevin Dew <kevindew@me.com>
2020-06-16 22:02:54 +0100

    Remove Whitehall's custom ASSET_ROOT env var
    Whitehall no longer makes use of this of this environment variable so
    this can be removed. It's rather confusing that Whitehall customises
    this as I interpret this environment variable to be the asset root for
    all of GOV.UK not a particular app.
    
    Anyway once this is cleared out I plan to change Whitehall to use the
    regular GOVUK_ASSET_ROOT env var as part of an idea that we can kill off
    the need for both GOVUK_ASSET_HOST and GOVUK_ASSET_ROOT.

--- modules/govuk/manifests/apps/whitehall.pp
+++ modules/govuk/manifests/apps/whitehall.pp
@@ -348,17 +348,10 @@ class govuk::apps::whitehall(
       memory_critical_threshold => 14000,
     }
 
-    # NOTE. The GOVUK_ASSET_ROOT environment variable uses a protocol relative
-    # URL so assets in admin are on the same domain but work in production and
-    # development. (This is needed for IE8)
     govuk::app::envvar {
       "${title}-ASSET_MANAGER_BEARER_TOKEN":
         varname => 'ASSET_MANAGER_BEARER_TOKEN',
         value   => $asset_manager_bearer_token;
-      "${title}-GOVUK_ASSET_ROOT":
-        app     => $app_name,
-        varname => 'GOVUK_ASSET_ROOT',
-        value   => "//whitehall-admin.${app_domain}";
       "${title}-HIGHLIGHT_WORDS_TO_AVOID":
         varname => 'HIGHLIGHT_WORDS_TO_AVOID',
         value   => bool2str($highlight_words_to_avoid);
