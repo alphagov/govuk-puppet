8195aa1483bc3030204e840b0b4f8a3cecab4d93
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2020-06-22 12:00:03 +0100

    add s3 sync to govuk_env_sync
    This will allow govuk_env_sync to do s3 syncing in addition to
    database ones.
    
    This work is done in the context of deprecating the old
    Jenkins env sync job.
    
    The content publisher s3 sync times were set to match the old
    Jenkins job

--- modules/govuk_env_sync/manifests/init.pp
+++ modules/govuk_env_sync/manifests/init.pp
@@ -3,6 +3,7 @@
 #
 class govuk_env_sync(
   $tasks = {},
+  $s3_sync_tasks = {},
   $user = 'govuk-backup',
   $conf_dir = '/etc/govuk_env_sync',
   $aws_region = 'eu-west-1',
@@ -24,6 +25,7 @@ class govuk_env_sync(
   }
 
   create_resources(govuk_env_sync::task, $tasks)
+  create_resources(govuk_env_sync::s3_sync_task, $s3_sync_tasks)
 
   if $document_db_credentials {
     create_resources(govuk_env_sync::documentdb_auth, $document_db_credentials)
