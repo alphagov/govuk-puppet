d31044431af9b797c8c660b3e9d0b1a25a5b284f
Robert Scott <github@humanleg.org.uk>
2020-07-03 13:54:03 +0100

    Revert " govuk_solr: add "remove" mode which will ensure a previous installation is removed"

--- modules/govuk_solr/manifests/install.pp
+++ modules/govuk_solr/manifests/install.pp
@@ -13,45 +13,24 @@
 class govuk_solr::install (
   $version  = '4.3.1',
   $base_url = 'https://archive.apache.org/dist/lucene/solr/',
-  $remove = undef,
 ) {
 
-  $ensure_user = $remove ? {
-    false => present,
-    true  => absent,
-  }
-
-  $ensure_dir = $remove ? {
-    false => directory,
-    true  => absent,
-  }
-
-  $ensure_archive = $remove ? {
-    false => present,
-    true  => absent,
-  }
-
-  $ensure_link = $remove ? {
-    false => link,
-    true  => absent,
-  }
-
   user { 'solr':
-    ensure     => $ensure_user,
+    ensure     => present,
     home       => '/var/lib/solr',
     managehome => true,
     shell      => '/bin/bash',
   }
 
   file { '/var/lib/solr':
-    ensure  => $ensure_dir,
+    ensure  => 'directory',
     owner   => 'solr',
     group   => 'solr',
     require => User['solr'],
   }
 
   archive { 'solr':
-    ensure       => $ensure_archive,
+    ensure       => present,
     path         => '/tmp/solr.tgz',
     extract      => true,
     source       => "${base_url}${version}/solr-${version}.tgz",
@@ -64,7 +43,7 @@ class govuk_solr::install (
   }
 
   file { '/var/lib/solr/current':
-    ensure  => $ensure_link,
+    ensure  => 'link',
     target  => "/var/lib/solr/solr-${version}",
     require => Archive['solr'],
   }
