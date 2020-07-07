c978b83ddb0a84784709a5ef26d6faa788f3be85
Kevin Dew <kevindew@me.com>
2020-07-07 11:12:39 +0100

    Revert "Add redirect for Leicestershire lockdown changes"
    This reverts commit 0ec4c1bf38c0b0cfab742a2a89b385f13d5b9379.
    
    This redirect was added because the draft link was tweeted and was used
    by many people. We've not had any requests for this link since June 30th
    and it no longer returns a content page (it returns a redirect to
    authenticate)

--- modules/router/manifests/nginx.pp
+++ modules/router/manifests/nginx.pp
@@ -49,9 +49,6 @@
 #
 # Default: ''
 #
-# [*website_root*]
-#   The URL to the root of the main GOV.UK host for redirects
-#
 class router::nginx (
   $app_specific_static_asset_routes = {},
   $vhost_protected,
@@ -61,7 +58,6 @@ class router::nginx (
   $check_requests_warning = '@25',
   $check_requests_critical = '@10',
   $robotstxt = '',
-  $website_root = undef,
 ) {
   validate_array($rate_limit_tokens)
 
