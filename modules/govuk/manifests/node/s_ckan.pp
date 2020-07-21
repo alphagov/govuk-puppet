18d46e92dddfa489b683fb13062afd62effe8a23
Robert Scott <code@humanleg.org.uk>
2020-07-21 16:12:48 +0100

    govuk::node::s_ckan: switch to openjdk8
    in preparation for solr 6.x

--- modules/govuk/manifests/node/s_ckan.pp
+++ modules/govuk/manifests/node/s_ckan.pp
@@ -21,7 +21,12 @@ class govuk::node::s_ckan inherits govuk::node::s_base {
   include govuk_python
   include nginx
   include postgresql::lib::devel
-  include govuk_java::openjdk7::jdk
+  include govuk_java::openjdk8::jre
+  include govuk_java::openjdk8::jdk
+  class { 'govuk_java::set_defaults':
+    jdk => 'openjdk8',
+    jre => 'openjdk8',
+  }
   include govuk_solr
 
   package { 'libgeos-c1':
