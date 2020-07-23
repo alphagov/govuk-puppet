alphagov/govuk-puppet

commit d347e922f399ce6ba617f9737c49aeeada58598c

Stop panicking about short CDN blips
Previously we alerted for a short-lived spike in CDN 5xx rates,
which is unactionable. This smoothes out the spikes to avoid alerting
when there is no action to take.

modules/monitoring/manifests/edge.pp
@@ -14,7 +14,7 @@ class monitoring::edge (
 
   if $enabled {
     icinga::check::graphite { "fastly_5xx_rate_on_${::hostname}":
-      target    => "${::fqdn_metrics}.cdn_fastly-govuk.requests-status_5xx",
+      target    => movingMedian("${::fqdn_metrics}.cdn_fastly-govuk.requests-status_5xx", '5min'),
       desc      => 'Fastly error rate for GOV.UK',
       warning   => 0.05,
       critical  => 0.1,

commit d9a626026dcb3aaafe487b4ca6e59b170687d653

Fix syntax error
Introduced in: d347e922f

modules/monitoring/manifests/edge.pp
@@ -14,7 +14,7 @@ class monitoring::edge (
 
   if $enabled {
     icinga::check::graphite { "fastly_5xx_rate_on_${::hostname}":
-      target    => movingMedian("${::fqdn_metrics}.cdn_fastly-govuk.requests-status_5xx", '5min'),
+      target    => "movingMedian(${::fqdn_metrics}.cdn_fastly-govuk.requests-status_5xx,\"5min\")",
       desc      => 'Fastly error rate for GOV.UK',
       warning   => 0.05,
       critical  => 0.1,