0bcbeea4f505bbf33012ab423b93fc399b90349c
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-07-31 14:48:31 +0100

    Remove redudant alerts
    We decided in [1] that there were some alerts that didn't offer us much
    value. These included the checks around a high sidekiq retry set size
    and the total email send requests to Notify. Both of these are purely
    informational and don't actually indicate that anything is wrong so we
    shouldn't be alerting on them.
    
    We also no longer have a daily limit on the amount of emails sent to
    Notify with a caveat that Notify itself is currently capped at
    delivering 30 million emails a day by amazon SES.
    
    [1] https://docs.google.com/document/d/1fTwyoSD7iWKJYrVk8-yhRSyYBRgZG1jePuW-LzNI74I/edit#heading=h.jvdcecid8r7s
    
    Trello:
    https://trello.com/c/TbOhx0B6/438-remove-redundant-alerts

--- modules/govuk/manifests/apps/email_alert_api/checks.pp
+++ modules/govuk/manifests/apps/email_alert_api/checks.pp
@@ -30,21 +30,6 @@ class govuk::apps::email_alert_api::checks(
       latency_critical => '5400'; # 90 minutes
   }
 
-  sidekiq_retry_size_check { 'retry_set_size':
-    retry_size_warning  => '40000',
-    retry_size_critical => '50000',
-  }
-
-  @@icinga::check::graphite { 'email-alert-api-notify-email-send-request-success':
-    host_name => $::fqdn,
-    target    => 'transformNull(sumSeries(consolidateBy(stats_counts.govuk.app.email-alert-api.*.notify.email_send_request.*, "sum")),0)',
-    args      => '--ignore-missing',
-    warning   => '20000000', # 25,000,000 * 0.8
-    critical  => '22500000', # 25,000,000 * 0.9
-    from      => '3hours',
-    desc      => 'email-alert-api - high number of email send requests',
-  }
-
   @@icinga::check::graphite { 'email-alert-api-delivery-attempt-status-update':
     ensure    => $ensure,
     host_name => $::fqdn,
