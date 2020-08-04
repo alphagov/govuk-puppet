ec4560351ff878624c9784831ff5c08da4fe4eed
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-08-04 12:59:09 +0100

    Remove checks for certain Sidekiq queues
    This commit removes our icinga checks for email-alert-api's non delivery
    related Sidekiq queues. This was decided as part of the work to improve
    our alerting system. The remaining delivery queue checks will also be
    removed in a future piece of work but can't be yet due to us lacking a
    suitable replacement.

--- modules/govuk/manifests/apps/email_alert_api/checks.pp
+++ modules/govuk/manifests/apps/email_alert_api/checks.pp
@@ -7,15 +7,6 @@ class govuk::apps::email_alert_api::checks(
 ) {
 
   sidekiq_queue_check {
-    [
-      'process_and_generate_emails',
-      'default',
-      'email_generation_digest',
-      'cleanup',
-    ]:
-      latency_warning  => '300', # 5 minutes
-      latency_critical => '600'; # 10 minutes
-
     'delivery_immediate':
       latency_warning  => '1200', # 20 minutes
       latency_critical => '1800'; # 30 minutes
