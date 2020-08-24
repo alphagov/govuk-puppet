dde7173442c60d927019efbac05261aece3d1847
Robert Scott <code@humanleg.org.uk>
2020-08-24 13:20:10 +0100

    govuk::apps::ckan::cronjobs: make it possible to disable all cronjobs
    this is useful for maintenance

--- modules/govuk/manifests/apps/ckan/cronjobs.pp
+++ modules/govuk/manifests/apps/ckan/cronjobs.pp
@@ -6,17 +6,32 @@
 # [*pycsw_config*]
 #   The location of the PyCSW configuration file.
 #
+# [*enable*]
+#   Allows disabling of all ckan cronjobs
+#   Default: true
+#
 # [*enable_solr_reindex*]
 #   Enable automatic Solr reindexing, this is useful to run after the data
-#   sync.
+#   sync. Requires $enable to be true.
 #   Default: false
 #
 class govuk::apps::ckan::cronjobs(
   $ckan_port,
   $pycsw_config,
+  $enable              = true,
   $enable_solr_reindex = false,
 ) {
+  if ($enable_solr_reindex and !$enable) {
+    fail "Can't \$enable_solr_reindex without \$enable"
+  }
+
+  $ensure = $enable ? {
+    true  => 'present',
+    false => 'absent',
+  }
+
   govuk::apps::ckan::paster_cronjob { 'harvester run':
+    ensure         => $ensure,
     paster_command => 'harvester run',
     plugin         => 'ckanext-harvest',
     minute         => '*/5',
@@ -25,6 +40,7 @@ class govuk::apps::ckan::cronjobs(
   }
 
   govuk::apps::ckan::paster_cronjob { 'harvester cleanup':
+    ensure         => $ensure,
     paster_command => 'harvester clean_harvest_log',
     plugin         => 'ckanext-harvest',
     hour           => '2',
@@ -44,6 +60,7 @@ class govuk::apps::ckan::cronjobs(
   }
 
   govuk::apps::ckan::paster_cronjob { 'pycsw load':
+    ensure         => $ensure,
     paster_command => "ckan-pycsw load -p ${pycsw_config} -u http://localhost:${ckan_port}",
     plugin         => 'ckanext-spatial',
     hour           => '6',
