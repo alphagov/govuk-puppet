1a679bb6aafaf437071cd4c035b649878a9dc809
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2020-09-15 15:09:52 +0100

    try hiera lookup again

--- modules/govuk/manifests/node/s_ci_agent.pp
+++ modules/govuk/manifests/node/s_ci_agent.pp
@@ -7,4 +7,5 @@ class govuk::node::s_ci_agent inherits govuk::node::s_base {
   notify {"hiera value of postgresql in ci agent ${::postgresql::globals::version},  aws_hostname ${::aws_hostname}, ec2_aws_hostname ${::ec2_aws_hostname}":}
 
   include ::govuk_ci::agent
+  include ::govuk_ci::agenttest
 }
---
+++ modules/govuk_ci/manifests/agent_test.pp
@@ -0,0 +1,15 @@
+# == Class: govuk_ci::agenttest
+#
+# Class to manage continuous deployment agents
+#
+# === Parameters:
+#
+# [*version*]
+#   version to print out
+#
+class govuk_ci::agenttest(
+  $version = undef,
+) {
+
+  notify {"hiera value of postgresql in ci agent ${version}"}
+}
