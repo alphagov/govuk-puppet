b4a382cd2ef2e0cd43637e1d4c6933bbcbf36a76
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-10-19 10:55:13 +0100

    Add environment variables for GOV.UK Accounts experiment
    These are the environment variables that will be used by the Transition
    Checker to communicate with the Account Manager app.

--- modules/govuk/manifests/apps/finder_frontend.pp
+++ modules/govuk/manifests/apps/finder_frontend.pp
@@ -21,6 +21,24 @@
 # [*email_alert_api_bearer_token*]
 #   Bearer token for communication with the email-alert-api
 #
+# [*account_oauth_client_id*]
+#   Client ID for the Transition Checker in GOV.UK Account Manager
+#
+# [*account_oauth_client_secret*]
+#   Client secret for the Transition Checker in GOV.UK Account Manager
+#
+# [*account_jwt_key_uuid*]
+#   An arbitrary UUID (used to identify the JWT signing key for Transition Checker
+#
+# [*account_jwt_key_pem*]
+#   Private key used to sign the JWT for Transition Checker
+#
+# [*feature_flag_accounts*]
+#   Feature flag to switch GOV.UK Accounts on or off for the Transition Checker
+#
+# [*plek_account_manager_uri*]
+#   Path to the GOV.UK Account Manager
+#
 class govuk::apps::finder_frontend(
   $port,
   $enabled = false,
@@ -30,6 +48,12 @@ class govuk::apps::finder_frontend(
   $secret_key_base = undef,
   $email_alert_api_bearer_token = undef,
   $unicorn_worker_processes = undef,
+  $account_oauth_client_id = undef,
+  $account_oauth_client_secret = undef,
+  $account_jwt_key_uuid = undef,
+  $account_jwt_key_pem = undef,
+  $feature_flag_accounts = false,
+  $plek_account_manager_uri = undef,
 ) {
 
   if $enabled {
@@ -55,6 +79,11 @@ class govuk::apps::finder_frontend(
     app => 'finder-frontend',
   }
 
+  $feature_flag_accounts = $feature_flag_accounts ? {
+                true    => 'enabled',
+                default => undef
+        }
+
   govuk::app::envvar {
     "${title}-EMAIL_ALERT_API_BEARER_TOKEN":
         varname => 'EMAIL_ALERT_API_BEARER_TOKEN',
@@ -62,6 +91,24 @@ class govuk::apps::finder_frontend(
     "${title}-SECRET_KEY_BASE":
         varname => 'SECRET_KEY_BASE',
         value   => $secret_key_base;
+    "${title}-ACCOUNT-OAUTH-CLIENT-ID":
+        varname => 'GOVUK_ACCOUNT_OAUTH_CLIENT_ID',
+        value   => $account_oauth_client_id;
+    "${title}-ACCOUNT-OAUTH-CLIENT-SECRET":
+        varname => 'GOVUK_ACCOUNT_OAUTH_CLIENT_SECRET',
+        value   => $account_oauth_client_secret;
+    "${title}-ACCOUNT-JWT-KEY-UUID":
+        varname => 'GOVUK_ACCOUNT_JWT_KEY_UUID',
+        value   => $account_jwt_key_uuid;
+    "${title}-ACCOUNT-JWT-KEY-PEM":
+        varname => 'GOVUK_ACCOUNT_JWT_KEY_PEM',
+        value   => $account_jwt_key_pem;
+    "${title}-FEATURE-FLAG-ACCOUNTS":
+        varname => 'FEATURE_FLAG_ACCOUNTS',
+        value   => $feature_flag_accounts;
+    "${title}-PLEK-ACCOUNT-MANAGER-URI":
+        varname => 'PLEK_SERVICE_ACCOUNT_MANAGER_URI',
+        value   => $plek_account_manager_uri;
   }
 
 }
