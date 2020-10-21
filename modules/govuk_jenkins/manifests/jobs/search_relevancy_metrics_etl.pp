89865279175c9337b671f680384062bd3a3af9f2
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-10-21 14:31:27 +0100

    Remove Search team from metric fetch failure contact groups
    When search metrics fetches do not succeed within a certain time,
    this triggers an Icinga alert 'freshness threshold exceeded'.
    
    Since there is no longer a Search team it makes sense to not send
    these alerts to a Slack channel. They will continue to appear in
    Icinga.

--- modules/govuk_jenkins/manifests/jobs/search_relevancy_metrics_etl.pp
+++ modules/govuk_jenkins/manifests/jobs/search_relevancy_metrics_etl.pp
@@ -30,7 +30,6 @@ class govuk_jenkins::jobs::search_relevancy_metrics_etl (
       host_name           => $::fqdn,
       freshness_threshold => 86400,
       action_url          => $job_url,
-      contact_groups      => ['slack-channel-search-team'],
     }
   }
 }
