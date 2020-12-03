b2567da4918e0391688557f53a3dfbc3a160a5a9
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2020-12-03 12:55:31 +0000

    Cleanup: remove disused user class for Tom W.
    This needs to wait until #10864 and alphagov/govuk-secrets#1095 are in
    prod.

--- modules/users/manifests/tomwhitwell.pp
+++
@@ -1,9 +0,0 @@
-# Creates the tomwhitwell user
-class users::tomwhitwell {
-  govuk_user { 'tomwhitwell':
-    ensure   => absent,
-    fullname => 'Tom Whitwell',
-    email    => 'tom.whitwell@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILb/0/9cf3s7x+LlOC+4xWJ0ogzWUInOeZCBHRqhxqoD tom.whitwell@digital.cabinet-office.gov.uk',
-  }
-}
