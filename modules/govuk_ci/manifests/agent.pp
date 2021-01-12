147521dc1d7320b85562c5b594ba67937327a1bf
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2021-01-12 10:44:02 +0000

    install python 3.6.12

--- modules/govuk_ci/manifests/agent.pp
+++ modules/govuk_ci/manifests/agent.pp
@@ -42,7 +42,6 @@ class govuk_ci::agent(
   include ::govuk_jenkins::user
   include ::govuk_rbenv::all
   include ::govuk_python
-  include ::govuk_python3
   include ::govuk_sysdig
   include ::govuk_testing_tools
   include ::yarn
--- modules/govuk_python/manifests/init.pp
+++ modules/govuk_python/manifests/init.pp
@@ -1,5 +1,9 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
-class govuk_python {
+class govuk_python (
+  $govuk_python_version = '3.6.12',
+) {
+
+  include govuk_python::apt_source
 
   class { '::python':
     pip        => 'present',
@@ -9,5 +13,13 @@ class govuk_python {
 
   Class['python::install'] -> Package <| provider == 'pip' and ensure != absent |>
 
-  ensure_packages(['python3.5', 'python3.5-dev', 'python3-dev', 'govuk-python'])
+  ensure_packages(['python3.5', 'python3.5-dev', 'python3-dev'])
+
+  ensure_packages(
+    ['govuk-python'],
+    {
+      ensure  => $govuk_python_version,
+      require => Apt::Source['govuk-python'],
+    }
+  )
 }
