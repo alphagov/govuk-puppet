5915ed565597248342ac7145ea4feccaec312b43
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2021-02-08 15:44:04 +0000

    Add go version 1.7.1 for govuk_crawler_worker
    See https://github.com/alphagov/govuk_crawler_worker/blob/master/.go-version.

--- modules/golang/manifests/init.pp
+++ modules/golang/manifests/init.pp
@@ -11,7 +11,12 @@ class golang {
     require        => Class['govuk_ppa'],
   }
 
-  goenv::version { ['1.11.13', '1.12.1', '1.15.8']: }
+  goenv::version { [
+    '1.7.1',   # Used by alphagov/govuk_crawler_worker
+    '1.11.13', # Used by alphagov/router
+    '1.12.1',  # Used by alphagov/govuk-csp-forwarder
+    '1.15.8',  # Used by alphagov/router
+    ]: }
 
   package { ['godep']:
     ensure  => latest,
