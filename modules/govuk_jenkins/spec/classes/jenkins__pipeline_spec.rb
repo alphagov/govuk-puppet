4caa400a17e56364d3fd74ce6929813d3eebec62
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2021-03-18 12:46:34 +0000

    Set default govuk-jenkinslib branch to main
    Once the branch has been renamed, we'll need to deploy this to all our environments.

--- modules/govuk_jenkins/spec/classes/jenkins__pipeline_spec.rb
+++ modules/govuk_jenkins/spec/classes/jenkins__pipeline_spec.rb
@@ -5,7 +5,7 @@ describe 'govuk_jenkins::pipeline', :type => :class do
     it { is_expected.to contain_file('/var/lib/jenkins/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml').with_content(/<name>govuk<\/name/) }
     it { is_expected.to contain_file('/var/lib/jenkins/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml').with_content(/<repoOwner>alphagov<\/repoOwner>/) }
     it { is_expected.to contain_file('/var/lib/jenkins/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml').with_content(/<repository>govuk-jenkinslib<\/repository>/) }
-    it { is_expected.to contain_file('/var/lib/jenkins/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml').with_content(/<defaultVersion>master<\/defaultVersion>/) }
+    it { is_expected.to contain_file('/var/lib/jenkins/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml').with_content(/<defaultVersion>main<\/defaultVersion>/) }
     it { is_expected.to contain_file('/var/lib/jenkins/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml').with_content(/<implicit>false<\/implicit>/) }
   end
 end
