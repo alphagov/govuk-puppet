60b96fb80fa6bc93972f23310b6b8e7cc02d65f1
Michael Walker <mike@barrucadu.co.uk>
2021-04-21 11:26:49 +0100

    Expose RFC 141 healthcheck paths for support-api

--- modules/govuk/manifests/apps/support_api.pp
+++ modules/govuk/manifests/apps/support_api.pp
@@ -113,13 +113,15 @@ class govuk::apps::support_api(
   $app_name = 'support-api'
 
   govuk::app { $app_name:
-    ensure             => $ensure,
-    app_type           => 'rack',
-    port               => $port,
-    sentry_dsn         => $sentry_dsn,
-    vhost_ssl_only     => true,
-    health_check_path  => '/healthcheck',
-    log_format_is_json => true,
+    ensure                     => $ensure,
+    app_type                   => 'rack',
+    port                       => $port,
+    sentry_dsn                 => $sentry_dsn,
+    vhost_ssl_only             => true,
+    health_check_path          => '/healthcheck',
+    has_liveness_health_check  => true,
+    has_readiness_health_check => true,
+    log_format_is_json         => true,
   }
 
   Govuk::App::Envvar {
