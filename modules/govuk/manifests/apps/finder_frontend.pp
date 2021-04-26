bd3dcf70e565d8cddafba9708e51fd82927a0d9e
Michael Walker <mike@barrucadu.co.uk>
2021-04-26 11:22:53 +0100

    Remove legacy healthcheck from finder-frontend

--- modules/govuk/manifests/apps/finder_frontend.pp
+++ modules/govuk/manifests/apps/finder_frontend.pp
@@ -33,8 +33,6 @@ class govuk::apps::finder_frontend(
       app_type                       => 'rack',
       port                           => $port,
       sentry_dsn                     => $sentry_dsn,
-      health_check_path              => '/healthcheck.json',
-      json_health_check              => true,
       has_liveness_health_check      => true,
       has_readiness_health_check     => true,
       log_format_is_json             => true,
