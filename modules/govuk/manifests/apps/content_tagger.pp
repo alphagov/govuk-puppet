32f3d5abbaa0d0f1c67ffe089d23e0a73154a6d4
Michael Walker <mike@barrucadu.co.uk>
2021-04-26 11:21:55 +0100

    Remove legacy healthcheck from content-tagger

--- modules/govuk/manifests/apps/content_tagger.pp
+++ modules/govuk/manifests/apps/content_tagger.pp
@@ -88,7 +88,6 @@ class govuk::apps::content_tagger(
     app_type                   => 'rack',
     port                       => $port,
     vhost_ssl_only             => true,
-    health_check_path          => '/healthcheck',
     has_liveness_health_check  => true,
     has_readiness_health_check => true,
     log_format_is_json         => true,
