2ba01cba3531ed33c7ee39a2269da62631ab3a6e
Michael Walker <mike@barrucadu.co.uk>
2021-04-26 11:23:01 +0100

    Remove legacy healthcheck from frontend

--- modules/govuk/manifests/apps/frontend.pp
+++ modules/govuk/manifests/apps/frontend.pp
@@ -79,7 +79,6 @@ class govuk::apps::frontend(
     port                       => $port,
     sentry_dsn                 => $sentry_dsn,
     vhost_protected            => $vhost_protected,
-    health_check_path          => '/',
     has_liveness_health_check  => true,
     has_readiness_health_check => true,
     log_format_is_json         => true,
