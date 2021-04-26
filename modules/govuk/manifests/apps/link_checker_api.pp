53fe8ce16850c05ad490fd95bed0f48cafb4a68f
Michael Walker <mike@barrucadu.co.uk>
2021-04-26 11:23:35 +0100

    Remove legacy healthcheck from link-checker-api

--- modules/govuk/manifests/apps/link_checker_api.pp
+++ modules/govuk/manifests/apps/link_checker_api.pp
@@ -100,7 +100,6 @@ class govuk::apps::link_checker_api (
     port                       => $port,
     sentry_dsn                 => $sentry_dsn,
     vhost_ssl_only             => true,
-    health_check_path          => '/healthcheck',
     has_liveness_health_check  => true,
     has_readiness_health_check => true,
   }
