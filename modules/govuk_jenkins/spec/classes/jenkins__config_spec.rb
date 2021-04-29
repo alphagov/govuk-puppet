8c1d0aa0d92f84d372910079bbee38a4fd0cc371
Thomas Leese <thomas@leese.io>
2021-04-29 07:38:46 +0100

    Refactor Jenkins config tests
    To better match our coding style.

--- modules/govuk_jenkins/spec/classes/jenkins__config_spec.rb
+++ modules/govuk_jenkins/spec/classes/jenkins__config_spec.rb
@@ -1,39 +1,38 @@
-require_relative '../../../../spec_helper'
+require_relative "../../../../spec_helper"
 
-describe 'govuk_jenkins::config', :type => :class do
-  let(:default_params) {{
-    :github_client_id               => 'bar',
-    :github_client_secret_encrypted => 'baz',
-  }}
+describe "govuk_jenkins::config", :type => :class do
+  let(:default_params) do
+    { github_client_id: "bar", github_client_secret_encrypted: "baz" }
+  end
 
-  describe 'manage config' do
-    context 'false (default)' do
-      let (:params) { default_params.merge({
-        :manage_config => false,
-      })}
+  describe "manage config" do
+    context "false (default)" do
+      let (:params) { default_params.merge(manage_config: false) }
 
-      it { is_expected.not_to contain_file('/var/lib/jenkins/config.xml') }
+      it { is_expected.not_to contain_file("/var/lib/jenkins/config.xml") }
     end
 
-    context 'true' do
-      let (:params) { default_params.merge({
-        :manage_config => true,
-      })}
+    context "true" do
+      let (:params) { default_params.merge(manage_config: true) }
 
-      it { is_expected.to contain_file('/var/lib/jenkins/config.xml').with_content(/<githubWebUri>wibble/) }
+      it { is_expected.to contain_file("/var/lib/jenkins/config.xml").with_content(/<githubWebUri>wibble/) }
     end
 
-    context 'authentication' do
-      let (:params) { default_params.merge({
-        :manage_permissions_github_teams => true,
-        :user_permissions                => [{
-          'user'        => 'admin_user',
-          'permissions' => ['jenkins.perm.admin', 'jenkins.perm.edit'],
-        }],
-      })}
+    context "authentication" do
+      let (:params) do
+        default_params.merge(
+          manage_permissions_github_teams: true,
+          user_permissions: [
+            {
+              "user" => "admin_user",
+              "permissions" => ["jenkins.perm.admin", "jenkins.perm.edit"],
+            },
+          ],
+        )
+      end
 
-      it { is_expected.to contain_file('/var/lib/jenkins/config.xml').with_content(/<authorizationStrategy class="hudson.security.GlobalMatrixAuthorizationStrategy">/) }
-      it { is_expected.to contain_file('/var/lib/jenkins/config.xml').with_content(/<permission>jenkins.perm.admin:admin_user<\/permission>\n\s+<permission>jenkins.perm.edit:admin_user<\/permission>/) }
+      it { is_expected.to contain_file("/var/lib/jenkins/config.xml").with_content(/<authorizationStrategy class="hudson.security.GlobalMatrixAuthorizationStrategy">/) }
+      it { is_expected.to contain_file("/var/lib/jenkins/config.xml").with_content(/<permission>jenkins.perm.admin:admin_user<\/permission>\n\s+<permission>jenkins.perm.edit:admin_user<\/permission>/) }
     end
   end
 end
