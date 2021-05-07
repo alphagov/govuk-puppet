3797721ac79d40d516986a8b2d9737524c4d235c
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2021-05-07 14:21:19 +0100

    Add govuk-python3 manifests

---
+++ modules/govuk_python3/manifests/init.pp
@@ -0,0 +1,25 @@
+# FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
+class govuk_python3 (
+  $govuk_python_version = '3.6.12',
+) {
+
+  include govuk_python3::apt_source
+
+  class { '::python':
+    pip        => 'present',
+    dev        => 'present',
+    virtualenv => 'present',
+  }
+
+  Class['python::install'] -> Package <| provider == 'pip' and ensure != absent |>
+
+  ensure_packages(['python3.5', 'python3.5-dev', 'python3-dev'])
+
+  ensure_packages(
+    ['govuk-python3'],
+    {
+      ensure  => $govuk_python_version,
+      require => Apt::Source['govuk-python3'],
+    }
+  )
+}
