ce3094d912b428d540879db650f9fb6fe8d97fd3
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2021-05-28 13:35:33 +0100

    install virtualenv for python3.6 for mapit
    Co-Authored-By: Chris Ashton <ChrisBAshton@users.noreply.github.com>

--- modules/govuk/manifests/node/s_mapit.pp
+++ modules/govuk/manifests/node/s_mapit.pp
@@ -5,6 +5,14 @@ class govuk::node::s_mapit inherits govuk::node::s_base {
   include govuk_python3
   include nginx
 
+  # Make sure virtualenv is installed for Python 3.6 as required by mapit
+  exec { 'check_virtualenv_for_python_3_6':
+    path    => ['/usr/bin', '/usr/sbin'],
+    command => '/usr/bin/pip3.6 install -I virtualenv==20.4.7',
+    require => [Class['govuk_python3'], Class['govuk_python']],
+    unless  => 'test -d /usr/lib/python3.6/site-packages/virtualenv',
+  }
+
   file { '/etc/postgresql':
     ensure  => directory,
     owner   => 'postgres',
