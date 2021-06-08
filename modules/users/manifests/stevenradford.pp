59aa910ec08277a857287e476b0b1566a8003c35
Thomas Leese <thomas@leese.io>
2021-06-08 11:32:24 +0100

    Add Steven Radford user
    They are contractors from Apto who are going to be working on
    Licensing.

---
+++ modules/users/manifests/stevenradford.pp
@@ -0,0 +1,7 @@
+class users::stevenradford {
+  govuk_user { 'stevenradford':
+    fullname => 'Steven Radford',
+    email    => 'steven.radford@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmW6QL+Qq8W1mmBFp7lCIY9wubQPo1PK9zeCTVjaPxUc9EHr471QGREHsqv/p+bDOyi71DCYzaVz9iwhuPH1hWTpiadY6Iu1qht94K29UZpCHHnqvMcOMNVG2y013ixCjEaytnXb7VidNjNSm5fNz5T0J4/vfdNJ5/x44cqpWTCOWeMtdOkHQ49wwP7fAULn4ziKwSccrM9ijVUyJw20fYJF9I46WSIFeP3nyR/KVjb01npWSdxQk9fnDLppjG2zmUUvs44mZtigAMLxN/hKNFRpIMq8if2HkT9bpsly3zwTgHJF3jao+fivN+0YDQ+ojHOyeFWnsnsLIW5Xk6Jcap stevenr@aptosolutions.co.uk',
+  }
+}
