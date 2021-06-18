9a8a0738637bfdb97dd764cbf9ce95adea348960
Max Froumentin <github@e97.me>
2021-06-18 11:47:07 +0100

    Alan Gabbianelli has left GDS

--- modules/users/manifests/alangabbianelli.pp
+++ modules/users/manifests/alangabbianelli.pp
@@ -1,6 +1,7 @@
 # Creates the alangabbianelli user
 class users::alangabbianelli {
   govuk_user { 'alangabbianelli':
+    ensure => absent,
     fullname => 'Alan Gabbianelli',
     email    => 'alan.gabbianelli@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDPwdFQe8MR9rDx6ofsY6I4CUN67A8rhsJevxF/xKZsi2U4oVUU6I2JiiQ4dCubu8QUIvDhUgErCed5C3A/dEu/2UqXr2/LcRbMUzoQs3spK2UsHLNDWZ898O2wTZzu5l2k4qbTbAsr9QotzRLpieSWvmFHoVqfgKNO2s+ywQZb08uxrrBAQn8Q2fxUiNPB+BLEa91uvXR5kR3gUe8ciqZUrR0KlKimZeAqz7p+600YZDK6c74uglnh6/Z6w4mZEuUbfVZGcigCE5QR/qUGAc1wF8T64AuDbUQhyDeUxX1ev6mH/qQ/icDaa5yzQcJHUaPFzOVE6RfXmAqs00fqNzbBgS7raSH3X3DezemO3Thk/CM+Sd6LU+sh5qflZdv6ov/4Av6aitgUNpEF1RWiZU4N6LR9kPgQAzXGjekg8pvI+hROWEj/RYkjXegQh0+qG4+h8TsvdRnySkGxZAD70JBaNBxcP8fq1fUbhtwsDNJipC2e2EmIidZw9yAOWkRNuiR6NLYe18SA4qhcHhmKVIesoSbPW4mY1mdMhXSoWPSutIA2ICcxQdJfQZm4PnX3/FMPX5hcasBZ77WHlVzGkKz1DfqW2APQ2a9mytxElmbV63iyea1B8J/nf78LD1Y/TMAfY8jPICGIafNeWWexedg/j+gW797GvncTjfMaW55qkQ== alan.gabbianelli@digital.cabinet-office.gov.uk',
