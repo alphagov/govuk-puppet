f3a031a5fad1ab5c67f33128934a4d3930ff9f8d
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2021-12-15 13:00:56 +0000

    Configure release_db_admin machine class
    This creates a dedicated database admin machine for Release, following the Account API convention devised in #11342.
    
    This is in order to facilitate the implementation of [RFC-143][], which concludes that every GOV.UK relational database should have its own RDS instance.
    
    Trello: https://trello.com/c/HjK4AbUS/49-configure-puppet-for-new-db-admin-mysql-instances
    
    [RFC-143]: https://github.com/alphagov/govuk-rfcs/blob/main/rfc-143-split-database-instances.md

---
+++ modules/govuk/manifests/node/s_release_db_admin.pp
@@ -0,0 +1,34 @@
+# == Class: govuk_node::s_release_db_admin
+#
+# This machine class is used to administer the Release
+# MySQL RDS instances.
+#
+# === Parameters
+#
+# [*mysql_db_host*]
+#  The database hostname
+#
+# [*mysql_db_password*]
+#  The database password
+#
+# [*mysql_db_user*]
+#  The database user to connect to the remote database as
+#
+class govuk::node::s_release_db_admin(
+  $mysql_db_host     = undef,
+  $mysql_db_password = undef,
+  $mysql_db_user     = undef,
+) {
+  include ::govuk::node::s_base
+  include govuk_env_sync
+
+  file { '/root/.my.cnf':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    content => template('govuk/mysql_my.cnf.erb'),
+  }
+
+  # include all MySQL classes that create databases and users
+  -> class { '::govuk::apps::release::db': }
+}
