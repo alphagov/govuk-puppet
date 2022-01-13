68e4830a9f6b6792ea38aa2ed92563a6f6f6fb5b
Alex Baker <alex.baker@digital.cabinet-office.gov.uk>
2022-01-13 16:22:43 +0000

    Adding in the last user, Seun.

---
+++ modules/users/manifests/seun.adiomowo.pp
@@ -0,0 +1,8 @@
+# Creates the seunadiomowo user
+class users::seunadiomowo {
+  govuk_user { 'seunadiomowo':
+    fullname => 'Seun Adiomowo',
+    email    => 'seun.adiomowo@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfwn2PHZxtS6mR3wSHAVf9D3dF270pLYwuwb5HQD+8Q seunadiomowo@Seuns-MBP',
+  }
+}
