0b553e22ec80a21d44443c147e8a9df0e5194dff
Alex Baker <alex.baker@digital.cabinet-office.gov.uk>
2022-01-13 12:17:46 +0000

    Add missing comma

--- modules/users/manifests/lukewiltshire.pp
+++ modules/users/manifests/lukewiltshire.pp
@@ -3,6 +3,6 @@ class users::lukewiltshire {
   govuk_user { 'lukewiltshire':
     fullname => 'Luke Wiltshire',
     email    => 'luke.wiltshire@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWJU1ku2E3LkeMTgKURjMBiv4MyvS+9Lg99vHIk+MPE luke.wiltshire@digital.cabinet-office.gov.uk'
+    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWJU1ku2E3LkeMTgKURjMBiv4MyvS+9Lg99vHIk+MPE luke.wiltshire@digital.cabinet-office.gov.uk',
   }
 }
