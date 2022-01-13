54578e6fed51b154bc15ed90dff51b0be5d001a7
Alex Baker <alexbaker@Alexs-MacBook-Pro.local>
2022-01-13 10:42:11 +0000

    Adding Alex Baker and Luke Wiltshire for integration SSH access

---
+++ modules/users/manifests/alexbaker.pp
@@ -0,0 +1,8 @@
+# Creates the alexbaker user
+class users::alexbaker {
+  govuk_user { 'alexbaker':
+    fullname => 'Alex Baker',
+    email    => 'alex.baker@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbCzicUco2TIwas6wuHr2B1d5wsXX3FlrZLsydhs3L1 alex.baker@digital.cabinet-office.gov.uk',
+  }
+}
