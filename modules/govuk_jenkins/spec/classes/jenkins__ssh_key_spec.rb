e2db9ff23deaab0255153221bef819c5e2724ddb
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2022-02-11 09:28:04 +0000

    Generate Jenkins' public ssh key
    Previously, the govuk_jenkins ssh_key class required you to pass in the
    private key and the public key separately.
    
    This meant configuring the public keys in hieradata_aws, and the private
    keys in govuk-secrets, and making sure you got the right public keys for
    the right private keys.
    
    Predictably, we did not get this right:
    
    * https://github.com/alphagov/govuk-puppet/pull/11543
    * https://github.com/alphagov/govuk-aws-data/pull/980
    
    There's no need to ask for both the private key and the public key.
    Since the private key _contains_ the public key we can just generate it.
    
    The only reason I can think of for specifying them separately would be
    if we wanted to allow the mistake of having mismatched keys.
    
    Using Puppet's Exec resource, we can run `ssh-keygen` to create the
    public key from the private key. The `creates` property ensures this
    runs if the key is not already there.
    
    Using the `subscribe` property means that the exec resource should be
    notified if the private key resource changes, and run the command again.

--- modules/govuk_jenkins/spec/classes/jenkins__ssh_key_spec.rb
+++ modules/govuk_jenkins/spec/classes/jenkins__ssh_key_spec.rb
@@ -2,6 +2,7 @@ require_relative '../../../../spec_helper'
 
 describe 'govuk_jenkins::ssh_key', :type => :class do
   private_key_filename = '/var/lib/jenkins/.ssh/id_rsa'
+  public_key_filename = '/var/lib/jenkins/.ssh/id_rsa.pub'
 
   context 'when a keypair is not provided (by default)' do
     it { is_expected.to contain_exec('Creating key pair for jenkins').with_creates(private_key_filename) }
@@ -10,10 +11,9 @@ describe 'govuk_jenkins::ssh_key', :type => :class do
   context 'when a keypair is provided' do
     let(:params) {{
       :private_key => '-----BEGIN KEY-----',
-      :public_key  => 'AAAA',
     }}
 
     it { is_expected.to contain_file(private_key_filename).with_content('-----BEGIN KEY-----') }
-    it { is_expected.to contain_file("#{private_key_filename}.pub").with_content('ssh-rsa AAAA') }
+    it { is_expected.to contain_exec("#{private_key_filename}.pub").with_creates(public_key_filename) }
   end
 end
