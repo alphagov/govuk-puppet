ce4f4006acfd27fe25d9d8b6eef869b0111a5ba6
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2022-02-24 11:52:48 +0000

    Install aws cloudwatch agent on the jumpboxes
    We'd like to use this to ship logs about who's doing stuff with ssh to
    cloudwatch (and then eventually to cyber security's splunk instance)

--- modules/govuk/manifests/node/s_jumpbox.pp
+++ modules/govuk/manifests/node/s_jumpbox.pp
@@ -1,6 +1,8 @@
 # FIXME: This class needs better documentation as per https://docs.puppetlabs.com/guides/style_guide.html#puppet-doc
 class govuk::node::s_jumpbox inherits govuk::node::s_base {
 
+  include govuk_awscloudwatch
+
   file { '/usr/local/bin/ssh-proxy':
     ensure => 'present',
     mode   => '0775',
