73856bf27c4b147c0fb130de03d5e4d4d1bb80cc
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2022-03-03 14:13:04 +0000

    Remove ununsed versions of go
    confirmed with https://sourcegraph.com/search?q=context:global+repo:alphagov/+file:go-version

--- modules/golang/manifests/init.pp
+++ modules/golang/manifests/init.pp
@@ -7,16 +7,14 @@ class golang {
   include govuk_ppa
 
   class { 'goenv':
-    global_version => '1.15.8',
+    global_version => '1.17.7',
     require        => Class['govuk_ppa'],
   }
 
   goenv::version { [
     '1.7.1',   # Used by alphagov/govuk_crawler_worker
-    '1.11.13', # Used by alphagov/router
     '1.12.1',  # Used by alphagov/govuk-csp-forwarder
-    '1.15.8',  # Used by alphagov/router
-    '1.17.6',  # Used by alphagov/router
+    '1.17.6',  # Used by alphagov/router (remove once router is running 1.17.7)
     '1.17.7',  # Used by alphagov/router
     ]: }
 
