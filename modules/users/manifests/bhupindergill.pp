6cd7d2918123e45612fe981508ba2cae757f5c38
BhupsAL1 <bhupinder.gill@digital.cabinet-office.gov.uk>
2022-03-07 14:34:59 +0000

    adding integration access for bhupinder

---
+++ modules/users/manifests/bhupindergill.pp
@@ -0,0 +1,8 @@
+# Creates the user bhupindergill
+class users::bhupindergill {
+  govuk_user { 'bhupindergill':
+    fullname => 'Bhupinder Gill',
+    email    => 'bhupinder.gill@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAH2celZRdqiHn5GGkQxZY/7MKOjMq376i7Dv4vkTCFB bhupinder.gill@digital.cabinet-office.gov.uk',
+  }
+}
