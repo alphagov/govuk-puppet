7c49148aa8b41c6310eb3d37ac0d349f3f7f105b
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2022-03-15 16:52:45 +0000

    Added my user name to integration manifests

---
+++ modules/users/manifests/jonathanyoung.pp
@@ -0,0 +1,8 @@
+# Creates the jonathanyoung user
+class users::jonathanyoung {
+  govuk_user { 'jonathanyoung':
+    fullname => 'Jonathan Young',
+    email    => 'jonathan.young@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFcjWkCw8XkoiarGeL0k2CQ8SOxDswIt7nWh1A0GyaFg jonathan.young@digital.cabinet-office.gov.uk',
+  }
+}
