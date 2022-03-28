4500cee6679ab955141923f122d729e5c1f87c0f
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-03-28 17:37:32 +0100

    Remove Xvfb from govuk_testing_tools module
    [Xvfb](https://www.x.org/releases/X11R7.6/doc/man/man1/Xvfb.1.xhtml) is a virtual X server, which was previously needed so capybara-webkit could headlessly render web pages.
    
    Capybara-webkit is no longer used now that we have ChromeDriver installed via the govuk_chromedriver module.
    
    This change removes the Xvfb package and its configuration file. It does this by renaming and repurposing the existing puppet class.
    
    This achieves the first step of removing Xvfb. Once this change has been applied, we'll be able to remove the configuration entirely as it'll no longer be needed.

--- modules/govuk_testing_tools/manifests/init.pp
+++ modules/govuk_testing_tools/manifests/init.pp
@@ -6,13 +6,13 @@ class govuk_testing_tools {
   include govuk_chromedriver
   include imagemagick
   include remove_selenium
-  include ::govuk_testing_tools::xvfb
+  include ::govuk_testing_tools::remove_xvfb # was previously needed by capybara-webkit
 
   package { [
     'qt4-qmake',     # needed for capybara-webkit
     'qt4-dev-tools', #    "            "
     ]:
-    ensure => installed;
+    ensure => absent; # we no longer use capybara-webkit
   }
 
   package { 'brakeman':
