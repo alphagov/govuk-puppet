1eea10fbe5dd14895907005c5e506f3eb96cfcb4
Rodrigo <hilgenberg-bastosr@Rodrigos-MBP-2.cust.communityfibre.co.uk>
2022-03-28 17:17:32 +0100

    created manifest file
    changed author

---
+++ modules/users/manifests/rodrigohilgenbergbastos.pp
@@ -0,0 +1,8 @@
+# Creates the Rodrigo Hilgenberg Bastos user
+class users::rodrigohilgenbergbastos {
+  govuk_user { 'rodrigohilgenbergbastos':
+    fullname => 'Rodrigo Hilgenberg-Bastos',
+    email    => 'rodrigo.hilgenberg-bastos@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCqG05jjKLT7hZc+98v4oNgFTsZ6BZhZ0Ig4POD/jbuPXhOn+asfWhn4Tvl8n07GaGzYSIyfPq+YGo0em8agIgLds8bPEawCpCXQ5jrr9CJbMPbKVY7MwfUj3ihw14TWz7YNQ9auUY3xXIqD8m7klaSMcVOLTKPU052k8hk3835Et6TabSY5++2HgaivwcnvEMrtle/+k/xU+7DWNxDzRrVLj7v1/8Drqh88/ALQd4tdKpPy0EqgDY1VqMl7I0VucH1ifG4a5ekx3LnevTimSVZV99wsuX29TckMKdY4BqerYqod8liJAMmAePyui3LaVvGZHDsy7RSzqmxazjClH3nIRKbaAGr7wF1ouO0K7c+RdI/lqmidMGZsJ7n0oBbHy/FfsFysn8P6D/6LXYysbA3tgNOZ/v+gGqv9NPGmiRF9XtHspBmG4ghVXRwoO/E+UiP8urtSJv5qEpu5tRPm2flL76WA0GIGMaA0Yb3fu0l6qn5n7VGCY7btF5cwCC4NB8= hilgenberg-bastosr@Rodrigos-MacBook-Pro-2.local',
+  }
+}
