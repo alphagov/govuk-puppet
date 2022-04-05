c816ef9563d69da2586a1668fcec9f858db3262d
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-04-05 14:17:10 +0100

    Add new ssh key for Becka Lelew

--- modules/users/manifests/beckalelew.pp
+++ modules/users/manifests/beckalelew.pp
@@ -3,6 +3,6 @@ class users::beckalelew {
   govuk_user { 'beckalelew':
     fullname => 'Becka Lelew',
     email    => 'becka.lelew@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIR1nueOVbg2PVlCMgAlgNJ+N+2tJcuEvDBMyoGsXgOT becka.lelew@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOUzfoH86Pby61x10zbeYEEfupbDslZQHSbultbZWssn becka.lelew@digital.cabinet-office.gov.uk',
   }
 }
