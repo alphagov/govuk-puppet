bb70af1f418a907d0318e41cac67643f89bc61f9
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-05-30 16:25:57 +0100

    Remove Bhupinder Gill
    Trello: https://trello.com/c/nKmH7cjm/1398-leaver-bhupinder-gill-tech-role

--- modules/users/manifests/bhupindergill.pp
+++
@@ -1,9 +0,0 @@
-# Creates the user bhupindergill
-class users::bhupindergill {
-  govuk_user { 'bhupindergill':
-    ensure   => absent,
-    fullname => 'Bhupinder Gill',
-    email    => 'bhupinder.gill@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAH2celZRdqiHn5GGkQxZY/7MKOjMq376i7Dv4vkTCFB bhupinder.gill@digital.cabinet-office.gov.uk',
-  }
-}
