17605ccfb70d1d9af547df67b09ff1be47747af4
Joshua Pedro <joshua.pedro@digital.cabinet-office.gov.uk>
2022-06-09 13:29:00 +0100

    creating/updating new user for SSH integration

--- modules/users/manifests/joshpedro.pp
+++ modules/users/manifests/joshpedro.pp
@@ -1,7 +1,6 @@
-# Creates the joshuapedro user
-class users::joshuapedro {
-  govuk_user { 'joshuapedro':
-    ensure   => absent
+# Creates the joshpedro user
+class users::joshpedro {
+  govuk_user { 'joshpedro':
     fullname => 'Joshua Pedro',
     email    => 'joshua.pedro@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNE5Jkpmvly3T46LirI5xtnJR44ZiEPa5JDXfOOUEu4 joshua.pedro@digital.cabinet-office.gov.uk',
