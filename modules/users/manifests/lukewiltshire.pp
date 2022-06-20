19c577e2ed25c7a951c8236d07dd79451d046f6e
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-06-20 12:57:06 +0100

    Remove user manifests for recent leavers
    Alex Baker, Luke Wiltshire and Seun Adiomowo have recently left GDS.

--- modules/users/manifests/lukewiltshire.pp
+++
@@ -1,9 +0,0 @@
-# Creates the lukewiltshire user
-class users::lukewiltshire {
-  govuk_user { 'lukewiltshire':
-    ensure   => absent,
-    fullname => 'Luke Wiltshire',
-    email    => 'luke.wiltshire@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWJU1ku2E3LkeMTgKURjMBiv4MyvS+9Lg99vHIk+MPE luke.wiltshire@digital.cabinet-office.gov.uk',
-  }
-}
