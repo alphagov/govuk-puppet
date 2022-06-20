448f76e9d77ea6a25b235afeb9bedfbd8dfb98ee
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-06-20 10:33:44 +0100

    Remove recent leavers home directories
    This removes the home directories for Alex Baker, Luke Wiltshire and
    Seun Adiomowo by marking them as absent.

--- modules/users/manifests/alexbaker.pp
+++ modules/users/manifests/alexbaker.pp
@@ -1,6 +1,7 @@
 # Creates the alexbaker user
 class users::alexbaker {
   govuk_user { 'alexbaker':
+    ensure   => absent,
     fullname => 'Alex Baker',
     email    => 'alex.baker@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbCzicUco2TIwas6wuHr2B1d5wsXX3FlrZLsydhs3L1 alex.baker@digital.cabinet-office.gov.uk',
