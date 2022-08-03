8fef7bc38ef1aa07916c021a332011539ac3109b
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2022-08-03 11:42:03 +0100

    Authenticate Fastly cache purge requests
    We are updating our Fastly configuration to use API tokens instead of
    an IP address allowlist to authenticate cache purge requests.
    
    This commit references API tokens already added to the `govuk-secrets`
    repo to authenticate purge requests.

--- modules/govuk_jenkins/manifests/jobs/clear_cdn_cache.pp
+++ modules/govuk_jenkins/manifests/jobs/clear_cdn_cache.pp
@@ -3,6 +3,7 @@
 # Create a file on disk that can be parsed by jenkins-job-builder
 #
 class govuk_jenkins::jobs::clear_cdn_cache(
+  $fastly_api_token = undef,
   $website_root_url = nil,
 ) {
   file { '/etc/jenkins_jobs/jobs/clear_cdn_cache.yaml':
