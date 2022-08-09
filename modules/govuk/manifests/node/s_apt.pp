e07410765159f0b6ed671079fa0ef949e0f5aeda
Roch Trinqué <roch.trinque@digital.cabinet-office.gov.uk>
2022-08-09 17:39:17 +0100

    fmt

--- modules/govuk/manifests/node/s_apt.pp
+++ modules/govuk/manifests/node/s_apt.pp
@@ -29,7 +29,7 @@ class govuk::node::s_apt (
       'architectures' => [$::architecture],
     },
   }
-  
+
   $pkgs = join($bootstrap_packages, ' ')
   exec { 'install_boostrap_packages':
     command     => "dpkg -i ${pkgs}",
