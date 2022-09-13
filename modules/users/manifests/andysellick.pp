1c1f656c5c2ad74f3762c950851a802588fa5d8b
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2022-09-13 17:39:47 +0100

    Remove some more 'ensure: absent' user classes.

--- modules/users/manifests/andysellick.pp
+++
@@ -1,9 +0,0 @@
-# Creates the andysellick user
-class users::andysellick {
-  govuk_user { 'andysellick':
-    ensure   => absent,
-    fullname => 'Andy Sellick',
-    email    => 'andy.sellick@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDBCDrnPg5tREFlkb/KEDhhpKf6Lx3QFg1zWRtEM5yxtQgnVBFSHmY8yJZN4clttaU2D+YToz1Y12giNq+LqEO+84eo1SIlAP8FLR1RVJsL9saswtxV/sgE+L7BVYnLEdUi7pzcbfgc9hgvfu8xMCJX5feTlILu0a+MBEETSXgW+6r3jmTrEZ2ZtXVf7/BSb5L2GPsnJW8rj4sU8x10o/PPAlDeOoxC5PxITl00B7V4yMmnhcBXGnRpdOsEHAvllLJkUDWN8p5WMxyOGWhmJB9drwXGLPCTR3e5E6+I06z2cbGf5ican7HxKkl5GuB+wl6BhJI7pju5q38IKwQXkNPidaIyiGPwIqHAXLJS92iNtSBqlb9WPTKKL4LeVwW19gTDe8srVDWi5bSv1UB6gYU3XRMcV7KYZB0EfE32N9pYDX6lkUIKLsKxhxASdOA0StQk2xnwXDuWx2qJv7T46vNhkZplK2EtIr5b7V2WiL5ibP5vRc/S2SIP+uZURm5g/QTJCchFT76x99q8PUOG/jHuOWwgtD1C+E9D4BGijbxve851pO/o48wKiRafYD8238P1C11yKiLKx/lfGDTJwvr+O3Bdyg4cLEAyAgE5w4sDGMHyEi36OlI3P/S6XyLWbiQww1nnhXG3xNodkm+5VJ8exfIDlOLV5UAZBmL/Q7Jqcw== andy.sellick@digital.cabinet-office.gov.uk',
-  }
-}
--- modules/users/manifests/davidcliff.pp
+++
@@ -1,9 +0,0 @@
-# Creates davidcliff user
-class users::davidcliff {
-  govuk_user { 'davidcliff':
-    ensure   => absent,
-    fullname => 'David Cliff',
-    email    => 'david.cliff@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCqXjcdnD4NLCCgmYRsee/PwN9yYFswcojkOX1QJ59cnnLw8h3YnG5BaFvEMk04f4SB+sxhIFBnqK6m1XbVF9/u9KL2Bo6Y2+cd9UT166I1nWPsQ76n3z8ADvqECMgxbJ3Vw7c6llvA45GvMjiqleBBM2D3GmMVtl0OB8z4kr5EKWh5TtPWkYMlYbjOpHeOifeDA5VOUAz76+9CFb3Mc7gzouAgZlTeb7LuyX4SmSC4fJ2sUjCbRd/alMu83wuKh0q5POpv05EKUvxzSwKsnT0HNwtdVhHiXtYUpCwxljSfa7UIW3vywYxqghmzJzrZWxotALFVWrw+thzknl3+a172FUhkIJ7sW7kl/HL8OgvV5bsbMcgWvkUGDmQzftjNLXTtIXNNuxKYX21OL4EXeMHikBvDLhD9YZ3Sk4dVdcuuyD8y73QAGE1qUZyUIuxNMMz6HyhB6buBe/GNcsG9iKlqyBDVZxM9zT3JxwMUOk2QHHxsxcgosvih9nXL5Rzj6BuLAj/MOP4+4mxslYvDb+MwtJSdM/t3PkPfg2YbiJh6rrStiGGTm0BaEVveAYROlHT4ULRp3oTWCyk9aQ/RdMECs+mQ2rn4ozwmJPdAO+ZJDn9YKERDxdHYlLen1cO2gs31wi3TCnwa0fjjYaCHDWzUswIp2NDSXChpTVZjzKoEvQ==',
-  }
-}
