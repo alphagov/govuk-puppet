2a76d93318044d262030803533b477b7e0143459
Patrick Cartlidge <patrickcartlidge@gmail.com>
2022-09-21 11:39:41 +0100

    Update ssh_key for patrickcartlidge

--- modules/users/manifests/patrickcartlidge.pp
+++ modules/users/manifests/patrickcartlidge.pp
@@ -3,6 +3,6 @@ class users::patrickcartlidge {
   govuk_user { 'patrickcartlidge':
     fullname => 'Patrick Cartlidge',
     email    => 'patrick.cartlidge@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMmypQq5y9p1zTW1/yXVRKaY/feGfJe8x+nuDohRGH9x patrick.cartlidge@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTzaSh3QhBF+unD1Hc4T6lda19gK3CUj8gEYRL9SOhy patrick.cartlidge@digital.cabinet-office.gov.uk',
   }
 }
