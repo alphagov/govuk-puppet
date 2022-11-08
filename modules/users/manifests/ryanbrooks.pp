fe05b8f749cbf95ddce6a12b7665c08918a80d8e
Rebecca Pearce <rebecca.pearce@digital.cabinet-office.gov.uk>
2022-11-08 14:55:10 +0000

    Remove absent users

--- modules/users/manifests/ryanbrooks.pp
+++
@@ -1,9 +0,0 @@
-# Create the ryanbrooks user
-class users::ryanbrooks {
-  govuk_user { 'ryanbrooks':
-    ensure   => absent,
-    fullname => 'Ryan Brooks',
-    email    => 'ryan.brooks@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKMvyB1xRc7UdzcDlPvoUGx+LbDbNU1272IjRK9sRUWP ryan.brooks@digital.cabinet-office.gov.uk',
-  }
-}
