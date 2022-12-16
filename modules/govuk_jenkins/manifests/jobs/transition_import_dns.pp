25d05bcf22680a2f4931b0ea2bed1d1665dcd0a3
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-12-16 15:42:50 +0000

    Add slack notifications on failure of the transition daily dns job

--- modules/govuk_jenkins/manifests/jobs/transition_import_dns.pp
+++ modules/govuk_jenkins/manifests/jobs/transition_import_dns.pp
@@ -3,6 +3,13 @@
 # Create a file on disk that can be parsed by jenkins-job-builder
 #
 class govuk_jenkins::jobs::transition_import_dns {
+  $slack_channel = '#govuk-publishing-platform'
+  $slack_credential_id = 'slack-notification-token'
+  $slack_team_domain = 'gds'
+  $deploy_jenkins_domain = hiera('deploy_jenkins_domain')
+  $slack_build_server_url = "https://${deploy_jenkins_domain}/"
+  $environment = hiera('govuk_jenkins::jobs::deploy_app_downstream::deploy_environment')
+
   file { '/etc/jenkins_jobs/jobs/transition_import_dns.yaml':
     ensure  => present,
     content => template('govuk_jenkins/jobs/transition_import_dns.yaml.erb'),
