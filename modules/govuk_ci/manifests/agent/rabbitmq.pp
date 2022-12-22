dfb0e66d62d116b63d18f38bfe94acf9a4b7e3a2
Al Davidson <apdavidson@gmail.com>
2022-12-22 11:00:33 +0000

    try passing explicit values as defaults to govuk_ci::agent::rabbitmq

--- modules/govuk_ci/manifests/agent/rabbitmq.pp
+++ modules/govuk_ci/manifests/agent/rabbitmq.pp
@@ -3,8 +3,8 @@
 # Installs and configures rabbitmq-server
 #
 class govuk_ci::agent::rabbitmq (
-  $monitoring_password => lookup('govuk_rabbitmq::monitoring_password'),
-  $monitoring_host     => 'localhost',
+  $monitoring_password = lookup('govuk_rabbitmq::monitoring_password'),
+  $monitoring_host     = 'localhost',
 )
 
   contain ::govuk_rabbitmq
