9d0babe680b49c685f6cff78ded023913cd4b898
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2023-01-06 12:18:35 +0000

    Configure Smokey job to run regularly via cron
    This has been configured to run every 10 minutes, which should be
    regular enough to catch systematic / 'probe' errors, whilst not
    putting Jenkins under too much load. In any case, we've also set
    a default timeout of 5 minutes, whereby the job should be aborted
    if the job isn't completed within that time frame.
    
    The hours have been constrained to 7am-7pm, as we would expect
    Smokey to fail overnight during the data sync. We can consider
    tweaking these values on a per-environment basis (e.g. setting
    production to simply '*', as everything should continue to work
    on production).
    
    Currently, Smokey seems to take less than two minutes to complete,
    so this should be ample time.

--- modules/govuk_jenkins/manifests/jobs/smokey.pp
+++ modules/govuk_jenkins/manifests/jobs/smokey.pp
@@ -7,8 +7,22 @@
 # [*environment*]
 #   The environment which is running Smokey. Used to build the rake task.
 #
+# [*smokey_cron_schedule*]
+#   The cron timings for the Smokey job.
+#   This replaces the old 'Smokey Loop', so should run as close to
+#   'continuously' as possible (without risking building up queues in the
+#   system).
+#   Default: '*/10 7-19 * * 1-5'
+#
+# [*smokey_timeout*]
+#   Time limit (in seconds) in which the Smokey job must be completed
+#   before being automatically aborted.
+#   Default: '300'
+#
 class govuk_jenkins::jobs::smokey (
   $environment = 'production',
+  $smokey_cron_schedule = '*/10 7-19 * * 1-5',
+  $smokey_timeout = '300',
 ) {
   include govuk::apps::smokey
 
