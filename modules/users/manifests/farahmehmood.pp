ab5826823c49878d727cf79ca132e22ac3c531a8
farahTW <farah.mehmood@digital.cabinet-office.gov.uk>
2023-04-25 16:15:43 +0100

    Corrected email address for integartion access

--- modules/users/manifests/farahmehmood.pp
+++ modules/users/manifests/farahmehmood.pp
@@ -2,7 +2,7 @@
 class users::farahmehmood {
   govuk_user { 'farahmehmood':
     fullname => 'Farah Mehmood',
-    email    => 'farahm.mehmood@digital.cabinet-office.gov.uk',
+    email    => 'farah.mehmood@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILZji3ESz+XeCoRRrNrIRSDPYno9+s0RQ/pjI9UpJ5n3 farah.mehmood@digital.cabinet-office.gov.uk',
   }
 }
