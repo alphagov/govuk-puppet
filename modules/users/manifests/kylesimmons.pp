fcd12f16226f1055037ca27afe6fb9cae2827348
kyle-c-simmons <kyle.simmons@digital.cabinet-office.gov.uk>
2023-05-24 10:07:12 +0100

    Add Kyle Simmons to integrations

---
+++ modules/users/manifests/kylesimmons.pp
@@ -0,0 +1,8 @@
+# Creates the kylesimmons user
+class users::kylesimmons {
+  govuk_user { 'kylesimmons':
+    fullname => 'Kyle Simmons',
+    email    => 'kyle.simmons@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKz99jgxT80k+NOG0WBsO7yurQnPUe7QHnUz+F9AzqIu kyle.simmons@digital.cabinet-office.gov.uk',
+  }
+}
