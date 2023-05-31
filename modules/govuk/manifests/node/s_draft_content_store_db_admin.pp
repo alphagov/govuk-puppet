b508ec56af9650ff9af24a7009869c2b22a1edf3
Al Davidson <apdavidson@gmail.com>
2023-05-31 16:37:48 +0100

    add: - config for new draft-content-store-db-admin node - draft_content_store::db config

---
+++ modules/govuk/manifests/node/s_draft_content_store_db_admin.pp
@@ -0,0 +1,45 @@
+# == Class: govuk_node::s_content_store_db_admin
+#
+# This machine class is used to administer the Draft Content Store
+# PostgreSQL RDS instance.
+#
+# === Parameters
+#
+# [*postgres_host*]
+#   Hostname of the RDS database to use.
+#   Default: undef
+#
+# [*postgres_user*]
+#   The PostgreSQL user to use for administering the database.
+#   Default: undef
+#
+# [*postgres_password*]
+#   The password corresponding to the above `postgres_user`.
+#   Default: undef
+#
+# [*postgres_port*]
+#   The port with which to connect to the `postgres_host`.
+#   Default: '5432'
+#
+class govuk::node::s_draft_content_store_db_admin(
+  $postgres_host        = undef,
+  $postgres_user        = undef,
+  $postgres_password    = undef,
+  $postgres_port        = '5432',
+  $apt_mirror_hostname,
+) {
+  include govuk_env_sync
+  include ::govuk::node::s_base
+
+  # include the common config/tooling required for our app-specific DB admin class
+  class { '::govuk::nodes::postgresql_db_admin':
+    postgres_host       => $postgres_host,
+    postgres_user       => $postgres_user,
+    postgres_password   => $postgres_password,
+    postgres_port       => $postgres_port,
+    apt_mirror_hostname => $apt_mirror_hostname,
+  } ->
+
+  # include all PostgreSQL classes that create databases and users
+  class { '::govuk::apps::draft_content_store::db': }
+}
