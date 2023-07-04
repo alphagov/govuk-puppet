1b8d6b73bb3a51e198c5a05e16e51a44faa7085c
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2023-07-04 13:09:20 +0100

    Update Rylan to absent
    This is the first step in the process of removing from Puppet[1].
    
    1: https://docs.publishing.service.gov.uk/manual/removing-a-user-from-puppet.html#header

--- modules/users/manifests/rylangooch.pp
+++ modules/users/manifests/rylangooch.pp
@@ -1,6 +1,7 @@
 # Creates the user rylangooch
 class users::rylangooch {
   govuk_user { 'rylangooch':
+    ensure   => absent,
     fullname => 'Rylan Gooch',
     email    => 'rylan.gooch@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFCcUCzC0e7KYulF2q07YwePrE2ocqmOwcMD9Xnhdj5E rylan.gooch@digital.cabinet-office.gov.uk',
