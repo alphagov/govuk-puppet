e391d05ab522828ca941e73f80b7a14faae93164
Kevin Dew <kevindew@me.com>
2023-07-20 12:48:24 +0100

    Revert "Add user danmoore to integration"

--- modules/users/manifests/danmoore.pp
+++
@@ -1,8 +0,0 @@
-# Creates the danmoore user
-class users::danmoore {
-  govuk_user { 'danmoore':
-    fullname => 'Dan Moore',
-    email    => 'dan.moore@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF9exfPUcNvwA72BF+dRQ0BncR9KMzbXjSZY4BQnYg04 dan.moore@digital.cabinet-office.gov.uk',
-  }
-}
