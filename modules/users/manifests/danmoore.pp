02f022fc14500e6403ead46d9a2dbbede5d6ebfe
Kevin Dew <kevindew@me.com>
2023-07-20 13:04:45 +0100

    Remove Dan Moore user
    He has left GDS, potentially temporarily but we're still following the
    off-boarding process.

--- modules/users/manifests/danmoore.pp
+++ modules/users/manifests/danmoore.pp
@@ -1,6 +1,7 @@
 # Creates the danmoore user
 class users::danmoore {
   govuk_user { 'danmoore':
+    ensure   => absent,
     fullname => 'Dan Moore',
     email    => 'dan.moore@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF9exfPUcNvwA72BF+dRQ0BncR9KMzbXjSZY4BQnYg04 dan.moore@digital.cabinet-office.gov.uk',
