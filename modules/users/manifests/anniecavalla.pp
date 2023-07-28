13c6eaa0b4c1f1cce5cac57e7d1ea6664bdf7c1b
Annie Cavalla <annie.cavalla@digital.cabinet-office.gov.uk>
2023-07-28 14:03:46 +0100

    Added anniecavalla to users::usernames in integration yaml for environment access

---
+++ modules/users/manifests/anniecavalla.pp
@@ -0,0 +1,8 @@
+# Creates the anniecavalla user
+class users::<anniecavalla> {
+  govuk_user { '<anniecavalla>':
+    fullname => 'annie cavalla',
+    email    => 'annie.cavalla@digital.cabinet-office.gov.uk',
+    ssh_key  => '<ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMrsoo8hap+Ei8ISjfumiAGLapMhk1A0XHLACKKg8tMz annie.cavalla@digital.cabinet-office.gov.uk>',
+  }
+}
