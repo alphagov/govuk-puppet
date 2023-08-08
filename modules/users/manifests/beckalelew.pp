7f0d10a985b1733a0bb57e6abb54bc9464444354
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-08-08 12:39:19 +0100

    Mark Becka Lelew as absent

--- modules/users/manifests/beckalelew.pp
+++ modules/users/manifests/beckalelew.pp
@@ -4,5 +4,6 @@ class users::beckalelew {
     fullname => 'Becka Lelew',
     email    => 'becka.lelew@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOUzfoH86Pby61x10zbeYEEfupbDslZQHSbultbZWssn becka.lelew@digital.cabinet-office.gov.uk',
+    ensure   => absent,
   }
 }
