4f9648f365eb9465f9da4ab433fdd9524f00245b
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2023-08-09 14:35:31 +0100

    Add Jessy Yeh to integration

---
+++ modules/users/manifests/jessyyeh.pp
@@ -0,0 +1,8 @@
+# Creates the jessyyeh user
+class users::jessyyeh {
+  govuk_user { 'jessyyeh':
+    fullname => 'Jessy Yeh',
+    email    => 'jessy.yeh@digital.cabinet-office.gov.uk',
+    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXMyN7oCusE0sGGDVvDzm6HwU9+mciIfTUskYp53edM jessy.yeh@digital.cabinet-office.gov.uk',
+  }
+}
