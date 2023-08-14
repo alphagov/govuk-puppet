c12453f5bab11c66c67d6eeba5b2244b7afda4ca
Tuomas Nylund <tuomas.nylund@thoughtworks.com>
2023-08-14 14:15:58 +0100

    Remove Neamah as she is on a long leave and doesn't need access anymore

--- modules/users/manifests/neamahalselwi.pp
+++
@@ -1,9 +0,0 @@
-# Creates the user neamahalselwi
-class users::neamahalselwi {
-  govuk_user { 'neamahalselwi':
-    ensure   => absent,
-    fullname => 'Neamah Al Selwi',
-    email    => 'neamah.alselwi@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJF4DKX97g/MgaesAf5s5ZAVTg5wHSvpnwd/RkV5qTY/ neamah.alselwi@digital.cabinet-office.gov.uk',
-  }
-}
