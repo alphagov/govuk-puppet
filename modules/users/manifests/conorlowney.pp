1c33a783ee50ee680b84aaafb8fdb1c1bf44af60
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2023-08-16 10:34:12 +0100

    Remove conorlowney user
    Add `ensure => absent` to Conor Lowney's entry
    
    He has left GDS

--- modules/users/manifests/conorlowney.pp
+++ modules/users/manifests/conorlowney.pp
@@ -1,6 +1,7 @@
 # Creates the user conorlowney
 class users::conorlowney {
   govuk_user { 'conorlowney':
+    ensure   => absent,
     fullname => 'Conor Lowney',
     email    => 'conor.lowney@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTX8PqeNjz8MA4E/kJ5mJBTVqjGd3mKBMknX3jm9pFP conor.lowney@digital.cabinet-office.gov.uk',
