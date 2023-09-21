5fa935574662ed581594d73955cb91ec0f60db32
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2023-09-21 14:47:30 +0100

    Remove Kelvin, part 1
    This will remove Kelvin's account from hosts managed by Puppet.
    
    Part 2 will follow, in which the configuration manifest for Kelvin's
    account will be removed.

--- modules/users/manifests/kelvingan.pp
+++ modules/users/manifests/kelvingan.pp
@@ -1,6 +1,7 @@
 # Create the kelvingan user
 class users::kelvingan {
   govuk_user { 'kelvingan':
+    ensure   => absent,
     fullname => 'Kelvin Gan',
     email    => 'kelvin.gan@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCUw2VAu3XgWepMdarEvhA/IPdtUme14JhbwBD44qym kelvin.gan@digital.cabinet-office.gov.uk',
