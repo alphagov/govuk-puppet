a20eb524d5188be2a7d7d4c2ce1096336eb63314
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2023-09-21 15:06:59 +0100

    Remove Kelvin, part 2
    This removes the configuration manifest for Kelvin's account, and
    removes them from integration users.
    
    As per this guide: https://docs.publishing.service.gov.uk/manual/removing-a-user-from-puppet.html

--- modules/users/manifests/kelvingan.pp
+++
@@ -1,9 +0,0 @@
-# Create the kelvingan user
-class users::kelvingan {
-  govuk_user { 'kelvingan':
-    ensure   => absent,
-    fullname => 'Kelvin Gan',
-    email    => 'kelvin.gan@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCUw2VAu3XgWepMdarEvhA/IPdtUme14JhbwBD44qym kelvin.gan@digital.cabinet-office.gov.uk',
-  }
-}
