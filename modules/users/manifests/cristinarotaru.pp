533e730e225e1aec49ee3141fe49174ee049b6e2
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2023-10-12 15:46:42 +0100

    Remove Cristina rotaru pt1

--- modules/users/manifests/cristinarotaru.pp
+++ modules/users/manifests/cristinarotaru.pp
@@ -1,6 +1,7 @@
 # Creates the cristinarotaru user
 class users::cristinarotaru {
   govuk_user { 'cristinarotaru':
+    ensure   => absent,
     fullname => 'Cristina Rotaru',
     email    => 'cristina.rotaru@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBmt64J19ewSIISNNWlyV7C9WUjQNyog6awF6QZ0xNSZ cristina.rotaru@digital.cabinet-office.gov.uk',
