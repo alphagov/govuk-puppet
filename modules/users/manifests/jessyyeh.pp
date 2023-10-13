bd63f6bb422499380594b136bda779a849f6bad3
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2023-10-13 12:42:21 +0100

    Remove Jessy Yeh pt2

--- modules/users/manifests/jessyyeh.pp
+++
@@ -1,9 +0,0 @@
-# Creates the jessyyeh user
-class users::jessyyeh {
-  govuk_user { 'jessyyeh':
-    ensure   => absent,
-    fullname => 'Jessy Yeh',
-    email    => 'jessy.yeh@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXMyN7oCusE0sGGDVvDzm6HwU9+mciIfTUskYp53edM jessy.yeh@digital.cabinet-office.gov.uk',
-  }
-}
