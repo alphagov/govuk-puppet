5aff8c22aa208490c977ed1cf0f6d053aff9a040
Sean Rankine <sean.rankine@me.com>
2023-10-16 13:44:13 +0100

    Remove govuk_crawler_worker and seed_crawler
    These process are responsible for crawling and scraping gov.uk and
    populating the S3 bucket used to serve the mirrors. The have been
    replaced by a K8s cronjob called govuk-mirror-sync in the the
    govuk-helm-charts repo. The repository that contains the new application
    that crawls and scrapes the site is found at govuk-mirror.

--- modules/govuk_ci/manifests/agent/rabbitmq.pp
+++ modules/govuk_ci/manifests/agent/rabbitmq.pp
@@ -8,11 +8,6 @@ class govuk_ci::agent::rabbitmq {
   rabbitmq_user {
     'email_alert_service_test':
       password => 'email_alert_service_test';
-    'govuk_seed_crawler':
-      password => 'govuk_seed_crawler';
-    'govuk_crawler_worker':
-      password => 'govuk_crawler_worker',
-      tags     => ['monitoring'];
     'publishing_api':
       password => 'publishing_api';
   }
@@ -22,14 +17,6 @@ class govuk_ci::agent::rabbitmq {
       configure_permission => '^(amq\.gen.*|email_alert_service_published_documents_test_exchange|email_alert_service_published_documents_test_queue)$',
       read_permission      => '^(amq\.gen.*|email_alert_service_published_documents_test_exchange|email_alert_service_published_documents_test_queue)$',
       write_permission     => '^(amq\.gen.*|email_alert_service_published_documents_test_exchange|email_alert_service_published_documents_test_queue)$';
-    'govuk_seed_crawler@/':
-      configure_permission => '^govuk_seed_crawler.*',
-      read_permission      => '^govuk_seed_crawler.*',
-      write_permission     => '^govuk_seed_crawler.*';
-    'govuk_crawler_worker@/':
-      configure_permission => '^govuk_crawler_worker.*',
-      read_permission      => '^govuk_crawler_worker.*',
-      write_permission     => '^govuk_crawler_worker.*';
     'publishing_api@/':
       configure_permission => '^amq\.gen.*$',
       read_permission      => '^(amq\.gen.*|published_documents_test)$',
