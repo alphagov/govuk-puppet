a8543c4c3a25774ae8254d910c5e9647522f6792
davidtrussler <mail@davidtrussler.net>
2023-11-08 13:07:25 +0000

    Removes user - removes manifest file - removes from Integration Users

--- modules/users/manifests/kylesimmons.pp
+++
@@ -1,9 +0,0 @@
-# Creates the kylesimmons user
-class users::kylesimmons {
-  govuk_user { 'kylesimmons':
-    ensure   => absent,
-    fullname => 'Kyle Simmons',
-    email    => 'kyle.simmons@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKz99jgxT80k+NOG0WBsO7yurQnPUe7QHnUz+F9AzqIu kyle.simmons@digital.cabinet-office.gov.uk',
-  }
-}
