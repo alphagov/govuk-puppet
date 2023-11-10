21fbb1eb8d58b98ba30cfc4a7db7926f24998618
davidtrussler <mail@davidtrussler.net>
2023-11-10 13:54:29 +0000

    Removes user from Puppet - Removes the user manifest file - Removes the user from Integration users

--- modules/users/manifests/sivasubramanian.pp
+++
@@ -1,9 +0,0 @@
-# Creates the sivasubramanian user
-class users::sivasubramanian {
-  govuk_user { 'sivasubramanian':
-    ensure   => absent,
-    fullname => 'Siva Subramanian',
-    email    => 'siva.subramanian@digital.cabinet-office.gov.uk',
-    ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBzg5BFA6p4HlZQs0y9jEv7Y+G3bCbRjfCObrKEImn9H siva.subramanian@thoughtworks.com',
-  }
-}
