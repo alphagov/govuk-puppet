f8fd83da37ed86b7a38a1773afc7ed1ba93e24f8
Mahmud Hussain <mahmud.hussain@digital.cabinet-office.gov.uk>
2023-12-05 10:28:07 +0000

    Remove Marc Pomfret

--- modules/users/manifests/marcpomfret.pp
+++ modules/users/manifests/marcpomfret.pp
@@ -1,6 +1,7 @@
 # Creates the marcpomfret user
 class users::marcpomfret {
   govuk_user { 'marcpomfret':
+    ensure   => absent,
     fullname => 'Marc Pomfret',
     email    => 'marc.pomfret@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHF28IbBpZDTgLtsmW0N55XbIu/gAm8NYKzwBsNx1XAy marc.pomfret@digital.cabinet-office.gov.uk',
