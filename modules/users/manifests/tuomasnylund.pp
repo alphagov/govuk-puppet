8585fdee279e76b0e90332d9f7aef627b190e00e
Patrick Cartlidge <patrickcartlidge@gmail.com>
2023-12-18 14:27:40 +0000

    Make Tuomas Nylund absent

--- modules/users/manifests/tuomasnylund.pp
+++ modules/users/manifests/tuomasnylund.pp
@@ -1,6 +1,7 @@
 # Creates the tuomasnylund user
 class users::tuomasnylund {
   govuk_user { 'tuomasnylund':
+    ensure   => 'absent',
     fullname => 'Tuomas Nylund',
     email    => 'tuomas.nylund@digital.cabinet-office.gov.uk',
     ssh_key  => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM61TGXGjps7QwKoM2Zlu/Y2O9hzzE971yfdP0hckx+R tuomas.nylund@digital.cabinet-office.gov.uk',
